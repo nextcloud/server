d672bb88623974aeb1f22e62c7894e8365f4d5e9
CharlyCoste <contact@ccoste.fr>
2010-03-14 00:46:28 +0100

    Adding xml namespace

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html><head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 <title>ownCloud</title>
 <link rel="stylesheet" type="text/css" href="/css/default.css" />
 </head>
