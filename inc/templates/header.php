23276de922b9527da4b933a92eb186f8a7d35445
CharlyCoste <contact@ccoste.fr>
2010-03-14 00:43:47 +0100

    Adding xhtml 1.0 strict doctype

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html><head>
 <title>ownCloud</title>
 <link rel="stylesheet" type="text/css" href="/css/default.css" />
