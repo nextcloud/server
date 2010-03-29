2a75c0d04495c5f05fdaaebf94d38ea6d0a3f062
Frank Karlitschek <frank@dev.(none)>
2010-03-29 16:21:00 +0200

    merge Adityas improvements

--- index.php
+++ index.php
@@ -28,14 +28,16 @@ if(isset($_GET['dir'])) $dir=$_GET['dir']; else $dir='';
 if(isset($_GET['file'])) {
 
   OC_FILES::get($dir,$_GET['file']);
-
+OC_FILES::get($dir,$_GET['file']);
+OC_FILES::get($dir,$_GET['file']);
+echo('heya');
 }else{
 
   OC_UTIL::addscript('js/ajax.js');
   OC_UTIL::showheader();
 
   OC_FILES::showbrowser($CONFIG_DATADIRECTORY,$dir);
-
+echo('hi');
   echo('<br /><br /><p class="hint">Hint: Mount it via webdav like this: <a href="webdav://'.$_SERVER["HTTP_HOST"].$WEBROOT.'/webdav/owncloud.php">webdav://'.$_SERVER["HTTP_HOST"].$WEBROOT.'/webdav/owncloud.php</a></p>');
 
   OC_UTIL::showfooter();
