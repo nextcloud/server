d15464a0d8d9c2e526bd5df70994df66b191bb87
Robin <robin@Amaya.(none)>
2010-04-28 13:59:01 +0200

    clear default config

--- config/config.php
+++ config/config.php
@@ -1,11 +1,3 @@
 <?php 
-$CONFIG_INSTALLED=true;
-$CONFIG_DATADIRECTORY='/var/www/owncloud/owncloud/data';
-$CONFIG_HTTPFORCESSL=false;
-$CONFIG_DATEFORMAT='j M Y G:i';
-$CONFIG_DBTYPE='mysql';
-$CONFIG_DBNAME='owncloud';
-$CONFIG_DBHOST='localhost';
-$CONFIG_DBUSER='owncloud';
-$CONFIG_DBPASSWORD='gehijm';
+
 ?> 
