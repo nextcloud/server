ccdb21958eec6026d87a547a9c87aaf9f55159f4
Robin Appelman <icewind1991@gmail.com>
2010-07-28 00:10:30 +0200

    use = instead of LIKE when selecting private attributes

--- inc/lib_ocs.php
+++ inc/lib_ocs.php
@@ -450,7 +450,7 @@ class OC_OCS {
     if (!trim($key)) {
         $result = OC_DB::select("select key,value,timestamp from {$CONFIG_DBTABLEPREFIX}privatedata order by timestamp desc");
     } else {
-        $result = OC_DB::select("select key,value,timestamp from {$CONFIG_DBTABLEPREFIX}privatedata where key like'% ".addslashes($key)."%' order by timestamp desc");
+        $result = OC_DB::select("select key,value,timestamp from {$CONFIG_DBTABLEPREFIX}privatedata where key ='".addslashes($key)."' order by timestamp desc");
     }
     $itemscount=count($result);
 
@@ -480,7 +480,6 @@ class OC_OCS {
 
     $result=OC_DB::select("select count(*) as co from {$CONFIG_DBTABLEPREFIX}privatedata where key = '".addslashes($key)."'");
     $totalcount=$result[0]['co'];
-    OC_DB::free_result($result);
 
     if ($totalcount != 0) {
         $result = OC_DB::query("update {$CONFIG_DBTABLEPREFIX}privatedata set value='".addslashes($value)."', timestamp = datetime('now') where key = '".addslashes($key)."'");
