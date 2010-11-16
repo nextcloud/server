e18898dfe140b091840107aa47ac4ece7a68edd6
Robin Appelman <icewind1991@gmail.com>
2010-11-16 21:20:23 +0100

    fix warning message that sometimes broke webdav

--- inc/MDB2/Driver/sqlite.php
+++ inc/MDB2/Driver/sqlite.php
@@ -926,7 +926,7 @@ class MDB2_Result_sqlite extends MDB2_Result_Common
             if ($object_class == 'stdClass') {
                 $row = (object) $row;
             } else {
-                $row = &new $object_class($row);
+                $row = new $object_class($row);
             }
         }
         ++$this->rownum;
