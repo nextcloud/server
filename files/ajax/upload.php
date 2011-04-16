d376b4c90ab76785ad98ef0d888733543b06043e
Arthur Schiwon <blizzz@arthur-schiwon.de>
2011-04-16 23:18:06 +0200

    i heard, we start to commit all files

---
+++ files/ajax/upload.php
@@ -0,0 +1,32 @@
+<?php
+
+// Init owncloud
+require_once('../../lib/base.php');
+
+// We send json data
+// header( "Content-Type: application/json" );
+// Firefox and Konqueror tries to download application/json for me.  --Arthur
+header( "Content-Type: text/plain" );
+
+// Check if we are a user
+if( !OC_USER::isLoggedIn()){
+	echo json_encode( array( "status" => "error", "data" => "Authentication error" ));
+	exit();
+}
+
+$fileName=$_FILES['file']['name'];
+$source=$_FILES['file']['tmp_name'];
+$dir = $_POST['dir'];
+if(!empty($dir)) $dir .= '/';
+$target='/' . stripslashes($dir) . $fileName;
+if(isset($_SESSION['username'])
+and $_SESSION['username'] and strpos($dir,'..') === false){
+	if(OC_FILESYSTEM::fromTmpFile($source,$target)){
+		echo json_encode(array( "status" => "success"));
+		exit();
+	}
+}
+
+echo json_encode(array( "status" => "error"));
+
+?>
