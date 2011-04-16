2df6e15fcaba3387ff6ed715a39807e3ca898667
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-04-16 23:29:54 +0200

    this is now HTML5

--- templates/layout.guest.php
+++ templates/layout.guest.php
@@ -1,10 +1,5 @@
-<?php
-/*
- * Template for guest pages
- */
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
 	<head>
 		<title>ownCloud</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
