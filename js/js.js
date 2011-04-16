3263b80ea94a2d3c8a7d68d8190a93db198e2203
Robin Appelman <icewind1991@gmail.com>
2011-04-17 00:45:05 +0200

    first-run wizzard wip

--- js/js.js
+++ js/js.js
@@ -9,4 +9,22 @@ $(document).ready(function() {
         $('#user_menu').slideToggle(250);
         return false;
     });
+	
+	//hide the advanced config
+	$('#advanced_options').hide();
+	$('#use_mysql').hide();
+	
+	// Sets advanced_options link behaviour :
+	$('#advanced_options_link').click(function() {
+		$('#advanced').toggleClass('userLinkOn');
+		$('#advanced_options').slideToggle(250);
+		return false;
+	});
+	
+	$('#mysql').click(function() {
+		$('#use_mysql').slideDown(250);
+	});
+	$('#sqlite').click(function() {
+		$('#use_mysql').slideUp(250);
+	});
 });
--- templates/error.php
+++ templates/error.php
@@ -9,7 +9,7 @@
 		<?php foreach($_["errors"] as $error):?>
 			<li class='error'>
 				<?php echo $error['error'] ?><br/>
-				<p class='hint'><?php echo $error['hint'] ?></p>
+				<p class='hint'><?php if(isset($error['hint']))echo $error['hint'] ?></p>
 			</li>
 		<?php endforeach ?>
 	</ul>
