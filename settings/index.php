542a0a251874f69f9cf2cf9472998dc0a04880c8
Robin Appelman <icewind1991@gmail.com>
2011-04-17 13:47:45 +0200

    show real data in account information

--- settings/index.php
+++ settings/index.php
@@ -8,7 +8,14 @@ if( !OC_USER::isLoggedIn()){
 }
 
 
-$tmpl = new OC_TEMPLATE( "settings", "index", "admin" );
+$tmpl = new OC_TEMPLATE( "settings", "index", "admin");
+$used=OC_FILESYSTEM::filesize('/');
+$free=disk_free_space(OC_CONFIG::getValue('datadirectory'));
+$total=$free+$used;
+$relative=round(($used/$total)*100);
+$tmpl->assign('usage',OC_HELPER::humanFileSize($used));
+$tmpl->assign('total_space',OC_HELPER::humanFileSize($total));
+$tmpl->assign('usage_relative',$relative);
 $tmpl->printPage();
 
 ?>
