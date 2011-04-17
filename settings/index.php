225ad85e909b3f65d8eda57ef13001b0000e4e76
Robin Appelman <icewind1991@gmail.com>
2011-04-17 16:40:44 +0200

    add free_space to OC_FILESYSTEM

--- settings/index.php
+++ settings/index.php
@@ -10,7 +10,7 @@ if( !OC_USER::isLoggedIn()){
 
 $tmpl = new OC_TEMPLATE( "settings", "index", "admin");
 $used=OC_FILESYSTEM::filesize('/');
-$free=disk_free_space(OC_CONFIG::getValue('datadirectory'));
+$free=OC_FILESYSTEM::free_space();
 $total=$free+$used;
 $relative=round(($used/$total)*100);
 $tmpl->assign('usage',OC_HELPER::humanFileSize($used));
