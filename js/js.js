398d4de0f51ac6326c92e909686258aa605a5b82
Robin Appelman <icewind1991@gmail.com>
2011-05-19 21:19:37 +0200

    fix some bugs in the first run dialog, and hide advanced setting initially.

--- js/js.js
+++ js/js.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {	
 	// Hide the MySQL config div if needed :
-	if(!$('#mysql').is(':checked')) {
+	if(!$('#mysql').is(':checked') && $('#hasSQLite').val()=='true') {
 		$('#use_mysql').hide();
 	}
-
+	
+	$('#datadirField').hide(250);
+	if($('#hasSQLite').val()=='true'){
+		$('#databaseField').hide(250);
+	}
+	
 	$('#sqlite').click(function() {
 		$('#use_mysql').slideUp(250);
 	});
 	
 	$('#mysql').click(function() {
 		$('#use_mysql').slideDown(250);
+	});
+	
+	$('#showAdvanced').click(function() {
+		$('#datadirField').slideToggle(250);
+		if($('#hasSQLite').val()=='true'){
+			$('#databaseField').slideToggle(250);
+		}
 	});
 });
--- templates/installation.php
+++ templates/installation.php
@@ -4,31 +4,41 @@
 		<input type="hidden" name="install" value="true" />
 		<p class="intro">
 			Welcome to <strong>ownCloud</strong>, your personnal cloud.<br />
-			To finish the installation, please follow the 3 remaining steps below.
+			To finish the installation, please follow the steps below.
 		</p>
 
 		<?php if(count($_['errors']) > 0): ?>
 		<ul class="errors">
 			<?php foreach($_['errors'] as $err): ?>
-			<li><?php print $err; ?></li>
+			<li>
+				<?php if(is_array($err)):?>
+					<?php print $err['error']; ?>
+					<p class='hint'><?php print $err['hint']; ?></p>
+				<?php else: ?>
+					<?php print $err; ?>
+				<?php endif; ?>
+			</li>
 			<?php endforeach; ?>
 		</ul>
 		<?php endif; ?>
 
 		<fieldset>
-			<legend><strong>STEP 1</strong> : Create an <strong>admin account.</strong></legend>
+			<legend>Create an <strong>admin account.</strong></legend>
 			<p><label for="adminlogin">Login :</label><input type="text" name="adminlogin" id="adminlogin" value="<?php print OC_HELPER::init_var('adminlogin'); ?>" /></p>
 			<p><label for="adminpass">Password :</label><input type="password" name="adminpass" id="adminpass" value="<?php print OC_HELPER::init_var('adminpass'); ?>" /></p>
         </fieldset>
         
-        <fieldset>
-			<legend><strong>STEP 2</strong> : Set where to store the data.</legend>
+        <a id='showAdvanced'>Advanced <img src='<?php echo OC_HELPER::imagePath('','drop-arrow.png'); ?>'></img></a>
+        
+        <fieldset id='datadirField'>
+			<legend>Set where to store the data.</legend>
 			<p><label for="directory">Data directory :</label><input type="text" name="directory" id="directory" value="<?php print OC_HELPER::init_var('directory', $_['directory']); ?>" /></p>
 		</fieldset>
 		
-		<fieldset>
-			<legend><strong>STEP 3</strong> : Configure your database.</legend>
+		<fieldset id='databaseField'>
+			<legend>Configure your database.</legend>
 			<?php if($_['hasSQLite']): ?>
+			<input type='hidden' id='hasSQLite' value='true'/>
 			<?php if(!$_['hasMySQL']): ?>
 			<p>I will use a SQLite database. You have nothing to do !</p>
 			<input type="hidden" id="dbtype" name="dbtype" value="sqlite" />
@@ -38,6 +48,7 @@
 			<?php endif; ?>
 
 			<?php if($_['hasMySQL']): ?>
+			<input type='hidden' id='hasMySQL' value='true'/>
 			<?php if(!$_['hasSQLite']): ?>
 			<p>I will use a MySQL database.</p>
 			<input type="hidden" id="dbtype" name="dbtype" value="mysql" />
