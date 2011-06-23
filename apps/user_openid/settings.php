bd4aec2c4c979cf88fd2e430b52eb22b34b56dba
Robin Appelman <icewind1991@gmail.com>
2011-06-23 17:04:32 +0200

    Experimental OpenID user backend and server.
    Currently all users can set one OpenID identity that allows access to the account.
    
    The identity url's for the server is owncloud/apps/user_openid/user.php/$username

---
+++ apps/user_openid/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('../../lib/base.php');
+require( 'template.php' );
+if( !OC_USER::isLoggedIn()){
+    header( "Location: ".OC_HELPER::linkTo( "index.php" ));
+    exit();
+}
+
+if(isset($_POST['input_identity'])){
+	OC_PREFERENCES::setValue(OC_USER::getUser(),'user_openid','identity',$_POST['input_identity']);
+}
+
+OC_APP::setActiveNavigationEntry( "user_openid_settings" );
+
+$identity=OC_PREFERENCES::getValue(OC_USER::getUser(),'user_openid','identity','');
+
+$tmpl = new OC_TEMPLATE( "user_openid", "settings", "admin");
+$tmpl->assign('identity',$identity);
+$tmpl->assign('user',OC_USER::getUser());
+
+$tmpl->printPage();
+
+?>
---
+++ apps/user_openid/user.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+* ownCloud
+*
+* @author Robin Appelman
+* @copyright 2011 Robin Appelman icewind1991@gmail.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+$USERNAME=substr($_SERVER["REQUEST_URI"],strpos($_SERVER["REQUEST_URI"],'.php/')+5);
+if(strpos($USERNAME,'?')){
+	$USERNAME=substr($USERNAME,0,strpos($USERNAME,'?'));
+}
+
+require_once '../../lib/base.php';
+
+require_once 'phpmyid.php';
+
+
+?>
