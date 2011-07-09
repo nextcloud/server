5c671c995cfb68866608e95b28e5be49087eb910
Hans Bakker <hansmbakker@gmail.com>
2011-07-09 21:44:50 +0200

    finish refactoring of files admin page

--- files/templates/admin.php
+++ files/templates/admin.php
@@ -1,21 +1,13 @@
-<script type="text/javascript">
-function switchPublicFolder()
-{
-	var publicEnable = $('#publicEnable').is(':checked');
-	var sharingaimGroup = $('input:radio[name=sharingaim]');
-	$.each(sharingaimGroup, function(index, sharingaimItem) {
-		sharingaimItem.disabled = !publicEnable;
-	});
-}
-</script>
+<?php OC_UTIL::addScript('files','admin'); ?>
+
 <form name="filesForm" action='#' method='post'>
 	<?php if($_['htaccessWorking']):?>
 		<label for="maxUploadSize"><?php echo $l->t( 'Maximum upload size' ); ?> </label><input name='maxUploadSize' id="maxUploadSize" value='<?php echo $_['uploadMaxFilesize'] ?>'/><br/>
 	<?php endif;?>
-	<input type="checkbox" name="publicEnable" id="publicEnable" onClick="switchPublicFolder()" /><label for="publicEnable"> <?php echo $l->t( 'Allow public folders' ); ?></label><br>
+	<input type="checkbox" name="publicEnable" id="publicEnable" /><label for="publicEnable"> <?php echo $l->t( 'Allow public folders' ); ?></label><br>
 
 	<div style="padding-left: 20px">
-		<input type="radio" name="sharingaim" id="separated" checked="checked" /><label for="separated"> <?php echo $l->t( 'separated from webdav storage' ); ?></label><br>
+		<input type="radio" name="sharingaim" id="separated" /><label for="separated"> <?php echo $l->t( 'separated from webdav storage' ); ?></label><br>
 		<input type="radio" name="sharingaim" id="userdecide" /><label for="userdecide"> <?php echo $l->t( 'let the user decide' ); ?></label><br>
 		<input type="radio" name="sharingaim" id="inwebdav" /><label for="inwebdav"> <?php echo $l->t( 'folder "/public" in webdav storage' ); ?></label>
 	</div>
