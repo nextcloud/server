4c0c5b64c2915e765ed4b5009eef2c283fc54024
Robin Appelman <icewind1991@gmail.com>
2011-07-17 16:36:15 +0200

    show a proper error message in help when it can't get the data from apps.owncloud.com

--- help/templates/index.php
+++ help/templates/index.php
@@ -1,23 +1,27 @@
 
 <h1><?php echo $l->t( 'Questions and Answers' ); ?></h1>
 
-<table cellspacing="0" width="100%">
-	<tbody>
-		<?php foreach($_["kbe"] as $kb): ?>
-			<tr>
-				<td width="1"><?php if($kb["preview1"] <> "") { echo('<img class="preview" border="0" src="'.$kb["preview1"].'" />'); } ?> </a></td>
-				<td class="name"><?php echo $kb["name"]; ?><br /><?php  echo('<span class="type">'.$kb['description'].'</span>'); ?><br />
-				<?php if($kb['answer']<>'') echo('<br /><span class="type"><b>Answer:</b></span><br /><span class="type">'.$kb['answer'].'</span>');?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
-<?php
-	$url=OC_HELPER::linkTo( "help", "index.php" ).'?page=';
-	$pageNavi=OC_UTIL::getPageNavi($_['pagecount'],$_['page'],$url);
-	$pageNavi->printPage();
-?>
-<a target="_blank" class="prettybutton" href="http://apps.owncloud.com/knowledgebase/editquestion.php?action=new"><?php echo $l->t( 'ASK A QUESTION' ); ?></a>
+<?php if(is_null($_["kbe"])):?>
+	Can't connect to Q&amp;A database
+<?php else:?>
+	<table cellspacing="0" width="100%">
+		<tbody>
+			<?php foreach($_["kbe"] as $kb): ?>
+				<tr>
+					<td width="1"><?php if($kb["preview1"] <> "") { echo('<img class="preview" border="0" src="'.$kb["preview1"].'" />'); } ?> </a></td>
+					<td class="name"><?php echo $kb["name"]; ?><br /><?php  echo('<span class="type">'.$kb['description'].'</span>'); ?><br />
+					<?php if($kb['answer']<>'') echo('<br /><span class="type"><b>Answer:</b></span><br /><span class="type">'.$kb['answer'].'</span>');?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<?php
+		$url=OC_HELPER::linkTo( "help", "index.php" ).'?page=';
+		$pageNavi=OC_UTIL::getPageNavi($_['pagecount'],$_['page'],$url);
+		$pageNavi->printPage();
+	?>
+	<a target="_blank" class="prettybutton" href="http://apps.owncloud.com/knowledgebase/editquestion.php?action=new"><?php echo $l->t( 'ASK A QUESTION' ); ?></a>
+<?php endif;?>
 
 
