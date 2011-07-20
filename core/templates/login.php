3fab6016f9a9b9648626e5ebdaa4ea25d5104443
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-07-20 16:11:39 +0200

    moved Remember login below password field

--- core/templates/login.php
+++ core/templates/login.php
@@ -7,12 +7,12 @@
 			<?php endif; ?>
 			<?php if(empty($_["username"])){?>
 			<input type="text" name="user" id="user" value="" autofocus />
-			<input type="checkbox" name="remember_login"/> <?php echo $l->t('Remember login'); ?>
 			<input type="password" name="password" id="password" value="" />
+			<input type="checkbox" name="remember_login"/> <?php echo $l->t('Remember login'); ?>
 			<?php }else{ ?>
 			<input type="text" name="user" id="user" value="<?php echo $_['username']; ?>">
-			<input type="checkbox" name="remember_login" checked /> <?php echo $l->t('Remember login'); ?>
 			<input type="password" name="password" id="password" value="" autofocus />
+			<input type="checkbox" name="remember_login" checked /> <?php echo $l->t('Remember login'); ?>
 			<?php } ?>
 			<input type="submit" value="Log in" />
 		</fieldset>
