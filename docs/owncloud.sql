0fa41e9171d8b96ccf871a8c3d200db8f8ea7720
Jakob Sack <kde@jakobsack.de>
2011-07-22 22:47:44 +0200

    Update docs, db scheme

--- docs/owncloud.sql
+++ docs/owncloud.sql
@@ -1,28 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.7deb5
+-- version 3.4.3.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 16. April 2011 um 10:40
--- Server Version: 5.1.49
--- PHP-Version: 5.3.3-7+squeeze1
+-- Generation Time: Jul 22, 2011 at 10:38 PM
+-- Server version: 5.1.58
+-- PHP Version: 5.3.6-13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET time_zone = "+00:00";
 
 --
--- Datenbank: `owncloud`
+-- Database: `owncloud`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `appconfig`
+-- Table structure for table `appconfig`
 --
 
 CREATE TABLE IF NOT EXISTS `appconfig` (
@@ -31,15 +26,22 @@ CREATE TABLE IF NOT EXISTS `appconfig` (
   `configvalue` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Daten für Tabelle `appconfig`
+-- Table structure for table `foldersize`
 --
 
+CREATE TABLE IF NOT EXISTS `foldersize` (
+  `path` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
+  `size` int(11) NOT NULL,
+  KEY `PATH_INDEX` (`path`(333))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `groups`
+-- Table structure for table `groups`
 --
 
 CREATE TABLE IF NOT EXISTS `groups` (
@@ -47,17 +49,10 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`gid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Daten für Tabelle `groups`
---
-
-INSERT INTO `groups` (`gid`) VALUES
-('admin');
-
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `group_user`
+-- Table structure for table `group_user`
 --
 
 CREATE TABLE IF NOT EXISTS `group_user` (
@@ -65,43 +60,29 @@ CREATE TABLE IF NOT EXISTS `group_user` (
   `uid` varchar(64) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Daten für Tabelle `group_user`
---
-
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `locks`
+-- Table structure for table `locks`
 --
 
 CREATE TABLE IF NOT EXISTS `locks` (
-  `token` varchar(255) NOT NULL DEFAULT '',
-  `path` varchar(200) NOT NULL DEFAULT '',
-  `created` int(11) NOT NULL DEFAULT '0',
-  `modified` int(11) NOT NULL DEFAULT '0',
-  `expires` int(11) NOT NULL DEFAULT '0',
-  `owner` varchar(200) DEFAULT NULL,
-  `recursive` int(11) DEFAULT '0',
-  `writelock` int(11) DEFAULT '0',
-  `exclusivelock` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`token`),
-  UNIQUE KEY `token` (`token`),
-  KEY `path` (`path`),
-  KEY `path_2` (`path`),
-  KEY `path_3` (`path`,`token`),
-  KEY `expires` (`expires`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `locks`
---
-
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `owner` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timeout` int(10) unsigned DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `scope` tinyint(4) DEFAULT NULL,
+  `depth` tinyint(4) DEFAULT NULL,
+  `uri` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `log`
+-- Table structure for table `log`
 --
 
 CREATE TABLE IF NOT EXISTS `log` (
@@ -112,17 +93,12 @@ CREATE TABLE IF NOT EXISTS `log` (
   `action` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `info` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Daten für Tabelle `log`
---
-
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3437 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `preferences`
+-- Table structure for table `preferences`
 --
 
 CREATE TABLE IF NOT EXISTS `preferences` (
@@ -132,53 +108,27 @@ CREATE TABLE IF NOT EXISTS `preferences` (
   `configvalue` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Daten für Tabelle `preferences`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `properties`
+-- Table structure for table `properties`
 --
 
 CREATE TABLE IF NOT EXISTS `properties` (
-  `path` varchar(255) NOT NULL DEFAULT '',
-  `name` varchar(120) NOT NULL DEFAULT '',
-  `ns` varchar(120) NOT NULL DEFAULT 'DAV:',
-  `value` text,
-  PRIMARY KEY (`path`,`name`,`ns`),
-  KEY `path` (`path`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `properties`
---
-
+  `userid` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `propertypath` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `propertyname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `propertyvalue` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Daten für Tabelle `users`
---
-
---
--- Table structure for table `foldersize`
---
-
-CREATE TABLE IF NOT EXISTS `foldersize` (
-  `path` varchar(512) NOT NULL,
-  `size` int(11) NOT NULL,
-  PRIMARY KEY (`path`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
