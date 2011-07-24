5148eb9121e3d19211629ea29ac3ebf70b111ef6
Michael Gapczynski <GapczynskiM@gmail.com>
2011-07-24 16:07:39 -0400

    Include groups in user autocomplete

--- apps/files_sharing/ajax/userautocomplete.php
+++ apps/files_sharing/ajax/userautocomplete.php
@@ -3,8 +3,8 @@ $RUNTIME_NOAPPS = true;
 
 require_once('../../../lib/base.php');
 
-if( !OC_USER::isLoggedIn()){
-	echo json_encode( array( "status" => "error", "data" => array( "message" => "Authentication error" )));
+if (!OC_USER::isLoggedIn()) {
+	echo json_encode(array("status" => "error", "data" => array("message" => "Authentication error")));
 	exit();
 }
 $query = $_GET['term'];
@@ -13,9 +13,15 @@ $query = strtolower($query);
 $users = array();
 $ocusers = OC_USER::getUsers();
 $self = OC_USER::getUser();
+$groups = OC_GROUP::getUserGroups($self);
 foreach ($ocusers as $user) {
 	if ($user != $self && substr(strtolower($user), 0, $length) == $query) {
 		$users[] = (object)array('id' => $user, 'label' => $user, 'name' => $user);
+	}
+}
+foreach ($groups as $group) {
+	if (substr(strtolower($group), 0, $length) == $query) {
+		$users[] = (object)array('id' => $group, 'label' => $group, 'name' => $group);
 	}
 }
 echo json_encode($users);
