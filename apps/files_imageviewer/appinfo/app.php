d6d38ac3fafa2c73e3da09a060ad1ae20949a26c
Robin Appelman <icewind1991@gmail.com>
2011-07-26 21:18:13 +0200

    only load image viewer when in the file browser

--- apps/files_imageviewer/appinfo/app.php
+++ apps/files_imageviewer/appinfo/app.php
@@ -1,6 +1,8 @@
 <?php
 
-OC_UTIL::addScript( 'files_imageviewer', 'lightbox' );
-OC_UTIL::addStyle( 'files_imageviewer', 'lightbox' );
+if(OC_APP::getCurrentApp()=='files'){
+	OC_UTIL::addScript( 'files_imageviewer', 'lightbox' );
+	OC_UTIL::addStyle( 'files_imageviewer', 'lightbox' );
+}
 
 ?>
