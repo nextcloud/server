f1616b0e629ec5fd59a859748e64418862691597
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-07-29 00:45:44 +0200

    implemented relative file size and coloring

--- files/templates/part.list.php
+++ files/templates/part.list.php
@@ -1,6 +1,9 @@
 		<?php foreach($_['files'] as $file):
 			$simple_file_size = simple_file_size($file['size']);
-			$simple_size_color = 200-intval(pow(($file['size']/(1024*1024)),2)); ?>
+			$simple_size_color = 200-intval(pow(($file['size']/(1024*1024)),2));
+			$relative_modified_date = relative_modified_date($file['mtime']);
+			$relative_date_color = round((time()-$file['mtime'])/60/60/24*5); //days ago
+			if($relative_date_color>200) $relative_date_color = 200; ?>
 			<tr data-file="<?php echo $file['name'];?>" data-type="<?php echo ($file['type'] == 'dir')?'dir':'file'?>" data-mime="<?php echo $file['mime']?>" data-size='<?php echo $file['size'];?>'>
 				<td class="filename">
 					<input type="checkbox" />
@@ -15,6 +18,6 @@
 					</a>
 				</td>
 				<td class="filesize" title="<?php echo human_file_size($file['size']); ?>" style="color:rgb(<?php echo $simple_size_color.','.$simple_size_color.','.$simple_size_color ?>)"><?php echo $simple_file_size; ?></td>
-				<td class="date"><span class="modified"><?php echo $file['date']; ?></span></td>
+				<td class="date"><span class="modified" title="<?php echo $file['date']; ?>" style="color:rgb(<?php echo $relative_date_color.','.$relative_date_color.','.$relative_date_color ?>)"><?php echo $relative_modified_date; ?></span></td>
 			</tr>
 		<?php endforeach; ?>
