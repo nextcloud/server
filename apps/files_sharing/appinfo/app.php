45222f34a6ad8e94aa3d5358adf3c8272d78df20
Michael Gapczynski <GapczynskiM@gmail.com>
2011-07-29 12:20:01 -0400

    Make 'Share' a subnavigation entry of of 'Files'

--- apps/files_sharing/appinfo/app.php
+++ apps/files_sharing/appinfo/app.php
@@ -3,8 +3,8 @@
 require_once('apps/files_sharing/lib_share.php');
 
 OC_UTIL::addScript("files_sharing", "share");
-
-OC_APP::addNavigationEntry( array( "id" => "files_sharing_list",
+OC_APP::addNavigationSubEntry("files_index", array(
+  "id" => "files_sharing_list",
   "order" => 10, 
   "href" => OC_HELPER::linkTo( "files_sharing", "list.php" ),
   "name" => "Share", 
