23f051ccd6eb3f6964cd6a61d867e367381dd8d4
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-07-29 02:35:16 +0200

    redesigned search box

--- core/templates/part.searchbox.php
+++ core/templates/part.searchbox.php
@@ -1,4 +1,3 @@
 <form class="searchbox" action="<?php echo $_['searchurl']?>" method="post">
-	<input type="text" name="query" value="<?php if(isset($_POST['query'])){echo $_POST['query'];};?>" class="prettybutton" />
-	<input type="submit" value="<?php echo $l->t( 'Search' ); ?>" class="prettybutton" />
+	<input type="search" name="query" value="<?php if(isset($_POST['query'])){echo $_POST['query'];};?>" class="prettybutton" />
 </form>
