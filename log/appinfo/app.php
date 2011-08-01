e79c827b22135810c3e66b3178c284d96b4232b7
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-01 18:08:11 +0200

    removed log feature because it is a privacy issue

--- log/appinfo/app.php
+++ log/appinfo/app.php
@@ -1,6 +1,6 @@
-<?php
+<?php /*
 
 OC_App::register( array( "order" => 1, "id" => "log", "name" => "Log" ));
 OC_App::addSettingsPage( array( "id" => "log", "order" => 999, "href" => OC_Helper::linkTo( "log", "index.php" ), "name" => "Log", "icon" => OC_Helper::imagePath( "log", "logs.png" )));
 
-?>
+*/ ?>
