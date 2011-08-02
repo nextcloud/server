d71cca0e819780cacf1d9653bf3cf7fb826fda92
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-02 18:58:33 +0200

    first style for sharing dropdown, some html fixes

--- apps/files_sharing/js/share.js
+++ apps/files_sharing/js/share.js
@@ -31,17 +31,17 @@ $(document).ready(function() {
 });
 
 function createShareDialog(filenames, files) {
-	var html = "<div id='dialog' style='position: absolute'>";
+	var html = "<div id='dialog'>";
 	html += "<div id='private'>";
-	html += "<label>Share with <input placeholder='User or Group' id='uid_shared_with' /></label>";
-	html += "<label><input type='checkbox' name='permissions' value='1' /> allow editing</label><br />";
+	html += "<label for='uid_shared_with'>Share with</label><input placeholder='User or Group' id='uid_shared_with' />";
+	html += "<input type='checkbox' name='permissions' id='permissions' value='1' /><label for='permissions'>allow editing</label><br />";
 	html += "<br />";
 	html += "<div id='shared_list'></div>";
 	$.getJSON(OC.linkTo('files_sharing','ajax/getitem.php'), { source: files }, function(users) {
 		var list = "";
 		$.each(users, function(index, row) {
 			list += row.uid_shared_with;
-			list += " <label><input type='checkbox' name='share_private_permissions' value='1' /> allow editing</label><br />";
+			list += "<input type='checkbox' name='share_private_permissions' value='1' /><label>allow editing</label><br />";
 			if (row.permissions > 0) {
 				$('share_private_permissions').prop('checked', true);
 			}
@@ -50,10 +50,10 @@ function createShareDialog(filenames, files) {
 	});
 	html += "</div>";
 	html += "<div id='public'>";
-	html += "<label><input type='checkbox' name='public_link' value='1' /> make public</label>";
-	html += "<label><input type='checkbox' name='public_link_write' value='1' /> writeable</label>";
+	html += "<input type='checkbox' name='public_link' id='public_link' value='1' /><label for='public_link'>make public</label>";
+	html += "<input type='checkbox' name='public_link_write' id='public_link_write' value='1' /><label for='public_link_write'>allow upload</label>";
 	html += "<div id='link'>";
 	html += "</div>";
 	html += "</div>";
-	$(html).appendTo($('tr[data-file="'+filenames+'"] '));
+	$(html).appendTo($('tr[data-file="'+filenames+'"] td.filename'));
 }
