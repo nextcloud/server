d8c86a2ad60db241bad4d7e43c54ef5157d8fdb8
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-03 05:09:42 +0200

    cleaned up settings a bit

--- admin/templates/appsinst.php
+++ admin/templates/appsinst.php
@@ -1,10 +1,3 @@
-<?php
-/*
- * Template for Installed Apps
- */
-?>
-<h1><?php echo $l->t( 'Installed Applications' ); ?></h1>
-
 <table>
 	<thead>
 		<tr>
@@ -20,7 +13,7 @@
 				<td class="name" width="200"><?php echo($app['name']); ?></td>
 				<td class="version"><?php echo($app['version']); ?></td>
 				<td><?php echo($app['author']); ?></td>
-				<td><input x-use="appenablebutton" type='button' value='<?php echo $l->t( $app['enabled'] ? 'enabled' : 'disabled' ); ?>' class='appbutton prettybutton <?php echo( $app['enabled'] ? 'enabled' : 'disabled' ); ?>'/></td>
+				<td><input x-use="appenablebutton" type="button" value="<?php echo $l->t( $app['enabled'] ? 'enabled' : 'disabled' ); ?>" class="appbutton prettybutton <?php echo( $app['enabled'] ? 'enabled' : 'disabled' ); ?>" /></td>
 			</tr>
 		<?php endforeach; ?>
 	</tbody>
