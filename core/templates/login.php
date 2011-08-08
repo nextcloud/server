41a4fa97b8dca0b64c4e0dcb99be201a7ac69c6f
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-08 09:05:16 +0200

    HTML5 form field placeholders

--- core/templates/login.php
+++ core/templates/login.php
@@ -10,12 +10,12 @@
 				<?php echo $l->t( 'Login failed!' ); ?>
 			<?php endif; ?>
 			<?php if(empty($_["username"])){?>
-			<input type="text" name="user" id="user" value="" autofocus />
-			<input type="password" name="password" id="password" value="" />
+			<input type="text" name="user" id="user" placeholder="Username" value="" required autofocus />
+			<input type="password" name="password" id="password" placeholder="Password" value="" required />
 			<input type="checkbox" name="remember_login"/> <?php echo $l->t('Remember login'); ?>
 			<?php }else{ ?>
-			<input type="text" name="user" id="user" value="<?php echo $_['username']; ?>">
-			<input type="password" name="password" id="password" value="" autofocus />
+			<input type="text" name="user" id="user" placeholder="Username" value="<?php echo $_['username']; ?>" required >
+			<input type="password" name="password" id="password" value="" placeholder="Password" required autofocus />
 			<input type="checkbox" name="remember_login" checked /> <?php echo $l->t('Remember login'); ?>
 			<?php } ?>
 			<input type="submit" value="Log in" />
