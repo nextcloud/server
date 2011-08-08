ecd2738e8397e8773c13a8bc4bf4ebfb360442ba
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-08 12:48:43 +0200

    fixed upload button

--- files/templates/index.php
+++ files/templates/index.php
@@ -4,17 +4,17 @@
 	</span>
 	<div class="actions">
 		<form data-upload-id='1' class="file_upload_form" action="ajax/upload.php" method="post" enctype="multipart/form-data" target="file_upload_target_1">
-			<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $_["uploadMaxFilesize"] ?>" id="max_upload">
-			<input type="hidden" class="max_human_file_size" value="(max <?php echo $_["uploadMaxHumanFilesize"]; ?>)">
-			<input type="hidden" name="dir" value="<?php echo $_["dir"] ?>" id="dir">
-			<div class='file_upload_wrapper'>
-				<input class="prettybutton file_upload_filename" value="Upload (max. <?php echo $_["uploadMaxHumanFilesize"];?>)"/>
-				<input class="prettybutton file_upload_start" type="file" name='files[]'/>
-			</div>&nbsp;
+			<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $_['uploadMaxFilesize'] ?>" id="max_upload">
+			<input type="hidden" class="max_human_file_size" value="(max <?php echo $_['uploadMaxHumanFilesize']; ?>)">
+			<input type="hidden" name="dir" value="<?php echo $_['dir'] ?>" id="dir">
+			<div class="file_upload_wrapper">
+				<input type="submit" class="file_upload_filename" value="Upload (max. <?php echo $_['uploadMaxHumanFilesize'];?>)"/>
+				<input class="file_upload_start" type="file" name='files[]'/>
+			</div>
 			<iframe name="file_upload_target_1" class='file_upload_target' src=""></iframe>
 		</form>
 		<form id="file_newfolder_form">
-			<input type="text" class="prettybutton" name="file_newfolder_name" id="file_newfolder_name" value="New Folder" />&nbsp;
+			<input type="text" name="file_newfolder_name" id="file_newfolder_name" value="" placeholder="New Folder" />
 		</form>
 	</div>
 	<div id="file_action_panel">
