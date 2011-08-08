51f74d021f9f302a9393af9ed6bbd75a4af45e97
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-08 09:47:58 +0200

    refactoring CSS part 1, input elements

--- core/templates/login.php
+++ core/templates/login.php
@@ -1,24 +1,22 @@
 <div id="login">
 	<header>
-		<div class='header'>
-			<img src="<?php echo image_path('', 'owncloud-logo-medium-white.png'); ?>" alt="ownCloud" />
-		</div>
+		<img src="<?php echo image_path('', 'owncloud-logo-medium-white.png'); ?>" alt="ownCloud" />
 	</header>
-	<form action="index.php" method="post" id="login_form">
+	<form action="index.php" method="post">
 		<fieldset>
 			<?php if($_['error']): ?>
 				<?php echo $l->t( 'Login failed!' ); ?>
 			<?php endif; ?>
 			<?php if(empty($_["username"])){?>
-			<input type="text" name="user" id="user" placeholder="Username" value="" required autofocus />
+			<input type="text" name="user" id="user" placeholder="Username" value="" autocomplete="off" required autofocus />
 			<input type="password" name="password" id="password" placeholder="Password" value="" required />
-			<input type="checkbox" name="remember_login"/> <?php echo $l->t('Remember login'); ?>
+			<input type="checkbox" name="remember_login" id="remember_login" /> <?php echo $l->t('remember'); ?>
 			<?php }else{ ?>
-			<input type="text" name="user" id="user" placeholder="Username" value="<?php echo $_['username']; ?>" required >
-			<input type="password" name="password" id="password" value="" placeholder="Password" required autofocus />
-			<input type="checkbox" name="remember_login" checked /> <?php echo $l->t('Remember login'); ?>
+			<input type="text" name="user" id="user" placeholder="Username" value="<?php echo $_['username']; ?>" autocomplete="off" required >
+			<input type="password" name="password" id="password" placeholder="Password" value="" required autofocus />
+			<input type="checkbox" name="remember_login" id="remember_login" checked /> <?php echo $l->t('remember'); ?>
 			<?php } ?>
-			<input type="submit" value="Log in" />
+			<input type="submit" id="submit" value="Log in" />
 		</fieldset>
 	</form>
 </div>
