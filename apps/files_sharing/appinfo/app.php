c6544429016f55440073161f37d905383b1a76c9
Michael Gapczynski <GapczynskiM@gmail.com>
2011-08-09 10:45:12 -0400

    No more subnavigation entries, fixing 'Shared'

--- apps/files_sharing/appinfo/app.php
+++ apps/files_sharing/appinfo/app.php
@@ -8,9 +8,9 @@ OC_Util::addScript("files_sharing", "share");
 OC_Util::addScript("3rdparty", "chosen/chosen.jquery.min");
 OC_Util::addStyle( 'files_sharing', 'sharing' );
 OC_Util::addStyle("3rdparty", "chosen/chosen");
-OC_App::addNavigationSubEntry("files_index", array(
+OC_App::addNavigationEntry(array(
   "id" => "files_sharing_list",
-  "order" => 10, 
+  "order" => 2, 
   "href" => OC_Helper::linkTo( "files_sharing", "list.php" ),
   "name" => "Shared"));
 
