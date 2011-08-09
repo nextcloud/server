97462b018d7aae7bd6044bcb42d42c1b51171d5a
Serge Martin <edb@sigluy.net>
2011-08-09 11:32:22 +0200

    Merge MySQL and PostgreSQL input fields

--- core/js/setup.js
+++ core/js/setup.js
@@ -1,27 +1,20 @@
 $(document).ready(function() {
-	// Hide the PostgreSQL config div if needed:
-	if(!$('#pgsql').is(':checked')) {
-		$('#use_postgresql').hide();
-	}
-
 	$('#datadirField').hide(250);
 	if($('#hasSQLite').val()=='true'){
 		$('#databaseField').hide(250);
+		$('#use_other_db').slideUp(250);
 	}
 
 	$('#sqlite').click(function() {
-		$('#use_mysql').slideUp(250);
-		$('#use_postgresql').slideUp(250);
+		$('#use_other_db').slideUp(250);
 	});
 
 	$('#mysql').click(function() {
-		$('#use_mysql').slideDown(250);
-		$('#use_postgresql').slideUp(250);
+		$('#use_other_db').slideDown(250);
 	});
 	
 	$('#pgsql').click(function() {
-		$('#use_postgresql').slideDown(250);
-		$('#use_mysql').slideUp(250);
+		$('#use_other_db').slideDown(250);
 	});
 
 	$('#showAdvanced').click(function() {
@@ -30,9 +23,4 @@ $(document).ready(function() {
 			$('#databaseField').slideToggle(250);
 		}
 	});
-	
-	// Hide the MySQL config div if needed :
-	if($('#dbtype').val()=='sqlite') {
-		$('#databaseField').hide();
-	}
 });
