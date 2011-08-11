110f96df78f1ba4074df59a6c13025dc69a6b26f
Jakob Sack <kde@jakobsack.de>
2011-08-11 10:08:02 +0200

    At least dome layout in Contacts

--- apps/contacts/templates/index.php
+++ apps/contacts/templates/index.php
@@ -3,13 +3,13 @@ OC_Util::addScript('contacts','interface');
 OC_Util::addStyle('contacts','styles');
 ?>
 
-<div id="contacts_contacts" class="leftcontent">
+<div id="leftcontent" class="leftcontent">
 	<ul>
 		<?php echo $this->inc("part.contacts"); ?>
 	</ul>
 	<a id="contacts_newcontact"><?php echo $l->t('Add Contact'); ?></a>
 </div>
-<div id="contacts_details" class="rightcontent" data-id="<?php echo $_['id']; ?>">
+<div id="rightcontent" class="rightcontent" data-id="<?php echo $_['id']; ?>">
 	<?php echo $this->inc("part.details"); ?>
 </div>
 <?php if(count($_['addressbooks']) == 1 ): ?>
