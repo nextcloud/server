6b2862fcfe900d796ed6a6f6d0aa44dfc926e2c0
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-13 16:00:10 +0200

    various CSS fixes

--- settings/templates/personal.php
+++ settings/templates/personal.php
@@ -1,15 +1,15 @@
 <div id="quota" class="personalblock"><div style="width:<?php echo $_['usage_relative'] ?>%;">
-	<p><?php echo $l->t( 'You\'re currently using' );?> <strong><?php echo $_['usage_relative'] ?>%</strong> (<?php echo $_['usage'] ?>) <?php echo $l->t( 'of your' );?> <?php echo $_['total_space'] ?> <?php echo $l->t( 'space' );?>.</p>
+	<p><?php echo $l->t( 'You use' );?> <strong><?php echo $_['usage'] ?></strong> <?php echo $l->t( 'of the available' );?> <?php echo $_['total_space'] ?></p>
 </div></div>
 
 <form id="passwordform">
 	<fieldset class="personalblock">
 		<div id="passwordchanged"><?php echo $l->t( 'Your password got changed');?></div>
 		<div id="passworderror"></div>
-		<input type="password" id="pass1" name="oldpassword" placeholder="<?php echo $l->t( 'Old password' );?>" />
+		<input type="password" id="pass1" name="oldpassword" placeholder="<?php echo $l->t( 'Current password' );?>" />
 		<input type="password" id="pass2" name="password" placeholder="<?php echo $l->t( 'New password' );?>" data-typetoggle="#show" />
 		<input type="checkbox" id="show" name="show" /><label for="show"><?php echo $l->t( 'show' );?></label>
-		<input id="passwordbutton" type="submit" value="<?php echo $l->t( 'Change Password' );?>" />
+		<input id="passwordbutton" type="submit" value="<?php echo $l->t( 'Change password' );?>" />
 	</fieldset>
 </form>
 
