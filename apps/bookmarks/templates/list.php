b885bad2eebed23a23f86bdb6297551096428c7f
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-16 12:45:44 +0200

    wording fix for bookmarks

--- apps/bookmarks/templates/list.php
+++ apps/bookmarks/templates/list.php
@@ -4,10 +4,10 @@
 	<input type="button" class="bookmarks_addBtn" value="Add Bookmark" />
 </div>
 <div class="bookmarks_add">
-	<p><label class="bookmarks_label">URL: </label><input type="text" id="bookmark_add_url" class="bookmarks_input" /></p>
-	<p><label class="bookmarks_label">Title: </label><input type="text" id="bookmark_add_title" class="bookmarks_input" /></p>
-	<p><label class="bookmarks_label">Description: </label><input type="text" id="bookmark_add_description" class="bookmarks_input" /></p>
-	<p><label class="bookmarks_label">Tags:</label><input type="text" id="bookmark_add_tags" class="bookmarks_input" /></p>
+	<p><label class="bookmarks_label">Address</label><input type="text" id="bookmark_add_url" class="bookmarks_input" /></p>
+	<p><label class="bookmarks_label">Title</label><input type="text" id="bookmark_add_title" class="bookmarks_input" /></p>
+	<p><label class="bookmarks_label">Description</label><input type="text" id="bookmark_add_description" class="bookmarks_input" /></p>
+	<p><label class="bookmarks_label">Tags</label><input type="text" id="bookmark_add_tags" class="bookmarks_input" /></p>
 	<p><label class="bookmarks_label"></label><input type="submit" id="bookmark_add_submit" /></p>
 </div>
 <div class="bookmarks_list">
