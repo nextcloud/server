1303e40b6e151a3ff1ef55da89684daf9ad887ec
Michael Gapczynski <GapczynskiM@gmail.com>
2011-08-17 17:44:10 -0400

    Remove navigation entry for shared files

--- apps/files_sharing/appinfo/app.php
+++ apps/files_sharing/appinfo/app.php
@@ -8,10 +8,5 @@ OC_Util::addScript("files_sharing", "share");
 OC_Util::addScript("3rdparty", "chosen/chosen.jquery.min");
 OC_Util::addStyle( 'files_sharing', 'sharing' );
 OC_Util::addStyle("3rdparty", "chosen/chosen");
-OC_App::addNavigationEntry(array(
-  "id" => "files_sharing_list",
-  "order" => 2, 
-  "href" => OC_Helper::linkTo( "files_sharing", "list.php" ),
-  "name" => "Shared"));
 
 ?>
