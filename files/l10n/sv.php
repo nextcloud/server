2ee9e80afe7a8bd0788e659c90e58f81995d690b
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-08-18 14:33:05 +0200

    Swedish translation, thanks to HakanS

---
+++ files/l10n/sv.php
@@ -0,0 +1,14 @@
+<?php $TRANSLATIONS = array(
+"Files" => "Filer",
+"Maximum upload size" => "Maximal storlek att lägga upp",
+"Nothing in here. Upload something!" => "Ingenting här. Lägg upp något!",
+"Upload" => "Lägg upp",
+"New Folder" => "Ny katalog",
+"Name" => "Namn",
+"Download" => "Ladda ned",
+"Size" => "Storlek",
+"Modified" => "Ändrad",
+"Delete" => "Ta bort",
+"Upload too large" => "För stor överföring",
+"The files you are trying to upload exceed the maximum size for file uploads on this server." => "Filerna du försöker lägga upp överstiger den maximala storleken för filöverföringar på servern."
+);
