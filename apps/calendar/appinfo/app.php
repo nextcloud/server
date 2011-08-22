b2fddc408cbb8c08831859355d4c7b604567ee01
Georg Ehrke <georg.stefan.germany@googlemail.com>
2011-08-22 20:09:33 +0200

    /appinfo/app.php bug fixed

--- apps/calendar/appinfo/app.php
+++ apps/calendar/appinfo/app.php
@@ -1,6 +1,6 @@
 <?php
 $l=new OC_L10N('calendar');
-OC::$CLASSPATH['OC_Calendat_Calendar'] = 'apps/calendar/lib/calendar.php';
+OC::$CLASSPATH['OC_Calendar_Calendar'] = 'apps/calendar/lib/calendar.php';
 OC::$CLASSPATH['OC_Calendar_Hooks'] = 'apps/calendar/lib/hooks.php';
 OC::$CLASSPATH['OC_Connector_Sabre_CalDAV'] = 'apps/calendar/lib/connector_sabre.php';
 OC_HOOK::connect('OC_User', 'post_createUser', 'OC_Calendar_Hooks', 'deleteUser');
