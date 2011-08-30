c8a544eaea0ffe9e5337f6c66662db6b9b4a22a6
Bart Visscher <bartv@thisnet.nl>
2011-08-30 11:17:52 +0200

    Use activew state of calendar in choosecalendar

--- apps/calendar/templates/part.choosecalendar.php
+++ apps/calendar/templates/part.choosecalendar.php
@@ -4,7 +4,7 @@
 $option_calendars = OC_Calendar_Calendar::allCalendars(OC_User::getUser());
 for($i = 0; $i < count($option_calendars); $i++){
 	echo "<tr>";
-	echo "<td width=\"20px\"><input id=\"active_" . $option_calendars[$i]["id"] . "\" type=\"checkbox\" onClick=\"oc_cal_calender_activation(this, " . $option_calendars[$i]["id"] . ")\"></td>";
+	echo "<td width=\"20px\"><input id=\"active_" . $option_calendars[$i]["id"] . "\" type=\"checkbox\" onClick=\"oc_cal_calender_activation(this, " . $option_calendars[$i]["id"] . ")\"" . ($option_calendars[$i]["active"] ? ' checked="checked"' : '') . "></td>";
 	echo "<td><label for=\"active_" . $option_calendars[$i]["id"] . "\">" . $option_calendars[$i]["displayname"] . "</label></td>";
 	echo "<td width=\"20px\"><a style=\"display: block; opacity: 0.214133;\" href=\"#\" title=\"" . $l->t("Download") . "\" class=\"action\"><img src=\"/owncloud/core/img/actions/download.svg\"></a></td><td width=\"20px\"><a style=\"display: block; opacity: 0.214133;\" href=\"#\" title=\"" . $l->t("Rename") . "\" class=\"action\"><img src=\"/owncloud/core/img/actions/rename.svg\"></a></td>";
 	echo "</tr>";
