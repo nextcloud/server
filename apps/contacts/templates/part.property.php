72aa1bf75c0b1ca6c02e912f9095eb07b18b1649
Alexander Wigen <alex@wigen.net>
2011-09-09 15:54:29 +0200

    Only show used address attributes in contact view

--- apps/contacts/templates/part.property.php
+++ apps/contacts/templates/part.property.php
@@ -44,13 +44,27 @@
 			<?php endif; ?>
 		</td>
 		<td class="contacts_details_right">
+			<?php if(!empty($_['property']['value'][0])): ?>
 			<?php echo $l->t('PO Box'); ?> <?php echo $_['property']['value'][0]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][1])): ?>
 			<?php echo $l->t('Extended Address'); ?> <?php echo $_['property']['value'][1]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][2])): ?>
 			<?php echo $l->t('Street Name'); ?> <?php echo $_['property']['value'][2]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][3])): ?>
 			<?php echo $l->t('City'); ?> <?php echo $_['property']['value'][3]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][4])): ?>
 			<?php echo $l->t('Region'); ?> <?php echo $_['property']['value'][4]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][5])): ?>
 			<?php echo $l->t('Postal Code'); ?> <?php echo $_['property']['value'][5]; ?><br>
+			<?php endif; ?>
+			<?php if(!empty($_['property']['value'][6])): ?>
 			<?php echo $l->t('Country'); ?> <?php echo $_['property']['value'][6]; ?> 
+			<?php endif; ?>
 			<span style="display:none;" data-use="edit"><img class="svg action" src="<?php echo image_path('', 'actions/rename.svg'); ?>" /></span>
 			<span style="display:none;" data-use="delete"><img class="svg action" src="<?php echo image_path('', 'actions/delete.svg'); ?>" /></span>
 		</td>
