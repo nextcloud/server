f928d4af564e719c34e6c03a9c9ca261a8a1076e
Michiel@unhosted <michiel@unhosted.org>
2011-09-12 14:35:15 +0200

    correct xml syntax

--- apps/user_webfinger/webfinger.php
+++ apps/user_webfinger/webfinger.php
@@ -17,8 +17,8 @@ if(substr($userName, 0, 5) == 'acct:') {
 }
 echo "<";
 ?>
-xml version="1.0" encoding="UTF-8"?>
+?xml version="1.0" encoding="UTF-8"?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">
 	<hm:Host xmlns="http://host-meta.net/xrd/1.0"><?php echo $_SERVER['SERVER_NAME'] ?></hm:Host>
-	<Link rel="http://unhosted.org/spec/dav/0.1" href="http<?php echo ($_SERVER['HTTPS']?'s':''); ?>://<?php echo $_SERVER['SERVER_NAME'] ?>/apps/unhosted/compat.php/<?php echo $userName ?>/unhosted/"></Link>
+	<Link rel="http://unhosted.org/spec/dav/0.1" href="http<?php echo ($_SERVER['HTTPS']?'s':''); ?>://<?php echo $_SERVER['SERVER_NAME'] ?>/apps/unhosted/compat.php/<?php echo $userName ?>/unhosted/" />
 </XRD>
