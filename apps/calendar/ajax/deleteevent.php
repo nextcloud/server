b0e23a16340428af2bae3330bddbf8988d01ce00
Florian Pritz <bluewind@xinu.at>
2011-09-22 22:09:03 +0200

    apps/calendar: add delete button to edit event form
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

---
+++ apps/calendar/ajax/deleteevent.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (c) 2011 Bart Visscher <bartv@thisnet.nl>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+require_once('../../../lib/base.php');
+
+$l10n = new OC_L10N('calendar');
+
+if(!OC_USER::isLoggedIn()) {
+	die('<script type="text/javascript">document.location = oc_webroot;</script>');
+}
+
+$id = $_POST['id'];
+$data = OC_Calendar_Object::find($id);
+if (!$data)
+{
+	echo json_encode(array('status'=>'error'));
+	exit;
+}
+$calendar = OC_Calendar_Calendar::findCalendar($data['calendarid']);
+if($calendar['userid'] != OC_User::getUser()){
+	echo json_encode(array('status'=>'error'));
+	exit;
+}
+$result = OC_Calendar_Object::delete($id);
+echo json_encode(array('status' => 'success'));
+?> 
--- apps/calendar/templates/part.editevent.php
+++ apps/calendar/templates/part.editevent.php
@@ -5,6 +5,7 @@
 	<div style="width: 100%;text-align: center;color: #FF1D1D;" id="errorbox"></div>
 	<span id="actions">
 		<input type="button" class="submit" style="float: left;" value="<?php echo $l->t("Submit");?>" onclick="Calendar.UI.validateEventForm('ajax/editevent.php');">
+		<input type="button" class="submit" style="float: left;" name="delete" value="<?php echo $l->t("Delete");?>" onclick="Calendar.UI.submitDeleteEventForm('ajax/deleteevent.php');">
 	</span>
 	</form>
 </div>
