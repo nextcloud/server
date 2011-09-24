57bffdb46a827b58d0762d6955e3809ec4ed25e5
Robin Appelman <icewind1991@gmail.com>
2011-09-25 01:20:43 +0200

    fix uploading files using webdav

--- lib/fileproxy/quota.php
+++ lib/fileproxy/quota.php
@@ -45,7 +45,7 @@ class OC_FileProxy_Quota extends OC_FileProxy{
 
 	public function preFile_put_contents($path,$data){
 		if (is_resource($data)) {
-			$data = stream_get_contents($data);
+			$data = '';//TODO: find a way to get the length of the stream without emptying it
 		}
 		return (strlen($data)<$this->getFreeSpace() or $this->getFreeSpace()==0);
 	}
