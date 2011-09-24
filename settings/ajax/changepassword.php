c16a9a83ba4410d969f60772c29ac48ee2116c01
Robin Appelman <icewind1991@gmail.com>
2011-09-25 01:06:00 +0200

    actually check the correct password when changing the password

--- settings/ajax/changepassword.php
+++ settings/ajax/changepassword.php
@@ -8,9 +8,10 @@ header( "Content-Type: application/jsonrequest" );
 
 $username = isset($_POST["username"]) ? $_POST["username"] : OC_User::getUser();
 $password = $_POST["password"];
+$oldPassword=isset($_POST["oldpassword"])?$_POST["oldpassword"]:'';
 
 // Check if we are a user
-if( !OC_User::isLoggedIn() || (!OC_Group::inGroup( OC_User::getUser(), 'admin' ) && ($username!=OC_User::getUser() || !OC_User::checkPassword($username,$password)))) {
+if( !OC_User::isLoggedIn() || (!OC_Group::inGroup( OC_User::getUser(), 'admin' ) && ($username!=OC_User::getUser() || !OC_User::checkPassword($username,$oldPassword)))) {
 	echo json_encode( array( "status" => "error", "data" => array( "message" => "Authentication error" )));
 	exit();
 }
