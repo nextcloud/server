a54ea3246cb2d20e3527211d3413f5d3a8dd529f
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-09-24 23:03:28 +0200

    removed 'share by publiclink' app as it has been superseded by Sharing

--- apps/files_publiclink/ajax/getlink.php
+++
@@ -1,8 +0,0 @@
-<?php
-$RUNTIME_NOAPPS = true;
-
-require_once('../../../lib/base.php');
-require_once('../lib_public.php');
-
-$path = $_GET['path'];
-echo json_encode(OC_PublicLink::getLink($path));
