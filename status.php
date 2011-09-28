18216fe71f778b299585de7cc42a568a5adcfa6c
Frank Karlitschek <karlitschek@kde.org>
2011-09-28 14:26:48 +0200

    xml so soooo oldschool. using json now like the cool kids do.

--- status.php
+++ status.php
@@ -26,18 +26,9 @@ $RUNTIME_NOAPPS = TRUE; //no apps, yet
 require_once('lib/base.php');
 
 if(OC_Config::getValue('installed')==1) $installed='true'; else $installed='false';
+$values=array('installed'=>$installed,'version'=>implode('.',OC_Util::getVersion()),'versionstring'=>OC_Util::getVersionString());
 
-
-$txt='
-<owncloud>
-  <installed>'.$installed.'</installed>
-  <version>'.implode('.',OC_Util::getVersion()).'</version>
-  <versionstring>ownCloud '.OC_Util::getVersionString().'</versionstring>
-</owncloud>
-';
-
-echo($txt);
-
+echo(json_encode($values));
 
 
 ?>
