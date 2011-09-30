05417284046eeb6af29fb41312b9ce3cc7408e38
Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
2011-09-30 20:23:59 +0200

    added text to localisation

--- core/templates/login.php
+++ core/templates/login.php
@@ -4,14 +4,14 @@
 			<a href="index.php?lostpassword"><?php echo $l->t('Lost your password?'); ?></a>
 		<?php endif;*/ ?>
 		<?php if(empty($_['username'])): ?>
-			<input type="text" name="user" id="user" placeholder="Username" value="" autocomplete="off" required autofocus />
-			<input type="password" name="password" id="password" placeholder="Password" value="" required />
+			<input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="" autocomplete="off" required autofocus />
+			<input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required />
 			<input type="checkbox" name="remember_login" value="1" id="remember_login" /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
 		<?php else: ?>
-		      <input type="text" name="user" id="user" placeholder="Username" value="<?php echo $_['username']; ?>" autocomplete="off" required >
-		      <input type="password" name="password" id="password" placeholder="Password" value="" required autofocus />
+		      <input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo $_['username']; ?>" autocomplete="off" required >
+		      <input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required autofocus />
 		      <input type="checkbox" name="remember_login" value="1" id="remember_login" checked /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
 		<?php endif; ?>
-		<input type="submit" id="submit" class="login" value="Log in" />
+		<input type="submit" id="submit" class="login" value="<?php echo $l->t( 'Log in' ); ?>" />
 	</fieldset>
 </form>
