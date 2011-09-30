e2251a4b389418d1f294d9bdb7da48ad1e3f1307
Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
2011-09-30 20:27:31 +0200

    show typed in username when log in has failed

--- core/templates/login.php
+++ core/templates/login.php
@@ -4,8 +4,8 @@
 			<a href="index.php?lostpassword"><?php echo $l->t('Lost your password?'); ?></a>
 		<?php endif;*/ ?>
 		<?php if(empty($_['username'])): ?>
-			<input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="" autocomplete="off" required autofocus />
-			<input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required />
+			<input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo !empty($_POST['user'])?$_POST['user'].'"':'" autofocus'; ?> autocomplete="off" required />
+			<input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required <?php echo !empty($_POST['user'])?'autofocus':''; ?> />
 			<input type="checkbox" name="remember_login" value="1" id="remember_login" /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
 		<?php else: ?>
 		      <input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo $_['username']; ?>" autocomplete="off" required >
