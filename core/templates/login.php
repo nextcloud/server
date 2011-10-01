c943f48547c686570dac3d678672693e7b46658e
Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
2011-10-01 11:08:49 +0200

    fixed link to password recovery. removed duplicated and unused code out of index.php.

--- core/templates/login.php
+++ core/templates/login.php
@@ -1,17 +1,11 @@
 <form action="index.php" method="post">
 	<fieldset>
 		<?php if($_['error']): ?>
-			<a href="index.php?lostpassword"><?php echo $l->t('Lost your password?'); ?></a>
+			<a href="./lostpassword/"><?php echo $l->t('Lost your password?'); ?></a>
 		<?php endif; ?>
-		<?php if(empty($_['username'])): ?>
-			<input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo !empty($_POST['user'])?$_POST['user'].'"':'" autofocus'; ?> autocomplete="off" required />
-			<input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required <?php echo !empty($_POST['user'])?'autofocus':''; ?> />
-			<input type="checkbox" name="remember_login" value="1" id="remember_login" /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
-		<?php else: ?>
-		      <input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo $_['username']; ?>" autocomplete="off" required >
-		      <input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required autofocus />
-		      <input type="checkbox" name="remember_login" value="1" id="remember_login" checked /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
-		<?php endif; ?>
+		<input type="text" name="user" id="user" placeholder="<?php echo $l->t( 'Username' ); ?>" value="<?php echo !empty($_POST['user'])?$_POST['user'].'"':'" autofocus'; ?> autocomplete="off" required />
+		<input type="password" name="password" id="password" placeholder="<?php echo $l->t( 'Password' ); ?>" value="" required <?php echo !empty($_POST['user'])?'autofocus':''; ?> />
+		<input type="checkbox" name="remember_login" value="1" id="remember_login" /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
 		<input type="submit" id="submit" class="login" value="<?php echo $l->t( 'Log in' ); ?>" />
 	</fieldset>
 </form>
