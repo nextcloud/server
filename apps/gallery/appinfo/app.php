7bcc0efd6a3688b04bcdda94f7cc920a044e442f
Robin Appelman <icewind1991@gmail.com>
2011-10-02 19:33:55 +0200

    show galleries in search results

--- apps/gallery/appinfo/app.php
+++ apps/gallery/appinfo/app.php
@@ -10,4 +10,18 @@ OC_App::addNavigationEntry( array(
  'href' => OC_Helper::linkTo('gallery', 'index.php'),
  'icon' => OC_Helper::linkTo('', 'core/img/filetypes/image.png'),
  'name' => 'Gallery'));
+
+ class OC_GallerySearchProvider extends OC_Search_Provider{
+	function search($query){
+		$stmt = OC_DB::prepare('SELECT * FROM *PREFIX*gallery_albums WHERE uid_owner = ? AND album_name LIKE ?');
+		$result = $stmt->execute(array(OC_User::getUser(),'%'.$query.'%'));
+		$results=array();
+		while($row=$result->fetchRow()){
+			$results[]=new OC_Search_Result($row['album_name'],'',OC_Helper::linkTo( 'apps/gallery', 'index.php?view='.$row['album_name']),'Galleries');
+		}
+		return $results;
+	}
+}
+
+new OC_GallerySearchProvider();
 ?>
