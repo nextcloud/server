39e7db7db4174e4d76c77c47a7b4aab6e3bdda37
Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
2011-10-08 13:11:13 +0200

    added missing files

---
+++ apps/bookmarks/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2011 Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+$tmpl = new OC_Template( 'bookmarks', 'settings');
+
+OC_Util::addScript('bookmarks','settings');
+
+return $tmpl->fetchPage();
