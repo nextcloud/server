e2468c4343c9b0939916bb20acae57aba1e62f95
Michiel@unhosted <michiel@unhosted.org>
2011-10-09 17:24:18 +0200

    fix webfinger install script

--- apps/user_webfinger/appinfo/install.php
+++ apps/user_webfinger/appinfo/install.php
@@ -1,5 +1,6 @@
 <?php
-$thisAppDir = __DIR__;
+$appInfoDir = __DIR__;
+$thisAppDir = dirname($appInfoDir);
 $appsDir = dirname($thisAppDir);
 $ownCloudDir = dirname($appsDir);
 symlink($thisAppDir, $ownCloudDir.'/.well-known');
