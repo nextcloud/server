c420001f2b28a619fe844d7366ae3984b38021e9
Jan-Christoph Borchardt <jan@unhosted.org>
2011-10-21 19:09:41 +0200

    bold current folder in breadcrumbs

--- files/templates/part.breadcrumb.php
+++ files/templates/part.breadcrumb.php
@@ -1,5 +1,10 @@
-	<?php foreach($_["breadcrumb"] as $crumb): ?>
+	<?php for($i=0; $i<count($_["breadcrumb"])-1; $i++):
+        $crumb = $_["breadcrumb"][$i]; ?>
 		<div class="crumb svg" data-dir='<?php echo $crumb["dir"];?>' style='background-image:url("<?php echo image_path('core','breadcrumb.png');?>")'>
-			<a href="<?php echo $_['baseURL'].$crumb["dir"]; ?>"><?php echo htmlspecialchars($crumb["name"]); ?></a>
+    		<a href="<?php echo $_['baseURL'].$crumb["dir"]; ?>"><?php echo htmlspecialchars($crumb["name"]); ?></a>
 		</div>
-	<?php endforeach; ?>
+	<?php endfor;
+    $crumb = $_["breadcrumb"][count($_["breadcrumb"])-1] ?>
+    <div class="crumb last svg" data-dir='<?php echo $crumb["dir"];?>' style='background-image:url("<?php echo image_path('core','breadcrumb.png');?>")'>
+    	<a href="<?php echo $_['baseURL'].$crumb["dir"]; ?>"><?php echo htmlspecialchars($crumb["name"]); ?></a>
+	</div>
