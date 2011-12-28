86b65c269a858cfba953f49d7ac86a759865fcc3
Michael Gapczynski <GapczynskiM@gmail.com>
2011-12-28 13:01:36 -0500

    Keep the urls pretty, decode forward slashes for newly added directories

--- files/js/filelist.js
+++ files/js/filelist.js
@@ -40,7 +40,7 @@ FileList={
 		html = $('<tr></tr>').attr({ "data-type": "dir", "data-size": size, "data-file": name});
 		td = $('<td></td>').attr({"class": "filename", "style": 'background-image:url('+OC.imagePath('core', 'filetypes/folder.png')+')' });
 		td.append('<input type="checkbox" />');
-		var link_elem = $('<a></a>').attr({ "class": "name", "href": "index.php?dir="+ encodeURIComponent($('#dir').val()+'/'+name) });
+		var link_elem = $('<a></a>').attr({ "class": "name", "href": "index.php?dir="+ encodeURIComponent($('#dir').val()+'/'+name).replace(/%2F/g, '/') });
 		link_elem.append($('<span></span>').addClass('nametext').text(name));
 		td.append(link_elem);
 		html.append(td);
