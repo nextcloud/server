604ca61d78881e3a1a0a41e4b50ef4cb7c1eeaf0
Thomas Tanghus <thomas@tanghus.net>
2012-01-04 22:36:34 +0100

    Some cleanup, better error messages and such.

--- apps/contacts/index.php
+++ apps/contacts/index.php
@@ -48,9 +48,10 @@ $details = array();
 if(is_null($id) && count($contacts) > 0) {
 	$id = $contacts[0]['id'];
 }
-$vcard = OC_Contacts_App::getContactVCard($id);
-$details = OC_Contacts_VCard::structureContact($vcard);
-
+if(!is_null($id)) {
+	$vcard = OC_Contacts_App::getContactVCard($id);
+	$details = OC_Contacts_VCard::structureContact($vcard);
+}
 // if( !is_null($id)/* || count($contacts)*/){
 // 	if(is_null($id)) $id = $contacts[0]['id'];
 // 	$vcard = OC_Contacts_App::getContactVCard($id);
