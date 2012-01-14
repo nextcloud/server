868cf4629051c557312b63acbb3e6facd8f0f86e
Georg Ehrke <ownclouddev@georgswebsite.de>
2012-01-14 14:22:30 +0100

    improve import function

--- apps/calendar/js/loader.js
+++ apps/calendar/js/loader.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2012 Georg Ehrke <ownclouddev at georgswebsite dot de>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
 Calendar_Import={
 	importdialog: function(filename){
 		var path = $('#dir').val();
@@ -5,23 +11,31 @@ Calendar_Import={
 		$('#calendar_import').load(OC.filePath('calendar', 'ajax', 'importdialog.php'), {filename:filename, path:path},	function(){Calendar_Import.initdialog(filename);});
 	},
 	initdialog: function(filename){
-		$("#calendar_import_dialog").dialog({
+		$('#calendar_import_dialog').dialog({
 			width : 500,
 			close : function() {
 				$(this).dialog('destroy').remove();
-				$("#calendar_import").remove();
+				$('#calendar_import').remove();
 			}
 		});
-		$('#progressbar').progressbar({value: 87});
+		$('#import_done_button').click(function(){
+			$('#calendar_import_dialog').dialog('destroy').remove();
+			$('#calendar_import').remove();
+		});
+		$('#progressbar').progressbar({value: 0});
 		$('#startimport').click(function(){
 			var filename = $('#filename').val();
 			var path = $('#path').val();
+			var calid = $('#calendar option:selected').val();
 			if($('#calendar option:selected').val() == 'newcal'){
 				var method = 'new';
 				var calname = $('#newcalendar').val();
 				var calname = $.trim(calname);
 				if(calname == ''){
 					$('#newcalendar').css('background-color', '#FF2626');
+					$('#newcalendar').focus(function(){
+						$('#newcalendar').css('background-color', '#F8F8F8');
+					});
 					return false;
 				}
 			}else{
@@ -29,8 +43,16 @@ Calendar_Import={
 			}
 			$('#newcalendar').attr('readonly', 'readonly');
 			$('#calendar').attr('disabled', 'disabled');
-			$.post(OC.filePath('calendar', '', 'import.php'), {'method':method, 'calname':calname, 'path':path, 'file':filename}, function(){});
-			$('#progressbar').slideDown('slow');
+			var progressfile = $('#progressfile').val();
+			$.post(OC.filePath('calendar', '', 'import.php'), {method: String (method), calname: String (calname), path: String (path), file: String (filename), id: String (calid)}, function(data){
+				if(data.status == 'success'){
+					$('#progressbar').progressbar('option', 'value', 100);
+					$('#import_done').css('display', 'block');
+				}
+			});
+			$('#form_container').css('display', 'none');
+			$('#progressbar_container').css('display', 'block');
+			window.setTimeout('Calendar_Import.getimportstatus(\'' + progressfile + '\')', 500);
 		});
 		$('#calendar').change(function(){
 			if($('#calendar option:selected').val() == 'newcal'){
@@ -40,8 +62,15 @@ Calendar_Import={
 			}
 		});
 	},
-	getimportstatus: function(){
-		
+	getimportstatus: function(progressfile){
+		$.get(OC.filePath('calendar', 'import_tmp', progressfile), function(percent){
+			$('#progressbar').progressbar('option', 'value', parseInt(percent));
+			if(percent < 100){
+				window.setTimeout('Calendar_Import.getimportstatus(\'' + progressfile + '\')', 500);
+			}else{
+				$('#import_done').css('display', 'block');
+			}
+		});
 	}
 }
 $(document).ready(function(){
--- apps/calendar/templates/part.import.php
+++ apps/calendar/templates/part.import.php
@@ -1,7 +1,9 @@
 <div id="calendar_import_dialog" title="<?php echo $l->t("Import a calendar file"); ?>">
-<input type="hidden" id="filename" value="<?php echo $_GET["filename"];?>">
-<input type="hidden" id="path" value="<?php echo $_GET["path"];?>">
-<p style="text-align:center;"><b><?php echo $l->t('Please choose the calendar'); ?></b></h2>
+<div id="form_container">
+<input type="hidden" id="filename" value="<?php echo $_['filename'];?>">
+<input type="hidden" id="path" value="<?php echo $_['path'];?>">
+<input type="hidden" id="progressfile" value="<?php echo md5(session_id()) . '.txt';?>">
+<p style="text-align:center;"><b><?php echo $l->t('Please choose the calendar'); ?></b>
 <select style="width:100%;" id="calendar" name="calendar">
 <?php
 $calendar_options = OC_Calendar_Calendar::allCalendars(OC_User::getUser());
@@ -13,6 +15,13 @@ echo html_select_options($calendar_options, $calendar_options[0]['id'], array('v
 	<input type="text" style="width: 97%;" placeholder="<?php echo $l->t('Name of new calendar'); ?>" id="newcalendar" name="newcalendar">
 </div>
 <input type="button" value="<?php echo $l->t("Import");?>!" id="startimport">
-<br><br>
-<div id="progressbar" style="display: none;"></div>
+</div>
+<div id="progressbar_container" style="display: none">
+<p style="text-align:center;"><b><?php echo $l->t('Importing calendar'); ?></b>
+<div id="progressbar"></div>
+<div id="import_done" style="display: none;">
+<p style="text-align:center;"><b><?php echo $l->t('Calendar imported successfully'); ?></b></p>
+<input type="button" value="<?php echo $l->t('Close Dialog'); ?>" id="import_done_button">
+</div>
+</div>
 </div>
