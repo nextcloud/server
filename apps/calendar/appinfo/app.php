830cf87697282cac254451079e21d2463cb86c92
Robin Appelman <icewind@owncloud.com>
2012-01-26 19:05:46 +0100

    use the correct hooks to remove contact/calendars of remvoed users

--- apps/calendar/appinfo/app.php
+++ apps/calendar/appinfo/app.php
@@ -6,7 +6,7 @@ if(version_compare(PHP_VERSION, '5.3.0', '>')){
 	OC::$CLASSPATH['OC_Calendar_Object'] = 'apps/calendar/lib/object.php';
 	OC::$CLASSPATH['OC_Calendar_Hooks'] = 'apps/calendar/lib/hooks.php';
 	OC::$CLASSPATH['OC_Connector_Sabre_CalDAV'] = 'apps/calendar/lib/connector_sabre.php';
-	OC_HOOK::connect('OC_User', 'post_createUser', 'OC_Calendar_Hooks', 'deleteUser');
+	OC_HOOK::connect('OC_User', 'post_deleteUser', 'OC_Calendar_Hooks', 'deleteUser');
 	OC_Util::addScript('calendar','loader');
 	OC_App::register( array( 
 	  'order' => 10,
--- apps/contacts/appinfo/app.php
+++ apps/contacts/appinfo/app.php
@@ -6,7 +6,7 @@ OC::$CLASSPATH['OC_Contacts_Addressbook'] = 'apps/contacts/lib/addressbook.php';
 OC::$CLASSPATH['OC_Contacts_VCard'] = 'apps/contacts/lib/vcard.php';
 OC::$CLASSPATH['OC_Contacts_Hooks'] = 'apps/contacts/lib/hooks.php';
 OC::$CLASSPATH['OC_Connector_Sabre_CardDAV'] = 'apps/contacts/lib/connector_sabre.php';
-OC_HOOK::connect('OC_User', 'post_createUser', 'OC_Contacts_Hooks', 'deleteUser');
+OC_HOOK::connect('OC_User', 'post_deleteUser', 'OC_Contacts_Hooks', 'deleteUser');
 
 OC_App::register( array(
   'order' => 10,
