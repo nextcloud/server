e229a6adecde57d919b6b1385729cbf8cd41fe89
David Iwanowitsch <david+git@unclouded.de>
2012-01-29 19:32:33 +0100

    Added searchprovider for bookmarks, initial l10n support for bookmark plugin moved some code from updateList.php to bookmarks.php, to make it reusable

---
+++ apps/bookmarks/js/bookmarksearch.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2012 David Iwanowitsch <david at unclouded dot de>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+$(document).ready(function(){
+	OC.search.customResults.Bookm.=function(row,item){
+		var a=row.find('a');
+		a.attr('target','_blank');
+		a.click(recordClick);
+	}
+});
+
+function recordClick(event) {
+	var jsFileLocation = $('script[src*=bookmarksearch]').attr('src');
+	jsFileLocation = jsFileLocation.replace('js/bookmarksearch.js', '');
+	$.ajax({
+		url: jsFileLocation + 'ajax/recordClick.php',
+		data: 'url=' + encodeURI($(this).attr('href')),
+	});	
+}
---
+++ apps/bookmarks/lib/search.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * ownCloud - bookmarks plugin
+ *
+ * @author David Iwanowitsch
+ * @copyright 2012 David Iwanowitsch <david at unclouded dot de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class OC_Search_Provider_Bookmarks extends OC_Search_Provider{
+	function search($query){
+		$results=array();
+		
+		$offset = 0;
+		$sqlSortColumn = 'id';
+		
+		$searchquery=array();
+		if(substr_count($query, ' ') > 0){
+			$searchquery = explode(' ', $query);
+		}else{
+			$searchquery = $query;
+		}
+		
+//		OC_Log::write('bookmarks', 'search ' .$query ,OC_Log::DEBUG);
+		$bookmarks = OC_Bookmarks_Bookmarks::findBookmarks($offset, $sqlSortColumn, $searchquery, false);
+//		OC_Log::write('bookmarks', 'found ' .count($bookmarks) ,OC_Log::DEBUG);
+		//$l = new OC_l10n('bookmarks'); //resulttype can't be localized, javascript relies on that type
+		foreach($bookmarks as $bookmark){
+			$results[]=new OC_Search_Result($bookmark['title'],'', $bookmark['url'],'Bookm.');
+		}
+		
+		return $results;
+	}
+}
+new OC_Search_Provider_Bookmarks();
+
+?>
