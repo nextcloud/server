df67c35017d8794a4b1c83fd4e41658b2e6d0e75
Robin Appelman <icewind@owncloud.com>
2012-01-31 16:12:38 +0100

    some fixes to scanning

--- files/ajax/scan.php
+++ files/ajax/scan.php
@@ -2,17 +2,25 @@
 
 require_once '../../lib/base.php';
 
-$eventSource=new OC_EventSource();
+set_time_limit(0);//scanning can take ages
 
 $force=isset($_GET['force']) and $_GET['force']=='true';
 $checkOnly=isset($_GET['checkonly']) and $_GET['checkonly']=='true';
+
+if(!$checkOnly){
+	$eventSource=new OC_EventSource();
+}
+
 
 //create the file cache if necesary
 if($force or !OC_FileCache::inCache('')){
 	if(!$checkOnly){
 		OC_FileCache::scan('',false,$eventSource);
+		$eventSource->send('success',true);
+	}else{
+		OC_JSON::success(array('data'=>array('done'=>true)));
+		exit;
 	}
-	$eventSource->send('success',true);
 }else{
 	$eventSource->send('success',false);
 }
