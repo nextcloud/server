f870f9efcf36a3c7e839b8c37195d94bfa92d6fe
Georg Ehrke <dev@georgswebsite.de>
2012-02-02 12:18:29 +0100

    validate parameters in changeview.php

--- apps/calendar/ajax/changeview.php
+++ apps/calendar/ajax/changeview.php
@@ -1,15 +1,24 @@
 <?php
 /**
- * Copyright (c) 2011 Georg Ehrke <ownclouddev at georgswebsite dot de>
+ * Copyright (c) 2012 Georg Ehrke <ownclouddev at georgswebsite dot de>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
  */
 
-require_once ("../../../lib/base.php");
+require_once ('../../../lib/base.php');
 OC_JSON::checkLoggedIn();
 OC_JSON::checkAppEnabled('calendar');
-$currentview = $_GET["v"];
-OC_Preferences::setValue(OC_USER::getUser(), "calendar", "currentview", $currentview);
+$currentview = $_GET['v'];
+switch($currentview){
+	case 'agendaWeek':
+	case 'month';
+	case 'list':
+		break;
+	default:
+		OC_JSON::error();
+		exit;
+}
+OC_Preferences::setValue(OC_USER::getUser(), 'calendar', 'currentview', $currentview);
 OC_JSON::success();
 ?>
