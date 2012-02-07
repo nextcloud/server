43d2266f5ca9f9f2802bf131fddeb21f849f3d12
Bart Visscher <bartv@thisnet.nl>
2012-02-07 22:33:47 +0100

    Add caching to gallery thumbnail generation

--- apps/gallery/ajax/thumbnail.php
+++ apps/gallery/ajax/thumbnail.php
@@ -25,22 +25,14 @@ require_once('../../../lib/base.php');
 OC_JSON::checkLoggedIn();
 OC_JSON::checkAppEnabled('gallery');
 
-$box_size = 200;
 $img = $_GET['img'];
 
-$imagePath = OC_Filesystem::getLocalFile($img);
-
-if(file_exists($imagePath)) {
-  $image = new OC_Image($imagePath);
-  $image->centerCrop();
-  $image->resize($box_size, $box_size);
-  $image->fixOrientation();
-
-  $offset = 3600 * 24;
-  // calc the string in GMT not localtime and add the offset
-  header("Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT");
-  header('Cache-Control: max-age='.$offset.', must-revalidate');
-  header('Pragma: public');
-
-  $image->show();
+$image = OC_Gallery_Photo::getThumbnail($img);
+if ($image) {
+	$offset = 3600 * 24; // 24 hour
+	// calc the string in GMT not localtime and add the offset
+	header("Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT");
+	header('Cache-Control: max-age='.$offset.', must-revalidate');
+	header('Pragma: public');
+	$image->show();
 }
