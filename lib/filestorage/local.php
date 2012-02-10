85853f9ec29be9a2ba92737de204da1469f72dd8
Klaas Freitag <freitag@owncloud.com>
2012-02-10 11:30:38 +0100

    - Added the ability to change a files mtime via webdavs propset. - Added some minor debug help to fscache

--- lib/filestorage/local.php
+++ lib/filestorage/local.php
@@ -65,6 +65,13 @@ class OC_Filestorage_Local extends OC_Filestorage{
 	public function filemtime($path){
 		return filemtime($this->datadir.$path);
 	}
+	
+	public function setFileMtime($path, $mtime){
+                   // sets the modification time of the file to the given value. If mtime is nil the current time is set.
+                  // note that the access time of the file always changes to the current time.
+                  return touch($this->datadir.$path, $mtime);
+         }
+
 	public function file_get_contents($path){
 		return file_get_contents($this->datadir.$path);
 	}
