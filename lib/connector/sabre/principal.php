192b8906a38aed893b9148954beaf6af85035d40
Bart Visscher <bartv@thisnet.nl>
2012-02-11 21:24:22 +0100

    Add copyright to OC_Connector_Sabre_Principal

--- lib/connector/sabre/principal.php
+++ lib/connector/sabre/principal.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright (c) 2011 Jakob Sack mail@jakobsack.de
+ * Copyright (c) 2012 Bart Visscher <bartv@thisnet.nl>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
 
 class OC_Connector_Sabre_Principal implements Sabre_DAVACL_IPrincipalBackend {
 	/**
