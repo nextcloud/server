f5385e4a63de85d777d20f93e56cbee859329608
Bart Visscher <bartv@thisnet.nl>
2012-02-12 16:12:46 +0100

    Contacts: Add id to vars with id in them

--- apps/contacts/export.php
+++ apps/contacts/export.php
@@ -9,20 +9,20 @@
 require_once ("../../lib/base.php");
 OC_Util::checkLoggedIn();
 OC_Util::checkAppEnabled('contacts');
-$book = isset($_GET['bookid']) ? $_GET['bookid'] : NULL;
-$contact = isset($_GET['contactid']) ? $_GET['contactid'] : NULL;
+$bookid = isset($_GET['bookid']) ? $_GET['bookid'] : NULL;
+$contactid = isset($_GET['contactid']) ? $_GET['contactid'] : NULL;
 $nl = "\n";
-if(isset($book)){
-	$addressbook = OC_Contacts_App::getAddressbook($book);
-	$cardobjects = OC_Contacts_VCard::all($book);
+if(isset($bookid)){
+	$addressbook = OC_Contacts_App::getAddressbook($bookid);
+	$cardobjects = OC_Contacts_VCard::all($bookid);
 	header('Content-Type: text/directory');
 	header('Content-Disposition: inline; filename=' . str_replace(' ', '_', $addressbook['displayname']) . '.vcf'); 
 
 	foreach($cardobjects as $card) {
 		echo $card['carddata'] . $nl;
 	}
-}elseif(isset($contact)){	
-	$data = OC_Contacts_App::getContactObject($contact);
+}elseif(isset($contactid)){
+	$data = OC_Contacts_App::getContactObject($contactid);
 	header('Content-Type: text/directory');
 	header('Content-Disposition: inline; filename=' . str_replace(' ', '_', $data['fullname']) . '.vcf'); 
 	echo $data['carddata'];
