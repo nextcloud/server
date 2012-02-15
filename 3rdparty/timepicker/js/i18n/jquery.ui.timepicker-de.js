dccdeca2581f705c69eb4266aa646173f588a9de
Frank Karlitschek <karlitschek@kde.org>
2012-02-15 20:40:37 +0100

    remove the 3rdparty files. everything is now in https://gitorious.org/owncloud/3rdparty

--- 3rdparty/timepicker/js/i18n/jquery.ui.timepicker-de.js
+++
@@ -1,9 +0,0 @@
-/* Deutsch initialisation for the timepicker plugin */
-/* Written by Bernd Plagge (bplagge@choicenet.ne.jp). */
-jQuery(function($){
-    $.timepicker.regional['de'] = {
-                hourText: 'Stunde',
-                minuteText: 'Minuten',
-                amPmText: ['AM', 'PM'] }
-    $.timepicker.setDefaults($.timepicker.regional['de']);
-});
