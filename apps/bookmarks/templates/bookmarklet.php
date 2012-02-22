358fd1c0241d1f8c21637490350c0eac6b29c230
Marvin Thomas Rabe <m.rabe@echtzeitraum.de>
2012-02-22 23:53:54 +0100

    Changed text

--- apps/bookmarks/templates/bookmarklet.php
+++ apps/bookmarks/templates/bookmarklet.php
@@ -2,7 +2,7 @@
 
 function createBookmarklet() {
 	$l = new OC_L10N('bookmarks');
-	echo '<small>' . $l->t('Drag this to your browser bookmarks and click it, when you want to bookmark a webpage:') . '</small>'
+	echo '<small>' . $l->t('Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:') . '</small>'
 	. '<a class="button" href="javascript:(function(){var a=window,b=document,c=encodeURIComponent,d=a.open(\'' . OC_Helper::linkToAbsolute('bookmarks', 'addBm.php') . '?output=popup&url=\'+c(b.location),\'bkmk_popup\',\'left=\'+((a.screenX||a.screenLeft)+10)+\',top=\'+((a.screenY||a.screenTop)+10)+\',height=230px,width=230px,resizable=1,alwaysRaised=1\');a.setTimeout(function(){d.focus()},300);})();">'
 	. $l->t('Read later') . '</a>';
 } 
