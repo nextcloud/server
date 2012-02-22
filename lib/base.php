2e05bd69f5b39f449f7e7475d81c27ee2b1f946a
Robin Appelman <icewind@owncloud.com>
2012-02-22 22:16:33 +0100

    add javascript bindings for OC_AppConfig

--- lib/base.php
+++ lib/base.php
@@ -205,6 +205,7 @@ class OC{
 		OC_Util::addScript( "jquery-tipsy" );
 		OC_Util::addScript( "js" );
 		OC_Util::addScript( "eventsource" );
+		OC_Util::addScript( "config" );
 		//OC_Util::addScript( "multiselect" );
 		OC_Util::addScript('search','result');
 		OC_Util::addStyle( "styles" );
