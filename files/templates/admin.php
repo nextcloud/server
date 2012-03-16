9c3ee830210e0184b30d0738bc2aba86c00dcf43
Arthur Schiwon <blizzz@owncloud.com>
2012-03-16 16:00:12 +0100

    make Files settings work

--- files/templates/admin.php
+++ files/templates/admin.php
@@ -1,10 +1,13 @@
 <?php OC_Util::addScript('files','admin'); ?>
 
 <form name="filesForm" action='#' method='post'>
+	<fieldset class="personalblock">
+		<legend><strong><?php echo $l->t('File handling');?></strong></legend>
 	<?php if($_['htaccessWorking']):?>
 		<label for="maxUploadSize"><?php echo $l->t( 'Maximum upload size' ); ?> </label><input name='maxUploadSize' id="maxUploadSize" value='<?php echo $_['uploadMaxFilesize'] ?>'/><br/>
 		<input type='submit' value='Save'/>
 	<?php else:?>
 		No settings currently available.
 	<?php endif;?>
+	</fieldset>
 </form>
