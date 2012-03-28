34a0128ddf01a44302f89a1dfa14bb9565a8ae47
Florian Hülsmann <fh@cbix.de>
2012-03-28 16:12:34 +0200

    whitespace indentation fix

--- apps/user_webfinger/appinfo/install.php
+++ apps/user_webfinger/appinfo/install.php
@@ -5,32 +5,32 @@ $appsDir = dirname($thisAppDir);
 $ownCloudDir = dirname($appsDir);
 $docRoot = $_SERVER['DOCUMENT_ROOT'];
 if(file_exists($docRoot . '/.well-known/host-meta')) {
-	OC_Log::write(
-		'user_webfinger',
-		$docRoot . "/.well-known already exists; installation aborted",
-		OC_Log::ERROR
-	);
+    OC_Log::write(
+        'user_webfinger',
+        $docRoot . "/.well-known already exists; installation aborted",
+        OC_Log::ERROR
+    );
 } else {
-	if(@symlink($thisAppDir, $docRoot . '/.well-known')) {
-		OC_Log::write(
-			'user_webfinger',
-			"Webfinger symlink created at " . $docRoot . "/.well-known",
-			OC_Log::INFO
-		);
-	} else {
-		if(@symlink($thisAppDir, $ownCloudDir . '/.well-known')) {
-			OC_Log::write(
-				'user_webfinger',
-				"Couldn't create webfinger symlink in document root, linked to " . $ownCloudDir . "/.well-known instead",
-				OC_Log::WARN
-			);
-		} else {
-			OC_Log::write(
-				'user_webfinger',
-				"Couldn't create webfinger symlink, either check write permissions or create the link manually!",
-				OC_Log::ERROR
-			);
-		}
-	}
+    if(@symlink($thisAppDir, $docRoot . '/.well-known')) {
+        OC_Log::write(
+            'user_webfinger',
+            "Webfinger symlink created at " . $docRoot . "/.well-known",
+            OC_Log::INFO
+        );
+    } else {
+        if(@symlink($thisAppDir, $ownCloudDir . '/.well-known')) {
+            OC_Log::write(
+                'user_webfinger',
+                "Couldn't create webfinger symlink in document root, linked to " . $ownCloudDir . "/.well-known instead",
+                OC_Log::WARN
+            );
+        } else {
+            OC_Log::write(
+                'user_webfinger',
+                "Couldn't create webfinger symlink, either check write permissions or create the link manually!",
+                OC_Log::ERROR
+            );
+        }
+    }
 }
 ?>
