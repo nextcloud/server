cf26f8a6a7751bd32eeb449ef39b743248ae3d1a
Georg Ehrke <dev@georgswebsite.de>
2012-03-31 17:32:43 +0100

    add public sharing for calendars and events

--- apps/calendar/share.php
+++ apps/calendar/share.php
@@ -1,1 +1,21 @@
- 
+<?php
+require_once('../../lib/base.php');
+$token = strip_tags($_GET['t']);
+$shared = OC_Calendar_Share::getElementByToken($token);
+$nl = "\n\r";
+if($shared['type'] == OC_Calendar_Share::CALENDAR){
+	$calendar = OC_Calendar_App::getCalendar($cal, false);
+	$calobjects = OC_Calendar_Object::all($cal);
+	header('Content-Type: text/Calendar');
+	header('Content-Disposition: inline; filename=' . $calendar['displayname'] . '.ics'); 
+	foreach($calobjects as $calobject){
+		echo $calobject['calendardata'] . $nl;
+	}
+}elseif($shared['type'] == OC_Calendar_Share::EVENT){
+	$data = OC_Calendar_App::getEventObject($shared['id'], false);
+	$calendarid = $data['calendarid'];
+	$calendar = OC_Calendar_App::getCalendar($calendarid);
+	header('Content-Type: text/Calendar');
+	header('Content-Disposition: inline; filename=' . $data['summary'] . '.ics'); 
+	echo $data['calendardata'];
+}
