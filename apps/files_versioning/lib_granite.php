6cf3f10d3546eda09ceb823af5c2cf1190f18743
Frank Karlitschek <karlitschek@kde.org>
2012-04-09 22:51:53 +0200

    fix includes

--- apps/files_versioning/lib_granite.php
+++ apps/files_versioning/lib_granite.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/blob.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/commit.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/repository.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tag.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tree.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tree/node.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/index.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/raw.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/loose.php');
-require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/packed.php');
+require_once(granite/git/blob.php');
+require_once(granite/git/commit.php');
+require_once(granite/git/repository.php');
+require_once(granite/git/tag.php');
+require_once(granite/git/tree.php');
+require_once(granite/git/tree/node.php');
+require_once(granite/git/object/index.php');
+require_once(granite/git/object/raw.php');
+require_once(granite/git/object/loose.php');
+require_once(granite/git/object/packed.php');
