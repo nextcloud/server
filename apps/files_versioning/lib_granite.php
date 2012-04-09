b0894b314845a52e979539cbf1e6bd94444dca54
Frank Karlitschek <karlitschek@kde.org>
2012-04-09 22:10:29 +0200

    Add Craigs granite library to 3rdparty and files_versioning. Still not working and lots ot stuff to do.

---
+++ apps/files_versioning/lib_granite.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/blob.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/commit.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/repository.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tag.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tree.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/tree/node.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/index.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/raw.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/loose.php');
+require_once(OC::$SERVERROOT . '/3rdparty/granite/git/object/packed.php');
