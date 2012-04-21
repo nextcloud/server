1a494978997a455806db2c48e32fc8e0f724b0ad
Frank Karlitschek <karlitschek@kde.org>
2012-04-21 22:55:18 +0200

    structure the information a bit more so that is easier readable

--- apps/contacts/templates/settings.php
+++ apps/contacts/templates/settings.php
@@ -3,10 +3,10 @@
 		<strong><?php echo $l->t('Contacts'); ?></strong><br />
 		<?php echo $l->t('CardDAV syncing addresses:'); ?>
 		<dl>
-		<dt><?php echo $l->t('Primary address (Kontact et al)'); ?></dt>
-		<dd><code><?php echo OC_Helper::linkToAbsolute('contacts', 'carddav.php'); ?>/</code></dd>
-		<dt><?php echo $l->t('iOS/OS X'); ?></dt>
-		<dd><code><?php echo OC_Helper::linkToAbsolute('contacts', 'carddav.php'); ?>/principals/<?php echo OC_User::getUser(); ?></code>/</dd>
+		<dt><b><?php echo $l->t('Primary address (Kontact et al)'); ?></b></dt>
+		<dd><code><i><?php echo OC_Helper::linkToAbsolute('contacts', 'carddav.php'); ?>/</i></code></dd>
+		<dt><b><?php echo $l->t('iOS/OS X'); ?></b></dt>
+		<dd><code><i><?php echo OC_Helper::linkToAbsolute('contacts', 'carddav.php'); ?>/principals/<?php echo OC_User::getUser(); ?></i></code>/</dd>
 		</dl>
 		Powered by <a href="http://geonames.org/" target="_blank">geonames.org webservice</a>
 	</fieldset>
