80ddf37bb76743672e4b5d754cb659b30cad2f71
Thomas Tanghus <thomas@tanghus.net>
2012-04-22 02:09:13 +0200

    Missing double quote.

--- apps/contacts/templates/index.php
+++ apps/contacts/templates/index.php
@@ -16,7 +16,7 @@
 </div>
 	<div id="bottomcontrols">
 		<form>
-			<img class="svg" id="contacts_newcontact" src="img/contact-new.svg" alt="<?php echo $l->t('Add Contact'); ?>  title="<?php echo $l->t('Add Contact'); ?>" />
+			<img class="svg" id="contacts_newcontact" src="img/contact-new.svg" alt="<?php echo $l->t('Add Contact'); ?>"  title="<?php echo $l->t('Add Contact'); ?>" />
 			<img class="svg" id="chooseaddressbook" src="../../core/img/actions/settings.svg" alt="<?php echo $l->t('Addressbooks'); ?>" title="<?php echo $l->t('Addressbooks'); ?>" />
 		</form>
 	</div>
