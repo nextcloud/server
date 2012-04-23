08fe6bdb27878e0fd860969b4a3325e12cde22fc
Georg Ehrke <dev@georgswebsite.de>
2012-04-23 16:17:35 +0200

    fix path in /apps/files/js/timezone.php

--- apps/files/js/timezone.js
+++ apps/files/js/timezone.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var visitortimezone = (-new Date().getTimezoneOffset()/60);
 	$.ajax({
 		type: "GET",
-		url: "ajax/timezone.php",
+		url: OC.filePath('files', 'ajax', 'timezone.php'),
 		data: 'time='+ visitortimezone,
 		success: function(){
 			location.reload();
