fb84d0aff803c77e65d2faa49f320c6e12639593
Georg Ehrke <dev@georgswebsite.de>
2012-04-24 21:37:19 +0200

    fix php fail

--- lib/base.php
+++ lib/base.php
@@ -287,7 +287,7 @@ class OC{
 		if(file_exists(OC::$APPSROOT . '/apps/' . OC::$REQUESTEDAPP . '/' . OC::$REQUESTEDFILE)){
 			require_once(OC::$APPSROOT . '/apps/' . OC::$REQUESTEDAPP . '/' . OC::$REQUESTEDFILE);
 		}else{
-			header('404 Not Found')
+			header('404 Not Found');
 		}
 	}
 
