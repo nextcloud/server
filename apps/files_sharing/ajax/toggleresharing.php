7d9587bab339fac0f8789d57180520a7d8e0a85c
Michael Gapczynski <GapczynskiM@gmail.com>
2012-04-25 11:54:16 -0400

    Forgot to remove error log messages...

--- apps/files_sharing/ajax/toggleresharing.php
+++ apps/files_sharing/ajax/toggleresharing.php
@@ -4,12 +4,9 @@ require_once('../../../lib/base.php');
 
 OC_JSON::checkAppEnabled('files_sharing');
 OC_JSON::checkAdminUser();
-error_log($_POST['resharing']);
 if ($_POST['resharing'] == true) {
-	error_log("enabling");
 	OC_Appconfig::setValue('files_sharing', 'resharing', 'yes');
 } else {
-	error_log("disabling");
 	OC_Appconfig::setValue('files_sharing', 'resharing', 'no');
 }
 
