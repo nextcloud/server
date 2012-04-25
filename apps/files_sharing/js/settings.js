6895c54a3fe4cbc690ce0e17154bfe2e40295bb0
Michael Gapczynski <GapczynskiM@gmail.com>
2012-04-25 14:35:30 -0400

    Fix javascript checkbox toggle for resharing

--- apps/files_sharing/js/settings.js
+++ apps/files_sharing/js/settings.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#allowResharing').bind('change', function() {
 		var checked = 1;
-		if (!$('#allowResharing').attr('checked')) {
+		if (!this.checked) {
 			checked = 0;
 		}
 		$.post(OC.filePath('files_sharing','ajax','toggleresharing.php'), 'resharing='+checked);
