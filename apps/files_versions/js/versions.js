a3dff248a1f4d34ccbf7bf0120109c0d910fc548
Sam Tuke <sam@donttravelempty.com>
2012-04-25 14:27:55 +0100

    fixed path to history script

--- apps/files_versions/js/versions.js
+++ apps/files_versions/js/versions.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	// Add history button to files/index.php
 	FileActions.register('file','History',function(){return OC.imagePath('core','actions/history')},function(filename){
-		window.location='../apps/files_versioning/history.php?file='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+		window.location='../apps/files_versions/history.php?path='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
 	});
 	
 });
