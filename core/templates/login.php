74b5e22a684c3095dbd48b56da1a10d955c20305
Frank Karlitschek <karlitschek@kde.org>
2012-04-26 23:17:46 +0200

    some more csrf fixes

--- core/templates/login.php
+++ core/templates/login.php
@@ -12,6 +12,7 @@
 		<p class="infield">
 			<label for="password" class="infield"><?php echo $l->t( 'Password' ); ?></label>
 			<input type="password" name="password" id="password" value="" required <?php echo !empty($_POST['user'])?'autofocus':''; ?> />
+			<input type="hidden" name="sectoken" id="sectoken" value="<?php echo($_['sectoken']); ?>"  />
 		</p>
 		<input type="checkbox" name="remember_login" value="1" id="remember_login" /><label for="remember_login"><?php echo $l->t('remember'); ?></label>
 		<input type="submit" id="submit" class="login" value="<?php echo $l->t( 'Log in' ); ?>" />
