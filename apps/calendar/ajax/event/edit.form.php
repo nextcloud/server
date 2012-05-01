5bf69237b2471df9ee979200b19e0ce203df2a31
Frank Karlitschek <frank@owncloud.org>
2012-05-01 18:46:15 +0200

    ported isLoggedIn

--- apps/calendar/ajax/event/edit.form.php
+++ apps/calendar/ajax/event/edit.form.php
@@ -8,7 +8,7 @@
 
  
 
-if(!OC_USER::isLoggedIn()) {
+if(!OCP\User::isLoggedIn()) {
 	die('<script type="text/javascript">document.location = oc_webroot;</script>');
 }
 OC_JSON::checkAppEnabled('calendar');
