eec7c3833c9c1f1e83549474d6ba4b34ce307761
Frank Karlitschek <frank@owncloud.org>
2012-05-02 19:02:41 +0200

    fix install

--- apps/contacts/appinfo/install.php
+++ apps/contacts/appinfo/install.php
@@ -1,4 +1,4 @@
 <?php
 if(!file_exists(OC::$WEBROOT.'/remote/carddav.php')){
-	file_put_contents(OC::$WEBROOT.'/remote/carddav.php', file_get_contents(OC::$APPROOT . '/apps/contacts/appinfo/remote.php'));
+	file_put_contents(OC::$WEBROOT.'/remote/carddav.php', file_get_contents(OC::$APPSROOT . '/apps/contacts/appinfo/remote.php'));
 }
--- apps/files/appinfo/install.php
+++ apps/files/appinfo/install.php
@@ -1,4 +1,4 @@
 <?php
 if(!file_exists(OC::$WEBROOT.'/remote/webdav.php')){
-	file_put_contents(OC::$WEBROOT.'/remote/webdav.php', file_get_contents(OC::$APPROOT . '/apps/files/appinfo/webdav.php'));
+	file_put_contents(OC::$WEBROOT.'/remote/webdav.php', file_get_contents(OC::$APPSROOT . '/apps/files/appinfo/webdav.php'));
 }
