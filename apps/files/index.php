0ff5ab65be708b8a96f12afb70fbc349db7cfd1d
Arthur Schiwon <blizzz@owncloud.com>
2012-05-04 16:38:06 +0200

    applying coding guideline

--- apps/files/index.php
+++ apps/files/index.php
@@ -25,26 +25,26 @@
 OCP\User::checkLoggedIn();
 
 // Load the files we need
-OCP\Util::addStyle( "files", "files" );
-OCP\Util::addscript( "files", "jquery.iframe-transport" );
-OCP\Util::addscript( "files", "jquery.fileupload" );
-OCP\Util::addscript( "files", "files" );
+OCP\Util::addStyle( 'files', 'files' );
+OCP\Util::addscript( 'files', 'jquery.iframe-transport' );
+OCP\Util::addscript( 'files', 'jquery.fileupload' );
+OCP\Util::addscript( 'files', 'files' );
 OCP\Util::addscript( 'files', 'filelist' );
 OCP\Util::addscript( 'files', 'fileactions' );
 if(!isset($_SESSION['timezone'])){
 	OCP\Util::addscript( 'files', 'timezone' );
 }
-OCP\App::setActiveNavigationEntry( "files_index" );
+OCP\App::setActiveNavigationEntry( 'files_index' );
 // Load the files
 $dir = isset( $_GET['dir'] ) ? stripslashes($_GET['dir']) : '';
 // Redirect if directory does not exist
 if(!OC_Filesystem::is_dir($dir.'/')) {
-	header("Location: ".$_SERVER['PHP_SELF']."");
+	header('Location: '.$_SERVER['PHP_SELF'].'');
 }
 
 $files = array();
 foreach( OC_Files::getdirectorycontent( $dir ) as $i ){
-	$i["date"] = OCP\Util::formatDate($i["mtime"] );
+	$i['date'] = OCP\Util::formatDate($i['mtime'] );
 	if($i['type']=='file'){
 		$fileinfo=pathinfo($i['name']);
 		$i['basename']=$fileinfo['filename'];
@@ -63,22 +63,22 @@ foreach( OC_Files::getdirectorycontent( $dir ) as $i ){
 
 // Make breadcrumb
 $breadcrumb = array();
-$pathtohere = "";
-foreach( explode( "/", $dir ) as $i ){
-	if( $i != "" ){
-		$pathtohere .= "/".str_replace('+','%20', urlencode($i));
-		$breadcrumb[] = array( "dir" => $pathtohere, "name" => $i );
+$pathtohere = '';
+foreach( explode( '/', $dir ) as $i ){
+	if( $i != '' ){
+		$pathtohere .= '/'.str_replace('+','%20', urlencode($i));
+		$breadcrumb[] = array( 'dir' => $pathtohere, 'name' => $i );
 	}
 }
 
 // make breadcrumb und filelist markup
 $list = new OCP\Template( "files", "part.list", "" );
-$list->assign( "files", $files );
-$list->assign( "baseURL", OCP\Util::linkTo("files", "index.php")."?dir=");
-$list->assign( "downloadURL", OCP\Util::linkTo("files", "download.php")."?file=");
+$list->assign( 'files', $files );
+$list->assign( 'baseURL', OCP\Util::linkTo('files', 'index.php').'?dir=');
+$list->assign( 'downloadURL', OCP\Util::linkTo('files', 'download.php').'?file=');
 $breadcrumbNav = new OCP\Template( "files", "part.breadcrumb", "" );
-$breadcrumbNav->assign( "breadcrumb", $breadcrumb );
-$breadcrumbNav->assign( "baseURL", OCP\Util::linkTo("files", "index.php")."?dir=");
+$breadcrumbNav->assign( 'breadcrumb', $breadcrumb );
+$breadcrumbNav->assign( 'baseURL', OCP\Util::linkTo('files', 'index.php').'?dir=');
 
 $upload_max_filesize = OCP\Util::computerFileSize(ini_get('upload_max_filesize'));
 $post_max_size = OCP\Util::computerFileSize(ini_get('post_max_size'));
@@ -89,11 +89,11 @@ $freeSpace=max($freeSpace,0);
 $maxUploadFilesize = min($maxUploadFilesize ,$freeSpace);
 
 $tmpl = new OCP\Template( "files", "index", "user" );
-$tmpl->assign( "fileList", $list->fetchPage() );
-$tmpl->assign( "breadcrumb", $breadcrumbNav->fetchPage() );
+$tmpl->assign( 'fileList', $list->fetchPage() );
+$tmpl->assign( 'breadcrumb', $breadcrumbNav->fetchPage() );
 $tmpl->assign( 'dir', $dir);
 $tmpl->assign( 'readonly', !OC_Filesystem::is_writable($dir));
-$tmpl->assign( "files", $files );
+$tmpl->assign( 'files', $files );
 $tmpl->assign( 'uploadMaxFilesize', $maxUploadFilesize);
 $tmpl->assign( 'uploadMaxHumanFilesize', OCP\Util::humanFileSize($maxUploadFilesize));
 $tmpl->assign( 'allowZipDownload', intval(OCP\Config::getSystemValue('allowZipDownload', true)));
