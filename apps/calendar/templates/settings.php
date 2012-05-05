aa4f01fbcc28615b01e41a3f34576adf5efd4761
Georg Ehrke <dev@georgswebsite.de>
2012-05-05 18:58:41 +0200

    fix pathes for cal andcarddav

--- apps/calendar/templates/settings.php
+++ apps/calendar/templates/settings.php
@@ -47,6 +47,6 @@
         </table>
 
         <?php echo $l->t('Calendar CalDAV syncing address:');?>
-        <code><?php echo OCP\Util::linkToAbsolute('calendar', 'caldav.php'); ?></code><br />
+        <code><?php echo OCP\Util::linkToAbsolute('remote', 'caldav.php'); ?></code><br />
         </fieldset>
 </form>
