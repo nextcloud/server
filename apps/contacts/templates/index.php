bede32be2445f609506b018deaa167dfb731399f
Thomas Tanghus <thomas@tanghus.net>
2012-05-06 13:49:40 +0200

    Contacts: Fix for Android browser again, again.

--- apps/contacts/templates/index.php
+++ apps/contacts/templates/index.php
@@ -8,12 +8,12 @@
 		<?php echo $this->inc("part.contacts"); ?>
 	</ul>
 </div>
-	<div id="bottomcontrols">
-		<form>
-			<button class="svg" id="contacts_newcontact" title="<?php echo $l->t('Add Contact'); ?>"><img class="svg" src="<?php echo OCP\Util::linkTo('contacts', 'img/contact-new.svg'); ?>" alt="<?php echo $l->t('Add Contact'); ?>"   /></button>
-			<button class="svg" id="chooseaddressbook" title="<?php echo $l->t('Addressbooks'); ?>"><img class="svg" src="core/img/actions/settings.svg" alt="<?php echo $l->t('Addressbooks'); ?>" /></button>
-		</form>
-	</div>
+<div id="bottomcontrols">
+	<form>
+		<button class="svg" id="contacts_newcontact" title="<?php echo $l->t('Add Contact'); ?>"><img class="svg" src="<?php echo OCP\Util::linkTo('contacts', 'img/contact-new.svg'); ?>" alt="<?php echo $l->t('Add Contact'); ?>"   /></button>
+		<button class="svg" id="chooseaddressbook" title="<?php echo $l->t('Addressbooks'); ?>"><img class="svg" src="core/img/actions/settings.svg" alt="<?php echo $l->t('Addressbooks'); ?>" /></button>
+	</form>
+</div>
 <div id="rightcontent" class="rightcontent" data-id="<?php echo $_['id']; ?>">
 	<?php
 		if ($_['id']){
