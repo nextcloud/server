890c57bad5fc98ba926c40312d1bc2673ae486e9
Michiel de Jong <michiel@unhosted.org>
2012-05-09 15:40:39 +0200

    fix requires in apps/remoteStorage/ajax/revoke.php

--- apps/remoteStorage/ajax/revokeToken.php
+++ apps/remoteStorage/ajax/revokeToken.php
@@ -29,9 +29,15 @@
 // Do not load FS ...
 $RUNTIME_NOSETUPFS = true;
 
+require_once('../../../lib/base.php');
+
+require_once('../../../lib/user.php');
+require_once('../../../lib/public/user.php');
+
+require_once('../../../lib/app.php');
+require_once('../../../lib/public/app.php');
  
 OCP\App::checkAppEnabled('remoteStorage');
-require_once('Sabre/autoload.php');
 require_once('../lib_remoteStorage.php');
 
 ini_set('default_charset', 'UTF-8');
