6f981738bb5285d01e839ff640c163acf1b2a184
Vitaly Kuznetsov <vitty@altlinux.ru>
2012-05-10 09:10:53 +0000

    Fix some non-utf8-friendly htmlentities (resulting in wrong display of utf8-encoded directory names for example)

--- apps/bookmarks/templates/list.php
+++ apps/bookmarks/templates/list.php
@@ -7,7 +7,7 @@
  * See the COPYING-README file.
  */
 ?>
-<input type="hidden" id="bookmarkFilterTag" value="<?php if(isset($_GET['tag'])) echo htmlentities($_GET['tag']); ?>" />
+<input type="hidden" id="bookmarkFilterTag" value="<?php if(isset($_GET['tag'])) echo htmlentities($_GET['tag'],ENT_COMPAT | ENT_HTML401,'utf-8'); ?>" />
 <div id="controls">
 	<input type="hidden" id="bookmark_add_id" value="0" />
 	<input type="text" id="bookmark_add_url" placeholder="<?php echo $l->t('Address'); ?>" class="bookmarks_input" />
