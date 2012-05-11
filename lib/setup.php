4462b261605e4ac8beff4f3f1b65b2ab9867a971
Michiel de Jong <michiel@unhosted.org>
2012-05-11 17:09:10 +0200

    oops, typo in lib/setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -322,7 +322,7 @@ class OC_Setup {
 		$content.= "RewriteRule ^.well-known/host-meta /public.php?service=host-meta [QSA,L]\n";
 		$content.= "RewriteRule ^.well-known/carddav /remote.php/carddav/ [R]\n";
 		$content.= "RewriteRule ^.well-known/caldav /remote.php/caldav/ [R]\n";
-		$content.= "RewriteRule ^apps/([^/]*)/(.*\.(css|php))$ index.php?app=$1&getfile=$2 [QSA,L]\n"
+		$content.= "RewriteRule ^apps/([^/]*)/(.*\.(css|php))$ index.php?app=$1&getfile=$2 [QSA,L]\n";
 		$content.= "RewriteRule ^remote/(.*) remote.php [QSA,L]\n";
 		$content.= "RewriteRule ^.well-known/carddav /apps/contacts/carddav.php [R]\n";
 		$content.= "RewriteRule ^.well-known/caldav /apps/calendar/caldav.php [R]\n";
