2e309aae9786f1dffd1e7aeed969833c2db7f00b
Michiel de Jong <michiel@unhosted.org>
2012-05-11 10:52:37 +0200

    announce OAuth2 implicit grant end-point the new way

--- apps/remoteStorage/appinfo/webfinger.php
+++ apps/remoteStorage/appinfo/webfinger.php
@@ -3,6 +3,6 @@
         rel="remoteStorage"
         template="<?php echo WF_BASEURL; ?>/apps/remoteStorage/WebDAV.php/<?php echo WF_USER; ?>/remoteStorage/{category}/"
         api="WebDAV"
-        auth="<?php echo WF_BASEURL; ?>/apps/remoteStorage/auth.php/<?php echo WF_USER; ?>">
+        auth="<?php echo WF_BASEURL; ?>/?app=remoteStorage&getfile=auth.php/<?php echo WF_USER; ?>">
     </Link>
 <?php } ?>
