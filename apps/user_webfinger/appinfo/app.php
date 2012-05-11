f0362dbf58b5df33c5a430587dd6970bb48d3a30
Michiel de Jong <michiel@unhosted.org>
2012-05-11 17:25:32 +0200

    register public.php?service=host-meta

--- apps/user_webfinger/appinfo/app.php
+++ apps/user_webfinger/appinfo/app.php
@@ -3,4 +3,5 @@ OCP\App::register( array(
   'order' => 11,
   'id' => 'user_webfinger',
   'name' => 'Webfinger' ));
+OCP\CONFIG::setAppValue('core', 'public_host-meta', '/apps/user_webfinger/host-meta.php');
 OCP\CONFIG::setAppValue('core', 'public_webfinger', '/apps/user_webfinger/webfinger.php');
