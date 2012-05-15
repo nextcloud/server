a3b06e96584432d49c8627281d57635824cf30d1
Michael Gapczynski <GapczynskiM@gmail.com>
2012-05-14 20:21:07 -0400

    Check if a private link points to a folder or file and change text accordingly

--- apps/files_sharing/ajax/email.php
+++ apps/files_sharing/ajax/email.php
@@ -3,9 +3,10 @@ OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('files_sharing');
 $user = OCP\USER::getUser();
 // TODO translations
-$subject = $user.' shared a file with you';
+$type = (strpos($_POST['file'], '.') === false) ? 'folder' : 'file';
+$subject = $user.' shared a '.$type.' with you';
 $link = $_POST['link'];
-$text = $user.' shared the file '.$_POST['file'].' with you. It is available for download here: '.$link;
+$text = $user.' shared the '.$type.' '.$_POST['file'].' with you. It is available for download here: '.$link;
 $fromaddress = OCP\Config::getUserValue($user, 'settings', 'email', 'sharing-noreply@'.$_SERVER['HTTP_HOST']);
 OC_Mail::send($_POST['toaddress'], $_POST['toaddress'], $subject, $text, $fromaddress, $user);
 
