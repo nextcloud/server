7d49bf7f6e71f05c69a9aa948b1988358b8b44dd
Georg Ehrke <dev@georgswebsite.de>
2012-05-18 11:48:18 +0200

    some changes in calendar repeat class

--- apps/calendar/lib/repeat.php
+++ apps/calendar/lib/repeat.php
@@ -12,12 +12,28 @@
 class OC_Calendar_Repeat{
 	/*
 	 * @brief returns the cache of an event
+	 * @param (int) $id - id of the event
+	 * @return (array) 
 	 */
-	public static function get();
+	public static function get($id){
+		$stmt = OCP\DB::prepare('SELECT * FROM *PREFIX*calendar_repeat WHERE eventid = ?');
+		$result = $stmt->execute(array($id));
+		$return = array();
+		while($row = $result->fetchRow()){
+			$return[] = $row;
+		}
+		return $return;
+	}
 	/*
 	 * @brief returns the cache of an event in a specific peroid
+	 * @param (int) $id - id of the event
+	 * @param (string) $from - start for period in UTC
+	 * @param (string) $until - end for period in UTC
+	 * @return (array)
 	 */
-	public static function get_inperiod();
+	public static function get_inperiod($id, $from, $until){
+		
+	}
 	/*
 	 * @brief returns the cache of all repeating events of a calendar
 	 */
