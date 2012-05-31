c3ccdbaa791ed20ed0c21718b11ffe110a0a1e6e
Frank Karlitschek <frank@owncloud.org>
2012-05-31 21:14:46 +0200

    more fixes

--- apps/files_versions/versions.php
+++ apps/files_versions/versions.php
@@ -81,7 +81,7 @@ class Storage {
 			Storage::init();
 
 			// check if filename is a directory
-			if(is_dir($filesfoldername.$filename)){
+			if(is_dir($filesfoldername.'/'.$filename)){
 				return false;
 			}
 
@@ -103,7 +103,7 @@ class Storage {
 
 			// check mininterval if the file is being modified by the owner (all shared files should be versioned despite mininterval)
 			if ($uid == \OCP\User::getUser()) {
-				$matches=glob($versionsfoldername.$filename.'.v*');
+				$matches=glob($versionsfoldername.'/'.$filename.'.v*');
 				sort($matches);
 				$parts=explode('.v',end($matches));
 				if((end($parts)+Storage::DEFAULTMININTERVAL)>time()){
@@ -114,10 +114,10 @@ class Storage {
 
 			// create all parent folders
 			$info=pathinfo($filename);	
-			@mkdir($versionsfoldername.$info['dirname'],0700,true);	
+			@mkdir($versionsfoldername.'/'.$info['dirname'],0700,true);	
 
 			// store a new version of a file
-			copy($filesfoldername.'/'.$filename,$versionsfoldername.$filename.'.v'.time());
+			copy($filesfoldername.'/'.$filename,$versionsfoldername.'/'.$filename.'.v'.time());
         
 			// expire old revisions
 			Storage::expire($filename);
@@ -143,7 +143,7 @@ class Storage {
 			$filesfoldername=\OCP\Config::getSystemValue('datadirectory').'/'. $uid .'/files';
 			
 			// rollback
-			if ( @copy($versionsfoldername.$filename.'.v'.$revision,$filesfoldername.$filename) ) {
+			if ( @copy($versionsfoldername.'/'.$filename.'.v'.$revision,$filesfoldername.'/'.$filename) ) {
 			
 				return true;
 				
@@ -172,7 +172,7 @@ class Storage {
 			$versionsfoldername=\OCP\Config::getSystemValue('datadirectory').'/'. $uid .'/'.\OCP\Config::getSystemValue('files_versionsfolder', Storage::DEFAULTFOLDER);
 
 			// check for old versions
-			$matches=glob($versionsfoldername.$filename.'.v*');
+			$matches=glob($versionsfoldername.'/'.$filename.'.v*');
 			if(count($matches)>1){
 				return true;
 			}else{
@@ -201,7 +201,7 @@ class Storage {
 			$versions=array();         
  
 	              // fetch for old versions
-			$matches=glob($versionsfoldername.$filename.'.v*');
+			$matches=glob($versionsfoldername.'/'.$filename.'.v*');
 			sort($matches);
 			foreach($matches as $ma) {
 				$parts=explode('.v',$ma);
@@ -238,14 +238,14 @@ class Storage {
 			$versionsfoldername=\OCP\Config::getSystemValue('datadirectory').'/'. $uid .'/'.\OCP\Config::getSystemValue('files_versionsfolder', Storage::DEFAULTFOLDER);
 
 			// check for old versions
-			$matches=glob($versionsfoldername.$filename.'.v*');
+			$matches=glob($versionsfoldername.'/'.$filename.'.v*');
 			if(count($matches)>\OCP\Config::getSystemValue('files_versionmaxversions', Storage::DEFAULTMAXVERSIONS)){
 				$numbertodelete=count($matches-\OCP\Config::getSystemValue('files_versionmaxversions', Storage::DEFAULTMAXVERSIONS));
 
 				// delete old versions of a file
 				$deleteitems=array_slice($matches,0,$numbertodelete);
 				foreach($deleteitems as $de){
-					unlink($versionsfoldername.$filename.'.v'.$de);
+					unlink($versionsfoldername.'/'.$filename.'.v'.$de);
 				}
 			}
                 }
