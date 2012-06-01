83c6714c723a1e224f2477192ca88bd6144b76e7
Frank Karlitschek <frank@owncloud.org>
2012-06-01 11:12:50 +0200

    don´t start a session here. I don´t think it´s needed. Let´s see if it breaks something

--- apps/files/ajax/timezone.php
+++ apps/files/ajax/timezone.php
@@ -1,6 +1,6 @@
 <?php
 	// FIXME: this should start a secure session if forcessl is enabled
 	// see lib/base.php for an example
-	session_start();
+	//session_start();
 	$_SESSION['timezone'] = $_GET['time'];
 ?>
