07e1e3a9454df55d27e3c52e6345a6a3aee1344a
Frank Karlitschek <frank@owncloud.org>
2012-06-01 19:54:07 +0200

    check if apps folder is writable

--- lib/util.php
+++ lib/util.php
@@ -28,6 +28,14 @@ class OC_Util {
 			exit;
 		}
 		
+		// Check if apps folder is writable.
+		if(!is_writable(OC::$SERVERROOT."/apps/")) {
+			$tmpl = new OC_Template( '', 'error', 'guest' );
+			$tmpl->assign('errors',array(1=>array('error'=>"Can't write into apps directory 'apps'",'hint'=>"You can usually fix this by giving the webserver user write access to the config directory in owncloud")));
+			$tmpl->printPage();
+			exit;
+		}
+		
 		// Create root dir.
 		if(!is_dir($CONFIG_DATADIRECTORY_ROOT)){
 			$success=@mkdir($CONFIG_DATADIRECTORY_ROOT);
