6b0a2ae60eb8d3beb5adad7022ce4f9ba9e25aee
Bartek Przybylski <bart.p.pl@gmail.com>
2012-06-08 20:34:10 +0200

    remove old code

--- apps/gallery/lib/hooks_handlers.php
+++ apps/gallery/lib/hooks_handlers.php
@@ -27,57 +27,6 @@ OCP\Util::connectHook(OC_Filesystem::CLASSNAME, OC_Filesystem::signal_delete, "O
 require_once(OC::$CLASSPATH['Pictures_Managers']);
 
 class OC_Gallery_Hooks_Handlers {
-	private static $APP_TAG = "Gallery";
-
-	private static function isPhoto($filename) {
-		$ext = strtolower(substr($filename, strrpos($filename, '.')+1));
-		return $ext=='png' || $ext=='jpeg' || $ext=='jpg' || $ext=='gif';
-	}
-
-	private static function directoryContainsPhotos($dirpath) {
-		$dirhandle = OC_Filesystem::opendir($dirpath.'/');
-		if ($dirhandle != FALSE) {
-			while (($filename = readdir($dirhandle)) != FALSE) {
-				if ($filename[0] == '.') continue;
-				if (self::isPhoto($dirpath.'/'.$filename)) return true;
-			}
-		}
-		return false;
-	}
-
-	private static function createAlbum($path) {
-		$new_album_name = trim(str_replace('/', '.', $path), '.');
-		if ($new_album_name == '')
-			$new_album_name = 'main';
-
-		OCP\Util::writeLog(self::$APP_TAG, 'Creating new album '.$new_album_name, OCP\Util::DEBUG);
-		OC_Gallery_Album::create(OCP\USER::getUser(), $new_album_name, $path);
-
-		return OC_Gallery_Album::find(OCP\USER::getUser(), null, $path);
-	}
-
-	public static function pathInRoot($path) {
-		$root = OCP\Config::getUserValue(OCP\USER::getUser(), 'gallery', 'root', '/');
-		return substr($path, 0, strlen($path)>strlen($root)?strlen($root):strlen($path)) == $root;
-	}
-
-	public static function addPhotoFromPath($params) {
-		$fullpath = $params[OC_Filesystem::signal_param_path];
-		$fullpath = rtrim(dirname($fullpath),'/').'/'.basename($fullpath);
-
-		if (!self::isPhoto($fullpath)) return;
-
-		$a = OC_Gallery_Album::find(OCP\USER::getUser(), null, dirname($fullpath));
-		if (!($r = $a->fetchRow())) {
-			OC_Gallery_Album::create(OCP\USER::getUser(), basename(dirname($fullpath)), dirname($fullpath));
-			$a = OC_Gallery_Album::find(OCP\USER::getUser(), null, dirname($fullpath));
-			$r = $a->fetchRow();
-		}
-		$albumId = $r['album_id'];
-		$p = OC_Gallery_Album::find($albumId, $fullpath);
-		if (!($p->fetchRow()))
-			OC_Gallery_Photo::create($albumId, $fullpath);
-	}
 
 	public static function removePhoto($params) {
 		\OC\Pictures\ThumbnailsManager::getInstance()->delete($params[OC_Filesystem::signal_param_path]);
@@ -86,46 +35,7 @@ class OC_Gallery_Hooks_Handlers {
 	public static function renamePhoto($params) {
 		$oldpath = $params[OC_Filesystem::signal_param_oldpath];
 		$newpath = $params[OC_Filesystem::signal_param_newpath];
-		if (OC_Filesystem::is_dir($newpath.'/') && self::directoryContainsPhotos($newpath)) {
-			OC_Gallery_Album::changePath($oldpath, $newpath, OCP\USER::getUser());
-		} elseif (self::isPhoto($newpath)) {
-			$olddir = dirname($oldpath);
-			$newdir = dirname($newpath);
-			if ($olddir == '') $olddir = '/';
-			if ($newdir == '') $newdir = '/';
-			if (!self::isPhoto($newpath)) return;
-			OCP\Util::writeLog(self::$APP_TAG, 'Moving photo from '.$oldpath.' to '.$newpath, OCP\Util::DEBUG);
-			$album;
-			$newAlbumId;
-			$oldAlbumId;
-			if ($olddir == $newdir) {
-				// album changing is not needed
-				$albums = OC_Gallery_Album::find(OCP\USER::getUser(), null, $olddir);
-				$album = $albums->fetchRow();
-				if (!$album) {
-					$albums = self::createAlbum($newdir);
-					$album = $albums->fetchRow();
-				}
-				$newAlbumId = $oldAlbumId = $album['album_id'];
-			} else {
-				$newalbum = OC_Gallery_Album::find(OCP\USER::getUser(), null, $newdir);
-				$oldalbum = OC_Gallery_Album::find(OCP\USER::getUser(), null, $olddir);
-
-				if (!($newalbum = $newalbum->fetchRow())) {
-					$newalbum = self::createAlbum($newdir);
-					$newalbum = $newalbum->fetchRow();
-				}
-				$oldalbum = $oldalbum->fetchRow();
-				if (!$oldalbum) {
-					OC_Gallery_Photo::create($newalbum['album_id'], $newpath);
-					return;
-				}
-				$newAlbumId = $newalbum['album_id'];
-				$oldAlbumId = $oldalbum['album_id'];
-
-			}
-			OC_Gallery_Photo::changePath($oldAlbumId, $newAlbumId, $oldpath, $newpath);
-		}
+		//TODO: implement this
 	}
 }
 
