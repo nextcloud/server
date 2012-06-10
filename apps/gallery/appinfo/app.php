2dca0926b031267a8414d4368ea01fae83daf6c8
Bartek Przybylski <bart.p.pl@gmail.com>
2012-06-10 09:52:01 +0200

    gallery: removing search provider for now

--- apps/gallery/appinfo/app.php
+++ apps/gallery/appinfo/app.php
@@ -55,6 +55,6 @@ class OC_GallerySearchProvider extends OC_Search_Provider{
 	}
 }
 
-OC_Search::registerProvider('OC_GallerySearchProvider');
+//OC_Search::registerProvider('OC_GallerySearchProvider');
 
 require_once('apps/gallery/lib/hooks_handlers.php');
