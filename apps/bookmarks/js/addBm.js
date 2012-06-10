f8337c9d723039760eecccf68bcb02752551e254
Lukas Reschke <lukas@statuscode.ch>
2012-06-10 23:38:26 +0200

    Using POST instead of GET.

--- apps/bookmarks/js/addBm.js
+++ apps/bookmarks/js/addBm.js
@@ -6,6 +6,7 @@ function addBookmark(event) {
 	var url = $('#bookmark_add_url').val();
 	var tags = $('#bookmark_add_tags').val();
 	$.ajax({
+		type: 'POST',
 		url: 'ajax/addBookmark.php',
 		data: 'url=' + encodeURI(url) + '&tags=' + encodeURI(tags),
 		success: function(data){ 
