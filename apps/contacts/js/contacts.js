e0a5e3e992bc59e3267c0ee3101c470adbb036f9
Thomas Tanghus <thomas@tanghus.net>
2012-06-11 22:13:45 +0200

    Fix js for missing address fields.

--- apps/contacts/js/contacts.js
+++ apps/contacts/js/contacts.js
@@ -842,22 +842,22 @@ Contacts={
 					$('#addressdisplay dl').last().data('checksum', this.data.ADR[adr]['checksum']);
 					var adrarray = this.data.ADR[adr]['value'];
 					var adrtxt = '';
-					if(adrarray[0].length > 0) {
+					if(adrarray[0] && adrarray[0].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[0].strip_tags() + '</li>';
 					}
-					if(adrarray[1].length > 0) {
+					if(adrarray[1] && adrarray[1].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[1].strip_tags() + '</li>';
 					}
-					if(adrarray[2].length > 0) {
+					if(adrarray[2] && adrarray[2].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[2].strip_tags() + '</li>';
 					}
-					if(adrarray[3].length > 0 || adrarray[5].length > 0) {
+					if((adrarray[3] && adrarray[5]) && adrarray[3].length > 0 || adrarray[5].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[5].strip_tags() + ' ' + adrarray[3].strip_tags() + '</li>';
 					}
-					if(adrarray[4].length > 0) {
+					if(adrarray[4] && adrarray[4].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[4].strip_tags() + '</li>';
 					}
-					if(adrarray[6].length > 0) {
+					if(adrarray[6] && adrarray[6].length > 0) {
 						adrtxt = adrtxt + '<li>' + adrarray[6].strip_tags() + '</li>';
 					}
 					$('#addressdisplay dl').last().find('.addresslist').html(adrtxt);
