d9607df26f63a60b7047d078876e004689190707
Bjoern Schiessle <schiessle@owncloud.com>
2012-06-20 16:43:31 +0200

    backport of sanitizeHTML() function

--- lib/public/util.php
+++ lib/public/util.php
@@ -266,6 +266,17 @@ class Util {
 		return(\OC_Util::callCheck());
 	}
 
+	/**
+	 * @brief Used to sanitize HTML
+	 *
+	 * This function is used to sanitize HTML and should be applied on any string or array of strings before displaying it on a web page.
+	 *
+	 * @param string or array of strings
+	 * @return array with sanitized strings or a single sinitized string, depends on the input parameter.
+	 */
+	public static function sanitizeHTML( $value ){
+		return(\OC_Util::sanitizeHTML($value));
+	}
 }
 
 ?>
