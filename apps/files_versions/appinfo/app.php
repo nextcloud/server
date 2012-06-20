72215a818d0e222af80000909f9fa78243112533
Sam Tuke <samtuke@owncloud.com>
2012-06-20 18:25:12 +0100

    added new user settings interface for deleting old file versions fixed OCA_VersionsexpireAll() to use native PHP readdir() removed local storage implementation of readdir()

--- apps/files_versions/appinfo/app.php
+++ apps/files_versions/appinfo/app.php
@@ -3,6 +3,8 @@
 require_once('apps/files_versions/versions.php');
 
 OCP\App::registerAdmin('files_versions', 'settings');
+OCP\App::registerPersonal('files_versions','settings-personal');
+
 OCP\Util::addscript('files_versions', 'versions');
 
 // Listen to write signals
