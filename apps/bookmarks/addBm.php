d0dbc8beabea8203c0f4d047a68cd5a440f3e2ab
Arthur Schiwon <blizzz@owncloud.com>
2012-06-20 20:03:25 +0200

    make bookmarks bookmarklet work again, kudos to Victor Dubiniuk

--- apps/bookmarks/addBm.php
+++ apps/bookmarks/addBm.php
@@ -5,20 +5,20 @@
 *
 * @author Arthur Schiwon
 * @copyright 2011 Arthur Schiwon blizzz@arthur-schiwon.de
-* 
+*
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
-* License as published by the Free Software Foundation; either 
+* License as published by the Free Software Foundation; either
 * version 3 of the License, or any later version.
-* 
+*
 * This library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
-*  
-* You should have received a copy of the GNU Lesser General Public 
+*
+* You should have received a copy of the GNU Lesser General Public
 * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-* 
+*
 */
 
 
@@ -28,6 +28,6 @@ OCP\User::checkLoggedIn();
 OCP\App::checkAppEnabled('bookmarks');
 
 require_once('bookmarksHelper.php');
-addBookmark($_POST['url'], '', 'Read-Later');
+addBookmark($_GET['url'], '', 'Read-Later');
 
 include 'templates/addBm.php';
