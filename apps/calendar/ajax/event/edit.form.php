37ed3b5bd20e516e9b99b024ff9d2ec0426fb1eb
Georg Ehrke <dev@georgswebsite.de>
2012-06-21 11:35:20 +0200

    make edit event form work with DateTime Property UTC

--- apps/calendar/ajax/event/edit.form.php
+++ apps/calendar/ajax/event/edit.form.php
@@ -27,6 +27,14 @@ $vevent = $object->VEVENT;
 $dtstart = $vevent->DTSTART;
 $dtend = OC_Calendar_Object::getDTEndFromVEvent($vevent);
 switch($dtstart->getDateType()) {
+	case Sabre_VObject_Property_DateTime::UTC:
+		$timeOffset = OC_Calendar_App::$tz*60;
+		$newDT      = $dtstart->getDateTime(); 
+		$newDT->add(new DateInterval("PT" . $timeOffset . "M"));
+		$dtstart->setDateTime($newDT); 
+		$newDT      = $dtend->getDateTime(); 
+		$newDT->add(new DateInterval("PT" . $timeOffset . "M"));
+		$dtend->setDateTime($newDT); 
 	case Sabre_VObject_Property_DateTime::LOCALTZ:
 	case Sabre_VObject_Property_DateTime::LOCAL:
 		$startdate = $dtstart->getDateTime()->format('d-m-Y');
