52f99a7570921f994035c96002b7ae46acdfea9b
Bjoern Schiessle <schiessle@owncloud.com>
2012-06-28 11:48:57 +0200

    delete file versions if the user deletes a file

--- apps/files_archive/appinfo/app.php
+++ apps/files_archive/appinfo/app.php
@@ -7,7 +7,9 @@
  */
 
 OC::$CLASSPATH['OC_Filestorage_Archive']='apps/files_archive/lib/storage.php';
+OC::$CLASSPATH['OC_Files_Versions_Hooks_Handler'] = 'files_versions/lib/hooks_handler.php';
 
 OCP\Util::connectHook('OC_Filesystem','get_mountpoint','OC_Filestorage_Archive','autoMount');
+OCP\Util::connectHook('OC_Filesystem', 'delete', "OC_Files_Versions_Hooks_Handler", "removeVersions");
 
 OCP\Util::addscript( 'files_archive', 'archive' );
