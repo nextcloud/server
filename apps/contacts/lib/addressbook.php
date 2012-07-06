cafbd023379073623295d11ae06be55a5f2cb569
Thomas Tanghus <thomas@tanghus.net>
2012-07-06 13:24:56 +0200

    Fix endless loop when no addressbooks.

--- apps/contacts/lib/addressbook.php
+++ apps/contacts/lib/addressbook.php
@@ -148,10 +148,17 @@ class OC_Contacts_Addressbook{
 	 * @return insertid
 	 */
 	public static function add($uid,$name,$description=''){
-		$all = self::all($uid);
+		try {
+			$stmt = OCP\DB::prepare( 'SELECT uri FROM *PREFIX*contacts_addressbooks WHERE userid = ? ' );
+			$result = $stmt->execute(array($uid));
+		} catch(Exception $e) {
+			OCP\Util::writeLog('contacts',__CLASS__.'::'.__METHOD__.' exception: '.$e->getMessage(),OCP\Util::ERROR);
+			OCP\Util::writeLog('contacts',__CLASS__.'::'.__METHOD__.' uid: '.$uid,OCP\Util::DEBUG);
+			return false;
+		}
 		$uris = array();
-		foreach($all as $i){
-			$uris[] = $i['uri'];
+		while($row = $result->fetchRow()){
+			$uris[] = $row['uri'];
 		}
 
 		$uri = self::createURI($name, $uris );
