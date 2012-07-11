d203fa2c50f4b2791e68e2b8ab9a0f8b94f9c9f8
Lukas Reschke <lukas@statuscode.ch>
2012-07-11 19:14:04 +0200

    Sanitizing file names

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -14,7 +14,7 @@ FileList={
 			var extension=false;
 		}
 		html+='<td class="filename" style="background-image:url('+img+')"><input type="checkbox" />';
-		html+='<a class="name" href="download.php?file='+$('#dir').val()+'/'+name+'"><span class="nametext">'+basename
+		html+='<a class="name" href="download.php?file='+$('#dir').val().replace(/</, '&lt;').replace(/>/, '&gt;')+'/'+name+'"><span class="nametext">'+basename
 		if(extension){
 			html+='<span class="extension">'+extension+'</span>';
 		}
