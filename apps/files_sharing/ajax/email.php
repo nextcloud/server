91a15bae76c4c4fca456c38fb51c8b944c92c341
Michael Gapczynski <mtgap@owncloud.com>
2012-07-11 16:02:43 -0400

    Revert santizing toaddress, because PHPMailer now throws exceptions

--- apps/files_sharing/ajax/email.php
+++ apps/files_sharing/ajax/email.php
@@ -5,10 +5,9 @@ OCP\JSON::callCheck();
 
 $user = OCP\USER::getUser();
 // TODO translations
-$toaddress = OCP\Util::sanitizeHtml($_POST['toaddress']);
 $type = (strpos($_POST['file'], '.') === false) ? 'folder' : 'file';
 $subject = $user.' shared a '.$type.' with you';
 $link = $_POST['link'];
 $text = $user.' shared the '.$type.' '.$_POST['file'].' with you. It is available for download here: '.$link;
 $fromaddress = OCP\Config::getUserValue($user, 'settings', 'email', 'sharing-noreply@'.OCP\Util::getServerHost());
-OCP\Util::sendMail($toaddress, $toaddress, $subject, $text, $fromaddress, $user);
+OCP\Util::sendMail($_POST['toaddress'], $_POST['toaddress'], $subject, $text, $fromaddress, $user);
