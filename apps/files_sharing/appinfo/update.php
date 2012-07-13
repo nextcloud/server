50eef9a71cfa88ef4b1e5162a288f29410306c95
Bjoern Schiessle <schiessle@owncloud.com>
2012-07-13 15:17:36 +0200

    update script to fix fscache

---
+++ apps/files_sharing/appinfo/update.php
@@ -0,0 +1,16 @@
+<?php
+
+// touch shared directories to trigger one-time re-scan for all users
+$datadir = \OCP\Config::getSystemValue('datadirectory');
+$currentVersion=OC_Appconfig::getValue('files_sharing', 'installed_version');
+if (version_compare($currentVersion, '0.2.2', '<')) {
+	if ($handle = opendir($datadir)) {
+		while (false !== ($entry = readdir($handle))) {
+			$sharedFolder = $datadir.'/'.$entry.'/files/Shared';
+			if ($entry != "." && $entry != ".." && is_dir($sharedFolder)) {
+				touch($sharedFolder);
+			}
+		}
+		closedir($handle);
+	}
+}
