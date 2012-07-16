7ab35af6dd9f0338f0a239aab20c045cfcbd34b5
Robin Appelman <icewind@owncloud.com>
2012-07-16 19:55:36 +0200

    use the remote.php infrastructure for remoteStorage

--- apps/remoteStorage/appinfo/webfinger.php
+++ apps/remoteStorage/appinfo/webfinger.php
@@ -1,8 +1,8 @@
-<?php if(OC_User::userExists(WF_USER)) { ?>
+<?php if(OC_User::userExists(WF_USER)): ?>
     {
         "rel":"remoteStorage",
-        "template":"<?php echo WF_BASEURL; ?>/apps/remoteStorage/WebDAV.php/<?php echo WF_USER; ?>/remoteStorage/{category}/",
+        "template":"<?php echo WF_BASEURL; ?>/remote.php/remoteStorage/<?php echo WF_USER; ?>/remoteStorage/{category}/",
         "api":"WebDAV",
         "auth":"<?php echo WF_BASEURL; ?>/?app=remoteStorage&getfile=auth.php&userid=<?php echo WF_USER; ?>"
     }
-<?php } ?>
+<?php endif ?>
