ffc55f351013b46c841ed1c477de6328169ac4cd
Georg Ehrke <dev@georgswebsite.de>
2012-07-19 16:35:14 +0200

    simplify code of remoteuser.php

--- settings/ajax/removeuser.php
+++ settings/ajax/removeuser.php
@@ -8,20 +8,10 @@ OCP\JSON::callCheck();
 
 $username = $_POST["username"];
 
-if(!OC_Group::inGroup(OC_User::getUser(), 'admin') && OC_SubAdmin::isSubAdmin(OC_User::getUser())){
-	$accessiblegroups = OC_SubAdmin::getSubAdminsGroups(OC_User::getUser());
-	$isuseraccessible = false;
-	foreach($accessiblegroups as $accessiblegroup){
-		if(OC_Group::inGroup($username, $accessiblegroup)){
-			$isuseraccessible = true;
-			break;
-		}
-	}
-	if(!$isuseraccessible){
-		$l = OC_L10N::get('core');
-		self::error(array( 'data' => array( 'message' => $l->t('Authentication error') )));
-		exit();
-	}
+if(!OC_Group::inGroup(OC_User::getUser(), 'admin') && !OC_SubAdmin::isUserAccessible(OC_User::getUser(), $username)){
+	$l = OC_L10N::get('core');
+	self::error(array( 'data' => array( 'message' => $l->t('Authentication error') )));
+	exit();
 }
 
 // Return Success story
