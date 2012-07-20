ad12e13f1473dfdf963bb69a196de5aa16e7ebe8
Bart Visscher <bartv@thisnet.nl>
2012-07-21 00:52:49 +0200

    Tasks: Remove unused code

--- apps/tasks/templates/part.edittaskform.php
+++
@@ -1,5 +0,0 @@
-<form id="tasks_edittaskform">
-	<input type="hidden" name="id" value="<?php echo $_['id']; ?>">
-	<?php echo $this->inc('part.taskform'); ?>
-	<input type="submit" name="submit" value="<?php echo $l->t('Update Task'); ?>">
-</form>
