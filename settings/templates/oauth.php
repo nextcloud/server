138c66a2ba1fdc7b44297bfe8498c200d6d2f250
Tom Needham <tom@owncloud.com>
2012-07-30 10:51:00 +0000

    Improve styling of permission request page

--- settings/templates/oauth.php
+++ settings/templates/oauth.php
@@ -5,15 +5,16 @@
  * See the COPYING-README file.
  */
 ?>
-
-<p><strong><?php echo $_['consumer']['name']; ?></strong> is requesting permission to read, write, modify and delete data from the following apps:</p>
-<ul>
-	<?php
-	// Foreach requested scope
-	foreach($_['consumer']['scopes'] as $app){
-		echo '<li>'.$app.'</li>';
-	}
-	?>
-</ul>
-<button>Disallow</button>
-<button>Allow</button>
+<div id="oauth-request" class="guest-container">
+	<p><strong><?php echo $_['consumer']['name']; ?></strong> is requesting permission to read, write, modify and delete data from the following apps:</p>
+	<ul>
+		<?php
+		// Foreach requested scope
+		foreach($_['consumer']['scopes'] as $app){
+			echo '<li>'.$app.'</li>';
+		}
+		?>
+	</ul>
+	<button>Allow</button>
+	<button>Disallow</button>
+</div>
