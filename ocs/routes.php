372fdf8077634d1b82db326db61a204ef6512892
Bart Visscher <bartv@thisnet.nl>
2012-07-30 20:37:35 +0200

    Add 'ocs' as app name to API registration

--- ocs/routes.php
+++ ocs/routes.php
@@ -6,20 +6,20 @@
  */
 
 // Config
-OC_API::register('get', '/config', array('OC_OCS_Config', 'apiConfig'));
+OC_API::register('get', '/config', array('OC_OCS_Config', 'apiConfig'), 'ocs');
 // Person
-OC_API::register('post', '/person/check', array('OC_OCS_Person', 'check'));
+OC_API::register('post', '/person/check', array('OC_OCS_Person', 'check'), 'ocs');
 // Activity
-OC_API::register('get', '/activity', array('OC_OCS_Activity', 'activityGet'));
+OC_API::register('get', '/activity', array('OC_OCS_Activity', 'activityGet'), 'ocs');
 // Privatedata
-OC_API::register('get', '/privatedata/getattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataGet'));
-OC_API::register('post', '/privatedata/setattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataPut'));
-OC_API::register('post', '/privatedata/deleteattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataDelete'));
+OC_API::register('get', '/privatedata/getattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataGet'), 'ocs');
+OC_API::register('post', '/privatedata/setattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataPut'), 'ocs');
+OC_API::register('post', '/privatedata/deleteattribute/{app}/{key}', array('OC_OCS_Privatedata', 'privatedataDelete'), 'ocs');
 // Cloud
-OC_API::register('get', '/cloud/system/webapps', array('OC_OCS_Cloud', 'systemwebapps'));
-OC_API::register('get', '/cloud/user/{user}', array('OC_OCS_Cloud', 'getQuota'));
-OC_API::register('post', '/cloud/user/{user}', array('OC_OCS_Cloud', 'setQuota'));
-OC_API::register('get', '/cloud/user/{user}/publickey', array('OC_OCS_Cloud', 'getPublicKey'));
-OC_API::register('get', '/cloud/user/{user}/privatekey', array('OC_OCS_Cloud', 'getPrivateKey'));
+OC_API::register('get', '/cloud/system/webapps', array('OC_OCS_Cloud', 'systemwebapps'), 'ocs');
+OC_API::register('get', '/cloud/user/{user}', array('OC_OCS_Cloud', 'getQuota'), 'ocs');
+OC_API::register('post', '/cloud/user/{user}', array('OC_OCS_Cloud', 'setQuota'), 'ocs');
+OC_API::register('get', '/cloud/user/{user}/publickey', array('OC_OCS_Cloud', 'getPublicKey'), 'ocs');
+OC_API::register('get', '/cloud/user/{user}/privatekey', array('OC_OCS_Cloud', 'getPrivateKey'), 'ocs');
 
 ?>
