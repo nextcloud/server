526e704c9f04bc689094083d070745ea8c661ff9
Frank Karlitschek <frank@owncloud.org>
2012-08-14 20:07:58 +0200

    4.0.7 and remove some ^M while at it

--- lib/util.php
+++ lib/util.php
@@ -83,7 +83,7 @@ class OC_Util {
 	 * @return array
 	 */
 	public static function getVersion(){
-		return array(4,00,6);
+		return array(4,00,7);
 	}
 
 	/**
@@ -91,7 +91,7 @@ class OC_Util {
 	 * @return string
 	 */
 	public static function getVersionString(){
-		return '4.0.6';
+		return '4.0.7';
 	}
 
         /**
