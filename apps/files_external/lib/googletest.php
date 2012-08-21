68f5dc2a44987945edb13754c9da8133a8f468f3
Michael Gapczynski <mtgap@owncloud.com>
2012-08-20 21:21:37 -0400

    Remove unnecessary files

--- apps/files_external/lib/googletest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'google.php';
-
-// $drive = new OC_Filestorage_Google(array('token' => '4/7nZMlRLlAEeXdY0AeH-eHNCL0YaK', 'token_secret' => 'NqO5VMGUVkwFtOYqHsex4257'));
-// $drive = new OC_Filestorage_Google(array('token' => '1/4Xo84YtTxL2MkQst-Ti3nqF1Isy70NUHDRk-BwsLMf4', 'token_secret' => 'jYnyJ_4-ITNxlX9f9RDNoRW-'));
-// var_export($drive->getFeed('https://docs.google.com/feeds/metadata/default', 'GET'));
-
