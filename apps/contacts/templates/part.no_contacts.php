069d16fdd441bcc4b1ba1ccd8f18f65cc4515ea4
Thomas Tanghus <thomas@tanghus.net>
2012-08-23 14:48:48 +0200

    Use correct js class in first-run template.

--- apps/contacts/templates/part.no_contacts.php
+++ apps/contacts/templates/part.no_contacts.php
@@ -2,7 +2,6 @@
 <div id="firstrun">
 	<?php echo $l->t('You have no contacts in your addressbook.') ?>
 	<div id="selections">
-		<input type="button" value="<?php echo $l->t('Add contact') ?>" onclick="Contacts.UI.Card.editNew()" />
-		<input type="button" value="<?php echo $l->t('Configure addressbooks') ?>" onclick="Contacts.UI.Addressbooks.overview()" />
+		<input type="button" value="<?php echo $l->t('Add contact') ?>" onclick="OC.Contacts.Card.editNew()" />
 	</div>
 </div>
