d91bd37bd7c576fcabab446d7c746727faaff5e1
Jörn Friedrich Dreyer <jfd@butonic.de>
2012-08-27 13:59:59 +0200

    fix two bugs in MDB2, patches committed upstream https://pear.php.net/bugs/bug.php?id=19582

--- 3rdparty/MDB2/Driver/mysql.php
+++ 3rdparty/MDB2/Driver/mysql.php
@@ -80,7 +80,7 @@ class MDB2_Driver_mysql extends MDB2_Driver_Common
 
     protected $start_transaction = false;
 
-    protected $varchar_max_length = 255;
+    public $varchar_max_length = 255;
 
     // }}}
     // {{{ constructor
