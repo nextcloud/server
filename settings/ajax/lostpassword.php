04033585b04f7811d036c19d56408ff97171e0c9
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 12:47:35 +0300

    Update settings/ajax/lostpassword.php
    respect coding style

--- settings/ajax/lostpassword.php
+++ settings/ajax/lostpassword.php
@@ -1,16 +1,16 @@
 <?php
 
 // Init owncloud
-require_once('../../lib/base.php');
+require_once '../../lib/base.php';
 OC_JSON::checkLoggedIn();
 OCP\JSON::callCheck();
 
 $l=OC_L10N::get('core');
 
 // Get data
-if( isset( $_POST['email'] ) && filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) ){
+if( isset( $_POST['email'] ) && filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 	$email=trim($_POST['email']);
-	OC_Preferences::setValue(OC_User::getUser(),'settings','email',$email);
+	OC_Preferences::setValue(OC_User::getUser(), 'settings', 'email', $email);
 	OC_JSON::success(array("data" => array( "message" => $l->t("Email saved") )));
 }else{
 	OC_JSON::error(array("data" => array( "message" => $l->t("Invalid email") )));
