4b3fc27a22214c322bf804b36043591a731183e5
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 12:58:07 +0300

    Update settings/ajax/togglegroups.php
    respect coding style

--- settings/ajax/togglegroups.php
+++ settings/ajax/togglegroups.php
@@ -1,7 +1,7 @@
 <?php
 
 // Init owncloud
-require_once('../../lib/base.php');
+require_once '../../lib/base.php';
 
 OC_JSON::checkSubAdminUser();
 OCP\JSON::callCheck();
@@ -13,23 +13,23 @@ $action = "add";
 $username = $_POST["username"];
 $group = OC_Util::sanitizeHTML($_POST["group"]);
 
-if(!OC_Group::inGroup(OC_User::getUser(), 'admin') && (!OC_SubAdmin::isUserAccessible(OC_User::getUser(), $username) || !OC_SubAdmin::isGroupAccessible(OC_User::getUser(), $group))){
+if(!OC_Group::inGroup(OC_User::getUser(), 'admin') && (!OC_SubAdmin::isUserAccessible(OC_User::getUser(), $username) || !OC_SubAdmin::isGroupAccessible(OC_User::getUser(), $group))) {
 	$l = OC_L10N::get('core');
 	OC_JSON::error(array( 'data' => array( 'message' => $l->t('Authentication error') )));
 	exit();
 }
 
-if(!OC_Group::groupExists($group)){
+if(!OC_Group::groupExists($group)) {
 	OC_Group::createGroup($group);
 }
 
 // Toggle group
-if( OC_Group::inGroup( $username, $group )){
+if( OC_Group::inGroup( $username, $group )) {
 	$action = "remove";
 	$error = "remove user from";
 	$success = OC_Group::removeFromGroup( $username, $group );
 	$usersInGroup=OC_Group::usersInGroup($group);
-	if(count($usersInGroup)==0){
+	if(count($usersInGroup)==0) {
 		OC_Group::deleteGroup($group);
 	}
 }
@@ -38,7 +38,7 @@ else{
 }
 
 // Return Success story
-if( $success ){
+if( $success ) {
 	OC_JSON::success(array("data" => array( "username" => $username, "action" => $action, "groupname" => $group )));
 }
 else{
