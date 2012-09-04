04aea1eb2ed0530f05928f680568c136acb1d5e5
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 12:50:16 +0300

    Update settings/ajax/removegroup.php
    $l->t added for error message

--- settings/ajax/removegroup.php
+++ settings/ajax/removegroup.php
@@ -13,5 +13,5 @@ if( OC_Group::deleteGroup( $name )) {
 	OC_JSON::success(array("data" => array( "groupname" => $name )));
 }
 else{
-	OC_JSON::error(array("data" => array( "message" => "Unable to delete group" )));
+	OC_JSON::error(array("data" => array( "message" => $l->t("Unable to delete group") )));
 }
