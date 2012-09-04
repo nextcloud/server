caaaf7a7bf55814a6a3bd44ae5a649c230317c31
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 12:45:54 +0300

    Update settings/ajax/creategroup.php
    respect coding style

--- settings/ajax/creategroup.php
+++ settings/ajax/creategroup.php
@@ -1,11 +1,11 @@
 <?php
 
 // Init owncloud
-require_once('../../lib/base.php');
+require_once '../../lib/base.php';
 OCP\JSON::callCheck();
 
 // Check if we are a user
-if( !OC_User::isLoggedIn() || !OC_Group::inGroup( OC_User::getUser(), 'admin' )){
+if( !OC_User::isLoggedIn() || !OC_Group::inGroup( OC_User::getUser(), 'admin' )) {
 	OC_JSON::error(array("data" => array( "message" => "Authentication error" )));
 	exit();
 }
@@ -15,13 +15,13 @@ OCP\JSON::callCheck();
 $groupname = $_POST["groupname"];
 
 // Does the group exist?
-if( in_array( $groupname, OC_Group::getGroups())){
+if( in_array( $groupname, OC_Group::getGroups())) {
 	OC_JSON::error(array("data" => array( "message" => "Group already exists" )));
 	exit();
 }
 
 // Return Success story
-if( OC_Group::createGroup( $groupname )){
+if( OC_Group::createGroup( $groupname )) {
 	OC_JSON::success(array("data" => array( "groupname" => $groupname )));
 }
 else{
