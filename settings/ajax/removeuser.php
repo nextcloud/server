93496a32cc3c28d36936d089857dd6fca870c0f8
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 12:54:04 +0300

    Update settings/ajax/removeuser.php
    $l->t added for error message

--- settings/ajax/removeuser.php
+++ settings/ajax/removeuser.php
@@ -19,5 +19,5 @@ if( OC_User::deleteUser( $username )) {
 	OC_JSON::success(array("data" => array( "username" => $username )));
 }
 else{
-	OC_JSON::error(array("data" => array( "message" => "Unable to delete user" )));
+	OC_JSON::error(array("data" => array( "message" => $l->t("Unable to delete user") )));
 }
