9e5807d1f9ae1b33c5cdbd6a1a126ec52270865e
Thomas Müller <thomas.mueller@tmit.eu>
2012-09-04 13:13:18 +0300

    fixing syntax error - sorry for that

--- core/lostpassword/templates/email.php
+++ core/lostpassword/templates/email.php
@@ -1,2 +1,2 @@
 <?php
-echo str_replace('{link}', $_['link'], $l->t('Use the following link to reset your password: {link}'))
+echo str_replace('{link}', $_['link'], $l->t('Use the following link to reset your password: {link}'));
