470b87f62574f62ce132cd24a9c014aac51ddc91
Tom Needham <needham.thomas@gmail.com>
2012-09-05 09:07:15 +0000

    Fix ocs/person/check

--- lib/ocs/person.php
+++ lib/ocs/person.php
@@ -3,10 +3,11 @@
 class OC_OCS_Person {
 
 	public static function check($parameters){
-	
-		if($parameters['login']<>''){
-			if(OC_User::login($parameters['login'],$parameters['password'])){
-				$xml['person']['personid'] = $parameters['login'];
+		$login = isset($_POST['login']) ? $_POST['login'] : false;
+		$password = isset($_POST['password']) ? $_POST['password'] : false;
+		if($login && $password){
+			if(OC_User::checkPassword($login,$password)){
+				$xml['person']['personid'] = $login;
 				return $xml;
 			}else{
 				return 102;
