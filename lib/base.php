bbf8bb0bb3f33c92933dfd499b571f56edda4a59
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2012-09-12 22:30:04 +0300

    Log PHP errors to the OC log

--- lib/base.php
+++ lib/base.php
@@ -317,6 +317,9 @@ class OC{
 
 		self::initPaths();
 
+		register_shutdown_function(array('OC_Log', 'onShutdown'));		
+		set_error_handler(array('OC_Log', 'onError' ));
+
 		// set debug mode if an xdebug session is active
 		if (!defined('DEBUG') || !DEBUG) {
 			if(isset($_COOKIE['XDEBUG_SESSION'])) {
--- lib/log.php
+++ lib/log.php
@@ -36,4 +36,21 @@ class OC_Log {
 		$log_class=self::$class;
 		$log_class::write($app, $message, $level);
 	}
+	
+	//Fatal errors handler
+	public static function onShutdown(){
+		$error = error_get_last();
+		if($error) {
+			//ob_end_clean();
+			self::write('PHP', $error['message'] . ' at ' . $error['file'] . '#' . $error['line'], self::FATAL);
+		} else {
+			return true; 
+		}
+	}
+
+	//Recoverable errors handler
+	public static function onError($number, $message, $file, $line){
+		self::write('PHP', $message . ' at ' . $file . '#' . $line, self::WARN);
+
+	}
 }
