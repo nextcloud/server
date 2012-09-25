e5487376dad48c73a0ba4acb6ed912bccfcd9623
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-25 02:05:18 +0200

    [tx-robot] updated from transifex

---
+++ apps/files_versions/l10n/es_AR.php
@@ -0,0 +1,8 @@
+<?php $TRANSLATIONS = array(
+"Expire all versions" => "Expirar todas las versiones",
+"History" => "Historia",
+"Versions" => "Versiones",
+"This will delete all existing backup versions of your files" => "Hacer estom borrará todas las versiones guardadas como copia de seguridad de tus archivos",
+"Files Versioning" => "Versionado de archivos",
+"Enable" => "Activar"
+);
--- apps/files_versions/l10n/eu.php
+++ apps/files_versions/l10n/eu.php
@@ -1,5 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Expire all versions" => "Iraungi bertsio guztiak",
+"History" => "Historia",
 "Versions" => "Bertsioak",
 "This will delete all existing backup versions of your files" => "Honek zure fitxategien bertsio guztiak ezabatuko ditu",
 "Files Versioning" => "Fitxategien Bertsioak",
