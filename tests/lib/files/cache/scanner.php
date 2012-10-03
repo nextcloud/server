08bb5dbe3a9b2bf502e5c6e0c944867015c3795b
Robin Appelman <icewind@owncloud.com>
2012-10-03 18:19:45 +0200

    few more test cases for scanning folders

--- tests/lib/files/cache/scanner.php
+++ tests/lib/files/cache/scanner.php
@@ -91,6 +91,13 @@ class Scanner extends \UnitTestCase {
 
 		$this->assertEqual($cachedDataFolder['size'], -1);
 		$this->assertEqual($cachedDataFolder2['size'], -1);
+
+		$this->scanner->scan('folder', \OC\Files\Cache\Scanner::SCAN_SHALLOW);
+
+		$cachedDataFolder = $this->cache->get('');
+		$cachedDataFolder2 = $this->cache->get('folder');
+
+		$this->assertNotEqual($cachedDataFolder['size'], -1);
 	}
 
 	function setUp() {
