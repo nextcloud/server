f96bf9eb814f292063456d362d179440aa3a08ac
Lukas Reschke <lukas@statuscode.ch>
2012-10-06 14:32:52 +0200

    Remove the webodf sources
    This is a backport of 683a0c1 /cc @DeepDiver1975

--- apps/files_odfviewer/src/update.sh
+++ apps/files_odfviewer/src/update.sh
@@ -1,9 +1,11 @@
-cd webodf
-git pull
-cd ..
+rm -rf webodf
+git clone https://git.gitorious.org/odfkit/webodf.git
 rm -Rf build
 mkdir build
 cd build
 cmake ../webodf
 make webodf.js webodf-debug.js
 cp webodf/webodf*.js ../../js/
+cd ..
+rm -rf webodf
+rm -rf build
