b934668b9e260866a465d36fef565aedf8f40339
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-08 02:07:01 +0200

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/ku_IQ.php
+++ apps/files_encryption/l10n/ku_IQ.php
@@ -1,3 +1,6 @@
 <?php $TRANSLATIONS = array(
-"Encryption" => "نهێنیکردن"
+"Encryption" => "نهێنیکردن",
+"Exclude the following file types from encryption" => "به‌ربه‌ست کردنی ئه‌م جۆره‌ په‌ڕگانه له‌ نهێنیکردن",
+"None" => "هیچ",
+"Enable Encryption" => "چالاکردنی نهێنیکردن"
 );
