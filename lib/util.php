fe5b4d2fbab18a9cf2bc690ce68ad3ba6ee7c787
Frank Karlitschek <frank@owncloud.org>
2012-10-10 15:35:19 +0200

    marks as 5.0 pre alpha

--- lib/util.php
+++ lib/util.php
@@ -81,7 +81,7 @@ class OC_Util {
 	 */
 	public static function getVersion() {
 		// hint: We only can count up. So the internal version number of ownCloud 4.5 will be 4.90.0. This is not visible to the user
-		return array(4,90,00);
+		return array(4,91,00);
 	}
 
 	/**
@@ -89,7 +89,7 @@ class OC_Util {
 	 * @return string
 	 */
 	public static function getVersionString() {
-		return '4.5';
+		return '5.0 pre alpha';
 	}
 
 	/**
