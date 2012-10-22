d8e945466da8aa39b7dfe849b305d5b1b33531d2
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-22 02:06:18 +0200

    [tx-robot] updated from transifex

---
+++ apps/user_ldap/l10n/id.php
@@ -0,0 +1,14 @@
+<?php $TRANSLATIONS = array(
+"Host" => "host",
+"Password" => "kata kunci",
+"User Login Filter" => "gunakan saringan login",
+"Group Filter" => "saringan grup",
+"Port" => "port",
+"Use TLS" => "gunakan TLS",
+"Do not use it for SSL connections, it will fail." => "jangan gunakan untuk koneksi SSL, itu akan gagal.",
+"Turn off SSL certificate validation." => "matikan validasi sertivikat SSL",
+"Not recommended, use for testing only." => "tidak disarankan, gunakan hanya untuk pengujian.",
+"in bytes" => "dalam bytes",
+"in seconds. A change empties the cache." => "dalam detik. perubahan mengosongkan cache",
+"Help" => "bantuan"
+);
