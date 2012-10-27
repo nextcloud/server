74665faec533430be37879bfc0ef24aa7f211f6e
Thomas Müller <thomas.mueller@tmit.eu>
2012-10-27 11:22:00 +0200

    exeuting tests now on apps as well - the apps repo shall be cloned into the sub folder apps2

---
+++ tests/preseed-config.php
@@ -0,0 +1,19 @@
+<?php
+$CONFIG = array (
+  "appstoreenabled" => false,
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/home/thomas/Development/owncloud/core/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/home/thomas/Development/owncloud/core/apps2',
+      'url' => '/apps2',
+      'writable' => false,
+    )
+    ),
+);
