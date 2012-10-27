a6f3a570c68440eeb51d8b25cf7a1cef0f723a15
Bernhard Posselt <nukeawhale@gmail.com>
2012-10-27 19:42:25 +0200

    dont resize content div with javascript but use css box-sizing to do it

--- core/js/js.js
+++ core/js/js.js
@@ -464,43 +464,7 @@ function object(o) {
 }
 
 
-/**
- * Fills height of window. (more precise than height: 100%;)
- */
-function fillHeight(selector) {
-	if (selector.length === 0) {
-		return;
-	}
-	var height = parseFloat($(window).height())-selector.offset().top;
-	selector.css('height', height + 'px');
-	if(selector.outerHeight() > selector.height()){
-		selector.css('height', height-(selector.outerHeight()-selector.height()) + 'px');
-	}
-}
-
-/**
- * Fills height and width of window. (more precise than height: 100%; or width: 100%;)
- */
-function fillWindow(selector) {
-	if (selector.length === 0) {
-		return;
-	}
-	fillHeight(selector);
-	var width = parseFloat($(window).width())-selector.offset().left;
-	selector.css('width', width + 'px');
-	if(selector.outerWidth() > selector.width()){
-		selector.css('width', width-(selector.outerWidth()-selector.width()) + 'px');
-	}
-}
-
 $(document).ready(function(){
-
-	$(window).resize(function () {
-		fillHeight($('#leftcontent'));
-		fillWindow($('#content'));
-		fillWindow($('#rightcontent'));
-	});
-	$(window).trigger('resize');
 
 	if(!SVGSupport()){ //replace all svg images with png images for browser that dont support svg
 		replaceSVG();
