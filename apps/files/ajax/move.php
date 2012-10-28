4fd0514f0af9286636c3665d02d10700fd0e3141
Georg Ehrke <dev@georgswebsite.de>
2012-10-28 15:20:20 +0100

    don't move file into subdirectory if a file with the same name already exists (fixes #92)

--- apps/files/ajax/move.php
+++ apps/files/ajax/move.php
@@ -12,6 +12,11 @@ $file = stripslashes($_GET["file"]);
 $target = stripslashes(urldecode($_GET["target"]));
 
 
+if(OC_Filesystem::file_exists($target . '/' . $file)){
+	OCP\JSON::error(array("data" => array( "message" => "Could not move $file - File with this name already exists" )));
+	exit;
+}
+
 if(OC_Files::move($dir, $file, $target, $file)) {
 	OCP\JSON::success(array("data" => array( "dir" => $dir, "files" => $file )));
 } else {
