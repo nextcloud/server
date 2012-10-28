7af8c6c3cbf00444b81a6d3d0c4cede983ff8edc
Jan-Christoph Borchardt <hey@jancborchardt.net>
2012-10-28 16:05:31 +0100

    first version of breadcrumb 'root', still have a weird feeling about this

--- apps/files/templates/part.breadcrumb.php
+++ apps/files/templates/part.breadcrumb.php
@@ -1,6 +1,13 @@
+	<?php if(count($_["breadcrumb"])):?>
+	<div class="crumb">
+		<a href="<?php echo $_['baseURL'].urlencode($crumb['dir']); ?>">
+			<img src="<?php echo OCP\image_path('core','places/home.svg');?>" />
+		</a>
+	</div>
+	<?php endif;?>
 	<?php for($i=0; $i<count($_["breadcrumb"]); $i++):
         $crumb = $_["breadcrumb"][$i]; ?>
-		<div class="crumb <?php if($i == count($_["breadcrumb"])-1) echo 'last';?> svg" data-dir='<?php echo urlencode($crumb["dir"]);?>' style='background-image:url("<?php echo OCP\image_path('core','breadcrumb.png');?>")'>
-		<a href="<?php echo $_['baseURL'].urlencode($crumb["dir"]); ?>"><?php echo OCP\Util::sanitizeHTML($crumb["name"]); ?></a>
+		<div class="crumb <?php if($i == count($_["breadcrumb"])-1) echo 'last';?> svg" data-dir='<?php echo urlencode($crumb["dir"]);?>'>
+		<a href="<?php echo $_['baseURL'].urlencode($crumb['dir']); ?>"><?php echo OCP\Util::sanitizeHTML($crumb["name"]); ?></a>
 		</div>
 	<?php endfor;?>
