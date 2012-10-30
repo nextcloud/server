b032819ab98ec1b06d66818d5d7a29189bd6720b
Jörn Friedrich Dreyer <jfd@butonic.de>
2012-10-30 11:08:32 +0100

    check for database error in userExists

--- lib/user/database.php
+++ lib/user/database.php
@@ -172,7 +172,10 @@ class OC_User_Database extends OC_User_Backend {
 	public function userExists($uid) {
 		$query = OC_DB::prepare( 'SELECT * FROM `*PREFIX*users` WHERE LOWER(`uid`) = LOWER(?)' );
 		$result = $query->execute( array( $uid ));
-
+		if (OC_DB::isError($result)) {
+			OC_Log::write('core', OC_DB::getErrorMessage($result), OC_Log::ERROR);
+			return false;
+		}
 		return $result->numRows() > 0;
 	}
 
