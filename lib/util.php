bcb9cf3c06cfea1059fceb4b180dca387adb389e
Frank Karlitschek <frank@owncloud.org>
2012-11-13 22:06:13 +0100

    4.5.2

--- lib/util.php
+++ lib/util.php
@@ -95,7 +95,7 @@ class OC_Util {
 	 */
 	public static function getVersion() {
 		// hint: We only can count up. So the internal version number of ownCloud 4.5 will be 4.90.0. This is not visible to the user
-		return array(4,90,2);
+		return array(4,90,3);
 	}
 
 	/**
@@ -103,7 +103,7 @@ class OC_Util {
 	 * @return string
 	 */
 	public static function getVersionString() {
-		return '4.5.1a';
+		return '4.5.2';
 	}
 
 	/**
