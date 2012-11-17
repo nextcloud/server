e28d71bf5511a8e6b687814a54e5448d2827cbbb
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-18 00:02:00 +0100

    [tx-robot] updated from transifex

---
+++ apps/files_encryption/l10n/ta_LK.php
@@ -0,0 +1,6 @@
+<?php $TRANSLATIONS = array(
+"Encryption" => "மறைக்குறியீடு",
+"Exclude the following file types from encryption" => "மறைக்குறியாக்கலில் பின்வரும் கோப்பு வகைகளை நீக்கவும்",
+"None" => "ஒன்றுமில்லை",
+"Enable Encryption" => "மறைக்குறியாக்கலை இயலுமைப்படுத்துக"
+);
--- lib/l10n/es.php
+++ lib/l10n/es.php
@@ -18,13 +18,17 @@
 "seconds ago" => "hace segundos",
 "1 minute ago" => "hace 1 minuto",
 "%d minutes ago" => "hace %d minutos",
+"1 hour ago" => "Hace 1 hora",
+"%d hours ago" => "Hace %d horas",
 "today" => "hoy",
 "yesterday" => "ayer",
 "%d days ago" => "hace %d días",
 "last month" => "este mes",
+"%d months ago" => "Hace %d meses",
 "last year" => "este año",
 "years ago" => "hace años",
 "%s is available. Get <a href=\"%s\">more information</a>" => "%s está disponible. Obtén <a href=\"%s\">más información</a>",
 "up to date" => "actualizado",
-"updates check is disabled" => "comprobar actualizaciones está desactivado"
+"updates check is disabled" => "comprobar actualizaciones está desactivado",
+"Could not find category \"%s\"" => "No puede encontrar la categoria \"%s\""
 );
