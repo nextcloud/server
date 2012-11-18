253e5f3cffdf9cff021cef7725aa7f30cb9825dd
Thomas Mueller <thomas.mueller@tmit.eu>
2012-11-18 12:27:55 +0100

    refs #505 - defining publicListView based on template parameter

--- apps/files_sharing/templates/public.php
+++ apps/files_sharing/templates/public.php
@@ -1,3 +1,11 @@
+<script type="text/javascript">
+	<?php if ( array_key_exists('publicListView', $_) && $_['publicListView'] == true ) {
+		echo "var publicListView = true;";
+	} else {
+		echo "var publicListView = false;";
+	}
+	?>
+</script>
 <input type="hidden" name="dir" value="<?php echo $_['dir'] ?>" id="dir">
 <input type="hidden" name="downloadURL" value="<?php echo $_['downloadURL'] ?>" id="downloadURL">
 <input type="hidden" name="filename" value="<?php echo $_['filename'] ?>" id="filename">
