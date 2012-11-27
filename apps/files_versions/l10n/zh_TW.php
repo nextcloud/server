1d4d4fd678cd3d76c4b8a0182ec97f65b9d97b0a
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-28 00:11:21 +0100

    [tx-robot] updated from transifex

---
+++ apps/files_versions/l10n/zh_TW.php
@@ -0,0 +1,6 @@
+<?php $TRANSLATIONS = array(
+"Expire all versions" => "所有逾期的版本",
+"History" => "歷史",
+"Versions" => "版本",
+"Enable" => "啟用"
+);
--- lib/l10n/pl.php
+++ lib/l10n/pl.php
@@ -18,13 +18,17 @@
 "seconds ago" => "sekund temu",
 "1 minute ago" => "1 minutę temu",
 "%d minutes ago" => "%d minut temu",
+"1 hour ago" => "1 godzine temu",
+"%d hours ago" => "%d godzin temu",
 "today" => "dzisiaj",
 "yesterday" => "wczoraj",
 "%d days ago" => "%d dni temu",
 "last month" => "ostatni miesiąc",
+"%d months ago" => "%d miesiecy temu",
 "last year" => "ostatni rok",
 "years ago" => "lat temu",
 "%s is available. Get <a href=\"%s\">more information</a>" => "%s jest dostępna. Uzyskaj <a href=\"%s\">więcej informacji</a>",
 "up to date" => "Aktualne",
-"updates check is disabled" => "wybór aktualizacji jest wyłączony"
+"updates check is disabled" => "wybór aktualizacji jest wyłączony",
+"Could not find category \"%s\"" => "Nie można odnaleźć kategorii \"%s\""
 );
