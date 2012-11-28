44dbdd9632d53b9398ae0ad0b1709641a879e496
Georg Ehrke <dev@georgswebsite.de>
2012-11-28 18:40:58 +0100

    add a client side check

--- settings/js/users.js
+++ settings/js/users.js
@@ -160,6 +160,9 @@ var UserList={
 			}
 			if(user){
 				var checkHandeler=function(group){
+					if(user==OC.currentUser && group=='admin'){
+						return false;
+					}
 					if(!isadmin && checked.length == 1 && checked[0] == group){
 						return false;
 					}
