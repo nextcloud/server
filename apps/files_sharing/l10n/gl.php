16a630024c3fc7a736dbcb150eb32701e4db6553
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-30 00:05:16 +0100

    [tx-robot] updated from transifex

--- apps/files_sharing/l10n/gl.php
+++ apps/files_sharing/l10n/gl.php
@@ -1,9 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Password" => "Contrasinal",
 "Submit" => "Enviar",
-"%s shared the folder %s with you" => "%s compartiu o cartafol %s contigo",
-"%s shared the file %s with you" => "%s compartiu ficheiro %s contigo",
-"Download" => "Baixar",
-"No preview available for" => "Sen vista previa dispoñible para ",
-"web services under your control" => "servizos web baixo o teu control"
+"%s shared the folder %s with you" => "%s compartiu o cartafol %s con vostede",
+"%s shared the file %s with you" => "%s compartiu o ficheiro %s con vostede",
+"Download" => "Descargar",
+"No preview available for" => "Sen vista previa dispoñíbel para",
+"web services under your control" => "servizos web baixo o seu control"
 );
