f512dea3bed0f86a53766e321f8bc64545db1721
Thomas Mueller <thomas.mueller@tmit.eu>
2012-12-06 22:23:39 +0100

    [files_external] fixing some undefined index

--- apps/files_external/ajax/removeMountPoint.php
+++ apps/files_external/ajax/removeMountPoint.php
@@ -3,6 +3,15 @@
 OCP\JSON::checkAppEnabled('files_external');
 OCP\JSON::callCheck();
 
+if (!isset($_POST['isPersonal']))
+	return;
+if (!isset($_POST['mountPoint']))
+	return;
+if (!isset($_POST['mountType']))
+	return;
+if (!isset($_POST['applicable']))
+	return;
+
 if ($_POST['isPersonal'] == 'true') {
 	OCP\JSON::checkLoggedIn();
 	$isPersonal = true;
@@ -10,4 +19,5 @@ if ($_POST['isPersonal'] == 'true') {
 	OCP\JSON::checkAdminUser();
 	$isPersonal = false;
 }
+
 OC_Mount_Config::removeMountPoint($_POST['mountPoint'], $_POST['mountType'], $_POST['applicable'], $isPersonal);
