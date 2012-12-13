727d95f091963ff0c0f5bebbdab0b788b204df53
Robin Appelman <icewind@owncloud.com>
2012-12-13 22:12:27 +0100

    remove unneeded line

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -286,7 +286,6 @@ var FileList={
 	},
 	finishDelete:function(ready,sync){
 		if(!FileList.deleteCanceled && FileList.deleteFiles){
-			var fileNames=FileList.deleteFiles.join(';');
 			var fileNames=JSON.stringify(FileList.deleteFiles);
 			$.ajax({
 				url: OC.filePath('files', 'ajax', 'delete.php'),
