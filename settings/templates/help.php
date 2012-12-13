2330d6bcc7a178d15b74eb8352f86e778afd717d
Frank Karlitschek <frank@owncloud.org>
2012-12-13 11:08:12 +0100

    show the link to the issue tracker only for Admins as discussed with Björn and Jan.

--- settings/templates/help.php
+++ settings/templates/help.php
@@ -3,7 +3,7 @@
         <?php if($_['admin']) { ?><a class="button newquestion <?php echo($_['style2']); ?>" href="<?php echo($_['url2']); ?>"><?php echo $l->t( 'Administrator Documentation' ); ?></a><?php } ?>
         <a class="button newquestion" href="http://owncloud.org/support" target="_blank"><?php echo $l->t( 'Online Documentation' ); ?></a>
         <a class="button newquestion" href="http://forum.owncloud.org" target="_blank"><?php echo $l->t( 'Forum' ); ?></a>
-        <a class="button newquestion" href="https://github.com/owncloud/core/issues" target="_blank"><?php echo $l->t( 'Bugtracker' ); ?></a>
+        <?php if($_['admin']) { ?><a class="button newquestion" href="https://github.com/owncloud/core/issues" target="_blank"><?php echo $l->t( 'Bugtracker' ); ?></a><?php } ?>
         <a class="button newquestion" href="http://owncloud.com" target="_blank"><?php echo $l->t( 'Commercial Support' ); ?></a>
 </div>
 <br /><br />
