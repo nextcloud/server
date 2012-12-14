9f5868cf187fe0eedef79c9e468c589560bab1e0
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-12-15 00:12:10 +0100

    [tx-robot] updated from transifex

--- core/l10n/it.php
+++ core/l10n/it.php
@@ -43,6 +43,7 @@
 "Share with link" => "Condividi con collegamento",
 "Password protect" => "Proteggi con password",
 "Password" => "Password",
+"Email link to person" => "Invia collegamento via email",
 "Send" => "Invia",
 "Set expiration date" => "Imposta data di scadenza",
 "Expiration date" => "Data di scadenza",
