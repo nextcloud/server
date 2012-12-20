436df6b67d1bcb9c4b5bd659319f2c0d38a0645b
Thomas Müller <thomas.mueller@tmit.eu>
2012-12-20 15:35:36 +0100

    enable translations for quota sizes 'none' and 'default'

--- settings/users.php
+++ settings/users.php
@@ -30,10 +30,15 @@ if($isadmin) {
 }
 
 foreach($accessibleusers as $i) {
+    $quota=OC_Preferences::getValue($i, 'files', 'quota', 'default');
+    // translation of old value 'none' to 'unlimited'
+    if ($quota=='none')
+        $quota='unlimited';
+
 	$users[] = array(
 		"name" => $i,
 		"groups" => join( ", ", /*array_intersect(*/OC_Group::getUserGroups($i)/*, OC_SubAdmin::getSubAdminsGroups(OC_User::getUser()))*/),
-		'quota'=>OC_Preferences::getValue($i, 'files', 'quota', 'Default'),
+		'quota'=>OC_Preferences::getValue($i, 'files', 'quota', 'default'),
 		'subadmin'=>implode(', ', OC_SubAdmin::getSubAdminsGroups($i)));
 }
 
@@ -41,13 +46,14 @@ foreach( $accessiblegroups as $i ) {
 	// Do some more work here soon
 	$groups[] = array( "name" => $i );
 }
-$quotaPreset=OC_Appconfig::getValue('files', 'quota_preset', 'Default, Unlimited, 1 GB, 5 GB, 10 GB');
+$quotaPreset=OC_Appconfig::getValue('files', 'quota_preset', '1 GB, 5 GB, 10 GB');
 $quotaPreset=explode(',', $quotaPreset);
 foreach($quotaPreset as &$preset) {
 	$preset=trim($preset);
 }
+$quotaPreset=array_diff($quotaPreset, array('default', 'none'));
 
-$defaultQuota=OC_Appconfig::getValue('files', 'default_quota', 'Unlimited');
+$defaultQuota=OC_Appconfig::getValue('files', 'default_quota', 'none');
 
 $tmpl = new OC_Template( "settings", "users", "user" );
 $tmpl->assign( "users", $users );
