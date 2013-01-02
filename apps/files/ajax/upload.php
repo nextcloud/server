0405edc7984c01b5ef4d6cb4ff5154e8bd4dca11
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-01-02 21:00:50 +0100

    add translation call for 'Not enough space available' upload error

--- apps/files/ajax/upload.php
+++ apps/files/ajax/upload.php
@@ -41,7 +41,7 @@ foreach($files['size'] as $size) {
 	$totalSize+=$size;
 }
 if($totalSize>OC_Filesystem::free_space($dir)) {
-	OCP\JSON::error(array('data' => array( 'message' => 'Not enough space available' )));
+	OCP\JSON::error(array('data' => array( 'message' => $l->t( 'Not enough space available' ))));
 	exit();
 }
 
