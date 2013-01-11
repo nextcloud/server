466cdab680d74cad2cbb902efa3e3c2f9e35f767
Lukas Reschke <lukas@statuscode.ch>
2013-01-11 14:18:51 +0100

    Add security section to admin menu
    Currently it only allows the admin to enable or disable the HTTPS
    enforcement, but in the future it could be expanded to further options.
    
    The HTTPS enforcement only allows the admin to enforce it, if he is
    connected via HTTPS. (To prevent admins to enable it without a proper
    SSL setup)

--- settings/routes.php
+++ settings/routes.php
@@ -58,6 +58,8 @@ $this->create('settings_ajax_getlog', '/settings/ajax/getlog.php')
 	->actionInclude('settings/ajax/getlog.php');
 $this->create('settings_ajax_setloglevel', '/settings/ajax/setloglevel.php')
 	->actionInclude('settings/ajax/setloglevel.php');
+$this->create('settings_ajax_setsecurity', '/settings/ajax/setsecurity.php')
+	->actionInclude('settings/ajax/setsecurity.php');
 
 // apps/user_openid
 $this->create('settings_ajax_openid', '/settings/ajax/openid.php')
