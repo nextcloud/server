9cfc195b7c233ceab6174039242ba34f200f0a31
Lukas Reschke <lukas@statuscode.ch>
2013-01-13 00:45:45 +0100

    The => This
    Sounds better for me.

--- apps/user_webdavauth/templates/settings.php
+++ apps/user_webdavauth/templates/settings.php
@@ -3,6 +3,6 @@
 		<legend><strong><?php echo $l->t('WebDAV Authentication');?></strong></legend>
 		<p><label for="webdav_url"><?php echo $l->t('URL: http://');?><input type="text" id="webdav_url" name="webdav_url" value="<?php echo $_['webdav_url']; ?>"></label>
 		<input type="submit" value="Save" />
-		<br /><?php echo $l->t('ownCloud will send the user credentials to this URL. The plugin checks the response and will interpret the HTTP statuscodes 401 and 403 as invalid credentials, and all other responses as valid credentials.'); ?>
+		<br /><?php echo $l->t('ownCloud will send the user credentials to this URL. This plugin checks the response and will interpret the HTTP statuscodes 401 and 403 as invalid credentials, and all other responses as valid credentials.'); ?>
 	</fieldset>
 </form>
