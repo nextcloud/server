a8f963d9cf5b243cae497450af78c20cbc367b4b
Bart Visscher <bartv@thisnet.nl>
2013-01-14 20:30:28 +0100

    Spaces to tabs

--- apps/files_versions/templates/settings.php
+++ apps/files_versions/templates/settings.php
@@ -1,6 +1,6 @@
 <form id="versionssettings">
-        <fieldset class="personalblock">
-	        <legend><strong><?php echo $l->t('Files Versioning');?></strong></legend>
-	        <input type="checkbox" name="versions" id="versions" value="1" <?php if (OCP\Config::getSystemValue('versions', 'true')=='true') echo ' checked="checked"'; ?> /> <label for="versions"><?php echo $l->t('Enable'); ?></label> <br/>
-        </fieldset>
+	<fieldset class="personalblock">
+		<legend><strong><?php echo $l->t('Files Versioning');?></strong></legend>
+		<input type="checkbox" name="versions" id="versions" value="1" <?php if (OCP\Config::getSystemValue('versions', 'true')=='true') echo ' checked="checked"'; ?> /> <label for="versions"><?php echo $l->t('Enable'); ?></label> <br/>
+	</fieldset>
 </form>
