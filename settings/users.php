nextcloud/server

commit 31b1a73e1f508a33843835b632ee05c7bfc2aaaf

Check if user is admin - bool
There was no "isAdminUser()" function which returned bool. This is
irritiating as there were a loooooooot of places in the code which
checked this itself with `OC_Group::inGroup($uid, 'admin)` - why not
use a function for this?
(Especially if you consider that we might change the group name in the
future, which would lead to problems then)

Additionally, @Raydiation needed such a method for his AppFramework :)

settings/users.php
@@ -18,8 +18,7 @@ OC_App::setActiveNavigationEntry( 'core_users' );
 $users = array();
 $groups = array();
 
-$isadmin = OC_Group::inGroup(OC_User::getUser(), 'admin')?true:false;
-if($isadmin) {
+if(OC_User::isAdminUser(OC_User::getUser())) {
 	$accessiblegroups = OC_Group::getGroups();
 	$accessibleusers = OC_User::getUsers('', 30);
 	$subadmins = OC_SubAdmin::getAllSubAdmins();

lib/api.php
@@ -127,8 +127,7 @@ class OC_API {
 					return false;
 				} else {
 					$subAdmin = OC_SubAdmin::isSubAdmin($user);
-					$admin = OC_Group::inGroup($user, 'admin');
-					if($subAdmin || $admin) {
+					if($subAdmin) {
 						return true;
 					} else {
 						return false;
@@ -141,7 +140,7 @@ class OC_API {
 				if(!$user) {
 					return false;
 				} else {
-					return OC_Group::inGroup($user, 'admin');
+					return OC_User::isAdminUser($user);
 				}
 				break;
 			default:

lib/app.php
@@ -313,14 +313,14 @@ class OC_App{
 				$settings[]=array( "id" => "settings", "order" => 1000, "href" => OC_Helper::linkToRoute( "settings_settings" ), "name" => $l->t("Settings"), "icon" => OC_Helper::imagePath( "settings", "settings.svg" ));
 
 			//SubAdmins are also allowed to access user management
-			if(OC_SubAdmin::isSubAdmin($_SESSION["user_id"]) || OC_Group::inGroup( $_SESSION["user_id"], "admin" )) {
+			if(OC_SubAdmin::isSubAdmin(OC_User::getUser())) {
 				// admin users menu
 				$settings[] = array( "id" => "core_users", "order" => 2, "href" => OC_Helper::linkToRoute( "settings_users" ), "name" => $l->t("Users"), "icon" => OC_Helper::imagePath( "settings", "users.svg" ));
 			}
 
 
 			// if the user is an admin
-			if(OC_Group::inGroup( $_SESSION["user_id"], "admin" )) {
+			if(OC_User::isAdminUser(OC_User::getUser())) {
 				// admin apps menu
 				$settings[] = array( "id" => "core_apps", "order" => 3, "href" => OC_Helper::linkToRoute( "settings_apps" ).'?installed', "name" => $l->t("Apps"), "icon" => OC_Helper::imagePath( "settings", "apps.svg" ));
 

lib/json.php
@@ -57,9 +57,7 @@ class OC_JSON{
 	* Check if the user is a admin, send json error msg if not
 	*/
 	public static function checkAdminUser() {
-		self::checkLoggedIn();
-		self::verifyUser();
-		if( !OC_Group::inGroup( OC_User::getUser(), 'admin' )) {
+		if( !OC_User::isAdminUser(OC_User::getUser())) {
 			$l = OC_L10N::get('lib');
 			self::error(array( 'data' => array( 'message' => $l->t('Authentication error') )));
 			exit();
@@ -70,9 +68,7 @@ class OC_JSON{
 	* Check if the user is a subadmin, send json error msg if not
 	*/
 	public static function checkSubAdminUser() {
-		self::checkLoggedIn();
-		self::verifyUser();
-		if(!OC_Group::inGroup(OC_User::getUser(), 'admin') && !OC_SubAdmin::isSubAdmin(OC_User::getUser())) {
+		if(!OC_SubAdmin::isSubAdmin(OC_User::getUser())) {
 			$l = OC_L10N::get('lib');
 			self::error(array( 'data' => array( 'message' => $l->t('Authentication error') )));
 			exit();

lib/migrate.php
@@ -219,7 +219,7 @@ class OC_Migrate{
 
 		// We need to be an admin if we are not importing our own data
 		if(($type == 'user' && self::$uid != $currentuser) || $type != 'user' ) {
-			if( !OC_Group::inGroup( OC_User::getUser(), 'admin' )) {
+			if( !OC_User::isAdminUser($currentuser)) {
 				// Naughty.
 				OC_Log::write( 'migration', 'Import not permitted.', OC_Log::ERROR );
 				return json_encode( array( 'success' => false ) );

lib/ocs/cloud.php
@@ -40,7 +40,7 @@ class OC_OCS_Cloud {
 	
 	public static function getUserQuota($parameters) {
 		$user = OC_User::getUser();
-		if(OC_Group::inGroup($user, 'admin') or ($user==$parameters['user'])) {
+		if(OC_User::isAdminUser($user) or ($user==$parameters['user'])) {
 
 			if(OC_User::userExists($parameters['user'])) {
 				// calculate the disc space
@@ -82,7 +82,7 @@ class OC_OCS_Cloud {
 	
 	public static function getUserPrivatekey($parameters) {
 		$user = OC_User::getUser();
-		if(OC_Group::inGroup($user, 'admin') or ($user==$parameters['user'])) {
+		if(OC_User::isAdminUser($user) or ($user==$parameters['user'])) {
 
 			if(OC_User::userExists($user)) {
 				// calculate the disc space

lib/subadmin.php
@@ -122,6 +122,11 @@ class OC_SubAdmin{
 	 * @return bool
 	 */
 	public static function isSubAdmin($uid) {
+		// Check if the user is already an admin
+		if(OC_Group::inGroup($uid, 'admin' )) {
+			return true;
+		}
+
 		$stmt = OC_DB::prepare('SELECT COUNT(*) AS `count` FROM `*PREFIX*group_admin` WHERE `uid` = ?');
 		$result = $stmt->execute(array($uid));
 		$result = $result->fetchRow();
@@ -141,7 +146,7 @@ class OC_SubAdmin{
 		if(!self::isSubAdmin($subadmin)) {
 			return false;
 		}
-		if(OC_Group::inGroup($user, 'admin')) {
+		if(OC_User::isAdminUser($user)) {
 			return false;
 		}
 		$accessiblegroups = self::getSubAdminsGroups($subadmin);

lib/user.php
@@ -300,6 +300,19 @@ class OC_User {
 	}
 
 	/**
+	 * @brief Check if the user is an admin user
+	 * @param $uid uid of the admin
+	 * @returns bool
+	 */
+	public static function isAdminUser($uid) {
+		if(OC_Group::inGroup($uid, 'admin' )) {
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
 	 * @brief get the user id of the user currently logged in.
 	 * @return string uid or false
 	 */

commit 65ef780ed69dcbddf803c57d720d625fb2848d84

Fix regression caused by 31b1a73e1f508a33843835b632ee05c7bfc2aaaf

settings/users.php
@@ -18,7 +18,9 @@ OC_App::setActiveNavigationEntry( 'core_users' );
 $users = array();
 $groups = array();
 
-if(OC_User::isAdminUser(OC_User::getUser())) {
+$isadmin = OC_User::isAdminUser(OC_User::getUser());
+
+if($isadmin) {
 	$accessiblegroups = OC_Group::getGroups();
 	$accessibleusers = OC_User::getUsers('', 30);
 	$subadmins = OC_SubAdmin::getAllSubAdmins();