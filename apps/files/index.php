bb43cf378b5e7c6a18a30d8fbb226b72f1e4eb88
Robin Appelman <icewind@owncloud.com>
2013-01-16 21:36:25 +0100

    Files: make sure keybinds js is loaded after files js

--- apps/files/index.php
+++ apps/files/index.php
@@ -30,7 +30,6 @@ OCP\Util::addscript('files', 'jquery.iframe-transport');
 OCP\Util::addscript('files', 'jquery.fileupload');
 OCP\Util::addscript('files', 'filelist');
 OCP\Util::addscript('files', 'fileactions');
-OCP\Util::addscript('files', 'keyboardshortcuts');
 
 OCP\App::setActiveNavigationEntry('files_index');
 // Load the files
@@ -122,6 +121,7 @@ if ($needUpgrade) {
 	$tmpl->printPage();
 } else {
 	OCP\Util::addscript('files', 'files');
+	OCP\Util::addscript('files', 'keyboardshortcuts');
 	$tmpl = new OCP\Template('files', 'index', 'user');
 	$tmpl->assign('fileList', $list->fetchPage(), false);
 	$tmpl->assign('breadcrumb', $breadcrumbNav->fetchPage(), false);
