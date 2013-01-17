eb9cd0858eb81b5783fa92c34d6f5b131b80e079
TheSFReader <TheSFReader@gmail.com>
2013-01-17 21:57:04 +0100

    Add ebook specific mimetypes

--- lib/mimetypes.list.php
+++ lib/mimetypes.list.php
@@ -95,4 +95,7 @@ return array(
 	'cdr' => 'application/coreldraw',
 	'impress' => 'text/impress',
 	'ai' => 'application/illustrator',
+	'epub' => 'application/epub+zip',
+	'mobi' => 'application/x-mobipocket-ebook',
+
 );
