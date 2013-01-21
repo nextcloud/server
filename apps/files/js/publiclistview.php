0d02caa684a4ecad8d69e74007c3f9893303f428
Lukas Reschke <lukas@statuscode.ch>
2013-01-21 20:34:28 +0100

    No inline JS in apps

---
+++ apps/files/js/publiclistview.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2013 Lukas Reschke <lukas@statuscode.ch>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+// Set the content type to Javascript
+header("Content-type: text/javascript");
+
+// Disallow caching
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+
+if ( array_key_exists('publicListView', $_) && $_['publicListView'] == true )  {
+	echo "var publicListView = true;";
+} else {
+	echo "var publicListView = false;";
+}
+?>
