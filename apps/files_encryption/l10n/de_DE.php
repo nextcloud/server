af4411e33d7a5dc8acde11a9dc88d1dc72dea0ec
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-01-26 00:10:07 +0100

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/de_DE.php
+++ apps/files_encryption/l10n/de_DE.php
@@ -1,4 +1,7 @@
 <?php $TRANSLATIONS = array(
+"Choose encryption mode:" => "Wählen Sie die Verschlüsselungsart:",
+"None (no encryption at all)" => "Keine (ohne Verschlüsselung)",
+"User specific (let the user decide)" => "Benutzerspezifisch (der Benutzer kann entscheiden)",
 "Encryption" => "Verschlüsselung",
 "Exclude the following file types from encryption" => "Die folgenden Dateitypen von der Verschlüsselung ausnehmen",
 "None" => "Keine"
