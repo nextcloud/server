9282641d06db4912098cb41f1c8a59b3eca4ac0f
Lukas Reschke <lukas@statuscode.ch>
2013-02-03 20:02:29 +0100

    [user_webdavauth] Add requesttoken

--- apps/user_webdavauth/templates/settings.php
+++ apps/user_webdavauth/templates/settings.php
@@ -2,6 +2,7 @@
 	<fieldset class="personalblock">
 		<legend><strong>WebDAV Authentication</strong></legend>
 		<p><label for="webdav_url"><?php echo $l->t('WebDAV URL: http://');?><input type="text" id="webdav_url" name="webdav_url" value="<?php echo $_['webdav_url']; ?>"></label>
+			<input type="hidden" name="requesttoken" value="<?php echo $_['requesttoken'] ?>" id="requesttoken">
 		<input type="submit" value="Save" />
 	</fieldset>
 </form>
