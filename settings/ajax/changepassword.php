81de09711b27bbe5ba421448671bfdf7cf48be22
Sam Tuke <samtuke@owncloud.com>
2013-02-06 17:42:29 +0000

    Fixed bug causing password change related hooks to not be called due to ajax

--- settings/ajax/changepassword.php
+++ settings/ajax/changepassword.php
@@ -4,6 +4,8 @@
 OCP\JSON::callCheck();
 OC_JSON::checkLoggedIn();
 
+OC_APP::loadApps();
+
 $username = isset($_POST["username"]) ? $_POST["username"] : OC_User::getUser();
 $password = $_POST["password"];
 $oldPassword=isset($_POST["oldpassword"])?$_POST["oldpassword"]:'';
