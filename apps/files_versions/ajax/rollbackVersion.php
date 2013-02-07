e0212c11747423abc3720901cf69e69171aa5f9d
Thomas Mueller <thomas.mueller@tmit.eu>
2013-02-07 16:34:57 +0100

    l10n support enhanced in files_versions

--- apps/files_versions/ajax/rollbackVersion.php
+++ apps/files_versions/ajax/rollbackVersion.php
@@ -12,6 +12,6 @@ if(OCA\Files_Versions\Storage::rollback( $file, $revision )) {
 	OCP\JSON::success(array("data" => array( "revision" => $revision, "file" => $file )));
 }else{
 	$l = OC_L10N::get('files_versions');
-	OCP\JSON::error(array("data" => array( "message" => $l->t("Could not revert: %s", $file ))));
+	OCP\JSON::error(array("data" => array( "message" => $l->t("Could not revert: %s", array($file) ))));
 }
 
