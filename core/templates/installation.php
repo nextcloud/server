b356b7065f406cd2e18ed7bb9842e292f888c8e4
raghunayyar <me@iraghu.com>
2013-02-08 16:38:28 +0530

    Password Toggle Works on Installation as well.

--- core/templates/installation.php
+++ core/templates/installation.php
@@ -34,13 +34,13 @@
 	<?php endif; ?>
 	<fieldset id="adminaccount">
 		<legend><?php echo $l->t( 'Create an <strong>admin account</strong>' ); ?></legend>
-		<p class="infield grouptop">
-			<input type="text" name="adminlogin" id="adminlogin" value="<?php print OC_Helper::init_var('adminlogin'); ?>" autocomplete="off" autofocus required />
+		<p class="infield grouptop">		
+			<input type="text" name="adminlogin" id="adminlogin" value="<?php print OC_Helper::init_var('adminlogin'); ?>" autocomplete="off" autofocus required />			
 			<label for="adminlogin" class="infield"><?php echo $l->t( 'Username' ); ?></label>
-			<img class="svg" src="<?php echo image_path('', 'actions/user.svg'); ?>" alt="" />
+			<img class="svg" src="<?php echo image_path('', 'actions/user.svg'); ?>" alt="" />	
 		</p>
 		<p class="infield groupbottom">
-			<input type="password" name="adminpass" id="adminpass" value="<?php print OC_Helper::init_var('adminpass'); ?>" required data-typetoggle="#show" />
+			<input type="password" name="adminpass" data-typetoggle="#show" id="adminpass" value="<?php print OC_Helper::init_var('adminpass'); ?>" />
 			<label for="adminpass" class="infield"><?php echo $l->t( 'Password' ); ?></label>
 			<img class="svg" id="adminpass-icon" src="<?php echo image_path('', 'actions/password.svg'); ?>" alt="" />
 			<input type="checkbox" id="show" name="show" />
