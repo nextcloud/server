d8fee28b3becfff155c73395c96d76a0315788a8
Björn Schießle <schiessle@owncloud.com>
2013-02-10 14:43:31 +0100

    add switch to enable/disable the possibility to change the display name by the user

--- config/config.sample.php
+++ config/config.sample.php
@@ -114,6 +114,9 @@ $CONFIG = array(
 /* How long should ownCloud keep deleted files in the trash bin, default value:  180 days */
 'trashbin_retention_obligation' => 180,
 
+/* allow user to change his display name, if it is supported by the back-end */
+'allow_user_to_change_display_name' => true,
+
 /* Check 3rdparty apps for malicious code fragments */
 "appcodechecker" => "",
 
