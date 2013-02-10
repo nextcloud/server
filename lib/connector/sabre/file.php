2644003bf1f82eb812981d27237ec0ce4e026604
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-02-10 11:44:34 +0100

    send back 'X-OC-MTime: accepted' when X-OC-MTime was applied

--- lib/connector/sabre/file.php
+++ lib/connector/sabre/file.php
@@ -56,7 +56,9 @@ class OC_Connector_Sabre_File extends OC_Connector_Sabre_Node implements Sabre_D
 		//allow sync clients to send the mtime along in a header
 		$mtime = OC_Request::hasModificationTime();
 		if ($mtime !== false) {
-			\OC\Files\Filesystem::touch($this->path,$mtime);
+			if(\OC\Files\Filesystem::touch($this->path, $mtime)) {
+				header('X-OC-MTime: accepted');
+			}
 		}
 
 		return OC_Connector_Sabre_Node::getETagPropertyForPath($this->path);
