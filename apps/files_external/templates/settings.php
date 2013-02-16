4708d4549d5574e39dfd18c722941b5512cabc89
Lukas Reschke <lukas@statuscode.ch>
2013-02-16 13:28:49 +0100

    Add missing requesttoken
    This is a trivial fix for a regression caused by #1648

--- apps/files_external/templates/settings.php
+++ apps/files_external/templates/settings.php
@@ -149,6 +149,7 @@
 			<?php endforeach; ?>
 			</tbody>
 		</table>
+		<input type="hidden" name="requesttoken" value="<?php echo $_['requesttoken']; ?>">
 		<input type="file" id="rootcert_import" name="rootcert_import" style="width:230px;">
 		<input type="submit" name="cert_import" value="<?php echo $l->t('Import Root Certificate'); ?>" />
 </fieldset>
