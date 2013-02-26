c94974cb97aae77b5089af27222523a156a087e6
Lukas Reschke <lukas@statuscode.ch>
2013-02-26 16:10:52 +0100

    Add missing echo

--- apps/files_encryption/templates/settings-personal.php
+++ apps/files_encryption/templates/settings-personal.php
@@ -8,7 +8,7 @@
 		</p>
 		<?php if ( ! empty( $_["blacklist"] ) ): ?>
 		<p>
-			<?php $l->t( 'The following file types will not be encrypted:' ); ?>
+			<?php echo $l->t( 'The following file types will not be encrypted:' ); ?>
 		</p>
 		<ul>
 			<?php foreach( $_["blacklist"] as $type ): ?>
