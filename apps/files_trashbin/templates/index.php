3ba8881bacedf4c1950c6198fca28577814cf807
Lukas Reschke <lukas@statuscode.ch>
2013-02-27 21:46:37 +0100

    [files_trashbin] From echo to p

--- apps/files_trashbin/templates/index.php
+++ apps/files_trashbin/templates/index.php
@@ -1,12 +1,12 @@
 <!--[if IE 8]><style>input[type="checkbox"]{padding:0;}table td{position:static !important;}</style><![endif]-->
 <div id="controls">
-	<?php echo($_['breadcrumb']); ?>
+	<?php print_unescaped($_['breadcrumb']); ?>
 		<div id="file_action_panel"></div>
 </div>
 <div id='notification'></div>
 
 <?php if (isset($_['files']) && count($_['files'])==0 && $_['dirlisting'] == false):?>
-	<div id="emptyfolder"><?php echo $l->t('Nothing in here. Your trash bin is empty!')?></div>
+	<div id="emptyfolder"><?php p($l->t('Nothing in here. Your trash bin is empty!'))?></div>
 <?php endif; ?>
 
 <table>
@@ -14,28 +14,28 @@
 		<tr>
 			<th id='headerName'>
 				<input type="checkbox" id="select_all" />
-				<span class='name'><?php echo $l->t( 'Name' ); ?></span>
+				<span class='name'><?php p($l->t( 'Name' )); ?></span>
 				<span class='selectedActions'>
 						<a href="" class="undelete">
-							<img class="svg" alt="<?php echo $l->t( 'Restore' ); ?>"
-								 src="<?php echo OCP\image_path("core", "actions/undelete.png"); ?>" />
-							<?php echo $l->t('Restore')?>
+							<img class="svg" alt="<?php p($l->t( 'Restore' )); ?>"
+								 src="<?php print_unescaped(OCP\image_path("core", "actions/undelete.png")); ?>" />
+							<?php p($l->t('Restore'))?>
 						</a>
 				</span>
 			</th>
 			<th id="headerDate">
-				<span id="modified"><?php echo $l->t( 'Deleted' ); ?></span>
+				<span id="modified"><?php p($l->t( 'Deleted' )); ?></span>
 				<span class="selectedActions">
 					<a href="" class="delete">
-						<?php echo $l->t('Delete')?>
-						<img class="svg" alt="<?php echo $l->t('Delete')?>"
-							src="<?php echo OCP\image_path("core", "actions/delete.svg"); ?>" />
+						<?php p($l->t('Delete'))?>
+						<img class="svg" alt="<?php p($l->t('Delete'))?>"
+							src="<?php print_unescaped(OCP\image_path("core", "actions/delete.svg")); ?>" />
 					</a>
 				</span>
 			</th>
 		</tr>
 	</thead>
 	<tbody id="fileList">
-		<?php echo($_['fileList']); ?>
+		<?php print_unescaped($_['fileList']); ?>
 	</tbody>
 </table>
