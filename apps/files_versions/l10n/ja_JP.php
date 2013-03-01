c9d9c381ec22d7a416a1096a909d8b6f386c8171
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-02 00:05:42 +0100

    [tx-robot] updated from transifex

--- apps/files_versions/l10n/ja_JP.php
+++ apps/files_versions/l10n/ja_JP.php
@@ -6,5 +6,6 @@
 "File %s could not be reverted to version %s" => "ファイル %s をバージョン %s に戻せませんでした",
 "No old versions available" => "利用可能な古いバージョンはありません",
 "No path specified" => "パスが指定されていません",
+"Versions" => "バージョン",
 "Revert a file to a previous version by clicking on its revert button" => "もとに戻すボタンをクリックすると、ファイルを過去のバージョンに戻します"
 );
