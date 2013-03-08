02e2f7384eca440ce38cec98808cbd8aeb04e9c9
Björn Schießle <schiessle@owncloud.com>
2013-03-08 17:32:04 +0100

    not only files can be reshared but also folders

--- apps/files_sharing/lib/updater.php
+++ apps/files_sharing/lib/updater.php
@@ -86,7 +86,7 @@ class Shared_Updater {
 	static public function shareHook($params) {
 		if ($params['itemType'] === 'file' || $params['itemType'] === 'folder') {
 			$uidOwner = \OCP\User::getUser();
-			$users = \OCP\Share::getUsersItemShared('file', $params['fileSource'], $uidOwner, true);
+			$users = \OCP\Share::getUsersItemShared($params['itemType'], $params['fileSource'], $uidOwner, true);
 			if (!empty($users)) {
 				while (!empty($users)) {
 					$reshareUsers = array();
