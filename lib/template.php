3de8570e3547116b422e6c578ee4f50c53ed1855
Bart Visscher <bartv@thisnet.nl>
2013-03-18 22:32:32 +0100

    Move basic template functions to their own class

--- lib/template.php
+++ lib/template.php
@@ -26,12 +26,9 @@ require_once __DIR__.'/template/functions.php';
 /**
  * This class provides the templates for owncloud.
  */
-class OC_Template{
+class OC_Template extends \OC\Template\Base {
 	private $renderas; // Create a full page?
-	private $application; // template Application
-	private $vars; // Vars
-	private $template; // The path to the template
-	private $l10n; // The l10n-Object
+	private $path; // The path to the template
 	private $headers=array(); //custom headers
 
 	/**
@@ -48,13 +45,24 @@ class OC_Template{
 	 * "admin".
 	 */
 	public function __construct( $app, $name, $renderas = "" ) {
+		// Read the selected theme from the config file
+		$theme = OC_Util::getTheme();
+
+		// Read the detected formfactor and use the right file name.
+		$fext = self::getFormFactorExtension();
+
+		$requesttoken = OC::$session ? OC_Util::callRegister() : '';
+
+		$parts = explode('/', $app); // fix translation when app is something like core/lostpassword
+		$l10n = OC_L10N::get($parts[0]);
+
+		list($path, $template) = $this->findTemplate($theme, $app, $name, $fext);
+
 		// Set the private data
 		$this->renderas = $renderas;
-		$this->application = $app;
-		$this->vars = array();
-		$this->vars['requesttoken'] = OC::$session ? OC_Util::callRegister() : '';
-		$parts = explode('/', $app); // fix translation when app is something like core/lostpassword
-		$this->l10n = OC_L10N::get($parts[0]);
+		$this->path = $path;
+
+		parent::__construct($template, $requesttoken, $l10n);
 
 		// Some headers to enhance security
 		header('X-XSS-Protection: 1; mode=block'); // Enforce browser based XSS filters
@@ -78,7 +86,6 @@ class OC_Template{
 			.'media-src *');
 		header('Content-Security-Policy:'.$policy); // Standard
 
-		$this->findTemplate($name);
 	}
 
 	/**
@@ -149,106 +156,17 @@ class OC_Template{
 	 * Will select the template file for the selected theme and formfactor.
 	 * Checking all the possible locations.
 	 */
-	protected function findTemplate($name)
-	{
-		// Read the selected theme from the config file
-		$theme = OC_Util::getTheme();
-
-		// Read the detected formfactor and use the right file name.
-		$fext = self::getFormFactorExtension();
-
-		$app = $this->application;
+	protected function findTemplate($theme, $app, $name, $fext) {
 		// Check if it is a app template or not.
-		if( $app != "" ) {
-			// Check if the app is in the app folder or in the root
-			if( file_exists(OC_App::getAppPath($app)."/templates/" )) {
-				// Check if the template is overwritten by the selected theme
-				if ($this->checkPathForTemplate(OC::$SERVERROOT."/themes/$theme/apps/$app/templates/", $name, $fext)) {
-				}elseif ($this->checkPathForTemplate(OC_App::getAppPath($app)."/templates/", $name, $fext)) {
-				}
-			}else{
-				// Check if the template is overwritten by the selected theme
-				if ($this->checkPathForTemplate(OC::$SERVERROOT."/themes/$theme/$app/templates/", $name, $fext)) {
-				}elseif ($this->checkPathForTemplate(OC::$SERVERROOT."/$app/templates/", $name, $fext)) {
-				}else{
-					echo('template not found: template:'.$name.' formfactor:'.$fext
-						.' webroot:'.OC::$WEBROOT.' serverroot:'.OC::$SERVERROOT);
-					die();
-				}
-
-			}
-		}else{
-			// Check if the template is overwritten by the selected theme
-			if ($this->checkPathForTemplate(OC::$SERVERROOT."/themes/$theme/core/templates/", $name, $fext)) {
-			} elseif ($this->checkPathForTemplate(OC::$SERVERROOT."/core/templates/", $name, $fext)) {
-			}else{
-				echo('template not found: template:'.$name.' formfactor:'.$fext
-					.' webroot:'.OC::$WEBROOT.' serverroot:'.OC::$SERVERROOT);
-				die();
-			}
+		if( $app != '' ) {
+			$dirs = $this->getAppTemplateDirs($theme, $app, OC::$SERVERROOT, OC_App::getAppPath($app));
+		} else {
+			$dirs = $this->getCoreTemplateDirs($theme, OC::$SERVERROOT);
 		}
-	}
-
-	/**
-	 * @brief check Path For Template with and without $fext
-	 * @param string $path to check
-	 * @param string $name of the template file (without suffix)
-	 * @param string $fext formfactor extension
-	 * @return bool true when found
-	 *
-	 * Will set $this->template and $this->path if there is a template at
-	 * the specific $path
-	 */
-	protected function checkPathForTemplate($path, $name, $fext)
-	{
-		if ($name =='') return false;
-		$template = null;
-		if( is_file( $path.$name.$fext.'.php' )) {
-			$template = $path.$name.$fext.'.php';
-		}elseif( is_file( $path.$name.'.php' )) {
-			$template = $path.$name.'.php';
-		}
-		if ($template) {
-			$this->template = $template;
-			$this->path = $path;
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * @brief Assign variables
-	 * @param string $key key
-	 * @param string $value value
-	 * @return bool
-	 *
-	 * This function assigns a variable. It can be accessed via $_[$key] in
-	 * the template.
-	 *
-	 * If the key existed before, it will be overwritten
-	 */
-	public function assign( $key, $value) {
-		$this->vars[$key] = $value;
-		return true;
-	}
-
-	/**
-	 * @brief Appends a variable
-	 * @param string $key key
-	 * @param string $value value
-	 * @return bool
-	 *
-	 * This function assigns a variable in an array context. If the key already
-	 * exists, the value will be appended. It can be accessed via
-	 * $_[$key][$position] in the template.
-	 */
-	public function append( $key, $value ) {
-		if( array_key_exists( $key, $this->vars )) {
-			$this->vars[$key][] = $value;
-		}
-		else{
-			$this->vars[$key] = array( $value );
-		}
+		$locator = new \OC\Template\TemplateFileLocator( $fext, $dirs );
+		$template = $locator->find($name);
+		$path = $locator->getPath();
+		return array($path, $template);
 	}
 
 	/**
@@ -262,31 +180,14 @@ class OC_Template{
 	}
 
 	/**
-	 * @brief Prints the proceeded template
+	 * @brief Process the template
 	 * @return bool
 	 *
-	 * This function proceeds the template and prints its output.
-	 */
-	public function printPage() {
-		$data = $this->fetchPage();
-		if( $data === false ) {
-			return false;
-		}
-		else{
-			print $data;
-			return true;
-		}
-	}
-
-	/**
-	 * @brief Proceeds the template
-	 * @return bool
-	 *
-	 * This function proceeds the template. If $this->renderas is set, it
+	 * This function process the template. If $this->renderas is set, it
 	 * will produce a full page.
 	 */
 	public function fetchPage() {
-		$data = $this->_fetch();
+		$data = parent::fetchPage();
 
 		if( $this->renderas ) {
 			$page = new OC_TemplateLayout($this->renderas);
@@ -306,27 +207,6 @@ class OC_Template{
 	}
 
 	/**
-	 * @brief doing the actual work
-	 * @return string content
-	 *
-	 * Includes the template file, fetches its output
-	 */
-	private function _fetch() {
-		// Register the variables
-		$_ = $this->vars;
-		$l = $this->l10n;
-
-		// Execute the template
-		ob_start();
-		include $this->template; // <-- we have to use include because we pass $_!
-		$data = ob_get_contents();
-		@ob_end_clean();
-
-		// return the data
-		return $data;
-	}
-
-	/**
 	 * @brief Include template
 	 * @return string returns content of included template
 	 *
@@ -334,21 +214,7 @@ class OC_Template{
 	 * do this.
 	 */
 	public function inc( $file, $additionalparams = null ) {
-		$_ = $this->vars;
-		$l = $this->l10n;
-
-		if( !is_null($additionalparams)) {
-			$_ = array_merge( $additionalparams, $this->vars );
-		}
-
-		// Include
-		ob_start();
-		include $this->path.$file.'.php';
-		$data = ob_get_contents();
-		@ob_end_clean();
-
-		// Return data
-		return $data;
+		return $this->load($this->path.$file.'.php', $additionalparams);
 	}
 
 	/**
