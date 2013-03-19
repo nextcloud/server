f0528469144ed46f7684ac1465dc7e9f8869d7f9
Thomas Tanghus <thomas@tanghus.net>
2013-03-19 01:35:12 +0100

    Add indexes and primary keys.

--- lib/util.php
+++ lib/util.php
@@ -75,7 +75,7 @@ class OC_Util {
 	public static function getVersion() {
 		// hint: We only can count up. Reset minor/patchlevel when
 		// updating major/minor version number.
-		return array(5, 80, 00);
+		return array(5, 80, 01);
 	}
 
 	/**
