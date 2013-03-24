56302ff9cfc66c202cb8b81f69f97f88c9e95c0d
Robin Appelman <icewind@owncloud.com>
2013-03-24 16:20:59 +0100

    Port Icewind's fix I

--- lib/files/cache/upgrade.php
+++ lib/files/cache/upgrade.php
@@ -127,6 +127,7 @@ class Upgrade {
 		 */
 		list($storage, $internalPath) = \OC\Files\Filesystem::resolvePath($data['path']);
 		if ($storage) {
+			$newData['etag'] = $data['etag'];
 			$newData['path_hash'] = md5($internalPath);
 			$newData['path'] = $internalPath;
 			$newData['storage'] = $this->getNumericId($storage);
