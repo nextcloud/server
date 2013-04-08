f1bcf6ef0289504f1f00e1265ad8ad88aa2302db
kondou <kondou@ts.unde.re>
2013-04-08 07:11:35 +0200

    Improve the password reset screen.
    Fixing #2752

--- core/lostpassword/templates/lostpassword.php
+++ core/lostpassword/templates/lostpassword.php
@@ -1,17 +1,31 @@
-<form action="<?php echo OC_Helper::linkToRoute('core_lostpassword_send_email') ?>" method="post">
-	<fieldset>
-		<?php echo $l->t('You will receive a link to reset your password via Email.'); ?>
-		<?php if ($_['requested']): ?>
-			<?php echo $l->t('Reset email send.'); ?>
-		<?php else: ?>
+<?php if $_['requested']): ?>
+	<div class="success"><p>
+	<?php
+		echo $l->t('The link to reset your password has been sent to your email.');
+		echo "</p><p>";
+		echo $l->t('If you do not receive it within a reasonable amount of time, check your spam/junk folders.');
+		echo "</p><p>";
+		echo $l->t('If it is not there ask your local administrator .');
+	?>
+	</p></div>
+<?php else: ?>
+	<form action="<?php echo OC_Helper::linkToRoute('core_lostpassword_send_email') ?>" method="post">
+		<fieldset>
 			<?php if ($_['error']): ?>
-				<?php echo $l->t('Request failed!'); ?>
+				<div class="errors"><p>
+				<?php
+					echo $l->t('Request failed!');
+					echo "</p><p>";
+					echo $l->t('Did you make sure the Email was right?');
+				?>
+				</p></div>
 			<?php endif; ?>
+			<?php echo $l->t('You will receive a link to reset your password via Email.'); ?>
 			<p class="infield">
 				<label for="user" class="infield"><?php echo $l->t( 'Username' ); ?></label>
 				<input type="text" name="user" id="user" placeholder="" value="" autocomplete="off" required autofocus />
 			</p>
 			<input type="submit" id="submit" value="<?php echo $l->t('Request reset'); ?>" />
-		<?php endif; ?>
-	</fieldset>
-</form>
+		</fieldset>
+	</form>
+<?php endif; ?>
