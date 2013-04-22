f6808617b343c0deb953d6251208af716dcab083
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-22 02:00:20 +0200

    [tx-robot] updated from transifex

--- apps/files_trashbin/l10n/cy_GB.php
+++ apps/files_trashbin/l10n/cy_GB.php
@@ -1,4 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Gwall",
+"Delete permanently" => "Dileu'n barhaol",
+"Name" => "Enw",
+"1 folder" => "1 blygell",
+"{count} folders" => "{count} plygell",
+"1 file" => "1 ffeil",
+"{count} files" => "{count} ffeil",
 "Delete" => "Dileu"
 );
