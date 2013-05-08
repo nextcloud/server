2e81efc37eeb95b7e5d392349ad393bb94d6928a
Björn Schießle <schiessle@owncloud.com>
2013-05-08 22:57:08 +0200

    don't call correctFolder() in touchHook, it will be called later in the writeUpdate()

--- lib/files/cache/updater.php
+++ lib/files/cache/updater.php
@@ -138,7 +138,6 @@ class Updater {
 		$id = $cache->getId($internalPath);
 		if ($id !== -1) {
 			$cache->update($id, array('etag' => $storage->getETag($internalPath)));
-			self::correctFolder($parent, $time);
 		}
 		self::writeUpdate($path);
 	}
