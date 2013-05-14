038f00d455e8219bd5063d4c995999e6a7fef851
Frank Karlitschek <frank@owncloud.org>
2013-05-14 08:09:46 +0200

    4.0.15

--- lib/util.php
+++ lib/util.php
@@ -83,7 +83,7 @@ class OC_Util {
 	 * @return array
 	 */
 	public static function getVersion(){
-		return array(4,00,14);
+		return array(4,00,15);
 	}
 
 	/**
@@ -91,7 +91,7 @@ class OC_Util {
 	 * @return string
 	 */
 	public static function getVersionString(){
-		return '4.0.14';
+		return '4.0.15';
 	}
 
         /**
