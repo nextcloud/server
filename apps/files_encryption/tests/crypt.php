b6572cedac8aa26d75ae09062b9e5500b00e845a
Florin Peter <github@florin-peter.de>
2013-05-17 22:16:56 +0200

    added folder move test

--- apps/files_encryption/tests/crypt.php
+++ apps/files_encryption/tests/crypt.php
@@ -649,7 +649,7 @@ class Test_Crypt extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals( $this->dataLong, $decrypt );
 
-        $newFolder = '/newfolder1';
+        $newFolder = '/newfolder'.time();
         $newFilename = 'tmp-new-'.time();
         $view = new \OC\Files\View('/' . $this->userId . '/files');
         $view->mkdir($newFolder);
@@ -663,6 +663,39 @@ class Test_Crypt extends \PHPUnit_Framework_TestCase {
         // tear down
         $view->unlink( $newFolder );
     }
+
+	function testMoveFolder() {
+
+		$view = new \OC\Files\View('/' . $this->userId . '/files');
+
+		$filename = '/tmp-'.time();
+		$folder = '/folder'.time();
+
+		$view->mkdir($folder);
+
+		// Save long data as encrypted file using stream wrapper
+		$cryptedFile = file_put_contents( 'crypt://' . $folder . $filename, $this->dataLong );
+
+		// Test that data was successfully written
+		$this->assertTrue( is_int( $cryptedFile ) );
+
+		// Get file decrypted contents
+		$decrypt = file_get_contents( 'crypt://' . $folder . $filename );
+
+		$this->assertEquals( $this->dataLong, $decrypt );
+
+		$newFolder = '/newfolder'.time();
+
+		$view->rename( $folder, $newFolder );
+
+		// Get file decrypted contents
+		$newDecrypt = file_get_contents( 'crypt://' . $newFolder . $filename );
+
+		$this->assertEquals( $this->dataLong, $newDecrypt );
+
+		// tear down
+		$view->unlink( $newFolder );
+	}
 
 	function testRenameFolder() {
 
