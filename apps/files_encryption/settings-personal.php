d59ae569ba2ee783bfb39ea487b02258097a9951
Florin Peter <github@florin-peter.de>
2013-05-29 23:13:45 +0200

    reformat code

--- apps/files_encryption/settings-personal.php
+++ apps/files_encryption/settings-personal.php
@@ -7,22 +7,22 @@
  */
 
 // Add CSS stylesheet
-\OC_Util::addStyle( 'files_encryption', 'settings-personal' );
- 
-$tmpl = new OCP\Template( 'files_encryption', 'settings-personal');
+\OC_Util::addStyle('files_encryption', 'settings-personal');
+
+$tmpl = new OCP\Template('files_encryption', 'settings-personal');
 
 $user = \OCP\USER::getUser();
-$view = new \OC_FilesystemView( '/' );
-$util = new \OCA\Encryption\Util( $view, $user );
+$view = new \OC_FilesystemView('/');
+$util = new \OCA\Encryption\Util($view, $user);
 
-$recoveryAdminEnabled = OC_Appconfig::getValue( 'files_encryption', 'recoveryAdminEnabled' );
+$recoveryAdminEnabled = OC_Appconfig::getValue('files_encryption', 'recoveryAdminEnabled');
 $recoveryEnabledForUser = $util->recoveryEnabledForUser();
 
-\OCP\Util::addscript( 'files_encryption', 'settings-personal' );
-\OCP\Util::addScript( 'settings', 'personal' );
+\OCP\Util::addscript('files_encryption', 'settings-personal');
+\OCP\Util::addScript('settings', 'personal');
 
-$tmpl->assign( 'recoveryEnabled', $recoveryAdminEnabled );
-$tmpl->assign( 'recoveryEnabledForUser', $recoveryEnabledForUser );
+$tmpl->assign('recoveryEnabled', $recoveryAdminEnabled);
+$tmpl->assign('recoveryEnabledForUser', $recoveryEnabledForUser);
 
 return $tmpl->fetchPage();
 
