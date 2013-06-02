9046c65af57f9e90d28621a1176d2c2334bdd5df
Frank Karlitschek <frank@owncloud.org>
2013-06-02 23:02:52 +0200

    5.0.7 RC

--- lib/util.php
+++ lib/util.php
@@ -76,7 +76,7 @@ class OC_Util {
 	public static function getVersion() {
 		// hint: We only can count up. Reset minor/patchlevel when
 		// updating major/minor version number.
-		return array(5, 00, 10);
+		return array(5, 00, 11);
 	}
 
 	/**
@@ -84,7 +84,7 @@ class OC_Util {
 	 * @return string
 	 */
 	public static function getVersionString() {
-		return '5.0.7 preview';
+		return '5.0.7 RC';
 	}
 
 	/**
