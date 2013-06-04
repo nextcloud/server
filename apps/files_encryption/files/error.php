03907da05befdf2cfb991ed3c43d899c31d43268
Florin Peter <github@florin-peter.de>
2013-06-04 22:31:36 +0200

    improved error msg and removed duplicate string

--- apps/files_encryption/files/error.php
+++ apps/files_encryption/files/error.php
@@ -4,15 +4,17 @@ if (!isset($_)) { //also provide standalone error page
 
 	$l = OC_L10N::get('files_encryption');
 
+	$errorMsg = $l->t('Your private key is not valid! Maybe your password was changed from outside.');
+
 	if(isset($_GET['p']) && $_GET['p'] === '1') {
-		header('HTTP/1.0 404 ' . $l->t('Your private key is not valid! Maybe the your password was changed from outside.'));
+		header('HTTP/1.0 404 ' . $errorMsg);
 	}
 
 	// check if ajax request
 	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-		\OCP\JSON::error(array('data' => array('message' => $l->t('Your private key is not valid! Maybe the your password was changed from outside.'))));
+		\OCP\JSON::error(array('data' => array('message' => $errorMsg)));
 	} else {
-		header('HTTP/1.0 404 ' . $l->t('Your private key is not valid! Maybe the your password was changed from outside.'));
+		header('HTTP/1.0 404 ' . $errorMsg);
 		$tmpl = new OC_Template('files_encryption', 'invalid_private_key', 'guest');
 		$tmpl->printPage();
 	}
