97e05b71f8615e17ef07066df2b53896c794b5e9
Björn Schießle <schiessle@owncloud.com>
2013-06-06 12:26:05 +0200

    add hint about what the recovery password field is for

--- settings/templates/users.php
+++ settings/templates/users.php
@@ -31,7 +31,11 @@ $_['subadmingroups'] = array_flip($items);
 	</form>
 	<?php if((bool)$_['recoveryAdminEnabled']): ?>
 	<div class="recoveryPassword">
-	<input id="recoveryPassword" type="password" placeholder="<?php p($l->t('Admin Recovery Password'))?>" />
+	<input id="recoveryPassword"
+		   type="password"
+		   placeholder="<?php p($l->t('Admin Recovery Password'))?>"
+		   title="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"
+		   alt="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"/>
 	</div>
 	<?php endif; ?>
 	<div class="quota">
