5efbf25c2a197d0b19367ccf47378390609de66e
Björn Schießle <schiessle@owncloud.com>
2013-06-13 16:10:33 +0200

    improved footer

--- apps/files_sharing/templates/public.php
+++ apps/files_sharing/templates/public.php
@@ -39,5 +39,9 @@
 		</ul>
 	<?php endif; ?>
 </div>
-<footer><p class="info"><a href="<?php p(OC_Defaults::getBaseUrl()); ?>"><?php p(OC_Defaults::getEntity()) ?></a> &ndash;
-<?php p(OC_Defaults::getSlogan()); ?></p></footer>
+<footer>
+	<p class="info"><a href="<?php p(OC_Defaults::getBaseUrl()); ?>"><?php p(OC_Defaults::getEntity()) ?></a>
+		<?php OC_Util::getEditionString() === '' ? print_unescaped(' &ndash; ') : print_unescaped('<br/>'); ?>
+		<?php p(OC_Defaults::getSlogan()); ?>
+	</p>
+</footer>
--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -45,7 +45,8 @@
 			<p class="info">
 				<?php OC_Util::getEditionString() === '' ? '' : p('© 2013 '); ?>
 				<a href="<?php p(OC_Defaults::getBaseUrl())?>">
-					<?php  p(OC_Defaults::getEntity()); ?></a> &ndash;
+					<?php  p(OC_Defaults::getEntity()); ?></a>
+				<?php OC_Util::getEditionString() === '' ? print_unescaped(' &ndash; ') : print_unescaped('<br/>'); ?>
 			<?php p(OC_Defaults::getSlogan()); ?></p></footer>
 	</body>
 </html>
