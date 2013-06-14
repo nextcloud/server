3cd7747e7b9c10449f986e5f696a01cf4e50c318
Stephan Arts <stephan@xfce.org>
2013-06-14 17:38:56 +0200

    Use templates to theme share e-mails (#3683)

---
+++ core/templates/altmail.php
@@ -0,0 +1,9 @@
+<?php
+print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared »%s« with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
+?>
+
+--
+<?php
+print_unescaped($l->t("ownCloud – web services under your control"));
+?>
+http://ownCloud.org
