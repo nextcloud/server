7093b0829f5f603e8ff1de7d5130130d34b6be9a
Morris Jobke <morris.jobke@gmail.com>
2013-06-14 11:28:33 +0300

    improve comment

--- lib/setup.php
+++ lib/setup.php
@@ -320,7 +320,7 @@ class OC_Setup {
 		$connection_string = "host='$e_host' dbname=postgres user='$e_user' password='$e_password'";
 		$connection = @pg_connect($connection_string);
 		if(!$connection) {
-			// Try if we can't connect with the specified DB name
+			// Try if we can connect to the DB with the specified name
 			$e_dbname = addslashes($dbname);
 			$connection_string = "host='$e_host' dbname='$e_dbname' user='$e_user' password='$e_password'";
 			$connection = @pg_connect($connection_string);
