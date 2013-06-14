f3d33a7d5bd98c9c0da62097e643e95f0b4b923d
Thomas Mueller <thomas.mueller@tmit.eu>
2013-06-14 20:04:20 +0200

    remove » and « from the text mail because they will be escaped

--- core/templates/altmail.php
+++ core/templates/altmail.php
@@ -1,5 +1,5 @@
 <?php
-print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared »%s« with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
+print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared %s with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
 ?>
 
 --
