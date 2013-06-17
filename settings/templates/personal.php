nextcloud/server

commit ca7699c8bdb85df9b69e3639f53d1b959287ba16

don't show translation hints in enterprise edition
Conflicts:

	settings/templates/personal.php

settings/templates/personal.php
@@ -91,8 +91,10 @@ if($_['passwordChangeSupported']) {
 			<option value="<?php p($language['code']);?>"><?php p($language['name']);?></option>
 		<?php endforeach;?>
 		</select>
-		<a href="https://www.transifex.net/projects/p/owncloud/team/<?php p($_['languages'][0]['code']);?>/"
+		<?php if (OC_Util::getEditionString() === ''): ?>
+		<a href="https://www.transifex.net/projects/p/owncloud/team/<?php p($_['languages']['code']);?>/"
 			target="_blank"><em><?php p($l->t('Help translate'));?></em></a>
+		<?php endif; ?>
 	</fieldset>
 </form>
 

commit 1171a5a2e02a33aa8658c44e74853bbfa3170a7a

fix link to translation, bug was introduced by https://github.com/owncloud/core/commit/ca7699c8bdb85df9b69e3639f53d1b959287ba16#settings/templates/personal.php

settings/templates/personal.php
@@ -92,7 +92,7 @@ if($_['passwordChangeSupported']) {
 		<?php endforeach;?>
 		</select>
 		<?php if (OC_Util::getEditionString() === ''): ?>
-		<a href="https://www.transifex.net/projects/p/owncloud/team/<?php p($_['languages']['code']);?>/"
+		<a href="https://www.transifex.net/projects/p/owncloud/team/<?php p($_['languages'][0]['code']);?>/"
 			target="_blank"><em><?php p($l->t('Help translate'));?></em></a>
 		<?php endif; ?>
 	</fieldset>