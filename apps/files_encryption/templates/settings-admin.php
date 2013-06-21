f40f49851c2ee371cbd2d7ce6bafaf0d96bd44ea
Björn Schießle <schiessle@owncloud.com>
2013-06-21 14:47:31 +0200

    fix some strings in the admin settings, it's not a 'recovery account' but a 'recovery key'

--- apps/files_encryption/templates/settings-admin.php
+++ apps/files_encryption/templates/settings-admin.php
@@ -7,11 +7,11 @@
 		</p>
 
 		<p>
-			<?php p($l->t("Enable encryption passwords recovery key (allow sharing to recovery key):")); ?>
+			<?php p($l->t("Enable recovery key (allow to recover users files in case of password loss):")); ?>
 			<br/>
 			<br/>
 			<input type="password" name="recoveryPassword" id="recoveryPassword"/>
-			<label for="recoveryPassword"><?php p($l->t("Recovery account password")); ?></label>
+			<label for="recoveryPassword"><?php p($l->t("Recovery key password")); ?></label>
 			<br/>
 			<input
 				type='radio'
@@ -31,21 +31,21 @@
 		<br/><br/>
 
 		<p>
-			<strong><?php p($l->t("Change encryption passwords recovery key:")); ?></strong>
+			<strong><?php p($l->t("Change recovery key password:")); ?></strong>
 			<br/><br/>
 			<input
 				type="password"
 				name="changeRecoveryPassword"
 				id="oldRecoveryPassword"
 				<?php echo($_["recoveryEnabled"] == 0 ? 'disabled' : ''); ?> />
-			<label for="oldRecoveryPassword"><?php p($l->t("Old Recovery account password")); ?></label>
+			<label for="oldRecoveryPassword"><?php p($l->t("Old Recovery key password")); ?></label>
 			<br/>
 			<input
 				type="password"
 				name="changeRecoveryPassword"
 				id="newRecoveryPassword"
 				<?php echo($_["recoveryEnabled"] == 0 ? 'disabled' : ''); ?> />
-			<label for="newRecoveryPassword"><?php p($l->t("New Recovery account password")); ?></label>
+			<label for="newRecoveryPassword"><?php p($l->t("New Recovery key password")); ?></label>
 			<br/>
 			<button
 				type="button"
