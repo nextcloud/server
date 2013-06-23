909d279e55783a5a4499e94a8959fa1745687e42
Robin Appelman <icewind@owncloud.com>
2013-06-23 17:49:38 +0200

    Add a CLI script for manually triggering checking a folder for updates

---
+++ apps/files/triggerupdate.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../../lib/base.php';
+
+if (OC::$CLI) {
+	if (count($argv) === 2) {
+		$file = $argv[1];
+		list(, $user) = explode('/', $file);
+		OC_Util::setupFS($user);
+		$view = new \OC\Files\View('');
+		/**
+		 * @var \OC\Files\Storage\Storage $storage
+		 */
+		list($storage, $internalPath) = $view->resolvePath($file);
+		$watcher = $storage->getWatcher($internalPath);
+		$watcher->checkUpdate($internalPath);
+	} else {
+		echo "Usage: php triggerupdate.php /path/to/file\n";
+	}
+} else {
+	echo "This script can be run from the command line only\n";
+}
