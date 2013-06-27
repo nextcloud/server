de93b21505e1281c529cc2d1dc350b62bb387cca
Thomas Mueller <thomas.mueller@tmit.eu>
2013-06-27 23:14:32 +0200

    missing $

--- lib/legacy/log.php
+++ lib/legacy/log.php
@@ -38,7 +38,7 @@ class OC_Log {
 	 * write a message in the log
 	 * @param string $app
 	 * @param string $message
-	 * @param int level
+	 * @param int $level
 	 */
 	public static function write($app, $message, $level) {
 		if (self::$enabled) {
