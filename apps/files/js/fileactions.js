89dc8be9f550310f86e2e7da9cdb1da630080f49
Bart Visscher <bartv@thisnet.nl>
2013-06-28 21:00:09 +0200

    update to !==

--- apps/files/js/fileactions.js
+++ apps/files/js/fileactions.js
@@ -191,7 +191,7 @@ FileActions.register('all', 'Rename', OC.PERMISSION_UPDATE, function () {
 
 FileActions.register('dir', 'Open', OC.PERMISSION_READ, '', function (filename) {
 	var dir = $('#dir').val()
-	if (dir != '/') {
+	if (dir !== '/') {
 		dir = dir + '/';
 	}
 	window.location = OC.linkTo('files', 'index.php') + '?dir=' + encodeURIComponent(dir + filename);
