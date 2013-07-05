83898e87be3486ecec76ee240a8810ce693be888
Björn Schießle <schiessle@owncloud.com>
2013-07-05 10:07:23 +0200

    Merge https://github.com/owncloud/core/pull/3900
    Squashed commit of the following:
    
    commit a34098427720c2d0470367cd9698c184d85b8dfd
    Merge: ccf849b 955646c
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Fri Jul 5 09:59:29 2013 +0200
    
        Merge branch 'master' into theme_aware_mail_footer
    
    commit ccf849bc6f18d80cdb593e851529b87579fe072b
    Merge: 8d5fb70 3c1308f
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Thu Jul 4 11:03:47 2013 +0200
    
        Merge branch 'master' into theme_aware_mail_footer
    
    commit 8d5fb70e6dcb2e2e34dcdf2a883680ef034afeea
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Thu Jul 4 10:38:39 2013 +0200
    
        switch to non-static defaults.php
    
    commit 88ab2768dcc4ced12c48872c887baa94ad938d62
    Merge: e199df8 84c25f9
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Wed Jul 3 16:59:48 2013 +0200
    
        Merge branch 'theme_aware_mail_footer' of github.com:owncloud/core into theme_aware_mail_footer
    
        Conflicts:
        	core/templates/altmail.php
    
    commit e199df844b5faf80a36d8380a5b0b58025cd425f
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Wed Jul 3 16:56:29 2013 +0200
    
        name and slogan should be in one line
    
    commit 84c25f9853e1bfbe4ae780371b1353aafb6b15c2
    Author: Thomas Müller <thomas.mueller@tmit.eu>
    Date:   Mon Jul 1 21:15:35 2013 +0200
    
        no ?> at the end of a file
    
    commit 3bd8d38d011f62ce78bcd0c108eb8cf2ce97eb99
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Mon Jul 1 12:58:36 2013 +0200
    
        print url unescaped
    
    commit d304ed18c8b7350e92e24565b9b9ba77b519d74c
    Author: Björn Schießle <schiessle@owncloud.com>
    Date:   Mon Jul 1 12:56:05 2013 +0200
    
        make new mail templates aware of different ownCloud themes

--- core/templates/altmail.php
+++ core/templates/altmail.php
@@ -1,9 +1,9 @@
 <?php
+$defaults = new OC_Defaults();
+
 print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared %s with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
 ?>
 
 --
-ownCloud - <?php
-print_unescaped($l->t("web services under your control"));
-?>
-http://ownCloud.org
+<?php p($defaults->getName() . ' - ' . $defaults->getSlogan()); ?>
+<?php print_unescaped("\n".$defaults->getBaseUrl());
