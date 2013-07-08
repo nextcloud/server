925d09cb0e3aed2ab89a11d636111ba699cc403f
Robin Appelman <icewind@owncloud.com>
2013-07-08 16:45:19 +0200

    add forwarding emitter for agregating multiple emitters

---
+++ lib/hooks/forwardingemitter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) 2013 Robin Appelman <icewind@owncloud.com>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+namespace OC\Hooks;
+
+/**
+ * Class ForwardingEmitter
+ *
+ * allows forwarding all listen calls to other emitters
+ *
+ * @package OC\Hooks
+ */
+abstract class ForwardingEmitter extends BasicEmitter {
+	/**
+	 * @var \OC\Hooks\Emitter[] array
+	 */
+	private $forwardEmitters = array();
+
+	/**
+	 * @param string $scope
+	 * @param string $method
+	 * @param callable $callback
+	 */
+	public function listen($scope, $method, $callback) {
+		parent::listen($scope, $method, $callback);
+		foreach ($this->forwardEmitters as $emitter) {
+			$emitter->listen($scope, $method, $callback);
+		}
+	}
+
+	/**
+	 * @param \OC\Hooks\Emitter $emitter
+	 */
+	protected function forward($emitter) {
+		$this->forwardEmitters[] = $emitter;
+	}
+}
