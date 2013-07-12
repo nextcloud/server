01378e19072b89c33af85a8a12a37f6c898e0941
Thomas Müller <thomas.mueller@tmit.eu>
2013-07-12 15:08:13 +0200

    incorporate development branch of  ssh://irodsguest@code.renci.org/gitroot/irodsphp

---
+++ apps/files_external/3rdparty/irodsphp/prods/src/packet/RP_BinBytesBuf.class.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../autoload.inc.php");
+class RP_BinBytesBuf extends RODSPacket
+{
+    public function __construct($buflen = '', $buf = '')
+    {
+        $packlets = array("buflen" => $buflen, "buf" => $buf);
+        parent::__construct("BinBytesBuf_PI", $packlets);
+    }
+
+}
+
+?>
