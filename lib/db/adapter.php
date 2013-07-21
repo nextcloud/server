suy/core

Move insertIfNotExist to Connection wrapper
Real implementation is in DB\Adapter* classes

lib/db/adapter.php
@@ -1,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2013 Bart Visscher <bartv@thisnet.nl>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+namespace OC\DB;
+
+class Adapter {
+	protected $conn;
+
+	public function __construct($conn) {
+		$this->conn = $conn;
+	}
+
+	public function insertIfNotExist($table, $input) {
+		$query = 'INSERT INTO `' .$table . '` ('
+			. implode(',', array_keys($input)) . ') SELECT \''
+			. implode('\',\'', array_values($input)) . '\' FROM ' . $table . ' WHERE ';
+
+		foreach($input as $key => $value) {
+			$query .= $key . " = '" . $value . '\' AND ';
+		}
+		$query = substr($query, 0, strlen($query) - 5);
+		$query .= ' HAVING COUNT(*) = 0';
+
+		// TODO: oci should be use " (quote) instead of ` (backtick).
+		//OC_Log::write('core', __METHOD__ . ', type: ' . $type . ', query: ' . $query, OC_Log::DEBUG);
+
+		try {
+			$result = $this->conn->prepare($query);
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query.'<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: ' . $entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		return $result->execute();
+	}
+}

lib/db/adaptersqlite.php
@@ -1,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) 2013 Bart Visscher <bartv@thisnet.nl>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+
+namespace OC\DB;
+
+class AdapterSqlite extends Adapter {
+	public function insertIfNotExist($table, $input) {
+		// NOTE: For SQLite we have to use this clumsy approach
+		// otherwise all fieldnames used must have a unique key.
+		$query = 'SELECT COUNT(*) FROM "' . $table . '" WHERE ';
+		foreach($input as $key => $value) {
+			$query .= $key . " = '" . $value . '\' AND ';
+		}
+		$query = substr($query, 0, strlen($query) - 5);
+		try {
+			$stmt = $this->conn->prepare($query);
+			$result = $stmt->execute();
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query . '<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: '.$entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		if ($stmt->fetchColumn() == 0) {
+			$query = 'INSERT INTO "' . $table . '" ("'
+				. implode('","', array_keys($input)) . '") VALUES("'
+				. implode('","', array_values($input)) . '")';
+		} else {
+			return true;
+		}
+
+		try {
+			$result = $this->conn->prepare($query);
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query.'<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: ' . $entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		return $result->execute();
+	}
+}

nextcloud/server

lib/db/adapter.php
@@ -22,4 +22,32 @@ class Adapter {
 	public function fixupStatement($statement) {
 		return $statement;
 	}
+
+	public function insertIfNotExist($table, $input) {
+		$query = 'INSERT INTO `' .$table . '` (`'
+			. implode('`,`', array_keys($input)) . '`) SELECT '
+			. str_repeat('?,', count($input)-1).'? ' // Is there a prettier alternative?
+			. 'FROM `' . $table . '` WHERE ';
+
+		foreach($input as $key => $value) {
+			$query .= '`' . $key . '` = ? AND ';
+		}
+		$query = substr($query, 0, strlen($query) - 5);
+		$query .= ' HAVING COUNT(*) = 0';
+		$inserts = array_values($input);
+		$inserts = array_merge($inserts, $inserts);
+
+		try {
+			$statement = $this->conn->prepare($query);
+			$result = $statement->execute($inserts);
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query.'<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: ' . $entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		return $result;
+	}
 }

lib/db/adaptersqlite.php
@@ -16,4 +16,45 @@ class AdapterSqlite extends Adapter {
 		$statement = str_ireplace( 'UNIX_TIMESTAMP()', 'strftime(\'%s\',\'now\')', $statement );
 		return $statement;
 	}
+
+	public function insertIfNotExist($table, $input) {
+		// NOTE: For SQLite we have to use this clumsy approach
+		// otherwise all fieldnames used must have a unique key.
+		$query = 'SELECT COUNT(*) FROM `' . $table . '` WHERE ';
+		foreach($input as $key => $value) {
+			$query .= '`' . $key . '` = ? AND ';
+		}
+		$query = substr($query, 0, strlen($query) - 5);
+		try {
+			$stmt = $this->conn->prepare($query);
+			$result = $stmt->execute(array($input));
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query . '<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: '.$entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		if ($stmt->fetchColumn() === 0) {
+			$query = 'INSERT INTO `' . $table . '` (`'
+				. implode('`,`', array_keys($input)) . '`) VALUES('
+				. str_repeat('?,', count($input)-1).'? ' . ')';
+		} else {
+			return 0; //no rows updated
+		}
+
+		try {
+			$statement = $this->conn->prepare($query);
+			$result = $statement->execute(array_values($input));
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			$entry = 'DB Error: "'.$e->getMessage() . '"<br />';
+			$entry .= 'Offending command was: ' . $query.'<br />';
+			OC_Log::write('core', $entry, OC_Log::FATAL);
+			error_log('DB error: ' . $entry);
+			OC_Template::printErrorPage( $entry );
+		}
+
+		return $result;
+	}
 }