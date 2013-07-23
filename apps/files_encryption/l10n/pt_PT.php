a94c55be987127d148b3a6d5820bf06fc9d74068
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-23 02:03:19 -0400

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/pt_PT.php
+++ apps/files_encryption/l10n/pt_PT.php
@@ -5,11 +5,15 @@
 "Could not disable recovery key. Please check your recovery key password!" => "Não foi possível desactivar a chave de recuperação. Por favor verifique a password da chave de recuperação.",
 "Password successfully changed." => "Password alterada com sucesso.",
 "Could not change the password. Maybe the old password was not correct." => "Não foi possivel alterar a password. Possivelmente a password antiga não está correcta.",
+"Your private key is not valid! Likely your password was changed outside the ownCloud system (e.g. your corporate directory). You can update your private key password in your personal settings to recover access to your encrypted files." => "Chave privada não é válida! Provavelmente senha foi alterada fora do sistema ownCloud (exemplo, o diretório corporativo). Pode atualizar password da chave privada em configurações personalizadas para recuperar o acesso aos seus arquivos encriptados.",
+"Please make sure that PHP 5.3.3 or newer is installed and that the OpenSSL PHP extension is enabled and configured properly. For now, the encryption app has been disabled." => "Por favor, certifique-se que PHP 5.3.3 ou mais recente está instalado e que a extensão PHP OpenSSL está ativada e corretamente configurada. Por agora, a encripitação está desativado.",
 "Saving..." => "A guardar...",
+"personal settings" => "configurações personalizadas ",
 "Encryption" => "Encriptação",
 "Enabled" => "Activado",
 "Disabled" => "Desactivado",
 "Change Password" => "Mudar a Password",
+"Enable password recovery:" => "ativar recuperação do password:",
 "File recovery settings updated" => "Actualizadas as definições de recuperação de ficheiros",
 "Could not update file recovery" => "Não foi possível actualizar a recuperação de ficheiros"
 );
