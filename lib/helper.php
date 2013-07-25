d042c8f1667c7fdf63c96c640acfe3af9cadeacf
Robin Appelman <icewind@owncloud.com>
2013-07-25 16:00:48 +0200

    use renamed constants

--- lib/helper.php
+++ lib/helper.php
@@ -815,14 +815,14 @@ class OC_Helper {
 		$post_max_size = OCP\Util::computerFileSize(ini_get('post_max_size'));
 		$freeSpace = \OC\Files\Filesystem::free_space($dir);
 		if ((int)$upload_max_filesize === 0 and (int)$post_max_size === 0) {
-			$maxUploadFilesize = \OC\Files\FREE_SPACE_UNLIMITED;
+			$maxUploadFilesize = \OC\Files\SPACE_UNLIMITED;
 		} elseif ((int)$upload_max_filesize === 0 or (int)$post_max_size === 0) {
 			$maxUploadFilesize = max($upload_max_filesize, $post_max_size); //only the non 0 value counts
 		} else {
 			$maxUploadFilesize = min($upload_max_filesize, $post_max_size);
 		}
 
-		if($freeSpace !== \OC\Files\FREE_SPACE_UNKNOWN){
+		if($freeSpace !== \OC\Files\SPACE_UNKNOWN){
 			$freeSpace = max($freeSpace, 0);
 
 			return min($maxUploadFilesize, $freeSpace);
