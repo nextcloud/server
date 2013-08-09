0e3dea7111e66c3619c6fe989e79ff80fb18e2b3
Thomas Müller <thomas.mueller@tmit.eu>
2013-08-09 23:36:23 +0200

    adding test case for a numeric value

--- tests/lib/l10n.php
+++ tests/lib/l10n.php
@@ -53,10 +53,15 @@ class Test_L10n extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	* Issue #4360: Do not call strtotime() on numeric strings.
-	*/
+	 * Issue #4360: Do not call strtotime() on numeric strings.
+	 */
 	public function testNumericStringToDateTime() {
 		$l = new OC_L10N('test');
 		$this->assertSame('February 13, 2009 23:31', $l->l('datetime', '1234567890'));
+	}
+
+	public function testNumericToDateTime() {
+		$l = new OC_L10N('test');
+		$this->assertSame('February 13, 2009 23:31', $l->l('datetime', 1234567890));
 	}
 }
