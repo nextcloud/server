30526ded803e352f3f7322ed96ebc480d5e3f9c1
kondou <kondou@ts.unde.re>
2013-08-30 13:26:13 +0200

    Fix \OCP\Avatar

--- lib/public/avatar.php
+++ lib/public/avatar.php
@@ -8,21 +8,5 @@
 
 namespace OCP;
 
-class Avatar {
-	private $avatar;
-
-	public function __construct () {
-		$this->avatar = new \OC_Avatar();
-
-	public function get ($user, $size = 64) {
-		return $avatar->get($user, $size);
-	}
-
-	public function set ($user, $data) {
-		return $avatar->set($user, $data);
-	}
-
-	public function remove ($user) {
-		return $avatar->remove($user);
-	}
+class Avatar extends \OC_Avatar {
 }
