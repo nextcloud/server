ce263df4c754e86051e7f90e0d164c7446620660
kondou <kondou@ts.unde.re>
2013-09-03 16:52:15 +0200

    Don't use HTML5 <header> to support avatars in IE8

--- core/js/avatar.js
+++ core/js/avatar.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('header .avatardiv').avatar(OC.currentUser, 32);
+	$('#header .avatardiv').avatar(OC.currentUser, 32);
 	// Personal settings
 	$('#avatar .avatardiv').avatar(OC.currentUser, 128);
 	// User settings
