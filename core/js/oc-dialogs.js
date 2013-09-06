c6ca9c1e9d8e25f4dad5ca18f2f335b19a4a3c0c
kondou <kondou@ts.unde.re>
2013-09-06 13:33:17 +0200

    Use shorter array-conversion

--- core/js/oc-dialogs.js
+++ core/js/oc-dialogs.js
@@ -245,9 +245,7 @@ var OCdialogs = {
 	},
 	_getFileList: function(dir, mimeType) {
 		if (typeof(mimeType) === "string") {
-			var tmp = mimeType;
-			mimeType = new Array();
-			mimeType[0] = tmp;
+			mimeType = [mimeType];
 		}
 
 		return $.getJSON(
