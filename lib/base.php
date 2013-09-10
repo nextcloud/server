7810e27dad3c67f310657d1b19db71e0e4f94631
Axel Roenn <axel@mpim-bonn.mpg.de>
2013-09-10 11:07:26 +0200

    Changed default behaviour to not log IP address in case of an auth failure. Can be configured in OC conf now. Log level changed to warning .

--- lib/base.php
+++ lib/base.php
@@ -730,8 +730,14 @@ class OC {
 			// Someone wants to log in :
 		} elseif (OC::tryFormLogin()) {
 			$error[] = 'invalidpassword';
-			OC_Log::write('core', 'Login failed: user \''.$_POST["user"].'\' , wrong password, IP:'.$_SERVER['REMOTE_ADDR'],
-			OC_Log::ERROR);
+			if ( OC_Config::getValue('log_authfailip', '') ) {
+				OC_Log::write('core', 'Login failed: user \''.$_POST["user"].'\' , wrong password, IP:'.$_SERVER['REMOTE_ADDR'],
+				OC_Log::WARN);
+			}
+			else { 
+				OC_Log::write('core', 'Login failed: user \''.$_POST["user"].'\' , wrong password, IP:set log_authfailip=true in conf',
+                                OC_Log::WARN);
+			}
 		}
 
 		OC_Util::displayLoginPage(array_unique($error));
