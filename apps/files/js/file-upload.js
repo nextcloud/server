7854cf04eec68da83655a819f081d2f2e12f607b
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-09-13 17:00:07 +0200

    refactor upload js & html to always use only js to fill form data

--- apps/files/js/file-upload.js
+++ apps/files/js/file-upload.js
@@ -46,6 +46,15 @@ $(document).ready(function() {
 				$('#uploadprogresswrapper input.stop').show();
 			}
 		},
+		submit: function(e, data) {
+			if ( ! data.formData ) {
+				// noone set update parameters, we set the minimum
+				data.formData = {
+					requesttoken: oc_requesttoken,
+							 dir: $('#dir').val()
+				};
+			}
+		},
 		/**
 		 * called after the first add, does NOT have the data param
 		 * @param e
@@ -141,15 +150,8 @@ $(document).ready(function() {
 			$('#uploadprogressbar').fadeOut();
 		}
 	};
-	var file_upload_handler = function() {
-		$('#file_upload_start').fileupload(file_upload_param);
-	};
-
-
-
-	if ( document.getElementById('data-upload-form') ) {
-		$(file_upload_handler);
-	}
+	$('#file_upload_start').fileupload(file_upload_param);
+	
 	$.assocArraySize = function(obj) {
 		// http://stackoverflow.com/a/6700/11236
 		var size = 0, key;
