00772a84701cbfbd794db1d9c40f8cd98a40b9b2
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-09-17 17:25:47 +0200

    use correct doublequotes in template, remove container

--- search/templates/part.results.php
+++ search/templates/part.results.php
@@ -1,15 +1,14 @@
-<div id='searchresults'>
+<div id="searchresults">
 	<table>
 		<tbody>
-			<tr class='template'>
-				<td class='type'></td>
-				<td class='result'>
+			<tr class="template">
+				<td class="type"></td>
+				<td class="result">
 					<a>
-						<div class='name'></div>
-						<div class='text'></div>
+						<div class="name"></div>
+						<div class="text"></div>
 					</a>
 				</td>
-				<td class='container'></td>
 			</tr>
 		</tbody>
 	</table>
