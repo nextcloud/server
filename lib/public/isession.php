d3f88ceeb49b9b86d32124163b0cea82567a4911
Thomas Tanghus <thomas@tanghus.net>
2013-09-18 12:01:01 +0200

    Add some docs to the sessions interface.

--- lib/public/isession.php
+++ lib/public/isession.php
@@ -10,34 +10,46 @@
 
 namespace OCP;
 
+/**
+ * Interface ISession
+ *
+ * wrap PHP's internal session handling into the ISession interface
+ */
 interface ISession {
+
 	/**
+	 * Set a value in the session
+	 *
 	 * @param string $key
 	 * @param mixed $value
 	 */
 	public function set($key, $value);
 
 	/**
+	 * Get a value from the session
+	 *
 	 * @param string $key
 	 * @return mixed should return null if $key does not exist
 	 */
 	public function get($key);
 
 	/**
+	 * Check if a named key exists in the session
+	 *
 	 * @param string $key
 	 * @return bool
 	 */
 	public function exists($key);
 
 	/**
-	 * should not throw any errors if $key does not exist
+	 * Remove a $key/$value pair from the session
 	 *
 	 * @param string $key
 	 */
 	public function remove($key);
 
 	/**
-	 * removes all entries within the cache namespace
+	 * Reset and recreate the session
 	 */
 	public function clear();
 
