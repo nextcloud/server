71e129f295996a65e2e7d73c5e6a964ba9f8bebf
Bjoern Schiessle <schiessle@owncloud.com>
2013-09-20 15:47:33 +0200

    initialize variable

--- core/js/share.js
+++ core/js/share.js
@@ -604,6 +604,7 @@ $(document).ready(function() {
 			var itemType = $('#dropdown').data('item-type');
 			var itemSource = $('#dropdown').data('item-source');
 			var allowPublicUpload = $('#sharingDialogAllowPublicUpload').is(':checked');
+			var permissions = 0;
 
 			// Calculate permissions
 			if (allowPublicUpload) {
