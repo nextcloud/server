0a7ee7c3f7b6825815a5aae1e41725481dbbfb08
Arthur Schiwon <blizzz@owncloud.com>
2013-09-24 14:11:47 +0200

    Fix return value from User object to User ID

--- lib/user.php
+++ lib/user.php
@@ -419,7 +419,7 @@ class OC_User {
 		$manager = self::getManager();
 		$username = $manager->checkPassword($uid, $password);
 		if ($username !== false) {
-			return $manger->get($username);
+			return $manager->get($username)->getUID();
 		}
 		return false;
 	}
