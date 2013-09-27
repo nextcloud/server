d124b6965b4ed9aed03396d97bc148038148372b
Thomas Tanghus <thomas@tanghus.net>
2013-09-28 01:35:24 +0200

    Implement PUT an PATCH support

--- tests/lib/appframework/http/RequestTest.php
+++ tests/lib/appframework/http/RequestTest.php
@@ -8,6 +8,7 @@
 
 namespace OC\AppFramework\Http;
 
+global $data;
 
 class RequestTest extends \PHPUnit_Framework_TestCase {
 
@@ -32,6 +33,8 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('Joey', $request->get['nickname']);
 		// Always returns null if variable not set.
 		$this->assertEquals(null, $request->{'flickname'});
+
+		require_once __DIR__ . '/requeststream.php';
 	}
 
 	// urlParams has precedence over POST which has precedence over GET
@@ -75,7 +78,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	* @expectedException BadMethodCallException
+	* @expectedException LogicException
 	*/
 	public function testGetTheMethodRight() {
 		$vars = array(
@@ -100,4 +103,100 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('Joey', $result['nickname']);
 	}
 
+	public function testJsonPost() {
+		$vars = array(
+			'post' => '{"name": "John Q. Public", "nickname": "Joey"}',
+			'method' => 'POST',
+			'server' => array('CONTENT_TYPE' => 'application/json; utf-8'),
+		);
+
+		$request = new Request($vars);
+		$this->assertEquals('POST', $request->method);
+		$result = $request->post;
+		$this->assertEquals('John Q. Public', $result['name']);
+		$this->assertEquals('Joey', $result['nickname']);
+	}
+
+	public function testPatch() {
+		global $data;
+		$data = http_build_query(array('name' => 'John Q. Public', 'nickname' => 'Joey'), '', '&');
+
+		if (in_array('fakeinput', stream_get_wrappers())) {
+			stream_wrapper_unregister('fakeinput');
+		}
+		stream_wrapper_register('fakeinput', 'RequestStream');
+
+		$vars = array(
+			'patch' => $data,
+			'method' => 'PATCH',
+			'server' => array('CONTENT_TYPE' => 'application/x-www-form-urlencoded'),
+		);
+
+		$request = new Request($vars);
+
+		$this->assertEquals('PATCH', $request->method);
+		$result = $request->patch;
+
+		$this->assertEquals('John Q. Public', $result['name']);
+		$this->assertEquals('Joey', $result['nickname']);
+
+		stream_wrapper_unregister('fakeinput');
+	}
+
+	public function testJsonPatch() {
+		global $data;
+		$data = '{"name": "John Q. Public", "nickname": null}';
+
+		if (in_array('fakeinput', stream_get_wrappers())) {
+			stream_wrapper_unregister('fakeinput');
+		}
+		stream_wrapper_register('fakeinput', 'RequestStream');
+
+		$vars = array(
+			'patch' => $data,
+			'method' => 'PATCH',
+			'server' => array('CONTENT_TYPE' => 'application/json; utf-8'),
+		);
+
+		$request = new Request($vars);
+
+		$this->assertEquals('PATCH', $request->method);
+		$result = $request->patch;
+
+		$this->assertEquals('John Q. Public', $result['name']);
+		$this->assertEquals(null, $result['nickname']);
+
+		stream_wrapper_unregister('fakeinput');
+	}
+
+	public function testPutSteam() {
+		global $data;
+		$data = file_get_contents(__DIR__ . '/../../../data/testimage.png');
+
+		if (in_array('fakeinput', stream_get_wrappers())) {
+			stream_wrapper_unregister('fakeinput');
+		}
+		stream_wrapper_register('fakeinput', 'RequestStream');
+
+		$vars = array(
+			'put' => $data,
+			'method' => 'PUT',
+			'server' => array('CONTENT_TYPE' => 'image/png'),
+		);
+
+		$request = new Request($vars);
+		$this->assertEquals('PUT', $request->method);
+		$resource = $request->put;
+		$contents = stream_get_contents($resource);
+		$this->assertEquals($data, $contents);
+
+		try {
+			$resource = $request->put;
+		} catch(\LogicException $e) {
+			stream_wrapper_unregister('fakeinput');
+			return;
+		}
+		$this->fail('Expected LogicException.');
+
+	}
 }
