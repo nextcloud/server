794a133f9ada6d26638f7aba56a2260cb6655df4
Andrew Brown <andrew@casabrown.com>
2013-09-30 18:35:02 -0400

    Re-add type to legacy OC_Search_Result class

--- lib/legacy/search/result.php
+++ lib/legacy/search/result.php
@@ -18,5 +18,18 @@
  */
 
 class OC_Search_Result extends \OC\Search\Result {
-
+    
+	/**
+	 * Create a new search result
+	 * @param string $id unique identifier from application: '[app_name]/[item_identifier_in_app]'
+	 * @param string $name displayed text of result
+	 * @param string $link URL to the result within its app
+	 * @param string $type @deprecated because it is now set in \OC\Search\Result descendants
+	 */
+	public function __construct($id = null, $name = null, $link = null, $type = null) {
+		$this->id = $id;
+		$this->name = $name;
+		$this->link = $link;
+		$this->type = $type;
+	}
 }
