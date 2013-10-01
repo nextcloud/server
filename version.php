bf0f3bf0da64b8b81405846169b5ee27a4289362
Frank Karlitschek <frank@owncloud.org>
2013-10-01 12:37:19 +0200

    use a 4 digit number

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 // We only can count up. Reset minor/patchlevel when updating major/minor version number.
-$OC_Version=array(5, 80, 8);
+$OC_Version=array(5, 80, 8, 0);
 
 // The human radable string
 $OC_VersionString='6.0 pre alpha';
