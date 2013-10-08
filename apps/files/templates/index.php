c14480616da2483b0486bb168a57b7651c94a40e
Bjoern Schiessle <schiessle@owncloud.com>
2013-10-08 17:21:12 +0200

    revert backport from here: https://github.com/owncloud/core/pull/5088 This shouldn't have been backported.

--- apps/files/templates/index.php
+++ apps/files/templates/index.php
@@ -36,7 +36,9 @@
 				</form>
 			</div>
 			<?php if ($_['trash'] ): ?>
-			<input id="trash" type="button" value="<?php p($l->t('Deleted files'));?>" class="button" <?php $_['trashEmpty'] ? p('disabled') : '' ?>></input>
+			<div id="trash" class="button">
+				<a><?php p($l->t('Deleted files'));?></a>
+			</div>
 			<?php endif; ?>
 			<div id="uploadprogresswrapper">
 				<div id="uploadprogressbar"></div>
