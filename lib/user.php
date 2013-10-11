09588b6f1cdf59f125e11f85ce8488aafac2f61b
Nico Kaiser <nico@kaiser.me>
2013-10-11 21:57:09 +0200

    Fix static function call. Sorry.

--- lib/user.php
+++ lib/user.php
@@ -372,7 +372,7 @@ class OC_User {
 			return self::determineDisplayName($user);
 		} else if( isset($_SESSION['display_name']) AND $_SESSION['display_name'] ) {
 			return $_SESSION['display_name'];
-		} else if ( $user = $this->getUser() ) {
+		} else if ( $user = self::getUser() ) {
 			return self::determineDisplayName($user);
 		}
 		else{
