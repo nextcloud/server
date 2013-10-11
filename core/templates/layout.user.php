f8c865993ffcb30750f9e4514d38e0d6b3e3fd07
Vincent Petry <pvince81@owncloud.com>
2013-10-11 12:51:40 +0200

    Fixed viewport layout using commas instead of semicolons
    Fixes #5285

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -14,7 +14,7 @@
 		</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0;">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="apple-itunes-app" content="app-id=543672169">
 		<link rel="shortcut icon" href="<?php print_unescaped(image_path('', 'favicon.png')); ?>" />
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>" />
