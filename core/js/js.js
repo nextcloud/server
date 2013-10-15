4eed2402967422d0a481a84a8bcc689668010289
raghunayyar <me@iraghu.com>
2013-10-15 11:36:44 +0530

    Should Display PasswordToggle Eye Everywhere.

--- core/js/js.js
+++ core/js/js.js
@@ -747,14 +747,8 @@ $(document).ready(function(){
 
 	var setShowPassword = function(input, label) {
 		input.showPassword().keyup(function(){
-			if (input.val().length == 0) {
-				label.hide();
-			}
-			else {
-				label.css("display", "inline").show();
-			}
+			label.css("display", "inline").show();
 		});
-		label.hide();
 	};
 	setShowPassword($('#adminpass'), $('label[for=show]'));
 	setShowPassword($('#pass2'), $('label[for=personal-show]'));
