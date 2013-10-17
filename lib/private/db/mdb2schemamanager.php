e55d2359b1746e29b764a0d366afbb857b797341
Thomas Müller <thomas.mueller@tmit.eu>
2013-10-17 14:54:37 +0200

    removing pointless calls on quoteIdentifier() - reason: name on $tableDiff doesn't exist and my design the name cannot be changed adding PHPDoc

--- lib/private/db/mdb2schemamanager.php
+++ lib/private/db/mdb2schemamanager.php
@@ -61,6 +61,7 @@ class MDB2SchemaManager {
 		$toSchema = $schemaReader->loadSchemaFromFile($file);
 
 		// remove tables we don't know about
+		/** @var $table \Doctrine\DBAL\Schema\Table */
 		foreach($fromSchema->getTables() as $table) {
 			if (!$toSchema->hasTable($table->getName())) {
 				$fromSchema->dropTable($table->getName());
@@ -75,12 +76,6 @@ class MDB2SchemaManager {
 
 		$comparator = new \Doctrine\DBAL\Schema\Comparator();
 		$schemaDiff = $comparator->compare($fromSchema, $toSchema);
-
-		$platform = $this->conn->getDatabasePlatform();
-		$tables = $schemaDiff->newTables + $schemaDiff->changedTables + $schemaDiff->removedTables;
-		foreach($tables as $tableDiff) {
-			$tableDiff->name = $platform->quoteIdentifier($tableDiff->name);
-		}
 
 		if ($generateSql) {
 			return $this->generateChangeScript($schemaDiff);
@@ -110,6 +105,7 @@ class MDB2SchemaManager {
 		$schemaReader = new MDB2SchemaReader(\OC_Config::getObject(), $this->conn->getDatabasePlatform());
 		$fromSchema = $schemaReader->loadSchemaFromFile($file);
 		$toSchema = clone $fromSchema;
+		/** @var $table \Doctrine\DBAL\Schema\Table */
 		foreach($toSchema->getTables() as $table) {
 			$toSchema->dropTable($table->getName());
 		}
