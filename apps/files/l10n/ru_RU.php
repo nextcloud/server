6aee83cadf20f11b23e3fa8db1dcc8a7773deb9e
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-19 08:27:13 -0400

    [tx-robot] updated from transifex

---
+++ apps/files/l10n/ru_RU.php
@@ -0,0 +1,14 @@
+<?php
+$TRANSLATIONS = array(
+"Files" => "Файлы",
+"Error" => "Ошибка",
+"Share" => "Сделать общим",
+"Rename" => "Переименовать",
+"_%n folder_::_%n folders_" => array("","",""),
+"_%n file_::_%n files_" => array("","",""),
+"_Uploading %n file_::_Uploading %n files_" => array("","",""),
+"Upload" => "Загрузка",
+"Cancel upload" => "Отмена загрузки",
+"Download" => "Загрузка"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
