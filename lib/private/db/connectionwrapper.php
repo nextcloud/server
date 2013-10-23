3c710696e8ae72ff2f264d2df8b20cd364fe5003
Thomas Müller <thomas.mueller@tmit.eu>
2013-10-23 12:27:54 +0200

    add missing file header

--- lib/private/db/connectionwrapper.php
+++ lib/private/db/connectionwrapper.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright (c) 2013 Thomas Müller <deepdiver@owncloud.com>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
 
 namespace OC\DB;
 
