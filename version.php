c3b48c69db0c84518897798f8bc78518e0ed6909
Frank Karlitschek <frank@owncloud.org>
2013-10-28 19:05:31 +0100

    beta 2

--- version.php
+++ version.php
@@ -1,10 +1,10 @@
 <?php
 
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel when updating major/minor version number.
-$OC_Version=array(6, 00, 0, 2);
+$OC_Version=array(6, 00, 0, 3);
 
 // The human radable string
-$OC_VersionString='6.0 beta 1';
+$OC_VersionString='6.0 beta 2';
 
 // The ownCloud edition
 $OC_Edition='';
