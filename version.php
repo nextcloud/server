de57e36b6b6ad7b947bb601f0824902401ec4fbe
Lukas Reschke <lukas@statuscode.ch>
2013-10-29 22:11:49 +0100

    Fix typo
    Whooah! - I'm even committing this total important change without peer review ;-)

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel when updating major/minor version number.
 $OC_Version=array(6, 00, 0, 3);
 
-// The human radable string
+// The human readable string
 $OC_VersionString='6.0 beta 2';
 
 // The ownCloud edition
