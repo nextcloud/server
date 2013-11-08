f93dcc5d77ca3f27e4b40903ac602fdc787521d9
Thomas Müller <thomas.mueller@tmit.eu>
2013-11-08 11:24:19 +0100

    the filesystem has to be initialized once we know the user

--- lib/private/api.php
+++ lib/private/api.php
@@ -245,6 +245,10 @@ class OC_API {
 		$return = OC_User::login($authUser, $authPw);
 		if ($return === true) {
 			self::$logoutRequired = true;
+
+			// initialize the user's filesystem
+			\OC_Util::setUpFS(\OC_User::getUser());
+
 			return $authUser;
 		}
 
@@ -252,6 +256,10 @@ class OC_API {
 		$loggedIn = OC_User::isLoggedIn();
 		$ocsApiRequest = isset($_SERVER['HTTP_OCS_APIREQUEST']) ? $_SERVER['HTTP_OCS_APIREQUEST'] === 'true' : false;
 		if ($loggedIn === true && $ocsApiRequest) {
+
+			// initialize the user's filesystem
+			\OC_Util::setUpFS(\OC_User::getUser());
+
 			return OC_User::getUser();
 		}
 
