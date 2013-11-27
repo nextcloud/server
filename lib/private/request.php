c47e46fda096563eadfae49308c7f436611a286b
Robin Appelman <icewind@owncloud.com>
2013-11-27 15:35:49 +0100

    fix custom routes defined by apps

--- lib/private/request.php
+++ lib/private/request.php
@@ -165,7 +165,7 @@ class OC_Request {
 		if (strpos($path_info, $name) === 0) {
 			$path_info = substr($path_info, strlen($name));
 		}
-		return rtrim($path_info, '/');
+		return $path_info;
 	}
 
 	/**
