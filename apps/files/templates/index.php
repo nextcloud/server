24a08c686df393e41fca251776ce0a414d75976f
Vincent Petry <pvince81@owncloud.com>
2013-11-28 10:17:01 +0100

    New file box now has default file name + extension
    Whenever a user creates a file or folder in the web UI, the input field
    will contain a default file name, pre-selected up to the extension for
    easier typing.
    
    The purpose is mostly to prevent users creating text files without an
    extension.
    
    Fixes #6045

--- apps/files/templates/index.php
+++ apps/files/templates/index.php
@@ -5,9 +5,9 @@
 				<a><?php p($l->t('New'));?></a>
 				<ul>
 					<li style="background-image:url('<?php p(OCP\mimetype_icon('text/plain')) ?>')"
-						data-type='file'><p><?php p($l->t('Text file'));?></p></li>
+						data-type='file' data-newname='<?php p($l->t('New text file.txt')) ?>'><p><?php p($l->t('Text file'));?></p></li>
 					<li style="background-image:url('<?php p(OCP\mimetype_icon('dir')) ?>')"
-						data-type='folder'><p><?php p($l->t('Folder'));?></p></li>
+						data-type='folder' data-newname='<?php p($l->t('New folder')) ?>'><p><?php p($l->t('Folder'));?></p></li>
 					<li style="background-image:url('<?php p(OCP\image_path('core', 'places/link.svg')) ?>')"
 						data-type='web'><p><?php p($l->t('From link'));?></p></li>
 				</ul>
