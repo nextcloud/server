71589e65d24657a6d61fc22b3c67d5a1f5b1404a
Bjoern Schiessle <schiessle@owncloud.com>
2013-11-29 16:43:50 +0100

    fix restore from files in sub-folders

--- apps/files_trashbin/js/trash.js
+++ apps/files_trashbin/js/trash.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 			deleteAction.removeClass('delete-icon').addClass('progress-icon');
 			disableActions();
 			$.post(OC.filePath('files_trashbin', 'ajax', 'undelete.php'),
-					{files: JSON.stringify([filename]), dirlisting: tr.attr('data-dirlisting')},
+					{files: JSON.stringify([$('#dir').val() + '/' + filename]), dirlisting: tr.attr('data-dirlisting')},
 					function(result) {
 						for (var i = 0; i < result.data.success.length; i++) {
 							var row = document.getElementById(result.data.success[i].filename);
@@ -229,7 +229,7 @@ function getSelectedFiles(property){
 	elements.each(function(i,element){
 		var file={
 			name:$(element).attr('data-filename'),
-			file:$(element).attr('data-file'),
+			file:$('#dir').val() + "/" + $(element).attr('data-file'),
 			timestamp:$(element).attr('data-timestamp'),
 			type:$(element).attr('data-type'),
 			dirlisting:$(element).attr('data-dirlisting')
