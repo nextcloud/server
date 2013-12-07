95c90ddbc459f287fa368dd2290191a902cdd6dc
Morris Jobke <morris.jobke@gmail.com>
2013-12-07 13:29:14 +0100

    fix wrong test data in l10n

--- tests/data/l10n/de.php
+++ tests/data/l10n/de.php
@@ -1,5 +1,5 @@
 <?php
 $TRANSLATIONS = array(
-	"_%n file__%n files_" => array("%n Datei", "%n Dateien")
+	"_%n file_::_%n files_" => array("%n Datei", "%n Dateien")
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
