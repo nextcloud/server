04f73275ba222f0aae80cf80890477b141cd4902
Vincent Petry <pvince81@owncloud.com>
2013-12-09 13:18:13 +0100

    Now settings CSS class with appid in content DIV

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -123,7 +123,7 @@
 		</div></nav>
 
 		<div id="content-wrapper">
-			<div id="content">
+			<div id="content" class="app-<?php p($_['appid']) ?>">
 				<?php print_unescaped($_['content']); ?>
 			</div>
 		</div>
