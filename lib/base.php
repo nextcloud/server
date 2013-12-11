f26ba5846d6859714e2f32f8f3625e57855ea703
Arthur Schiwon <blizzz@owncloud.com>
2013-12-11 14:01:48 +0100

    coding style

--- lib/base.php
+++ lib/base.php
@@ -527,6 +527,7 @@ class OC {
 
 		OC_User::useBackend(new OC_User_Database());
 		OC_Group::useBackend(new OC_Group_Database());
+
 		if (isset($_SERVER['PHP_AUTH_USER']) && self::$session->exists('user_id')
 			&& $_SERVER['PHP_AUTH_USER'] !== self::$session->get('loginname')) {
 			$sessionUser = self::$session->get('loginname');
