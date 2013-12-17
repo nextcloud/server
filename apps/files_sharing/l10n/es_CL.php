6488ff2c759b475d20a762741714e50d451a98c6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-17 16:43:17 -0500

    [tx-robot] updated from transifex

---
+++ apps/files_sharing/l10n/es_CL.php
@@ -0,0 +1,6 @@
+<?php
+$TRANSLATIONS = array(
+"Password" => "Clave",
+"Upload" => "Subir"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- core/l10n/es_CL.php
+++ core/l10n/es_CL.php
@@ -1,9 +1,12 @@
 <?php
 $TRANSLATIONS = array(
+"Settings" => "Configuración",
 "_%n minute ago_::_%n minutes ago_" => array("",""),
 "_%n hour ago_::_%n hours ago_" => array("",""),
 "_%n day ago_::_%n days ago_" => array("",""),
 "_%n month ago_::_%n months ago_" => array("",""),
-"_{count} file conflict_::_{count} file conflicts_" => array("","")
+"_{count} file conflict_::_{count} file conflicts_" => array("",""),
+"Password" => "Clave",
+"Username" => "Usuario"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
