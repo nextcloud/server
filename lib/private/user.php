c2e2c59ca7aa873bd07de04ea701a8b351383aec
nhirokinet <nhirokinet@nhiroki.net>
2013-12-22 01:31:04 +0900

    Update user.php to fix duplicate session-duplicate

--- lib/private/user.php
+++ lib/private/user.php
@@ -243,7 +243,6 @@ class OC_User {
 		OC_Hook::emit( "OC_User", "pre_login", array( "run" => &$run, "uid" => $uid ));
 
 		if($uid) {
-			session_regenerate_id(true);
 			self::setUserId($uid);
 			self::setDisplayName($uid);
 			OC_Hook::emit( "OC_User", "post_login", array( "uid" => $uid, 'password'=>'' ));
