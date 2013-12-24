dbbd99db0929b3dfdc4a56ee8b93e5447ad36265
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-24 01:55:40 -0500

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/ko.php
+++ apps/files_encryption/l10n/ko.php
@@ -1,26 +1,26 @@
 <?php
 $TRANSLATIONS = array(
-"Recovery key successfully enabled" => "복구키가 성공적으로 활성화 되었습니다",
-"Could not enable recovery key. Please check your recovery key password!" => "복구키를 활성화 할수 없습니다. 복구키의 비밀번호를 확인해주세요!",
-"Recovery key successfully disabled" => "복구키가 성공적으로 비활성화 되었습니다",
-"Could not disable recovery key. Please check your recovery key password!" => "복구키를 비활성화 할수 없습니다. 복구키의 비밀번호를 확인해주세요!",
+"Recovery key successfully enabled" => "복구 키가 성공적으로 활성화되었습니다",
+"Could not enable recovery key. Please check your recovery key password!" => "복구 키를 활성화 할 수 없습니다. 복구 키의 암호를 확인해 주세요!",
+"Recovery key successfully disabled" => "복구 키가 성공적으로 비활성화 되었습니다",
+"Could not disable recovery key. Please check your recovery key password!" => "복구 키를 비활성화 할 수 없습니다. 복구 키의 암호를 확인해주세요!",
 "Password successfully changed." => "암호가 성공적으로 변경되었습니다",
-"Could not change the password. Maybe the old password was not correct." => "암호를 변경할수 없습니다. 아마도 예전 암호가 정확하지 않은것 같습니다.",
-"Private key password successfully updated." => "개인키 암호가 성공적으로 업데이트 됨.",
+"Could not change the password. Maybe the old password was not correct." => "암호를 변경할 수 없습니다. 예전 암호가 정확하지 않은 것 같습니다.",
+"Private key password successfully updated." => "개인 키 암호가 성공적으로 업데이트 됨.",
 "Saving..." => "저장 중...",
 "personal settings" => "개인 설정",
 "Encryption" => "암호화",
-"Recovery key password" => "키 비밀번호 복구",
+"Recovery key password" => "복구 키 암호",
 "Enabled" => "활성화",
 "Disabled" => "비활성화",
-"Change recovery key password:" => "복구 키 비밀번호 변경",
-"Old Recovery key password" => "예전 복구 키 비밀번호",
-"New Recovery key password" => "새 복구 키 비밀번호",
+"Change recovery key password:" => "복구 키 암호 변경:",
+"Old Recovery key password" => "이전 복구 키 암호",
+"New Recovery key password" => "새 복구 키 암호",
 "Change Password" => "암호 변경",
-"Old log-in password" => "예전 로그인 암호",
+"Old log-in password" => "이전 로그인 암호",
 "Current log-in password" => "현재 로그인 암호",
 "Update Private Key Password" => "개인 키 암호 업데이트",
 "File recovery settings updated" => "파일 복구 설정 업데이트됨",
-"Could not update file recovery" => "파일 복구를 업데이트 할수 없습니다"
+"Could not update file recovery" => "파일 복구를 업데이트 할 수 없습니다"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
