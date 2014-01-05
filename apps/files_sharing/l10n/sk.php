e4616199df5ca6b5ba490455a505d36dc954f0f6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-05 01:55:53 -0500

    [tx-robot] updated from transifex

--- apps/files_sharing/l10n/sk.php
+++ apps/files_sharing/l10n/sk.php
@@ -1,5 +1,5 @@
 <?php
 $TRANSLATIONS = array(
-"Delete" => "Odstrániť"
+"Download" => "Stiahnuť"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
--- apps/user_ldap/l10n/sk.php
+++ apps/user_ldap/l10n/sk.php
@@ -1,6 +1,7 @@
 <?php
 $TRANSLATIONS = array(
 "_%s group found_::_%s groups found_" => array("","",""),
-"_%s user found_::_%s users found_" => array("","","")
+"_%s user found_::_%s users found_" => array("","",""),
+"Save" => "Uložiť"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
