d2f2645a6a7e802c6cda31747481fe49b7ca0807
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-07 01:56:11 -0500

    [tx-robot] updated from transifex

--- apps/files_trashbin/l10n/el.php
+++ apps/files_trashbin/l10n/el.php
@@ -3,11 +3,11 @@ $TRANSLATIONS = array(
 "Couldn't delete %s permanently" => "Αδύνατη η μόνιμη διαγραφή του %s",
 "Couldn't restore %s" => "Αδυναμία επαναφοράς %s",
 "Error" => "Σφάλμα",
-"restored" => "έγινε επαναφορά",
+"restored" => "επαναφέρθηκαν",
 "Nothing in here. Your trash bin is empty!" => "Δεν υπάρχει τίποτα εδώ. Ο κάδος σας είναι άδειος!",
 "Name" => "Όνομα",
 "Restore" => "Επαναφορά",
-"Deleted" => "Διαγράφηκε",
+"Deleted" => "Διαγραμμένα",
 "Delete" => "Διαγραφή",
 "Deleted Files" => "Διαγραμμένα Αρχεία"
 );
