39d2c18793d32fd429cb44d3f61f0e6d6a018cda
Robin Appelman <icewind@owncloud.com>
2014-01-13 14:42:14 +0100

    Extends phpdoc for \OCP\File\FileInfo

--- lib/public/files/fileinfo.php
+++ lib/public/files/fileinfo.php
@@ -22,65 +22,98 @@ interface FileInfo extends \ArrayAccess, \JsonSerializable {
 	public function jsonSerialize();
 
 	/**
+	 * Get the Etag of the file or folder
+	 *
 	 * @return string
 	 */
 	public function getEtag();
 
 	/**
+	 * Get the size in bytes for the file or folder
+	 *
 	 * @return int
 	 */
 	public function getSize();
 
 	/**
+	 * Get the last modified date as timestamp for the file or folder
+	 *
 	 * @return int
 	 */
 	public function getMtime();
 
 	/**
+	 * Get the name of the file or folder
+	 *
 	 * @return string
 	 */
 	public function getName();
 
 	/**
+	 * Get the path relative to the storage
+	 *
 	 * @return string
 	 */
 	public function getInternalPath();
 
 	/**
+	 * Get the absolute path
+	 *
 	 * @return string
 	 */
 	public function getPath();
 
 	/**
+	 * Get the full mimetype of the file or folder i.e. 'image/png'
+	 *
 	 * @return string
 	 */
 	public function getMimetype();
 
 	/**
+	 * Get the first part of the mimetype of the file or folder i.e. 'image'
+	 *
+	 * @return string
+	 */
+	public function getMimePart();
+
+	/**
+	 * Get the storage the file or folder is storage on
+	 *
 	 * @return \OCP\Files\Storage
 	 */
 	public function getStorage();
 
 	/**
+	 * Get the file id of the file or folder
+	 *
 	 * @return int
 	 */
 	public function getId();
 
 	/**
-	 * @return string
-	 */
-	public function getMimePart();
-	/**
+	 * Check whether the file is encrypted
+	 *
 	 * @return bool
 	 */
 	public function isEncrypted();
 
 	/**
+	 * Get the permissions of the file or folder as bitmasked combination of the following constants
+	 * \OCP\PERMISSION_CREATE
+	 * \OCP\PERMISSION_READ
+	 * \OCP\PERMISSION_UPDATE
+	 * \OCP\PERMISSION_DELETE
+	 * \OCP\PERMISSION_SHARE
+	 * \OCP\PERMISSION_ALL
+	 *
 	 * @return int
 	 */
 	public function getPermissions();
 
 	/**
+	 * Check whether this is a file or a folder
+	 *
 	 * @return \OCP\Files\FileInfo::TYPE_FILE | \OCP\Files\FileInfo::TYPE_FOLDER
 	 */
 	public function getType();
