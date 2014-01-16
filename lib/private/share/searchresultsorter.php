8a4dbdedcf6fe5aab1fc084c3e834e5a04e1bdf2
Arthur Schiwon <blizzz@owncloud.com>
2014-01-16 10:30:18 +0100

    wrong tld

--- lib/private/share/searchresultsorter.php
+++ lib/private/share/searchresultsorter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2014 Arthur Schiwon <blizzz@owncloud.bzoc>
+ * Copyright (c) 2014 Arthur Schiwon <blizzz@owncloud.com>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
