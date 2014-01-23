4734146580d8175e9e6178097258b7f87e7aba86
Vincent Petry <pvince81@owncloud.com>
2014-01-23 20:50:09 +0100

    Fixed max upload size input field style
    Added "type=text" makes it look like nice like all other fields

--- apps/files/templates/admin.php
+++ apps/files/templates/admin.php
@@ -5,7 +5,7 @@
 		<h2><?php p($l->t('File handling')); ?></h2>
 		<?php if($_['uploadChangable']):?>
 			<label for="maxUploadSize"><?php p($l->t( 'Maximum upload size' )); ?> </label>
-			<input name='maxUploadSize' id="maxUploadSize" value='<?php p($_['uploadMaxFilesize']) ?>'/>
+			<input type="text" name='maxUploadSize' id="maxUploadSize" value='<?php p($_['uploadMaxFilesize']) ?>'/>
 			<?php if($_['displayMaxPossibleUploadSize']):?>
 				(<?php p($l->t('max. possible: ')); p($_['maxPossibleUploadSize']) ?>)
 			<?php endif;?>
