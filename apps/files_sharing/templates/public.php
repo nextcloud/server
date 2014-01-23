617aa3cf29955a23eed2ececc8e1876d77002223
Thomas Müller <thomas.mueller@tmit.eu>
2014-01-23 20:15:10 +0100

    Instead of 'No preview available for ...' we simple display the mieme-type icon

--- apps/files_sharing/templates/public.php
+++ apps/files_sharing/templates/public.php
@@ -32,16 +32,10 @@
 						<source src="<?php p($_['downloadURL']); ?>" type="<?php p($_['mimetype']); ?>" />
 					</video>
 				</div>
-			<?php elseif (\OC\Preview::isMimeSupported($_['mimetype'])): ?>
+			<?php else: ?>
 				<div id="imgframe">
 					<img src="<?php p(OCP\Util::linkToRoute( 'core_ajax_public_preview', array('x' => 500, 'y' => 500, 'file' => urlencode($_['directory_path']), 't' => $_['dirToken']))); ?>" class="publicpreview"/>
 				</div>
-			<?php else: ?>
-				<ul id="noPreview">
-					<li class="error">
-						<?php p($l->t('No preview available for').' '.$_['filename']); ?><br />
-					</li>
-				</ul>
 			<?php endif; ?>
 			<div class="directDownload">
 				<a href="<?php p($_['downloadURL']); ?>" id="download" class="button">
@@ -55,8 +49,10 @@
 			</div>
 		<?php endif; ?>
 	</div>
-	<footer>
-		<p class="info">
-			<?php print_unescaped($theme->getLongFooter()); ?>
-		</p>
-	</footer>
+
+</div>
+<footer>
+	<p class="info">
+		<?php print_unescaped($theme->getLongFooter()); ?>
+	</p>
+</footer>
