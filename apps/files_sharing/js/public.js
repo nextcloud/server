37d2c0a4befb194c007ed20f3c82f9f298097d4e
Jörn Friedrich Dreyer <jfd@butonic.de>
2014-02-04 16:32:39 +0100

    remove unused js code and css rules

--- apps/files_sharing/js/public.js
+++ apps/files_sharing/js/public.js
@@ -9,8 +9,6 @@ function fileDownloadPath(dir, file) {
 
 $(document).ready(function() {
 
-	$('#data-upload-form').tipsy({gravity:'ne', fade:true});
-
 	if (typeof FileActions !== 'undefined') {
 		var mimetype = $('#mimetype').val();
 		// Show file preview if previewer is available, images are already handled by the template
@@ -56,16 +54,6 @@ $(document).ready(function() {
 			dirToken: $('#dirToken').val(),
 			subdir: $('input#dir').val()
 		};
-	});
-
-	// Add Uploadprogress Wrapper to controls bar
-	$('#controls').append($('#controls .actions div#uploadprogresswrapper'));
-	$('#uploadprogresswrapper').addClass('public_actions');
-
-	// Cancel upload trigger
-	$('#cancel_upload_button').click(function() {
-		OC.Upload.cancelUploads();
-		procesSelection();
 	});
 
 	$('#directLink').focus();
