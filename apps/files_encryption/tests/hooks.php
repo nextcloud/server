91254c304d454b6f7977037dbd5dd2db5e00ff9f
Bjoern Schiessle <schiessle@owncloud.com>
2014-02-07 15:57:13 +0100

    name users after test

--- apps/files_encryption/tests/hooks.php
+++ apps/files_encryption/tests/hooks.php
@@ -36,8 +36,8 @@ use OCA\Encryption;
  */
 class Test_Encryption_Hooks extends \PHPUnit_Framework_TestCase {
 
-	const TEST_ENCRYPTION_HOOKS_USER1 = "test-proxy-user1";
-	const TEST_ENCRYPTION_HOOKS_USER2 = "test-proxy-user2";
+	const TEST_ENCRYPTION_HOOKS_USER1 = "test-encryption-hooks-user1";
+	const TEST_ENCRYPTION_HOOKS_USER2 = "test-encryption-hooks-user2";
 
 	/**
 	 * @var \OC_FilesystemView
