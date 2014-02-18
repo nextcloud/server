2166683e3b5810bfeca2540a038e212fb8ca75ed
Robin Appelman <icewind@owncloud.com>
2014-02-18 16:31:40 +0100

    Allow setting the frequency of which the file watcher checks for updates

--- lib/private/files/cache/watcher.php
+++ lib/private/files/cache/watcher.php
@@ -12,6 +12,14 @@ namespace OC\Files\Cache;
  * check the storage backends for updates and change the cache accordingly
  */
 class Watcher {
+	const CHECK_NEVER = 0; // never check the underlying filesystem for updates
+	const CHECK_ONCE = 1; // check the underlying filesystem for updates once every request for each file
+	const CHECK_ALWAYS = 2; // always check the underlying filesystem for updates
+
+	protected $watchPolicy = self::CHECK_ONCE;
+
+	protected $checkedPaths = array();
+
 	/**
 	 * @var \OC\Files\Storage\Storage $storage
 	 */
@@ -23,7 +31,7 @@ class Watcher {
 	protected $cache;
 
 	/**
-	 * @var Scanner $scanner;
+	 * @var Scanner $scanner ;
 	 */
 	protected $scanner;
 
@@ -37,26 +45,38 @@ class Watcher {
 	}
 
 	/**
+	 * @param int $policy either \OC\Files\Cache\Watcher::UPDATE_NEVER, \OC\Files\Cache\Watcher::UPDATE_ONCE, \OC\Files\Cache\Watcher::UPDATE_ALWAYS
+	 */
+	public function setPolicy($policy) {
+		$this->watchPolicy = $policy;
+	}
+
+	/**
 	 * check $path for updates
 	 *
 	 * @param string $path
 	 * @return boolean | array true if path was updated, otherwise the cached data is returned
 	 */
 	public function checkUpdate($path) {
-		$cachedEntry = $this->cache->get($path);
-		if ($this->storage->hasUpdated($path, $cachedEntry['storage_mtime'])) {
-			if ($this->storage->is_dir($path)) {
-				$this->scanner->scan($path, Scanner::SCAN_SHALLOW);
-			} else {
-				$this->scanner->scanFile($path);
+		if ($this->watchPolicy === self::CHECK_ALWAYS or ($this->watchPolicy === self::CHECK_ONCE and array_search($path, $this->checkedPaths) === false)) {
+			$cachedEntry = $this->cache->get($path);
+			$this->checkedPaths[] = $path;
+			if ($this->storage->hasUpdated($path, $cachedEntry['storage_mtime'])) {
+				if ($this->storage->is_dir($path)) {
+					$this->scanner->scan($path, Scanner::SCAN_SHALLOW);
+				} else {
+					$this->scanner->scanFile($path);
+				}
+				if ($cachedEntry['mimetype'] === 'httpd/unix-directory') {
+					$this->cleanFolder($path);
+				}
+				$this->cache->correctFolderSize($path);
+				return true;
 			}
-			if ($cachedEntry['mimetype'] === 'httpd/unix-directory') {
-				$this->cleanFolder($path);
-			}
-			$this->cache->correctFolderSize($path);
-			return true;
+			return $cachedEntry;
+		} else {
+			return false;
 		}
-		return $cachedEntry;
 	}
 
 	/**
