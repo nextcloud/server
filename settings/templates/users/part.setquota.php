63645461f01c915ac0a4aadd0b81ab534209cdbc
raghunayyar <me@iraghu.com>
2014-02-19 21:32:10 +0530

    Fixes Markup in templates.

--- settings/templates/users/part.setquota.php
+++ settings/templates/users/part.setquota.php
@@ -6,37 +6,34 @@
 		<!-- Default storage -->
 		<span><?php p($l->t('Default Quota'));?></span>
 		<?php if((bool) $_['isadmin']): ?>
-		<select class='quota' data-inputtitle="<?php p($l->t('Please enter storage quota (ex: "512 MB" or "12 GB")')) ?>">
-			<option
-				<?php if($_['default_quota'] === 'none') print_unescaped('selected="selected"');?>
-				value='none'>
-				<?php p($l->t('Unlimited'));?>
-			</option>
-			<?php foreach($_['quota_preset'] as $preset):?>
-				<?php if($preset !== 'default'):?>
-				<option
-					<?php if($_['default_quota']==$preset) print_unescaped('selected="selected"');?>
-					value='<?php p($preset);?>'>
-					<?php p($preset);?>
+			<select class='quota' data-inputtitle="<?php p($l->t('Please enter storage quota (ex: "512 MB" or "12 GB")')) ?>">
+				<option <?php if($_['default_quota'] === 'none') print_unescaped('selected="selected"');?> value='none'>
+					<?php p($l->t('Unlimited'));?>
 				</option>
+				<?php foreach($_['quota_preset'] as $preset):?>
+					<?php if($preset !== 'default'):?>
+						<option <?php if($_['default_quota']==$preset) print_unescaped('selected="selected"');?> value='<?php p($preset);?>'>
+							<?php p($preset);?>
+						</option>
+					<?php endif;?>
+				<?php endforeach;?>
+				<?php if($_['defaultQuotaIsUserDefined']):?>
+					<option selected="selected" value='<?php p($_['default_quota']);?>'>
+						<?php p($_['default_quota']);?>
+					</option>
 				<?php endif;?>
-			<?php endforeach;?>
-			<?php if($_['defaultQuotaIsUserDefined']):?>
-				<option selected="selected" value='<?php p($_['default_quota']);?>'>
+				<option data-new value='other'>
+					<?php p($l->t('Other'));?>
+					...
+				</option>
+			</select>
+		<?php endif; ?>
+		<?php if((bool) !$_['isadmin']): ?>
+			<select class='quota' disabled="disabled">
+				<option selected="selected">
 					<?php p($_['default_quota']);?>
 				</option>
-			<?php endif;?>
-			<option data-new value='other'>
-				<?php p($l->t('Other'));?>
-				...
-			</option>
-		</select>
-	<?php endif; ?>
-	<?php if((bool) !$_['isadmin']): ?>
-		<select class='quota' disabled="disabled">
-			<option selected="selected">
-		<?php p($_['default_quota']);?>
-			</option>
-		</select>
-	<?php endif; ?>
+			</select>
+		<?php endif; ?>
+	</div>
 </div>
