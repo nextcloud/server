7bc69c7ea5ca73883ed1b8869929789bf7e85cd7
Lukas Reschke <lukas@statuscode.ch>
2014-02-22 08:07:57 +0100

    Backport #7259 to stable5

--- lib/updater.php
+++ lib/updater.php
@@ -97,6 +97,19 @@ class OC_Updater extends BasicEmitter {
 		$currentVersion = implode('.', \OC_Util::getVersion());
 		\OC_Log::write('core', 'starting upgrade from ' . $installedVersion . ' to ' . $currentVersion, \OC_Log::WARN);
 		$this->emit('\OC_Updater', 'maintenanceStart');
+
+		/*
+		 * START CONFIG CHANGES FOR OLDER VERSIONS
+		 */
+		if (version_compare($currentVersion, '5.00.29', '<')) {
+			// Add the overwriteHost config if it is not existant
+			// This is added to prevent host header poisoning
+			\OC_Config::setValue('trusted_domains', \OC_Config::getValue('trusted_domains', array(\OC_Request::serverHost()))); 
+		}
+		/*
+		 * STOP CONFIG CHANGES FOR OLDER VERSIONS
+		 */
+
 		try {
 			\OC_DB::updateDbFromStructure(\OC::$SERVERROOT . '/db_structure.xml');
 			$this->emit('\OC_Updater', 'dbUpgrade');
