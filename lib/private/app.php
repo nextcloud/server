195bdff91fc55bf4d2c80ff7d978d74e6efac7a8
Myles McNamara <myles@smyl.es>
2014-02-28 11:19:19 -0500

    move replace to inside isset

--- lib/private/app.php
+++ lib/private/app.php
@@ -218,9 +218,9 @@ class OC_App{
 			}else{
 				$appdata=OC_OCSClient::getApplication($app);
 				$download=OC_OCSClient::getApplicationDownload($app, 1);
-				// Replace spaces in download link without encoding entire URL
-				$download['downloadlink'] = str_replace(' ', '%20', $download['downloadlink']);
 				if(isset($download['downloadlink']) and $download['downloadlink']!='') {
+					// Replace spaces in download link without encoding entire URL
+					$download['downloadlink'] = str_replace(' ', '%20', $download['downloadlink']);
 					$info = array('source'=>'http', 'href'=>$download['downloadlink'], 'appdata'=>$appdata);
 					$app=OC_Installer::installApp($info);
 				}
