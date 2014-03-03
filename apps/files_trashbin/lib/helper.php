5ddabd7cd02108718b286b36de5927d90afc150a
Bjoern Schiessle <schiessle@owncloud.com>
2014-03-03 12:23:21 +0100

    fix mimetype detection, introduce fake etag to identify preview images

--- apps/files_trashbin/lib/helper.php
+++ apps/files_trashbin/lib/helper.php
@@ -41,7 +41,7 @@ class Helper
 					$result[] = array(
 						'id' => $id,
 						'timestamp' => $timestamp,
-						'mime' => $view->getMimeType($dir . '/' . $entryName),
+						'mime' => \OC_Helper::getFileNameMimeType($id),
 						'type' => $view->is_dir($dir . '/' . $entryName) ? 'dir' : 'file',
 						'location' => $dir,
 					);
@@ -58,6 +58,7 @@ class Helper
 			$i['name'] = $r['id'];
 			$i['date'] = \OCP\Util::formatDate($r['timestamp']);
 			$i['timestamp'] = $r['timestamp'];
+			$i['etag'] = $r['timestamp']; // add fake etag, it is only needed to identify the preview image
 			$i['mimetype'] = $r['mime'];
 			$i['type'] = $r['type'];
 			if ($i['type'] === 'file') {
