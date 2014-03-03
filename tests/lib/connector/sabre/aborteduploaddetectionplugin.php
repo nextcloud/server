331fc55e2d876123580986264f47318afcf9352b
Robin Appelman <icewind@owncloud.com>
2014-03-03 14:27:24 +0100

    Update unit tests to use the new injection

--- tests/lib/connector/sabre/aborteduploaddetectionplugin.php
+++ tests/lib/connector/sabre/aborteduploaddetectionplugin.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Copyright (c) 2013 Thomas Müller <thomas.mueller@tmit.eu>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
  */
-
 class Test_OC_Connector_Sabre_AbortedUploadDetectionPlugin extends PHPUnit_Framework_TestCase {
 
 	/**
@@ -18,17 +18,18 @@ class Test_OC_Connector_Sabre_AbortedUploadDetectionPlugin extends PHPUnit_Frame
 	 */
 	private $plugin;
 
-	public function setUp() {
+	private function init($view) {
 		$this->server = new Sabre_DAV_Server();
-		$this->plugin = new OC_Connector_Sabre_AbortedUploadDetectionPlugin();
+		$this->plugin = new OC_Connector_Sabre_AbortedUploadDetectionPlugin($view);
 		$this->plugin->initialize($this->server);
 	}
 
 	/**
 	 * @dataProvider lengthProvider
 	 */
-	public function testLength($expected, $headers)
-	{
+	public function testLength($expected, $headers) {
+		$this->init(null);
+
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
 		$length = $this->plugin->getLength();
 		$this->assertEquals($expected, $length);
@@ -37,9 +38,8 @@ class Test_OC_Connector_Sabre_AbortedUploadDetectionPlugin extends PHPUnit_Frame
 	/**
 	 * @dataProvider verifyContentLengthProvider
 	 */
-	public function testVerifyContentLength($method, $fileSize, $headers)
-	{
-		$this->plugin->fileView = $this->buildFileViewMock($fileSize);
+	public function testVerifyContentLength($method, $fileSize, $headers) {
+		$this->init($this->buildFileViewMock($fileSize));
 
 		$headers['REQUEST_METHOD'] = $method;
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
@@ -51,12 +51,11 @@ class Test_OC_Connector_Sabre_AbortedUploadDetectionPlugin extends PHPUnit_Frame
 	 * @dataProvider verifyContentLengthFailedProvider
 	 * @expectedException Sabre_DAV_Exception_BadRequest
 	 */
-	public function testVerifyContentLengthFailed($method, $fileSize, $headers)
-	{
-		$this->plugin->fileView = $this->buildFileViewMock($fileSize);
-
+	public function testVerifyContentLengthFailed($method, $fileSize, $headers) {
+		$view = $this->buildFileViewMock($fileSize);
+		$this->init($view);
 		// we expect unlink to be called
-		$this->plugin->fileView->expects($this->once())->method('unlink');
+		$view->expects($this->once())->method('unlink');
 
 		$headers['REQUEST_METHOD'] = $method;
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
@@ -92,7 +91,7 @@ class Test_OC_Connector_Sabre_AbortedUploadDetectionPlugin extends PHPUnit_Frame
 
 	private function buildFileViewMock($fileSize) {
 		// mock filesystem
-		$view = $this->getMock('\OC\Files\View', array('filesize', 'unlink'), array(), '', FALSE);
+		$view = $this->getMock('\OC\Files\View', array('filesize', 'unlink'), array(), '', false);
 		$view->expects($this->any())->method('filesize')->withAnyParameters()->will($this->returnValue($fileSize));
 
 		return $view;
--- tests/lib/connector/sabre/quotaplugin.php
+++ tests/lib/connector/sabre/quotaplugin.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Copyright (c) 2013 Thomas Müller <thomas.mueller@tmit.eu>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
  */
-
 class Test_OC_Connector_Sabre_QuotaPlugin extends PHPUnit_Framework_TestCase {
 
 	/**
@@ -18,17 +18,18 @@ class Test_OC_Connector_Sabre_QuotaPlugin extends PHPUnit_Framework_TestCase {
 	 */
 	private $plugin;
 
-	public function setUp() {
+	private function init($quota) {
+		$view = $this->buildFileViewMock($quota);
 		$this->server = new Sabre_DAV_Server();
-		$this->plugin = new OC_Connector_Sabre_QuotaPlugin();
+		$this->plugin = new OC_Connector_Sabre_QuotaPlugin($view);
 		$this->plugin->initialize($this->server);
 	}
 
 	/**
 	 * @dataProvider lengthProvider
 	 */
-	public function testLength($expected, $headers)
-	{
+	public function testLength($expected, $headers) {
+		$this->init(0);
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
 		$length = $this->plugin->getLength();
 		$this->assertEquals($expected, $length);
@@ -37,9 +38,8 @@ class Test_OC_Connector_Sabre_QuotaPlugin extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider quotaOkayProvider
 	 */
-	public function testCheckQuota($quota, $headers)
-	{
-		$this->plugin->fileView = $this->buildFileViewMock($quota);
+	public function testCheckQuota($quota, $headers) {
+		$this->init($quota);
 
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
 		$result = $this->plugin->checkQuota('');
@@ -50,9 +50,8 @@ class Test_OC_Connector_Sabre_QuotaPlugin extends PHPUnit_Framework_TestCase {
 	 * @expectedException Sabre_DAV_Exception_InsufficientStorage
 	 * @dataProvider quotaExceededProvider
 	 */
-	public function testCheckExceededQuota($quota, $headers)
-	{
-		$this->plugin->fileView = $this->buildFileViewMock($quota);
+	public function testCheckExceededQuota($quota, $headers) {
+		$this->init($quota);
 
 		$this->server->httpRequest = new Sabre_HTTP_Request($headers);
 		$this->plugin->checkQuota('');
@@ -92,7 +91,7 @@ class Test_OC_Connector_Sabre_QuotaPlugin extends PHPUnit_Framework_TestCase {
 
 	private function buildFileViewMock($quota) {
 		// mock filesysten
-		$view = $this->getMock('\OC\Files\View', array('free_space'), array(), '', FALSE);
+		$view = $this->getMock('\OC\Files\View', array('free_space'), array(), '', false);
 		$view->expects($this->any())->method('free_space')->withAnyParameters()->will($this->returnValue($quota));
 
 		return $view;
