a687498547448d6ff11eccc0acddcf0647873ce8
Robin Appelman <icewind@owncloud.com>
2014-03-05 13:19:08 +0100

    Fix encryption webdav tests

--- apps/files_encryption/tests/util.php
+++ apps/files_encryption/tests/util.php
@@ -510,7 +510,11 @@ class Test_Encryption_Util extends \PHPUnit_Framework_TestCase {
 	 */
 	public static function loginHelper($user, $create = false, $password = false) {
 		if ($create) {
-			\OC_User::createUser($user, $user);
+			try {
+				\OC_User::createUser($user, $user);
+			} catch(\Exception $e) { // catch username is already being used from previous aborted runs
+
+			}
 		}
 
 		if ($password === false) {
