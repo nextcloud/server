b442a9b4c9efe90f164de683cb7c4a773bba4563
Lukas Reschke <lukas@statuscode.ch>
2014-03-06 22:46:16 +0100

    Revert "Use $installedVersion instead of $currentVersion"
    This reverts commit aef1e5112f0f4a64f814d78fccd738a842bd5730.

--- lib/updater.php
+++ lib/updater.php
@@ -101,7 +101,7 @@ class OC_Updater extends BasicEmitter {
 		/*
 		 * START CONFIG CHANGES FOR OLDER VERSIONS
 		 */
-		if (version_compare($installedVersion, '5.00.32', '<')) {
+		if (version_compare($currentVersion, '5.00.29', '<')) {
 			// Add the overwriteHost config if it is not existant
 			// This is added to prevent host header poisoning
 			\OC_Config::setValue('trusted_domains', \OC_Config::getValue('trusted_domains', array(\OC_Request::serverHost()))); 
