ada8d4e0c91a885ca21db6b2bf4ca5d5c932e51b
Thomas Tanghus <thomas@tanghus.net>
2014-03-07 02:44:34 +0100

    Fix unit tests

--- tests/lib/urlgenerator.php
+++ tests/lib/urlgenerator.php
@@ -12,22 +12,46 @@ class Test_Urlgenerator extends PHPUnit_Framework_TestCase {
 	/**
 	 * @small
 	 * @brief test absolute URL construction
-	 * @dataProvider provideURLs
+	 * @dataProvider provideDocRootURLs
 	 */
-	function testGetAbsoluteURL($url, $expectedResult) {
+	function testGetAbsoluteURLDocRoot($url, $expectedResult) {
 
+		\OC::$WEBROOT = '';
 		$urlGenerator = new \OC\URLGenerator(null);
 		$result = $urlGenerator->getAbsoluteURL($url);
 
 		$this->assertEquals($expectedResult, $result);
 	}
 
-	public function provideURLs() {
+	/**
+	 * @small
+	 * @brief test absolute URL construction
+	 * @dataProvider provideSubDirURLs
+	 */
+	function testGetAbsoluteURLSubDir($url, $expectedResult) {
+
+		\OC::$WEBROOT = '/owncloud';
+		$urlGenerator = new \OC\URLGenerator(null);
+		$result = $urlGenerator->getAbsoluteURL($url);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	public function provideDocRootURLs() {
 		return array(
 			array("index.php", "http://localhost/index.php"),
 			array("/index.php", "http://localhost/index.php"),
 			array("/apps/index.php", "http://localhost/apps/index.php"),
 			array("apps/index.php", "http://localhost/apps/index.php"),
+			);
+	}
+
+	public function provideSubDirURLs() {
+		return array(
+			array("index.php", "http://localhost/owncloud/index.php"),
+			array("/index.php", "http://localhost/owncloud/index.php"),
+			array("/apps/index.php", "http://localhost/owncloud/apps/index.php"),
+			array("apps/index.php", "http://localhost/owncloud/apps/index.php"),
 			);
 	}
 }
