4a2b16d76dafbd83c3f8c137cba9edda375b206a
Robin Appelman <icewind@owncloud.com>
2014-03-10 14:40:05 +0100

    update copyright header

--- lib/public/route/irouter.php
+++ lib/public/route/irouter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2012 Bart Visscher <bartv@thisnet.nl>
+ * Copyright (c) 2014 Robin Appelman <icewind@owncloud.com>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
