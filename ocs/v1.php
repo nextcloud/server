743addd1e317c760e8b1e4ad7dc149476e4ad282
Thomas Müller <thomas.mueller@tmit.eu>
2014-03-12 00:35:19 +0100

    set content-type on ocs exceptions

--- ocs/v1.php
+++ ocs/v1.php
@@ -28,8 +28,11 @@ use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 try {
 	OC::getRouter()->match('/ocs'.OC_Request::getRawPathInfo());
 } catch (ResourceNotFoundException $e) {
+	OC_API::setContentType();
 	OC_OCS::notFound();
 } catch (MethodNotAllowedException $e) {
+	setOcsContentType();
+	OC_API::setContentType();
 	OC_Response::setStatus(405);
 }
 
