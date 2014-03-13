a8eb7a5092fad987b96f9dac947dc9bf4a2b0eaf
Robin Appelman <icewind@owncloud.com>
2014-03-13 13:33:09 +0100

    Allow setting the config dir to use as enviroment variable for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,14 @@
 
 define('PHPUNIT_RUN', 1);
 
-require_once __DIR__.'/../lib/base.php';
+$configDir = getenv('CONFIG_DIR');
+if ($configDir) {
+	define('PHPUNIT_CONFIG_DIR', $configDir);
+}
 
-if(!class_exists('PHPUnit_Framework_TestCase')) {
+require_once __DIR__ . '/../lib/base.php';
+
+if (!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 
