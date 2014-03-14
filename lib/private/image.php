f2b072bceb95eacef42adc27a6faff49e151b360
Georg Ehrke <developer@georgehrke.com>
2014-03-14 18:12:33 +0100

    remove image/pjpeg from OC_Image::_output

--- lib/private/image.php
+++ lib/private/image.php
@@ -209,7 +209,6 @@ class OC_Image {
 					$imageType = IMAGETYPE_GIF;
 					break;
 				case 'image/jpeg':
-				case 'image/pjpeg':
 					$imageType = IMAGETYPE_JPEG;
 					break;
 				case 'image/png':
