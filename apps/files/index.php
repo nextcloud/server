da5541ac025e5f5f9dbd7318b903266eedbed263
Robin Appelman <icewind@owncloud.com>
2014-04-02 17:10:57 +0200

    Allow reusing FileInfo for getStorageInfo

--- apps/files/index.php
+++ apps/files/index.php
@@ -38,7 +38,7 @@ OCP\App::setActiveNavigationEntry('files_index');
 // Load the files
 $dir = isset($_GET['dir']) ? stripslashes($_GET['dir']) : '';
 $dir = \OC\Files\Filesystem::normalizePath($dir);
-$dirInfo = \OC\Files\Filesystem::getFileInfo($dir);
+$dirInfo = \OC\Files\Filesystem::getFileInfo($dir, false);
 // Redirect if directory does not exist
 if (!$dirInfo || !$dirInfo->getType() === 'dir') {
 	header('Location: ' . OCP\Util::getScriptName() . '');
@@ -94,7 +94,7 @@ $breadcrumbNav->assign('baseURL', OCP\Util::linkTo('files', 'index.php') . '?dir
 $permissions = $dirInfo->getPermissions();
 
 // information about storage capacities
-$storageInfo=OC_Helper::getStorageInfo($dir);
+$storageInfo=OC_Helper::getStorageInfo($dir, $dirInfo);
 $freeSpace=$storageInfo['free'];
 $uploadLimit=OCP\Util::uploadLimit();
 $maxUploadFilesize=OCP\Util::maxUploadFilesize($dir);
