76cdd85cd990bd51de564512e2f5c8e6351dd660
Fabian Henze <flyser42@gmx.de>
2014-04-07 15:31:34 +0200

    Add unit tests for OC_Helper::phpFileSize function

--- tests/lib/helper.php
+++ tests/lib/helper.php
@@ -30,6 +30,28 @@ class Test_Helper extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
+	 * @dataProvider phpFileSizeProvider
+	 */
+	public function testPhpFileSize($expected, $input)
+	{
+		$result = OC_Helper::phpFileSize($input);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function phpFileSizeProvider()
+	{
+		return array(
+			array('0B', 0),
+			array('1K', 1024),
+			array('9.5M', 10000000),
+			array('1.3G', 1395864371),
+			array('465.7G', 500000000000),
+			array('465661.3G', 500000000000000),
+			array('465661287.3G', 500000000000000000),
+		);
+	}
+
+	/**
 	 * @dataProvider computerFileSizeProvider
 	 */
 	function testComputerFileSize($expected, $input) {
