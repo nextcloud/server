bfb1e0867f82a61a2e19e900d3e62d2bafa0b514
Vincent Petry <pvince81@owncloud.com>
2014-04-08 17:21:11 +0200

    Enable autowatch for karma
    This way unit tests can be run in multiple browsers with the command
    line and tests will re-run automatically when files are changed.

--- tests/karma.config.js
+++ tests/karma.config.js
@@ -167,7 +167,7 @@ module.exports = function(config) {
 		logLevel: config.LOG_INFO,
 
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+		autoWatch: true,
 
 		// Start these browsers, currently available:
 		// - Chrome
