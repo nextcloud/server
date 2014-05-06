7177d3a4963ab43a6b24e4f1676bff5d74fc0a52
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-05-06 12:07:53 +0200

    first step of infield label removal, fix login screen

--- lib/base.php
+++ lib/base.php
@@ -320,7 +320,6 @@ class OC {
 		OC_Util::addScript("jquery-migrate-1.2.1.min");
 		OC_Util::addScript("jquery-ui-1.10.0.custom");
 		OC_Util::addScript("jquery-showpassword");
-		OC_Util::addScript("jquery.infieldlabel");
 		OC_Util::addScript("jquery.placeholder");
 		OC_Util::addScript("jquery-tipsy");
 		OC_Util::addScript("compatibility");
