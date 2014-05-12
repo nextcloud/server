2a5e1a188508b66f8b4e363382c3f789c3adc1a4
Lukas Reschke <lukas@statuscode.ch>
2014-05-12 20:05:52 +0200

    Back to the future

--- core/command/resetpassword.php
+++ core/command/resetpassword.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2013 Christopher Schäpers <christopher@schaepers.it>
+ * Copyright (c) 2014 Christopher Schäpers <christopher@schaepers.it>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
