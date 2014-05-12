e9011a8c5525678c1574d59d243af17fca16e9ae
Thomas Müller <thomas.mueller@tmit.eu>
2014-05-12 13:55:23 +0200

    adding unit tests for encryption data migration

--- apps/files_encryption/appinfo/update.php
+++ apps/files_encryption/appinfo/update.php
@@ -1,19 +1,10 @@
 <?php
 
+use OCA\Files_Encryption\Migration;
+
 $installedVersion=OCP\Config::getAppValue('files_encryption', 'installed_version');
-// migrate settings from oc_encryption to oc_preferences
+
 if (version_compare($installedVersion, '0.6', '<')) {
-	$sql = 'SELECT * FROM `*PREFIX*encryption`';
-	$query = \OCP\DB::prepare($sql);
-	$result = $query->execute(array())->fetchAll();
-
-	foreach ($result as $row) {
-			\OC_Preferences::setValue($row['uid'], 'files_encryption', 'recovery_enabled', $row['recovery_enabled']);
-			\OC_Preferences::setValue($row['uid'], 'files_encryption', 'migration_status', $row['migration_status']);
-	}
-
-	$deleteOldTable = 'DROP TABLE `*PREFIX*encryption`';
-	$query = \OCP\DB::prepare($deleteOldTable);
-	$query->execute(array());
-
+	$m = new Migration();
+	$m->dropTableEncryption();
 }
