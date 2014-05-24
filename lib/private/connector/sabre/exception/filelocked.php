6b94732bdfdc59836968240e0f4682b964a5253a
ringmaster <epithet@gmail.com>
2014-05-24 09:18:24 -0400

    Respect locked files, surface correct exception.

---
+++ lib/private/connector/sabre/exception/filelocked.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ownCloud
+ *
+ * @author Owen Winkler
+ * @copyright 2013 Owen Winkler <owen@owncloud.com>
+ *
+ */
+
+class OC_Connector_Sabre_Exception_FileLocked extends Sabre_DAV_Exception {
+
+	public function __construct($message = "", $code = 0, Exception $previous = null) {
+		if($previous instanceof \OCP\Files\LockNotAcquiredException) {
+			$message = sprintf('Target file %s is locked by another process. %s', $previous->path, $previous->getMessage());
+		}
+		parent::__construct($message, $code, $previous);
+	}
+
+	/**
+	 * Returns the HTTP statuscode for this exception
+	 *
+	 * @return int
+	 */
+	public function getHTTPCode() {
+
+		return 503;
+	}
+}
