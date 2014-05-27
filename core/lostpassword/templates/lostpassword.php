46555b50a123ebd367e28f0a4e67034bd3b086b6
Volkan Gezer <volkangezer@gmail.com>
2014-05-27 02:11:58 +0200

    fix warning text and margin

--- core/lostpassword/templates/lostpassword.php
+++ core/lostpassword/templates/lostpassword.php
@@ -21,10 +21,10 @@ OCP\Util::addStyle('lostpassword', 'lostpassword');
 				<label for="user" class="infield"><?php print_unescaped($l->t( 'Username' )); ?></label>
 				<img class="svg" src="<?php print_unescaped(image_path('', 'actions/user.svg')); ?>" alt=""/>
 				<?php if ($_['isEncrypted']): ?>
-				<br /><br />
-				<?php print_unescaped($l->t("Your files are encrypted. If you haven't enabled the recovery key, there will be no way to get your data back after your password is reset. If you are not sure what to do, please contact your administrator before you continue. Do you really want to continue?")); ?><br />
+				<br />
+				<p class="warning"><?php print_unescaped($l->t("Your files are encrypted. If you haven't enabled the recovery key, there will be no way to get your data back after your password is reset. If you are not sure what to do, please contact your administrator before you continue. Do you really want to continue?")); ?><br />
 				<input type="checkbox" name="continue" value="Yes" />
-					<?php print_unescaped($l->t('Yes, I really want to reset my password now')); ?><br/><br/>
+					<?php print_unescaped($l->t('Yes, I really want to reset my password now')); ?></p>
 				<?php endif; ?>
 			</p>
 			<input type="submit" id="submit" value="<?php print_unescaped($l->t('Reset')); ?>" />
