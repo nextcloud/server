b713f15b99c2f80be74fd133a98f3003a27324e8
Volkan Gezer <volkangezer@gmail.com>
2014-05-29 01:28:59 +0200

    document always links to the current version of OC

--- apps/user_ldap/templates/part.wizardcontrols.php
+++ apps/user_ldap/templates/part.wizardcontrols.php
@@ -7,7 +7,7 @@
 	<button class="ldap_action_continue" name="ldap_action_continue" type="button">
 		<?php p($l->t('Continue'));?>
 	</button>
-	<a href="<?php p($theme->getDocBaseUrl()); ?>/server/7.0/go.php?to=admin-ldap"
+	<a href="<?php p(\OC_Helper::linkToDocs('admin-ldap')); ?>"
 		target="_blank">
 		<img src="<?php print_unescaped(OCP\Util::imagePath('', 'actions/info.png')); ?>"
 			style="height:1.75ex" />
