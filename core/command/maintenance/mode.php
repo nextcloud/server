43759f1f468fac8de4a8db0b8d94d004f6fb3745
scolebrook <scolebrook@mac.com>
2014-05-28 16:26:13 -0400

    Use OC\Config instead of OC_Config

--- core/command/maintenance/mode.php
+++ core/command/maintenance/mode.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright (c) 2013 Robin Appelman <icewind@owncloud.com>
- * and Stephen Colebrook <scolebrook@mac.com>
+ * Copyright (c) 2013 Robin Appelman <icewind@owncloud.com> and
+ * Copyright (c) 2014 Stephen Colebrook <scolebrook@mac.com>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
@@ -9,12 +9,21 @@
 
 namespace OC\Core\Command\Maintenance;
 
+use OC\Config;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Mode extends Command {
+
+	protected $config;
+
+	public function __construct(Config $config) {
+		$this->config = $config;
+		parent::__construct();
+	}
 
 	protected function configure() {
 		$this
@@ -36,13 +45,13 @@ class Mode extends Command {
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		if ($input->getOption('on')) {
-			\OC_Config::setValue('maintenance', true);
+			$this->config->setValue('maintenance', true);
 			$output->writeln('Maintenance mode enabled');
 		} elseif ($input->getOption('off')) {
-			\OC_Config::setValue('maintenance', false);
+			$this->config->setValue('maintenance', false);
 			$output->writeln('Maintenance mode disabled');
 		} else {
-			if (\OC_Config::getValue('maintenance', false)) {
+			if ($this->config->getValue('maintenance', false)) {
 				$output->writeln('Maintenance mode is currently enabled');
 			} else {
 				$output->writeln('Maintenance mode is currently disabled');
