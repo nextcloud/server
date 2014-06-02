2e6a27614e8da443bd6b29fb2a3f4a64b274c2a0
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-06-02 22:59:16 +0200

    move Help section from controls bar to sidebar

--- settings/templates/help.php
+++ settings/templates/help.php
@@ -1,21 +1,48 @@
-<div id="controls">
+<div id="app-navigation">
+	<ul>
 	<?php if($_['admin']) { ?>
-		<a class="button newquestion <?php p($_['style1']); ?>"
-			href="<?php print_unescaped($_['url1']); ?>"><?php p($l->t( 'User Documentation' )); ?></a>
-		<a class="button newquestion <?php p($_['style2']); ?>"
-			href="<?php print_unescaped($_['url2']); ?>"><?php p($l->t( 'Administrator Documentation' )); ?></a>
+		<li>
+			<a class="<?php p($_['style1']); ?>"
+				href="<?php print_unescaped($_['url1']); ?>">
+				<?php p($l->t( 'User Documentation' )); ?>
+			</a>
+		</li>
+		<li>
+			<a class="<?php p($_['style2']); ?>"
+				href="<?php print_unescaped($_['url2']); ?>">
+				<?php p($l->t( 'Administrator Documentation' )); ?>
+			</a>
+		</li>
 	<?php } ?>
-	<a class="button newquestion" href="http://owncloud.org/support" target="_blank"><?php
-		p($l->t( 'Online Documentation' )); ?></a>
-	<a class="button newquestion" href="http://forum.owncloud.org" target="_blank"><?php
-		p($l->t( 'Forum' )); ?></a>
+
+		<li>
+			<a href="http://owncloud.org/support" target="_blank">
+				<?php p($l->t( 'Online Documentation' )); ?>
+			</a>
+		</li>
+		<li>
+			<a href="https://forum.owncloud.org" target="_blank">
+				<?php p($l->t( 'Forum' )); ?>
+			</a>
+		</li>
+
 	<?php if($_['admin']) { ?>
-		<a class="button newquestion" href="https://github.com/owncloud/core/blob/master/CONTRIBUTING.md" target="_blank"><?php
-			p($l->t( 'Bugtracker' )); ?></a>
+		<li>
+			<a href="https://github.com/owncloud/core/blob/master/CONTRIBUTING.md"
+				target="_blank">
+				<?php p($l->t( 'Bugtracker' )); ?>
+			</a>
+		</li>
 	<?php } ?>
-	<a class="button newquestion" href="http://owncloud.com" target="_blank"><?php
-		p($l->t( 'Commercial Support' )); ?></a>
+
+	<li>
+		<a href="https://owncloud.com" target="_blank">
+			<?php p($l->t( 'Commercial Support' )); ?>
+		</a>
+	</li>
 </div>
-<div class="help-includes">
-	<iframe src="<?php print_unescaped($_['url']); ?>" class="help-iframe">abc</iframe>
+
+<div id="app-content" class="help-includes">
+	<iframe src="<?php print_unescaped($_['url']); ?>" class="help-iframe">
+	</iframe>
 </div>
