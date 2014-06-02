2bb902d02864aa73d0985537552df4b076f5860b
Thomas Müller <thomas.mueller@tmit.eu>
2014-06-02 18:57:53 +0200

    use OC_DB::dropTable()

--- apps/files_versions/appinfo/update.php
+++ apps/files_versions/appinfo/update.php
@@ -3,6 +3,5 @@
 $installedVersion=OCP\Config::getAppValue('files_versions', 'installed_version');
 // move versions to new directory
 if (version_compare($installedVersion, '1.0.4', '<')) {
-	$query = \OCP\DB::prepare("DROP TABLE `*PREFIX*files_versions`");
-	$query->execute(array());
+	\OC_DB::dropTable("files_versions");
 }
