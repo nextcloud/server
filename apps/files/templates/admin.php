7ad7f591624049e4c0020039e2831e5160203e38
Morris Jobke <morris.jobke@gmail.com>
2014-06-03 09:38:19 +0200

    fix empty file handling option

--- apps/files/templates/admin.php
+++ apps/files/templates/admin.php
@@ -1,16 +1,18 @@
-<?php OCP\Util::addscript('files', 'admin'); ?>
+<?php if($_['uploadChangable']):?>
 
-<form name="filesForm" class="section" action="#" method="post">
-	<h2><?php p($l->t('File handling')); ?></h2>
-	<?php if($_['uploadChangable']):?>
+	<?php OCP\Util::addscript('files', 'admin'); ?>
+
+	<form name="filesForm" class="section" action="#" method="post">
+		<h2><?php p($l->t('File handling')); ?></h2>
 		<label for="maxUploadSize"><?php p($l->t( 'Maximum upload size' )); ?> </label>
 		<input type="text" name='maxUploadSize' id="maxUploadSize" value='<?php p($_['uploadMaxFilesize']) ?>'/>
 		<?php if($_['displayMaxPossibleUploadSize']):?>
 			(<?php p($l->t('max. possible: ')); p($_['maxPossibleUploadSize']) ?>)
 		<?php endif;?>
 		<br/>
-	<?php endif;?>
-	<input type="hidden" value="<?php p($_['requesttoken']); ?>" name="requesttoken" />
-	<input type="submit" name="submitFilesAdminSettings" id="submitFilesAdminSettings"
-		   value="<?php p($l->t( 'Save' )); ?>"/>
-</form>
+		<input type="hidden" value="<?php p($_['requesttoken']); ?>" name="requesttoken" />
+		<input type="submit" name="submitFilesAdminSettings" id="submitFilesAdminSettings"
+			   value="<?php p($l->t( 'Save' )); ?>"/>
+	</form>
+
+<?php endif;?>
