844c6c33281cc7207dbb5e3196284db82c21bcbe
Bernhard Posselt <dev@bernhard-posselt.com>
2014-06-04 19:11:04 +0200

    use slide toggle for files app

--- apps/files/templates/appnavigation.php
+++ apps/files/templates/appnavigation.php
@@ -6,7 +6,7 @@
 	</ul>
 	<div id="app-settings">
 		<div id="app-settings-header">
-			<button class="settings-button"></button>
+			<button class="settings-button" data-apps-slide-toggle="#app-settings-content"></button>
 		</div>
 		<div id="app-settings-content">
 				<h2><?php p($l->t('WebDAV'));?></h2>
