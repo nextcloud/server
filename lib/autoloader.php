498aa6664807cca87a1ca8d2fd0d3d609430bae8
Georg Ehrke <developer@georgehrke.com>
2014-06-06 09:33:34 +0200

    add additional type check

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -90,7 +90,7 @@ class Autoloader {
 			list(, $app, $rest) = explode('\\', $class, 3);
 			$app = strtolower($app);
 			$appPath = \OC_App::getAppPath($app);
-			if (stream_resolve_include_path($appPath)) {
+			if ($appPath && stream_resolve_include_path($appPath)) {
 				$paths[] = $appPath . '/' . strtolower(str_replace('\\', '/', $rest) . '.php');
 				// If not found in the root of the app directory, insert '/lib' after app id and try again.
 				$paths[] = $appPath . '/lib/' . strtolower(str_replace('\\', '/', $rest) . '.php');
