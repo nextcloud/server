909e505c2e24cbc06ce19e9642ce141a45e4808c
Vincent Petry <pvince81@owncloud.com>
2014-06-23 15:57:45 +0200

    Remove warning when deleting all entries
    When deleting all entries, only "allfiles" is defined but not "file" or
    "files", which caused a PHP warning to be logged.

--- apps/files/ajax/delete.php
+++ apps/files/ajax/delete.php
@@ -7,10 +7,11 @@ OCP\JSON::callCheck();
 
 // Get data
 $dir = stripslashes($_POST["dir"]);
-$files = isset($_POST["file"]) ? $_POST["file"] : $_POST["files"];
 $allFiles = isset($_POST["allfiles"]) ? $_POST["allfiles"] : false;
 if ($allFiles === 'true') {
 	$allFiles = true;
+} else {
+	$files = isset($_POST["file"]) ? $_POST["file"] : $_POST["files"];
 }
 
 // delete all files in dir ?
