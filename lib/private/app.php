2547945779a9ef907cf1db6d95c345f0f6177788
Robin Appelman <icewind@owncloud.com>
2014-06-23 15:14:35 +0200

    Add comment

--- lib/private/app.php
+++ lib/private/app.php
@@ -195,6 +195,9 @@ class OC_App {
 		}
 		sort($apps);
 		array_unshift($apps, 'files');
+		// Only cache the app list, when the user is logged in.
+		// Otherwise we cache the list with disabled apps, although
+		// the apps are enabled for the user after he logged in.
 		if ($user) {
 			self::$enabledAppsCache = $apps;
 		}
