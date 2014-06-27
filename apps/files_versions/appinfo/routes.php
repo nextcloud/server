3a1c7182e678b6e42b132592b1470dfe3ef20464
Jörn Friedrich Dreyer <jfd@butonic.de>
2014-06-27 19:22:10 +0200

    change order of registering api and capabilities to fix file version previews

--- apps/files_versions/appinfo/routes.php
+++ apps/files_versions/appinfo/routes.php
@@ -5,11 +5,11 @@
  * See the COPYING-README file.
  */
 
-// Register with the capabilities API
-OC_API::register('get', '/cloud/capabilities', array('OCA\Files_Versions\Capabilities', 'getCapabilities'), 'files_versions', OC_API::USER_AUTH);
-
 /** @var $this \OCP\Route\IRouter */
 $this->create('core_ajax_versions_preview', '/preview')->action(
 function() {
 	require_once __DIR__ . '/../ajax/preview.php';
 });
+
+// Register with the capabilities API
+OC_API::register('get', '/cloud/capabilities', array('OCA\Files_Versions\Capabilities', 'getCapabilities'), 'files_versions', OC_API::USER_AUTH);
