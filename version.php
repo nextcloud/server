2e15c64825ce505b41f8e9af49311010db0a7bba
Frank Karlitschek <frank@owncloud.org>
2014-07-03 18:30:49 -0400

    master if 8.0 pre alpha now

--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version=array(7, 0, 0, 3);
+$OC_Version=array(7, 8, 0, 0);
 
 // The human readable string
-$OC_VersionString='7.0 beta 1';
+$OC_VersionString='8.0 pre alpha';
 
 // The ownCloud edition
 $OC_Edition='';
