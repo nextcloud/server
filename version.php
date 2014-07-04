f1b3e03f7fdb6272bbc4320d75b8bb0df4a05557
Morris Jobke <hey@morrisjobke.de>
2014-07-04 07:50:18 +0200

    fix version number - @karlitschek ;)

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version=array(7, 8, 0, 0);
+$OC_Version=array(8, 0, 0, 0);
 
 // The human readable string
 $OC_VersionString='8.0 pre alpha';
