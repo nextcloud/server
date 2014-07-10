b395ff549bf70ffb86b79e41ec92f1bf2f2c7960
libasys <sebastian.doell@libasys.de>
2014-07-10 13:02:04 +0200

    Update share.php
    Added isset for error:
    ```
    Undefined index: file_target at /var/www/owncloud/lib/private/share/share.php#1911
    ```

--- lib/private/share/share.php
+++ lib/private/share/share.php
@@ -1908,7 +1908,7 @@ class Share extends \OC\Share\Constants {
 				} else if (!isset($statuses[$item[$column]])) {
 					$statuses[$item[$column]]['link'] = false;
 				}
-				if ($item['file_target']) {
+				if (isset($item['file_target'])) {
 					$statuses[$item[$column]]['path'] = $item['path'];
 				}
 			}
