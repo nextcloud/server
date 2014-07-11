5d7deefd95c3108ff4deb17d23e78b39dbe80ee3
Stephan Peijnik <speijnik@anexia-it.com>
2014-07-11 11:21:32 +0200

    Use tabs for indentation.
    Signed-off-by: Stephan Peijnik <speijnik@anexia-it.com>

--- lib/private/files/filesystem.php
+++ lib/private/files/filesystem.php
@@ -33,9 +33,9 @@ namespace OC\Files;
 use OC\Files\Storage\Loader;
 
 class Filesystem {
-    const SPACE_NOT_COMPUTED = -1;
-    const SPACE_UNKNOWN = -2;
-    const SPACE_UNLIMITED = -3;
+	const SPACE_NOT_COMPUTED = -1;
+	const SPACE_UNKNOWN = -2;
+	const SPACE_UNLIMITED = -3;
 
 	/**
 	 * @var Mount\Manager $mounts
