75236b0ee6cde8daa538eae5623077c838e6be02
Robin Appelman <icewind@owncloud.com>
2014-07-25 13:40:51 +0200

    Set maintaince mode when updating an app from the app store

--- settings/ajax/updateapp.php
+++ settings/ajax/updateapp.php
@@ -33,7 +33,10 @@ if (!is_numeric($appId)) {
 
 $appId = OC_App::cleanAppId($appId);
 
+\OC_Config::setValue('maintenance', true);
 $result = OC_Installer::updateAppByOCSId($appId, $isShipped);
+\OC_Config::setValue('maintenance', false);
+
 if($result !== false) {
 	OC_JSON::success(array('data' => array('appid' => $appId)));
 } else {
