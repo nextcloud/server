727374a6f3e693b3237d6766d3bce86d73c00833
Frank Karlitschek <frank@owncloud.org>
2014-07-30 17:39:38 -0400

    7.0.1 RC1

--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version=array(7, 0, 0, 8);
+$OC_Version=array(7, 0, 1, 0);
 
 // The human readable string
-$OC_VersionString='7.0.0';
+$OC_VersionString='7.0.1 RC1';
 
 // The ownCloud edition
 $OC_Edition='';
