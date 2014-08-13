e2f23b24f7d1213bd37b75d9386cea6f2a0a264a
Robin Appelman <icewind@owncloud.com>
2014-08-13 18:06:28 +0200

    Cast file id's to int so we can compare them properly

--- lib/private/files/view.php
+++ lib/private/files/view.php
@@ -1179,6 +1179,7 @@ class View {
 	 * @return string|null
 	 */
 	public function getPath($id) {
+		$id = (int) $id;
 		$manager = Filesystem::getMountManager();
 		$mounts = $manager->findIn($this->fakeRoot);
 		$mounts[] = $manager->find($this->fakeRoot);
