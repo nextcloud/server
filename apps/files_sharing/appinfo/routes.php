ad202250a8f30486b0694f9d2660d33b2453521c
Lukas Reschke <lukas@owncloud.com>
2014-08-19 15:22:54 +0200

    Add routing to files_sharing

--- apps/files_sharing/appinfo/routes.php
+++ apps/files_sharing/appinfo/routes.php
@@ -5,10 +5,14 @@ $this->create('core_ajax_public_preview', '/publicpreview')->action(
 		require_once __DIR__ . '/../ajax/publicpreview.php';
 	});
 
-$this->create('sharing_external_shareinfo', '/shareinfo')->actionInclude('files_sharing/ajax/shareinfo.php');
-$this->create('sharing_external_add', '/external')->actionInclude('files_sharing/ajax/external.php');
-$this->create('sharing_external_test_remote', '/testremote')->actionInclude('files_sharing/ajax/testremote.php');
-
+$this->create('files_sharing_ajax_list', 'ajax/list.php')
+	->actionInclude('files_sharing/ajax/list.php');
+$this->create('sharing_external_shareinfo', '/shareinfo')
+	->actionInclude('files_sharing/ajax/shareinfo.php');
+$this->create('sharing_external_add', '/external')
+	->actionInclude('files_sharing/ajax/external.php');
+$this->create('sharing_external_test_remote', '/testremote')
+	->actionInclude('files_sharing/ajax/testremote.php');
 // OCS API
 
 //TODO: SET: mail notification, waiting for PR #4689 to be accepted
