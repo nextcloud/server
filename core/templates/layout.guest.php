73f50287ff0965c1be6a520c92c9f0d297ec5685
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2014-08-28 10:12:59 +0200

    templates: use p() for getiTunesAppId

--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -12,7 +12,7 @@
 		</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
-		<meta name="apple-itunes-app" content="app-id=<?php print_unescaped($theme->getiTunesAppId()); ?>">
+		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
 		<link rel="shortcut icon" type="image/png" href="<?php print_unescaped(image_path('', 'favicon.png')); ?>" />
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>" />
 		<?php foreach($_['cssfiles'] as $cssfile): ?>
