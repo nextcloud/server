96a931929ea837a40a7e9b836252587c949a8127
Thomas Müller <thomas.mueller@tmit.eu>
2014-08-29 12:09:33 +0200

    adding OC-ETag header

--- lib/private/connector/sabre/file.php
+++ lib/private/connector/sabre/file.php
@@ -181,21 +181,6 @@ class OC_Connector_Sabre_File extends OC_Connector_Sabre_Node implements \Sabre\
 	}
 
 	/**
-	 * Returns the ETag for a file
-	 *
-	 * An ETag is a unique identifier representing the current version of the
-	 * file. If the file changes, the ETag MUST change.  The ETag is an
-	 * arbitrary string, but MUST be surrounded by double-quotes.
-	 *
-	 * Return null if the ETag can not effectively be determined
-	 *
-	 * @return mixed
-	 */
-	public function getETag() {
-		return '"' . $this->info->getEtag() . '"';
-	}
-
-	/**
 	 * Returns the mime-type for a file
 	 *
 	 * If null is returned, we'll assume application/octet-stream
