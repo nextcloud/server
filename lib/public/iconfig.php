0a1e5aebf1198cc155862fad8c890121f5f379e9
Robin Appelman <icewind@owncloud.com>
2014-08-31 15:27:36 +0200

    Extend public config interface

--- lib/public/iconfig.php
+++ lib/public/iconfig.php
@@ -51,6 +51,13 @@ interface IConfig {
 	 */
 	public function getSystemValue($key, $default = '');
 
+	/**
+	 * Delete a system wide defined value
+	 *
+	 * @param string $key the key of the value, under which it was saved
+	 */
+	public function deleteSystemValue($key);
+
 
 	/**
 	 * Writes a new app wide value
@@ -71,6 +78,14 @@ interface IConfig {
 	 * @return string the saved value
 	 */
 	public function getAppValue($appName, $key, $default = '');
+
+	/**
+	 * Delete an app wide defined value
+	 *
+	 * @param string $appName the appName that we stored the value under
+	 * @param string $key the key of the value, under which it was saved
+	 */
+	public function deleteAppValue($appName, $key);
 
 
 	/**
@@ -94,4 +109,22 @@ interface IConfig {
 	 * @return string
 	 */
 	public function getUserValue($userId, $appName, $key, $default = '');
+
+	/**
+	 * Get the keys of all stored by an app for the user
+	 *
+	 * @param string $userId the userId of the user that we want to store the value under
+	 * @param string $appName the appName that we stored the value under
+	 * @return string[]
+	 */
+	public function getUserKeys($userId, $appName);
+
+	/**
+	 * Delete a user value
+	 *
+	 * @param string $userId the userId of the user that we want to store the value under
+	 * @param string $appName the appName that we stored the value under
+	 * @param string $key the key under which the value is being stored
+	 */
+	public function deleteUserValue($userId, $appName, $key);
 }
