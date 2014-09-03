eaab067716ed7f502bb267cad9704b0597669010
Olivier Paroz <oparoz@users.noreply.github.com>
2014-09-03 16:08:40 +0200

    env:UserInstallation was already there in master
    My changes were made against stable7 and I merged them to the file in master, so this version is now the properly merged file for master.
    I'm just adding `owncloud-<instanceid>` to the path where temp files are stored.

--- lib/private/preview/office-cl.php
+++ lib/private/preview/office-cl.php
@@ -29,10 +29,10 @@ if (!\OC_Util::runningOnWindows()) {
 
 			$tmpDir = get_temp_dir();
 
-			$defaultParameters = ' -env:UserInstallation=file://' . escapeshellarg($tmpDir) . ' --headless --nologo --nofirststartwizard --invisible --norestore -convert-to pdf -outdir ';
+			$defaultParameters = ' -env:UserInstallation=file://' . escapeshellarg($tmpDir . '/owncloud-' . \OC_Util::getInstanceId().'/') . ' --headless --nologo --nofirststartwizard --invisible --norestore -convert-to pdf -outdir ';
 			$clParameters = \OCP\Config::getSystemValue('preview_office_cl_parameters', $defaultParameters);
 
-			$exec = $this->cmd . $clParameters . escapeshellarg($tmpDir) . ' ' . escapeshellarg($absPath) . ' -env:UserInstallation=file://' . escapeshellarg(get_temp_dir() . '/owncloud-' . \OC_Util::getInstanceId().'/');
+			$exec = $this->cmd . $clParameters . escapeshellarg($tmpDir) . ' ' . escapeshellarg($absPath);
 
 			shell_exec($exec);
 
