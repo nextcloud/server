c04346918bb697b5aaa9d154845056abb457c385
Volkan Gezer <volkangezer@gmail.com>
2014-09-13 01:48:18 +0200

    replace ownCloud with placeholder

--- core/templates/update.user.php
+++ core/templates/update.user.php
@@ -1,7 +1,7 @@
 <ul>
 	<li class='update'>
-		<?php p($l->t('This ownCloud instance is currently being updated, which may take a while.')) ?><br/><br/>
-		<?php p($l->t('This page will refresh itself when the ownCloud instance is available again.')) ?><br/><br/>
+		<?php p($l->t('This %s instance is currently being updated, which may take a while.', array($theme->getName()))) ?><br/><br/>
+		<?php p($l->t('This page will refresh itself when the %s instance is available again.', array($theme->getName()))) ?><br/><br/>
 		<?php p($l->t('Contact your system administrator if this message persists or appeared unexpectedly.')) ?><br/><br/>
 		<?php p($l->t('Thank you for your patience.')); ?><br/><br/>
 	</li>
