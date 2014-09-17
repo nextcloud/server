a280859bf888b0573e24f47b335ba3d65b9212ae
Bjoern Schiessle <schiessle@owncloud.com>
2014-09-17 10:40:41 +0200

    make sure that we really catch the files folder only

--- apps/files_encryption/lib/proxy.php
+++ apps/files_encryption/lib/proxy.php
@@ -54,7 +54,7 @@ class Proxy extends \OC_FileProxy {
 		$view = new \OC\Files\View();
 
 		// files outside of the files-folder are excluded
-		if(strpos($path, '/' . $uid . '/files') !== 0) {
+		if(strpos($path, '/' . $uid . '/files/') !== 0) {
 			return true;
 		}
 
