33c0d2f743c82facc7847b62497913952baef296
Lukas Reschke <lukas@owncloud.com>
2014-09-17 11:36:08 +0200

    Fix mapping of relative paths

--- lib/private/files/storage/common.php
+++ lib/private/files/storage/common.php
@@ -99,7 +99,7 @@ abstract class Common implements \OC\Files\Storage\Storage {
 			return false;
 		}
 		$parent = dirname($path);
-		return $this->isUpdatable($parent) and $this->isUpdatable($path);
+		return $this->isUpdatable($parent) && $this->isUpdatable($path);
 	}
 
 	public function isSharable($path) {
