7f45226b8e091058388c30d51c2033c2a55d8c56
Lukas Reschke <lukas@owncloud.com>
2014-09-18 17:45:30 +0200

    Prevent updates between multiple major versions
    Ref https://github.com/owncloud/core/issues/11078

---
+++ tests/lib/updater.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright (c) 2014 Lukas Reschke <lukas@owncloud.com>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+namespace OC;
+
+class UpdaterTest extends \PHPUnit_Framework_TestCase {
+
+	public function testVersionCompatbility() {
+		return array(
+			array('1.0.0.0', '2.2.0', true),
+			array('1.1.1.1', '2.0.0', true),
+			array('5.0.3', '4.0.3', false),
+			array('12.0.3', '13.4.5', true),
+			array('1', '2', true),
+			array('2', '2', true),
+			array('6.0.5', '6.0.6', true),
+			array('5.0.6', '7.0.4', false)
+		);
+	}
+
+	/**
+	 * @dataProvider testVersionCompatbility
+	 */
+	function testIsUpgradePossible($oldVersion, $newVersion, $result) {
+		$updater = new Updater();
+		$this->assertSame($result, $updater->isUpgradePossible($oldVersion, $newVersion));
+	}
+
+}
