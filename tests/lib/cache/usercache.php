2f1270278dd4a33ae1d3985774ea864302937898
Clark Tomlinson <fallen013@gmail.com>
2014-09-22 13:38:46 -0400

    fixing cache routes
    Conflicts:
    	tests/lib/cache/file.php
    	tests/lib/cache/usercache.php

--- tests/lib/cache/usercache.php
+++ tests/lib/cache/usercache.php
@@ -47,8 +47,8 @@ class UserCache extends \Test_Cache {
 		$storage = new \OC\Files\Storage\Temporary(array());
 		\OC\Files\Filesystem::mount($storage,array(),'/');
 		$datadir = str_replace('local::', '', $storage->getId());
-		$this->datadir = \OC_Config::getValue('datadirectory', \OC::$SERVERROOT.'/data');
-		\OC_Config::setValue('datadirectory', $datadir);
+		$this->datadir = \OC_Config::getValue('cachedirectory', \OC::$SERVERROOT.'/data/cache');
+		\OC_Config::setValue('cachedirectory', $datadir);
 
 		\OC_User::clearBackends();
 		\OC_User::useBackend(new \OC_User_Dummy());
@@ -68,7 +68,7 @@ class UserCache extends \Test_Cache {
 
 	public function tearDown() {
 		\OC_User::setUserId($this->user);
-		\OC_Config::setValue('datadirectory', $this->datadir);
+		\OC_Config::setValue('cachedirectory', $this->datadir);
 
 		// Restore the original mount point
 		\OC\Files\Filesystem::clearMounts();
