e37f44e56a9e18b794242b9d855de80c2b48a6c3
Robin Appelman <icewind@owncloud.com>
2014-09-24 17:49:52 +0200

    Expose creatable permissions trough fileinfo

--- lib/public/files/fileinfo.php
+++ lib/public/files/fileinfo.php
@@ -123,6 +123,13 @@ interface FileInfo {
 	public function isUpdateable();
 
 	/**
+	 * Check whether new files or folders can be created inside this folder
+	 *
+	 * @return bool
+	 */
+	public function isCreatable();
+
+	/**
 	 * Check if a file or folder can be deleted
 	 *
 	 * @return bool
