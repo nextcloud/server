c38454a5b243a1c135250326d76867284aec6328
Vincent Petry <pvince81@owncloud.com>
2014-10-13 17:30:25 +0200

    Revert "Set overwrite.cli.url configs on setup"
    This reverts commit 48a1e69f5e803d61edcd7d083fd8e794f5bf194d.
    
    This was backported too quickly.

--- lib/private/urlgenerator.php
+++ lib/private/urlgenerator.php
@@ -147,14 +147,13 @@ class URLGenerator implements IURLGenerator {
 	 * @return string the absolute version of the url
 	 */
 	public function getAbsoluteURL($url) {
-		$url = ($url[0] !== '/') ? '/' . $url : $url;
-
-		if (\OC::$CLI) {
-			return $this->config->getSystemValue('overwrite.cli.url') . $url;
-		}
+		$separator = $url[0] === '/' ? '' : '/';
 
 		// The ownCloud web root can already be prepended.
-		$webRoot = (strpos($url, \OC::$WEBROOT) === 0) ? '' : \OC::$WEBROOT;
-		return \OC_Request::serverProtocol() . '://' . \OC_Request::serverHost() . $webRoot . $url;
+		$webRoot = substr($url, 0, strlen(\OC::$WEBROOT)) === \OC::$WEBROOT
+			? ''
+			: \OC::$WEBROOT;
+
+		return \OC_Request::serverProtocol() . '://' . \OC_Request::serverHost(). $webRoot . $separator . $url;
 	}
 }
