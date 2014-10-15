6ec9c99d48571103eafb0997cd3434baa421ccbf
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-10-15 14:16:17 +0200

    Use this.$el instead of the absolute selector

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -51,7 +51,7 @@
 
 		// number of files per page, calculated dynamically
 		pageSize: function() {
-			return Math.ceil($('#app-content').height() / 50);
+			return Math.ceil(this.$el.height() / 50);
 		},
 
 		/**
