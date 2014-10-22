299ff5a3b121bff8f4c72c5f0b1eb4e0831aed10
Robin Appelman <icewind@owncloud.com>
2014-10-22 17:33:36 +0200

    Add \OC\TempManager to handle creating and cleaning temporary files

--- lib/public/iservercontainer.php
+++ lib/public/iservercontainer.php
@@ -174,4 +174,11 @@ interface IServerContainer {
 	 * @return \OC\HTTPHelper
 	 */
 	function getHTTPHelper();
+
+	/**
+	 * Get the manager for temporary files and folders
+	 *
+	 * @return \OCP\ITempManager
+	 */
+	function getTempManager();
 }
