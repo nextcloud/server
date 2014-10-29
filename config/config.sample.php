e8703f648d70bbe4d5232b9a14e2a1d45bbf2314
Morris Jobke <hey@morrisjobke.de>
2014-10-29 20:38:46 +0100

    fix style of generated documentation

--- config/config.sample.php
+++ config/config.sample.php
@@ -808,7 +808,8 @@ $CONFIG = array(
 ),
 
 /**
- * Database types that are supported for installation
+ * Database types that are supported for installation.
+ *
  * Available:
  * 	- sqlite (SQLite3)
  * 	- mysql (MySQL)
