be5ae6c44f8fae9364c0be65f4f685d7c1afa104
Lukas Reschke <lukas@owncloud.com>
2014-11-03 21:13:07 +0100

    Support HTML in logo claim

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -49,7 +49,7 @@
 						if(OC_Util::getEditionString() === '') {
 							p(!empty($_['application'])?$_['application']: $l->t('Apps'));
 						} else {
-							p($theme->getName());
+							print_unescaped($theme->getHTMLName());
 						}
 					?>
 				</div>
