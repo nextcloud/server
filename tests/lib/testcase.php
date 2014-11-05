9384fa65b5651ca032816579ec39b9e7e3610222
Joas Schilling <nickvergessen@gmx.de>
2014-11-05 13:20:43 +0100

    Testcase base class

---
+++ tests/lib/testcase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ownCloud
+ *
+ * @author Joas Schilling
+ * @copyright 2014 Joas Schilling nickvergessen@owncloud.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Test;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase {
+	protected function getUniqueID($prefix = '', $length = 13) {
+		// Do not use dots and slashes as we use the value for file names
+		return $prefix . \OC::$server->getSecureRandom()->getLowStrengthGenerator()->generate(
+			$length,
+			'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+		);
+	}
+}
