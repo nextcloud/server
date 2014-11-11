c5c74792d1314f32d4513841fafbfeea4ed9658c
Arthur Schiwon <blizzz@owncloud.com>
2014-11-11 18:26:42 +0100

    add 'namespace' for automatically created navigation divs, fixes #12080

--- settings/admin.php
+++ settings/admin.php
@@ -92,7 +92,7 @@ $formsMap = array_map(function ($form) {
 		$anchor = str_replace(' ', '-', $anchor);
 
 		return array(
-			'anchor' => $anchor,
+			'anchor' => 'goto-' . $anchor,
 			'section-name' => $sectionName,
 			'form' => $form
 		);
