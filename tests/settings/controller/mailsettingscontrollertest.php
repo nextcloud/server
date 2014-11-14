3fa892aecce983ca31d5e43caa2c380b13980f49
Lukas Reschke <lukas@owncloud.com>
2014-11-14 13:42:13 +0100

    Fail...

--- tests/settings/controller/mailsettingscontrollertest.php
+++ tests/settings/controller/mailsettingscontrollertest.php
@@ -151,7 +151,7 @@ class MailSettingscontrollerTest extends \PHPUnit_Framework_TestCase {
 		 * FIXME: Disabled due to missing DI on mail class. 
 		 * TODO: Re-enable when https://github.com/owncloud/core/pull/12085 is merged.
 		 */
-		$this->markSkipped();
+		$this->markTestSkipped('Disable test until OC_Mail is rewritten.');
 		
 		$user = $this->getMockBuilder('\OC\User\User')
 			->disableOriginalConstructor()
