61641293f47914413179b60c8124e5e966c772bb
Lukas Reschke <lukas@owncloud.com>
2014-11-18 23:06:39 +0100

    Only show undelete capability if files_trashbin is enabled
    Fixes the OCS capability API at /ocs/v1.php/cloud/capabilities

--- apps/files_trashbin/lib/capabilities.php
+++ apps/files_trashbin/lib/capabilities.php
@@ -1,20 +1,29 @@
 <?php
 /**
- * Copyright (c) 2013 Tom Needham <tom@owncloud.com>
+ * Copyright (c) Lukas Reschke <lukas@owncloud.com>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
  */
  
-namespace OCA\Files; 
+namespace OCA\Files_Trashbin;
 
+
+/**
+ * Class Capabilities
+ *
+ * @package OCA\Files_Trashbin
+ */
 class Capabilities {
-	
+
+	/**
+	 * @return \OC_OCS_Result
+	 */
 	public static function getCapabilities() {
 		return new \OC_OCS_Result(array(
 			'capabilities' => array(
 				'files' => array(
-					'bigfilechunking' => true,
+					'undelete' => true,
 					),
 				),
 			));
