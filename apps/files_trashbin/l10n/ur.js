995fe4a176e45e57cf49e9c47802c23764c4f064
Volkan Gezer <volkangezer@gmail.com>
2014-11-20 14:46:17 +0100

    cleanup languages. closes #11274

--- apps/files_trashbin/l10n/ur.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "files_trashbin",
-    {
-    "Error" : "خرابی"
-},
-"nplurals=2; plural=(n != 1);");
--- apps/user_webdavauth/l10n/sk.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "user_webdavauth",
-    {
-    "Save" : "Uložiť"
-},
-"nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
