a109f94feec821cad92b25a63c767b0a21f140c0
Joas Schilling <nickvergessen@gmx.de>
2014-11-25 18:30:25 +0100

    Depcrate namespaced functions, since they can not be autoloaded

--- lib/public/template.php
+++ lib/public/template.php
@@ -38,6 +38,7 @@ namespace OCP;
  * @return string to the image
  *
  * @see OC_Helper::imagePath
+ * @deprecated Use \OCP\Template::image_path() instead
  */
 function image_path( $app, $image ) {
 	return(\image_path( $app, $image ));
@@ -48,6 +49,7 @@ function image_path( $app, $image ) {
  * Make OC_Helper::mimetypeIcon available as a simple function
  * @param string $mimetype
  * @return string to the image of this file type.
+ * @deprecated Use \OCP\Template::mimetype_icon() instead
  */
 function mimetype_icon( $mimetype ) {
 	return(\mimetype_icon( $mimetype ));
@@ -57,6 +59,7 @@ function mimetype_icon( $mimetype ) {
  * Make preview_icon available as a simple function
  * @param string $path path to file
  * @return string to the preview of the image
+ * @deprecated Use \OCP\Template::preview_icon() instead
  */
 function preview_icon( $path ) {
 	return(\preview_icon( $path ));
@@ -68,6 +71,7 @@ function preview_icon( $path ) {
  * @param string $path of file
  * @param string $token
  * @return string link to the preview
+ * @deprecated Use \OCP\Template::publicPreview_icon() instead
  */
 function publicPreview_icon ( $path, $token ) {
 	return(\publicPreview_icon( $path, $token ));
@@ -76,8 +80,9 @@ function publicPreview_icon ( $path, $token ) {
 /**
  * Make OC_Helper::humanFileSize available as a simple function
  * Example: 2048 to 2 kB.
- * @param int $size in bytes
+ * @param int $bytes in bytes
  * @return string size as string
+ * @deprecated Use \OCP\Template::human_file_size() instead
  */
 function human_file_size( $bytes ) {
 	return(\human_file_size( $bytes ));
@@ -89,6 +94,7 @@ function human_file_size( $bytes ) {
  * @param int $timestamp unix timestamp
  * @param boolean $dateOnly
  * @return \OC_L10N_String human readable interpretation of the timestamp
+ * @deprecated Use \OCP\Template::relative_modified_date() instead
  */
 function relative_modified_date( $timestamp, $dateOnly = false ) {
 	return(\relative_modified_date($timestamp, null, $dateOnly));
@@ -97,9 +103,9 @@ function relative_modified_date( $timestamp, $dateOnly = false ) {
 
 /**
  * Return a human readable outout for a file size.
- * @deprecated use human_file_size() instead
  * @param integer $bytes size of a file in byte
  * @return string human readable interpretation of a file size
+ * @deprecated Use \OCP\Template::human_file_size() instead
  */
 function simple_file_size($bytes) {
 	return(\human_file_size($bytes));
@@ -112,6 +118,7 @@ function simple_file_size($bytes) {
  * @param mixed $selected which one is selected?
  * @param array $params the parameters
  * @return string html options
+ * @deprecated Use \OCP\Template::html_select_options() instead
  */
 function html_select_options($options, $selected, $params=array()) {
 	return(\html_select_options($options, $selected, $params));
@@ -123,5 +130,83 @@ function html_select_options($options, $selected, $params=array()) {
  * specific templates, add data and generate the html code
  */
 class Template extends \OC_Template {
+	/**
+	 * Make OC_Helper::imagePath available as a simple function
+	 *
+	 * @see OC_Helper::imagePath
+	 *
+	 * @param string $app
+	 * @param string $image
+	 * @return string to the image
+	 */
+	public static function image_path($app, $image) {
+		return \image_path($app, $image);
+	}
 
+
+	/**
+	 * Make OC_Helper::mimetypeIcon available as a simple function
+	 *
+	 * @param string $mimetype
+	 * @return string to the image of this file type.
+	 */
+	public static function mimetype_icon($mimetype) {
+		return \mimetype_icon($mimetype);
+	}
+
+	/**
+	 * Make preview_icon available as a simple function
+	 *
+	 * @param string $path path to file
+	 * @return string to the preview of the image
+	 */
+	public static function preview_icon($path) {
+		return \preview_icon($path);
+	}
+
+	/**
+	 * Make publicpreview_icon available as a simple function
+	 * Returns the path to the preview of the image.
+	 *
+	 * @param string $path of file
+	 * @param string $token
+	 * @return string link to the preview
+	 */
+	public static function publicPreview_icon($path, $token) {
+		return \publicPreview_icon($path, $token);
+	}
+
+	/**
+	 * Make OC_Helper::humanFileSize available as a simple function
+	 * Example: 2048 to 2 kB.
+	 *
+	 * @param int $bytes in bytes
+	 * @return string size as string
+	 */
+	public static function human_file_size($bytes) {
+		return \human_file_size($bytes);
+	}
+
+	/**
+	 * Return the relative date in relation to today. Returns something like "last hour" or "two month ago"
+	 *
+	 * @param int $timestamp unix timestamp
+	 * @param boolean $dateOnly
+	 * @return \OC_L10N_String human readable interpretation of the timestamp
+	 */
+	public static function relative_modified_date($timestamp, $dateOnly = false) {
+		return \relative_modified_date($timestamp, null, $dateOnly);
+	}
+
+	/**
+	 * Generate html code for an options block.
+	 *
+	 * @param array $options the options
+	 * @param mixed $selected which one is selected?
+	 * @param array $params the parameters
+	 * @return string html options
+	 */
+	public static function html_select_options($options, $selected, $params=array()) {
+		return \html_select_options($options, $selected, $params);
+	}
 }
