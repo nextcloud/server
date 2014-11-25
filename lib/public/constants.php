711912a7b3f48065b220a909e8889aba5a93d105
Joas Schilling <nickvergessen@gmx.de>
2014-11-25 16:27:27 +0100

    Move namespaced constants to namespaced class

--- lib/public/constants.php
+++ lib/public/constants.php
@@ -26,15 +26,37 @@
 
 namespace OCP;
 
-/**
- * CRUDS permissions.
- */
+/** @deprecated Use \OCP\Constants::PERMISSION_CREATE instead */
 const PERMISSION_CREATE = 4;
+
+/** @deprecated Use \OCP\Constants::PERMISSION_READ instead */
 const PERMISSION_READ = 1;
+
+/** @deprecated Use \OCP\Constants::PERMISSION_UPDATE instead */
 const PERMISSION_UPDATE = 2;
+
+/** @deprecated Use \OCP\Constants::PERMISSION_DELETE instead */
 const PERMISSION_DELETE = 8;
+
+/** @deprecated Use \OCP\Constants::PERMISSION_SHARE instead */
 const PERMISSION_SHARE = 16;
+
+/** @deprecated Use \OCP\Constants::PERMISSION_ALL instead */
 const PERMISSION_ALL = 31;
 
+/** @deprecated Use \OCP\Constants::FILENAME_INVALID_CHARS instead */
 const FILENAME_INVALID_CHARS = "\\/<>:\"|?*\n";
 
+class Constants {
+	/**
+	 * CRUDS permissions.
+	 */
+	const PERMISSION_CREATE = 4;
+	const PERMISSION_READ = 1;
+	const PERMISSION_UPDATE = 2;
+	const PERMISSION_DELETE = 8;
+	const PERMISSION_SHARE = 16;
+	const PERMISSION_ALL = 31;
+
+	const FILENAME_INVALID_CHARS = "\\/<>:\"|?*\n";
+}
