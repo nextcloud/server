96b0328d39f857683b601946c2108ce5b71e5012
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-28 01:55:29 -0500

    [tx-robot] updated from transifex

--- apps/files/l10n/af_ZA.js
+++ apps/files/l10n/af_ZA.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Share" : "Deel",
     "Unshare" : "Deel terug neem",
+    "Error" : "Fout",
     "_%n folder_::_%n folders_" : ["",""],
     "_%n file_::_%n files_" : ["",""],
     "_Uploading %n file_::_Uploading %n files_" : ["",""],
--- apps/user_ldap/l10n/af_ZA.js
+++ apps/user_ldap/l10n/af_ZA.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "user_ldap",
     {
+    "Error" : "Fout",
     "_%s group found_::_%s groups found_" : ["",""],
     "_%s user found_::_%s users found_" : ["",""],
     "Help" : "Hulp",
