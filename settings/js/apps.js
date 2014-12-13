ysaimanojkumar/owncloud-core

fix broken link on app management page for apps without ocsid - fix #9574, fix #10461

settings/js/apps.js
@@ -104,7 +104,12 @@ OC.Settings.Apps = OC.Settings.Apps || {
 		if (app.internal === false) {
 			page.find('span.score').show();
 			page.find('p.appstore').show();
-			page.find('a#appstorelink').attr('href', 'http://apps.owncloud.com/content/show.php?content=' + app.id);
+			var link = page.find('a#appstorelink');
+			if(app.ocsid) {
+				link.attr('href', 'http://apps.owncloud.com/content/show.php?content=' + app.ocsid);
+			} else {
+				link.hide();
+			}
 			page.find('small.externalapp').hide();
 		} else {
 			page.find('p.appslink').hide();

nextcloud/server

settings/js/apps.js
@@ -104,7 +104,12 @@ OC.Settings.Apps = OC.Settings.Apps || {
 		if (app.internal === false) {
 			page.find('span.score').show();
 			page.find('p.appstore').show();
-			page.find('a#appstorelink').attr('href', 'http://apps.owncloud.com/content/show.php?content=' + app.id);
+			var link = page.find('a#appstorelink');
+			if(app.ocsid) {
+				link.attr('href', 'https://apps.owncloud.com/content/show.php?content=' + app.ocsid);
+			} else {
+				link.hide();
+			}
 			page.find('small.externalapp').hide();
 		} else {
 			page.find('p.appslink').hide();