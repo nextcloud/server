80ded08ec46d56476f20f3e71d5bb115e070f228
Jörn Friedrich Dreyer <jfd@butonic.de>
2014-12-19 00:49:05 +0100

    hide irrelevant stuff in empty filelist

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -1669,7 +1669,7 @@
 		setFilter:function(filter) {
 			this._filter = filter;
 			this.fileSummary.setFilter(filter, this.files);
-			this.hideHeaderWhenNoFilesMatch();
+			this.hideIrrelevantUIWhenNoFilesMatch();
 			var that = this;
 			this.$fileList.find('tr').each(function(i,e) {
 				var $e = $(e);
@@ -1681,11 +1681,13 @@
 				}
 			});
 		},
-		hideHeaderWhenNoFilesMatch:function() {
+		hideIrrelevantUIWhenNoFilesMatch:function() {
 			if (this._filter && this.fileSummary.summary.totalDirs + this.fileSummary.summary.totalFiles === 0) {
 				this.$el.find('#filestable thead th').addClass('hidden');
+				this.$el.find('#emptycontent').addClass('hidden');
 			} else {
-				this.$el.find('#filestable thead th').removeClass('hidden');
+				this.$el.find('#filestable thead th').toggleClass('hidden', this.isEmpty);
+				this.$el.find('#emptycontent').toggleClass('hidden', !this.isEmpty);
 			}
 		},
 		/**
