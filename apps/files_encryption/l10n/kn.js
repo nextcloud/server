0836ce9cd83aac7a6cd2a6396632f6ddd8b09f3a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-26 01:54:28 -0500

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/kn.js
+++ apps/files_encryption/l10n/kn.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Unknown error" : "ಗೊತ್ತಿಲ್ಲದ ದೋಷ",
     "Encryption" : "ರಹಸ್ಯ ಸಂಕೇತೀಕರಿಸು",
+    "Enabled" : "ಸಕ್ರಿಯಗೊಳಿಸಿದೆ",
     "Disabled" : "ನಿಷ್ಕ್ರಿಯಗೊಳಿಸಲಾಗಿದೆ"
 },
 "nplurals=1; plural=0;");
