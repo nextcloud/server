nextcloud/server

commit 3d997e8c62bd752f38bcc11f02fb2af0e46d9154

Fix incorrect warning about setting the overwrite.cli.url

settings/admin.php
@@ -56,11 +56,11 @@ $template->assign('forceSSLforSubdomainsEnabled', $config->getSystemValue('force
 
 // If the current web root is non-empty but the web root from the config is,
 // and system cron is used, the URL generator fails to build valid URLs.
-$shouldSuggestOverwriteWebRoot = $config->getAppValue('core', 'backgroundjobs_mode', 'ajax') === 'cron' &&
+$shouldSuggestOverwriteCliUrl = $config->getAppValue('core', 'backgroundjobs_mode', 'ajax') === 'cron' &&
 	\OC::$WEBROOT && \OC::$WEBROOT !== '/' &&
-	!$config->getSystemValue('overwritewebroot', '');
-$suggestedOverwriteWebRoot = ($shouldSuggestOverwriteWebRoot) ? \OC::$WEBROOT : '';
-$template->assign('suggestedOverwriteWebroot', $suggestedOverwriteWebRoot);
+	!$config->getSystemValue('overwrite.cli.url', '');
+$suggestedOverwriteCliUrl = ($shouldSuggestOverwriteCliUrl) ? \OC::$WEBROOT : '';
+$template->assign('suggestedOverwriteCliUrl', $suggestedOverwriteCliUrl);
 
 $template->assign('allowLinks', $appConfig->getValue('core', 'shareapi_allow_links', 'yes'));
 $template->assign('enforceLinkPassword', \OCP\Util::isPublicLinkPasswordRequired());

settings/templates/admin.php
@@ -178,13 +178,13 @@ if (!$_['isLocaleWorking']) {
 <?php
 }
 
-if ($_['suggestedOverwriteWebroot']) {
+if ($_['suggestedOverwriteCliUrl']) {
 	?>
 	<div class="section">
 		<h2><?php p($l->t('URL generation in notification emails'));?></h2>
 
 		<span class="connectionwarning">
-		<?php p($l->t('If your installation is not installed in the root of the domain and uses system cron, there can be issues with the URL generation. To avoid these problems, please set the "overwritewebroot" option in your config.php file to the webroot path of your installation (Suggested: "%s")', $_['suggestedOverwriteWebroot'])); ?>
+		<?php p($l->t('If your installation is not installed in the root of the domain and uses system cron, there can be issues with the URL generation. To avoid these problems, please set the "overwrite.cli.url" option in your config.php file to the webroot path of your installation (Suggested: "%s")', $_['suggestedOverwriteCliUrl'])); ?>
 	</span>
 
 	</div>

commit 7b9343f385cf98e68f367a01df23f3960170ba5a

fix typo - caused by 3d997e8c62bd752f38bcc11f02fb2af0e46d9154

settings/admin.php
@@ -87,7 +87,7 @@ $forms = OC_App::getForms('admin');
 $l = OC_L10N::get('settings');
 $formsAndMore = array();
 if (OC_Request::serverProtocol() !== 'https' || !OC_Util::isAnnotationsWorking() ||
-	$suggestedOverwriteWebRoot || !OC_Util::isSetLocaleWorking() || !OC_Util::isPhpCharSetUtf8() ||
+	$suggestedOverwriteCliUrl || !OC_Util::isSetLocaleWorking() || !OC_Util::isPhpCharSetUtf8() ||
 	!OC_Util::fileInfoLoaded() || $databaseOverload
 ) {
 	$formsAndMore[] = array('anchor' => 'security-warning', 'section-name' => $l->t('Security & Setup Warnings'));