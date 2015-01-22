bb80cf4ecab3a92541bf9c2fccaa204b6ea914f9
Lukas Reschke <lukas@owncloud.com>
2015-01-22 13:46:40 +0100

    Add check for `HTTP_RAW_POST_DATA` setting for >= 5.6
    PHP 5.6 otherwise throws notices for perfectly valid code which results in broken endpoints.
    
    Fixes https://github.com/owncloud/core/issues/13592

--- lib/private/util.php
+++ lib/private/util.php
@@ -628,6 +628,21 @@ class OC_Util {
 			);
 			$webServerRestart = true;
 		}
+
+		/**
+		 * PHP 5.6 ships with a PHP setting which throws notices by default for a
+		 * lot of endpoints. Thus we need to ensure that the value is set to -1
+		 *
+		 * @link https://github.com/owncloud/core/issues/13592
+		 */
+		if(version_compare(phpversion(), '5.6.0', '>=') &&
+			\OC::$server->getIniWrapper()->getNumeric('always_populate_raw_post_data') !== -1) {
+			$errors[] = array(
+				'error' => 'PHP is configured to populate raw post data. Since PHP 5.6 this will lead to PHP throwing notices for perfectly valid code.',
+				'hint' => 'To fix this issue set <code>always_populate_raw_post_data</code> to <code>-1</code> in your php.ini'
+			);
+		}
+
 		if (!self::isAnnotationsWorking()) {
 			$errors[] = array(
 				'error' => 'PHP is apparently setup to strip inline doc blocks. This will make several core apps inaccessible.',
