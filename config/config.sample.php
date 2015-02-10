c11edb9138cf4c3a4eaf535088e4cc4b5c8c7251
RealRancor <Fisch.666@gmx.de>
2015-02-10 21:20:05 +0100

    Add hint for troubleshooting MS Word previews

--- config/config.sample.php
+++ config/config.sample.php
@@ -649,6 +649,10 @@ $CONFIG = array(
  *  - OC\Preview\StarOffice
  *  - OC\Preview\SVG
  *
+ * .. note:: Troubleshooting steps for the MS Word previews are available
+ *    at the :doc:`collaborative_documents_configuration#troubleshooting` section
+ *    of the Administrators Manual.
+ *
  * The following providers are not available in Microsoft Windows:
  *
  *  - OC\Preview\Movie
