ecb7d4477525942d6104168f9361337247af9c48
Roeland Jago Douma <roeland@famdouma.nl>
2015-02-12 17:05:59 +0100

    Small unit test rewrite

--- core/js/tests/specs/shareSpec.js
+++ core/js/tests/specs/shareSpec.js
@@ -435,7 +435,7 @@ describe('OC.Share tests', function() {
 				expect(avatarStub.calledOnce).toEqual(true);
 				var args = avatarStub.getCall(0).args;
 
-				expect($('#avatar-user1')[0]).toEqual(jasmine.anything());
+				expect($('#avatar-user1').length).toEqual(1);
 				expect(args.length).toEqual(2);
 				expect(args[0]).toEqual('user1');
 			});
