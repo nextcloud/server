bdd785f5e6071b8cfb65c0a6252284d202162800
Roeland Jago Douma <roeland@famdouma.nl>
2015-02-12 07:47:15 +0100

    Always display max upload size in admin panel

--- apps/files/templates/admin.php
+++ apps/files/templates/admin.php
@@ -1,18 +1,18 @@
-<?php if($_['uploadChangable']):?>
-
 	<?php OCP\Util::addscript('files', 'admin'); ?>
 
 	<form name="filesForm" class="section" action="#" method="post">
 		<h2><?php p($l->t('File handling')); ?></h2>
 		<label for="maxUploadSize"><?php p($l->t( 'Maximum upload size' )); ?> </label>
-		<input type="text" name='maxUploadSize' id="maxUploadSize" value='<?php p($_['uploadMaxFilesize']) ?>'/>
+		<input type="text" name='maxUploadSize' id="maxUploadSize" value='<?php p($_['uploadMaxFilesize']) ?>' <?php if(!$_['uploadChangable']) { p('disabled'); } ?> />
 		<?php if($_['displayMaxPossibleUploadSize']):?>
 			(<?php p($l->t('max. possible: ')); p($_['maxPossibleUploadSize']) ?>)
 		<?php endif;?>
 		<br/>
 		<input type="hidden" value="<?php p($_['requesttoken']); ?>" name="requesttoken" />
-		<input type="submit" name="submitFilesAdminSettings" id="submitFilesAdminSettings"
-			   value="<?php p($l->t( 'Save' )); ?>"/>
+		<?php if($_['uploadChangable']): ?>
+			<input type="submit" name="submitFilesAdminSettings" id="submitFilesAdminSettings"
+				   value="<?php p($l->t( 'Save' )); ?>"/>
+		<?php else: ?>
+			<?php p($l->t('Can not be edited from here due to insufficient permissions.')); ?>
+		<?php endif; ?>
 	</form>
-
-<?php endif;?>
