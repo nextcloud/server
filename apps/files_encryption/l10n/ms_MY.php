e52a63eccb2a745f5c0403a09211b28050df3279
Joas Schilling <nickvergessen@gmx.de>
2015-02-19 11:58:40 +0100

    Remove some old l10n files they arent loaded anymore

--- apps/files_encryption/l10n/ms_MY.php
+++
@@ -1,5 +0,0 @@
-<?php
-$TRANSLATIONS = array(
-"Saving..." => "Simpan..."
-);
-$PLURAL_FORMS = "nplurals=1; plural=0;";
--- apps/files_encryption/l10n/te.php
+++
@@ -1,5 +0,0 @@
-<?php
-$TRANSLATIONS = array(
-"personal settings" => "వ్యక్తిగత అమరికలు"
-);
-$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
