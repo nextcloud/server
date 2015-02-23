f63b09facae0e70010cc675aee960d4a0f826078
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-23 01:55:10 -0500

    [tx-robot] updated from transifex

--- apps/files_encryption/l10n/sr.js
+++ apps/files_encryption/l10n/sr.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Unknown error" : "Непозната грешка",
     "Encryption App is enabled but your keys are not initialized, please log-out and log-in again" : "Апликација шифровања је укључена али није иницијализована. Одјавите се и поново се пријавите.",
-    "Enabled" : "Омогућено"
+    "Enabled" : "Омогућено",
+    "Disabled" : "искључена"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- apps/files_versions/l10n/sr.js
+++ apps/files_versions/l10n/sr.js
@@ -1,6 +1,11 @@
 OC.L10N.register(
     "files_versions",
     {
+    "Could not revert: %s" : "Не могу да вратим: %s",
+    "Versions" : "Верзије",
+    "Failed to revert {file} to revision {timestamp}." : "Не могу давратим {file} на ревизију {timestamp}.",
+    "More versions..." : "Још верзија...",
+    "No other versions available" : "Нема других верзија",
     "Restore" : "Врати"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
