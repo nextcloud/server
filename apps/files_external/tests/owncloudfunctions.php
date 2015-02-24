1c5a86a115e555e3bc1ea133d6911f897bbe7a53
Robin McCorkell <rmccorkell@karoshi.org.uk>
2015-02-24 23:06:47 +0000

    Fix bugs in ownCloud external storage

--- apps/files_external/tests/owncloudfunctions.php
+++ apps/files_external/tests/owncloudfunctions.php
@@ -68,6 +68,14 @@ class OwnCloudFunctions extends \PHPUnit_Framework_TestCase {
 				),
 				'http://testhost/testroot/remote.php/webdav/subdir/',
 			),
+			array(
+				array(
+					'host' => 'http://testhost/testroot/',
+					'root' => '/subdir',
+					'secure' => false
+				),
+				'http://testhost/testroot/remote.php/webdav/subdir/',
+			),
 		);
 	}
 
