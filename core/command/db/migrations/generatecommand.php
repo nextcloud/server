c02e88023653be82345f3e22e8ddb7c74a0e1233
Thomas Müller <thomas.mueller@tmit.eu>
2015-03-13 00:12:11 +0100

    Adding doctrine/migrations
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

---
+++ core/command/db/migrations/generatecommand.php
@@ -0,0 +1,51 @@
+<?php
+ /**
+ * @author Thomas Müller
+ * @copyright 2015 Thomas Müller deepdiver@owncloud.com
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+namespace OC\Core\Command\Db\Migrations;
+
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand as DBALGenerateCommand;
+use OCP\IConfig;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateCommand extends DBALGenerateCommand {
+	use MigrationTrait;
+
+	/** @var Connection */
+	private $ocConnection;
+
+	/**
+	 * @param \OCP\IConfig $config
+	 */
+	public function __construct(IConfig $config, Connection $connection) {
+		$this->config = $config;
+		$this->ocConnection = $connection;
+
+		parent::__construct();
+	}
+
+	protected function configure() {
+		$this->addArgument('app', InputArgument::REQUIRED, 'Name of the app this migration command shall work on');
+
+		parent::configure();
+	}
+
+	public function execute(InputInterface $input, OutputInterface $output) {
+		$appName = $input->getArgument('app');
+		$mc = $this->buildConfiguration($appName, $this->ocConnection);
+		$this->setMigrationConfiguration($mc);
+
+		parent::execute($input, $output);
+	}
+
+}
