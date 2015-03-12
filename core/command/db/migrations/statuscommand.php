27c826526a709520cc7015633afe728ebc8fba10
Thomas Müller <thomas.mueller@tmit.eu>
2015-03-13 00:12:11 +0100

    Adding doctrine/migrations

---
+++ core/command/db/migrations/statuscommand.php
@@ -0,0 +1,9 @@
+<?php
+ /**
+ * @author Thomas Müller
+ * @copyright 2015 Thomas Müller deepdiver@owncloud.com
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
