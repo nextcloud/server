908538d8638e901f391162a078cf476194f30eea
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-03-26 21:23:49 +0100

    fix layout for Sharing settings

--- apps/files_sharing/templates/settings-admin.php
+++ apps/files_sharing/templates/settings-admin.php
@@ -2,20 +2,22 @@
 /** @var OC_L10N $l */
 /** @var array $_ */
 ?>
-<div id="fileSharingSettings" >
-
+<div id="fileSharingSettings">
 	<h3><?php p($l->t('Federated Cloud Sharing'));?></h3>
 
-	<input type="checkbox" name="outgoing_server2server_share_enabled" id="outgoingServer2serverShareEnabled"
-		   value="1" <?php if ($_['outgoingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
-	<label for="outgoingServer2serverShareEnabled">
-		<?php p($l->t('Allow users on this server to send shares to other servers'));?>
-	</label><br/>
+	<p>
+		<input type="checkbox" name="outgoing_server2server_share_enabled" id="outgoingServer2serverShareEnabled"
+			   value="1" <?php if ($_['outgoingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
+		<label for="outgoingServer2serverShareEnabled">
+			<?php p($l->t('Allow users on this server to send shares to other servers'));?>
+		</label>
+	</p>
 
-	<input type="checkbox" name="incoming_server2server_share_enabled" id="incomingServer2serverShareEnabled"
-		   value="1" <?php if ($_['incomingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
-	<label for="incomingServer2serverShareEnabled">
-		<?php p($l->t('Allow users on this server to receive shares from other servers'));?>
-	</label><br/>
-
+	<p>
+		<input type="checkbox" name="incoming_server2server_share_enabled" id="incomingServer2serverShareEnabled"
+			   value="1" <?php if ($_['incomingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
+		<label for="incomingServer2serverShareEnabled">
+			<?php p($l->t('Allow users on this server to receive shares from other servers'));?>
+		</label><br/>
+	</p>
 </div>
