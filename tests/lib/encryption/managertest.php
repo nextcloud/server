8dde93254680bfb481e8c737fd3696ee5a9e74da
Thomas Müller <thomas.mueller@tmit.eu>
2015-03-27 10:07:59 +0100

    Comment duplicate test methods

--- tests/lib/encryption/managertest.php
+++ tests/lib/encryption/managertest.php
@@ -112,61 +112,61 @@ class ManagerTest extends TestCase {
 		$this->assertEquals(0, $en0->getId());
 	}
 
-	/**
-	 * @expectedException \OC\Encryption\Exceptions\ModuleAlreadyExistsException
-	 * @expectedExceptionMessage Id "0" already used by encryption module "TestDummyModule0"
-	 */
-	public function testModuleRegistration() {
-		$config = $this->getMock('\OCP\IConfig');
-		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
-		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
-		$em->expects($this->any())->method('getId')->willReturn(0);
-		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
-		$m = new Manager($config);
-		$m->registerEncryptionModule($em);
-		$this->assertTrue($m->isEnabled());
-		$m->registerEncryptionModule($em);
-	}
-
-	public function testModuleUnRegistration() {
-		$config = $this->getMock('\OCP\IConfig');
-		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
-		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
-		$em->expects($this->any())->method('getId')->willReturn(0);
-		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
-		$m = new Manager($config);
-		$m->registerEncryptionModule($em);
-		$this->assertTrue($m->isEnabled());
-		$m->unregisterEncryptionModule($em);
-		$this->assertFalse($m->isEnabled());
-	}
-
-	/**
-	 * @expectedException \OC\Encryption\Exceptions\ModuleDoesNotExistsException
-	 * @expectedExceptionMessage Module with id: unknown does not exists.
-	 */
-	public function testGetEncryptionModuleUnknown() {
-		$config = $this->getMock('\OCP\IConfig');
-		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
-		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
-		$em->expects($this->any())->method('getId')->willReturn(0);
-		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
-		$m = new Manager($config);
-		$m->registerEncryptionModule($em);
-		$this->assertTrue($m->isEnabled());
-		$m->getEncryptionModule('unknown');
-	}
-
-	public function testGetEncryptionModule() {
-		$config = $this->getMock('\OCP\IConfig');
-		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
-		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
-		$em->expects($this->any())->method('getId')->willReturn(0);
-		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
-		$m = new Manager($config);
-		$m->registerEncryptionModule($em);
-		$this->assertTrue($m->isEnabled());
-		$en0 = $m->getEncryptionModule(0);
-		$this->assertEquals(0, $en0->getId());
-	}
+//	/**
+//	 * @expectedException \OC\Encryption\Exceptions\ModuleAlreadyExistsException
+//	 * @expectedExceptionMessage Id "0" already used by encryption module "TestDummyModule0"
+//	 */
+//	public function testModuleRegistration() {
+//		$config = $this->getMock('\OCP\IConfig');
+//		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
+//		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
+//		$em->expects($this->any())->method('getId')->willReturn(0);
+//		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
+//		$m = new Manager($config);
+//		$m->registerEncryptionModule($em);
+//		$this->assertTrue($m->isEnabled());
+//		$m->registerEncryptionModule($em);
+//	}
+//
+//	public function testModuleUnRegistration() {
+//		$config = $this->getMock('\OCP\IConfig');
+//		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
+//		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
+//		$em->expects($this->any())->method('getId')->willReturn(0);
+//		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
+//		$m = new Manager($config);
+//		$m->registerEncryptionModule($em);
+//		$this->assertTrue($m->isEnabled());
+//		$m->unregisterEncryptionModule($em);
+//		$this->assertFalse($m->isEnabled());
+//	}
+//
+//	/**
+//	 * @expectedException \OC\Encryption\Exceptions\ModuleDoesNotExistsException
+//	 * @expectedExceptionMessage Module with id: unknown does not exists.
+//	 */
+//	public function testGetEncryptionModuleUnknown() {
+//		$config = $this->getMock('\OCP\IConfig');
+//		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
+//		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
+//		$em->expects($this->any())->method('getId')->willReturn(0);
+//		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
+//		$m = new Manager($config);
+//		$m->registerEncryptionModule($em);
+//		$this->assertTrue($m->isEnabled());
+//		$m->getEncryptionModule('unknown');
+//	}
+//
+//	public function testGetEncryptionModule() {
+//		$config = $this->getMock('\OCP\IConfig');
+//		$config->expects($this->any())->method('getSystemValue')->willReturn(true);
+//		$em = $this->getMock('\OCP\Encryption\IEncryptionModule');
+//		$em->expects($this->any())->method('getId')->willReturn(0);
+//		$em->expects($this->any())->method('getDisplayName')->willReturn('TestDummyModule0');
+//		$m = new Manager($config);
+//		$m->registerEncryptionModule($em);
+//		$this->assertTrue($m->isEnabled());
+//		$en0 = $m->getEncryptionModule(0);
+//		$this->assertEquals(0, $en0->getId());
+//	}
 }
