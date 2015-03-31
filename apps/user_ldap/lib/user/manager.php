01328b46229a0c574b262b84bba6035ede06454e
Jörn Friedrich Dreyer <jfd@butonic.de>
2015-03-31 13:42:23 +0200

    do not hide exception when ldap server has a hiccup

--- apps/user_ldap/lib/user/manager.php
+++ apps/user_ldap/lib/user/manager.php
@@ -166,6 +166,7 @@ class Manager {
 	 * @brief returns a User object by it's DN or ownCloud username
 	 * @param string the DN or username of the user
 	 * @return \OCA\user_ldap\lib\user\User|\OCA\user_ldap\lib\user\OfflineUser|null
+	 * @throws \Exception when connection could not be established
 	 */
 	public function get($id) {
 		$this->checkAccess();
@@ -182,12 +183,7 @@ class Manager {
 			}
 		}
 
-		try {
-			$user = $this->createInstancyByUserName($id);
-			return $user;
-		} catch (\Exception $e) {
-			return null;
-		}
+		return $this->createInstancyByUserName($id);
 	}
 
 }
