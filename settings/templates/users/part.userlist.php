b6efee8079ac67da5e72c281c3a9cacf047d7fd8
Volker E <volker.e@temporaer.net>
2015-04-04 02:07:05 +0200

    fixing #15396 accessibility of table#userlist

--- settings/templates/users/part.userlist.php
+++ settings/templates/users/part.userlist.php
@@ -2,20 +2,20 @@
 	<thead>
 		<tr>
 			<?php if ($_['enableAvatars']): ?>
-			<th id='headerAvatar'></th>
+			<th id='headerAvatar' scope="col"></th>
 			<?php endif; ?>
-			<th id='headerName'><?php p($l->t('Username'))?></th>
-			<th id="headerDisplayName"><?php p($l->t( 'Full Name' )); ?></th>
-			<th id="headerPassword"><?php p($l->t( 'Password' )); ?></th>
-			<th class="mailAddress"><?php p($l->t( 'Email' )); ?></th>
-			<th id="headerGroups"><?php p($l->t( 'Groups' )); ?></th>
+			<th id='headerName' scope="col"><?php p($l->t('Username'))?></th>
+			<th id="headerDisplayName" scope="col"><?php p($l->t( 'Full Name' )); ?></th>
+			<th id="headerPassword" scope="col"><?php p($l->t( 'Password' )); ?></th>
+			<th class="mailAddress" scope="col"><?php p($l->t( 'Email' )); ?></th>
+			<th id="headerGroups" scope="col"><?php p($l->t( 'Groups' )); ?></th>
 			<?php if(is_array($_['subadmins']) || $_['subadmins']): ?>
-			<th id="headerSubAdmins"><?php p($l->t('Group Admin for')); ?></th>
+			<th id="headerSubAdmins" scope="col"><?php p($l->t('Group Admin for')); ?></th>
 			<?php endif;?>
-			<th id="headerQuota"><?php p($l->t('Quota')); ?></th>
-			<th class="storageLocation"><?php p($l->t('Storage Location')); ?></th>
-			<th class="userBackend"><?php p($l->t('User Backend')); ?></th>
-			<th class="lastLogin"><?php p($l->t('Last Login')); ?></th>
+			<th id="headerQuota" scope="col"><?php p($l->t('Quota')); ?></th>
+			<th class="storageLocation" scope="col"><?php p($l->t('Storage Location')); ?></th>
+			<th class="userBackend" scope="col"><?php p($l->t('User Backend')); ?></th>
+			<th class="lastLogin" scope="col"><?php p($l->t('Last Login')); ?></th>
 			<th id="headerRemove">&nbsp;</th>
 		</tr>
 	</thead>
@@ -25,7 +25,7 @@
 			<?php if ($_['enableAvatars']): ?>
 				<td class="avatar"><div class="avatardiv"></div></td>
 			<?php endif; ?>
-			<td class="name"></td>
+			<th class="name" scope="row"></th>
 			<td class="displayName"><span></span> <img class="svg action"
 				src="<?php p(image_path('core', 'actions/rename.svg'))?>"
 				alt="<?php p($l->t("change full name"))?>" title="<?php p($l->t("change full name"))?>"/>
