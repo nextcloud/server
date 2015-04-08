84c2fe85b72a0ef2c3b56075316f75153ba07ae7
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-04-08 19:25:28 +0200

    add icons to files sidebar, fix shade, fix position of settings icon and text

--- apps/files/templates/appnavigation.php
+++ apps/files/templates/appnavigation.php
@@ -1,7 +1,12 @@
 <div id="app-navigation">
-	<ul>
+	<ul class="with-icon">
 		<?php foreach ($_['navigationItems'] as $item) { ?>
-		<li data-id="<?php p($item['id']) ?>" class="nav-<?php p($item['id']) ?>"><a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"><?php p($item['name']);?></a></li>
+		<li data-id="<?php p($item['id']) ?>" class="nav-<?php p($item['id']) ?>">
+			<a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"
+				class="nav-icon-<?php p($item['id']) ?>">
+				<?php p($item['name']);?>
+			</a>
+		</li>
 		<?php } ?>
 	</ul>
 	<div id="app-settings">
