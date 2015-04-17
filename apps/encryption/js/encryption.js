3bc5c1c3cf285baa4d0ceabaa09c49f85c1ae4f8
Thomas Müller <thomas.mueller@tmit.eu>
2015-04-17 18:04:02 +0200

    use a simple function - OC.Encryption is already defined - fixes #15707
    This reverts commit 0ca6398aa34e0a3698f3aed0305f0e9299578bbc.

--- apps/encryption/js/encryption.js
+++ apps/encryption/js/encryption.js
@@ -5,37 +5,27 @@
  * See the COPYING-README file.
  */
 
-/**
- * @namespace
- * @memberOf OC
- */
-OC.Encryption= {
-	MIGRATION_OPEN: 0,
-	MIGRATION_COMPLETED: 1,
-	MIGRATION_IN_PROGRESS: -1,
 
+function displayEncryptionWarning () {
 
-	displayEncryptionWarning: function () {
-
-		if (!OC.Notification.isHidden()) {
-			return;
-		}
-
-		$.get(
-			OC.generateUrl('/apps/encryption/ajax/getStatus')
-			,  function( result ) {
-				if (result.status === "success") {
-					OC.Notification.show(result.data.message);
-				}
-			}
-		);
+	if (!OC.Notification.isHidden()) {
+		return;
 	}
-};
+
+	$.get(
+		OC.generateUrl('/apps/encryption/ajax/getStatus'),
+		function( result ) {
+			if (result.status === "success") {
+				OC.Notification.show(result.data.message);
+			}
+		}
+	);
+}
 
 $(document).ready(function() {
 	// wait for other apps/extensions to register their event handlers and file actions
 	// in the "ready" clause
 	_.defer(function() {
-		OC.Encryption.displayEncryptionWarning();
+		displayEncryptionWarning();
 	});
 });
