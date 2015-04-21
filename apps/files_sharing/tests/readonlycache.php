9695e33e340158ead3d54811c8b99deabd49d50b
Olivier Paroz <github@oparoz.com>
2015-04-21 14:40:11 +0200

    Renamed class + split methods

--- apps/files_sharing/tests/readonlycache.php
+++ apps/files_sharing/tests/readonlycache.php
@@ -18,39 +18,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
-use OCA\Files_sharing\Tests\TestCase;
+namespace OCA\Files_Sharing\Tests;
 
-class Test_Files_Sharing_ReadOnly_Cache extends TestCase {
+class ReadOnlyCache extends TestCase {
 
-	/**
-	 * @type \OC\Files\Storage\Storage
-	 */
+	/** @var \OC\Files\Storage\Storage */
 	protected $storage;
 
-	/**
-	 * @type OC\Files\Storage\StorageFactory
-	 */
+	/** @var \OC\Files\Storage\StorageFactory */
 	protected $loader;
 
-	/**
-	 * @type OC\Files\Mount\MountPoint
-	 */
+	/** @var \OC\Files\Mount\MountPoint */
 	protected $readOnlyMount;
 
-	/**
-	 * @type \OCA\Files_Sharing\ReadOnlyWrapper
-	 */
+	/** @var \OCA\Files_Sharing\ReadOnlyWrapper */
 	protected $readOnlyStorage;
 
-	/**
-	 * @type \OC\Files\Cache\Cache
-	 */
+	/** @var \OC\Files\Cache\Cache */
 	protected $readOnlyCache;
 
 	protected function setUp() {
 		parent::setUp();
-
-		//error_reporting(E_ERROR | E_PARSE);
 
 		$this->view->mkdir('readonly');
 		$this->view->file_put_contents('readonly/foo.txt', 'foo');
@@ -66,10 +54,6 @@ class Test_Files_Sharing_ReadOnly_Cache extends TestCase {
 		$this->readOnlyCache = $this->readOnlyStorage->getCache();
 	}
 
-	protected function tearDown() {
-		parent::tearDown();
-	}
-
 	public function testSetup() {
 		$this->assertTrue($this->view->file_exists('/readonly/foo.txt'));
 
@@ -82,22 +66,26 @@ class Test_Files_Sharing_ReadOnly_Cache extends TestCase {
 		$this->assertInstanceOf('\OCA\Files_Sharing\ReadOnlyCache', $this->readOnlyCache);
 	}
 
-	public function testGet() {
+	public function testGetWhenFileExists() {
 		$result = $this->readOnlyCache->get('files/readonly/foo.txt');
 		$this->assertNotEmpty($result);
+	}
 
+	public function testGetWhenFileDoesNotExist() {
 		$result = $this->readOnlyCache->get('files/readonly/proof does not exist.md');
 		$this->assertFalse($result);
 	}
 
-	public function testGetFolderContents() {
+	public function testGetFolderContentsWhenFolderExists() {
 		$results = $this->readOnlyCache->getFolderContents('files/readonly');
 		$this->assertNotEmpty($results);
 
 		foreach ($results as $result) {
 			$this->assertNotEmpty($result);
 		}
+	}
 
+	public function testGetFolderContentsWhenFolderDoesNotExist() {
 		$results = $this->readOnlyCache->getFolderContents('files/iamaghost');
 		$this->assertEmpty($results);
 	}
