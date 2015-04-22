owncloud/core

do not hide exception when ldap server has a hiccup

apps/user_ldap/lib/connection.php
@@ -158,7 +158,8 @@ class Connection extends LDAPUtility {
 			$this->establishConnection();
 		}
 		if(is_null($this->ldapConnectionRes)) {
-			\OCP\Util::writeLog('user_ldap', 'Connection could not be established', \OCP\Util::ERROR);
+			\OCP\Util::writeLog('user_ldap', 'No LDAP Connection to server ' . $this->connection->ldapHost, \OCP\Util::ERROR);
+			throw new \Exception('Connection to LDAP server could not be established');
 		}
 		return $this->ldapConnectionRes;
 	}

nextcloud/server

apps/user_ldap/lib/connection.php
@@ -45,7 +45,7 @@ class Connection extends LDAPUtility {
 	//cache handler
 	protected $cache;
 
-	//settings handler
+	/** @var Configuration settings handler **/
 	protected $configuration;
 
 	protected $doNotValidate = false;
@@ -158,7 +158,8 @@ class Connection extends LDAPUtility {
 			$this->establishConnection();
 		}
 		if(is_null($this->ldapConnectionRes)) {
-			\OCP\Util::writeLog('user_ldap', 'Connection could not be established', \OCP\Util::ERROR);
+			\OCP\Util::writeLog('user_ldap', 'No LDAP Connection to server ' . $this->configuration->ldapHost, \OCP\Util::ERROR);
+			throw new \Exception('Connection to LDAP server could not be established');
 		}
 		return $this->ldapConnectionRes;
 	}