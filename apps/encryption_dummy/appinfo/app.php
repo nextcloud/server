7795265d9395d24371d73b6e9ff01839ec4646d4
Thomas Müller <thomas.mueller@tmit.eu>
2015-04-30 10:51:51 +0200

    Moving encryption_dummy to the apps repo - fixes #15950

--- apps/encryption_dummy/appinfo/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$manager = \OC::$server->getEncryptionManager();
-$module = new \OCA\Encryption_Dummy\DummyModule();
-$manager->registerEncryptionModule('OC_DUMMY_MODULE', 'Dummy Encryption Module', function() use ($module) {
-	return $module;
-});
-
