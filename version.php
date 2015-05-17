3abc6e6c44cae59ced68e7e498d78debd74f3534
Frank Karlitschek <frank@owncloud.org>
2015-05-17 14:25:56 -0400

    8.1 beta 2

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version=array(8, 1, 0, 5);
+$OC_Version=array(8, 1, 0, 6);
 
 // The human readable string
-$OC_VersionString='8.1 beta 1';
+$OC_VersionString='8.1 beta 2';
 
 // The ownCloud channel
 $OC_Channel='git';
