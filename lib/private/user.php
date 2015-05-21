694d639f94f04263d6010eff5cf76fc28bb6c001
Lukas Reschke <lukas@owncloud.com>
2015-05-21 13:07:54 +0200

    Fix PHPDoc :see_no_evil:
    *sigh*

--- lib/private/user.php
+++ lib/private/user.php
@@ -421,7 +421,7 @@ class OC_User {
 	/**
 	 * get the user id of the user currently logged in.
 	 *
-	 * @return string uid or false
+	 * @return string|bool uid or false
 	 */
 	public static function getUser() {
 		$uid = \OC::$server->getSession() ? \OC::$server->getSession()->get('user_id') : null;
