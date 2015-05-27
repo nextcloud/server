f9e258437f77bb81929b01d85201d8f39cfd3a0f
Lukas Reschke <lukas@owncloud.com>
2015-05-27 18:43:17 +0200

    Add check for availability of /dev/urandom
    Backport of https://github.com/owncloud/core/pull/16565 to stable8

--- settings/ajax/checksetup.php
+++ settings/ajax/checksetup.php
@@ -10,6 +10,22 @@ OCP\JSON::callCheck();
 
 \OC::$server->getSession()->close();
 
+/**
+ * Whether /dev/urandom is available to the PHP controller
+ *
+ * @return bool
+ */
+function isUrandomAvailable() {
+	if(@file_exists('/dev/urandom')) {
+		$file = fopen('/dev/urandom', 'rb');
+		if($file) {
+			fclose($file);
+			return true;
+		}
+	}
+	return false;
+}
+
 // no warning when has_internet_connection is false in the config
 $hasInternet = true;
 if (OC_Util::isInternetConnectionEnabled()) {
@@ -21,5 +37,7 @@ OCP\JSON::success(
 		'serverHasInternetConnection' => $hasInternet,
 		'dataDirectoryProtected' => OC_Util::isHtaccessWorking(),
 		'hasCurlInstalled' => function_exists('curl_init'),
+		'isUrandomAvailable' => isUrandomAvailable(),
+		'securityDocs' => \OC::$server->getURLGenerator()->linkToDocs('admin-security'),
 	)
 );
