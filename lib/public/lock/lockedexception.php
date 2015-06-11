0e3a3dd5d7fd9d57a099f3720af20b6f9a22d63f
Vincent Petry <pvince81@owncloud.com>
2015-06-11 18:32:15 +0200

    Rethrow LockedException with full path
    Because the path is converted to md5 from the original exception,
    rethrow the exception with the correct full path

--- lib/public/lock/lockedexception.php
+++ lib/public/lock/lockedexception.php
@@ -28,7 +28,10 @@ namespace OCP\Lock;
  * @since 8.1.0
  */
 class LockedException extends \Exception {
+
 	/**
+	 * Locked path
+	 *
 	 * @var string
 	 */
 	private $path;
@@ -36,11 +39,13 @@ class LockedException extends \Exception {
 	/**
 	 * LockedException constructor.
 	 *
-	 * @param string $path
+	 * @param string $path locked path
+	 * @param \Exception $previous previous exception for cascading
+	 *
 	 * @since 8.1.0
 	 */
-	public function __construct($path) {
-		parent::__construct($path . ' is locked');
+	public function __construct($path, \Exception $previous = null) {
+		parent::__construct('"' . $path . '" is locked', 0, $previous);
 		$this->path = $path;
 	}
 
