8ff4a66f0150624b8b08c4c47765016bd2e3a2ab
Olivier Paroz <github@oparoz.com>
2015-06-12 18:50:21 +0200

    Determine the size we need before asking for a thumbnail We need to set the size of the preview using the devicePixelRatio and use that as arguments when using the preview endpoint or that endpoint will use the default 36x36 and thumbnails will be blurry on high DPI devices

--- apps/files_sharing/js/public.js
+++ apps/files_sharing/js/public.js
@@ -159,6 +159,8 @@ OCA.Sharing.PublicApp = {
 
 			this.fileList.generatePreviewUrl = function (urlSpec) {
 				urlSpec.t = $('#dirToken').val();
+				urlSpec.y = 36 * window.devicePixelRatio;
+				urlSpec.x = 36 * window.devicePixelRatio;
 				return OC.generateUrl('/apps/files_sharing/ajax/publicpreview.php?') + $.param(urlSpec);
 			};
 
