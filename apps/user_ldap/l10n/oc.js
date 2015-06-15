2806c9476c1d3daff5c97772cba1a43df4df7936
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-15 01:54:58 -0400

    [tx-robot] updated from transifex

--- apps/user_ldap/l10n/oc.js
+++ apps/user_ldap/l10n/oc.js
@@ -1,12 +1,14 @@
 OC.L10N.register(
     "user_ldap",
     {
+    "Server" : "Servidor",
     "Users" : "Usancièrs",
     "Groups" : "Grops",
     "Help" : "Ajuda",
     "Host" : "Òste",
     "Port" : "Pòrt",
     "Password" : "Senhal",
+    "Continue" : "Contunhar",
     "Advanced" : "Avançat"
 },
 "nplurals=2; plural=(n > 1);");
