ebfbb97e66ef5d4c3dbe6b2eb23d19cba9ab74bd
Morris Jobke <hey@morrisjobke.de>
2015-07-08 15:23:01 +0200

    Fix parsing of sharetime as string
    In some cases the ajax/share.php will return the share time as string.
    If this is the case it would get parsed completely wrong and cause the
    share dropdown to not work anymore. This change will properly cast the
    string to an interger and also fallback if this is not possible.

--- core/js/share.js
+++ core/js/share.js
@@ -820,6 +820,21 @@ OC.Share={
 		return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');
 	},
 	/**
+	 * Parses a string to an valid integer (unix timestamp)
+	 * @param time
+	 * @returns {*}
+	 * @internal Only used to work around a bug in the backend
+	 */
+	_parseTime: function(time) {
+		if (_.isString(time)) {
+			time = parseInt(time, 10);
+			if(isNaN(time)) {
+				time = null;
+			}
+		}
+		return time;
+	},
+	/**
 	 * Displays the expiration date field
 	 *
 	 * @param {Date} date current expiration date
@@ -834,6 +849,8 @@ OC.Share={
 			minDate: minDate,
 			maxDate: null
 		};
+		// TODO: hack: backend returns string instead of integer
+		shareTime = OC.Share._parseTime(shareTime);
 		if (_.isNumber(shareTime)) {
 			shareTime = new Date(shareTime * 1000);
 		}
