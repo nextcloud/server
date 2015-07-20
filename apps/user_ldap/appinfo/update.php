89fdca7748961bd88ddbb975566185d5690a6e67
Morris Jobke <hey@morrisjobke.de>
2015-07-20 11:24:28 +0200

    Only throw exception if on new LDAP setups
    * this is done by setting enforce_home_folder_naming_rule for the user_ldap
      app in the app settings

---
+++ apps/user_ldap/appinfo/update.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Morris Jobke <hey@morrisjobke.de>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+$config = \OC::$server->getConfig();
+$installedVersion = $config->getAppValue('user_ldap', 'installed_version');
+
+if (version_compare($installedVersion, '0.5.2', '<')) {
+	$config->setAppValue('user_ldap', 'enforce_home_folder_naming_rule', false);
+}
