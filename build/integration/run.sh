db0a8aa795710d94f88f3c036983992792dd4191
Thomas Müller <thomas.mueller@tmit.eu>
2015-08-06 17:29:39 +0200

    Use fixed port for now

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -3,16 +3,17 @@
 composer install
 
 # TODO: avoid port collision on jenkins - use $EXECUTOR_NUMBER
-if [ -z "$EXECUTOR_NUMBER" ]; then
-    EXECUTOR_NUMBER=0
-fi
-PORT=$((8080 + $EXECUTOR_NUMBER))
+#if [ -z "$EXECUTOR_NUMBER" ]; then
+#    EXECUTOR_NUMBER=0
+#fi
+#PORT=$((8080 + $EXECUTOR_NUMBER))
+PORT=8080
 echo $PORT
 php -S localhost:$PORT -t ../.. &
 PHPPID=$!
 echo $PHPPID
 
-export BEHAT_PARAMS="context[parameters][base_url]=http://localhost:$PORT/ocs"
+#export BEHAT_PARAMS="context[parameters][base_url]=http://localhost:$PORT/ocs"
 vendor/bin/behat
 
 kill $PHPPID
