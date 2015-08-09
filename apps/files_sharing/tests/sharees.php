b2fbecc39fa5d0538a946244e5b85a7145cb4505
Roeland Jago Douma <roeland@famdouma.nl>
2015-08-09 21:19:35 +0200

    Empty skeleton for tests

---
+++ apps/files_sharing/tests/sharees.php
@@ -0,0 +1,50 @@
+<?php
+
+use OCA\Files_Sharing\API\Sharees;
+use OCA\Files_sharing\Tests\TestCase;
+
+class ShareesTest extends TestCase {
+
+	/**
+	 * Properly mock all injected elements
+	 */
+	protected function setup() {
+
+	}
+
+	public function testArguments() {
+
+	}
+
+	public function testsOnlyUsers() {
+
+	}
+
+	public function testOnlyGroups() {
+
+	}
+
+	public function testRemoteAddress() {
+
+	}
+
+	public function testRemoteFromContacts() {
+
+	}
+
+	public function testAll() {
+
+	}
+
+	public function testSorting() {
+
+	}
+
+	public function testPagination() {
+
+	}
+
+	public function testShareWithinGroup() {
+
+	}
+}
