e8c3eb74730171a0f997708d36cbcf9994cab217
Morris Jobke <hey@morrisjobke.de>
2015-08-20 14:51:28 +0200

    Clear cron errors on change of background job mode
    * fixes #18454

--- settings/js/admin.js
+++ settings/js/admin.js
@@ -46,6 +46,8 @@ $(document).ready(function(){
 			var mode = $(this).val();
 			if (mode === 'ajax' || mode === 'webcron' || mode === 'cron') {
 				OC.AppConfig.setValue('core', 'backgroundjobs_mode', mode);
+				// clear cron errors on background job mode change
+				OC.AppConfig.deleteKey('core', 'cronErrors');
 			}
 		}
 	});
