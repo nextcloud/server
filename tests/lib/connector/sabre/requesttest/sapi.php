3cf4fc02f9c99ba523f384f3def9f04eac21a067
Robin Appelman <icewind@owncloud.com>
2015-09-16 15:27:22 +0200

    fix tests with empty response body

--- tests/lib/connector/sabre/requesttest/sapi.php
+++ tests/lib/connector/sabre/requesttest/sapi.php
@@ -45,7 +45,7 @@ class Sapi {
 		$copyStream = fopen('php://temp', 'r+');
 		if (is_string($response->getBody())) {
 			fwrite($copyStream, $response->getBody());
-		} else {
+		} else if (is_resource($response->getBody())) {
 			stream_copy_to_stream($response->getBody(), $copyStream);
 		}
 		rewind($copyStream);
