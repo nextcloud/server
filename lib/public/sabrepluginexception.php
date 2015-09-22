1493e86deafb19f3f98ecee35389119daf2911a4
Joas Schilling <nickvergessen@owncloud.com>
2015-09-22 11:43:40 +0200

    Allow listeners to set status code and message

---
+++ lib/public/sabrepluginexception.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Joas Schilling <nickvergessen@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCP;
+
+
+use Sabre\DAV\Exception;
+
+class SabrePluginException extends Exception {
+
+	/**
+	 * Returns the HTTP statuscode for this exception
+	 *
+	 * @return int
+	 */
+	public function getHTTPCode() {
+		return $this->code;
+	}
+}
