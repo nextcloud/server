7940a3fb659221580fb2f5663048718fe67e4d02
Phil Davis <phil.davis@inf.org>
2015-09-29 12:17:47 +0545

    Session closed exception wording
    and a small comment typo

--- lib/private/session/internal.php
+++ lib/private/session/internal.php
@@ -106,7 +106,7 @@ class Internal extends Session {
 
 	private function validateSession() {
 		if ($this->sessionClosed) {
-			throw new \Exception('Session has been closed - no further changes to the session as allowed');
+			throw new \Exception('Session has been closed - no further changes to the session are allowed');
 		}
 	}
 }
