ee80cea2bdced165fc5e41f0e021a1602100dd65
Hendrik Leppelsack <hendrik@leppelsack.de>
2015-10-13 14:56:55 +0200

    apply new checkbox style to admin settings

--- apps/files_sharing/templates/settings-admin.php
+++ apps/files_sharing/templates/settings-admin.php
@@ -9,7 +9,7 @@
 		href="<?php p(link_to_docs('admin-sharing-federated')); ?>"></a>
 
 	<p>
-		<input type="checkbox" name="outgoing_server2server_share_enabled" id="outgoingServer2serverShareEnabled"
+		<input type="checkbox" name="outgoing_server2server_share_enabled" id="outgoingServer2serverShareEnabled" class="checkbox"
 			   value="1" <?php if ($_['outgoingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
 		<label for="outgoingServer2serverShareEnabled">
 			<?php p($l->t('Allow users on this server to send shares to other servers'));?>
@@ -17,7 +17,7 @@
 	</p>
 
 	<p>
-		<input type="checkbox" name="incoming_server2server_share_enabled" id="incomingServer2serverShareEnabled"
+		<input type="checkbox" name="incoming_server2server_share_enabled" id="incomingServer2serverShareEnabled" class="checkbox"
 			   value="1" <?php if ($_['incomingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
 		<label for="incomingServer2serverShareEnabled">
 			<?php p($l->t('Allow users on this server to receive shares from other servers'));?>
