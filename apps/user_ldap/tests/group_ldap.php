d486949705f52481f4155804ba21fe7d68834395
Lukas Reschke <lukas@owncloud.com>
2015-10-16 10:35:47 +0200

    Adjust unit tests

--- apps/user_ldap/tests/group_ldap.php
+++ apps/user_ldap/tests/group_ldap.php
@@ -42,14 +42,9 @@ class Test_Group_Ldap extends \Test\TestCase {
 		$connector = $this->getMock('\OCA\user_ldap\lib\Connection',
 									$conMethods,
 									array($lw, null, null));
-		$um = new \OCA\user_ldap\lib\user\Manager(
-				$this->getMock('\OCP\IConfig'),
-				$this->getMock('\OCA\user_ldap\lib\FilesystemHelper'),
-				$this->getMock('\OCA\user_ldap\lib\LogWrapper'),
-				$this->getMock('\OCP\IAvatarManager'),
-				$this->getMock('\OCP\Image'),
-				$this->getMock('\OCP\IDBConnection')
-			);
+		$um = $this->getMockBuilder('\OCA\user_ldap\lib\user\Manager')
+			->disableOriginalConstructor()
+			->getMock();
 		$access = $this->getMock('\OCA\user_ldap\lib\Access',
 								 $accMethods,
 								 array($connector, $lw, $um));
