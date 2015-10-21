2ca5b1aa1f9961948be2b1dbc1694203e6392f79
Tom Needham <tom@owncloud.com>
2015-10-21 10:00:29 +0000

    Add test for remote share info tooltop

--- core/js/tests/specs/sharedialogviewSpec.js
+++ core/js/tests/specs/sharedialogviewSpec.js
@@ -676,5 +676,21 @@ describe('OC.Share.ShareDialogView', function() {
 			});
 		});
 	});
+	describe('remote sharing', function() {
+		it('shows remote share info when allows', function() {
+			configModel.set({
+				isRemoteShareAllowed: true
+			});
+			dialog.render();
+			expect(dialog.$el.find('.shareWithRemoteInfo').length).toEqual(1);
+		});
+		it('does not show remote share info when not allowed', function() {
+			configModel.set({
+				isRemoteShareAllowed: false
+			});
+			dialog.render();
+			expect(dialog.$el.find('.shareWithRemoteInfo').length).toEqual(0);
+		});
+	});
 });
 
