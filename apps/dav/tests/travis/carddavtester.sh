af94c880b380264ecdec17183468170620be1382
Thomas Müller <thomas.mueller@tmit.eu>
2015-10-31 00:28:27 +0100

    Add CalDAVTester to travis

---
+++ apps/dav/tests/travis/carddavtester.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+
+# start the server
+php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../.." &
+
+
+if [ ! -f CalDAVTester/run.py ]; then
+	cd "$SCRIPTPATH"
+    git clone https://github.com/DeepDiver1975/CalDAVTester.git
+	cd "$SCRIPTPATH/CalDAVTester"
+    python run.py -s
+	cd "$SCRIPTPATH"
+fi
+
+# create test user
+cd "$SCRIPTPATH/../../../../"
+OC_PASS=user01 php occ user:add --password-from-env user01
+OC_PASS=user02 php occ user:add --password-from-env user02
+cd "$SCRIPTPATH/../../../../"
+
+# run the tests
+cd "$SCRIPTPATH/CalDAVTester"
+PYTHONPATH="$SCRIPTPATH/pycalendar/src" python testcaldav.py --print-details-onfail -s "$SCRIPTPATH/caldavtest/config/serverinfo.xml" -o cdt.txt \
+	"$SCRIPTPATH/caldavtest/tests/CardDAV/current-user-principal.xml"
+
