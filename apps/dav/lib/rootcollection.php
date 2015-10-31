2b13c5db0f6116858ee6e118e8ca22d73d764e11
Thomas Müller <thomas.mueller@tmit.eu>
2015-10-31 01:28:21 +0100

    Introducing CalDAV into core

--- apps/dav/lib/rootcollection.php
+++ apps/dav/lib/rootcollection.php
@@ -2,8 +2,10 @@
 
 namespace OCA\DAV;
 
+use OCA\DAV\CalDAV\CalDavBackend;
 use OCA\DAV\CardDAV\CardDavBackend;
 use OCA\DAV\Connector\Sabre\Principal;
+use Sabre\CalDAV\CalendarRoot;
 use Sabre\CalDAV\Principal\Collection;
 use Sabre\CardDAV\AddressBookRoot;
 use Sabre\DAV\SimpleCollection;
@@ -12,9 +14,10 @@ class RootCollection extends SimpleCollection {
 
 	public function __construct() {
 		$config = \OC::$server->getConfig();
+		$db = \OC::$server->getDatabaseConnection();
 		$principalBackend = new Principal(
-			$config,
-			\OC::$server->getUserManager()
+				$config,
+				\OC::$server->getUserManager()
 		);
 		// as soon as debug mode is enabled we allow listing of principals
 		$disableListing = !$config->getSystemValue('debug', false);
@@ -24,14 +27,18 @@ class RootCollection extends SimpleCollection {
 		$principalCollection->disableListing = $disableListing;
 		$filesCollection = new Files\RootCollection($principalBackend);
 		$filesCollection->disableListing = $disableListing;
-		$cardDavBackend = new CardDavBackend(\OC::$server->getDatabaseConnection());
+		$caldavBackend = new CalDavBackend($db);
+		$calendarRoot = new CalendarRoot($principalBackend, $caldavBackend);
+		$calendarRoot->disableListing = $disableListing;
+		$cardDavBackend = new CardDavBackend($db);
 		$addressBookRoot = new AddressBookRoot($principalBackend, $cardDavBackend);
 		$addressBookRoot->disableListing = $disableListing;
 
 		$children = [
-			$principalCollection,
-			$filesCollection,
-			$addressBookRoot,
+				$principalCollection,
+				$filesCollection,
+				$calendarRoot,
+				$addressBookRoot,
 		];
 
 		parent::__construct('root', $children);
