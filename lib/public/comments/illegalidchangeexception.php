d660c6162fdca619e98a5096f660a3998853171f
Arthur Schiwon <blizzz@owncloud.com>
2015-11-26 12:15:00 +0100

    proper description for IllegalIDChangeException

--- lib/public/comments/illegalidchangeexception.php
+++ lib/public/comments/illegalidchangeexception.php
@@ -3,7 +3,7 @@
 namespace OCP\Comments;
 
 /**
- * Exception for not found entity
+ * Exception for illegal attempts to modify a comment ID
  * @since 9.0.0
  */
 class IllegalIDChangeException extends \Exception {}
