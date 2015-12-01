21e063a9e7e4bb8f5beb808389eb888641b3f84f
Thomas Müller <thomas.mueller@tmit.eu>
2015-12-01 09:24:40 +0100

    tail the server log and exit with the exit code of behat

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -2,12 +2,11 @@
 
 composer install
 
-# TODO: avoid port collision on jenkins - use $EXECUTOR_NUMBER
+# avoid port collision on jenkins - use $EXECUTOR_NUMBER
 if [ -z "$EXECUTOR_NUMBER" ]; then
     EXECUTOR_NUMBER=0
 fi
 PORT=$((8080 + $EXECUTOR_NUMBER))
-#PORT=8080
 echo $PORT
 php -S localhost:$PORT -t ../.. &
 PHPPID=$!
@@ -23,6 +22,12 @@ export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 export TEST_SERVER_FED_URL="http://localhost:$PORT_FED/ocs/"
 
 vendor/bin/behat -f junit -f pretty
+RESULT=$?
 
 kill $PHPPID
 kill $PHPPID_FED
+
+tail "../../data/owncloud.log"
+
+exit $RESULT
+
