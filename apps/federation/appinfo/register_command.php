0bf026cceab5f5e910f5e7c163397dbe76dbe0f2
Thomas Müller <thomas.mueller@tmit.eu>
2015-12-04 13:53:53 +0100

    Fix registration of sync command

--- apps/federation/appinfo/register_command.php
+++ apps/federation/appinfo/register_command.php
@@ -1,9 +1,6 @@
 <?php
 
-$config = \OC::$server->getConfig();
 $dbConnection = \OC::$server->getDatabaseConnection();
-$userManager = OC::$server->getUserManager();
-$config = \OC::$server->getConfig();
 
 /** @var Symfony\Component\Console\Application $application */
-$application->add(new \OCA\Federation\Command\SyncFederationAddressBooks($userManager, $dbConnection, $config));
+$application->add(new \OCA\Federation\Command\SyncFederationAddressBooks($dbConnection));
