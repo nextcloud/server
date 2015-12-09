693a3c353ef88df2372426d4d1ea9f6527a59d62
Robin Appelman <icewind@owncloud.com>
2015-12-09 14:41:15 +0100

    ttl for memcache locking backends that support it

--- lib/private/lock/memcachelockingprovider.php
+++ lib/private/lock/memcachelockingprovider.php
@@ -21,6 +21,7 @@
 
 namespace OC\Lock;
 
+use OCP\IMemcacheTTL;
 use OCP\Lock\LockedException;
 use OCP\IMemcache;
 
@@ -35,6 +36,12 @@ class MemcacheLockingProvider extends AbstractLockingProvider {
 	 */
 	public function __construct(IMemcache $memcache) {
 		$this->memcache = $memcache;
+	}
+
+	private function setTTL($path) {
+		if ($this->memcache instanceof IMemcacheTTL) {
+			$this->memcache->setTTL($path, self::TTL);
+		}
 	}
 
 	/**
@@ -69,6 +76,7 @@ class MemcacheLockingProvider extends AbstractLockingProvider {
 				throw new LockedException($path);
 			}
 		}
+		$this->setTTL($path);
 		$this->markAcquire($path, $type);
 	}
 
@@ -106,6 +114,7 @@ class MemcacheLockingProvider extends AbstractLockingProvider {
 				throw new LockedException($path);
 			}
 		}
+		$this->setTTL($path);
 		$this->markChange($path, $targetType);
 	}
 }
