7a86f10ebc324e279b09ad94660ab444535da1c7
michag86 <micha_g@arcor.de>
2015-12-17 12:43:44 +0100

    Update users.js

--- settings/js/users/users.js
+++ settings/js/users/users.js
@@ -650,7 +650,7 @@ $(document).ready(function () {
 							{username: uid, password: $(this).val(), recoveryPassword: recoveryPasswordVal},
 							function (result) {
 								if (result.status != 'success') {
-									OC.Notification.show(t('admin', result.data.message));
+									OC.Notification.showTemporary(t('admin', result.data.message));
 								}
 							}
 						);
