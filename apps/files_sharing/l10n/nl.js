7080594670a2accd6a874f857a1588012a4ea823
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-20 01:55:09 -0500

    [tx-robot] updated from transifex

--- apps/files_sharing/l10n/nl.js
+++ apps/files_sharing/l10n/nl.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Server to server sharing is not enabled on this server" : "Server met server delen is niet geactiveerd op deze server",
     "The mountpoint name contains invalid characters." : "De naam van het mountpoint bevat ongeldige karakters.",
+    "Not allowed to create a federated share with the same user server" : "Het is niet toegestaan om een gefedereerde share met dezelfde gebruikersserver te maken",
     "Invalid or untrusted SSL certificate" : "Ongeldig of onvertrouwd SSL-certificaat",
     "Could not authenticate to remote share, password might be wrong" : "Kon niet authenticeren bij externe share, misschien verkeerd wachtwoord",
     "Storage not valid" : "Opslag ongeldig",
