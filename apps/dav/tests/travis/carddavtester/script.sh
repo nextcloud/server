ac78d248b7f5d478029d44d0c91cb6920db2a428
Thomas Müller <thomas.mueller@tmit.eu>
2015-12-21 11:43:15 +0100

    Sleep in carddavtester as well

--- apps/dav/tests/travis/carddavtester/script.sh
+++ apps/dav/tests/travis/carddavtester/script.sh
@@ -5,6 +5,8 @@ SCRIPTPATH=`dirname $SCRIPT`
 # start the server
 php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../../.." &
 
+sleep 30
+
 # run the tests
 cd "$SCRIPTPATH/CalDAVTester"
 PYTHONPATH="$SCRIPTPATH/pycalendar/src" python testcaldav.py --print-details-onfail -s "$SCRIPTPATH/../caldavtest/config/serverinfo.xml" -o cdt.txt \
