1a592e574593e79df1e0ed6b418cd880c4a5f7ee
Roeland Jago Douma <rullzer@owncloud.com>
2016-01-10 21:36:14 +0100

    Only '/tests' to be autoloaded when running unit tests

--- lib/base.php
+++ lib/base.php
@@ -488,8 +488,10 @@ class OC {
 			OC::$SERVERROOT . '/settings',
 			OC::$SERVERROOT . '/ocs',
 			OC::$SERVERROOT . '/ocs-provider',
-			OC::$SERVERROOT . '/tests',
 		]);
+		if (defined('PHPUNIT_RUN')) {
+			self::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+		}
 		spl_autoload_register(array(self::$loader, 'load'));
 		$loaderEnd = microtime(true);
 
