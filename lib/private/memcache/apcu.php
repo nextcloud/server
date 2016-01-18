a225fe4c969bdff5fb2041b9c680a8fe988cc1b0
Robin Appelman <icewind@owncloud.com>
2016-01-18 12:20:05 +0100

    Fix constructor arguments for APCUIterator

--- lib/private/memcache/apcu.php
+++ lib/private/memcache/apcu.php
@@ -60,7 +60,7 @@ class APCu extends Cache implements IMemcache {
 		if(class_exists('\APCIterator')) {
 			$iter = new \APCIterator('user', '/^' . $ns . '/', APC_ITER_KEY);
 		} else {
-			$iter = new \APCUIterator('user', '/^' . $ns . '/', APC_ITER_KEY);
+			$iter = new \APCUIterator('/^' . $ns . '/', APC_ITER_KEY);
 		}
 		return apcu_delete($iter);
 	}
