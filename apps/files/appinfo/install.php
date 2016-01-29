Pookay/core

Cleanup job for system and user tag orphaned when file was deleted
Whenever a file was deleted (also from trash), the tag mapping is
pointing at a fileid that doesn't exist any more.

This cleanup job will delete such orphaned mapping entries.

apps/files/appinfo/install.php
@@ -24,3 +24,4 @@ $jobList = \OC::$server->getJobList();
 $job = 'OCA\Files\BackgroundJob\ScanFiles';
 \OC::$server->getJobList()->add($job);
 
+\OC::$server->getJobList()->add('OCA\Files\Lib\DeleteOrphanedTagsJob');

apps/files/appinfo/update.php
@@ -102,6 +102,8 @@ $jobList = \OC::$server->getJobList();
 $job = 'OCA\Files\BackgroundJob\ScanFiles';
 \OC::$server->getJobList()->add($job);
 
+\OC::$server->getJobList()->add('OCA\Files\Lib\DeleteOrphanedTagsJob');
+
 /**
  * migrate old constant DEBUG to new config value 'debug'
  *

nextcloud/server

apps/files/appinfo/install.php
@@ -20,7 +20,5 @@
  */
 
 // Cron job for scanning user storages
-$jobList = \OC::$server->getJobList();
-$job = 'OCA\Files\BackgroundJob\ScanFiles';
-\OC::$server->getJobList()->add($job);
-
+\OC::$server->getJobList()->add('OCA\Files\BackgroundJob\ScanFiles');
+\OC::$server->getJobList()->add('OCA\Files\BackgroundJob\DeleteOrphanedTagsJob');

apps/files/appinfo/update.php
@@ -98,15 +98,5 @@ if ($installedVersion === '1.1.9' && (
 }
 
 // Add cron job for scanning user storages
-$jobList = \OC::$server->getJobList();
-$job = 'OCA\Files\BackgroundJob\ScanFiles';
-\OC::$server->getJobList()->add($job);
-
-/**
- * migrate old constant DEBUG to new config value 'debug'
- *
- * TODO: remove this in ownCloud 8.3
- */
-if(defined('DEBUG') && DEBUG === true) {
-	\OC::$server->getConfig()->setSystemValue('debug', true);
-}
+\OC::$server->getJobList()->add('OCA\Files\BackgroundJob\ScanFiles');
+\OC::$server->getJobList()->add('OCA\Files\BackgroundJob\DeleteOrphanedTagsJob');