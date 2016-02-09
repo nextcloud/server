e749a02e6b1f61d7667f7c63a97f2218ae4db7df
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-09 01:55:36 -0500

    [tx-robot] updated from transifex

--- settings/l10n/fr.js
+++ settings/l10n/fr.js
@@ -217,7 +217,7 @@ OC.L10N.register(
     "Enable only for specific groups" : "Activer uniquement pour certains groupes",
     "Uninstall App" : "Désinstaller l'application",
     "Enable experimental apps" : "Activer les applications expérimentales",
-    "SSL Root Certificates" : "Certificats Racine SSL",
+    "SSL Root Certificates" : "Certificats Racines SSL",
     "Common Name" : "Nom d'usage",
     "Valid until" : "Valide jusqu'à",
     "Issued By" : "Délivré par",
