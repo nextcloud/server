c392cc2b8f4a348eb5cef9846f5efabd1d19ac9d
C Montero-Luque <cml@owncloud.com>
2016-02-10 17:31:16 -0500

    9.0.0 beta 1

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version = array(9, 0, 0, 10);
+$OC_Version = array(9, 0, 0, 11);
 
 // The human readable string
-$OC_VersionString = '9.0 pre alpha';
+$OC_VersionString = '9.0 beta 1';
 
 $OC_VersionCanBeUpgradedFrom = array(8, 2);
 
