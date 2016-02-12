f8607ac1327f3254f6f4fe86c8418f1b3d447730
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-12 01:56:04 -0500

    [tx-robot] updated from transifex

--- apps/files_sharing/l10n/mn.js
+++ apps/files_sharing/l10n/mn.js
@@ -5,8 +5,8 @@ OC.L10N.register(
     "A file or folder has been <strong>shared</strong>" : "Файл эсвэл хавтас амжилттай <strong>түгээгдлээ</strong>",
     "You shared %1$s with %2$s" : "Та %1$s-ийг %2$s руу түгээлээ",
     "You shared %1$s with group %2$s" : "Та %1$s-ийг %2$s групп руу түгээлээ",
-    "%2$s shared %1$s with you" : "%2$s %1$s-ийг тань руу түгээлээ",
     "You shared %1$s via link" : "Та %1$s-ийг холбоосоор түгээлээ",
+    "%2$s shared %1$s with you" : "%2$s %1$s-ийг тань руу түгээлээ",
     "Shares" : "Түгээлтүүд",
     "Password" : "Нууц үг"
 },
