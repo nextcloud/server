6031ae1ad41cb39ac8783b4a95f98574c28c92f1
Robin Appelman <icewind@owncloud.com>
2016-02-19 14:27:55 +0100

    improve reuse in getUserFolder

--- lib/private/files/node/root.php
+++ lib/private/files/node/root.php
@@ -337,18 +337,18 @@ class Root extends Folder implements IRootFolder {
 		$dir = '/' . $userId;
 		$folder = null;
 
-		if (!$this->nodeExists($dir)) {
-			$folder = $this->newFolder($dir);
-		} else {
+		try {
 			$folder = $this->get($dir);
+		} catch (NotFoundException $e) {
+			$folder = $this->newFolder($dir);
 		}
 
 		$dir = '/files';
-		if (!$folder->nodeExists($dir)) {
+		try {
+			$folder = $folder->get($dir);
+		} catch (NotFoundException $e) {
 			$folder = $folder->newFolder($dir);
 			\OC_Util::copySkeleton($userId, $folder);
-		} else {
-			$folder = $folder->get($dir);
 		}
 
 		return $folder;
