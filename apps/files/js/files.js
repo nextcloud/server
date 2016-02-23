4e5f37a05dc3c97d6fd1a122dafd25630f809ecf
Erik Pellikka <erik@pellikka.org>
2016-02-23 09:40:41 -0500

    code style fix

--- apps/files/js/files.js
+++ apps/files/js/files.js
@@ -408,18 +408,10 @@ var dragOptions={
 		var bottom = $(window).innerHeight() - scrollArea;
 		var top = $(window).scrollTop() + scrollArea;
 		if (event.pageY < top){
-			
-			$(scrollingArea).scrollTop(
-				currentScrollTop -= 10
-				);
-			
+			$(scrollingArea).scrollTop(currentScrollTop - 10);
 		}
-		else if (event.pageY > bottom) {
-			
-			$(scrollingArea).scrollTop(
-				currentScrollTop += 10
-				);
-			
+			else if (event.pageY > bottom) {
+			$(scrollingArea).scrollTop(currentScrollTop + 10);
 		}
 
 	}
