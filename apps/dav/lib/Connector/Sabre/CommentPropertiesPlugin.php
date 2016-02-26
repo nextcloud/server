2b30136ae9286026af4c8be8f0311f39bbedff06
Arthur Schiwon <blizzz@owncloud.com>
2016-02-27 00:59:46 +0100

    ensure comments-href returns a value also when propfind is done against remote.php/files

--- apps/dav/lib/Connector/Sabre/CommentPropertiesPlugin.php
+++ apps/dav/lib/Connector/Sabre/CommentPropertiesPlugin.php
@@ -99,13 +99,13 @@ class CommentPropertiesPlugin extends ServerPlugin {
 	 */
 	public function getCommentsLink(Node $node) {
 		$href =  $this->server->getBaseUri();
-		$entryPoint = strrpos($href, '/webdav/');
+		$entryPoint = strpos($href, '/remote.php/');
 		if($entryPoint === false) {
 			// in case we end up somewhere else, unexpectedly.
 			return null;
 		}
-		$href = substr_replace($href, '/dav/', $entryPoint);
-		$href .= 'comments/files/' . rawurldecode($node->getId());
+		$commentsPart = 'dav/comments/files/' . rawurldecode($node->getId());
+		$href = substr_replace($href, $commentsPart, $entryPoint + strlen('/remote.php/'));
 		return $href;
 	}
 
