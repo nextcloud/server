12e006c1f5266a7f3dfbed3e9dbd3f22825e5712
Morris Jobke <hey@morrisjobke.de>
2016-03-04 15:42:35 +0100

    Run cleanup of expired DB file locks to background job
    * fixes #22819
    
    The old way fired a DELETE statement on each destruction of the
    DBLockingProvider. Which could cause a lot of queries. It's enough
    to run this every 5 minutes in a background job, which in the end
    could result in file locks that exists 5 minutes longer - in the
    worst case and for not properly released locks.
    
    This makes the DB based locking a lot more performant and could
    result in a similar performance to the Redis based locking provider.

---
+++ apps/files/lib/backgroundjob/cleanupfilelocks.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author Morris Jobke <hey@morrisjobke.de>
+ *
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Files\BackgroundJob;
+
+use OC\BackgroundJob\TimedJob;
+use OC\Lock\DBLockingProvider;
+
+/**
+ * Clean up all file locks that are expired for the DB file locking provider
+ */
+class CleanupFileLocks extends TimedJob {
+
+	/**
+	 * Default interval in minutes
+	 *
+	 * @var int $defaultIntervalMin
+	 **/
+	protected $defaultIntervalMin = 5;
+
+	/**
+	 * sets the correct interval for this timed job
+	 */
+	public function __construct() {
+		$this->interval = $this->defaultIntervalMin * 60;
+	}
+
+	/**
+	 * Makes the background job do its work
+	 *
+	 * @param array $argument unused argument
+	 */
+	public function run($argument) {
+		$lockingProvider = \OC::$server->getLockingProvider();
+		if($lockingProvider instanceof DBLockingProvider) {
+			$lockingProvider->cleanExpiredLocks();
+		}
+	}
+}
