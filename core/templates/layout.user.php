7a45f05ed526cd0cf14c701c8e96aa675a2cc82b
Daniel Aleksandersen <code@daniel.priv.no>
2016-03-08 21:09:34 +0100

    Stupid clients only literally understand rel="icon"
    rel="shortcut icon" hasn’t been relevant in years, isn’t in any
    standards, and causes problems for simple pattern matching clients.
    https://www.w3.org/TR/html/links.html#linkTypes

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -19,7 +19,7 @@
 		<meta name="apple-mobile-web-app-title" content="<?php p((!empty($_['application']) && $_['appid']!='files')? $_['application']:'ownCloud'); ?>">
 		<meta name="mobile-web-app-capable" content="yes">
 		<meta name="theme-color" content="<?php p($theme->getMailHeaderColor()); ?>">
-		<link rel="shortcut icon" href="<?php print_unescaped(image_path($_['appid'], 'favicon.ico')); /* IE11+ supports png */ ?>">
+		<link rel="icon" href="<?php print_unescaped(image_path($_['appid'], 'favicon.ico')); /* IE11+ supports png */ ?>">
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path($_['appid'], 'favicon-touch.png')); ?>">
 		<link rel="mask-icon" sizes="any" href="<?php print_unescaped(image_path($_['appid'], 'favicon-mask.svg')); ?>" color="#1d2d44">
 		<?php foreach($_['cssfiles'] as $cssfile): ?>
