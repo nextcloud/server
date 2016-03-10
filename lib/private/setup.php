3e8decf5e9fbbaf27ef7ce59383e10244f7a6da5
Lukas Reschke <lukas@owncloud.com>
2016-03-10 22:18:53 +0100

    Improved rewrite rule
    As per https://github.com/owncloud/core/issues/23098 and https://github.com/owncloud/core/issues/23117

--- lib/private/setup.php
+++ lib/private/setup.php
@@ -423,7 +423,7 @@ class Setup {
 		// Add rewrite base
 		$webRoot = !empty(\OC::$WEBROOT) ? \OC::$WEBROOT : '/';
 		$content .= "\n<IfModule mod_rewrite.c>";
-		$content .= "\n  RewriteRule .* index.php [PT,E=PATH_INFO:$1]";
+		$content .= "\n  RewriteRule . index.php [PT,E=PATH_INFO:$1]";
 		$content .= "\n  RewriteBase ".$webRoot;
 		$content .= "\n  <IfModule mod_env.c>";
 		$content .= "\n    SetEnv front_controller_active true";
