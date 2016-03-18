a6c921267e00d0fb5021e8bdbd4d202931d7a58a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-18 01:59:00 -0400

    [tx-robot] updated from transifex

--- apps/files/l10n/fil.js
+++ apps/files/l10n/fil.js
@@ -1,9 +1,6 @@
 OC.L10N.register(
     "files",
     {
-    "_%n folder_::_%n folders_" : ["",""],
-    "_%n file_::_%n files_" : ["",""],
-    "_Uploading %n file_::_Uploading %n files_" : ["",""],
-    "_matches '{filter}'_::_match '{filter}'_" : ["",""]
+    "New folder" : "Bagong folder"
 },
 "nplurals=2; plural=(n > 1);");
