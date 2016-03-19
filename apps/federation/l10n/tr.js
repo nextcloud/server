9c053ed46521467efc1838e806a5481e8390eac7
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-19 01:55:39 -0400

    [tx-robot] updated from transifex

---
+++ apps/federation/l10n/tr.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "federation",
+    {
+    "Server added to the list of trusted ownClouds" : "Sunucu güvenilen ownCloud sunucuları listesine eklendi",
+    "Server is already in the list of trusted servers." : "Sunucu zaten güvenilen sunucu listesine eklenmiş.",
+    "No ownCloud server found" : "Hiçbir ownCloud sunucusu bulunamadı",
+    "Could not add server" : "Sunucu eklenemedi",
+    "Trusted ownCloud Servers" : "Güvenilen ownCloud Sunucuları",
+    "+ Add ownCloud server" : "ownCloud sunucusu ekle",
+    "ownCloud Server" : "ownCloud Sunucusu"
+},
+"nplurals=2; plural=(n > 1);");
