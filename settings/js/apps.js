7365bd798a30b8ac584080532bf97970a34bb6c3
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-04-11 16:48:07 +0200

    Show tooltip if the app is downloaded from a remote server fixes #14405

--- settings/js/apps.js
+++ settings/js/apps.js
@@ -119,6 +119,8 @@ OC.Settings.Apps = OC.Settings.Apps || {
 					$('#apps-list-empty').removeClass('hidden').find('h2').text(t('settings', 'No apps found for your version'));
 				}
 
+				$('.enable.needs-download').tipsy({fallback: t('settings', 'The app will be downloaded from the app store')});
+
 				$('.app-level .official').tipsy({fallback: t('settings', 'Official apps are developed by and within the ownCloud community. They offer functionality central to ownCloud and are ready for production use.')});
 				$('.app-level .approved').tipsy({fallback: t('settings', 'Approved apps are developed by trusted developers and have passed a cursory security check. They are actively maintained in an open code repository and their maintainers deem them to be stable for casual to normal use.')});
 				$('.app-level .experimental').tipsy({fallback: t('settings', 'This app is not checked for security issues and is new or known to be unstable. Install at your own risk.')});
