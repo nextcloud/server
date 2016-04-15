SteKoe/core

Adding https://github.com/icewind1991/files_external_ftp

apps/files_external_ftp/tests/travis/install.sh
@@ -1,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -qq
+udo apt-get install -yqq bc $FTPD
+sudo /etc/init.d/$FTPD restart
+if [ "$FTPD" = "vsftpd" ]; then echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf; echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf; fi
+sudo /etc/init.d/$FTPD restart
+
+pass=$(perl -e 'print crypt("test", "password")')
+sudo useradd -m -p $pass test

nextcloud/server

apps/files_external_ftp/tests/travis/install.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+
+FTPD=$1
+
+sudo apt-get update -qq
+sudo apt-get install -yqq bc $FTPD
+sudo /etc/init.d/$FTPD restart
+if [ "$FTPD" = "vsftpd" ]; then echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf; echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf; fi
+sudo /etc/init.d/$FTPD restart
+
+pass=$(perl -e 'print crypt("test", "password")')
+sudo useradd -m -p $pass test