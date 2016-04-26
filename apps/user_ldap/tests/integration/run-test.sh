d51447f43b7490824c4d161e1f3160e536d36ac0
Arthur Schiwon <blizzz@owncloud.com>
2016-04-27 00:58:10 +0200

    On exit code 0, ensure that test run successfully.
    It might be just as well, that ownCloud caught an error making php exit
    with 0.

--- apps/user_ldap/tests/integration/run-test.sh
+++ apps/user_ldap/tests/integration/run-test.sh
@@ -13,9 +13,19 @@ fi
 
 # sleep is necessary, otherwise the LDAP server cannot be connected to, yet.
 WEBUSER=`stat -c %U ../../../../config/config.php`
+OUT=""
 setup-scripts/start.sh > /dev/null && sleep 5 && \
     sudo -u "$WEBUSER" ../../../../occ app:enable user_ldap && \
-    sudo -u "$WEBUSER" php -f "$TESTSCRIPT"
+    OUT=$(sudo -u "$WEBUSER" php -f "$TESTSCRIPT")
+    echo "$OUT"
 CODE=$?
 setup-scripts/stop.sh > /dev/null
+if [ $CODE -eq 0 ]; then
+    # ownCloud likes to catch errors, resulting in an exit code of 0.
+    # so we add an extra check whether tests really succeeded
+    if [[ $OUT != *"Tests succeeded"* ]]; then
+        echo "a superordinate error occurred – please check the owncloud.log"
+        CODE=1
+    fi
+fi
 exit ${CODE}
