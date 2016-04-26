03a5bd6b88714ac896797accb2e068968e598b37
Arthur Schiwon <blizzz@owncloud.com>
2016-04-27 00:38:26 +0200

    enable user_ldap on test run, and execute scripts as web user (config file owner)

--- apps/user_ldap/tests/integration/run-test.sh
+++ apps/user_ldap/tests/integration/run-test.sh
@@ -11,9 +11,11 @@ if [ ! -e "$TESTSCRIPT" ] ; then
     exit
 fi
 
-
 # sleep is necessary, otherwise the LDAP server cannot be connected to, yet.
-setup-scripts/start.sh > /dev/null && sleep 5 && php -f "$TESTSCRIPT"
+WEBUSER=`stat -c %U ../../../../config/config.php`
+setup-scripts/start.sh > /dev/null && sleep 5 && \
+    sudo -u "$WEBUSER" ../../../../occ app:enable user_ldap && \
+    sudo -u "$WEBUSER" php -f "$TESTSCRIPT"
 CODE=$?
 setup-scripts/stop.sh > /dev/null
 exit ${CODE}
