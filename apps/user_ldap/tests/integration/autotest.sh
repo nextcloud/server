0c656f59a825cea4cacd87c31ce5b2cb8e5e3898
Arthur Schiwon <blizzz@owncloud.com>
2016-04-26 08:21:00 +0200

    Fetch start.sh, stop.sh and config.php if files are missing

--- apps/user_ldap/tests/integration/autotest.sh
+++ apps/user_ldap/tests/integration/autotest.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+SETUP_SCRIPTS_DIR="setup-scripts"
+
 cd "$(dirname "$0")"
+
+for SCRIPT in start.sh stop.sh config.php
+do
+    if [ ! -e "$SETUP_SCRIPTS_DIR/$SCRIPT" ] ; then
+        wget "https://raw.githubusercontent.com/owncloud/administration/master/ldap-testing/$SCRIPT" \
+            -O "$SETUP_SCRIPTS_DIR/$SCRIPT"
+        if [[ "$SCRIPT" == *.sh ]] ; then
+            chmod +x "$SETUP_SCRIPTS_DIR/$SCRIPT"
+        fi
+    fi
+done
 
 TESTSCRIPTS=`find ./lib/ -name "*.php"`
 
