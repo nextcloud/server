fb19a733b9b6f3adabb62c172295341fd0839a2d
Joas Schilling <nickvergessen@owncloud.com>
2016-04-26 12:47:57 +0200

    Register a ActivityTabView plugin

---
+++ apps/comments/js/activitytabviewplugin.js
@@ -0,0 +1,57 @@
+/*
+ * @author Joas Schilling <nickvergessen@owncloud.com>
+ * Copyright (c) 2016
+ *
+ * This file is licensed under the Affero General Public License version 3
+ * or later.
+ *
+ * See the COPYING-README file.
+ */
+
+(function() {
+	OCA.Comments.ActivityTabViewPlugin = {
+
+		/**
+		 * Prepare activity for display
+		 *
+		 * @param {OCA.Activity.ActivityModel} model for this activity
+		 * @param {jQuery} $el jQuery handle for this activity
+		 * @param {string} view The view that displayes this activity
+		 */
+		prepareModelForDisplay: function (model, $el, view) {
+			if (model.get('app') !== 'comments' || model.get('type') !== 'comments') {
+				return;
+			}
+
+			if (view === 'ActivityTabView') {
+				$el.addClass('comment');
+				if (this._isLong(model.get('message_prepared'))) {
+					$el.addClass('collapsed');
+					var $overlay = $('<div>').addClass('message-overlay');
+					$el.find('.activitymessage').after($overlay);
+					$el.on('click', this._onClickCollapsedComment);
+				}
+			}
+		},
+
+		_onClickCollapsedComment: function(ev) {
+			var $row = $(ev.target);
+			if (!$row.is('.comment')) {
+				$row = $row.closest('.comment');
+			}
+			$row.removeClass('collapsed');
+		},
+
+		/**
+		 * Returns whether the given message is long and needs
+		 * collapsing
+		 */
+		_isLong: function(message) {
+			return message.length > 250 || (message.match(/\n/g) || []).length > 1;
+		}
+	};
+
+
+})();
+
+OC.Plugins.register('OCA.Activity.Plugins', OCA.Comments.ActivityTabViewPlugin);
