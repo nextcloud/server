18704a9fc879b13322d6ddc6f8e0ef331415a5c3
Christoph Wurst <christoph@owncloud.com>
2016-04-27 11:10:56 +0200

    increase version number to trigger db migration

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version = array(9, 1, 0, 0);
+$OC_Version = array(9, 1, 0, 1);
 
 // The human readable string
 $OC_VersionString = '9.1.0 pre alpha';
