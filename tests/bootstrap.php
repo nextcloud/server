54250ed06554cb59683f5c03a1e4529660febc52
Joas Schilling <nickvergessen@owncloud.com>
2016-05-19 15:49:27 +0200

    Add PSR-4 loading for Test\ and Tests\

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,8 @@ if ($configDir) {
 
 require_once __DIR__ . '/../lib/base.php';
 
+\OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
+\OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/', true);
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
 // load all enabled apps
