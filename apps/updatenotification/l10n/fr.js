c870e7754696d179a9ba8f84a1c1379e5ad1a274
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-29 01:55:25 -0400

    [tx-robot] updated from transifex

--- apps/updatenotification/l10n/fr.js
+++ apps/updatenotification/l10n/fr.js
@@ -1,14 +1,17 @@
 OC.L10N.register(
     "updatenotification",
     {
+    "Update notifications" : "Notifications de mises à jour",
     "{version} is available. Get more information on how to update." : "La version {version} est disponible. Cliquez ici pour plus d'informations sur comment mettre à jour.",
     "Updated channel" : "Canal à jour",
+    "Update for %1$s to version %2$s is available." : "Une mise à jour de %1$s vers la version %2$s est disponible.",
     "Updater" : "Mises à jour",
     "A new version is available: %s" : "Une nouvelle version est disponible : %s",
     "Open updater" : "Ouvrir le système de mise à jour",
     "Your version is up to date." : "Votre version est à jour.",
     "Checked on %s" : "Vérifié le %s",
     "Update channel:" : "Canal de mise à jour :",
-    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Vous pouvez à tout moment mettre à jour vers une version plus récente ou un canal expérimental. Cependant vous ne pourrez jamais revenir à un canal plus stable."
+    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Vous pouvez à tout moment mettre à jour vers une version plus récente ou un canal expérimental. Cependant vous ne pourrez jamais revenir à un canal plus stable.",
+    "Notify members of the following groups about available updates:" : "Notifier les membres des groupes suivants des mises à jours disponibles :"
 },
 "nplurals=2; plural=(n > 1);");
--- apps/updatenotification/l10n/pt_BR.js
+++ apps/updatenotification/l10n/pt_BR.js
@@ -1,14 +1,19 @@
 OC.L10N.register(
     "updatenotification",
     {
+    "Update notifications" : "Notificações de atualização",
     "{version} is available. Get more information on how to update." : "{version} está disponível. Obtenha mais informações sobre como atualizar.",
     "Updated channel" : "Canal atualizado",
+    "ownCloud core" : "Núcleo ownCloud",
+    "Update for %1$s to version %2$s is available." : "Atualização de %1$s para versão %2$s está disponível.",
     "Updater" : "Atualizador",
     "A new version is available: %s" : "Uma nova versão está disponível: %s",
     "Open updater" : "Abrir o atualizador",
     "Your version is up to date." : "Sua versão está atualizada",
     "Checked on %s" : "Verificada em %s",
     "Update channel:" : "Atualização do canal:",
-    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Você pode sempre atualizar para uma versão / canal experimental mais recente. Mas você nunca pode voltar para um canal mais estável."
+    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Você pode sempre atualizar para uma versão / canal experimental mais recente. Mas você nunca pode voltar para um canal mais estável.",
+    "Notify members of the following groups about available updates:" : "Notificar membros dos seguintes grupos sobre atualizações disponíveis.",
+    "Only notification for app updates are available, because the selected update channel for ownCloud itself does not allow notifications." : "Somente notificação para atualizações de aplicativos estão disponíveis, porque o canal de atualização selecionada para  ownCloud não permite notificações."
 },
 "nplurals=2; plural=(n > 1);");
