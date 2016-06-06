20d250a674b1d89cafb324cd7157b77bedc2e5fd
Bjoern Schiessle <bjoern@schiessle.org>
2016-06-06 17:40:38 +0200

    initial commit for the theming app

---
+++ apps/theming/templates/settings-admin.php
@@ -0,0 +1,11 @@
+<?php
+/** @var array $_ */
+/** @var OC_L10N $l */
+script('theming', 'settings-admin');
+style('theming', 'settings-admin')
+?>
+<div id="ncTheming" class="section">
+	<h2><?php p($l->t('Theming')); ?></h2>
+        Hello World
+</div>
+
