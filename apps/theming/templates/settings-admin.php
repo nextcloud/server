55f3a659c4304649d2a6f70513eb3e874ebb18bd
Jan-Christoph Borchardt <hey@jancborchardt.net>
2016-06-07 19:09:49 +0200

    basic information architecture for the theming app

--- apps/theming/templates/settings-admin.php
+++ apps/theming/templates/settings-admin.php
@@ -4,8 +4,22 @@
 script('theming', 'settings-admin');
 style('theming', 'settings-admin')
 ?>
-<div id="ncTheming" class="section">
+<div id="theming" class="section">
 	<h2><?php p($l->t('Theming')); ?></h2>
-        Hello World
-</div>
 
+	<p>
+		<input class="theming-name" type="text" placeholder="<?php p($l->t('Name')); ?>"></input>
+	</p>
+	<p>
+		<input class="theming-address" type="text" placeholder="<?php p($l->t('Web address https://…')); ?>"></input>
+	</p>
+	<p>
+		<input class="theming-slogan" type="text" placeholder="<?php p($l->t('Slogan')); ?>"></input>
+	</p>
+	<p>
+		<input class="theming-color" type="text" placeholder="<?php p($l->t('Color #0082c9')); ?>"></input>
+	</p>
+	<p>
+		<input class="theming-logo" type="text" placeholder="<?php p($l->t('Logo')); ?>"></input>
+	</p>
+</div>
