957d35f5907e63ebab9136d169db2c799d8936aa
Morris Jobke <hey@morrisjobke.de>
2016-06-15 15:20:21 +0200

    Update strengthify to 0.5.1

--- core/vendor/strengthify/jquery.strengthify.js
+++ core/vendor/strengthify/jquery.strengthify.js
@@ -2,14 +2,16 @@
  * Strengthify - show the weakness of a password (uses zxcvbn for this)
  * https://github.com/MorrisJobke/strengthify
  *
- * Version: 0.4.2
- * Author: Morris Jobke (github.com/MorrisJobke)
+ * Version: 0.5.1
+ * Author: Morris Jobke (github.com/MorrisJobke) - original
+ *         Eve Ragins @ Eve Corp (github.com/eve-corp)
+ *
  *
  * License:
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2015 Morris Jobke <morris.jobke@gmail.com>
+ * Copyright (c) 2013-2016 Morris Jobke <morris.jobke@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -30,108 +32,183 @@
  */
 
 /* global jQuery */
-(function ($) {
-	$.fn.strengthify = function(paramOptions) {
-		var me = this,
-			defaults = {
-				zxcvbn: 'zxcvbn/zxcvbn.js',
-				titles: [
-					'Weakest',
-					'Weak',
-					'So-so',
-					'Good',
-					'Perfect'
-				]
-			},
-			options = $.extend(defaults, paramOptions),
-			drawStrengthify = function() {
-				var password = $(me).val(),
-					// hide strengthigy if no input is provided
-					opacity = (password === '') ? 0 : 1,
-					// calculate result
-					result = zxcvbn(password),
-					css = '',
-					// cache jQuery selections
-					$container = $('.strengthify-container'),
-					$wrapper = $('.strengthify-wrapper');
+(function($) {
+    $.fn.strengthify = function(paramOptions) {
+        "use strict";
 
-				$wrapper.children().css(
-					'opacity',
-					opacity
-				).css(
-					'-ms-filter',
-					'"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + opacity * 100 + ')"'
-				);
+        var defaults = {
+            zxcvbn: 'zxcvbn/zxcvbn.js',
+            titles: [
+                'Weakest',
+                'Weak',
+                'So-so',
+                'Good',
+                'Perfect'
+            ],
+            tilesOptions:{
+              tooltip: true,
+              element: false
+            },
+            drawTitles: false,
+            drawMessage: false,
+            drawBars: true
+        };
 
-				// style strengthify bar
-				// possible scores: 0-4
-				switch(result.score) {
-					case 0:
-					case 1:
-						css = 'password-bad';
-						break;
-					case 2:
-						css = 'password-medium';
-						break;
-					case 3:
-					case 4:
-						css = 'password-good';
-						break;
-				}
+        return this.each(function() {
+            var options = $.extend(defaults, paramOptions);
 
-				$container
-					.attr('class', css + ' strengthify-container')
-					// possible scores: 0-4
-					.css(
-						'width',
-						// if score is '0' it will be changed to '1' to
-						// not hide strengthify if the password is extremely weak
-						((result.score === 0 ? 1 : result.score) * 25) + '%'
-					);
+            if (!options.drawTitles
+                && !options.drawMessage
+                && !options.drawBars)
+                console.warn("expect at least one of 'drawTitles', 'drawMessage', or 'drawBars' to be true");
 
-				// set a title for the wrapper
-				$wrapper.attr(
-					'title',
-					options.titles[result.score]
-				).tooltip({
-					placement: 'bottom',
-					trigger: 'manual',
-				}).tooltip(
-					'show'
-				);
+            function getWrapperFor(id) {
+                return $('div[data-strengthifyFor="' + id + '"]');
+            };
 
-				if(opacity === 0) {
-					$wrapper.tooltip(
-						'hide'
-					);
-				}
+            function drawStrengthify() {
+                var password = $(this).val(),
+                    elemId = $(this).attr('id'),
+                    // hide strengthify if no input is provided
+                    opacity = (password === '') ? 0 : 1,
+                    // calculate result
+                    result = zxcvbn(password),
+                    // setup some vars for later
+                    css = '',
+                    bsLevel = '',
+                    message = '',
+                    // cache jQuery selections
+                    $wrapper = getWrapperFor(elemId),
+                    $container = $wrapper.find('.strengthify-container'),
+                    $message = $wrapper.find('[data-strengthifyMessage]');
 
-				// reset state for empty string password
-				if(password === '') {
-					$container.css('width', 0);
-				}
 
-			};
+                $wrapper.children()
+                    .css('opacity', opacity)
+                    .css('-ms-filter',
+                    '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + opacity * 100 + ')"'
+                    );
 
-		// add elements
-		$('.strengthify-wrapper')
-			.append('<div class="strengthify-bg" />')
-			.append('<div class="strengthify-container" />')
-			.append('<div class="strengthify-separator" style="left: 25%" />')
-			.append('<div class="strengthify-separator" style="left: 50%" />')
-			.append('<div class="strengthify-separator" style="left: 75%" />');
+                // style strengthify bar
+                // possible scores: 0-4
+                switch (result.score) {
+                    case 0:
+                    case 1:
+                        css = 'password-bad';
+                        bsLevel = 'danger';
+                        message = result.feedback ? result.feedback.suggestions.join('<br/>') : "";
+                        break;
+                    case 2:
+                        bsLevel = 'warning';
+                        message = result.feedback ? result.feedback.suggestions.join('<br/>') : "";
+                        css = 'password-medium';
+                        break;
+                    case 3:
+                        css = 'password-good';
+                        bsLevel = 'info';
+                        message = "Getting better.";
+                    case 4:
+                        css = 'password-good';
+                        bsLevel = 'success';
+                        message = "Looks good.";
+                        break;
+                }
 
-		me.parents().on('scroll', drawStrengthify);
+                if ($message) {
+                    $message.removeAttr('class');
+                    $message.addClass('bg-' + bsLevel);
 
-		$.ajax({
-			cache: true,
-			dataType: 'script',
-			url: options.zxcvbn
-		}).done(function() {
-			me.bind('keyup input change', drawStrengthify);
-		});
+                    // reset state for empty string password
+                    if (password === '') {
+                        message = '';
+                    }
+                    $message.html(message);
+                }
+                if ($container) {
+                    $container
+                        .attr('class', css + ' strengthify-container')
+                        // possible scores: 0-4
+                        .css(
+                        'width',
+                        // if score is '0' it will be changed to '1' to
+                        // not hide strengthify if the password is extremely weak
+                        ((result.score === 0 ? 1 : result.score) * 25) + '%'
+                        );
 
-		return me;
-	};
+                    // reset state for empty string password
+                    if (password === '') {
+                        $container.css('width', 0);
+                    }
+                }
 
-}(jQuery));
+                if (options.drawTitles) {
+                    // set a title for the wrapper
+                    if(options.tilesOptions.tooltip){
+                        $wrapper.attr(
+                            'title',
+                            options.titles[result.score]
+                        ).tooltip({
+                            placement: 'bottom',
+                            trigger: 'manual',
+                        }).tooltip(
+                            'fixTitle'
+                        ).tooltip(
+                            'show'
+                        );
+
+                        if (opacity === 0) {
+                            $wrapper.tooltip(
+                                'hide'
+                            );
+                        }
+                    }
+
+                    if(options.tilesOptions.element){
+                        $wrapper.find(".strengthify-tiles").text(options.titles[result.score]);
+                    }
+                }
+            };
+
+            function init() {
+                var $elem = $(this),
+                    elemId = $elem.attr('id');
+                var drawSelf = drawStrengthify.bind(this);
+
+                // add elements
+                $elem.after('<div class="strengthify-wrapper" data-strengthifyFor="' + $elem.attr('id') + '"></div>');
+
+                if (options.drawBars) {
+                    getWrapperFor(elemId)
+                        .append('<div class="strengthify-bg" />')
+                        .append('<div class="strengthify-container" />')
+                        .append('<div class="strengthify-separator" style="left: 25%" />')
+                        .append('<div class="strengthify-separator" style="left: 50%" />')
+                        .append('<div class="strengthify-separator" style="left: 75%" />');
+                }
+
+                if (options.drawMessage) {
+                    getWrapperFor(elemId).append('<div data-strengthifyMessage></div>');
+                }
+
+                if (options.drawTitles && options.tilesOptions) {
+                    getWrapperFor(elemId).append('<div class="strengthify-tiles"></div>');
+                }
+
+                $elem.parent().on('scroll', drawSelf);
+
+                $.ajax({
+                    cache: true,
+                    dataType: 'script',
+                    url: options.zxcvbn
+                }).done(function() {
+                    $elem.bind('keyup input change', drawSelf);
+                });
+            };
+
+            init.call(this);
+
+            //return me;
+        });
+    };
+
+} (jQuery));
