bd13353c4d246398d4ed31c6696338f8032221ae
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-06-20 01:54:50 -0400

    [tx-robot] updated from transifex

--- apps/comments/l10n/pl.js
+++ apps/comments/l10n/pl.js
@@ -14,7 +14,10 @@ OC.L10N.register(
     "Allowed characters {count} of {max}" : "Dozwolone znaki {count} z {max}",
     "{count} unread comments" : "{count} nieprzeczytanych komentarzy",
     "Comment" : "Komentarz",
+    "<strong>Comments</strong> for files <em>(always listed in stream)</em>" : "<strong>Komentarze</strong> dla plików <em>(zawsze wypisane w strumieniu)</em>",
+    "You commented" : "Skomentowałeś/łaś",
     "%1$s commented" : "%1$s skomentował",
+    "You commented on %2$s" : "Skomentowałeś/łaś %2$s",
     "%1$s commented on %2$s" : "%1$s skomentował %2$s"
 },
 "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
