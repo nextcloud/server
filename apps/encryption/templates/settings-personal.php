e7e0dc608b21c9ef962a8a2f705de775ce8b87b1
Morris Jobke <hey@morrisjobke.de>
2016-06-20 09:08:21 +0200

    fix strings

--- apps/encryption/templates/settings-personal.php
+++ apps/encryption/templates/settings-personal.php
@@ -5,7 +5,7 @@ script('encryption', 'settings-personal');
 script('core', 'multiselect');
 ?>
 <form id="ocDefaultEncryptionModule" class="section">
-	<h2><?php p($l->t('ownCloud basic encryption module')); ?></h2>
+	<h2><?php p($l->t('basic encryption module')); ?></h2>
 
 	<?php if ($_["initialized"] === \OCA\Encryption\Session::NOT_INITIALIZED ): ?>
 
