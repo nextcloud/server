981c38f6d91be5a6de0ad319028afc3a4e931a2e
Thomas Citharel <tcit@tcit.fr>
2016-07-06 12:42:32 +0200

    Remove unnecessary implementation

--- apps/dav/lib/CalDAV/Calendar.php
+++ apps/dav/lib/CalDAV/Calendar.php
@@ -24,14 +24,13 @@
 namespace OCA\DAV\CalDAV;
 
 use OCA\DAV\DAV\Sharing\IShareable;
-use Sabre\CalDAV\IShareableCalendar;
 use OCP\IL10N;
 use Sabre\CalDAV\Backend\BackendInterface;
 use Sabre\DAV\Exception\Forbidden;
 use Sabre\DAV\Exception\NotFound;
 use Sabre\DAV\PropPatch;
 
-class Calendar extends \Sabre\CalDAV\Calendar implements IShareable, IShareableCalendar {
+class Calendar extends \Sabre\CalDAV\Calendar implements IShareable {
 
 	public function __construct(BackendInterface $caldavBackend, $calendarInfo, IL10N $l10n) {
 		parent::__construct($caldavBackend, $calendarInfo);
