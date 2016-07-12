9ebe0c8d640a555c5d82da3832ea8da0dd22ea67
Julius Haertl <jus@bitgrid.net>
2016-07-12 21:35:43 +0200

    Colorize checkboxes depending on theming color

--- apps/theming/lib/controller/themingcontroller.php
+++ apps/theming/lib/controller/themingcontroller.php
@@ -219,6 +219,14 @@ class ThemingController extends Controller {
 				'#body-user #header,#body-settings #header,#body-public #header,#body-login,.searchbox input[type="search"]:focus,.searchbox input[type="search"]:active,.searchbox input[type="search"]:valid {background-color: %s}',
 				$color
 			);
+			$responseCss .= sprintf('
+				input[type="checkbox"].checkbox:checked + label:before {
+					background-image:url(/core/img/actions/checkmark-white.svg);
+					background-color: %s; background-position: center center; background-size:contain;
+					width:12px; height:12px; padding:0; margin:1px 6px 7px 2px;
+        		}',
+				$color
+			);
 		}
 		$logo = $this->config->getAppValue($this->appName, 'logoMime');
 		if($logo !== '') {
