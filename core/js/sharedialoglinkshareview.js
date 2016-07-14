1df65e987229ecf1044e01743b0a33df04001e53
Joas Schilling <coding@schilljs.com>
2016-07-14 10:25:53 +0200

    Don't need update permissions on hidden files

--- core/js/sharedialoglinkshareview.js
+++ core/js/sharedialoglinkshareview.js
@@ -229,7 +229,7 @@
 
 			var permissions = OC.PERMISSION_UPDATE | OC.PERMISSION_CREATE | OC.PERMISSION_READ | OC.PERMISSION_DELETE;
 			if ($checkbox.is(':checked')) {
-				permissions = OC.PERMISSION_UPDATE | OC.PERMISSION_CREATE;
+				permissions = OC.PERMISSION_CREATE;
 			}
 
 			this.model.saveLinkShare({
