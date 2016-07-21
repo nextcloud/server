9c6c902220e6609eeccf42a7044631d73080e619
Joas Schilling <coding@schilljs.com>
2016-07-21 12:26:48 +0200

    Make the changes from https://github.com/nextcloud/documentation/commit/6ccd2c5678b85d35a8673d784bef5c6c89e7ecb9 persistant

--- config/config.sample.php
+++ config/config.sample.php
@@ -435,7 +435,7 @@ $CONFIG = array(
  * Both minimum and maximum times can be set together to explicitly define
  * file and folder deletion. For migration purposes, this setting is installed
  * initially set to "auto", which is equivalent to the default setting in
- * ownCloud 8.1 and before.
+ * Nextloud.
  *
  * Available values:
  *
@@ -475,7 +475,7 @@ $CONFIG = array(
  * Both minimum and maximum times can be set together to explicitly define
  * version deletion. For migration purposes, this setting is installed
  * initially set to "auto", which is equivalent to the default setting in
- * ownCloud 8.1 and before. 
+ * Nextloud.
  *
  * Available values:
  *
@@ -659,7 +659,7 @@ $CONFIG = array(
 'customclient_android' =>
 	'https://play.google.com/store/apps/details?id=com.nextcloud.client',
 'customclient_ios' =>
-	'https://itunes.apple.com/us/app/owncloud/id543672169?mt=8',
+	'https://itunes.apple.com/us/app/nextcloud/id1125420102?mt=8',
 
 /**
  * Apps
@@ -1136,12 +1136,12 @@ $CONFIG = array(
 'cipher' => 'AES-256-CFB',
 
 /**
- * The minimum ownCloud desktop client version that will be allowed to sync with
+ * The minimum Nextcloud desktop client version that will be allowed to sync with
  * this server instance. All connections made from earlier clients will be denied
- * by the server. Defaults to the minimum officially supported ownCloud desktop
+ * by the server. Defaults to the minimum officially supported Nextcloud desktop
  * clientversion at the time of release of this server version.
  *
- * When changing this, note that older unsupported versions of the ownCloud desktop
+ * When changing this, note that older unsupported versions of the Nextcloud desktop
  * client may not function as expected, and could lead to permanent data loss for
  * clients or other unexpected results.
  */
