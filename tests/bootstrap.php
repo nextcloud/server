b247e4e5ff33c67785ca010e12ed372b5c0d7f8c
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-07-25 09:26:13 +0200

    [stable9.1] Especially with coverage enabled execution times are pretty high (#25517) (#25595)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,9 @@ if ($configDir) {
 
 require_once __DIR__ . '/../lib/base.php';
 
+// especially with code coverage it will require some more time
+set_time_limit(0);
+
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
 \OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/', true);
 
