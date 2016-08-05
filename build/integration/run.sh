f72fea8d0ec5ff00a31c81a17059547becb2c37e
Sergio Bertolin <sbertolin@solidgear.es>
2016-08-05 13:13:49 +0000

    Modified run.sh to mount a local storage

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -36,11 +36,19 @@ echo $PHPPID_FED
 export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 export TEST_SERVER_FED_URL="http://localhost:$PORT_FED/ocs/"
 
+APACHE_USER=`ps -ef | egrep '(httpd|apache2|apache)' | grep -v \`whoami\` | grep -v root | head -n1 | awk '{print $1}'`
+
+OUTPUT_CREATE_STORAGE=`sudo -u $APACHE_USER ../../occ files_external:create local_storage local null::null -c datadir=./build/integration/local_storage` 
+
+ID_STORAGE=`echo $OUTPUT_CREATE_STORAGE | awk {'print $5'}`
+
 vendor/bin/behat -f junit -f pretty $SCENARIO_TO_RUN
 RESULT=$?
 
 kill $PHPPID
 kill $PHPPID_FED
+
+sudo -u $APACHE_USER ../../occ files_external:delete -y $ID_STORAGE
 
 if [ -z $HIDE_OC_LOGS ]; then
 	tail "../../data/nextcloud.log"
