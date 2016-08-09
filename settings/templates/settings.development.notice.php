3f859d8a3825168a286b7a9dd9ba3b915800d81a
Roeland Jago Douma <roeland@famdouma.nl>
2016-08-09 13:37:25 +0200

    Remove AGPL title

--- settings/templates/settings.development.notice.php
+++ settings/templates/settings.development.notice.php
@@ -1,4 +1,6 @@
-<?php if (OC_Util::getEditionString() === ''): ?>
+<?php
+/** @var \OCP\IL10N $l */
+if (OC_Util::getEditionString() === ''): ?>
 	<p>
 		<?php print_unescaped(str_replace(
 			[
@@ -13,7 +15,7 @@
 				'<a href="https://www.gnu.org/licenses/agpl-3.0.html" target="_blank" rel="noreferrer">',
 				'</a>',
 			],
-			$l->t('Developed by the {communityopen}Nextcloud community{linkclose}, the {githubopen}source code{linkclose} is licensed under the {licenseopen}<abbr title="Affero General Public License">AGPL</abbr>{linkclose}.')
+			$l->t('Developed by the {communityopen}Nextcloud community{linkclose}, the {githubopen}source code{linkclose} is licensed under the {licenseopen}AGPL{linkclose}.')
 		)); ?>
 	</p>
 <?php endif; ?>
