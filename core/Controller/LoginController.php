ed59de57bb305e7c27d5f647b5f635e2ce48cf11
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-08-10 22:48:45 +0200

    [stable9.1] Add missing array element - fixes #25714 (#25732)

--- core/Controller/LoginController.php
+++ core/Controller/LoginController.php
@@ -185,7 +185,7 @@ class LoginController extends Controller {
 		}
 		if ($loginResult === false) {
 			$this->session->set('loginMessages', [
-				['invalidpassword']
+				['invalidpassword'], []
 			]);
 			// Read current user and append if possible - we need to return the unmodified user otherwise we will leak the login name
 			$args = !is_null($user) ? ['user' => $originalUser] : [];
