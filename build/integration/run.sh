b7b0f4780c0e229bd6fea7e0d9f00da0c7c93607
Sergio Bertolin <sbertolin@solidgear.es>
2016-08-10 07:26:35 +0000

    Run as current user

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -36,13 +36,11 @@ echo $PHPPID_FED
 export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 export TEST_SERVER_FED_URL="http://localhost:$PORT_FED/ocs/"
 
-APACHE_USER=`ps -ef | egrep '(httpd|apache2|apache)' | grep -v \`whoami\` | grep -v root | head -n1 | awk '{print $1}'`
-
-OUTPUT_CREATE_STORAGE=`sudo -u $APACHE_USER ../../occ files_external:create local_storage local null::null -c datadir=./build/integration/local_storage` 
+OUTPUT_CREATE_STORAGE=`../../occ files_external:create local_storage local null::null -c datadir=./build/integration/local_storage` 
 
 ID_STORAGE=`echo $OUTPUT_CREATE_STORAGE | awk {'print $5'}`
 
-sudo -u $APACHE_USER ../../occ files_external:option $ID_STORAGE enable_sharing true
+../../occ files_external:option $ID_STORAGE enable_sharing true
 
 vendor/bin/behat -f junit -f pretty $SCENARIO_TO_RUN
 RESULT=$?
@@ -50,7 +48,7 @@ RESULT=$?
 kill $PHPPID
 kill $PHPPID_FED
 
-sudo -u $APACHE_USER ../../occ files_external:delete -y $ID_STORAGE
+../../occ files_external:delete -y $ID_STORAGE
 
 if [ -z $HIDE_OC_LOGS ]; then
 	tail "../../data/nextcloud.log"
