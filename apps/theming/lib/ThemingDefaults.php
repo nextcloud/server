338c7dfc8bfdcadc1c0f5d1db6f9abfea4eb4bea
Julius Haertl <jus@bitgrid.net>
2016-08-10 13:34:16 +0200

    Use IRootFolder for checking image existence

--- apps/theming/lib/ThemingDefaults.php
+++ apps/theming/lib/ThemingDefaults.php
@@ -28,6 +28,7 @@ namespace OCA\Theming;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IURLGenerator;
+use OCP\Files\IRootFolder;
 
 
 class ThemingDefaults extends \OC_Defaults {
@@ -46,6 +47,8 @@ class ThemingDefaults extends \OC_Defaults {
 	private $slogan;
 	/** @var string */
 	private $color;
+	/** @var IRootFolder */
+	private $rootFolder;
 
 	/**
 	 * ThemingDefaults constructor.
@@ -54,16 +57,19 @@ class ThemingDefaults extends \OC_Defaults {
 	 * @param IL10N $l
 	 * @param IURLGenerator $urlGenerator
 	 * @param \OC_Defaults $defaults
+	 * @param IRootFolder $rootFolder
 	 */
 	public function __construct(IConfig $config,
 								IL10N $l,
 								IURLGenerator $urlGenerator,
-								\OC_Defaults $defaults
+								\OC_Defaults $defaults,
+								IRootFolder $rootFolder
 	) {
 		parent::__construct();
 		$this->config = $config;
 		$this->l = $l;
 		$this->urlGenerator = $urlGenerator;
+		$this->rootFolder = $rootFolder;
 
 		$this->name = $defaults->getName();
 		$this->url = $defaults->getBaseUrl();
@@ -120,8 +126,7 @@ class ThemingDefaults extends \OC_Defaults {
 	 */
 	public function getLogo() {
 		$logo = $this->config->getAppValue('theming', 'logoMime');
-		$pathToLogo = $this->config->getSystemValue('datadirectory', \OC::$SERVERROOT . '/data/') . '/themedinstancelogo';
-		if(!$logo || !file_exists($pathToLogo)) {
+		if(!$logo || !$this->rootFolder->nodeExists('themedinstancelogo')) {
 			return $this->urlGenerator->imagePath('core','logo.svg');
 		} else {
 			return $this->urlGenerator->linkToRoute('theming.Theming.getLogo');
@@ -134,8 +139,7 @@ class ThemingDefaults extends \OC_Defaults {
 	 */
 	public function getBackground() {
 		$backgroundLogo = $this->config->getAppValue('theming', 'backgroundMime');
-		$pathToLogo = $this->config->getSystemValue('datadirectory', \OC::$SERVERROOT . '/data/') . '/themedbackgroundlogo';
-		if(!$backgroundLogo || !file_exists($pathToLogo)) {
+		if(!$backgroundLogo || !$this->rootFolder->nodeExists('themedbackgroundlogo')) {
 			return $this->urlGenerator->imagePath('core','background.jpg');
 		} else {
 			return $this->urlGenerator->linkToRoute('theming.Theming.getLoginBackground');
--- apps/theming/tests/ThemingDefaultsTest.php
+++ apps/theming/tests/ThemingDefaultsTest.php
@@ -27,6 +27,7 @@ use OCA\Theming\ThemingDefaults;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IURLGenerator;
+use OCP\Files\IRootFolder;
 use Test\TestCase;
 
 class ThemingDefaultsTest extends TestCase {
@@ -40,11 +41,14 @@ class ThemingDefaultsTest extends TestCase {
 	private $defaults;
 	/** @var ThemingDefaults */
 	private $template;
+	/** @var IRootFolder */
+	private $rootFolder;
 
 	public function setUp() {
 		$this->config = $this->getMock('\\OCP\\IConfig');
 		$this->l10n = $this->getMock('\\OCP\\IL10N');
 		$this->urlGenerator = $this->getMock('\\OCP\\IURLGenerator');
+		$this->rootFolder = $this->getMock('\\OCP\\Files\\IRootFolder');
 		$this->defaults = $this->getMockBuilder('\\OC_Defaults')
 			->disableOriginalConstructor()
 			->getMock();
@@ -68,7 +72,8 @@ class ThemingDefaultsTest extends TestCase {
 			$this->config,
 			$this->l10n,
 			$this->urlGenerator,
-			$this->defaults
+			$this->defaults,
+			$this->rootFolder
 		);
 
 		return parent::setUp();
