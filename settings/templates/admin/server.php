3ad89a76b506a234948019ec5eb8bb3247528c69
Lukas Reschke <lukas@statuscode.ch>
2016-08-11 19:39:32 +0200

    Resolve conflict

--- settings/templates/admin/server.php
+++ settings/templates/admin/server.php
@@ -38,14 +38,14 @@ $mail_smtpsecure = [
 ];
 
 $mail_smtpmode = [
-	'php',
-	'smtp',
+	['php', 'PHP'],
+	['smtp', 'SMTP'],
 ];
 if ($_['sendmail_is_available']) {
-	$mail_smtpmode[] = 'sendmail';
+	$mail_smtpmode[] = ['sendmail', 'Sendmail'];
 }
 if ($_['mail_smtpmode'] == 'qmail') {
-	$mail_smtpmode[] = 'qmail';
+	$mail_smtpmode[] = ['qmail', 'qmail'];
 }
 ?>
 
@@ -244,10 +244,10 @@ if ($_['mail_smtpmode'] == 'qmail') {
 			<select name='mail_smtpmode' id='mail_smtpmode'>
 				<?php foreach ($mail_smtpmode as $smtpmode):
 					$selected = '';
-					if ($smtpmode == $_['mail_smtpmode']):
+					if ($smtpmode[0] == $_['mail_smtpmode']):
 						$selected = 'selected="selected"';
 					endif; ?>
-					<option value='<?php p($smtpmode)?>' <?php p($selected) ?>><?php p($smtpmode) ?></option>
+					<option value='<?php p($smtpmode[0])?>' <?php p($selected) ?>><?php p($smtpmode[1]) ?></option>
 				<?php endforeach;?>
 			</select>
 
