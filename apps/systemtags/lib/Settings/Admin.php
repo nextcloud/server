b99ecb3c235436bbdf50952b502465ba440d57bc
Arthur Schiwon <blizzz@arthur-schiwon.de>
2016-08-13 00:33:09 +0200

    move systemstags to sharign section, drop collaboration section

--- apps/systemtags/lib/Settings/Admin.php
+++ apps/systemtags/lib/Settings/Admin.php
@@ -39,7 +39,7 @@ class Admin implements ISettings {
 	 * @return string the section ID, e.g. 'sharing'
 	 */
 	public function getSection() {
-		return 'collaboration';
+		return 'sharing';
 	}
 
 	/**
@@ -50,7 +50,7 @@ class Admin implements ISettings {
 	 * E.g.: 70
 	 */
 	public function getPriority() {
-		return 30;
+		return 70;
 	}
 
 }
