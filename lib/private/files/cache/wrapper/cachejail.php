0182b9db72d7cc3047309ccc12ea9cd92da2129b
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-08-12 10:46:47 +0200

    [stable9] getJailedPath expects $path to have a trailing /  (#25703) (#25738)
    * getJailedPath expects $path to have a trailing / - fixes #25464
    
    * Adding test case for getPathById including a jailed cache where root is just empty

--- lib/private/files/cache/wrapper/cachejail.php
+++ lib/private/files/cache/wrapper/cachejail.php
@@ -56,6 +56,9 @@ class CacheJail extends CacheWrapper {
 	 * @return null|string the jailed path or null if the path is outside the jail
 	 */
 	protected function getJailedPath($path) {
+		if ($this->root === '') {
+			return $path;
+		}
 		$rootLength = strlen($this->root) + 1;
 		if ($path === $this->root) {
 			return '';
