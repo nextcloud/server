1ebbcdcc960499527357f35f1c9c6ec88b3c6e7e
Arthur Schiwon <blizzz@arthur-schiwon.de>
2016-08-15 13:38:02 +0200

    satisfy dependencies for files_external

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -1288,6 +1288,11 @@ class Server extends ServerContainer implements IServerContainer {
 		return $this->query('MountManager');
 	}
 
+	/** @return \OCP\Files\Config\IUserMountCache */
+	function getUserMountCache() {
+		return $this->query('UserMountCache');
+	}
+
 	/**
 	 * Get the MimeTypeDetector
 	 *
