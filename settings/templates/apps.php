c07090ef6e6facfe0ef2d8b16b824eec6212bc00
Roeland Jago Douma <roeland@famdouma.nl>
2016-08-18 15:54:19 +0200

    SVG defintions always take the first one
    * Move filter definition out

--- settings/templates/apps.php
+++ settings/templates/apps.php
@@ -180,6 +180,9 @@ script(
 	</div>
 </div>
 <div id="app-content">
+	<svg height="0">
+		<defs><filter id="invertIcon"><feColorMatrix in="SourceGraphic" type="matrix" values="-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0"></feColorMatrix></filter></defs>
+	</svg>
 	<div id="apps-list" class="icon-loading"></div>
 	<div id="apps-list-empty" class="hidden emptycontent emptycontent-search">
 		<div class="icon-search"></div>
