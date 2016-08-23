508924bea0de452e57c1bd3c0f15475c8af2f2db
elie195 <elie195@gmail.com>
2016-08-23 16:11:52 +0300

    Add ability for TwoFactorController.php to display a custom CSP if provided by IProvider (#25882)
    * Add ability for TwoFactorController.php to display a custom CSP if provided by IProvider
    
    * Fixed indentation issues and added the getCSP() method to the OCP\Authentication\TwoFactorAuth\IProvider interface
    
    * Created new IProvider2 interface and moved the getCSP() method to the IProvider2 interface
    
    * Added check for provider to only call the getCSP() method if the provider implements the IProvider2 interface

--- core/Controller/TwoFactorChallengeController.php
+++ core/Controller/TwoFactorChallengeController.php
@@ -111,6 +111,10 @@ class TwoFactorChallengeController extends Controller {
 		} else {
 			$error = false;
 		}
+		//Attempt to get custom ContentSecurityPolicy(CSP) from 2FA provider
+		if ($provider instanceof \OCP\Authentication\TwoFactorAuth\IProvider2) {
+			$csp  = $provider->getCSP();
+		}
 		$tmpl = $provider->getTemplate($user);
 		$tmpl->assign('redirect_url', $redirect_url);
 		$data = [
@@ -119,8 +123,13 @@ class TwoFactorChallengeController extends Controller {
 			'logout_attribute' => $this->getLogoutAttribute(),
 			'template' => $tmpl->fetchPage(),
 		];
-		return new TemplateResponse($this->appName, 'twofactorshowchallenge', $data, 'guest');
-	}
+		//Generate the response and add the custom CSP (if defined)
+		$response = new TemplateResponse($this->appName, 'twofactorshowchallenge', $data, 'guest');
+		if (!is_null($csp)) {
+			$response->setContentSecurityPolicy($csp);
+		}
+		return $response;
+	}	
 
 	/**
 	 * @NoAdminRequired
