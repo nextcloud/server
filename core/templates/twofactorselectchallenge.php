c93c5d142ee0185af01536b967839c4b2eff1b48
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-08-27 11:10:36 +0200

    fix 2fa challenge page wording

--- core/templates/twofactorselectchallenge.php
+++ core/templates/twofactorselectchallenge.php
@@ -19,4 +19,4 @@
 <?php endforeach; ?>
 </ul>
 </fieldset>
-<a class="two-factor-cancel" <?php print_unescaped($_['logout_attribute']); ?>><?php p($l->t('Cancel login')) ?></a>
+<a class="two-factor-cancel" <?php print_unescaped($_['logout_attribute']); ?>><?php p($l->t('Cancel log in')) ?></a>
--- core/templates/twofactorshowchallenge.php
+++ core/templates/twofactorshowchallenge.php
@@ -11,10 +11,9 @@ $template = $_['template'];
 
 <div class="warning">
 		<h2><?php p($provider->getDisplayName()); ?></h2>
-		<p><?php p($l->t('Please authenticate using the selected factor.')) ?></p>
 		<?php if ($error): ?>
 		<p><?php p($l->t('An error occured while verifying the token')); ?></p>
 		<?php endif; ?>
 		<?php print_unescaped($template); ?>
 </div>
-<a class="two-factor-cancel" <?php print_unescaped($_['logout_attribute']); ?>><?php p($l->t('Cancel login')) ?></a>
+<a class="two-factor-cancel" <?php print_unescaped($_['logout_attribute']); ?>><?php p($l->t('Cancel log in')) ?></a>
