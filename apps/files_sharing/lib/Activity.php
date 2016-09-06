cfdb46ab4d80d2b7198038e8f8919368a44421e9
oparoz <github@oparoz.com>
2016-09-07 00:51:19 +0200

    Update copyright

--- apps/files_sharing/lib/Activity.php
+++ apps/files_sharing/lib/Activity.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @copyright Copyright (c) 2016, Olivier Paroz
  *
  * @author Björn Schießle <bjoern@schiessle.org>
  * @author Joas Schilling <coding@schilljs.com>
  * @author Morris Jobke <hey@morrisjobke.de>
+ * @author Olivier Paroz <developer@oparoz.com>
  *
  * @license AGPL-3.0
  *
