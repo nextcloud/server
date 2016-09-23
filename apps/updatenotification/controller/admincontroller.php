7a135d8d81896d2e2a26b19d941cdcc8237ba393
Morris Jobke <hey@morrisjobke.de>
2016-09-23 13:59:46 +0200

    Revert "Show an update button instead of the updater"
    This reverts commit b42ee95383cbedb6a0fadf315fcdccba226286a9.
    
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- apps/updatenotification/controller/admincontroller.php
+++ apps/updatenotification/controller/admincontroller.php
@@ -134,7 +134,6 @@ class AdminController extends Controller {
 			'channels' => $channels,
 			'newVersionString' => ($updateState === []) ? '' : $updateState['updateVersion'],
 			'updaterRequirementsFulfilled' => $this->isCompatibleWithUpdater(),
-			'downloadLink' => (empty($updateState['downloadLink'])) ? '' : $updateState['downloadLink'],
 		];
 
 		return new TemplateResponse($this->appName, 'admin', $params, '');
