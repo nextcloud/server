587eca3ccd83d2aa03f723d2f438d2d731e83045
Joas Schilling <coding@schilljs.com>
2016-09-30 14:41:37 +0200

    Remove the magic parsing of the description
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/legacy/app.php
+++ lib/private/legacy/app.php
@@ -1307,30 +1307,8 @@ class OC_App {
 			$data['description'] = self::findBestL10NOption($data['description'], $lang);
 		}
 
-		// just modify the description if it is available
-		// otherwise this will create a $data element with an empty 'description'
-		if (isset($data['description'])) {
-			if (is_string($data['description'])) {
-				// sometimes the description contains line breaks and they are then also
-				// shown in this way in the app management which isn't wanted as HTML
-				// manages line breaks itself
-
-				// first of all we split on empty lines
-				$paragraphs = preg_split("!\n[[:space:]]*\n!mu", $data['description']);
-
-				$result = [];
-				foreach ($paragraphs as $value) {
-					// replace multiple whitespace (tabs, space, newlines) inside a paragraph
-					// with a single space - also trims whitespace
-					$result[] = trim(preg_replace('![[:space:]]+!mu', ' ', $value));
-				}
-
-				// join the single paragraphs with a empty line in between
-				$data['description'] = implode("\n\n", $result);
-
-			} else {
-				$data['description'] = '';
-			}
+		if (!isset($data['description'])) {
+			$data['description'] = '';
 		}
 
 		return $data;
