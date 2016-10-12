83b752de213571e48a1a7a422fd8c04dff2e2ff8
RealRancor <RealRancor@users.noreply.github.com>
2016-10-12 13:02:11 +0200

    Backport #26316 to stable9.1 (#26348)

--- index.php
+++ index.php
@@ -32,6 +32,13 @@ if (version_compare(PHP_VERSION, '5.4.0') === -1) {
 	return;
 }
 
+// running oC on Windows is unsupported since 8.1, this has to happen here because
+// is seems that the autoloader on Windows fails later and just throws an exception.
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+	echo 'ownCloud Server does not support Microsoft Windows.';
+	return;
+}
+
 try {
 	
 	require_once 'lib/base.php';
