726994ecaee5af9a8e1f91db9bb92fc5bf188530
Thomas Müller <thomas.mueller@tmit.eu>
2016-10-24 22:29:50 +0200

    Remove any detailed information about the exception in the browser error plugin

--- apps/dav/templates/exception.php
+++ apps/dav/templates/exception.php
@@ -30,18 +30,5 @@ style('core', ['styles', 'header']);
 	<ul>
 		<li><?php p($l->t('Remote Address: %s', $_['remoteAddr'])) ?></li>
 		<li><?php p($l->t('Request ID: %s', $_['requestID'])) ?></li>
-		<?php if($_['debugMode']): ?>
-			<li><?php p($l->t('Type: %s', $_['errorClass'])) ?></li>
-			<li><?php p($l->t('Code: %s', $_['errorCode'])) ?></li>
-			<li><?php p($l->t('Message: %s', $_['errorMsg'])) ?></li>
-			<li><?php p($l->t('File: %s', $_['file'])) ?></li>
-			<li><?php p($l->t('Line: %s', $_['line'])) ?></li>
-		<?php endif; ?>
 	</ul>
-
-	<?php if($_['debugMode']): ?>
-		<br />
-		<h2><strong><?php p($l->t('Trace')) ?></strong></h2>
-		<pre><?php p($_['trace']) ?></pre>
-	<?php endif; ?>
 </span>
