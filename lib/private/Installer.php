086d43f26d047f7078762585ce95c75ad155a6ba
Lukas Reschke <lukas@statuscode.ch>
2016-10-31 18:42:19 +0100

    Move to non-static version
    The static version is used nowhere in the code and just decreases coverage
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- lib/private/Installer.php
+++ lib/private/Installer.php
@@ -46,6 +46,7 @@ use OC\App\CodeChecker\CodeChecker;
 use OC\App\CodeChecker\EmptyCheck;
 use OC\App\CodeChecker\PrivateCheck;
 use OC\Archive\Archive;
+use OC\Archive\TAR;
 use OC_App;
 use OC_DB;
 use OC_Helper;
@@ -254,7 +255,7 @@ class Installer {
 				if($verified === true) {
 					// Seems to match, let's proceed
 					$extractDir = $this->tempManager->getTemporaryFolder();
-					$archive = Archive::open($tempFile);
+					$archive = new TAR($tempFile);
 
 					if($archive) {
 						$archive->extract($extractDir);
