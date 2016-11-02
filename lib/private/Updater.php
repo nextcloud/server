9b66e8f7d96cc5ca014d3f6b8031eb380d981adb
Joas Schilling <coding@schilljs.com>
2016-11-02 09:40:10 +0100

    Throw an exception when a shipped app was not replaced before the update
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Updater.php
+++ lib/private/Updater.php
@@ -381,6 +381,9 @@ class Updater extends BasicEmitter {
 			// check if the app is compatible with this version of ownCloud
 			$info = OC_App::getAppInfo($app);
 			if(!OC_App::isAppCompatible($version, $info)) {
+				if (OC_App::isShipped($app)) {
+					throw new \UnexpectedValueException('The files of the app "' . $app . '" were not correctly replaced before running the update');
+				}
 				OC_App::disable($app);
 				$this->emit('\OC\Updater', 'incompatibleAppDisabled', array($app));
 			}
