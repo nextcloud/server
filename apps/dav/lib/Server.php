c016d947e2243329243b62a41caab82a4ff46a10
Roeland Jago Douma <roeland@famdouma.nl>
2016-11-04 15:05:15 +0100

    Fix plugin
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -118,7 +118,7 @@ class Server {
 		// calendar plugins
 		$this->server->addPlugin(new \OCA\DAV\CalDAV\Plugin());
 		$this->server->addPlugin(new \Sabre\CalDAV\ICSExportPlugin());
-		$this->server->addPlugin(new \Sabre\CalDAV\Schedule\Plugin());
+		$this->server->addPlugin(new \OCA\DAV\CalDAV\Schedule\Plugin());
 		$this->server->addPlugin(new IMipPlugin($mailer, $logger));
 		$this->server->addPlugin(new \Sabre\CalDAV\Subscriptions\Plugin());
 		$this->server->addPlugin(new \Sabre\CalDAV\Notifications\Plugin());
