2f5e3e66f0737b1bbf76dc518679727b0f4631c5
Vincent Petry <pvince81@owncloud.com>
2016-11-07 22:52:07 +0100

    Add transfer ownership integration tests (#26543)
    * Add transfer ownership integration tests
    
    * Added more transfer ownership tests and OCC checks

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -2,6 +2,9 @@
 
 composer install
 
+OC_PATH=../../
+OCC=${OC_PATH}occ
+
 SCENARIO_TO_RUN=$1
 HIDE_OC_LOGS=$2
 
@@ -25,16 +28,15 @@ export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 export TEST_SERVER_FED_URL="http://localhost:$PORT_FED/ocs/"
 
 #Enable external storage app
-../../occ app:enable files_external
+$OCC app:enable files_external
 
 
 mkdir -p work/local_storage
-OUTPUT_CREATE_STORAGE=`../../occ files_external:create local_storage local null::null -c datadir=./build/integration/work/local_storage` 
+OUTPUT_CREATE_STORAGE=`$OCC files_external:create local_storage local null::null -c datadir=./build/integration/work/local_storage` 
 
 ID_STORAGE=`echo $OUTPUT_CREATE_STORAGE | awk {'print $5'}`
 
-
-../../occ files_external:option $ID_STORAGE enable_sharing true
+$OCC files_external:option $ID_STORAGE enable_sharing true
 
 vendor/bin/behat --strict -f junit -f pretty $SCENARIO_TO_RUN
 RESULT=$?
@@ -42,13 +44,13 @@ RESULT=$?
 kill $PHPPID
 kill $PHPPID_FED
 
-../../occ files_external:delete -y $ID_STORAGE
+$OCC files_external:delete -y $ID_STORAGE
 
 #Disable external storage app
-../../occ app:disable files_external
+$OCC app:disable files_external
 
 if [ -z $HIDE_OC_LOGS ]; then
-	tail "../../data/owncloud.log"
+	tail "${OC_PATH}/data/owncloud.log"
 fi
 
 echo "runsh: Exit code: $RESULT"
