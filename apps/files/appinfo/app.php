1dc23f2fd62ca8beb5d12c5dfb04b54ee4ffcf76
Joas Schilling <coding@schilljs.com>
2016-11-10 16:03:51 +0100

    Remove the old activity extension
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files/appinfo/app.php
+++ apps/files/appinfo/app.php
@@ -67,13 +67,3 @@ $templateManager->registerTemplate('application/vnd.oasis.opendocument.spreadshe
 		'name' => $l->t('Recent'),
 	];
 });
-
-\OC::$server->getActivityManager()->registerExtension(function() {
-	return new \OCA\Files\Activity(
-		\OC::$server->query('L10NFactory'),
-		\OC::$server->getURLGenerator(),
-		\OC::$server->getActivityManager(),
-		\OC::$server->getDatabaseConnection(),
-		\OC::$server->getConfig()
-	);
-});
