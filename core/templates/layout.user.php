80abb69b60699f04a3c4bc68a8cb28ec7c9c562b
Joas Schilling <coding@schilljs.com>
2016-11-11 09:59:20 +0100

    Show a little explanation above the input field
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -148,6 +148,7 @@
 
 		<div id="sudo-login-background" class="hidden"></div>
 		<div id="sudo-login-form" class="hidden">
+			<?php p($l->t('This action requires you to confirm your password:')); ?><br>
 			<input type="password" class="question" autocomplete="off" name="question" value=" <?php /* Hack against firefox ignoring autocomplete="off" */ ?>"
 				placeholder="<?php p($l->t('Confirm your password')); ?>" />
 			<input class="confirm icon-confirm" title="<?php p($l->t('Confirm')); ?>" value="" type="submit">
