7e272adf25f22b5f807d99aefd4f1efad08e0c51
Lukas Reschke <lukas@statuscode.ch>
2016-12-01 19:13:28 +0100

    [stable9] Catch status code 400
    In case the server returns a statuscode 400 we should also gracefully return to the home directory.
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -1489,7 +1489,7 @@
 				return false;
 			}
 
-			if (status === 404 || status === 405) {
+			if (status === 400 || status === 404 || status === 405) {
 				// go back home
 				this.changeDirectory('/');
 				return false;
