25a5c655f7cfb24b2bac597d6ec6ca68316098e9
justin-sleep <justin@quarterfull.com>
2016-12-02 14:07:45 -0600

    Move integer casting to the top of the chain
    Signed-off-by: justin-sleep <justin@quarterfull.com>

--- core/Controller/LoginController.php
+++ core/Controller/LoginController.php
@@ -240,7 +240,7 @@ class LoginController extends Controller {
 		// TODO: remove password checks from above and let the user session handle failures
 		// requires https://github.com/owncloud/core/pull/24616
 		$this->userSession->login($user, $password);
-		$this->userSession->createSessionToken($this->request, $loginResult->getUID(), $user, $password, $remember_login);
+		$this->userSession->createSessionToken($this->request, $loginResult->getUID(), $user, $password, (int)$remember_login);
 
 		// User has successfully logged in, now remove the password reset link, when it is available
 		$this->config->deleteUserValue($loginResult->getUID(), 'core', 'lostpassword');
