737d9f3504ec5dbeec9b692655303936e5f6321a
Lukas Reschke <lukas@statuscode.ch>
2016-12-07 15:29:45 +0100

    Increase version string to 11,0,0,6

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(11, 0, 0, 5);
+$OC_Version = array(11, 0, 0, 6);
 
 // The human readable string
 $OC_VersionString = '11.0 RC1';
