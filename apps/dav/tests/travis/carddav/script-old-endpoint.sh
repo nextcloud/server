f823024416ba7db2999cdf01a76111996a73a512
Lukas Reschke <lukas@statuscode.ch>
2016-12-15 00:40:12 +0100

    Add test execution against legacy DAV backend
    Since the tests to quite hugely rely on sync tokens being present I also included those in the legacy backend.

--- apps/dav/tests/travis/carddav/script-old-endpoint.sh
+++ apps/dav/tests/travis/carddav/script-old-endpoint.sh
@@ -2,17 +2,18 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
+# Move the endpoint to the serverinfo file
+cp "$SCRIPTPATH/../caldavtest/serverinfo-old-carddav-endpoint.xml" "$SCRIPTPATH/../caldavtest/serverinfo.xml"
+
 # start the server
 php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../../.." &
 
-sleep 30
 
 # run the tests
 cd "$SCRIPTPATH/CalDAVTester"
 PYTHONPATH="$SCRIPTPATH/pycalendar/src" python testcaldav.py --print-details-onfail --basedir "$SCRIPTPATH/../caldavtest/" -o cdt.txt \
-	"CalDAV/current-user-principal.xml" \
-	"CalDAV/sync-report.xml" \
-	"CalDAV/sharing-calendars.xml"
+	"CardDAV/current-user-principal.xml" \
+	"CardDAV/sync-report.xml"
 
 RESULT=$?
 
