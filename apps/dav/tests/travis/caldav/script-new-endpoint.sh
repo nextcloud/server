8ecae031250e8d2a7d86c64b375b35ff88a4486d
Lukas Reschke <lukas@statuscode.ch>
2016-12-14 21:52:24 +0100

    Test
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- apps/dav/tests/travis/caldav/script-new-endpoint.sh
+++ apps/dav/tests/travis/caldav/script-new-endpoint.sh
@@ -3,7 +3,7 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 # Move the endpoint to the serverinfo file
-cp "$SCRIPTPATH/../caldavtest/serverinfo-new-endpoint.dtd" "$SCRIPTPATH/../caldavtest/serverinfo.dtd"
+cp "$SCRIPTPATH/../caldavtest/serverinfo-new-endpoint.xml" "$SCRIPTPATH/../caldavtest/serverinfo.xml"
 
 # start the server
 php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../../.." &
--- apps/dav/tests/travis/caldav/script-old-endpoint.sh
+++ apps/dav/tests/travis/caldav/script-old-endpoint.sh
@@ -3,7 +3,7 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 # Move the endpoint to the serverinfo file
-cp "$SCRIPTPATH/../caldavtest/serverinfo-old-endpoint.dtd" "$SCRIPTPATH/../caldavtest/serverinfo.dtd"
+cp "$SCRIPTPATH/../caldavtest/serverinfo-old-endpoint.xml" "$SCRIPTPATH/../caldavtest/serverinfo.xml"
 
 # start the server
 php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../../.." &
