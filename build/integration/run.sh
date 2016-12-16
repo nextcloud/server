7b5c28ab2ae746381bf88fc59336672a20aca31d
Lukas Reschke <lukas@statuscode.ch>
2016-12-16 17:29:27 +0100

    Adjust to Nextcloud changes
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- build/integration/run.sh
+++ build/integration/run.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-composer install
-
 OC_PATH=../../
 OCC=${OC_PATH}occ
-
 SCENARIO_TO_RUN=$1
 HIDE_OC_LOGS=$2
+
+INSTALLED=$($OCC status | grep installed: | cut -d " " -f 5)
+
+if [ "$INSTALLED" == "true" ]; then
+    # Disable bruteforce protection because the integration tests do trigger them
+    $OCC config:system:set auth.bruteforce.protection.enabled --value false --type bool
+else
+    if [ "$SCENARIO_TO_RUN" != "setup_features/setup.feature" ]; then
+        echo "Nextcloud instance needs to be installed" >&2
+        exit 1
+    fi
+fi
+
+composer install
 
 # avoid port collision on jenkins - use $EXECUTOR_NUMBER
 if [ -z "$EXECUTOR_NUMBER" ]; then
@@ -27,15 +38,19 @@ echo $PHPPID_FED
 export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 export TEST_SERVER_FED_URL="http://localhost:$PORT_FED/ocs/"
 
-#Enable external storage app
-$OCC app:enable files_external
+if [ "$INSTALLED" == "true" ]; then
 
-mkdir -p work/local_storage
-OUTPUT_CREATE_STORAGE=`$OCC files_external:create local_storage local null::null -c datadir=./build/integration/work/local_storage`
+    #Enable external storage app
+    $OCC app:enable files_external
 
-ID_STORAGE=`echo $OUTPUT_CREATE_STORAGE | awk {'print $5'}`
+    mkdir -p work/local_storage
+    OUTPUT_CREATE_STORAGE=`$OCC files_external:create local_storage local null::null -c datadir=./build/integration/work/local_storage`
 
-$OCC files_external:option $ID_STORAGE enable_sharing true
+    ID_STORAGE=`echo $OUTPUT_CREATE_STORAGE | awk {'print $5'}`
+
+    $OCC files_external:option $ID_STORAGE enable_sharing true
+
+fi
 
 vendor/bin/behat --strict -f junit -f pretty $SCENARIO_TO_RUN
 RESULT=$?
@@ -43,13 +58,16 @@ RESULT=$?
 kill $PHPPID
 kill $PHPPID_FED
 
-$OCC files_external:delete -y $ID_STORAGE
+if [ "$INSTALLED" -eq "true" ]; then
 
-#Disable external storage app
-$OCC app:disable files_external
+    $OCC files_external:delete -y $ID_STORAGE
+
+    #Disable external storage app
+    $OCC app:disable files_external
+fi
 
 if [ -z $HIDE_OC_LOGS ]; then
-	tail "${OC_PATH}/data/owncloud.log"
+	tail "${OC_PATH}/data/nextcloud.log"
 fi
 
 echo "runsh: Exit code: $RESULT"
