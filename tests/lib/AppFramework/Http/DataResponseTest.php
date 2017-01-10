e3815b382d8a57288e958a60ab8c27a6aecc8e2c
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-01-10 10:13:08 +0100

    fix data response test expected cache headers
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- tests/lib/AppFramework/Http/DataResponseTest.php
+++ tests/lib/AppFramework/Http/DataResponseTest.php
@@ -67,7 +67,7 @@ class DataResponseTest extends \Test\TestCase {
 		$response = new DataResponse($data, $code, $headers);
 
 		$expectedHeaders = [
-			'Cache-Control' => 'no-cache, must-revalidate',
+			'Cache-Control' => 'no-cache, no-store, must-revalidate',
 			'Content-Security-Policy' => "default-src 'none';script-src 'self' 'unsafe-eval';style-src 'self' 'unsafe-inline';img-src 'self' data: blob:;font-src 'self';connect-src 'self';media-src 'self'",
 		];
 		$expectedHeaders = array_merge($expectedHeaders, $headers);
