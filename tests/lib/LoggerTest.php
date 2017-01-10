7fa063ceca5d78741c47f645c6dfdf65e4be7330
Joas Schilling <coding@schilljs.com>
2017-01-10 12:58:23 +0100

    Better fallback for unknown log types
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/lib/LoggerTest.php
+++ tests/lib/LoggerTest.php
@@ -140,11 +140,13 @@ class LoggerTest extends TestCase {
 
 	public function dataGetLogClass() {
 		return [
-			['owncloud', \OC\Log\File::class],
-			['nextcloud', \OC\Log\File::class],
 			['file', \OC\Log\File::class],
 			['errorlog', \OC\Log\Errorlog::class],
 			['syslog', \OC\Log\Syslog::class],
+
+			['owncloud', \OC\Log\File::class],
+			['nextcloud', \OC\Log\File::class],
+			['foobar', \OC\Log\File::class],
 		];
 	}
 
