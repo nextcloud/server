1ee9bee489dc1ddb98692227c9e0ece0c902c418
Joas Schilling <coding@schilljs.com>
2017-02-09 14:53:17 +0100

    Backport more fixes
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files/lib/Activity/Provider.php
+++ apps/files/lib/Activity/Provider.php
@@ -272,7 +272,8 @@ class Provider implements IProvider {
 			'type' => 'file',
 			'id' => $id,
 			'name' => basename($path),
-			'path' => $path,
+			'path' => trim($path, '/'),
+			'link' => $this->url->linkToRouteAbsolute('files.viewcontroller.showFile', ['fileid' => $id]),
 		];
 	}
 
