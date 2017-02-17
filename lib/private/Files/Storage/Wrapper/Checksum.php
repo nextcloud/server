b174da8b89071c16f93f169b1db4d606f57c9577
Ilja Neumann <ineumann@owncloud.com>
2017-02-17 14:03:29 +0100

    Files without checksum in oc_filecache get a checksum on first read #26655

--- lib/private/Files/Storage/Wrapper/Checksum.php
+++ lib/private/Files/Storage/Wrapper/Checksum.php
@@ -20,18 +20,33 @@
  */
 namespace OC\Files\Storage\Wrapper;
 
+use Icewind\Streams\CallbackWrapper;
+use OC\Cache\CappedMemoryCache;
 use OC\Files\Stream\Checksum as ChecksumStream;
 
 /**
  * Class Checksum
  *
- * Computes checksums (default: SHA1) on all files under the /files path.
+ * Computes checksums (default: SHA1, MD5, ADLER32) on all files under the /files path.
  * The resulting checksum can be retrieved by call getMetadata($path)
+ *
+ * If a file is read and has no checksum oc_filecache gets updated accordingly.
+ *
  *
  * @package OC\Files\Storage\Wrapper
  */
 class Checksum extends Wrapper {
 
+
+	const NOT_REQUIRED = 0;
+	/** Calculate checksum on write (to be stored in oc_filecache) */
+	const PATH_NEW_OR_UPDATED = 1;
+	/** File needs to be checksummed on first read because it is already in cache but has no checksum */
+	const PATH_IN_CACHE_WITHOUT_CHECKSUM = 2;
+
+
+	/** @var array */
+	private $pathsInCacheWithoutChecksum = [];
 
 	/**
 	 * @param string $path
@@ -40,23 +55,86 @@ class Checksum extends Wrapper {
 	 */
 	public function fopen($path, $mode) {
 		$stream = $this->getWrapperStorage()->fopen($path, $mode);
-		if (!self::requiresChecksum($path, $mode)) {
-			return $stream;
+		$requirement = $this->getChecksumRequirement($path, $mode);
+
+		if ($requirement === self::PATH_NEW_OR_UPDATED) {
+			return \OC\Files\Stream\Checksum::wrap($stream, $path);
 		}
 
-		return \OC\Files\Stream\Checksum::wrap($stream, $path);
+		// If file is without checksum we save the path and create
+		// a callback becaause we can only calculate the checksum
+		// after the client has read the entire filestream once.
+		// the checksum is then saved to oc_filecache for subsequent
+		// retrieval (see onClose())
+		if ($requirement == self::PATH_IN_CACHE_WITHOUT_CHECKSUM) {
+			$this->pathsInCacheWithoutChecksum[] = $path;
+			$checksumStream = \OC\Files\Stream\Checksum::wrap($stream, $path);
+			return CallbackWrapper::wrap(
+				$checksumStream,
+				null,
+				null,
+				[$this, 'onClose']
+			);
+		}
+
+		return $stream;
 	}
 
-
 	/**
-	 * Checksum is only required for everything under files/
 	 * @param $mode
 	 * @param $path
-	 * @return bool
+	 * @return int
 	 */
-	private static function requiresChecksum($path, $mode) {
-		return substr($path, 0, 6) === 'files/'
-			&&  $mode !== 'r' && $mode !== 'rb';
+	private function getChecksumRequirement($path, $mode) {
+		$isNormalFile = substr($path, 0, 6) === 'files/';
+		$fileIsWritten = $mode !== 'r' && $mode !== 'rb';
+
+		if ($isNormalFile && $fileIsWritten) {
+			return self::PATH_NEW_OR_UPDATED;
+		}
+
+		// file could be in cache but without checksum for example
+		// if mounted from ext. storage
+		$cache = $this->getCache($path);
+
+		if ($cache->inCache($path)) {
+			$cacheEntry = $cache->get($path);
+			if (empty($cacheEntry['checksum'])) {
+				return self::PATH_IN_CACHE_WITHOUT_CHECKSUM;
+			}
+		}
+
+		return self::NOT_REQUIRED;
+	}
+
+	/**
+	 * Callback registered in fopen
+	 */
+	public function onClose() {
+		$cache = $this->getCache();
+		foreach ($this->pathsInCacheWithoutChecksum as $path) {
+			$entry = $cache->get($path);
+			$cache->update(
+				$entry->getId(),
+				['checksum' => self::getChecksumsInDbFormat($path)]
+			);
+		}
+
+		$this->pathsInCacheWithoutChecksum = [];
+	}
+
+	/**
+	 * @param $path
+	 * Format like "SHA1:abc MD5:def ADLER32:ghi"
+	 * @return string
+	 */
+	private static function getChecksumsInDbFormat($path) {
+		$checksumString = '';
+		foreach (ChecksumStream::getChecksums($path) as $algo => $checksum) {
+			$checksumString .= sprintf('%s:%s ', strtoupper($algo), $checksum);
+		}
+
+		return rtrim($checksumString);
 	}
 
 	/**
@@ -79,14 +157,8 @@ class Checksum extends Wrapper {
 	 * @return array
 	 */
 	public function getMetaData($path) {
-		$checksumString = '';
-
-		foreach (ChecksumStream::getChecksums($path) as $algo => $checksum) {
-			$checksumString .= sprintf('%s:%s ', strtoupper($algo), $checksum);
-		}
-
 		$parentMetaData = $this->getWrapperStorage()->getMetaData($path);
-		$parentMetaData['checksum'] = rtrim($checksumString);
+		$parentMetaData['checksum'] = self::getChecksumsInDbFormat($path);
 
 		if (!isset($parentMetaData['mimetype'])) {
 			$parentMetaData['mimetype'] = 'application/octet-stream';
