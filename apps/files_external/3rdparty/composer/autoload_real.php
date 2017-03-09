d895e71ed94709e695f8bc4f58750d44319c799b
Arthur Schiwon <blizzz@arthur-schiwon.de>
2017-03-09 12:08:52 +0100

    update php-spo
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/files_external/3rdparty/composer/autoload_real.php
+++ apps/files_external/3rdparty/composer/autoload_real.php
@@ -23,12 +23,19 @@ class ComposerAutoloaderInit98fe9b281934250b3a93f69a5ce843b3
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit98fe9b281934250b3a93f69a5ce843b3', 'loadClassLoader'));
 
-		$classMap = require __DIR__ . '/autoload_classmap.php';
-		if ($classMap) {
-			$loader->addClassMap($classMap);
-		}
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-		$loader->setClassMapAuthoritative(true);
+            call_user_func(\Composer\Autoload\ComposerStaticInit98fe9b281934250b3a93f69a5ce843b3::getInitializer($loader));
+        } else {
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
+
+        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         return $loader;
