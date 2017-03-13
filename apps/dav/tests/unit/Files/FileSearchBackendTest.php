ef14dc671f540419a044d353036763a02fe68d58
Morris Jobke <hey@morrisjobke.de>
2017-03-13 15:59:04 -0600

    fix unit test
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- apps/dav/tests/unit/Files/FileSearchBackendTest.php
+++ apps/dav/tests/unit/Files/FileSearchBackendTest.php
@@ -216,7 +216,7 @@ class FileSearchBackendTest extends TestCase {
 				new \OC\Files\Node\Folder($this->rootFolder, $this->view, '/test/path')
 			]));
 
-		$query = $this->getBasicQuery(Operator::OPERATION_GREATER_THAN, '{DAV:}getlastmodifed', 10);
+		$query = $this->getBasicQuery(Operator::OPERATION_GREATER_THAN, '{DAV:}getlastmodified', 10);
 		$result = $this->search->search($query);
 
 		$this->assertCount(1, $result);
