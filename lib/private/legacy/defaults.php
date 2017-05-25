771b422952881838176b26fc081f127d3b0236d3
Marius Blüm <marius@lineone.io>
2017-05-25 11:38:03 +0200

    Change slogan to “Your Secure Workspace”
    * according to https://github.com/nextcloud/nextcloud.com/commit/d698ddba6a29f8074de9e601bcdb84c7cacf2055
    
    Signed-off-by: Marius Blüm <marius@lineone.io>

--- lib/private/legacy/defaults.php
+++ lib/private/legacy/defaults.php
@@ -61,7 +61,7 @@ class OC_Defaults {
 		$this->defaultAndroidClientUrl = 'https://play.google.com/store/apps/details?id=com.nextcloud.client';
 		$this->defaultDocBaseUrl = 'https://docs.nextcloud.com';
 		$this->defaultDocVersion = '12'; // used to generate doc links
-		$this->defaultSlogan = $this->l->t('a safe home for all your data');
+		$this->defaultSlogan = $this->l->t('Your Secure Workspace');
 		$this->defaultLogoClaim = '';
 		$this->defaultColorPrimary = '#0082c9';
 
