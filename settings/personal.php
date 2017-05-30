593f6281dc0843868699629461285e549512656a
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-30 09:54:10 +0200

    Combine sessions and app passwords view into one single view
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- settings/personal.php
+++ settings/personal.php
@@ -226,7 +226,6 @@ $tmpl->assign('groups', $groups2);
 $formsAndMore = [];
 $formsAndMore[]= ['anchor' => 'personal-settings', 'section-name' => $l->t('Personal info')];
 $formsAndMore[]= ['anchor' => 'sessions', 'section-name' => $l->t('Sessions')];
-$formsAndMore[]= ['anchor' => 'apppasswords', 'section-name' => $l->t('App passwords')];
 $formsAndMore[]= ['anchor' => 'clientsbox', 'section-name' => $l->t('Sync clients')];
 
 $forms=OC_App::getForms('personal');
