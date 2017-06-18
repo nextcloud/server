140fce8a23d31c06ac242d7d0e2db48ac58764b8
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-19 01:00:54 +0300

    LDAP: Add SSL/TLS status text and CSS styling for it
    The status is shown in the server tab after the ldap_port field.
    
    Signed-off-by: Jarkko Lehtoranta <devel@jlranta.com>

--- apps/user_ldap/templates/part.wizard-server.php
+++ apps/user_ldap/templates/part.wizard-server.php
@@ -40,8 +40,9 @@
 						<span class="hostPortCombinatorSpan">
 							<input type="number" id="ldap_port" name="ldap_port"
 								placeholder="<?php p($l->t('Port'));?>" />
+							<span id="ldap_tls" name="ldap_tls"><?php p($l->t('SSL/TLS'));?></span>
 							<button class="ldapDetectPort" name="ldapDetectPort" type="button">
-								<?php p($l->t('Detect Port'));?>
+								<?php p($l->t('Detect Port and StartTLS'));?>
 							</button>
 						</span>
 					</div>
