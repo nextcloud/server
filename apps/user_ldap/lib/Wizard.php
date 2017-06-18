21e042d13e1f890a6b962282b2738148951cd34d
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-19 01:53:35 +0300

    LDAP: Add ldap_tls to guessPortAndTLS results
    Signed-off-by: Jarkko Lehtoranta <devel@jlranta.com>

--- apps/user_ldap/lib/Wizard.php
+++ apps/user_ldap/lib/Wizard.php
@@ -702,8 +702,9 @@ class Wizard extends LDAPUtility {
 					'ldapTLS' => (int)$t
 				);
 				$this->configuration->setConfiguration($config);
-				\OCP\Util::writeLog('user_ldap', 'Wiz: detected Port ' . $p, \OCP\Util::DEBUG);
+				\OCP\Util::writeLog('user_ldap', 'Wiz: detected Port ' . $p . ', TLS '. $t, \OCP\Util::DEBUG);
 				$this->result->addChange('ldap_port', $p);
+				$this->result->addChange('ldap_tls', $t);
 				return $this->result;
 			}
 		}
