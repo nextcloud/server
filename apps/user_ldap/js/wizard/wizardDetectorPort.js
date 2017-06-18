bb53ef45aa7819fc547f67779158c9022f0580b2
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-19 01:55:42 +0300

    LDAP: Add support for the ldap_tls item to the wizardDetectorPort
    Signed-off-by: Jarkko Lehtoranta <devel@jlranta.com>

--- apps/user_ldap/js/wizard/wizardDetectorPort.js
+++ apps/user_ldap/js/wizard/wizardDetectorPort.js
@@ -32,11 +32,25 @@ OCA = OCA || {};
 		 */
 		run: function(model, configID) {
 			model.notifyAboutDetectionStart('ldap_port');
+			model.notifyAboutDetectionStart('ldap_tls');
 			var params = OC.buildQueryString({
 				action: 'guessPortAndTLS',
 				ldap_serverconfig_chooser: configID
 			});
-			return model.callWizard(params, this.processResult, this);
+			return model.callWizard(params, this.processResultPort.bind(this), this);
+		},
+
+		/**
+		 * processes the results and sends an extra signal
+		 * about completion of the 'ldap_tls' item.
+		 *
+		 * @param {OCA.LDAP.Wizard.ConfigModel} model
+		 * @param {WizardDetectorGeneric} detector
+		 * @param {object} result
+		 */
+		processResultPort: function(model, detector, result) {
+			this.processResult(model, detector, result);
+			model['notifyAboutDetectionCompletion']('ldap_tls');
 		}
 	});
 
