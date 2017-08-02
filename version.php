d34bd3f17c0b323bd6077c67afacedb2ddff2dc0
Roeland Jago Douma <roeland@famdouma.nl>
2017-08-02 13:36:50 +0200

    Bump version
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(13, 0, 0, 2);
+$OC_Version = array(13, 0, 0, 3);
 
 // The human readable string
 $OC_VersionString = '13.0.0 alpha';
