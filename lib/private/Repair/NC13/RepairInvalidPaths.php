skyformat99/server-2

Add a method to compare empty strings with an expression
Signed-off-by: Joas Schilling <coding@schilljs.com>

lib/private/Repair/NC13/RepairInvalidPaths.php
@@ -70,7 +70,7 @@ class RepairInvalidPaths implements IRepairStep {
 			->from('filecache', 'f')
 			->innerJoin('f', 'filecache', 'p', $builder->expr()->andX(
 				$builder->expr()->eq('f.parent', 'p.fileid'),
-				$builder->expr()->isNotNull('p.name')
+				$builder->expr()->isNotEmptyString('p.name')
 			))
 			->where($builder->expr()->neq('f.path', $computedPath))
 			->setMaxResults(self::MAX_ROWS);

nextcloud/server

lib/private/Repair/NC13/RepairInvalidPaths.php
@@ -66,7 +66,7 @@ class RepairInvalidPaths implements IRepairStep {
 			->from('filecache', 'f')
 			->innerJoin('f', 'filecache', 'p', $builder->expr()->andX(
 				$builder->expr()->eq('f.parent', 'p.fileid'),
-				$builder->expr()->isNotNull('p.name')
+				$builder->expr()->nonEmptyString('p.name')
 			))
 			->where($builder->expr()->neq('f.path', $computedPath))
 			->setMaxResults(self::MAX_ROWS);