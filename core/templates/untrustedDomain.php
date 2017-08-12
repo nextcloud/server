faea9cb2948cd748b672a6f6985ed9bcaf168444
Jan-Christoph Borchardt <hey@jancborchardt.net>
2017-08-12 19:01:42 +0200

    Shorten text of untrusted domain error
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- core/templates/untrustedDomain.php
+++ core/templates/untrustedDomain.php
@@ -4,10 +4,10 @@
 	<h2><?php p($l->t('Access through untrusted domain')); ?></h2>
 
 	<p>
-		<?php p($l->t('Please contact your administrator. If you are an administrator of this instance, configure the "trusted_domains" setting in config/config.php. An example configuration is provided in config/config.sample.php.')); ?>
+		<?php p($l->t('Please contact your administrator. If you are an administrator, edit the "trusted_domains" setting in config/config.php like the example in config.sample.php.')); ?>
 	</p>
 	<p>
-		<?php p($l->t('Depending on your configuration, as an administrator you might also be able to use the button below to trust this domain.')); ?>
+		<?php p($l->t('Depending on your configuration, this button could also work to trust the domain:')); ?>
 	</p>
 	<p style="text-align:center;">
 		<a href="<?php print_unescaped(\OC::$server->getURLGenerator()->getAbsoluteURL(\OCP\Util::linkToRoute('settings.AdminSettings.index'))); ?>?trustDomain=<?php p($_['domain']); ?>" class="button">
