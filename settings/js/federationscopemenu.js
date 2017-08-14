3d1f6a041d249afd2ca74d765c5942e635b07d02
Morris Jobke <hey@morrisjobke.de>
2017-08-14 11:16:24 +0200

    Fix federation scope dropdown
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- settings/js/federationscopemenu.js
+++ settings/js/federationscopemenu.js
@@ -111,7 +111,7 @@
 		 */
 		show: function(context) {
 			this._context = context;
-			var currentlyActiveValue = $('#'+context.target.closest('form').id).find('.icon-checkmark > input')[0].value;
+			var currentlyActiveValue = $('#'+context.target.closest('form').id).find('input[type="hidden"]')[0].value;
 
 			for(var i in this._scopes) {
 				this._scopes[i].active = false;
