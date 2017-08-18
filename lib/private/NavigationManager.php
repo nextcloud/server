2e4cd445564ba4882ab239bbfd82b6aa800436e3
Lukas Reschke <lukas@statuscode.ch>
2017-08-18 15:32:40 +0200

    Inject \OCP\IURLGenerator to make tests work
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- lib/private/NavigationManager.php
+++ lib/private/NavigationManager.php
@@ -187,7 +187,7 @@ class NavigationManager implements INavigationManager {
 				'icon' => $this->urlGenerator->imagePath('settings', 'admin.svg'),
 			]);
 
-			$logoutUrl = \OC_User::getLogoutUrl();
+			$logoutUrl = \OC_User::getLogoutUrl($this->urlGenerator);
 			if($logoutUrl !== '') {
 				// Logout
 				$this->add([
