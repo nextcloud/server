eea7de4c9fc57e1a5c3e8c2f3b94200e7415cd10
Roeland Jago Douma <roeland@famdouma.nl>
2017-08-22 18:46:47 +0200

    Correctly format OCS response with favorites
    The helper funtion did not handle the response correctly and basically
    only returned the last share with tags.
    
    This is a simple rewrite. That is still understandable. Loops maybe more
    than strictly required. But preformance is not the issue here.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/files_sharing/lib/Controller/ShareAPIController.php
+++ apps/files_sharing/lib/Controller/ShareAPIController.php
@@ -514,7 +514,7 @@ class ShareAPIController extends OCSController {
 		}
 
 		if ($includeTags) {
-			$formatted = Helper::populateTags($formatted, 'file_source');
+			$formatted = Helper::populateTags($formatted, 'file_source', \OC::$server->getTagManager());
 		}
 
 		return new DataResponse($formatted);
@@ -644,7 +644,7 @@ class ShareAPIController extends OCSController {
 		}
 
 		if ($include_tags) {
-			$formatted = Helper::populateTags($formatted, 'file_source');
+			$formatted = Helper::populateTags($formatted, 'file_source', \OC::$server->getTagManager());
 		}
 
 		return new DataResponse($formatted);
