6c4b382389a6ce96c51b5d0094df69e42bf10a30
Morris Jobke <hey@morrisjobke.de>
2017-08-30 22:56:14 +0200

    Implement metadata for email templates of remaining emails
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- apps/sharebymail/lib/ShareByMailProvider.php
+++ apps/sharebymail/lib/ShareByMailProvider.php
@@ -462,6 +462,13 @@ class ShareByMailProvider implements IShareProvider {
 		$message = $this->mailer->createMessage();
 
 		$emailTemplate = $this->mailer->createEMailTemplate();
+		$emailTemplate->setMetaData('sharebymail.RecipientPasswordNotification', [
+			'filename' => $filename,
+			'password' => $password,
+			'initiator' => $initiatorDisplayName,
+			'initiatorEmail' => $initiatorEmailAddress,
+			'shareWith' => $shareWith,
+		]);
 		$emailTemplate->addHeader();
 		$emailTemplate->addHeading($this->l->t('Password to access »%s«', [$filename]), false);
 		$emailTemplate->addBodyText($htmlBodyPart, $plainBodyPart);
@@ -523,6 +530,13 @@ class ShareByMailProvider implements IShareProvider {
 
 		$message = $this->mailer->createMessage();
 		$emailTemplate = $this->mailer->createEMailTemplate();
+		$emailTemplate->setMetaData('sharebymail.OwnerPasswordNotification', [
+			'filename' => $filename,
+			'password' => $password,
+			'initiator' => $initiatorDisplayName,
+			'initiatorEmail' => $initiatorEMailAddress,
+			'shareWith' => $shareWith,
+		]);
 
 		$emailTemplate->addHeader();
 		$emailTemplate->addHeading($this->l->t('Password to access »%s«', [$filename]), false);
