9ccf35beeef14064cbb34ddedd0808881df9ecb8
Leon Klingele <git@leonklingele.de>
2017-09-06 21:21:31 +0200

    info.xml: Fix error being displayed on empty 'navigations' section
    This is a fix for #6385
    
    Signed-off-by: Leon Klingele <git@leonklingele.de>

--- lib/private/NavigationManager.php
+++ lib/private/NavigationManager.php
@@ -230,7 +230,7 @@ class NavigationManager implements INavigationManager {
 
 			// load plugins and collections from info.xml
 			$info = $this->appManager->getAppInfo($app);
-			if (empty($info['navigations'])) {
+			if (!isset($info['navigations']) || !is_array($info['navigations'])) {
 				continue;
 			}
 			foreach ($info['navigations'] as $nav) {
