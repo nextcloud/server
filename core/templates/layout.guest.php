699c64c7504ecd1518827e031eac202da1dcd228
Julius Härtl <jus@bitgrid.net>
2017-09-11 15:07:45 +0200

    Add manifest.json to the login page
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -13,6 +13,7 @@
 		<link rel="icon" href="<?php print_unescaped(image_path('', 'favicon.ico')); /* IE11+ supports png */ ?>">
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>">
 		<link rel="mask-icon" sizes="any" href="<?php print_unescaped(image_path('', 'favicon-mask.svg')); ?>" color="<?php p($theme->getColorPrimary()); ?>">
+		<link rel="manifest" href="<?php print_unescaped(image_path('', 'manifest.json')); ?>">
 		<?php emit_css_loading_tags($_); ?>
 		<?php emit_script_loading_tags($_); ?>
 		<?php print_unescaped($_['headers']); ?>
