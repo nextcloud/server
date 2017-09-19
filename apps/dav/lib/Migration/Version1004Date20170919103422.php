740d8201948f16db15798507fa1220cbd9725259
Joas Schilling <coding@schilljs.com>
2017-09-19 12:42:07 +0200

    Migrate ids to bigint in the dav app
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ apps/dav/lib/Migration/Version1004Date20170919103422.php
@@ -0,0 +1,35 @@
+<?php
+namespace OCA\DAV\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use OCP\Migration\BigIntMigration;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version1004Date20170919103422 extends BigIntMigration {
+
+	/**
+	 * @return array Returns an array with the following structure
+	 * ['table1' => ['column1', 'column2'], ...]
+	 * @since 13.0.0
+	 */
+	protected function getColumnsByTable() {
+		return [
+			'addressbooks' => ['id'],
+			'addressbookchanges' => ['id', 'addressbookid'],
+			'calendars' => ['id', 'calendarid'],
+			'calendarchanges' => ['id', 'calendarid'],
+			'calendarobjects' => ['id', 'calendarid'],
+			'calendarobjects_props' => ['id', 'calendarid', 'objectid'],
+			'calendarsubscriptions' => ['id', 'calendarid'],
+			'cards' => ['id', 'addressbookid'],
+			'cards_properties' => ['id', 'addressbookid', 'cardid'],
+			'dav_shares' => ['id', 'resourceid'],
+			'schedulingobjects' => ['id', 'addressbookid'],
+		];
+	}
+
+}
