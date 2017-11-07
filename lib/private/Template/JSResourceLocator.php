06ba1a8a02f08e066ad97cdd192534c296f3a640
Kyle Fazzari <kyrofa@ubuntu.com>
2017-11-07 14:54:21 -0800

    JSResourceLocator: account for symlinks in app path
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- lib/private/Template/JSResourceLocator.php
+++ lib/private/Template/JSResourceLocator.php
@@ -75,9 +75,14 @@ class JSResourceLocator extends ResourceLocator {
 		$app_path = \OC_App::getAppPath($app);
 		$app_url = \OC_App::getAppWebPath($app);
 
+		// Account for the possibility of having symlinks in app path. Doing
+		// this in a separate variable, because an empty argument to realpath
+		// gets turned into cwd, which makes it hard to see if app_path got set.
+		$real_app_path = realpath($app_path);
+
 		// missing translations files fill be ignored
 		if (strpos($script, 'l10n/') === 0) {
-			$this->appendIfExist($app_path, $script . '.js', $app_url);
+			$this->appendIfExist($real_app_path, $script . '.js', $app_url);
 			return;
 		}
 
@@ -89,8 +94,8 @@ class JSResourceLocator extends ResourceLocator {
 			return;
 		}
 
-		if (!$this->cacheAndAppendCombineJsonIfExist($app_path, $script.'.json', $app)) {
-			$this->append($app_path, $script . '.js', $app_url);
+		if (!$this->cacheAndAppendCombineJsonIfExist($real_app_path, $script.'.json', $app)) {
+			$this->append($real_app_path, $script . '.js', $app_url);
 		}
 	}
 
