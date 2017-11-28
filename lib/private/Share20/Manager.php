nextcloud/server

commit 3820d6883dffcaa49deb1054ae0f32ab3d3e39b1

Fix accesslist when a user has an ID only containting 0-9
Signed-off-by: Joas Schilling <coding@schilljs.com>

lib/private/Share20/Manager.php
@@ -1390,7 +1390,7 @@ class Manager implements IManager {
 			foreach ($tmp as $k => $v) {
 				if (isset($al[$k])) {
 					if (is_array($al[$k])) {
-						$al[$k] = array_merge($al[$k], $v);
+						$al[$k] += $v;
 					} else {
 						$al[$k] = $al[$k] || $v;
 					}

commit 80b34f5f7df48a8632fd92116bc14e8a1a7fb3e2

Only in case of $currentAccess the array uses the id as index
Otherwise its a normal string[] with the user ids, in that
case the array_merge did it's job just fine, apart from it
not being deduplicated.
The array+array is only needed when the user id is the key,
so integer only user ids are kept as they are instead of being
reindexed.

Regression from 3820d6883dffcaa49deb1054ae0f32ab3d3e39b1

Signed-off-by: Joas Schilling <coding@schilljs.com>

lib/private/Share20/Manager.php
@@ -1395,7 +1395,13 @@ class Manager implements IManager {
 			foreach ($tmp as $k => $v) {
 				if (isset($al[$k])) {
 					if (is_array($al[$k])) {
-						$al[$k] += $v;
+						if ($currentAccess) {
+							$al[$k] += $v;
+						} else {
+							$al[$k] = array_merge($al[$k], $v);
+							$al[$k] = array_unique($al[$k]);
+							$al[$k] = array_values($al[$k]);
+						}
 					} else {
 						$al[$k] = $al[$k] || $v;
 					}