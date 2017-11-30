d10ba721630f4d9bb216a8f79b73189c5c457980
Morris Jobke <hey@morrisjobke.de>
2017-11-30 16:25:04 +0100

    Release 12.0.4
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(12, 0, 4, 2);
+$OC_Version = array(12, 0, 4, 3);
 
 // The human readable string
-$OC_VersionString = '12.0.4 RC3';
+$OC_VersionString = '12.0.4';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
