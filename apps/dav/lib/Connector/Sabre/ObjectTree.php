d2fe30d464adac9b3a4dc300aad135d1cf682017
Roeland Jago Douma <roeland@famdouma.nl>
2017-12-01 12:34:37 +0100

    Fix tests
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/dav/lib/Connector/Sabre/ObjectTree.php
+++ apps/dav/lib/Connector/Sabre/ObjectTree.php
@@ -156,7 +156,7 @@ class ObjectTree extends CachingTree {
 			try {
 				$info = $this->fileView->getFileInfo($path);
 
-				if ($info->getStorage()->instanceOfStorage(FailedStorage::class)) {
+				if ($info instanceof \OCP\Files\FileInfo && $info->getStorage()->instanceOfStorage(FailedStorage::class)) {
 					throw new StorageNotAvailableException();
 				}
 			} catch (StorageNotAvailableException $e) {
