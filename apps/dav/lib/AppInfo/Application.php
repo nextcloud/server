wibicms/nextcloud-server

Make ContactsStore a public API
Signed-off-by: Tobia De Koninck <tobia@ledfan.be>

apps/dav/lib/AppInfo/Application.php
@@ -72,6 +72,16 @@ class Application extends App {
 		$cm->setupContactsProvider($contactsManager, $userID, $urlGenerator);
 	}
 
+	/**
+	 * @param IManager $contactsManager
+	 */
+	public function setupSystemContactsProvider(IManager $contactsManager) {
+		/** @var ContactsManager $cm */
+		$cm = $this->getContainer()->query(ContactsManager::class);
+		$urlGenerator = $this->getContainer()->getServer()->getURLGenerator();
+		$cm->setupSystemContactsProvider($contactsManager, $urlGenerator);
+	}
+
 	public function registerHooks() {
 		/** @var HookManager $hm */
 		$hm = $this->getContainer()->query(HookManager::class);

nextcloud/server

apps/dav/lib/AppInfo/Application.php
@@ -76,6 +76,16 @@ class Application extends App {
 	}
 
 	/**
+	 * @param IManager $contactsManager
+	 */
+	public function setupSystemContactsProvider(IContactsManager $contactsManager) {
+		/** @var ContactsManager $cm */
+		$cm = $this->getContainer()->query(ContactsManager::class);
+		$urlGenerator = $this->getContainer()->getServer()->getURLGenerator();
+		$cm->setupSystemContactsProvider($contactsManager, $urlGenerator);
+	}
+
+	/**
 	 * @param ICalendarManager $calendarManager
 	 * @param string $userId
 	 */