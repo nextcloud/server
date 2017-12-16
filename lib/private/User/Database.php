ec28c54dbca77a3aad8d34a327c89458889f70ff
Abijeet <abijeetpatro@gmail.com>
2017-12-16 17:18:05 +0530

    Adds search by email function on the users screen.
    Fixes #7175.
    
    - Updated the query to fetch the users in users > everyone tab.
    - Updated the query to fetch the users in users > admin tab.
    - Tested to ensure that the disabled users are also being fetched.
    - Added test cases.
    
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- lib/private/User/Database.php
+++ lib/private/User/Database.php
@@ -194,7 +194,10 @@ class Database extends Backend implements IUserBackend {
 		if ($search !== '') {
 			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
 			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
-			$searchLike = ' WHERE LOWER(`displayname`) LIKE LOWER(?) OR '
+			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
+			$searchLike .= ' LEFT JOIN `*PREFIX*preferences` ON `userid` = `uid` AND `appid` = \'settings\' AND `configkey` = \'email\'';
+			$searchLike .= ' WHERE LOWER(`configvalue`) LIKE LOWER(?)';
+			$searchLike .= ' OR LOWER(`displayname`) LIKE LOWER(?) OR '
 				. 'LOWER(`uid`) LIKE LOWER(?)';
 		}
 
@@ -288,8 +291,14 @@ class Database extends Backend implements IUserBackend {
 		$parameters = [];
 		$searchLike = '';
 		if ($search !== '') {
+			// Email
 			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
-			$searchLike = ' WHERE LOWER(`uid`) LIKE LOWER(?)';
+			$searchLike .= ' LEFT JOIN `*PREFIX*preferences` ON `userid` = `uid` AND `appid` = \'settings\' AND `configkey` = \'email\'';
+			$searchLike .= ' WHERE LOWER(`configvalue`) LIKE LOWER(?)';
+			// UID
+			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
+			$searchLike .= ' OR LOWER(`uid`) LIKE LOWER(?)';
+			// Display name
 			$parameters[] = '%' . \OC::$server->getDatabaseConnection()->escapeLikeParameter($search) . '%';
 			$searchLike .= ' OR LOWER(`displayname`) LIKE LOWER(?)';
 		}
