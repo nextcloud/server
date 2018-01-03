1a788eb01fe6d0a130fec66fa6344a48b14547c1
Arne Hamann <kontakt+github@arne.email>
2018-01-03 21:36:36 +0530

    changed == to ===
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

--- core/templates/untrustedDomain.php
+++ core/templates/untrustedDomain.php
@@ -11,11 +11,11 @@
 	</p>
 	<p style="text-align:center;">
 		<a href="<?php
-				if (\OC::$server->getConfig()->getSystemValue('overwrite.cli.url') == '') {
+				if (\OC::$server->getConfig()->getSystemValue('overwrite.cli.url') === '') {
 					print_unescaped(\OC::$server->getURLGenerator()->linkToRouteAbsolute('settings.AdminSettings.index', $arguments = [trustDomain => $_['domain'], ]));
 				} else {
 					$link = substr(\OC::$server->getURLGenerator()->linkToRoute('settings.AdminSettings.index',$arguments = [trustDomain => $_['domain']]),strlen(\OC::$WEBROOT));
-					$separator = $link[0] == '/' ? '' : '/';
+					$separator = $link[0] === '/' ? '' : '/';
 					print_unescaped(\OC::$server->getConfig()->getSystemValue('overwrite.cli.url').$separator.$link);
 				}?>" class="button">
 			<?php p($l->t('Add "%s" as trusted domain', array($_['domain']))); ?>
