20f1523c0e13632708526f4d78de0333fa57b1a9
Morris Jobke <hey@morrisjobke.de>
2018-01-12 15:24:28 +0100

    Log full exception in cron instead of only the message
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- cron.php
+++ cron.php
@@ -155,7 +155,7 @@ try {
 	exit();
 
 } catch (Exception $ex) {
-	\OCP\Util::writeLog('cron', $ex->getMessage(), \OCP\Util::FATAL);
+	\OC::$server->getLogger()->logException($ex, ['app' => 'cron']);
 } catch (Error $ex) {
-	\OCP\Util::writeLog('cron', $ex->getMessage(), \OCP\Util::FATAL);
+	\OC::$server->getLogger()->logException($ex, ['app' => 'cron']);
 }
