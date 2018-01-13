48ea7251d74302993bbd1a86d7c01b45417c1965
Roeland Jago Douma <roeland@famdouma.nl>
2018-01-13 20:36:02 +0100

    Use \OC\User\Manager internally in the avatarmanager
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/AvatarManager.php
+++ lib/private/AvatarManager.php
@@ -28,13 +28,13 @@ declare(strict_types=1);
 
 namespace OC;
 
+use OC\User\Manager;
 use OCP\Files\IAppData;
 use OCP\Files\NotFoundException;
 use OCP\IAvatar;
 use OCP\IAvatarManager;
 use OCP\IConfig;
 use OCP\ILogger;
-use OCP\IUserManager;
 use OCP\IL10N;
 
 /**
@@ -42,7 +42,7 @@ use OCP\IL10N;
  */
 class AvatarManager implements IAvatarManager {
 
-	/** @var  IUserManager */
+	/** @var Manager */
 	private $userManager;
 
 	/** @var IAppData */
@@ -60,14 +60,14 @@ class AvatarManager implements IAvatarManager {
 	/**
 	 * AvatarManager constructor.
 	 *
-	 * @param IUserManager $userManager
+	 * @param Manager $userManager
 	 * @param IAppData $appData
 	 * @param IL10N $l
 	 * @param ILogger $logger
 	 * @param IConfig $config
 	 */
 	public function __construct(
-			IUserManager $userManager,
+			Manager $userManager,
 			IAppData $appData,
 			IL10N $l,
 			ILogger $logger,
