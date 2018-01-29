96480af159f57fd670cd12056e7a13ec76ac6d36
Julius Härtl <jus@bitgrid.net>
2018-01-29 12:04:45 +0100

    Add navigation endpoint
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ core/Controller/NavigationController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OC\Core\Controller;
+
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\INavigationManager;
+use OCP\IRequest;
+
+class NavigationController extends Controller {
+
+	/** @var INavigationManager */
+	private $navigationManager;
+
+	public function __construct(IRequest $request, INavigationManager $navigationManager) {
+		parent::__construct('core', $request);
+		$this->navigationManager = $navigationManager;
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 *
+	 * @return JSONResponse
+	 */
+	public function getAppsNavigation($absolute = false) {
+		return new JSONResponse($this->navigationManager->getAll('link', $absolute));
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 *
+	 * @return JSONResponse
+	 */
+	public function getSettingsNavigation($absolute = false) {
+		return new JSONResponse($this->navigationManager->getAll('settings', $absolute));
+	}
+}
