e1762998b3a7a453b033340c62a3915b7288a95d
michaelletzgus <michaelletzgus@users.noreply.github.com>
2018-02-04 15:59:23 +0100

    Fix undefined index problem
    Nextcloud 13RC4, error in logfile, triggered by "occ config:list":
    
    Invalid argument supplied for foreach() at lib/private/AppConfig.php#297
    PHP	Undefined index: workflowengine at lib/private/AppConfig.php#297
    
    Fix: Check if index exists in array before using it.

--- lib/private/AppConfig.php
+++ lib/private/AppConfig.php
@@ -293,9 +293,11 @@ class AppConfig implements IAppConfig {
 	public function getFilteredValues($app) {
 		$values = $this->getValues($app, false);
 
-		foreach ($this->sensitiveValues[$app] as $sensitiveKey) {
-			if (isset($values[$sensitiveKey])) {
-				$values[$sensitiveKey] = IConfig::SENSITIVE_VALUE;
+		if (array_key_exists($app, $this->sensitiveValues)) {
+			foreach ($this->sensitiveValues[$app] as $sensitiveKey) {
+				if (isset($values[$sensitiveKey])) {
+					$values[$sensitiveKey] = IConfig::SENSITIVE_VALUE;
+				}
 			}
 		}
 
