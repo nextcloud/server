3384e251542b07c633f6e72aeecea941bf9ce62b
Roland Tapken <roland@bitarbeiter.net>
2018-02-08 13:14:57 +0100

    Fixed unit test: groupsMatchFilter will not be called multiple times anymore.
    Signed-off-by: Roland Tapken <roland@bitarbeiter.net>

--- apps/user_ldap/tests/Group_LDAPTest.php
+++ apps/user_ldap/tests/Group_LDAPTest.php
@@ -625,7 +625,7 @@ class Group_LDAPTest extends TestCase {
 			->method('dn2groupname')
 			->will($this->returnArgument(0));
 
-		$access->expects($this->exactly(3))
+		$access->expects($this->exactly(1))
 			->method('groupsMatchFilter')
 			->will($this->returnArgument(0));
 
