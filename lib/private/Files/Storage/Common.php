dc99442bc16972046522888002ad379c170c138a
Roland Tapken <roland@bitarbeiter.net>
2018-02-09 17:30:27 +0100

    External storage test: log exceptions
    If an external storage test fails the exception's message is now written
    into the logfile (level INFO). Additionally, the stack trace is printed
    as DEBUG. This helps to resolve the reason for a failing connection.
    
    Signed-off-by: Roland Tapken <roland@bitarbeiter.net>

--- lib/private/Files/Storage/Common.php
+++ lib/private/Files/Storage/Common.php
@@ -449,8 +449,11 @@ abstract class Common implements Storage, ILockingStorage {
 			if ($this->stat('')) {
 				return true;
 			}
+			\OC::$server->getLogger()->info("External storage not available: stat() failed");
 			return false;
 		} catch (\Exception $e) {
+			\OC::$server->getLogger()->info("External storage not available: " . $e->getMessage());
+			\OC::$server->getLogger()->logException($e, ['level' => \OCP\Util::DEBUG]);
 			return false;
 		}
 	}
