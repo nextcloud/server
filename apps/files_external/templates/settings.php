887241b355b76a22292525ea0463281337d70212
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-02-22 18:14:19 +0100

    Show error if no external storage configured
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/files_external/templates/settings.php
+++ apps/files_external/templates/settings.php
@@ -87,6 +87,12 @@
 	}
 ?>
 
+<div id="emptycontent" class="hidden">
+	<div class="icon-external"></div>
+	<h2><?php p($l->t('No external storage configured')); ?></h2>
+	<p><a href="<?php p(link_to('', 'index.php/settings/personal#files_external' )); ?>"><?php p($l->t('You can add external storages in the personal settings')); ?></a></p>
+</div>
+
 <form data-can-create="<?php echo $canCreateMounts?'true':'false' ?>" id="files_external" class="section" data-encryption-enabled="<?php echo $_['encryptionEnabled']?'true': 'false'; ?>">
 	<h2 data-anchor-name="external-storage"><?php p($l->t('External storages')); ?></h2>
 	<?php if (isset($_['dependencies']) and ($_['dependencies'] !== '') and $canCreateMounts) print_unescaped(''.$_['dependencies'].''); ?>
