4fc4e6dcaa8959991f34d2a78ba6db1f3b555fd0
rakekniven <mark.ziegler@rakekniven.de>
2018-03-02 17:46:12 +0100

    Update ThemingController.php
    Improved translation string.
    Reported at Transifex. See https://www.transifex.com/nextcloud/nextcloud/translate/#de/$/121491256?issue=yes

--- apps/theming/lib/Controller/ThemingController.php
+++ apps/theming/lib/Controller/ThemingController.php
@@ -221,7 +221,7 @@ class ThemingController extends Controller {
 			UPLOAD_ERR_OK => $this->l10n->t('There is no error, the file uploaded with success'),
 			UPLOAD_ERR_INI_SIZE => $this->l10n->t('The uploaded file exceeds the upload_max_filesize directive in php.ini'),
 			UPLOAD_ERR_FORM_SIZE => $this->l10n->t('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form'),
-			UPLOAD_ERR_PARTIAL => $this->l10n->t('The uploaded file was only partially uploaded'),
+			UPLOAD_ERR_PARTIAL => $this->l10n->t('The file was only partially uploaded'),
 			UPLOAD_ERR_NO_FILE => $this->l10n->t('No file was uploaded'),
 			UPLOAD_ERR_NO_TMP_DIR => $this->l10n->t('Missing a temporary folder'),
 			UPLOAD_ERR_CANT_WRITE => $this->l10n->t('Failed to write file to disk.'),
