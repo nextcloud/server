97c4c00e3f3e1bc9561953fb110b83c11eda1d2c
Joas Schilling <coding@schilljs.com>
2018-03-05 16:06:29 +0100

    Better debugging for "Your test case is not allowed to access the database."
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/AppFramework/Utility/SimpleContainer.php
+++ lib/private/AppFramework/Utility/SimpleContainer.php
@@ -66,6 +66,12 @@ class SimpleContainer extends Container implements IContainer {
 				try {
 					$parameters[] = $this->query($resolveName);
 				} catch (\Exception $e) {
+					if (class_exists("PHPUnit_Framework_AssertionFailedError") &&
+						$e instanceof \PHPUnit_Framework_AssertionFailedError) {
+						// Easier debugging of "Your test case is not allowed to access the database."
+						throw $e;
+					}
+
 					// Service not found, use the default value when available
 					if ($parameter->isDefaultValueAvailable()) {
 						$parameters[] = $parameter->getDefaultValue();
