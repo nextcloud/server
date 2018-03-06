a6458745bd5ae49cedf6e5c60df6c8c69b5ef93c
Joas Schilling <coding@schilljs.com>
2018-03-06 14:19:47 +0100

    Fix code errors
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/workflowengine/js/requestuseragentplugin.js
+++ apps/workflowengine/js/requestuseragentplugin.js
@@ -44,7 +44,7 @@
 
 			var placeholder = 'Mozilla/5.0 User Agent';
 
-			if (check['operator'] === 'matches' || check['operator'] === '!matches') {
+			if (check.operator === 'matches' || check.operator === '!matches') {
 				placeholder = '/^Mozilla\\/5\\.0 (.*)$/i';
 			}
 
@@ -56,8 +56,8 @@
 					placement: 'bottom'
 				});
 
-			if (check['operator'] === 'matches' || check['operator'] === '!matches') {
-				if (this._validateRegex(check['value'])) {
+			if (check.operator === 'matches' || check.operator === '!matches') {
+				if (this._validateRegex(check.value)) {
 					$(element).removeClass('invalid-input');
 				} else {
 					$(element).addClass('invalid-input');
@@ -74,17 +74,17 @@
 						]
 					}
 				];
-				if (this.predefinedValues.indexOf(check['value']) === -1) {
+				if (this.predefinedValues.indexOf(check.value) === -1) {
 					data.unshift({
-						id: check['value'],
-						text: check['value']
-					})
+						id: check.value,
+						text: check.value
+					});
 				}
 
 				$(element).select2({
 					data: data,
 					createSearchChoice: function(term) {
-						if (self.predefinedValues.indexOf(check['value']) === -1) {
+						if (self.predefinedValues.indexOf(check.value) === -1) {
 							return {
 								id: term,
 								text: term
