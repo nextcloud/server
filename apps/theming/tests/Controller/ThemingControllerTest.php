789221a0fcef435ae95b6aec22382b6757dd0469
rakekniven <mark.ziegler@rakekniven.de>
2018-03-19 15:07:49 +0100

    Update ThemingControllerTest.php
    Adjusted test

--- apps/theming/tests/Controller/ThemingControllerTest.php
+++ apps/theming/tests/Controller/ThemingControllerTest.php
@@ -529,8 +529,8 @@ class ThemingControllerTest extends TestCase {
 			[UPLOAD_ERR_PARTIAL, 'The file was only partially uploaded'],
 			[UPLOAD_ERR_NO_FILE, 'No file was uploaded'],
 			[UPLOAD_ERR_NO_TMP_DIR, 'Missing a temporary folder'],
-			[UPLOAD_ERR_CANT_WRITE, 'Failed to write file to disk.'],
-			[UPLOAD_ERR_EXTENSION, 'A PHP extension stopped the file upload.'],
+			[UPLOAD_ERR_CANT_WRITE, 'Could not write file to disk'],
+			[UPLOAD_ERR_EXTENSION, 'A PHP extension stopped the file upload'],
 		];
 	}
 
