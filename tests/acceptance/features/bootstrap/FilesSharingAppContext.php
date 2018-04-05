5598d30ef1bf1f4b4dc763b9528cf8bc2b6e34ed
Julius Härtl <jus@bitgrid.net>
2018-04-05 21:10:33 +0200

    Fix files acceptance test
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/acceptance/features/bootstrap/FilesSharingAppContext.php
+++ tests/acceptance/features/bootstrap/FilesSharingAppContext.php
@@ -90,7 +90,7 @@ class FilesSharingAppContext implements Context, ActorAwareInterface {
 	 * @return Locator
 	 */
 	public static function saveItemInShareMenu() {
-		return Locator::forThe()->id("save")->
+		return Locator::forThe()->id("save-external-share")->
 				descendantOf(self::shareMenu())->
 				describedAs("Save item in Share menu in Shared file page");
 	}
