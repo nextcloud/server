1c00f82ea5b6502a0cb4ac82c80927ca88c58105
Jan-Christoph Borchardt <hey@jancborchardt.net>
2018-04-18 15:51:52 +0200

    Do not restrict zooming in
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -10,7 +10,7 @@
 		</title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="referrer" content="no-referrer">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
