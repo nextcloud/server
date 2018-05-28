29b777e7d1d26a3b75b0aabcca394170e6043939
Georg Ehrke <developer@georgehrke.com>
2018-05-28 14:37:29 +0200

    add resource and room interfaces
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

---
+++ lib/public/Calendar/Room/IBackend.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @copyright 2018, Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCP\Calendar\Room;
+
+/**
+ * Interface IBackend
+ *
+ * @package OCP\Calendar\Room
+ * @since 14.0.0
+ */
+interface IBackend {
+
+	/**
+	 * get a list of all rooms in this backend
+	 *
+	 * @return IRoom[]
+	 */
+	public function getAllRooms();
+
+	/**
+	 * get a list of all room identifiers in this backend
+	 *
+	 * @return string[]
+	 */
+	public function listAllRooms();
+
+	/**
+	 * get a room by it's id
+	 *
+	 * @param string $id
+	 * @return IRoom|null
+	 */
+	public function getRoom($id);
+}
