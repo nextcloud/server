749fd5ffd6740cd3adefc421f939be54649a558a
fnuesse <fnuesse@techfak.uni-bielefeld.de>
2018-06-03 19:16:20 +0200

    Revert webdavurl which was changed by mistake

--- apps/files/templates/appnavigation.php
+++ apps/files/templates/appnavigation.php
@@ -91,7 +91,7 @@
 				<label for="showfavoritequickaccessToggle"><?php p($l->t('Enable Favorites Quick Access')); ?></label>
 			</div>
 			<label for="webdavurl"><?php p($l->t('WebDAV'));?></label>
-			<input id="webdavurl" type="text" readonly="readonly" value="<?php p(\OCP\Util::linkToRemote('webdav')); ?>" />
+			<input id="webdavurl" type="text" readonly="readonly" value="<?php p($_['webdavurl']); ?>" />
 			<em><?php print_unescaped($l->t('Use this address to <a href="%s" target="_blank" rel="noreferrer noopener">access your Files via WebDAV</a>', array(link_to_docs('user-webdav'))));?></em>
 		</div>
 	</div>
