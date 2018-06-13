fd4330c38fcb35c3a4d0cf1807a6cacea63636c6
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-06-13 22:07:40 +0200

    Fix indent
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- lib/public/IAvatar.php
+++ lib/public/IAvatar.php
@@ -37,59 +37,59 @@ use OCP\Files\NotFoundException;
  */
 interface IAvatar {
 
-    /**
-     * get the users avatar
-     * @param int $size size in px of the avatar, avatars are square, defaults to 64, -1 can be used to not scale the image
-     * @return boolean|\OCP\IImage containing the avatar or false if there's no image
-     * @since 6.0.0 - size of -1 was added in 9.0.0
-     */
-    public function get($size = 64);
+	/**
+	 * get the users avatar
+	 * @param int $size size in px of the avatar, avatars are square, defaults to 64, -1 can be used to not scale the image
+	 * @return boolean|\OCP\IImage containing the avatar or false if there's no image
+	 * @since 6.0.0 - size of -1 was added in 9.0.0
+	 */
+	public function get($size = 64);
 
-    /**
-     * Check if an avatar exists for the user
-     *
-     * @return bool
-     * @since 8.1.0
-     */
-    public function exists();
+	/**
+	 * Check if an avatar exists for the user
+	 *
+	 * @return bool
+	 * @since 8.1.0
+	 */
+	public function exists();
 
-    /**
-     * sets the users avatar
-     * @param \OCP\IImage|resource|string $data An image object, imagedata or path to set a new avatar
-     * @throws \Exception if the provided file is not a jpg or png image
-     * @throws \Exception if the provided image is not valid
-     * @throws \OC\NotSquareException if the image is not square
-     * @return void
-     * @since 6.0.0
-     */
-    public function set($data);
+	/**
+	 * sets the users avatar
+	 * @param \OCP\IImage|resource|string $data An image object, imagedata or path to set a new avatar
+	 * @throws \Exception if the provided file is not a jpg or png image
+	 * @throws \Exception if the provided image is not valid
+	 * @throws \OC\NotSquareException if the image is not square
+	 * @return void
+	 * @since 6.0.0
+	 */
+	public function set($data);
 
-    /**
-     * remove the users avatar
-     * @return void
-     * @since 6.0.0
-     */
-    public function remove();
+	/**
+	 * remove the users avatar
+	 * @return void
+	 * @since 6.0.0
+	 */
+	public function remove();
 
-    /**
-     * Get the file of the avatar
-     * @param int $size -1 can be used to not scale the image
-     * @return File
-     * @throws NotFoundException
-     * @since 9.0.0
-     */
-    public function getFile($size);
+	/**
+	 * Get the file of the avatar
+	 * @param int $size -1 can be used to not scale the image
+	 * @return File
+	 * @throws NotFoundException
+	 * @since 9.0.0
+	 */
+	public function getFile($size);
 
 	/**
 	 * @param string $text
 	 * @return Color Object containting r g b int in the range [0, 255]
-     * @since 14.0.0
+	 * @since 14.0.0
 	 */
 	public function avatarBackgroundColor(string $text);
 
-    /**
-     * Handle a changed user
-     * @since 13.0.0
-     */
-    public function userChanged($feature, $oldValue, $newValue);
+	/**
+	 * Handle a changed user
+	 * @since 13.0.0
+	 */
+	public function userChanged($feature, $oldValue, $newValue);
 }
