916fbd7f9302f5d7ee523b7c23e605d60512d8ad
fnuesse <felix.nuesse@t-online.de>
2018-06-17 21:15:10 +0200

    Refactored Code
    Signed-off-by: fnuesse <felix.nuesse@t-online.de>

--- apps/files/templates/appnavigation.php
+++ apps/files/templates/appnavigation.php
@@ -5,14 +5,14 @@
 		<?php foreach ($_['navigationItems'] as $item) {
 			strpos($item['classes'], 'pinned')!==false ? $pinned++ : '';
 			?>
+
 			<li data-id="<?php p(isset($item['href']) ? $item['href'] : $item['id']) ?>"
 				class="nav-<?php p($item['id']) ?> <?php p($item['classes']) ?> <?php p($pinned===1?'first-pinned':'') ?>"
 				<?php if(isset($item['folderPosition'])){ ?> folderPos="<?php p($item['folderPosition']);?>"<?php } ?>>
+
 				<a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"
 				   class="nav-icon-<?php p($item['icon'] !== '' ? $item['icon'] : $item['id']) ?> svg"><?php p($item['name']);?></a>
 				<?php if($item['id']=='favorites'){?>
-<!--				<button id="button---><?php //p($item['id']) ?><!--" class="app-navigation-entry-utils-menu-button icon icon-more"></button>-->
-
 
 				<div class="app-navigation-entry-utils">
 					<ul>
@@ -20,50 +20,41 @@
 					</ul>
 				</div>
 				<div class="app-navigation-entry-menu" id="menu-<?php p($item['id']) ?>">
-						<ul>
-							<li>
-								<span class="menuitem">
-									<input id="enableQuickAccess" type="checkbox" class="checkbox"  <?php if($item['enableQuickaccess']=='true'){ ?> checked<?php } ?>/>
-									<label for="enableQuickAccess"><?php p($l->t('Enable Quickaccess')); ?></label>
-								</span>
-							</li>
-							<li>
-								<span class="menuitem">
-									<input id="sortByAlphabet" type="checkbox" class="checkbox" data-group='SortingStrategy'<?php if($item['quickaccessSortingStrategy']=='alphabet'){ ?> checked<?php } ?>/>
-									<label for="sortByAlphabet"><?php p($l->t('Sort by Alphabet')); ?></label>
-								</span>
-							</li>
-							<li>
-								<span class="menuitem">
-									<input id="sortByDate" type="checkbox" class="checkbox" data-group='SortingStrategy'<?php if($item['quickaccessSortingStrategy']=='date'){ ?> checked<?php } ?>/>
-									<label for="sortByDate"><?php p($l->t('Sort by Date')); ?></label>
-								</span>
-							</li>
-							<li>
-								<span class="menuitem">
-									<input id="enableReverse" type="checkbox" class="checkbox" <?php if($item['quickaccessSortingReverse']==true){ ?> checked<?php } ?>/>
-									<label for="enableReverse"><?php p($l->t('Reverse List')); ?></label>
-								</span>
-							</li>
-						</ul>
+					<ul>
+						<li>
+							<span class="menuitem">
+								<input id="enableQuickAccess" type="checkbox" class="checkbox"  <?php if($item['enableQuickaccess']=='true'){ ?> checked<?php } ?>/>
+								<label for="enableQuickAccess"><?php p($l->t('Enable Quickaccess')); ?></label>
+							</span>
+						</li>
+						<li>
+							<span class="menuitem">
+								<input id="sortByAlphabet" type="checkbox" class="checkbox" data-group='SortingStrategy'<?php if($item['quickaccessSortingStrategy']=='alphabet'){ ?> checked<?php } ?>/>
+								<label for="sortByAlphabet"><?php p($l->t('Sort by Alphabet')); ?></label>
+							</span>
+						</li>
+						<li>
+							<span class="menuitem">
+								<input id="sortByDate" type="checkbox" class="checkbox" data-group='SortingStrategy'<?php if($item['quickaccessSortingStrategy']=='date'){ ?> checked<?php } ?>/>
+								<label for="sortByDate"><?php p($l->t('Sort by Date')); ?></label>
+							</span>
+						</li>
+						<li>
+							<span class="menuitem">
+								<input id="enableReverse" type="checkbox" class="checkbox" <?php if($item['quickaccessSortingReverse']==true){ ?> checked<?php } ?>/>
+								<label for="enableReverse"><?php p($l->t('Reverse List')); ?></label>
+							</span>
+						</li>
+					</ul>
 				</div>
-
-					<ul id="quickaccess-list"  <?php if($item['enableQuickaccess']=='false'){ ?> style="display: none"<?php } ?>>
+				<ul id="quickaccess-list"  <?php if($item['enableQuickaccess']=='false'){ ?> style="display: none"<?php } ?>>
 				<?php } ?>
 				<?php if($item['quickaccesselement']=='last'){?>
-					</ul>
+				</ul>
 				<?php } ?>
 			</li>
 
-
-
-
 		<?php } ?>
-
-
-
-
-
 
 		<li id="quota" class="pinned <?php p($pinned===0?'first-pinned ':'') ?><?php
 		if ($_['quota'] !== \OCP\Files\FileInfo::SPACE_UNLIMITED) {
@@ -94,11 +85,6 @@
 				<input class="checkbox" id="showhiddenfilesToggle" checked="checked" type="checkbox">
 				<label for="showhiddenfilesToggle"><?php p($l->t('Show hidden files')); ?></label>
 			</div>
-			<!--Removed to disable disabling, but kept to allow easy reenabling-->
-			<!--<div id="files-setting-showFavoriteQuickAccess">
-				<input class="checkbox" id="showfavoritequickaccessToggle" <?php /*if($_['showQuickAccess']){ */?>checked="checked"<?php /*} */?> type="checkbox">
-				<label for="showfavoritequickaccessToggle"><?php /*p($l->t('Enable Favorites Quick Access')); */?></label>
-			</div>-->
 			<label for="webdavurl"><?php p($l->t('WebDAV'));?></label>
 			<input id="webdavurl" type="text" readonly="readonly" value="<?php p(\OCP\Util::linkToRemote('webdav')); ?>" />
 			<em><?php print_unescaped($l->t('Use this address to <a href="%s" target="_blank" rel="noreferrer noopener">access your Files via WebDAV</a>', array(link_to_docs('user-webdav'))));?></em>
