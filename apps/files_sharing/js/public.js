a596b062f520469ca52eed10a407fc4cf8891239
Roeland Jago Douma <roeland@famdouma.nl>
2018-06-20 17:54:11 +0200

    Fix index.php links
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/files_sharing/js/public.js
+++ apps/files_sharing/js/public.js
@@ -157,7 +157,7 @@ OCA.Sharing.PublicApp = {
 			imgcontainer.appendTo('#imgframe');
 		}
 		else if (previewSupported === 'true') {
-			$('#imgframe > video').attr('poster', OC.linkTo('files_sharing', '/publicpreview/'+token) + '?' + OC.buildQueryString(params));
+			$('#imgframe > video').attr('poster', OC.generateUrl(OC.linkTo('files_sharing', '/publicpreview/'+token)) + '?' + OC.buildQueryString(params));
 		}
 
 		if (this.fileList) {
@@ -223,7 +223,7 @@ OCA.Sharing.PublicApp = {
 				urlSpec.x = Math.ceil(urlSpec.x);
 				urlSpec.y = Math.ceil(urlSpec.y);
 				var token = $('#dirToken').val();
-				return OC.linkTo('files_sharing', '/publicpreview/'+token) + '?' + OC.buildQueryString(urlSpec);
+				return OC.generateUrl(OC.linkTo('files_sharing', '/publicpreview/'+token) + '?' + OC.buildQueryString(urlSpec));
 			};
 
 			this.fileList.updateEmptyContent = function() {
