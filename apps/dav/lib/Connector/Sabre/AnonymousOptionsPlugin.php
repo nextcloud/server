b21bec5c738da812ef2df744416feaa33ce6a328
Robin Appelman <robin@icewind.nl>
2018-06-28 14:53:44 +0200

    Fix search related results in OPTIONS requests
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/dav/lib/Connector/Sabre/AnonymousOptionsPlugin.php
+++ apps/dav/lib/Connector/Sabre/AnonymousOptionsPlugin.php
@@ -56,6 +56,9 @@ class AnonymousOptionsPlugin extends ServerPlugin {
 			// setup a fake tree for anonymous access
 			$this->server->tree = new Tree(new Directory(''));
 			$corePlugin->httpOptions($request, $response);
+			$this->server->emit('afterMethod', [$request, $response]);
+			$this->server->emit('afterMethod:OPTIONS', [$request, $response]);
+
 			$this->server->sapi->sendResponse($response);
 			return false;
 		}
