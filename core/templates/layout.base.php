7b848368d52ea84f3ce8e8b8792263de9f394c60
Roeland Jago Douma <roeland@famdouma.nl>
2018-07-06 09:23:01 +0200

    Add locale to templates
    It was already added to the user template but we should add it to all
    templates. Else apps that want the locale but use special templating (hi
    richdocuments!) will fail.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- core/templates/layout.base.php
+++ core/templates/layout.base.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="ng-csp" data-placeholder-focus="false" lang="<?php p($_['language']); ?>" >
+<html class="ng-csp" data-placeholder-focus="false" lang="<?php p($_['language']); ?>" data-locale="<?php p($_['locale']); ?>" >
 	<head data-requesttoken="<?php p($_['requesttoken']); ?>">
 		<meta charset="utf-8">
 		<title>
