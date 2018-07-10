ced87feafd564f4ff7dce0f1e20560f9dc88a7bd
Morris Jobke <hey@morrisjobke.de>
2018-07-10 09:00:55 +0200

    12.0.10 RC 1
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(12, 0, 9, 0);
+$OC_Version = array(12, 0, 10, 0);
 
 // The human readable string
-$OC_VersionString = '12.0.9';
+$OC_VersionString = '12.0.10 RC 1';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
