03971d3695f1abe7ec7012196447b07151768731
Julius Härtl <jus@bitgrid.net>
2018-08-01 09:40:21 +0200

    Move encryption settings to security
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/Settings/Manager.php
+++ lib/private/Settings/Manager.php
@@ -230,7 +230,6 @@ class Manager implements IManager {
 			1 => [new Section('server', $this->l->t('Basic settings'), 0, $this->url->imagePath('core', 'actions/settings-dark.svg'))],
 			5 => [new Section('sharing', $this->l->t('Sharing'), 0, $this->url->imagePath('core', 'actions/share.svg'))],
 			10 => [new Section('security', $this->l->t('Security'), 0, $this->url->imagePath('core', 'actions/password.svg'))],
-			45 => [new Section('encryption', $this->l->t('Encryption'), 0, $this->url->imagePath('core', 'actions/password.svg'))],
 			50 => [new Section('groupware', $this->l->t('Groupware'), 0, $this->url->imagePath('core', 'places/contacts.svg'))],
 			98 => [new Section('additional', $this->l->t('Additional settings'), 0, $this->url->imagePath('core', 'actions/settings-dark.svg'))],
 		];
@@ -270,7 +269,7 @@ class Manager implements IManager {
 			$form = new Admin\Mail($this->config);
 			$forms[$form->getPriority()] = [$form];
 		}
-		if ($section === 'encryption') {
+		if ($section === 'security') {
 			/** @var ISettings $form */
 			$form = new Admin\Encryption($this->encryptionManager, $this->userManager);
 			$forms[$form->getPriority()] = [$form];
