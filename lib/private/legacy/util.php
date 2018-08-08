c74a5250e5b2a34ee8e070cfc6ee7fac8c06891b
Roeland Jago Douma <roeland@famdouma.nl>
2018-08-08 13:32:03 +0200

    Check if HTTP_USER_AGENT is set before using it
    Sentry reported some errors regarding this. Apparently not everybody
    sets a user agent. If it is not set we assume this is not IE ;)
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/legacy/util.php
+++ lib/private/legacy/util.php
@@ -1535,6 +1535,10 @@ class OC_Util {
 	 * @return boolean
 	 */
 	public static function isIe() {
+		if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+			return false;
+		}
+
 		return preg_match(Request::USER_AGENT_IE, $_SERVER['HTTP_USER_AGENT']) === 1;
 	}
 
