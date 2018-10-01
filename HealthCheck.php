537a34c92229e39fe6a430af67c87b34f9a9e7f2
voxdemonix <voxdemonix@users.noreply.github.com>
2018-10-01 10:53:45 +0200

    reduce code & impact
    tested on web cluster

--- HealthCheck.php
+++ HealthCheck.php
@@ -3,7 +3,7 @@
 * - Send http 204 when all working
 * - Send http 503 if not
 * Just add following line in your HAProxy Backend for accept only working webserver node
-*		http-check expect status 204
+*               http-check expect status 204
 */
 
 require('./config/config.php');
@@ -11,18 +11,12 @@ $port = "";
 if (!empty($CONFIG["dbport"])){
         $port = ":".$CONFIG["dbport"];
 }
-$dsn = $CONFIG["dbtype"].':host='.$CONFIG["dbhost"].$port.';dbname='.$CONFIG["dbname"];
-
 try{
- // create a PDO connection with the configuration data
- $conn = new PDO($dsn, $CONFIG["dbuser"], $CONFIG["dbpassword"]);
- 
- // display a message if connected to database successfully
+ $conn = new PDO($CONFIG["dbtype"].':host='.$CONFIG["dbhost"].$port.';dbname='.$CONFIG["dbname"], $CONFIG["dbuser"], $CONFIG["dbpassword"]);
  if($conn){
         header('HTTP/1.1 204');
         }
 }catch (PDOException $e){
-                // report error message
-        echo '<html><head><title>503 Service Unavailable</title></head><body><h1>503 Service Unavailable</h1></body></html>'; header('HTTP/1.1 503');
+        header('HTTP/1.1 503');
 }
 ?>
