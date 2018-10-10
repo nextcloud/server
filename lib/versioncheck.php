e7d20cdb5e8b7be335b73d3f67dffcef4bec31e2
Roeland Jago Douma <roeland@famdouma.nl>
2018-10-10 21:32:06 +0200

    Allow php7.3
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/versioncheck.php
+++ lib/versioncheck.php
@@ -9,10 +9,10 @@ if (version_compare(PHP_VERSION, '7.0') === -1) {
 	exit(-1);
 }
 
-// Show warning if > PHP 7.2 is used as Nextcloud is not compatible with > PHP 7.2 for now
-if (version_compare(PHP_VERSION, '7.3.0') !== -1) {
+// Show warning if > PHP 7.3 is used as Nextcloud is not compatible with > PHP 7.3 for now
+if (version_compare(PHP_VERSION, '7.4.0') !== -1) {
 	http_response_code(500);
-	echo 'This version of Nextcloud is not compatible with > PHP 7.2.<br/>';
+	echo 'This version of Nextcloud is not compatible with > PHP 7.3.<br/>';
 	echo 'You are currently running ' . PHP_VERSION . '.';
 	exit(-1);
 }
