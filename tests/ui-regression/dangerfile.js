774e48e321ff437af357469ea6bd4318ded36223
Julius Härtl <jus@bitgrid.net>
2018-10-11 14:59:43 +0200

    Fix screenshot rendering
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/dangerfile.js
+++ tests/ui-regression/dangerfile.js
@@ -3,45 +3,54 @@ const {readFileSync} = require('fs');
 const includes = require('lodash.includes');
 const minimatch = require('minimatch');
 
-// Now do work against our lists of files
-const currentBuildResults = JSON.parse(
-	readFileSync('./out/install.json')
-);
-
-let failingTests = currentBuildResults.failures;
-if (failingTests.length > 0) {
-	warn(`There are some ui comparison tests failing. Please have a look at ` +
-	     `https://ci-assets.nextcloud.com/nextcloud-ui-regression/nextcloud/server/${danger.github.pr.number}/index.html to check if your PR introduces an UI regression.`)
-}
-
-function getImagePath(test, type) {
+function getImagePath(suite, test, type) {
 	return 'https://ci-assets.nextcloud.com/nextcloud-ui-regression/nextcloud/server/' + 
 		process.env.DRONE_PULL_REQUEST  + '/' + 
-		test.suite + '/' + test.title + type + '.png';
+		suite + '/' + test.title + type + '.png';
 }
 
-failingTests.forEach(test => {
-	const url = 'https://ci-assets.nextcloud.com/nextcloud-ui-regression/nextcloud/server/10507/index.html#' + 
-		test.fullTitle.replace(/\W/g, '_');
-	const jsonData = JSON.stringify(test, null, 2);
+function runDangerForSuite(suite) {
+	const currentBuildResults = JSON.parse(
+		readFileSync(`./out/${suite}.json`)
+	);
 
-	const base = getImagePath(test, '.base');
-	const diff = getImagePath(test, '.diff');
-	const change = getImagePath(test, '.change');
-	warn(`[${test.fullTitle}](${url}) has visual differences
-	
-<details>
-  <summary>Test details</summary>
+	let failingTests = currentBuildResults.failures;
+	if (failingTests.length > 0) {
+		warn(`There are some ui comparison tests failing. Please have a look at ` +
+		     `https://ci-assets.nextcloud.com/nextcloud-ui-regression/nextcloud/server/${danger.github.pr.number}/index.html to check if your PR introduces an UI regression.`)
+	}
 
-<table><tr>
-<td><img src="${base}" /></td>
-<td><img src="${diff}" /></td>
-<td><img src="${change}" /></td>
-</tr></table>
 
-\`\`\`
-${jsonData}
-\`\`\`
-</details>`);
+	failingTests.forEach(test => {
+		const url = 'https://ci-assets.nextcloud.com/nextcloud-ui-regression/nextcloud/server/10507/index.html#' + 
+			test.fullTitle.replace(/\W/g, '_');
+		const jsonData = JSON.stringify(test, null, 2);
 
-})
+		const base = getImagePath(suite, test, '.base');
+		const diff = getImagePath(suite, test, '.diff');
+		const change = getImagePath(suite, test, '.change');
+		warn(`[${test.fullTitle}](${url}) has visual differences
+		
+	<details>
+	  <summary>Test details</summary>
+
+	<table><tr>
+	<td><img src="${base}" /></td>
+	<td><img src="${diff}" /></td>
+	<td><img src="${change}" /></td>
+	</tr></table>
+
+	\`\`\`
+	${jsonData}
+	\`\`\`
+	</details>`);
+
+	})
+}
+
+runDangerForSuite('install');
+//runDangerForSuite('login');
+//runDangerForSuite('files');
+//runDangerForSuite('public');
+//runDangerForSuite('settings');
+//runDangerForSuite('apps');
