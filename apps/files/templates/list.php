8013dab044e7dabb2b61c2aa7e0d47d095ad8e40
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-10-23 16:49:56 +0200

    Allow focus on input with keyboard
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/files/templates/list.php
+++ apps/files/templates/list.php
@@ -24,9 +24,10 @@
 	<?php endif;?>
 	<input type="hidden" class="max_human_file_size"
 		   value="(max <?php isset($_['uploadMaxHumanFilesize']) ? p($_['uploadMaxHumanFilesize']) : ''; ?>)">
-	<input type="checkbox" class="hidden" id="showgridview"
+	<input type="checkbox" class="hidden-visually" id="showgridview"
 		<?php if($_['showgridview']) { ?>checked="checked" <?php } ?>/>
-	<label id="view-toggle" for="showgridview" class="button <?php p($_['showgridview'] ? 'icon-toggle-filelist' : 'icon-toggle-pictures') ?>" title="<?php p($l->t('Toggle grid view'))?>" tabindex="0"></label>
+	<label id="view-toggle" for="showgridview" class="button <?php p($_['showgridview'] ? 'icon-toggle-filelist' : 'icon-toggle-pictures') ?>"
+		   title="<?php p($l->t('Toggle grid view'))?>"></label>
 </div>
 
 <div id="emptycontent" class="hidden">
