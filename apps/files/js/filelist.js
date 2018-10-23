b662977862848bf623c3c461a9fc12389c540b6e
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-10-23 08:18:14 +0200

    Fix table hidden on empty folder
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -2780,8 +2780,9 @@
 			var permissions = this.getDirectoryPermissions();
 			var isCreatable = (permissions & OC.PERMISSION_CREATE) !== 0;
 			this.$el.find('#emptycontent').toggleClass('hidden', !this.isEmpty);
+			this.$el.find('#emptycontent').toggleClass('hidden', !this.isEmpty);
 			this.$el.find('#emptycontent .uploadmessage').toggleClass('hidden', !isCreatable || !this.isEmpty);
-			this.$el.find('#filestable thead th').toggleClass('hidden', this.isEmpty);
+			this.$el.find('#filestable').toggleClass('hidden', this.isEmpty);
 		},
 		/**
 		 * Shows the loading mask.
