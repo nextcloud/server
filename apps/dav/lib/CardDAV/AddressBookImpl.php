b9c5e569d36fb57baae6c340d31970d5f852261e
Julius Härtl <jus@bitgrid.net>
2018-10-30 14:43:08 +0100

    Update AddressBookImpl documentation
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/dav/lib/CardDAV/AddressBookImpl.php
+++ apps/dav/lib/CardDAV/AddressBookImpl.php
@@ -88,7 +88,15 @@ class AddressBookImpl implements IAddressBook {
 	/**
 	 * @param string $pattern which should match within the $searchProperties
 	 * @param array $searchProperties defines the properties within the query pattern should match
-	 * @param array $options - for future use. One should always have options!
+	 * @param array $options Options to define the output format
+	 * 	- types boolean (since 15.0.0) If set to true, fields that come with a TYPE property will be an array
+	 *    example: ['id' => 5, 'FN' => 'Thomas Tanghus', 'EMAIL' => ['type => 'HOME', 'value' => 'g@h.i']]
+	 * @return array an array of contacts which are arrays of key-value-pairs
+	 *  example result:
+	 *  [
+	 *		['id' => 0, 'FN' => 'Thomas Müller', 'EMAIL' => 'a@b.c', 'GEO' => '37.386013;-122.082932'],
+	 *		['id' => 5, 'FN' => 'Thomas Tanghus', 'EMAIL' => ['d@e.f', 'g@h.i']]
+	 *	]
 	 * @return array an array of contacts which are arrays of key-value-pairs
 	 * @since 5.0.0
 	 */
