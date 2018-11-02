b1fd72c1cf499188db7ddc26c925d7d5eed7aa2a
Roeland Jago Douma <roeland@famdouma.nl>
2018-11-02 22:45:25 +0100

    Set the filemodel before rending the detailsview
    fixes #10934
    
    Else it triggers the rendering two times. Resulting is weird state in
    for example the comments. Because the comments for OLD_FILEID are
    retrieved but then the model is changed to NEW_FILEID. But the old
    comments still get in and get parsed.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -576,8 +576,8 @@
 			}
 
 			this._currentFileModel = model;
-			this._detailsView.render();
 			this._detailsView.setFileInfo(model);
+			this._detailsView.render();
 			this._detailsView.$el.scrollTop(0);
 		},
 
