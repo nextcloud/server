8d35b0e7a708fc7e9ba284c39bb8f70d57b5361a
Oliver Wegner <void1976@gmail.com>
2018-11-12 14:01:58 +0100

    adding tests for IpAddressV6
    Signed-off-by: Oliver Wegner <void1976@gmail.com>

---
+++ tests/lib/Net/IpAddressV6Test.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Test\Net;
+
+use OC\Net\IpAddressV6;
+use OC\Net\IIpAddress;
+
+class IpAddressV6Test extends \Test\TestCase {
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testIsRangeSingle() {
+		$ipaddress = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348');
+
+		$this->assertFalse($ipaddress->isRange());
+	}
+
+	public function testIsRangeRangeSome() {
+		$ipaddress = new IpAddressV6('2001:db8:85a3:8d3:1319::/80');
+
+		$this->assertTrue($ipaddress->isRange());
+	}
+
+	public function testIsRangeRangeAll() {
+		$ipaddress = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348/128');
+
+		$this->assertFalse($ipaddress->isRange());
+	}
+
+	public function testIsRangeRangeNone() {
+		$ipaddress = new IpAddressV6('::/0');
+
+		$this->assertTrue($ipaddress->isRange());
+	}
+
+	public function testContainsAddressSingleMatch() {
+		$ip1 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348');
+		$ip2 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348');
+
+		$this->assertTrue($ip1->containsAddress($ip2));
+	}
+
+	public function testContainsAddressSingleNoMatch() {
+		$ip1 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348');
+		$ip2 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7349');
+
+		$this->assertFalse($ip1->containsAddress($ip2));
+	}
+
+	public function testContainsAddressRangeMatch() {
+		$ip1 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e::/96');
+		$ip2 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e:370:7348');
+
+		$this->assertTrue($ip1->containsAddress($ip2));
+	}
+
+	public function testContainsAddressRangeNoMatch() {
+		$ip1 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2e::/96');
+		$ip2 = new IpAddressV6('2001:db8:85a3:8d3:1319:8a2f:370:7348');
+
+		$this->assertFalse($ip1->containsAddress($ip2));
+	}
+}
+
