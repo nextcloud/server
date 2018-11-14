9561a7a1e2cacca3a1d2a775a2492d411fe02636
Kyle Fazzari <kyrofa@ubuntu.com>
2018-11-14 08:41:38 -0800

    Verify that isPhpMailerUsed uses config as expected
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- tests/Settings/Controller/CheckSetupControllerTest.php
+++ tests/Settings/Controller/CheckSetupControllerTest.php
@@ -520,6 +520,40 @@ class CheckSetupControllerTest extends TestCase {
 		$this->assertEquals($expected, $this->checkSetupController->check());
 	}
 
+	public function testIsPhpMailerUsed() {
+		$checkSetupController = $this->getMockBuilder('\OC\Settings\Controller\CheckSetupController')
+			->setConstructorArgs([
+				'settings',
+				$this->request,
+				$this->config,
+				$this->clientService,
+				$this->urlGenerator,
+				$this->util,
+				$this->l10n,
+				$this->checker,
+				$this->logger,
+				$this->dispatcher,
+				$this->db,
+				$this->lockingProvider,
+				$this->dateTimeFormatter,
+				$this->memoryInfo,
+				$this->secureRandom,
+			])
+			->setMethods(null)->getMock();
+
+		$this->config->expects($this->at(0))
+			->method('getSystemValue')
+			->with('mail_smtpmode', null)
+			->will($this->returnValue('php'));
+		$this->config->expects($this->at(1))
+			->method('getSystemValue')
+			->with('mail_smtpmode', null)
+			->will($this->returnValue('not-php'));
+
+		$this->assertTrue($this->invokePrivate($checkSetupController, 'isPhpMailerUsed'));
+		$this->assertFalse($this->invokePrivate($checkSetupController, 'isPhpMailerUsed'));
+	}
+
 	public function testGetCurlVersion() {
 		$checkSetupController = $this->getMockBuilder('\OC\Settings\Controller\CheckSetupController')
 			->setConstructorArgs([
