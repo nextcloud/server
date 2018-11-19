511eade1d4e4f71d79b71c2e08c6087811816c95
Morris Jobke <hey@morrisjobke.de>
2018-11-19 10:57:51 +0100

    Use the proper default values
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- settings/Controller/CheckSetupController.php
+++ settings/Controller/CheckSetupController.php
@@ -529,7 +529,7 @@ Raw output
 	}
 
 	protected function isPhpMailerUsed(): bool {
-		return $this->config->getSystemValue('mail_smtpmode') === 'php';
+		return $this->config->getSystemValue('mail_smtpmode', 'smtp') === 'php';
 	}
 
 	protected function hasOpcacheLoaded(): bool {
