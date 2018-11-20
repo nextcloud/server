adf1488e4ee46b8fd49aca7fbef7f72f3104f565
Roeland Jago Douma <roeland@famdouma.nl>
2018-11-20 09:23:57 +0100

    Bearer tokens are app token
    Fixes #12498
    
    This means that we set that it is a proper app token once it is
    validated. This will allow the 2FA middleware to just run the same
    check.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/User/Session.php
+++ lib/private/User/Session.php
@@ -776,6 +776,10 @@ class Session implements IUserSession, Emitter {
 		if(!$this->validateToken($token)) {
 			return false;
 		}
+
+		// Set the session variable so we know this is an app password
+		$this->session->set('app_password', $token);
+
 		return true;
 	}
 
