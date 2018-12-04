b5eac6301bb982d6525a60657c6c761c2a19fde1
Morris Jobke <hey@morrisjobke.de>
2018-12-04 15:40:40 +0100

    PHP 7.0 is end of life
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- settings/Controller/CheckSetupController.php
+++ settings/Controller/CheckSetupController.php
@@ -261,7 +261,7 @@ class CheckSetupController extends Controller {
 	 * @return bool
 	 */
 	protected function isPhpOutdated() {
-		if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+		if (version_compare(PHP_VERSION, '7.1.0', '<')) {
 			return true;
 		}
 
