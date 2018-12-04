nextcloud/server

commit c23a66cda46d9a1f0ce8dc60a6c160809c87d9f6

fixes dav share issue with owner
Signed-off-by: Georg Ehrke <developer@georgehrke.com>

apps/dav/lib/CardDAV/AddressBook.php
@@ -105,12 +105,17 @@ class AddressBook extends \Sabre\CardDAV\AddressBook implements IShareable {
 				'privilege' => '{DAV:}read',
 				'principal' => $this->getOwner(),
 				'protected' => true,
-			]];
-		$acl[] = [
+			],[
 				'privilege' => '{DAV:}write',
 				'principal' => $this->getOwner(),
 				'protected' => true,
-			];
+			]
+		];
+
+		if (!$this->isShared()) {
+			return $acl;
+		}
+
 		if ($this->getOwner() !== parent::getOwner()) {
 			$acl[] =  [
 					'privilege' => '{DAV:}read',
@@ -133,11 +138,11 @@ class AddressBook extends \Sabre\CardDAV\AddressBook implements IShareable {
 			];
 		}
 
-		if ($this->isShared()) {
-			return $acl;
-		}
-
-		return $this->carddavBackend->applyShareAcl($this->getResourceId(), $acl);
+		$acl = $this->carddavBackend->applyShareAcl($this->getResourceId(), $acl);
+		$allowedPrincipals = [$this->getOwner(), parent::getOwner(), 'principals/system/system'];
+		return array_filter($acl, function($rule) use ($allowedPrincipals) {
+			return \in_array($rule['principal'], $allowedPrincipals, true);
+		});
 	}
 
 	public function getChildACL() {

apps/dav/lib/CalDAV/Calendar.php
@@ -139,6 +139,10 @@ class Calendar extends \Sabre\CalDAV\Calendar implements IShareable {
 			];
 		}
 
+		if (!$this->isShared()) {
+			return $acl;
+		}
+
 		if ($this->getOwner() !== parent::getOwner()) {
 			$acl[] =  [
 					'privilege' => '{DAV:}read',
@@ -168,14 +172,9 @@ class Calendar extends \Sabre\CalDAV\Calendar implements IShareable {
 		}
 
 		$acl = $this->caldavBackend->applyShareAcl($this->getResourceId(), $acl);
-
-		if (!$this->isShared()) {
-			return $acl;
-		}
-
 		$allowedPrincipals = [$this->getOwner(), parent::getOwner(), 'principals/system/public'];
 		return array_filter($acl, function($rule) use ($allowedPrincipals) {
-			return in_array($rule['principal'], $allowedPrincipals);
+			return \in_array($rule['principal'], $allowedPrincipals, true);
 		});
 	}
 

commit 29eff7b1227d3b36d009b1b2bd3e5d146109ba1e

Fix the system address book
c23a66cda46d9a1f0ce8dc60a6c160809c87d9f6 broke the system address book.
We now move the ACL rules for this special case up and all is good in
the world again.

Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

apps/dav/lib/CardDAV/AddressBook.php
@@ -112,6 +112,14 @@ class AddressBook extends \Sabre\CardDAV\AddressBook implements IShareable {
 			]
 		];
 
+		if ($this->getOwner() === 'principals/system/system') {
+			$acl[] = [
+				'privilege' => '{DAV:}read',
+				'principal' => '{DAV:}authenticated',
+				'protected' => true,
+			];
+		}
+
 		if (!$this->isShared()) {
 			return $acl;
 		}
@@ -129,13 +137,6 @@ class AddressBook extends \Sabre\CardDAV\AddressBook implements IShareable {
 					'protected' => true,
 				];
 			}
-		}
-		if ($this->getOwner() === 'principals/system/system') {
-			$acl[] = [
-					'privilege' => '{DAV:}read',
-					'principal' => '{DAV:}authenticated',
-					'protected' => true,
-			];
 		}
 
 		$acl = $this->carddavBackend->applyShareAcl($this->getResourceId(), $acl);