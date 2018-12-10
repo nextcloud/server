6c9f2644cf8bac151b9af743802eb72a51278e8b
Robin Appelman <robin@icewind.nl>
2018-12-10 17:20:45 +0100

    Add objectExists to objectstore interface
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/public/Files/ObjectStore/IObjectStore.php
+++ lib/public/Files/ObjectStore/IObjectStore.php
@@ -63,4 +63,13 @@ interface IObjectStore {
 	 * @since 7.0.0
 	 */
 	public function deleteObject($urn);
+
+	/**
+	 * Check if an object exists in the object store
+	 *
+	 * @param string $urn
+	 * @return bool
+	 * @since 16.0.0
+	 */
+	public function objectExists($urn);
 }
