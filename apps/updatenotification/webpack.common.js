15dc9d8d158f3769b2b72a31f89eb618e56cf01c
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-12-14 16:49:00 +0100

    Transpile arrow function for ie11 compatibility
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/updatenotification/webpack.common.js
+++ apps/updatenotification/webpack.common.js
@@ -13,16 +13,18 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-      }
+      },
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
     ]
   },
   plugins: [
     new VueLoaderPlugin()
   ],
   resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
     extensions: ['*', '.js', '.vue', '.json']
   }
 }
