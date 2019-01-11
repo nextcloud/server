ee2c45d60d60cc083d5a255acabf675f70ac67ed
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2019-01-11 13:37:30 +0100

    Fix grid toggle button on public page
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/files_sharing/templates/public.php
+++ apps/files_sharing/templates/public.php
@@ -46,14 +46,16 @@ $maxUploadFilesize = min($upload_max_filesize, $post_max_size);
 	</div>
 <?php endif; ?>
 
-<?php if ($_['showgridview'] && empty($_['dir']) === false) { ?>
-	<input type="checkbox" class="hidden-visually" id="showgridview"
-		<?php if($_['showgridview']) { ?>checked="checked" <?php } ?>/>
-	<label id="view-toggle" for="showgridview" class="button <?php p($_['showgridview'] ? 'icon-toggle-filelist' : 'icon-toggle-pictures') ?>"
-		title="<?php p($l->t('Toggle grid view'))?>"></label>
-<?php } ?>
-
 <?php if (!isset($_['hideFileList']) || (isset($_['hideFileList']) && $_['hideFileList'] === false)) { ?>
+	<!-- ONLY if this is a folder, we show the grid toggle button -->
+	<?php if (empty($_['dir']) === false) { ?>
+		<input type="checkbox" class="hidden-visually" id="showgridview"
+			<?php if($_['showgridview']) { ?>checked="checked" <?php } ?>/>
+		<label id="view-toggle" for="showgridview" class="button <?php p($_['showgridview'] ? 'icon-toggle-filelist' : 'icon-toggle-pictures') ?>"
+			title="<?php p($l->t('Toggle grid view'))?>"></label>
+	<?php } ?>
+	
+	<!-- files listing -->
 	<div id="files-public-content">
 		<div id="preview">
 			<?php if (isset($_['folder'])): ?>
