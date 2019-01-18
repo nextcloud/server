11acc1a2d481f8011231594e31534ee818a10db6
Roeland Jago Douma <roeland@famdouma.nl>
2019-01-18 09:17:26 +0100

    Fix template paramter
    Else we get shown an error page instead of the correct 403.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- tests/Core/Controller/ClientFlowLoginControllerTest.php
+++ tests/Core/Controller/ClientFlowLoginControllerTest.php
@@ -315,7 +315,7 @@ class ClientFlowLoginControllerTest extends TestCase {
 			'core',
 			'403',
 			[
-				'file' => 'State token does not match',
+				'message' => 'State token does not match',
 			],
 			'guest'
 		);
