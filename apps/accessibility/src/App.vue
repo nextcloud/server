4b88ac4d2ab808a8942ebba34d8b3573399faa2c
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-01-24 18:55:58 +0100

    Accessibility: Simplify design, more obvious selectability
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- apps/accessibility/src/App.vue
+++ apps/accessibility/src/App.vue
@@ -1,21 +1,18 @@
 <template>
 	<div id="accessibility" class="section">
 		<h2>{{t('accessibility', 'Accessibility')}}</h2>
-		<p class="settings-hint">
-			{{t('accessibility', 'Universal access is very important to us. We follow web standards and check to make everything usable also without mouse, and assistive software such as screenreaders.')}}</br>
-			We aim to be compliant with the <a target="_blank" href="https://www.w3.org/WAI/standards-guidelines/wcag/" rel="noreferrer nofollow">Web Content Accessibility Guidelines</a>  2.1 on AA level, with the high contrast theme even on AAA level.</br>
+		<p>
+			{{t('accessibility', 'Universal access is very important to us. We follow web standards and check to make everything usable also without mouse, and assistive software such as screenreaders.')}} We aim to be compliant with the <a target="_blank" href="https://www.w3.org/WAI/standards-guidelines/wcag/" rel="noreferrer nofollow">Web Content Accessibility Guidelines</a>  2.1 on AA level, with the high contrast theme even on AAA level.</br>
 			If you find any issues, don’t hesitate to report them on <a target="_blank" href="https://github.com/nextcloud/server/issues/" rel="noreferrer nofollow">our issue tracker</a>. And if you want to get involved, come join <a target="_blank" href="https://nextcloud.com/design" rel="noreferrer nofollow">our design team</a>!
 		</p>
 
-		<div class="themes-list preview-list">
+		<div class="preview-list">
 			<preview v-for="preview in themes" :preview="preview"
-					 :key="preview.id" :selected="selected.theme"
-					 v-on:select="selectTheme"></preview>
-		</div>
-		<div class="fonts-list preview-list">
+				 :key="preview.id" :selected="selected.theme"
+				 v-on:select="selectTheme"></preview>
 			<preview v-for="preview in fonts" :preview="preview"
-					 :key="preview.id" :selected="selected.font"
-					 v-on:select="selectFont"></preview>
+				 :key="preview.id" :selected="selected.font"
+				 v-on:select="selectFont"></preview>
 		</div>
 	</div>
 </template>
