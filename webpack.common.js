58ecc7d000e682df51c14562f369900dcd0f1eec
Roeland Jago Douma <roeland@famdouma.nl>
2019-02-11 11:27:51 +0100

    Move files_trashbin to webpack
    Combine the 2 js files and the scss. This saves compiling the scss on
    the go. And going via Nextcloud storage to obtain it.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const core = require('./core/webpack');
+const files_trashbin = require('./apps/files_trashbin/webpack')
 const oauth2 = require('./apps/oauth2/webpack')
 
-module.exports = [].concat(core, oauth2);
+module.exports = [].concat(core, files_trashbin, oauth2);
