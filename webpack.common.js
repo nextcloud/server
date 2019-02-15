d6ba42057f05d19e57ca4300b059c635a8613b60
Roeland Jago Douma <roeland@famdouma.nl>
2019-02-15 20:31:47 +0100

    Move updatenotifications to unified webpack
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,13 @@ const files_trashbin = require('./apps/files_trashbin/webpack')
 const files_versions = require('./apps/files_versions/webpack');
 const oauth2 = require('./apps/oauth2/webpack')
 const systemtags = require('./apps/systemtags/webpack')
+const updatenotifications = require('./apps/updatenotification/webpack')
 
 module.exports = [].concat(
 	core,
 	files_trashbin,
 	files_versions,
 	oauth2,
-	systemtags
+	systemtags,
+	updatenotifications
 );
