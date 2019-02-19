221c2f2d9d5d0b60fa1f24cfe54fa381f30ece0d
Roeland Jago Douma <roeland@famdouma.nl>
2019-02-19 13:06:05 +0100

    Move accessibility to main webpack
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,15 @@
 const core = require('./core/webpack');
+const accessibility = require('./apps/accessibility/webpack')
 const files_sharing = require('./apps/files_sharing/webpack')
 const files_trashbin = require('./apps/files_trashbin/webpack')
-const files_versions = require('./apps/files_versions/webpack');
+const files_versions = require('./apps/files_versions/webpack')
 const oauth2 = require('./apps/oauth2/webpack')
 const systemtags = require('./apps/systemtags/webpack')
 const updatenotifications = require('./apps/updatenotification/webpack')
 
 module.exports = [].concat(
 	core,
+	accessibility,
 	files_sharing,
 	files_trashbin,
 	files_versions,
