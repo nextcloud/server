40a63422068b15061632eaf91272ddc2e3b777dd
Morris Jobke <hey@morrisjobke.de>
2019-03-06 16:49:49 +0100

    16.0.0 Beta 1
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(16, 0, 0, 2);
+$OC_Version = array(16, 0, 0, 3);
 
 // The human readable string
-$OC_VersionString = '16.0.0 alpha 1';
+$OC_VersionString = '16.0.0 Beta 1';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
