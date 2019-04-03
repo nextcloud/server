1d6cd7c4681d9e5821fbeb973358e30cef0340db
Robin Appelman <robin@icewind.nl>
2019-04-03 15:06:12 +0200

    set request tokens on request made using fetch
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- core/js/oc-requesttoken.js
+++ core/js/oc-requesttoken.js
@@ -4,3 +4,13 @@ $(document).on('ajaxSend',function(elm, xhr, settings) {
 		xhr.setRequestHeader('OCS-APIREQUEST', 'true');
 	}
 });
+
+(function () {
+	var originalFetch = window.fetch;
+	window.fetch = function(input, init) {
+		init = init || {};
+		init.headers = init.headers || {};
+		init.headers.requesttoken = oc_requesttoken;
+		return originalFetch(input, init);
+	}
+})();
