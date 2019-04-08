cf918133017ea64c84495499152f82f7cd91ae09
Morris Jobke <hey@morrisjobke.de>
2019-04-08 16:43:16 +0200

    14.0.10
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(14, 0, 9, 1);
+$OC_Version = array(14, 0, 10, 0);
 
 // The human readable string
-$OC_VersionString = '14.0.9';
+$OC_VersionString = '14.0.10';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
