e1ae0d1a6ea79cadabfb706aba69ed8b978c7722
Julius Härtl <jus@bitgrid.net>
2019-04-08 13:11:07 +0200

    Rename collections to projects
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- core/src/OCP/collaboration.js
+++ core/src/OCP/collaboration.js
@@ -34,13 +34,15 @@ let types = {};
 
 /**
  * Those translations will be used by the vue component but they should be shipped with the server
- * t('core', 'Add to a collection')
+ * t('core', 'Add to a project')
  * t('core', 'Show details')
  * t('core', 'Hide details')
- * t('core', 'Rename collection')
- * t('core', 'Failed to rename collection')
- * t('core', 'Failed to create collection')
- * t('core', 'Failed to add resource to collection')
+ * t('core', 'Rename project')
+ * t('core', 'Failed to rename the project')
+ * t('core', 'Failed to create a project')
+ * t('core', 'Failed to add the item to the project')
+ * t('core', 'Connect items to a project to make them easier to find')
+ * t('core', 'Type to search for existing projects')
  */
 
 export default {
