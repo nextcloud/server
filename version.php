4c19b3b2c6d8c62d1b82e2d78174960c96294d63
Joas Schilling <coding@schilljs.com>
2019-04-11 10:09:11 +0200

    Bump versions of server and the requirements of apps
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- version.php
+++ version.php
@@ -29,15 +29,15 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(16, 0, 0, 6);
+$OC_Version = array(17, 0, 0, 0);
 
 // The human readable string
-$OC_VersionString = '16.0.0 RC 1';
+$OC_VersionString = '17.0.0 alpha';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
-		'15.0' => true,
 		'16.0' => true,
+		'17.0' => true,
 	],
 	'owncloud' => [
 	],
