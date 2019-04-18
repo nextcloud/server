ff4f9a1132349e0072c4c1b37b91552f49310b6f
Michael Bonfils <bonfils.michael@protonmail.com>
2019-04-18 07:35:24 +0000

    Preview\CBZ: Add test
    Signed-off-by: Michael Bonfils <bonfils.michael@protonmail.com>

---
+++ tests/lib/Preview/CBZTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019, Michael Bonfils (<bonfils.michael@protonmail.com>)
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Test\Preview;
+
+/**
+ * Class CBZTest
+ *
+ * @group DB
+ *
+ * @package Test\Preview
+ */
+class CBZTest extends Provider {
+
+	public function setUp() {
+		parent::setUp();
+
+		$fileName = 'testcbz.cbz';
+		$this->imgPath = $this->prepareTestFile($fileName, \OC::$SERVERROOT . '/tests/data/' . $fileName);
+		$this->width = 1680;
+		$this->height = 1050;
+		$this->provider = new \OC\Preview\CBZ();
+	}
+
+}
