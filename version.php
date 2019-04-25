6b6ba81d3aaca052ae5ad83a08eece718766b4ae
Roeland Jago Douma <roeland@famdouma.nl>
2019-04-25 07:54:48 +0200

    15.0.8 RC1
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(15, 0, 7, 1);
+$OC_Version = array(15, 0, 8, 0);
 
 // The human readable string
-$OC_VersionString = '15.0.7';
+$OC_VersionString = '15.0.8 RC 1';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
