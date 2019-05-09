nextcloud/viewer

Add cypress testing
Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

cypress/integration/image-small.png.spec.js
@@ -1,0 +1,86 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open image-small.png in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('image-small.png', 'image/png')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See image-small.png in the list', function() {
+		cy.get('#fileList tr[data-file="image-small.png"]', { timeout: 10000 })
+			.should('contain', 'image-small.png')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('image-small.png')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'image-small.png')
+		cy.get('#viewer-content .modal-header a.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Have the proper height and width values', function() {
+		// not using should('have.css'), we want the inline styling
+		cy.get('#viewer-content .modal-container img.active')
+			.should('have.attr', 'style')
+			.should('match', /width: 252px/i)
+			.should('match', /height: 120px/i)
+	})
+
+	it('Does not have any visual regression', function() {
+		cy.matchImageSnapshot()
+	})
+})

cypress/integration/video.mkv.spec.js
@@ -1,0 +1,80 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open video.mkv in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('video.mkv', 'video/mkv')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See video.mkv in the list', function() {
+		cy.get('#fileList tr[data-file="video.mkv"]', { timeout: 10000 })
+			.should('contain', 'video.mkv')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('video.mkv')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'video.mkv')
+		cy.get('#viewer-content .modal-header a.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Take screenshot', function() {
+		// video are impossible to match with existing screenshot
+		// just taking a screenshot to manually compare if needed
+		cy.screenshot()
+	})
+})

cypress/integration/video.ogv.spec.js
@@ -1,0 +1,80 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open video.ogv in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('video.ogv', 'video/ogv')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See video.ogv in the list', function() {
+		cy.get('#fileList tr[data-file="video.ogv"]', { timeout: 10000 })
+			.should('contain', 'video.ogv')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('video.ogv')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'video.ogv')
+		cy.get('#viewer-content .modal-header a.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Take screenshot', function() {
+		// video are impossible to match with existing screenshot
+		// just taking a screenshot to manually compare if needed
+		cy.screenshot()
+	})
+})

cypress/start.sh
@@ -1,0 +1,9 @@
+#! /bin/sh
+appname=$1
+dockername="cypress_testing_$appname"
+
+docker run -p 8000:80 --rm --name $dockername --env BRANCH=stable16 --detach nextcloudci/server:server-10
+docker cp . $dockername:/var/www/html/apps/$appname
+sleep 10
+docker exec --user www-data $dockername php occ app:enable $appname
+docker exec --user www-data $dockername php occ config:system:set force_language --value en

nextcloud/server

apps/viewer/cypress/integration/image-small.png.spec.js
@@ -1,0 +1,86 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open image-small.png in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('image-small.png', 'image/png')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See image-small.png in the list', function() {
+		cy.get('#fileList tr[data-file="image-small.png"]', { timeout: 10000 })
+			.should('contain', 'image-small.png')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('image-small.png')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'image-small.png')
+		cy.get('#viewer-content .modal-header button.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Have the proper height and width values', function() {
+		// not using should('have.css'), we want the inline styling
+		cy.get('#viewer-content .modal-container img.active')
+			.should('have.attr', 'style')
+			.should('match', /width: 252px/i)
+			.should('match', /height: 120px/i)
+	})
+
+	it('Does not have any visual regression', function() {
+		cy.matchImageSnapshot()
+	})
+})

apps/viewer/cypress/integration/video.mkv.spec.js
@@ -1,0 +1,80 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open video.mkv in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('video.mkv', 'video/mkv')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See video.mkv in the list', function() {
+		cy.get('#fileList tr[data-file="video.mkv"]', { timeout: 10000 })
+			.should('contain', 'video.mkv')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('video.mkv')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'video.mkv')
+		cy.get('#viewer-content .modal-header button.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Take screenshot', function() {
+		// video are impossible to match with existing screenshot
+		// just taking a screenshot to manually compare if needed
+		cy.screenshot()
+	})
+})

apps/viewer/cypress/integration/video.ogv.spec.js
@@ -1,0 +1,80 @@
+/**
+ * @copyright Copyright (c) 2019 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { randHash } from '../utils/'
+const randUser = randHash()
+
+describe('Open video.ogv in viewer', function() {
+	before(function() {
+		// Init user
+		cy.nextcloudCreateUser(randUser, 'password')
+		cy.login(randUser, 'password')
+
+		// Upload test files
+		cy.uploadFile('video.ogv', 'video/ogv')
+		cy.visit('/apps/files')
+
+		// wait a bit for things to be settled
+		cy.wait(1000)
+	})
+	after(function() {
+		cy.logout()
+	})
+
+	it('See video.ogv in the list', function() {
+		cy.get('#fileList tr[data-file="video.ogv"]', { timeout: 10000 })
+			.should('contain', 'video.ogv')
+	})
+
+	it('Open the viewer on file click', function() {
+		cy.openFile('video.ogv')
+		cy.get('#viewer-content').should('be.visible')
+	})
+
+	it('Does not see a loading animation', function() {
+		cy.get('#viewer-content', { timeout: 4000 })
+			.should('be.visible')
+			.and('have.class', 'modal-mask')
+			.and('not.have.class', 'icon-loading')
+	})
+
+	it('Is not in mobile fullscreen mode', function() {
+		cy.get('#viewer-content .modal-wrapper').should('not.have.class', 'modal-wrapper--full')
+	})
+
+	it('See the share and menu icons and title on the viewer header', function() {
+		cy.get('#viewer-content .modal-title').should('contain', 'video.ogv')
+		cy.get('#viewer-content .modal-header button.icon-share-white-forced').should('be.visible')
+		cy.get('#viewer-content .modal-header a.icon-close').should('be.visible')
+	})
+
+	it('Does not see navigation arrows', function() {
+		cy.get('#viewer-content a.prev').should('not.be.visible')
+		cy.get('#viewer-content a.next').should('not.be.visible')
+	})
+
+	it('Take screenshot', function() {
+		// video are impossible to match with existing screenshot
+		// just taking a screenshot to manually compare if needed
+		cy.screenshot()
+	})
+})

apps/viewer/cypress/start.sh
@@ -1,0 +1,9 @@
+#! /bin/sh
+appname=$1
+dockername="cypress_testing_$appname"
+
+docker run -p 8000:80 --rm --name $dockername --env BRANCH=master --detach nextcloudci/server:server-10
+docker cp . $dockername:/var/www/html/apps/$appname
+sleep 10
+docker exec --user www-data $dockername php occ app:enable $appname
+docker exec --user www-data $dockername php occ config:system:set force_language --value en