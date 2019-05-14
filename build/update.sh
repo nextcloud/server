bc61b15e2f4f724d68dfed1a0ad0707b5266faef
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-05-14 15:08:38 +0200

    Adjust script to remove branches which are deleted on GitHub
    Co-Authored-By: Morris Jobke <hey@morrisjobke.de>

--- build/update.sh
+++ build/update.sh
@@ -6,7 +6,7 @@
 # Update server
 printf "\n\033[1m${PWD##*/}\033[0m\n"
 git checkout master
-git pull --quiet
+git pull --quiet -p
 git --no-pager log -3 --pretty=format:"%h %Cblue%ar%x09%an %Creset%s"
 printf "\n"
 git branch --merged master | grep -v "master$" | xargs --no-run-if-empty git branch -d
