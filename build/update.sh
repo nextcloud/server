fa066da77c7d56939ec1a73675c358cb782a56c6
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-05-17 22:49:33 +0200

    Fix app update script for macOS
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- build/update.sh
+++ build/update.sh
@@ -9,7 +9,7 @@ git checkout master
 git pull --quiet -p
 git --no-pager log -3 --pretty=format:"%h %Cblue%ar%x09%an %Creset%s"
 printf "\n"
-git branch --merged master | grep -v "master$" | xargs --no-run-if-empty git branch -d
+git branch --merged master | grep -v "master$" | xargs git branch -d
 git submodule update --init
 
 # Update apps
