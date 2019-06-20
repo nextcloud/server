05381f00d25e380c557a3a1100dd83e75298f130
Joas Schilling <coding@schilljs.com>
2019-06-20 15:23:06 +0200

    Fall back to black for non-color values
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Template/IconsCacher.php
+++ lib/private/Template/IconsCacher.php
@@ -184,6 +184,11 @@ class IconsCacher {
 	 * @return string
 	 */
 	public function colorizeSvg($svg, $color): string {
+		if (!preg_match('/^[0-9a-f]{3,6}$/i', $color)) {
+			// Prevent not-sane colors from being written into the SVG
+			$color = '000';
+		}
+
 		// add fill (fill is not present on black elements)
 		$fillRe = '/<((circle|rect|path)((?!fill)[a-z0-9 =".\-#():;,])+)\/>/mi';
 		$svg = preg_replace($fillRe, '<$1 fill="#' . $color . '"/>', $svg);
