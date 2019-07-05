4bd639640b9e9f502358fea3c2370995dd3f1c1c
Akshala Bhatnagar <47988358+akshala@users.noreply.github.com>
2019-07-05 18:53:38 +0530

    Update core/src/components/login/ResetPassword.vue
    Co-Authored-By: Julius Härtl <jus@bitgrid.net>

--- core/src/components/login/ResetPassword.vue
+++ core/src/components/login/ResetPassword.vue
@@ -49,7 +49,7 @@
 		</div>
 		<p v-if="message === 'send-success'"
 		   class="update">
-			{{ t('core', 'A password reset message has been sent to the e-mail address for this account. If you do not receive it, check your spam/junk folders then ask your local administrator for help.') }}
+			{{ t('core', 'A password reset message has been sent to the e-mail address of this account. If you do not receive it, check your spam/junk folders or ask your local administrator for help.') }}
 			<br>
 			{{ t('core', 'If it is not there ask your local administrator.') }}
 		</p>
