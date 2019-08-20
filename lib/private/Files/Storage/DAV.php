323642454ad6bed5675c57f84e35286a4528374b
Joas Schilling <coding@schilljs.com>
2019-08-20 12:54:58 +0200

    Undefined variable response when server is no nextcloud anymore
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Files/Storage/DAV.php
+++ lib/private/Files/Storage/DAV.php
@@ -268,7 +268,7 @@ class DAV extends Common {
 				);
 				$this->statCache->set($path, $response);
 			} catch (ClientHttpException $e) {
-				if ($e->getHttpStatus() === 404) {
+				if ($e->getHttpStatus() === 404 || $e->getHttpStatus() === 405) {
 					$this->statCache->clear($path . '/');
 					$this->statCache->set($path, false);
 					return false;
