068f1d28f3fd3a97c2c5abfd408dec8e0d0cbcd7
ochorocho <rothjochen@gmail.com>
2019-08-26 20:01:44 +0200

    Move alternative login above standard login, optimize alignment
    Signed-off-by: ochorocho <rothjochen@gmail.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -3,15 +3,12 @@
 script('core', 'dist/login');
 ?>
 
-<div id="login"></div>
 <?php if (!empty($_['alt_login'])) { ?>
-<form id="alternative-logins">
-	<fieldset>
-		<ul>
-			<?php foreach($_['alt_login'] as $login): ?>
-				<li><a class="button" href="<?php print_unescaped($login['href']); ?>" ><?php p($login['name']); ?></a></li>
-			<?php endforeach; ?>
-		</ul>
-	</fieldset>
-</form>
-<?php }
+	<div id="alternative-logins">
+		<?php foreach($_['alt_login'] as $login): ?>
+			<a class="button primary" href="<?php print_unescaped($login['href']); ?>" ><?php p($login['name']); ?></a>
+		<?php endforeach; ?>
+	</div>
+<?php } ?>
+
+<div id="login"></div>
