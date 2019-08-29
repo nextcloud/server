c3e5841532fecc6ef72b6b9554c1599c80866d41
Roeland Jago Douma <roeland@famdouma.nl>
2019-08-29 08:51:35 +0200

    17 Beta 4
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(17, 0, 0, 5);
+$OC_Version = array(17, 0, 0, 6);
 
 // The human readable string
-$OC_VersionString = '17.0.0 Beta 3';
+$OC_VersionString = '17.0.0 Beta 4';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
