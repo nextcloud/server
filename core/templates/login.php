dc30ffc1e5170ab1af1c407cbdaf43d8e2beab41
Jochen <rothjochen@gmail.com>
2019-09-02 17:50:56 +0200

    Add more space to buttonsy
    Signed-off-by: Jochen <rothjochen@gmail.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -3,12 +3,16 @@
 script('core', 'dist/login');
 ?>
 
-<?php if (!empty($_['alt_login'])) { ?>
-	<div id="alternative-logins">
-		<?php foreach($_['alt_login'] as $login): ?>
-			<a class="button primary" href="<?php print_unescaped($login['href']); ?>" ><?php p($login['name']); ?></a>
-		<?php endforeach; ?>
-	</div>
-<?php } ?>
 
 <div id="login"></div>
+
+<?php if (!empty($_['alt_login'])) { ?>
+    <div id="alternative-logins">
+        <?php foreach($_['alt_login'] as $login): ?>
+            <a class="button primary" href="<?php print_unescaped($login['href']); ?>" >
+                <?php p($l->t('Log in with')) ?>
+                <?php p($login['name']); ?>
+            </a>
+        <?php endforeach; ?>
+    </div>
+<?php } ?>
