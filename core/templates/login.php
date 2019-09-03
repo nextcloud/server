e710976b9991969f7cde69f9d5483cd53062e05a
Jochen <rothjochen@gmail.com>
2019-09-03 22:44:31 +0200

    Add more space between form and alternate login
    Signed-off-by: Jochen <rothjochen@gmail.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -3,13 +3,12 @@
 script('core', 'dist/login');
 ?>
 
-
 <div id="login"></div>
 
 <?php if (!empty($_['alt_login'])) { ?>
     <div id="alternative-logins">
         <?php foreach($_['alt_login'] as $login): ?>
-            <a class="button primary" href="<?php print_unescaped($login['href']); ?>" >
+            <a class="button primary <?php p($login['style']); ?>" href="<?php print_unescaped($login['href']); ?>" >
                 <?php p($l->t('Log in with')) ?>
                 <?php p($login['name']); ?>
             </a>
