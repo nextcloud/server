d38f8dc746cc94feb3422704fe9228a3bd5c8893
Jochen <rothjochen@gmail.com>
2019-09-09 19:00:32 +0200

    Add aria label
    Signed-off-by: Jochen <rothjochen@gmail.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -8,8 +8,8 @@ script('core', 'dist/login');
 <?php if (!empty($_['alt_login'])) { ?>
     <div id="alternative-logins">
         <?php foreach($_['alt_login'] as $login): ?>
-            <a class="button <?php p(isset($login['style']) ? $login['style'] : ''); ?>" href="<?php print_unescaped($login['href']); ?>" >
-                <?php echo $login['name']; ?>
+            <a class="button <?php p(isset($login['style']) ? $login['style'] : ''); ?>" aria-label="<?php p($login['label']); ?>" href="<?php print_unescaped($login['href']); ?>" >
+                <?php p($login['name']); ?>
             </a>
         <?php endforeach; ?>
     </div>
