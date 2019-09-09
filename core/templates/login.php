e0ee53a5f0f859514c6fa985f4c22ef218503a3c
Jochen <rothjochen@gmail.com>
2019-09-09 20:31:08 +0200

    Added name as fallback, in case label is not set
    https://github.com/nextcloud/server/pull/16832#discussion_r322380822
    
    Signed-off-by: Jochen <rothjochen@gmail.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -8,7 +8,7 @@ script('core', 'dist/login');
 <?php if (!empty($_['alt_login'])) { ?>
     <div id="alternative-logins">
         <?php foreach($_['alt_login'] as $login): ?>
-            <a class="button <?php p($login['style'] ?? ''); ?>" aria-label="<?php p($login['label'] ?? ''); ?>" href="<?php print_unescaped($login['href']); ?>" >
+            <a class="button <?php p($login['style'] ?? ''); ?>" aria-label="<?php p($login['label'] ?? $login['name']); ?>" href="<?php print_unescaped($login['href']); ?>" >
                 <?php p($login['name']); ?>
             </a>
         <?php endforeach; ?>
