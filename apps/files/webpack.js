5274c5426818270460ce05ee0071aa9f189281e6
Roeland Jago Douma <roeland@famdouma.nl>
2019-10-29 12:22:15 +0100

    Add a transfer ownership background job
    This job can be initiated by a user to transfer a file/folder to a
    target user.
    
    The target user will have to accept the job.
    Once that is done the transfers is initiated in the background.
    
    Both parties get notified when the job is done.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/files/webpack.js
+++ apps/files/webpack.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: {
 		'sidebar': path.join(__dirname, 'src', 'sidebar.js'),
+		'personal-settings': path.join(__dirname, 'src', 'main-personal-settings.js'),
 	},
 	output: {
 		path: path.resolve(__dirname, './js/dist/'),
