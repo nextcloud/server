4621a4202040b2a9dfe88d2e806ccaf167f06308
Robin Appelman <robin@icewind.nl>
2019-11-06 15:10:15 +0100

    fix tests for background file scan job
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files/tests/BackgroundJob/ScanFilesTest.php
+++ apps/files/tests/BackgroundJob/ScanFilesTest.php
@@ -59,6 +59,11 @@ class ScanFilesTest extends TestCase {
 	public function testRunWithoutUsers() {
 		$this->config
 				->expects($this->at(0))
+				->method('getSystemValueBool')
+				->with('files_no_background_scan', false)
+				->will($this->returnValue(false));
+		$this->config
+				->expects($this->at(1))
 				->method('getAppValue')
 				->with('files', 'cronjob_scan_files', 0)
 				->will($this->returnValue(50));
@@ -73,7 +78,7 @@ class ScanFilesTest extends TestCase {
 				->with('', 500)
 				->will($this->returnValue([]));
 		$this->config
-				->expects($this->at(1))
+				->expects($this->at(2))
 				->method('setAppValue')
 				->with('files', 'cronjob_scan_files', 500);
 
@@ -84,6 +89,11 @@ class ScanFilesTest extends TestCase {
 		$fakeUser = $this->createMock(IUser::class);
 		$this->config
 				->expects($this->at(0))
+				->method('getSystemValueBool')
+				->with('files_no_background_scan', false)
+				->will($this->returnValue(false));
+		$this->config
+				->expects($this->at(1))
 				->method('getAppValue')
 				->with('files', 'cronjob_scan_files', 0)
 				->will($this->returnValue(50));
@@ -95,7 +105,7 @@ class ScanFilesTest extends TestCase {
 						$fakeUser
 				]));
 		$this->config
-				->expects($this->at(1))
+				->expects($this->at(2))
 				->method('setAppValue')
 				->with('files', 'cronjob_scan_files', 550);
 		$this->scanFiles
@@ -109,6 +119,11 @@ class ScanFilesTest extends TestCase {
 	public function testRunWithUsersAndOffsetAtEndOfUserList() {
 		$this->config
 				->expects($this->at(0))
+				->method('getSystemValueBool')
+				->with('files_no_background_scan', false)
+				->will($this->returnValue(false));
+		$this->config
+				->expects($this->at(1))
 				->method('getAppValue')
 				->with('files', 'cronjob_scan_files', 0)
 				->will($this->returnValue(50));
@@ -123,7 +138,7 @@ class ScanFilesTest extends TestCase {
 				->with('', 500)
 				->will($this->returnValue([]));
 		$this->config
-				->expects($this->at(1))
+				->expects($this->at(2))
 				->method('setAppValue')
 				->with('files', 'cronjob_scan_files', 500);
 		$this->scanFiles
