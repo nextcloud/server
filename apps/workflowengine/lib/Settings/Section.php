cef0e622fbe36fc72216241695a24b57c2193c32
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-11-06 15:54:36 +0100

    Add Flow app icon
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- apps/workflowengine/lib/Settings/Section.php
+++ apps/workflowengine/lib/Settings/Section.php
@@ -23,6 +23,7 @@
 
 namespace OCA\WorkflowEngine\Settings;
 
+use OCA\WorkflowEngine\AppInfo\Application;
 use OCP\IL10N;
 use OCP\IURLGenerator;
 use OCP\Settings\IIconSection;
@@ -67,6 +68,6 @@ class Section implements IIconSection {
 	 * {@inheritdoc}
 	 */
 	public function getIcon() {
-		return $this->url->imagePath('core', 'actions/tag.svg');
+		return $this->url->imagePath(Application::APP_ID, 'app-dark.svg');
 	}
 }
