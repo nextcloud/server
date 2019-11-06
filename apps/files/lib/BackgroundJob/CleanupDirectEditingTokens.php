103c6fb39eab36063b16767abb33da0395b2b9c8
Julius Härtl <jus@bitgrid.net>
2019-11-06 10:41:08 +0100

    Add background job for token cleanup
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ apps/files/lib/BackgroundJob/CleanupDirectEditingTokens.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\Files\BackgroundJob;
+
+use OC\BackgroundJob\TimedJob;
+use OCP\DirectEditing\IManager;
+
+class CleanupDirectEditingTokens extends TimedJob {
+
+	const INTERVAL_MINUTES = 15 * 60;
+
+	/**
+	 * @var IManager
+	 */
+	private $manager;
+
+	public function __construct(IManager $manager) {
+		$this->interval = self::INTERVAL_MINUTES;
+		$this->manager = $manager;
+	}
+
+	/**
+	 * Makes the background job do its work
+	 *
+	 * @param array $argument unused argument
+	 * @throws \Exception
+	 */
+	public function run($argument) {
+		$this->manager->cleanup();
+	}
+}
