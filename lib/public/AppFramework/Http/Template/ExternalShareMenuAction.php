33b2f4e29589e6a415612f7d15525659847f3a8a
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-11-07 14:19:04 +0100

    Format HTML elements
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/public/AppFramework/Http/Template/ExternalShareMenuAction.php
+++ lib/public/AppFramework/Http/Template/ExternalShareMenuAction.php
@@ -67,14 +67,14 @@ class ExternalShareMenuAction extends SimpleMenuAction {
 	 */
 	public function render(): string {
 		return '<li>' .
-			'<a id="save-external-share" data-protected="false" data-owner-display-name="' . Util::sanitizeHTML($this->displayname) . '" data-owner="' . Util::sanitizeHTML($this->owner) . '" data-name="' . Util::sanitizeHTML($this->shareName) . '">' .
-			'<span class="icon ' . Util::sanitizeHTML($this->getIcon()) . '"></span>' .
-			'<label for="remote_address">' . Util::sanitizeHTML($this->getLabel()) . '</label>' .
-			'<form class="save-form hidden" action="#">' .
-			'<input type="text" id="remote_address" placeholder="user@yourNextcloud.org">' .
-			'<input type="submit" value=" " id="save-button-confirm" class="icon-confirm" disabled="disabled"></button>' .
-			'</form>' .
-			'</a>' .
+			'    <a id="save-external-share" data-protected="false" data-owner-display-name="' . Util::sanitizeHTML($this->displayname) . '" data-owner="' . Util::sanitizeHTML($this->owner) . '" data-name="' . Util::sanitizeHTML($this->shareName) . '">' .
+			'        <span class="icon ' . Util::sanitizeHTML($this->getIcon()) . '"></span>' .
+			'        <label for="remote_address">' . Util::sanitizeHTML($this->getLabel()) . '</label>' .
+			'        <form class="save-form hidden" action="#">' .
+			'            <input type="text" id="remote_address" placeholder="user@yourNextcloud.org">' .
+			'            <input type="submit" value=" " id="save-button-confirm" class="icon-confirm" disabled="disabled"></button>' .
+			'        </form>' .
+			'    </a>' .
 			'</li>';
 	}
 }
