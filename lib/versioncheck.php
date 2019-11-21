e0f32814e33f9ebb8c42744611048cbfac1eb588
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-11-21 09:16:42 +0100

    Allow php7.4
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/versioncheck.php
+++ lib/versioncheck.php
@@ -8,10 +8,10 @@ if (version_compare(PHP_VERSION, '7.1') === -1) {
 	exit(-1);
 }
 
-// Show warning if > PHP 7.3 is used as Nextcloud is not compatible with > PHP 7.3 for now
-if (version_compare(PHP_VERSION, '7.4.0') !== -1) {
+// Show warning if > PHP 7.4 is used as Nextcloud is not compatible with > PHP 7.4 for now
+if (version_compare(PHP_VERSION, '7.5.0') !== -1) {
 	http_response_code(500);
-	echo 'This version of Nextcloud is not compatible with > PHP 7.3.<br/>';
+	echo 'This version of Nextcloud is not compatible with > PHP 7.4.<br/>';
 	echo 'You are currently running ' . PHP_VERSION . '.';
 	exit(-1);
 }
