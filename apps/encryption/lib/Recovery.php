8f403b66d19011b1061430a9e82f1166c7bb7cd6
Daniel Kesselberg <mail@danielkesselberg.de>
2019-12-09 14:29:22 +0100

    Fix Argument 3 passed to OCA\Encryption\Recovery::__construct() must be an instance of OCA\Encryption\KeyManager
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- apps/encryption/lib/Recovery.php
+++ apps/encryption/lib/Recovery.php
@@ -30,12 +30,10 @@ namespace OCA\Encryption;
 use OC\Files\View;
 use OCA\Encryption\Crypto\Crypt;
 use OCP\Encryption\IFile;
-use OCP\Encryption\Keys\IStorage;
 use OCP\IConfig;
 use OCP\IUser;
 use OCP\IUserSession;
 use OCP\PreConditionNotMetException;
-use OCP\Security\ISecureRandom;
 
 class Recovery {
 
