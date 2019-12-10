848ca717304996eb80d390872ebc7990d99e8c4a
Joas Schilling <coding@schilljs.com>
2019-12-10 10:56:47 +0100

    Try more methods
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Files/Type/Detection.php
+++ lib/private/Files/Type/Detection.php
@@ -219,7 +219,10 @@ class Detection implements IMimeTypeDetector {
 			if ($info) {
 				$info = strtolower($info);
 				$mimeType = strpos($info, ';') !== false ? substr($info, 0, strpos($info, ';')) : $info;
-				return $this->getSecureMimeType($mimeType);
+				$mimeType = $this->getSecureMimeType($mimeType);
+				if ($mimeType !== 'application/octet-stream') {
+					return $mimeType;
+				}
 			}
 		}
 
@@ -232,7 +235,10 @@ class Detection implements IMimeTypeDetector {
 			// use mime magic extension if available
 			$mimeType = mime_content_type($path);
 			if ($mimeType !== false) {
-				return $this->getSecureMimeType($mimeType);
+				$mimeType = $this->getSecureMimeType($mimeType);
+				if ($mimeType !== 'application/octet-stream') {
+					return $mimeType;
+				}
 			}
 		}
 
@@ -247,7 +253,10 @@ class Detection implements IMimeTypeDetector {
 			if ($mimeType !== false) {
 				//trim the newline
 				$mimeType = trim($mimeType);
-				return $this->getSecureMimeType($mimeType);
+				$mimeType = $this->getSecureMimeType($mimeType);
+				if ($mimeType !== 'application/octet-stream') {
+					return $mimeType;
+				}
 			}
 
 		}
