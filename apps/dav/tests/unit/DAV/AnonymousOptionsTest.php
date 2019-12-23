ef1e12e69d54c621825d7edabda441f4b208df3d
Julius Härtl <jus@bitgrid.net>
2019-12-23 13:08:10 +0100

    Check for empty authorization headers for office requests and allow anonymous option on the whole tree
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/dav/tests/unit/DAV/AnonymousOptionsTest.php
+++ apps/dav/tests/unit/DAV/AnonymousOptionsTest.php
@@ -30,7 +30,7 @@ use Sabre\HTTP\Sapi;
 use Test\TestCase;
 
 class AnonymousOptionsTest extends TestCase {
-	private function sendRequest($method, $path) {
+	private function sendRequest($method, $path, $userAgent = '') {
 		$server = new Server();
 		$server->addPlugin(new AnonymousOptionsPlugin());
 		$server->addPlugin(new Plugin(new BasicCallBack(function() {
@@ -39,6 +39,7 @@ class AnonymousOptionsTest extends TestCase {
 
 		$server->httpRequest->setMethod($method);
 		$server->httpRequest->setUrl($path);
+		$server->httpRequest->setHeader('User-Agent', $userAgent);
 
 		$server->sapi = new SapiMock();
 		$server->exec();
@@ -60,7 +61,19 @@ class AnonymousOptionsTest extends TestCase {
 	public function testAnonymousOptionsNonRootSubDir() {
 		$response = $this->sendRequest('OPTIONS', 'foo/bar');
 
-		$this->assertEquals(401, $response->getStatus());
+		$this->assertEquals(200, $response->getStatus());
+	}
+
+	public function testAnonymousHead() {
+		$response = $this->sendRequest('HEAD', '', 'Microsoft Office does strange things');
+
+		$this->assertEquals(200, $response->getStatus());
+	}
+
+	public function testAnonymousHeadNoOffice() {
+		$response = $this->sendRequest('HEAD', '');
+
+		$this->assertEquals(401, $response->getStatus(), 'curl');
 	}
 }
 
