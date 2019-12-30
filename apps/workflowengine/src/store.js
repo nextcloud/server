df54bb901c6adc9ed2dcaf9e64e5f1a0c857676f
Julius Härtl <jus@bitgrid.net>
2019-12-30 13:25:59 +0100

    Only ask for password when updating admin rules
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/workflowengine/src/store.js
+++ apps/workflowengine/src/store.js
@@ -114,7 +114,9 @@ const store = new Vuex.Store({
 			context.commit('removeRule', rule)
 		},
 		async pushUpdateRule(context, rule) {
-			await confirmPassword()
+			if (context.state.scope === 0) {
+				await confirmPassword()
+			}
 			let result
 			if (rule.id < 0) {
 				result = await axios.post(getApiUrl(''), rule)
