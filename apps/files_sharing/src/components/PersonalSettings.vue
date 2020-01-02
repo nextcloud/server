272aa9231c0857c2e88258dad884d35f0ee89b04
Julius Härtl <jus@bitgrid.net>
2020-01-02 09:39:12 +0100

    Fix style of the sharing setting checkbox
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files_sharing/src/components/PersonalSettings.vue
+++ apps/files_sharing/src/components/PersonalSettings.vue
@@ -22,11 +22,14 @@
 <template>
 	<div id="files-sharing-personal-settings" class="section">
 		<h2>{{ t('files', 'Sharing') }}</h2>
-		<input id="files-sharing-personal-settings-accept"
-			v-model="accepting"
-			type="checkbox"
-			@change="toggleEnabled">
-		<label for="files-sharing-personal-settings-accept">{{ t('files_sharing', 'Accept user and group shares by default') }}</label>
+		<p>
+			<input id="files-sharing-personal-settings-accept"
+				v-model="accepting"
+				class="checkbox"
+				type="checkbox"
+				@change="toggleEnabled">
+			<label for="files-sharing-personal-settings-accept">{{ t('files_sharing', 'Accept user and group shares by default') }}</label>
+		</p>
 	</div>
 </template>
 
