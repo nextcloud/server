fc54df81d0b57dd743dae200e61805a18ad0abcb
Maxence Lange <maxence@artificial-owl.com>
2020-01-07 09:40:41 -0100

    remove non-circles-generated shares only
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- apps/federatedfilesharing/lib/FederatedShareProvider.php
+++ apps/federatedfilesharing/lib/FederatedShareProvider.php
@@ -599,7 +599,8 @@ class FederatedShareProvider implements IShareProvider {
 	private function removeShareFromTableById($shareId) {
 		$qb = $this->dbConnection->getQueryBuilder();
 		$qb->delete('share')
-			->where($qb->expr()->eq('id', $qb->createNamedParameter($shareId)));
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($shareId)))
+			->where($qb->expr()->neq('share_type', $qb->createNamedParameter(IShare::TYPE_CIRCLE)));
 		$qb->execute();
 
 		$qb->delete('federated_reshares')
