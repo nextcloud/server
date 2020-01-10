14f6030140eebed1d439ebd62c79c224c2405196
Julius Härtl <jus@bitgrid.net>
2020-01-10 13:06:27 +0100

    CHeck if file already exists during file creation
    Signed-off-by: Julius Härtl <jus@bitgrid.net>
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- tests/lib/DirectEditing/ManagerTest.php
+++ tests/lib/DirectEditing/ManagerTest.php
@@ -12,6 +12,7 @@ use OCP\DirectEditing\IEditor;
 use OCP\DirectEditing\IToken;
 use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
+use OCP\Files\NotFoundException;
 use OCP\IDBConnection;
 use OCP\IL10N;
 use OCP\IUserSession;
@@ -151,6 +152,10 @@ class ManagerTest extends TestCase {
 		$this->random->expects($this->once())
 			->method('generate')
 			->willReturn($expectedToken);
+		$this->userFolder
+			->method('get')
+			->with('/File.txt')
+			->willThrowException(new NotFoundException());
 		$this->userFolder->expects($this->once())
 			->method('newFile')
 			->willReturn($file);
@@ -167,6 +172,10 @@ class ManagerTest extends TestCase {
 		$this->random->expects($this->once())
 			->method('generate')
 			->willReturn($expectedToken);
+		$this->userFolder
+			->method('get')
+			->with('/File.txt')
+			->willThrowException(new NotFoundException());
 		$this->userFolder->expects($this->once())
 			->method('newFile')
 			->willReturn($file);
@@ -175,6 +184,12 @@ class ManagerTest extends TestCase {
 		$secondResult = $this->manager->edit($expectedToken);
 		$this->assertInstanceOf(DataResponse::class, $firstResult);
 		$this->assertInstanceOf(NotFoundResponse::class, $secondResult);
+	}
+
+	public function testCreateFileAlreadyExists() {
+		$this->expectException(\RuntimeException::class);
+
+		$this->manager->create('/File.txt', 'testeditor', 'createEmpty');
 	}
 
 }
