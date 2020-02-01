5d8ed5c144bb795772f92492a5a327cdf11bfb99
Thomas Citharel <tcit@tcit.fr>
2020-02-01 15:29:02 +0100

    Hash event UID to make sure it's not too long for PushProvider notifications
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- apps/dav/lib/CalDAV/Reminder/NotificationProvider/PushProvider.php
+++ apps/dav/lib/CalDAV/Reminder/NotificationProvider/PushProvider.php
@@ -89,6 +89,9 @@ class PushProvider extends AbstractProvider {
 
 		$eventDetails = $this->extractEventDetails($vevent);
 		$eventDetails['calendar_displayname'] = $calendarDisplayName;
+		$eventUUID = (string) $vevent->UID;
+		// Empty Notification ObjectId will be catched by OC\Notification\Notification
+		$eventUUIDHash = $eventUUID ? hash('sha256', $eventUUID, false) : '';
 
 		foreach($users as $user) {
 			/** @var INotification $notification */
@@ -96,7 +99,7 @@ class PushProvider extends AbstractProvider {
 			$notification->setApp(Application::APP_ID)
 				->setUser($user->getUID())
 				->setDateTime($this->timeFactory->getDateTime())
-				->setObject(Application::APP_ID, (string) $vevent->UID)
+				->setObject(Application::APP_ID, $eventUUIDHash)
 				->setSubject('calendar_reminder', [
 					'title' => $eventDetails['title'],
 					'start_atom' => $eventDetails['start_atom']
