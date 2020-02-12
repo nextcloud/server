fb53eb3c8b7d69e5e5915fc2c8dbab7c03366dfa
GretaD <gretadoci@gmail.com>
2020-02-12 15:08:14 +0100

    Add text restore after restore icon
    Signed-off-by: GretaD <gretadoci@gmail.com>
    Signed-off-by: npmbuildbot[bot] <npmbuildbot[bot]@users.noreply.github.com>

--- apps/files_sharing/js/app.js
+++ apps/files_sharing/js/app.js
@@ -231,7 +231,7 @@ OCA.Sharing.App = {
 		var fileActions = new OCA.Files.FileActions();
 		fileActions.registerAction({
 			name: 'Restore',
-			displayName: '',
+			displayName: t('files_sharing', 'Restore'),
 			altText: t('files_sharing', 'Restore share'),
 			mime: 'all',
 			permissions: OC.PERMISSION_ALL,
