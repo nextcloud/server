49eaef63c293271c0c328d41270faee6aa537b92
Arthur Schiwon <blizzz@arthur-schiwon.de>
2020-03-19 19:42:46 +0100

    fixes auto-detecting UUID attributes
    the continue (and later the early return) avoided proper looping over the
    attribute candidates.
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/user_ldap/lib/Access.php
+++ apps/user_ldap/lib/Access.php
@@ -1698,7 +1698,6 @@ class Access extends LDAPUtility {
 					$this->connection->$uuidAttr = $attribute;
 					return true;
 				}
-				continue;
 			}
 
 			$value = $this->readAttribute($dn, $attribute);
@@ -1714,9 +1713,6 @@ class Access extends LDAPUtility {
 				$this->connection->$uuidAttr = $attribute;
 				$this->connection->writeToCache($uuidAttr, $attribute);
 				return true;
-			} elseif ($value === false) {
-				// record not available
-				return false;
 			}
 		}
 		\OC::$server->getLogger()->debug('Could not autodetect the UUID attribute', ['app' => 'user_ldap']);
