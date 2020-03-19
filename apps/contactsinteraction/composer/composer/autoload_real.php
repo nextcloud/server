37786c4d74618bcedc9f6087a8994aa224a349ff
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-03-19 09:47:36 +0100

    Convert share event to an interaction event
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/contactsinteraction/composer/composer/autoload_real.php
+++ apps/contactsinteraction/composer/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitContactsInteraction
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
