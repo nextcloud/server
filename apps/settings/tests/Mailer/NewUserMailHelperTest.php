2fc35d10e4abf4d54397ff98ae680e3d01699a45
Roeland Jago Douma <roeland@famdouma.nl>
2020-04-17 16:57:18 +0200

    Added trailing space
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/settings/tests/Mailer/NewUserMailHelperTest.php
+++ apps/settings/tests/Mailer/NewUserMailHelperTest.php
@@ -377,8 +377,8 @@ Set your password: https://example.com/resetPassword/MySuperLongSecureRandomToke
 Install Client: https://nextcloud.com/install/#install-clients
 
 
---
-TestCloud -
+-- 
+TestCloud - 
 This is an automatically sent email, please do not reply.
 EOF;
 
@@ -614,8 +614,8 @@ Go to TestCloud: https://example.com/
 Install Client: https://nextcloud.com/install/#install-clients
 
 
---
-TestCloud -
+-- 
+TestCloud - 
 This is an automatically sent email, please do not reply.
 EOF;
 
@@ -838,8 +838,8 @@ Go to TestCloud: https://example.com/
 Install Client: https://nextcloud.com/install/#install-clients
 
 
---
-TestCloud -
+-- 
+TestCloud - 
 This is an automatically sent email, please do not reply.
 EOF;
 
