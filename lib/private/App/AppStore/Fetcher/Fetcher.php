3e0d8df0364bfb9da5d36e714e3de1c56f02dcd4
Morris Jobke <hey@morrisjobke.de>
2020-05-19 23:04:25 +0200

    Cache appstore requests for 60 instead of 5 minutes
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- lib/private/App/AppStore/Fetcher/Fetcher.php
+++ lib/private/App/AppStore/Fetcher/Fetcher.php
@@ -41,7 +41,7 @@ use OCP\IConfig;
 use OCP\ILogger;
 
 abstract class Fetcher {
-	public const INVALIDATE_AFTER_SECONDS = 300;
+	public const INVALIDATE_AFTER_SECONDS = 3600;
 
 	/** @var IAppData */
 	protected $appData;
@@ -153,7 +153,7 @@ abstract class Fetcher {
 				// No caching when the version has been updated
 				if (isset($jsonBlob['ncversion']) && $jsonBlob['ncversion'] === $this->getVersion()) {
 
-					// If the timestamp is older than 300 seconds request the files new
+					// If the timestamp is older than 3600 seconds request the files new
 					if ((int)$jsonBlob['timestamp'] > ($this->timeFactory->getTime() - self::INVALIDATE_AFTER_SECONDS)) {
 						return $jsonBlob['data'];
 					}
