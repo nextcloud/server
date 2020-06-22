b0a8781cefb8e729ceed6903a96167eaaabe70b8
Julius Härtl <jus@bitgrid.net>
2020-06-22 14:15:42 +0200

    Remove rescanDelay from directory mtime
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files_external/lib/Lib/Storage/AmazonS3.php
+++ apps/files_external/lib/Lib/Storage/AmazonS3.php
@@ -56,11 +56,6 @@ class AmazonS3 extends \OC\Files\Storage\Common {
 		return false;
 	}
 
-	/**
-	 * @var int in seconds
-	 */
-	private $rescanDelay = 10;
-
 	/** @var CappedMemoryCache|Result[] */
 	private $objectCache;
 
@@ -377,7 +372,7 @@ class AmazonS3 extends \OC\Files\Storage\Common {
 			if ($this->is_dir($path)) {
 				//folders don't really exist
 				$stat['size'] = -1; //unknown
-				$stat['mtime'] = time() - $this->rescanDelay * 1000;
+				$stat['mtime'] = time();
 			} else {
 				$stat['size'] = $this->getContentLength($path);
 				$stat['mtime'] = strtotime($this->getLastModified($path));
