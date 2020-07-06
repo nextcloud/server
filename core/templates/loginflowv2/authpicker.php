145081f07e00185d13cef36d77db7d2c209c5e78
Joas Schilling <coding@schilljs.com>
2020-07-06 13:44:12 +0200

    Add a clear message why you could end up there
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/templates/loginflowv2/authpicker.php
+++ core/templates/loginflowv2/authpicker.php
@@ -35,6 +35,10 @@ $urlGenerator = $_['urlGenerator'];
 							])) ?>
 	</p>
 
+	<p class="info">
+		<?php print_unescaped($l->t('If you are not trying to set up a new device or app, someone is trying to trick you into granting them access to your data. In this case do not proceed and instead contact your system administrator.')) ?>
+	</p>
+
 	<br/>
 
 	<p id="redirect-link">
