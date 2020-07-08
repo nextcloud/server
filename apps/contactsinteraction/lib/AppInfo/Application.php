a5488c01de7f8caffd9a4972bfb5bd321f271d4b
Morris Jobke <hey@morrisjobke.de>
2020-07-08 12:23:59 +0200

    Move contactsinteraction to bootstrap mechanism
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- apps/contactsinteraction/lib/AppInfo/Application.php
+++ apps/contactsinteraction/lib/AppInfo/Application.php
@@ -27,19 +27,22 @@ namespace OCA\ContactsInteraction\AppInfo;
 
 use OCA\ContactsInteraction\Listeners\ContactInteractionListener;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\Contacts\Events\ContactInteractedWithEvent;
-use OCP\EventDispatcher\IEventDispatcher;
 
-class Application extends App {
+class Application extends App implements IBootstrap {
 	public const APP_ID = 'contactsinteraction';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
-
-		$this->registerListeners($this->getContainer()->query(IEventDispatcher::class));
 	}
 
-	private function registerListeners(IEventDispatcher $dispatcher): void {
-		$dispatcher->addServiceListener(ContactInteractedWithEvent::class, ContactInteractionListener::class);
+	public function register(IRegistrationContext $context): void {
+		$context->registerEventListener(ContactInteractedWithEvent::class, ContactInteractionListener::class);
+	}
+
+	public function boot(IBootContext $context): void {
 	}
 }
