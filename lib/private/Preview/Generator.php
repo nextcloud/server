9aff51eb52d531a576158d02a6cb626167a3de00
Morris Jobke <hey@morrisjobke.de>
2020-08-05 15:52:40 +0200

    Use class that actually has the destroy() method in preview generator
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- lib/private/Preview/Generator.php
+++ lib/private/Preview/Generator.php
@@ -194,7 +194,7 @@ class Generator {
 
 		// Free memory being used by the embedded image resource.  Without this the image is kept in memory indefinitely.
 		// Garbage Collection does NOT free this memory.  We have to do it ourselves.
-		if ($maxPreviewImage instanceof IImage) {
+		if ($maxPreviewImage instanceof \OC_Image) {
 			$maxPreviewImage->destroy();
 		}
 
