5242d9c485c70650b2ea6389e9023ecab4991be2
Joas Schilling <coding@schilljs.com>
2020-08-18 11:44:38 +0200

    Don't lowercase types as that doesn't work in some languages
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/src/views/UnifiedSearch.vue
+++ core/src/views/UnifiedSearch.vue
@@ -37,7 +37,7 @@
 				v-model="query"
 				class="unified-search__input"
 				type="search"
-				:placeholder="t('core', 'Search {types} …', { types: typesNames.filter(unique).join(', ').toLowerCase() })"
+				:placeholder="t('core', 'Search {types} …', { types: typesNames.filter(unique).join(', ') })"
 				@input="onInputDebounced"
 				@keypress.enter.prevent.stop="onInputEnter">
 			<!-- Search filters -->
