794e925125c2803d0e5f3c9025752e66a5471a8b
Roeland Jago Douma <roeland@famdouma.nl>
2020-08-24 13:37:39 +0200

    Make legacy format opt-in
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- apps/encryption/tests/Crypto/CryptTest.php
+++ apps/encryption/tests/Crypto/CryptTest.php
@@ -214,6 +214,10 @@ class CryptTest extends TestCase {
 	 * @dataProvider dataTestSplitMetaData
 	 */
 	public function testSplitMetaData($data, $expected) {
+		$this->config->method('getSystemValue')
+			->with('encryption_skip_signature_check', false)
+			->willReturn(true);
+
 		$result = self::invokePrivate($this->crypt, 'splitMetaData', array($data, 'AES-256-CFB'));
 		$this->assertTrue(is_array($result));
 		$this->assertSame(3, count($result));
@@ -238,6 +242,10 @@ class CryptTest extends TestCase {
 	 * @dataProvider dataTestHasSignature
 	 */
 	public function testHasSignature($data, $expected) {
+		$this->config->method('getSystemValue')
+			->with('encryption_skip_signature_check', false)
+			->willReturn(true);
+
 		$this->assertSame($expected,
 			$this->invokePrivate($this->crypt, 'hasSignature', array($data, 'AES-256-CFB'))
 		);
@@ -395,6 +403,10 @@ class CryptTest extends TestCase {
 	 * @dataProvider dataTestDecryptPrivateKey
 	 */
 	public function testDecryptPrivateKey($header, $privateKey, $expectedCipher, $isValidKey, $expected) {
+		$this->config->method('getSystemValueBool')
+			->with('encryption.legacy_format_support', true)
+			->willReturn(true);
+
 		/** @var \OCA\Encryption\Crypto\Crypt | \PHPUnit_Framework_MockObject_MockObject $crypt */
 		$crypt = $this->getMockBuilder(Crypt::class)
 			->setConstructorArgs(
