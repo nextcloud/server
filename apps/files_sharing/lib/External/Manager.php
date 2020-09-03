f96853a19c6925af5153fe4f5316d9e7b3f49d7f
Julius Härtl <jus@bitgrid.net>
2020-09-03 11:17:59 +0200

    Do not fail if share for mountpoint is no longer available
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files_sharing/lib/External/Manager.php
+++ apps/files_sharing/lib/External/Manager.php
@@ -511,7 +511,7 @@ class Manager {
 			WHERE `id` = ?
 			');
 			$result = (bool)$query->execute([(int)$share['id']]);
-		} elseif ($result && (int)$share['share_type'] === IShare::TYPE_GROUP) {
+		} elseif ($result && $share !== false && (int)$share['share_type'] === IShare::TYPE_GROUP) {
 			$query = $this->connection->prepare('
 				UPDATE `*PREFIX*share_external`
 				SET `accepted` = ?
