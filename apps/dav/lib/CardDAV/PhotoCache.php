ac1ca28d5ed9d68c5c81aa30fa505b5834f1e7a0
Jacob Neplokh <me@jacobneplokh.com>
2020-09-05 11:44:01 -0700

    Change Content-Disposition Header
    - Make ALLOWED_CONTENT_TYPES public in order to use
    - Add $fileName variable which uses "$node->getName()" to get the proper file name and "$file->getMimeType()" along with the ALLOWED_CONTENT_TYPES array in PhotoCache.php to get the proper file extension
    - Make "$fileName" the name of the file in the Content-Disposition header when downloading a Contact's photo
    - Add filename to the CardDAV integration image export test header
    - Change headers in ImageExportPluginTest to reflect changes
    
    Signed-off-by: Jacob Neplokh <me@jacobneplokh.com>
    Signed-off-by: Henry-Nicolas Tourneur <debian@nilux.be>

--- apps/dav/lib/CardDAV/PhotoCache.php
+++ apps/dav/lib/CardDAV/PhotoCache.php
@@ -43,7 +43,7 @@ use Sabre\VObject\Reader;
 class PhotoCache {
 
 	/** @var array  */
-	protected const ALLOWED_CONTENT_TYPES = [
+	public const ALLOWED_CONTENT_TYPES = [
 		'image/png' => 'png',
 		'image/jpeg' => 'jpg',
 		'image/gif' => 'gif',
