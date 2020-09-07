32c632796b62a83d2663945c475447ca40e44caf
Morris Jobke <hey@morrisjobke.de>
2020-09-07 18:02:57 +0200

    Only load viewer in settings if the viewer app is enabled
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>
    Signed-off-by: Henry-Nicolas Tourneur <debian@nilux.be>

--- apps/settings/lib/Settings/Personal/ServerDevNotice.php
+++ apps/settings/lib/Settings/Personal/ServerDevNotice.php
@@ -78,6 +78,9 @@ class ServerDevNotice implements ISettings {
 
 		$hasInitialState = false;
 
+		// viewer is default enabled and this makes a zero-cost assertion for Psalm
+		assert(class_exists(LoadViewer::class));
+
 		// If the Reasons to use Nextcloud.pdf file is here, let's init Viewer
 		if ($userFolder->nodeExists('Reasons to use Nextcloud.pdf')) {
 			$this->eventDispatcher->dispatch(LoadViewer::class, new LoadViewer());
