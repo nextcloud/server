ee225ec776a226c61b7d7fd3b8baf23ce8c9dd6e
Julien Veyssier <eneiluj@posteo.net>
2020-10-02 15:55:52 +0200

    refs #23140 split dashboard attributions in multiple lines to allow translation parsing
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>
    Signed-off-by: Henry-Nicolas Tourneur <debian@nilux.be>

--- apps/dashboard/src/App.vue
+++ apps/dashboard/src/App.vue
@@ -75,7 +75,9 @@
 					{{ t('dashboard', 'For your privacy, the weather data is requested by your Nextcloud server on your behalf so the weather service receives no personal information.') }}
 				</p>
 				<p class="credits--end">
-					<a href="https://api.met.no/doc/TermsOfService" target="_blank" rel="noopener">{{ t('dashboard', 'Weather data from Met.no') }}</a>, <a href="https://wiki.osmfoundation.org/wiki/Privacy_Policy" target="_blank" rel="noopener">{{ t('dashboard', 'geocoding with Nominatim') }}</a>, <a href="https://www.opentopodata.org/#public-api" target="_blank" rel="noopener">{{ t('dashboard', 'elevation data from OpenTopoData') }}</a>.
+					<a href="https://api.met.no/doc/TermsOfService" target="_blank" rel="noopener">{{ t('dashboard', 'Weather data from Met.no') }}</a>,
+					<a href="https://wiki.osmfoundation.org/wiki/Privacy_Policy" target="_blank" rel="noopener">{{ t('dashboard', 'geocoding with Nominatim') }}</a>,
+					<a href="https://www.opentopodata.org/#public-api" target="_blank" rel="noopener">{{ t('dashboard', 'elevation data from OpenTopoData') }}</a>.
 				</p>
 			</div>
 		</Modal>
