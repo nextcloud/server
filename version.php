dcc146a5c0f4b68cba87149c0ef6d2d77aa311c0
Morris Jobke <hey@morrisjobke.de>
2020-10-22 23:11:04 +0200

    20.0.1
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [20, 0, 1, 0];
+$OC_Version = [20, 0, 1, 1];
 
 // The human readable string
-$OC_VersionString = '20.0.1 RC 1';
+$OC_VersionString = '20.0.1';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
