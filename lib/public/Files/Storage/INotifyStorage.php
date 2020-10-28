32e4a61cac2294ecc9eb9f73323b8dd51c55c514
Morris Jobke <hey@morrisjobke.de>
2020-10-28 23:02:25 +0100

    Remove deprecated INotifyStorage::listen
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- lib/public/Files/Storage/INotifyStorage.php
+++ lib/public/Files/Storage/INotifyStorage.php
@@ -38,24 +38,6 @@ interface INotifyStorage {
 	public const NOTIFY_RENAMED = 4;
 
 	/**
-	 * Start listening for update notifications
-	 *
-	 * The provided callback will be called for every incoming notification with the following parameters
-	 *  - int $type the type of update, one of the INotifyStorage::NOTIFY_* constants
-	 *  - string $path the path of the update
-	 *  - string $renameTarget the target of the rename operation, only provided for rename updates
-	 *
-	 * Note that this call is blocking and will not exit on it's own, to stop listening for notifications return `false` from the callback
-	 *
-	 * @param string $path
-	 * @param callable $callback
-	 *
-	 * @since 9.1.0
-	 * @deprecated 12.0.0 use INotifyStorage::notify()->listen() instead
-	 */
-	public function listen($path, callable $callback);
-
-	/**
 	 * Start the notification handler for this storage
 	 *
 	 * @param $path
