0a7fef627229f83296ab67e1ae9ac74399957ab8
Joas Schilling <coding@schilljs.com>
2020-10-30 09:47:27 +0100

    Also run all tests when drone config is modified
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/drone-run-php-tests.sh
+++ tests/drone-run-php-tests.sh
@@ -8,6 +8,8 @@ echo "========================="
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | wc -l) -eq 0 ]] && echo "No files are modified => merge commit" && exit 0
 
+[[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c ".yml") -gt 0 ]] && echo "YML files are modified" && exit 0
+
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c ".xml") -gt 0 ]] && echo "info.xml files are modified" && exit 0
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c ".php$") -gt 0 ]] && echo "PHP files are modified" && exit 0
