c832e6180bcefbd82748c8e240438d6c2e21b358
Morris Jobke <hey@morrisjobke.de>
2020-11-22 21:56:00 +0100

    Remove unused dependencies in encryption app setup
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- apps/encryption/lib/Users/Setup.php
+++ apps/encryption/lib/Users/Setup.php
@@ -29,40 +29,15 @@ namespace OCA\Encryption\Users;
 
 use OCA\Encryption\Crypto\Crypt;
 use OCA\Encryption\KeyManager;
-use OCP\ILogger;
-use OCP\IUserSession;
 
 class Setup {
-	/**
-	 * @var Crypt
-	 */
+	/** @var Crypt */
 	private $crypt;
-	/**
-	 * @var KeyManager
-	 */
+	/** @var KeyManager */
 	private $keyManager;
-	/**
-	 * @var ILogger
-	 */
-	private $logger;
-	/**
-	 * @var bool|string
-	 */
-	private $user;
 
-
-	/**
-	 * @param ILogger $logger
-	 * @param IUserSession $userSession
-	 * @param Crypt $crypt
-	 * @param KeyManager $keyManager
-	 */
-	public function __construct(ILogger $logger,
-								IUserSession $userSession,
-								Crypt $crypt,
+	public function __construct(Crypt $crypt,
 								KeyManager $keyManager) {
-		$this->logger = $logger;
-		$this->user = $userSession && $userSession->isLoggedIn() ? $userSession->getUser()->getUID() : false;
 		$this->crypt = $crypt;
 		$this->keyManager = $keyManager;
 	}
