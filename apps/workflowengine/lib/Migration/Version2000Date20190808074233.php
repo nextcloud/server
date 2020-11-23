4850019fcd65ddf5b1db620a657a1403afe4a704
Arthur Schiwon <blizzz@arthur-schiwon.de>
2020-11-23 21:17:37 +0100

    avoid empty null default with value that will be inserted anyways
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/workflowengine/lib/Migration/Version2000Date20190808074233.php
+++ apps/workflowengine/lib/Migration/Version2000Date20190808074233.php
@@ -7,6 +7,7 @@ namespace OCA\WorkflowEngine\Migration;
 use Closure;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Types\Types;
+use OCA\WorkflowEngine\Entity\File;
 use OCP\DB\ISchemaWrapper;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
@@ -116,7 +117,7 @@ class Version2000Date20190808074233 extends SimpleMigrationStep {
 			$table->addColumn('entity', Types::STRING, [
 				'notnull' => true,
 				'length' => 256,
-				'default' => '',
+				'default' => File::class,
 			]);
 		}
 		if (!$table->hasColumn('events')) {
