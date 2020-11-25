299aeb917c14e22d3e05998c1c7216c2aaed2199
Olivier Perdaens <olivier.perdaens@student.uclouvain.be>
2020-11-25 18:08:35 +0100

    reset config.sample.php as before
    Signed-off-by: Olivier Perdaens <olivier.perdaens@student.uclouvain.be>

--- config/config.sample.php
+++ config/config.sample.php
@@ -821,7 +821,7 @@ $CONFIG = [
  *
  * Defaults to ``2``
  */
-'loglevel' => 0,
+'loglevel' => 2,
 
 /**
  * If you maintain different instances and aggregate the logs, you may want
@@ -1776,7 +1776,7 @@ $CONFIG = [
  *
  * Defaults to ``false``
  */
-'debug' => true,
+'debug' => false,
 
 /**
  * Sets the data-fingerprint of the current data served
