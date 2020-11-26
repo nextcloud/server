88f35d52d2baf6421725ce5f2a40331afe13f1de
Robin Appelman <robin@icewind.nl>
2020-11-26 15:39:46 +0100

    rename cache event to follow new naming standards
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/public/Files/Cache/CacheUpdateEvent.php
+++ lib/public/Files/Cache/CacheUpdateEvent.php
@@ -26,12 +26,11 @@ declare(strict_types=1);
 
 namespace OCP\Files\Cache;
 
-use OC\Files\Cache\AbstractCacheEvent;
-
 /**
  * Event for when an existing entry in the cache gets updated
  *
  * @since 16.0.0
+ * @deprecated 21.0.0 use CacheEntryUpdatedEvent instead
  */
-class CacheUpdateEvent extends AbstractCacheEvent {
+class CacheUpdateEvent extends CacheEntryUpdatedEvent {
 }
