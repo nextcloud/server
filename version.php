556a14f283222c46bd72fb0cf53743935fa4d4cc
Roeland Jago Douma <roeland@famdouma.nl>
2020-12-14 15:36:19 +0100

    20.0.4
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [20, 0, 3, 2];
+$OC_Version = [20, 0, 4, 0];
 
 // The human readable string
-$OC_VersionString = '20.0.3';
+$OC_VersionString = '20.0.4';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
