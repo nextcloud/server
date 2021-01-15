b8acf2fe69a856511e004a512e2168ac6990c87a
Joas Schilling <coding@schilljs.com>
2021-01-15 08:32:09 +0100

    Add a hint about the direction of priority
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/public/AppFramework/Bootstrap/IRegistrationContext.php
+++ lib/public/AppFramework/Bootstrap/IRegistrationContext.php
@@ -119,7 +119,8 @@ interface IRegistrationContext {
 	 * @psalm-param string|class-string<T> $event preferably the fully-qualified class name of the Event sub class to listen for
 	 * @param string $listener fully qualified class name (or ::class notation) of a \OCP\EventDispatcher\IEventListener that can be built by the DI container
 	 * @psalm-param class-string<\OCP\EventDispatcher\IEventListener<T>> $listener fully qualified class name that can be built by the DI container
-	 * @param int $priority
+	 * @param int $priority The higher this value, the earlier an event
+	 *                      listener will be triggered in the chain (defaults to 0)
 	 *
 	 * @see IEventDispatcher::addServiceListener()
 	 *
