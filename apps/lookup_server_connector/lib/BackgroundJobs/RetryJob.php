a274bcaf300f7e483fb51f57797263ffc9984c6a
Samuel MARTIN MORO <faust64@gmail.com>
2021-01-24 11:59:07 +0100

    fix(lookupserver): nextcloud/server#25290
    Signed-off-by: Samuel MARTIN MORO <faust64@gmail.com>

--- apps/lookup_server_connector/lib/BackgroundJobs/RetryJob.php
+++ apps/lookup_server_connector/lib/BackgroundJobs/RetryJob.php
@@ -194,23 +194,28 @@ class RetryJob extends Job {
 		$publicData = [];
 		foreach ($account->getProperties() as $property) {
 			if ($property->getScope() === IAccountManager::VISIBILITY_PUBLIC) {
+				if ($property->getName() === IAccountManager::PROPERTY_TWITTER) {
+				    $publicData[IAccountManager::PROPERTY_TWITTER . '_verified'] = $property->getVerified();
+				} else if ($property->getName() === IAccountManager::PROPERTY_WEBSITE) {
+				    $publicData[IAccountManager::PROPERTY_WEBSITE . '_verified'] = $property->getVerified();
+				}
 				$publicData[$property->getName()] = $property->getValue();
 			}
 		}
 
 		$data = ['federationId' => $user->getCloudId()];
 		if (!empty($publicData)) {
-			$data['name'] = $publicData[IAccountManager::PROPERTY_DISPLAYNAME]['value'] ?? '';
-			$data['email'] = $publicData[IAccountManager::PROPERTY_EMAIL]['value'] ?? '';
-			$data['address'] = $publicData[IAccountManager::PROPERTY_ADDRESS]['value'] ?? '';
-			$data['website'] = $publicData[IAccountManager::PROPERTY_WEBSITE]['value'] ?? '';
-			$data['twitter'] = $publicData[IAccountManager::PROPERTY_TWITTER]['value'] ?? '';
-			$data['phone'] = $publicData[IAccountManager::PROPERTY_PHONE]['value'] ?? '';
-			$data['twitter_signature'] = $publicData[IAccountManager::PROPERTY_TWITTER]['signature'] ?? '';
-			$data['website_signature'] = $publicData[IAccountManager::PROPERTY_WEBSITE]['signature'] ?? '';
+			$data['name'] = $publicData[IAccountManager::PROPERTY_DISPLAYNAME] ?? '';
+			$data['email'] = $publicData[IAccountManager::PROPERTY_EMAIL] ?? '';
+			$data['address'] = $publicData[IAccountManager::PROPERTY_ADDRESS] ?? '';
+			$data['website'] = $publicData[IAccountManager::PROPERTY_WEBSITE] ?? '';
+			$data['twitter'] = $publicData[IAccountManager::PROPERTY_TWITTER] ?? '';
+			$data['phone'] = $publicData[IAccountManager::PROPERTY_PHONE] ?? '';
+			$data['twitter_signature'] = '';
+			$data['website_signature'] = '';
 			$data['verificationStatus'] = [
-				IAccountManager::PROPERTY_WEBSITE => $publicData[IAccountManager::PROPERTY_WEBSITE]['verified'] ?? '',
-				IAccountManager::PROPERTY_TWITTER => $publicData[IAccountManager::PROPERTY_TWITTER]['verified'] ?? '',
+				IAccountManager::PROPERTY_WEBSITE => $publicData[IAccountManager::PROPERTY_WEBSITE .'_verified'] ?? '',
+				IAccountManager::PROPERTY_TWITTER => $publicData[IAccountManager::PROPERTY_TWITTER .'_verified'] ?? '',
 			];
 		}
 
