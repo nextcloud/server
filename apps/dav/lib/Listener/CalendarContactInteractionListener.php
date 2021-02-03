f4b32f967602310dd3f4438c938ddfe562d4f11b
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-02-03 15:01:58 +0100

    Parse calendar object for attendees and emit interaction events
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/dav/lib/Listener/CalendarContactInteractionListener.php
+++ apps/dav/lib/Listener/CalendarContactInteractionListener.php
@@ -35,7 +35,13 @@ use OCP\EventDispatcher\IEventDispatcher;
 use OCP\EventDispatcher\IEventListener;
 use OCP\IUser;
 use OCP\IUserSession;
+use OCP\Mail\IMailer;
 use Psr\Log\LoggerInterface;
+use Sabre\VObject\Component\VEvent;
+use Sabre\VObject\Parameter;
+use Sabre\VObject\Property;
+use Sabre\VObject\Reader;
+use Throwable;
 use function strlen;
 use function strpos;
 use function substr;
@@ -47,38 +53,58 @@ class CalendarContactInteractionListener implements IEventListener {
 	private $dispatcher;
 
 	/** @var IUserSession */
-	private $userManager;
+	private $userSession;
 
 	/** @var Principal */
 	private $principalConnector;
+
+	/** @var IMailer */
+	private $mailer;
 
 	/** @var LoggerInterface */
 	private $logger;
 
 	public function __construct(IEventDispatcher $dispatcher,
-								IUserSession $userManager,
+								IUserSession $userSession,
 								Principal $principalConnector,
+								IMailer $mailer,
 								LoggerInterface $logger) {
 		$this->dispatcher = $dispatcher;
-		$this->userManager = $userManager;
+		$this->userSession = $userSession;
 		$this->principalConnector = $principalConnector;
+		$this->mailer = $mailer;
 		$this->logger = $logger;
 	}
 
 	public function handle(Event $event): void {
-		if (($user = $this->userManager->getUser()) === null) {
+		if (($user = $this->userSession->getUser()) === null) {
 			// Without user context we can't do anything
 			return;
 		}
 
 		if ($event instanceof CalendarObjectCreatedEvent || $event instanceof CalendarObjectUpdatedEvent) {
 			// users: href => principal:principals/users/admin
-			// TODO: parse (email) attendees from the VCARD
 			foreach ($event->getShares() as $share) {
 				if (!isset($share['href'])) {
 					continue;
 				}
 				$this->emitFromUri($share['href'], $user);
+			}
+
+			// emit interaction for email attendees as well
+			if (isset($event->getObjectData()['calendardata'])) {
+				try {
+					$calendar = Reader::read($event->getObjectData()['calendardata']);
+					if ($calendar->VEVENT) {
+						foreach ($calendar->VEVENT as $calendarEvent) {
+							$this->emitFromObject($calendarEvent, $user);
+						}
+					}
+				} catch (Throwable $e) {
+					$this->logger->warning('Could not read calendar data for interaction events: ' . $e->getMessage(), [
+						'exception' => $e,
+					]);
+				}
 			}
 		}
 
@@ -113,5 +139,39 @@ class CalendarContactInteractionListener implements IEventListener {
 		$this->dispatcher->dispatchTyped(
 			(new ContactInteractedWithEvent($user))->setUid($uid)
 		);
+	}
+
+	private function emitFromObject(VEvent $vevent, IUser $user): void {
+		if (!$vevent->ATTENDEE) {
+			// Nothing left to do
+			return;
+		}
+
+		foreach ($vevent->ATTENDEE as $attendee) {
+			if (!($attendee instanceof Property)) {
+				continue;
+			}
+
+			$cuType = $attendee->offsetGet('CUTYPE');
+			if ($cuType instanceof Parameter && $cuType->getValue() !== 'INDIVIDUAL') {
+				// Don't care about those
+				continue;
+			}
+
+			$mailTo = $attendee->getValue();
+			if (strpos($mailTo, 'mailto:') !== 0) {
+				// Doesn't look like an email
+				continue;
+			}
+			$email = substr($mailTo, strlen('mailto:'));
+			if (!$this->mailer->validateMailAddress($email)) {
+				// This really isn't a valid email
+				continue;
+			}
+
+			$this->dispatcher->dispatchTyped(
+				(new ContactInteractedWithEvent($user))->setEmail($email)
+			);
+		}
 	}
 }
