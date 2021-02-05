038def23286ac685e4a0a5f259d2d5e0b69a8b96
Samuel <faust64@gmail.com>
2021-02-05 12:06:35 +0100

    fix(translation): replace static error message
    as suggested by @kesselb in nextcloud/server#25490
    
    Signed-off-by: Samuel <faust64@gmail.com>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -542,7 +542,8 @@ class Manager implements IManager {
 					$this->groupManager->getUserGroupIds($sharedWith)
 			);
 			if (empty($groups)) {
-				throw new \Exception('Sharing is only allowed with group members');
+				$message_t = $this->l->t('Sharing is only allowed with group members');
+				throw new GenericShareException($message_t, $message_t, 404);
 			}
 		}
 
