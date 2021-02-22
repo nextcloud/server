ee9efcfffba087dee58908a4da5acae0df714d28
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-02-22 10:03:52 +0100

    Remove ie and last 2 versions
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/viewer/cypress/plugins/index.js
+++ apps/viewer/cypress/plugins/index.js
@@ -11,10 +11,12 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const {
-	addMatchImageSnapshotPlugin
-} = require('cypress-image-snapshot/plugin')
+const browserify = require('@cypress/browserify-preprocessor')
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 
 module.exports = (on, config) => {
+	// Fix browserslist extend https://github.com/cypress-io/cypress/issues/2983#issuecomment-570616682
+	on('file:preprocessor', browserify())
+
 	addMatchImageSnapshotPlugin(on, config)
 }
