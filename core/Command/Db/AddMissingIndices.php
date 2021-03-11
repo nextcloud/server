dcb99c43ed20e4e5d31333dbe79770607860688a
Robin Appelman <robin@icewind.nl>
2021-03-11 16:37:23 +0100

    add a prefix index to filecache.path
    The reason that `filecache.path` hasn't had an index added is the mysql limitation of ~1kb for indexeded fields,
    which is to small for the `path`, however mysql supports indexing only the first N bytes of a column instead of the entire column,
    allowing us to add an index even if the column is to long.
    
    Because the index doesn't cover the entire column it can't be used in all situations where a normal index would be used, but it does cover the `path like 'folder/path/%'` queries that are used in various places.
    
    Sqlite and Postgresql don't support prefix indexes, but they also don't have the 1kb limit and DBAL handles the differences in index creation.
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- core/Command/Db/AddMissingIndices.php
+++ core/Command/Db/AddMissingIndices.php
@@ -144,6 +144,13 @@ class AddMissingIndices extends Command {
 				$updated = true;
 				$output->writeln('<info>Filecache table updated successfully.</info>');
 			}
+			if (!$table->hasIndex('fs_storage_path_prefix')) {
+				$output->writeln('<info>Adding additional path index to the filecache table, this can take some time...</info>');
+				$table->addIndex(['storage', 'path'], 'fs_storage_path_prefix', [], ['lengths' => [null, 64]]);
+				$this->connection->migrateToSchema($schema->getWrappedSchema());
+				$updated = true;
+				$output->writeln('<info>Filecache table updated successfully.</info>');
+			}
 		}
 
 		$output->writeln('<info>Check indices of the twofactor_providers table.</info>');
