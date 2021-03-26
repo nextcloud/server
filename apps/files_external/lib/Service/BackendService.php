8afedb562e13bb2f32593c63127c9fc4712643f1
Robin Appelman <robin@icewind.nl>
2021-03-26 14:16:36 +0100

    add typed event for loading additional external storage backends
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_external/lib/Service/BackendService.php
+++ apps/files_external/lib/Service/BackendService.php
@@ -28,12 +28,14 @@
 namespace OCA\Files_External\Service;
 
 use OCA\Files_External\Config\IConfigHandler;
+use OCA\Files_External\Events\LoadAdditionalStorageBackendsEvent;
 use OCA\Files_External\Lib\Auth\AuthMechanism;
 
 use OCA\Files_External\Lib\Backend\Backend;
 use OCA\Files_External\Lib\Config\IAuthMechanismProvider;
 use OCA\Files_External\Lib\Config\IBackendProvider;
 use OCP\EventDispatcher\GenericEvent;
+use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IConfig;
 
 /**
@@ -117,6 +119,9 @@ class BackendService {
 				'OCA\\Files_External::loadAdditionalBackends',
 				new GenericEvent()
 			);
+			/** @var IEventDispatcher $dispatcher */
+			$dispatcher = \OC::$server->get(IEventDispatcher::class);
+			$dispatcher->dispatchTyped(new LoadAdditionalStorageBackendsEvent());
 			$eventSent = true;
 		}
 	}
