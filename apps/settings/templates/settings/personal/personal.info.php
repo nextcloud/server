749a266f782bc97445357503ddba63a663184284
Jan C. Borchardt <hey@jancborchardt.net>
2021-04-13 17:35:00 +0200

    Add proper labels for personal settings privacy toggles
    Signed-off-by: Jan C. Borchardt <hey@jancborchardt.net>

--- apps/settings/templates/settings/personal/personal.info.php
+++ apps/settings/templates/settings/personal/personal.info.php
@@ -40,7 +40,7 @@ script('settings', [
 			<form id="avatarform" class="section" method="post" action="<?php p(\OC::$server->getURLGenerator()->linkToRoute('core.avatar.postAvatar')); ?>">
 				<h3>
 					<label><?php p($l->t('Profile picture')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of profile picture')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -101,7 +101,7 @@ script('settings', [
 			<form id="displaynameform" class="section">
 				<h3>
 					<label for="displayname"><?php p($l->t('Full name')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of full name')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -131,7 +131,7 @@ script('settings', [
 			<form id="emailform" class="section">
 				<h3>
 					<label for="email"><?php p($l->t('Email')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of email')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -181,7 +181,7 @@ script('settings', [
 			<form id="phoneform" class="section">
 				<h3>
 					<label for="phone"><?php p($l->t('Phone number')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of phone number')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -200,7 +200,7 @@ script('settings', [
 			<form id="addressform" class="section">
 				<h3>
 					<label for="address"><?php p($l->t('Address')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of address')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -219,7 +219,7 @@ script('settings', [
 			<form id="websiteform" class="section">
 				<h3>
 					<label for="website"><?php p($l->t('Website')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of website')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
@@ -268,7 +268,7 @@ script('settings', [
 			<form id="twitterform" class="section">
 				<h3>
 					<label for="twitter"><?php p($l->t('Twitter')); ?></label>
-					<div class="federation-menu" tabindex="0">
+					<div class="federation-menu" tabindex="0" aria-label="<?php p($l->t('Change privacy level of Twitter profile')); ?>">
 						<span class="icon-federation-menu icon-password">
 							<span class="icon-triangle-s"></span>
 						</span>
