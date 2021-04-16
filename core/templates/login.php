69290781ff0a59e72c1cf7c9c3b1c6a5ffc2596a
Joas Schilling <coding@schilljs.com>
2021-04-16 11:03:12 +0200

    Handle device login like an alternative login
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/templates/login.php
+++ core/templates/login.php
@@ -6,7 +6,7 @@ script('core', 'dist/login');
 <div id="login"></div>
 
 <?php if (!empty($_['alt_login'])) { ?>
-    <div id="alternative-logins">
+    <div id="alternative-logins" class="alternative-logins">
         <?php foreach ($_['alt_login'] as $login): ?>
             <a class="button <?php p($login['style'] ?? ''); ?>" href="<?php print_unescaped($login['href']); ?>" >
                 <?php p($login['name']); ?>
