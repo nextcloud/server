9dfd89a73b81844b19430da021789504472109d0
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-04-22 15:32:33 +0200

    l10n: Spelling unification
    Spelling unification in Transifex.
    
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- lib/private/Setup/PostgreSQL.php
+++ lib/private/Setup/PostgreSQL.php
@@ -98,7 +98,7 @@ class PostgreSQL extends AbstractDatabase {
 			$connection->connect();
 		} catch (\Exception $e) {
 			$this->logger->logException($e);
-			throw new \OC\DatabaseSetupException($this->trans->t('PostgreSQL username and/or password not valid'),
+			throw new \OC\DatabaseSetupException($this->trans->t('PostgreSQL username and / or password not valid'),
 				$this->trans->t('You need to enter details of an existing account.'), 0, $e);
 		}
 	}
