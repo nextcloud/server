fd1d9798e84f21144902075cbc694ca5f54a51a2
Julius Härtl <jus@bitgrid.net>
2021-04-29 12:09:06 +0200

    Implement local filtering in file list
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files/src/templates.js
+++ apps/files/src/templates.js
@@ -32,6 +32,8 @@ import TemplatePickerView from './views/TemplatePicker'
 import { getCurrentUser } from '@nextcloud/auth'
 import { showError } from '@nextcloud/dialogs'
 
+import './legacy/filelistSearch'
+
 // Set up logger
 const logger = getLoggerBuilder()
 	.setApp('files')
