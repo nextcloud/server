c059deeafe1364786ff428cd3bb689de159d6bf8
Morris Jobke <hey@morrisjobke.de>
2021-05-21 09:20:35 +0200

    19.0.12
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [19, 0, 11, 1];
+$OC_Version = [19, 0, 12, 0];
 
 // The human readable string
-$OC_VersionString = '19.0.11';
+$OC_VersionString = '19.0.12';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
