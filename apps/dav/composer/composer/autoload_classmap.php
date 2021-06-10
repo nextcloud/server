f432dd2e2ea9fe4e7c19d5ea9d60481e662a0367
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-06-10 20:15:13 +0200

    Add scheduling availability settings
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/dav/composer/composer/autoload_classmap.php
+++ apps/dav/composer/composer/autoload_classmap.php
@@ -277,6 +277,7 @@ return array(
     'OCA\\DAV\\Search\\EventsSearchProvider' => $baseDir . '/../lib/Search/EventsSearchProvider.php',
     'OCA\\DAV\\Search\\TasksSearchProvider' => $baseDir . '/../lib/Search/TasksSearchProvider.php',
     'OCA\\DAV\\Server' => $baseDir . '/../lib/Server.php',
+    'OCA\\DAV\\Settings\\AvailabilitySettings' => $baseDir . '/../lib/Settings/AvailabilitySettings.php',
     'OCA\\DAV\\Settings\\CalDAVSettings' => $baseDir . '/../lib/Settings/CalDAVSettings.php',
     'OCA\\DAV\\Storage\\PublicOwnerWrapper' => $baseDir . '/../lib/Storage/PublicOwnerWrapper.php',
     'OCA\\DAV\\SystemTag\\SystemTagMappingNode' => $baseDir . '/../lib/SystemTag/SystemTagMappingNode.php',
