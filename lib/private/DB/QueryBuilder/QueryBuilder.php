3b510bf0f64d51a6ffd990ae55e41e5fce88e74e
Daniel Kesselberg <mail@danielkesselberg.de>
2021-06-16 11:38:41 +0200

    Activate constraint check for oracle / pqsql also for 20.
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/private/DB/QueryBuilder/QueryBuilder.php
+++ lib/private/DB/QueryBuilder/QueryBuilder.php
@@ -31,6 +31,7 @@ namespace OC\DB\QueryBuilder;
 use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
+use Doctrine\DBAL\Query\QueryException;
 use OC\DB\OracleConnection;
 use OC\DB\QueryBuilder\ExpressionBuilder\ExpressionBuilder;
 use OC\DB\QueryBuilder\ExpressionBuilder\MySqlExpressionBuilder;
@@ -214,7 +215,6 @@ class QueryBuilder implements IQueryBuilder {
 			}
 		}
 
-		return $this->queryBuilder->execute();
 		$numberOfParameters = 0;
 		$hasTooLargeArrayParameter = false;
 		foreach ($this->getParameters() as $parameter) {
@@ -244,6 +244,8 @@ class QueryBuilder implements IQueryBuilder {
 				'app' => 'core',
 			]);
 		}
+
+		return $this->queryBuilder->execute();
 	}
 
 	/**
