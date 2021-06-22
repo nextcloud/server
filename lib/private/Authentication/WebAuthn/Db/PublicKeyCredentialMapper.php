ba12eb7aba2ce8ac6f0919ac7ca840a6f628292c
Lukas Reschke <lukas@statuscode.ch>
2021-06-22 13:54:03 +0200

    Use execute instead of executeStatement
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- lib/private/Authentication/WebAuthn/Db/PublicKeyCredentialMapper.php
+++ lib/private/Authentication/WebAuthn/Db/PublicKeyCredentialMapper.php
@@ -96,6 +96,6 @@ class PublicKeyCredentialMapper extends QBMapper {
 			->where(
 				$qb->expr()->eq('uid', $qb->createNamedParameter($uid))
 			);
-		$qb->executeStatement();
+		$qb->execute();
 	}
 }
