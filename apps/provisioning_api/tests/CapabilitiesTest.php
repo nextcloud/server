09a5413d26564bfef603c26b1f2d36509774054a
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-06-24 01:47:07 +0200

    Split capability
    The "federated" and "published" scopes are independent one from each
    other, so the capability that encompassed both needs to be split.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/provisioning_api/tests/CapabilitiesTest.php
+++ apps/provisioning_api/tests/CapabilitiesTest.php
@@ -57,20 +57,25 @@ class CapabilitiesTest extends TestCase {
 
 	public function getCapabilitiesProvider() {
 		return [
-			[false, false, false],
-			[true, false, false],
-			[true, true, true],
+			[true, false, false, true, false],
+			[true, true, false, true, false],
+			[true, true, true, true, true],
+			[false, false, false, false, false],
+			[false, true, false, false, false],
+			[false, true, true, false, true],
 		];
 	}
 
 	/**
 	 * @dataProvider getCapabilitiesProvider
 	 */
-	public function testGetCapabilities($federationAppEnabled, $lookupServerEnabled, $expectedFederationScopesEnabled) {
-		$this->appManager->expects($this->once())
-		   ->method('isEnabledForUser')
-		   ->with('federatedfilesharing')
-		   ->willReturn($federationAppEnabled);
+	public function testGetCapabilities($federationAppEnabled, $federatedFileSharingAppEnabled, $lookupServerEnabled, $expectedFederatedScopeEnabled, $expectedPublishedScopeEnabled) {
+		$this->appManager->expects($this->any())
+			->method('isEnabledForUser')
+			->will($this->returnValueMap([
+				['federation', null, $federationAppEnabled],
+				['federatedfilesharing', null, $federatedFileSharingAppEnabled],
+			]));
 
 		$federatedShareProvider = $this->createMock(FederatedShareProvider::class);
 		$this->overwriteService(FederatedShareProvider::class, $federatedShareProvider);
@@ -83,7 +88,8 @@ class CapabilitiesTest extends TestCase {
 			'provisioning_api' => [
 				'version' => '1.12',
 				'AccountPropertyScopesVersion' => 2,
-				'AccountPropertyScopesFederationEnabled' => $expectedFederationScopesEnabled,
+				'AccountPropertyScopesFederatedEnabled' => $expectedFederatedScopeEnabled,
+				'AccountPropertyScopesPublishedEnabled' => $expectedPublishedScopeEnabled,
 			],
 		];
 		$this->assertSame($expected, $this->capabilities->getCapabilities());
