0a184a3201edbc01eca6759bb95871fb17bb5069
Vincent Petry <vincent@nextcloud.com>
2021-08-05 10:42:38 +0200

    Make "name" column nullable in workflow operations
    The "name" column is now unused and the code is always inserting an
    empty string. While this works with most databases, Oracle complains
    because an empty string is equivalent to null.
    
    To fix this, the column definition is changed to allow null values now.
    
    Also added some logging in case of database exceptions, because without
    this nothing would be logged to detect the above problem.
    
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- apps/workflowengine/composer/composer/autoload_static.php
+++ apps/workflowengine/composer/composer/autoload_static.php
@@ -47,6 +47,7 @@ class ComposerStaticInitWorkflowEngine
         'OCA\\WorkflowEngine\\Manager' => __DIR__ . '/..' . '/../lib/Manager.php',
         'OCA\\WorkflowEngine\\Migration\\PopulateNewlyIntroducedDatabaseFields' => __DIR__ . '/..' . '/../lib/Migration/PopulateNewlyIntroducedDatabaseFields.php',
         'OCA\\WorkflowEngine\\Migration\\Version2000Date20190808074233' => __DIR__ . '/..' . '/../lib/Migration/Version2000Date20190808074233.php',
+        'OCA\\WorkflowEngine\\Migration\\Version2200Date20210805101925' => __DIR__ . '/..' . '/../lib/Migration/Version2200Date20210805101925.php',
         'OCA\\WorkflowEngine\\Service\\Logger' => __DIR__ . '/..' . '/../lib/Service/Logger.php',
         'OCA\\WorkflowEngine\\Service\\RuleMatcher' => __DIR__ . '/..' . '/../lib/Service/RuleMatcher.php',
         'OCA\\WorkflowEngine\\Settings\\ASettings' => __DIR__ . '/..' . '/../lib/Settings/ASettings.php',
