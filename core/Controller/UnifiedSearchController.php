7c76e85ddefc481fd9582560c59c4883dbd7caad
Jonas Meurer <jonas@freesources.org>
2021-08-10 11:06:24 +0200

    Use IURLGenerator function to get value of `\OC::$WEBROOT` global
    Signed-off-by: Jonas Meurer <jonas@freesources.org>

--- core/Controller/UnifiedSearchController.php
+++ core/Controller/UnifiedSearchController.php
@@ -33,6 +33,7 @@ use OCP\AppFramework\OCSController;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\IRequest;
+use OCP\IURLGenerator;
 use OCP\IUserSession;
 use OCP\Route\IRouter;
 use OCP\Search\ISearchQuery;
@@ -49,15 +50,20 @@ class UnifiedSearchController extends OCSController {
 	/** @var IRouter */
 	private $router;
 
+	/** @var IURLGenerator */
+	private $urlGenerator;
+
 	public function __construct(IRequest $request,
 								IUserSession $userSession,
 								SearchComposer $composer,
-								IRouter $router) {
+								IRouter $router,
+								IURLGenerator $urlGenerator) {
 		parent::__construct('core', $request);
 
 		$this->composer = $composer;
 		$this->userSession = $userSession;
 		$this->router = $router;
+		$this->urlGenerator = $urlGenerator;
 	}
 
 	/**
@@ -127,7 +133,7 @@ class UnifiedSearchController extends OCSController {
 
 			// Optionally strip webroot from URL. Required for route matching on setups
 			// with Nextcloud in a webserver subfolder (webroot).
-			$webroot = \OC::$WEBROOT;
+			$webroot = $this->urlGenerator->getWebroot();
 			if ($webroot !== '' && substr($urlPath, 0, strlen($webroot)) === $webroot) {
 				$urlPath = substr($urlPath, strlen($webroot));
 			}
