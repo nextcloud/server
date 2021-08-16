4566423e85dc902c1b2ad72780b6ebcbf8ae5ca6
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-08-16 08:25:35 +0200

    Add missing files for Composer v2
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ apps/federatedfilesharing/composer/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'reference' => 'fb5ee6087bfd1f4cc2f37cda7a7cab7072aaae86',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'reference' => 'fb5ee6087bfd1f4cc2f37cda7a7cab7072aaae86',
+            'dev_requirement' => false,
+        ),
+    ),
+);
