5444db1ebf07aef85618a78afdf38983adbd5e7b
Robin Appelman <robin@icewind.nl>
2021-08-17 13:50:13 +0200

    use case insensitive like when limiting search to jail
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Files/Cache/SearchBuilder.php
+++ lib/private/Files/Cache/SearchBuilder.php
@@ -39,6 +39,7 @@ use OCP\Files\Search\ISearchOrder;
 class SearchBuilder {
 	protected static $searchOperatorMap = [
 		ISearchComparison::COMPARE_LIKE => 'iLike',
+		ISearchComparison::COMPARE_LIKE_CASE_SENSITIVE => 'like',
 		ISearchComparison::COMPARE_EQUAL => 'eq',
 		ISearchComparison::COMPARE_GREATER_THAN => 'gt',
 		ISearchComparison::COMPARE_GREATER_THAN_EQUAL => 'gte',
@@ -48,6 +49,7 @@ class SearchBuilder {
 
 	protected static $searchOperatorNegativeMap = [
 		ISearchComparison::COMPARE_LIKE => 'notLike',
+		ISearchComparison::COMPARE_LIKE_CASE_SENSITIVE => 'notLike',
 		ISearchComparison::COMPARE_EQUAL => 'neq',
 		ISearchComparison::COMPARE_GREATER_THAN => 'lte',
 		ISearchComparison::COMPARE_GREATER_THAN_EQUAL => 'lt',
@@ -186,8 +188,8 @@ class SearchBuilder {
 		$comparisons = [
 			'mimetype' => ['eq', 'like'],
 			'mtime' => ['eq', 'gt', 'lt', 'gte', 'lte'],
-			'name' => ['eq', 'like'],
-			'path' => ['eq', 'like'],
+			'name' => ['eq', 'like', 'clike'],
+			'path' => ['eq', 'like', 'clike'],
 			'size' => ['eq', 'gt', 'lt', 'gte', 'lte'],
 			'tagname' => ['eq', 'like'],
 			'favorite' => ['eq'],
