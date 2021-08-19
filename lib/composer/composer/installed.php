28e8121c20d29491e237f5678f75291d0d429c28
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-08-19 10:48:44 +0200

    Scope composer tools with the bin plugin
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/composer/composer/installed.php
+++ lib/composer/composer/installed.php
@@ -1,24 +1,23 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'reference' => '3dc7fd27f177993e4032c71789fac55e0af01880',
+        'name' => '__root__',
+        'dev' => false,
     ),
-    'reference' => '619b35b480a2d348436156a2a6144895b00b1e07',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '619b35b480a2d348436156a2a6144895b00b1e07',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'reference' => '3dc7fd27f177993e4032c71789fac55e0af01880',
+            'dev_requirement' => false,
+        ),
     ),
-  ),
 );
