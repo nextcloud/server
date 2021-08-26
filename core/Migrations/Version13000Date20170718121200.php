88cc1ba064bb279e79386af469cc33705a65a688
Robin Appelman <robin@icewind.nl>
2021-08-26 14:25:17 +0200

    disable path prefix index on postgresql for now
    having the index work properly for the queries we need it for requires some additional options which dbal does not support at the momement.
    to prevent making it harder to add the correct index later on we don't create the index for now on postgresql
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- core/Migrations/Version13000Date20170718121200.php
+++ core/Migrations/Version13000Date20170718121200.php
@@ -31,6 +31,7 @@
  */
 namespace OC\Core\Migrations;
 
+use Doctrine\DBAL\Platforms\PostgreSQL94Platform;
 use OCP\DB\Types;
 use OCP\DB\ISchemaWrapper;
 use OCP\IDBConnection;
@@ -263,7 +264,9 @@ class Version13000Date20170718121200 extends SimpleMigrationStep {
 			$table->addIndex(['storage', 'size', 'fileid'], 'fs_storage_size');
 			$table->addIndex(['mtime'], 'fs_mtime');
 			$table->addIndex(['size'], 'fs_size');
-			$table->addIndex(['storage', 'path'], 'fs_storage_path_prefix', [], ['lengths' => [null, 64]]);
+			if (!$schema->getDatabasePlatform() instanceof PostgreSQL94Platform) {
+				$table->addIndex(['storage', 'path'], 'fs_storage_path_prefix', [], ['lengths' => [null, 64]]);
+			}
 		}
 
 		if (!$schema->hasTable('group_user')) {
