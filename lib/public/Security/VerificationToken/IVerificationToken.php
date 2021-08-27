414676468679ba93befde00322fcf2b8af9f3d2d
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-08-28 00:07:23 +0200

    Confirm mails only per POST
    - this is to avoid automatic confirmation by certain softwares that open
      links
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/public/Security/VerificationToken/IVerificationToken.php
+++ lib/public/Security/VerificationToken/IVerificationToken.php
@@ -52,4 +52,11 @@ interface IVerificationToken {
 	 * @since 22.2.0
 	 */
 	public function create(IUser $user, string $subject, string $passwordPrefix = ''): string;
+
+	/**
+	 * Deletes the token identified by the provided parameters
+	 *
+	 * @since 22.2.0
+	 */
+	public function delete(string $token, IUser $user, string $subject): void;
 }
