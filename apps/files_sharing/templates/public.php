ce8a8c0c83eaf9ab6a57b73bfcd76522c82ca1e3
Sangramsinh Desai <sangramsinh.desai@t-systems.com>
2021-08-30 10:29:21 +0530

    Guest view download button seperated for video
    Signed-off-by: Sangramsinh Desai <sangramsinh.desai@t-systems.com>
    Signed-off-by: sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>

--- apps/files_sharing/templates/public.php
+++ apps/files_sharing/templates/public.php
@@ -75,9 +75,12 @@ $maxUploadFilesize = min($upload_max_filesize, $post_max_size);
 				<?php endif; ?>
 				<?php if ($_['previewURL'] === $_['downloadURL'] && !$_['hideDownload']): ?>
 					<div class="directDownload">
+						<div>
+							<?php p($l->t('%s', [$_['filename']]))?> (<?php p($_['fileSize']) ?>)
+						</div>
 						<a href="<?php p($_['downloadURL']); ?>" id="downloadFile" class="button">
 							<span class="icon icon-download"></span>
-							<?php p($l->t('Download %s', [$_['filename']]))?> (<?php p($_['fileSize']) ?>)
+							<?php p($l->t('Download'))?>
 						</a>
 					</div>
 				<?php endif; ?>
