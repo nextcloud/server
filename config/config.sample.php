ce283c12b6ba8d27dc0d2934ffe7d6f7b6743a02
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-08-31 21:01:27 +0200

    make it possible to  override the default collation
    - allows admins to configure it for edge cases like accent sensitivity
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- config/config.sample.php
+++ config/config.sample.php
@@ -1554,6 +1554,24 @@ $CONFIG = [
 'mysql.utf8mb4' => false,
 
 /**
+ * For search queries in the database, a default collation – depending on the
+ * character set – is chosen. In some cases a different behaviour is desired,
+ * for instances when a accent sensitive search is desired.
+ *
+ * MariaDB and MySQL have an overlap in available collations, but also
+ * incompatible ones, also depending on the version of the database server.
+ *
+ * This option allows to override the automatic choice. Example:
+ *
+ * 'mysql.collation' => 'utf8mb4_0900_as_ci',
+ *
+ * This setting has no effect on setup or creating tables. In those cases
+ * always utf8[mb4]_bin is being used. This setting is only taken into
+ * consideration in SQL queries that utilize LIKE comparison operators.
+ */
+'mysql.collation' => null,
+
+/**
  * Database types that are supported for installation.
  *
  * Available:
