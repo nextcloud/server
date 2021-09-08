1237d64bdba24efdc219a4198c5bf82289d4d668
Bjoern Schiessle <bjoern@schiessle.org>
2021-09-08 23:43:17 +0200

    add 'supported'-label to all supported apps, also if they are not downloaded yet
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

--- lib/private/legacy/OC_App.php
+++ lib/private/legacy/OC_App.php
@@ -777,6 +777,18 @@ class OC_App {
 	}
 
 	/**
+	 * List all supported apps
+	 *
+	 * @return array
+	 */
+	public function getSupportedApps(): array {
+		/** @var \OCP\Support\Subscription\IRegistry $subscriptionRegistry */
+		$subscriptionRegistry = \OC::$server->query(\OCP\Support\Subscription\IRegistry::class);
+		$supportedApps = $subscriptionRegistry->delegateGetSupportedApps();
+		return $supportedApps;
+	}
+
+	/**
 	 * List all apps, this is used in apps.php
 	 *
 	 * @return array
@@ -790,9 +802,7 @@ class OC_App {
 		$appList = [];
 		$langCode = \OC::$server->getL10N('core')->getLanguageCode();
 		$urlGenerator = \OC::$server->getURLGenerator();
-		/** @var \OCP\Support\Subscription\IRegistry $subscriptionRegistry */
-		$subscriptionRegistry = \OC::$server->query(\OCP\Support\Subscription\IRegistry::class);
-		$supportedApps = $subscriptionRegistry->delegateGetSupportedApps();
+		$supportedApps = $this->getSupportedApps();
 
 		foreach ($installedApps as $app) {
 			if (array_search($app, $blacklist) === false) {
