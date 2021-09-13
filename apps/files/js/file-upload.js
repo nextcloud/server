ff44f952ae53ad96ff80613f3748610afef1723a
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2021-09-13 10:33:42 +0530

    Conflict Pre Dialog added
    Signed-off-by: Sangramsinh Desai <sangramsinh.desai@t-systems.com>

--- apps/files/js/file-upload.js
+++ apps/files/js/file-upload.js
@@ -44,6 +44,7 @@ OC.FileUpload = function(uploader, data) {
 OC.FileUpload.CONFLICT_MODE_DETECT = 0;
 OC.FileUpload.CONFLICT_MODE_OVERWRITE = 1;
 OC.FileUpload.CONFLICT_MODE_AUTORENAME = 2;
+OC.conflictsData = null;
 
 // IE11 polyfill
 // TODO: nuke out of orbit as well as this legacy code
@@ -790,14 +791,16 @@ OC.Uploader.prototype = _.extend({
 			}
 			return true;
 		});
+		OC.conflictsData = conflicts;
 		if (conflicts.length) {
 			// wait for template loading
-			OC.dialogs.fileexists(null, null, null, this).done(function() {
-				_.each(conflicts, function(conflictData) {
-					OC.dialogs.fileexists(conflictData[1], conflictData[0], conflictData[1].getFile(), this);
+			OC.dialogs.fileexistsConflictPreDlg(null, null, null, this).done(function () {
+				_.each(conflicts, function (conflictData) {
+					OC.dialogs.fileexistsConflictPreDlg(conflictData[1], conflictData[0], conflictData[1].getFile(), this);
 				});
 			});
 		}
+
 
 		// upload non-conflicting files
 		// note: when reaching the server they might still meet conflicts
@@ -862,6 +865,50 @@ OC.Uploader.prototype = _.extend({
 		}
 
 		return ($tr.attr('data-mounttype') === 'shared-root' && $tr.attr('data-mime') !== 'httpd/unix-directory');
+	},
+
+	/** Custom Pre File conflict dialog*/
+
+	/**
+	 * Bypass to nexcloud default confluct box
+	 */
+	moreDetails:function() {
+		var conflicts = OC.conflictsData;
+		var self = this;
+		this.cancelUploads();
+		if (conflicts.length) {
+			// wait for template loading
+			OC.dialogs.fileexists(null, null, null, this).done(function () {
+				_.each(conflicts, function (conflictData) {
+					OC.dialogs.fileexists(conflictData[1], conflictData[0], conflictData[1].getFile(), this);
+				});
+			});
+		}
+	},
+
+	/**
+	 * callback for the conflicts dialog
+	 * calls onSkip, onReplace or onAutorename for each conflict
+	 * @param keepOriginal - boolean value. If true will keep original file
+	 * @param keepOriginal - boolean value. If true will repleace file
+	 */
+	onContinueConflictPreDlg:function(keepOriginal, keepReplacement) {
+		var self = this;
+		//iterate over all conflicts
+		jQuery.each(OC.conflictsData, function (i, conflict) {
+			var conflictData = conflict[1]
+			if (keepOriginal && keepReplacement) {
+				// when both selected -> autorename
+				self.onAutorename(conflictData);
+			} else if (keepReplacement) {
+				// when only replacement selected -> overwrite
+				self.onReplace(conflictData);
+			} else {
+				// when only original selected -> skip
+				// when none selected -> skip
+				self.onSkip(conflictData);
+			}
+		});
 	},
 
 	/**
--- core/src/jquery/ocdialog.js
+++ core/src/jquery/ocdialog.js
@@ -279,3 +279,263 @@ $.widget('oc.ocdialog', {
 		this.$dialog.remove()
 	},
 })
+
+$.widget('oc.ocdialogconflictpredlg', {
+	options: {
+		width: 'auto',
+		height: 'auto',
+		closeButton: true,
+		closeOnEscape: true,
+		closeCallback: null,
+		modal: false,
+	},
+	_create() {
+		const self = this
+
+		this.originalCss = {
+			display: this.element[0].style.display,
+			width: this.element[0].style.width,
+			height: this.element[0].style.height,
+		}
+
+		this.originalTitle = this.element.attr('title')
+		this.options.title = this.options.title || this.originalTitle
+
+		this.$dialog = $('<div class="oc-dialog oc-conflict-pre-dlg" />')
+			.attr({
+				// Setting tabIndex makes the div focusable
+				tabIndex: -1,
+				role: 'dialog',
+			})
+			.insertBefore(this.element)
+		this.$dialog.append(this.element.detach())
+		this.element.removeAttr('title').addClass('oc-dialog-content').appendTo(this.$dialog)
+
+		this.$dialog.css({
+			display: 'inline-block',
+			position: 'fixed',
+		})
+
+		this.enterCallback = null
+
+		$(document).on('keydown keyup', function(event) {
+			if (
+				event.target !== self.$dialog.get(0)
+				&& self.$dialog.find($(event.target)).length === 0
+			) {
+				return
+			}
+			// Escape
+			if (
+				event.keyCode === 27
+				&& event.type === 'keydown'
+				&& self.options.closeOnEscape
+			) {
+				event.stopImmediatePropagation()
+				self.close()
+				return false
+			}
+			// Enter
+			if (event.keyCode === 13) {
+				event.stopImmediatePropagation()
+				if (self.enterCallback !== null) {
+					self.enterCallback()
+					event.preventDefault()
+					return false
+				}
+				if (event.type === 'keyup') {
+					event.preventDefault()
+					return false
+				}
+				// If no button is selected we trigger the primary
+				if (
+					self.$buttonrow
+					&& self.$buttonrow.find($(event.target)).length === 0
+				) {
+					const $button = self.$buttonrow.find('button.primary')
+					if ($button && !$button.prop('disabled')) {
+						$button.trigger('click')
+					}
+				} else if (self.$buttonrow) {
+					$(event.target).trigger('click')
+				}
+				return false
+			}
+		})
+
+		this._setOptions(this.options)
+		this._createOverlay()
+	},
+	_init() {
+		this.$dialog.focus()
+		this._trigger('open')
+	},
+	_setOption(key, value) {
+		const self = this
+		switch (key) {
+		case 'title':
+			if (this.$title) {
+				this.$title.text(value)
+			} else {
+				const $title = $('<div class="oc-conflict-pre-dlg-title-div"><h2 class="oc-dialog-title">'
+						+ value
+						+ '</h2></div><div class="oc-conflict-pre-dlg-close-div"><a class="close-conflict-pre-dlg">&nbsp;</a></div><br class="clearboth" />')
+				this.$title = $title.prependTo(this.$dialog)
+			}
+			this._setSizes()
+			break
+		case 'buttons':
+			if (this.$buttonrow) {
+				this.$buttonrow.empty()
+			} else {
+				const $buttonrow = $('<div class="" />')
+				this.$buttonrow = $buttonrow.appendTo(this.$dialog)
+			}
+			if (value.length === 1) {
+				this.$buttonrow.addClass('onebutton')
+			} else if (value.length === 2) {
+				this.$buttonrow.addClass('twobuttons')
+			} else if (value.length === 3) {
+				this.$buttonrow.addClass('threebuttons')
+			} else if (value.length === 4) {
+				this.$buttonrow.addClass('fourbuttons')
+			}
+
+			$.each(value, function(idx, val) {
+				const $button = $('<button>').text(val.text)
+				if (val.classes) {
+					$button.addClass(val.classes)
+				}
+
+				if (val.defaultButton) {
+					$button.addClass('primary')
+					self.$defaultButton = $button
+				}
+				self.$buttonrow.append($button)
+				console.log($button);
+				$button.click(function() {
+					val.click.apply(self.element[0], arguments)
+				})
+			})
+			this.$buttonrow.find('button')
+				.on('focus', function(event) {
+					self.$buttonrow.find('button').removeClass('primary')
+					$(this).addClass('primary')
+				})
+			this._setSizes()
+			break
+		case 'style':
+			if (value.buttons !== undefined) {
+				this.$buttonrow.addClass(value.buttons)
+			}
+			break
+		case 'closeButton':
+			if (value) {
+				const $closeButton = $('<a class="oc-dialog-close"></a>')
+				this.$dialog.prepend($closeButton)
+				$closeButton.on('click', function() {
+					self.options.closeCallback && self.options.closeCallback()
+					self.close()
+				})
+			} else {
+				this.$dialog.find('.oc-dialog-close').remove()
+			}
+			break
+		case 'width':
+			this.$dialog.css('width', value)
+			break
+		case 'height':
+			this.$dialog.css('height', value)
+			break
+		case 'close':
+			this.closeCB = value
+			break
+		}
+		// this._super(key, value);
+		$.Widget.prototype._setOption.apply(this, arguments)
+	},
+	_setOptions(options) {
+		// this._super(options);
+		$.Widget.prototype._setOptions.apply(this, arguments)
+	},
+	_setSizes() {
+		let lessHeight = 0
+		if (this.$title) {
+			lessHeight += this.$title.outerHeight(true)
+		}
+		if (this.$buttonrow) {
+			lessHeight += this.$buttonrow.outerHeight(true)
+		}
+		this.element.css({
+			height: 'calc(100% - ' + lessHeight + 'px)',
+		})
+	},
+	_createOverlay() {
+		if (!this.options.modal) {
+			return
+		}
+
+		const self = this
+		let contentDiv = $('#content')
+		if (contentDiv.length === 0) {
+			// nextcloud-vue compatibility
+			contentDiv = $('.content')
+		}
+		this.overlay = $('<div>')
+			.addClass('oc-dialog-dim')
+			.appendTo(contentDiv)
+		this.overlay.on('click keydown keyup', function(event) {
+			if (event.target !== self.$dialog.get(0) && self.$dialog.find($(event.target)).length === 0) {
+				event.preventDefault()
+				event.stopPropagation()
+
+			}
+		})
+	},
+	_destroyOverlay() {
+		if (!this.options.modal) {
+			return
+		}
+
+		if (this.overlay) {
+			this.overlay.off('click keydown keyup')
+			this.overlay.remove()
+			this.overlay = null
+		}
+	},
+	widget() {
+		return this.$dialog
+	},
+	setEnterCallback(callback) {
+		this.enterCallback = callback
+	},
+	unsetEnterCallback() {
+		this.enterCallback = null
+	},
+	close() {
+		this._destroyOverlay()
+		const self = this
+		// Ugly hack to catch remaining keyup events.
+		setTimeout(function() {
+			self._trigger('close', self)
+		}, 200)
+
+		self.$dialog.remove()
+		this.destroy()
+	},
+	destroy() {
+		if (this.$title) {
+			this.$title.remove()
+		}
+		if (this.$buttonrow) {
+			this.$buttonrow.remove()
+		}
+
+		if (this.originalTitle) {
+			this.element.attr('title', this.originalTitle)
+		}
+		this.element.removeClass('oc-dialog-content')
+			.css(this.originalCss).detach().insertBefore(this.$dialog)
+		this.$dialog.remove()
+	},
+})
