6ab30a669b3dde5dce2b63fb33b1ec44f1889b2f
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-09-17 19:15:46 +0200

    ensure that user and group IDs in LDAP's tables are also max 64chars
    - limitation by core tables (e.g. sharing), IDs are always 64chars
    - when longer group IDs were requested they are hashed (does not affect
      displaynames)
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/user_ldap/composer/composer/autoload_classmap.php
+++ apps/user_ldap/composer/composer/autoload_classmap.php
@@ -61,6 +61,7 @@ return array(
     'OCA\\User_LDAP\\Migration\\UUIDFixUser' => $baseDir . '/../lib/Migration/UUIDFixUser.php',
     'OCA\\User_LDAP\\Migration\\UnsetDefaultProvider' => $baseDir . '/../lib/Migration/UnsetDefaultProvider.php',
     'OCA\\User_LDAP\\Migration\\Version1010Date20200630192842' => $baseDir . '/../lib/Migration/Version1010Date20200630192842.php',
+    'OCA\\User_LDAP\\Migration\\Version1120Date20210917155206' => $baseDir . '/../lib/Migration/Version1120Date20210917155206.php',
     'OCA\\User_LDAP\\Notification\\Notifier' => $baseDir . '/../lib/Notification/Notifier.php',
     'OCA\\User_LDAP\\PagedResults\\IAdapter' => $baseDir . '/../lib/PagedResults/IAdapter.php',
     'OCA\\User_LDAP\\PagedResults\\Php73' => $baseDir . '/../lib/PagedResults/Php73.php',
