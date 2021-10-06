8ee3b84e0a65667e9e2d1c1f16ff5a11f2efe1f7
acsfer <carlos@reendex.com>
2021-10-06 16:28:15 +0200

    Reintroduce libxml_disable_entity_loader() calls
    TL;DR https://blog.sonarsource.com/wordpress-xxe-security-vulnerability

--- lib/private/App/InfoParser.php
+++ lib/private/App/InfoParser.php
@@ -61,13 +61,9 @@ class InfoParser {
 		}
 
 		libxml_use_internal_errors(true);
-		if ((PHP_VERSION_ID < 80000)) {
-			$loadEntities = libxml_disable_entity_loader(false);
-			$xml = simplexml_load_file($file);
-			libxml_disable_entity_loader($loadEntities);
-		} else {
-			$xml = simplexml_load_file($file);
-		}
+		$loadEntities = @libxml_disable_entity_loader(false);
+		$xml = simplexml_load_file($file);
+		@libxml_disable_entity_loader($loadEntities);
 
 		if ($xml === false) {
 			libxml_clear_errors();
