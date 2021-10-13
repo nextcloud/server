01b8291c76f103bf9c31b19e08b28dd482d28829
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-10-13 10:54:44 +0200

    Type the autentication provider passwords as nullable strings
    For historic reasons we couldn't add a nullable type hint before
    nullable type hints were supported by our target php versions. This is
    now possible.
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/private/Authentication/Token/IProvider.php
+++ lib/private/Authentication/Token/IProvider.php
@@ -53,7 +53,7 @@ interface IProvider {
 	public function generateToken(string $token,
 								  string $uid,
 								  string $loginName,
-								  $password,
+								  ?string $password,
 								  string $name,
 								  int $type = IToken::TEMPORARY_TOKEN,
 								  int $remember = IToken::DO_NOT_REMEMBER): IToken;
