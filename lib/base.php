b392321e732ad5f9474d691c235bd9bdc68cedd9
Joas Schilling <coding@schilljs.com>
2021-10-19 13:03:23 +0200

    Fix basic auth for OAuth token endpoint
    Don't try to login when a client is trying to get a OAuth token.
    OAuth needs to support basic auth too, so the login is not valid
    inside Nextcloud and the Login exception would ruin it.
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/base.php
+++ lib/base.php
@@ -987,7 +987,13 @@ class OC {
 			} else {
 				// For guests: Load only filesystem and logging
 				OC_App::loadApps(['filesystem', 'logging']);
-				self::handleLogin($request);
+
+				// Don't try to login when a client is trying to get a OAuth token.
+				// OAuth needs to support basic auth too, so the login is not valid
+				// inside Nextcloud and the Login exception would ruin it.
+				if ($request->getRawPathInfo() !== '/apps/oauth2/api/v1/token') {
+					self::handleLogin($request);
+				}
 			}
 		}
 
