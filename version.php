614cc82d489bd548a2aa48953835e29c411ec191
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-11-26 15:37:06 +0100

    23.0.0
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [23, 0, 0, 9];
+$OC_Version = [23, 0, 0, 10];
 
 // The human readable string
-$OC_VersionString = '23.0.0 RC3';
+$OC_VersionString = '23.0.0';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
