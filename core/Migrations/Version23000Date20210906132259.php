17f32cab0c68273ad2670212e749b348cd1b3037
Joas Schilling <coding@schilljs.com>
2021-12-13 09:28:13 +0100

    Add a primary key to ratelimit_entries table
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/Migrations/Version23000Date20210906132259.php
+++ core/Migrations/Version23000Date20210906132259.php
@@ -11,8 +11,6 @@ use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
 class Version23000Date20210906132259 extends SimpleMigrationStep {
-	private const TABLE_NAME = 'ratelimit_entries';
-
 	/**
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
@@ -20,24 +18,29 @@ class Version23000Date20210906132259 extends SimpleMigrationStep {
 	 * @return null|ISchemaWrapper
 	 */
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
-		/** @var ISchemaWrapper $schema */
-		$schema = $schemaClosure();
-
-		$hasTable = $schema->hasTable(self::TABLE_NAME);
-
-		if (!$hasTable) {
-			$table = $schema->createTable(self::TABLE_NAME);
-			$table->addColumn('hash', Types::STRING, [
-				'notnull' => true,
-				'length' => 128,
-			]);
-			$table->addColumn('delete_after', Types::DATETIME, [
-				'notnull' => true,
-			]);
-			$table->addIndex(['hash'], 'ratelimit_hash');
-			$table->addIndex(['delete_after'], 'ratelimit_delete_after');
-			return $schema;
-		}
+		/**
+		 * Table was missing a primary key
+		 * Therefore it was dropped with Version24000Date20211213081506
+		 * and then recreated with a primary key in Version24000Date20211213081604
+		 */
+//		/** @var ISchemaWrapper $schema */
+//		$schema = $schemaClosure();
+//
+//		$hasTable = $schema->hasTable(self::TABLE_NAME);
+//
+//		if (!$hasTable) {
+//			$table = $schema->createTable(self::TABLE_NAME);
+//			$table->addColumn('hash', Types::STRING, [
+//				'notnull' => true,
+//				'length' => 128,
+//			]);
+//			$table->addColumn('delete_after', Types::DATETIME, [
+//				'notnull' => true,
+//			]);
+//			$table->addIndex(['hash'], 'ratelimit_hash');
+//			$table->addIndex(['delete_after'], 'ratelimit_delete_after');
+//			return $schema;
+//		}
 
 		return null;
 	}
