a47d23c1e4b1493b9867878e27a2a3c40562be77
rakekniven <2069590+rakekniven@users.noreply.github.com>
2022-01-09 22:01:52 +0100

    Removed obselete language files
    Those files are remnants of old languages after renaming, mapping or deleting.
    
    Fix for #30501
    
    Related to https://github.com/nextcloud/docker-ci/issues/341

--- apps/comments/l10n/af_ZA.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "comments",
-    {
-    "Cancel" : "Kanselleer"
-},
-"nplurals=2; plural=(n != 1);");
--- apps/comments/l10n/de_AT.js
+++
@@ -1,7 +0,0 @@
-OC.L10N.register(
-    "comments",
-    {
-    "Cancel" : "Abbrechen",
-    "Save" : "Speichern"
-},
-"nplurals=2; plural=(n != 1);");
