fffc19f5c3e2e1789564542f3562a7b56b587c8c
Carl Schwan <carl@carlschwan.eu>
2022-01-12 14:15:08 +0100

    Fix types warnings from psalm
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- apps/sharebymail/lib/ShareByMailProvider.php
+++ apps/sharebymail/lib/ShareByMailProvider.php
@@ -334,13 +334,13 @@ class ShareByMailProvider implements IShareProvider {
 			$share->getNote()
 		);
 
-		if ($this->mailer->validateMailAddress($share->getSharedWith())) {
+		if (!$this->mailer->validateMailAddress($share->getSharedWith())) {
 			$this->removeShareFromTable($shareId);
 			$e = new HintException('Failed to send share by mail. Got an invalid email address: ' . $share->getSharedWith(),
 				$this->l->t('Failed to send share by email. Got an invalid email address'));
-			$this->logger->error($e->getMessage(), [
-				'message' => 'Failed to send share by mail. Got an invalid email address ' . $share->getSharedWith(),
+			$this->logger->error('Failed to send share by mail. Got an invalid email address ' . $share->getSharedWith(), [
 				'app' => 'sharebymail',
+				'exception' => $e,
 			]);
 		}
 
@@ -689,7 +689,7 @@ class ShareByMailProvider implements IShareProvider {
 	 * @param \DateTime|null $expirationTime
 	 * @return int
 	 */
-	protected function addShareToDB($itemSource, $itemType, $shareWith, $sharedBy, $uidOwner, $permissions, $token, $password, $sendPasswordByTalk, $hideDownload, $label, $expirationTime, $note = '') {
+	protected function addShareToDB($itemSource, $itemType, $shareWith, $sharedBy, $uidOwner, $permissions, $token, $password, $sendPasswordByTalk, $hideDownload, $label, $expirationTime, $note = ''): int {
 		$qb = $this->dbConnection->getQueryBuilder();
 		$qb->insert('share')
 			->setValue('share_type', $qb->createNamedParameter(IShare::TYPE_EMAIL))
@@ -784,7 +784,7 @@ class ShareByMailProvider implements IShareProvider {
 		} catch (\Exception $e) {
 		}
 
-		$this->removeShareFromTable($share->getId());
+		$this->removeShareFromTable((int)$share->getId());
 	}
 
 	/**
@@ -980,9 +980,9 @@ class ShareByMailProvider implements IShareProvider {
 	/**
 	 * remove share from table
 	 *
-	 * @param string $shareId
+	 * @param int $shareId
 	 */
-	protected function removeShareFromTable($shareId) {
+	protected function removeShareFromTable(int $shareId): void {
 		$qb = $this->dbConnection->getQueryBuilder();
 		$qb->delete('share')
 			->where($qb->expr()->eq('id', $qb->createNamedParameter($shareId)));
--- apps/sharebymail/tests/ShareByMailProviderTest.php
+++ apps/sharebymail/tests/ShareByMailProviderTest.php
@@ -459,6 +459,7 @@ class ShareByMailProviderTest extends TestCase {
 	public function testCreateMailShare() {
 		$this->share->expects($this->any())->method('getToken')->willReturn('token');
 		$this->share->expects($this->once())->method('setToken')->with('token');
+		$this->share->expects($this->any())->method('getSharedWith')->willReturn('valid@valid.com');
 		$node = $this->getMockBuilder('OCP\Files\Node')->getMock();
 		$node->expects($this->any())->method('getName')->willReturn('fileName');
 		$this->share->expects($this->any())->method('getNode')->willReturn($node);
@@ -483,6 +484,7 @@ class ShareByMailProviderTest extends TestCase {
 
 		$this->share->expects($this->any())->method('getToken')->willReturn('token');
 		$this->share->expects($this->once())->method('setToken')->with('token');
+		$this->share->expects($this->any())->method('getSharedWith')->willReturn('valid@valid.com');
 		$node = $this->getMockBuilder('OCP\Files\Node')->getMock();
 		$node->expects($this->any())->method('getName')->willReturn('fileName');
 		$this->share->expects($this->any())->method('getNode')->willReturn($node);
@@ -987,6 +989,7 @@ class ShareByMailProviderTest extends TestCase {
 			->willReturn(new \OC\Share20\Share($rootFolder, $userManager));
 
 		$provider = $this->getInstance(['sendMailNotification', 'createShareActivity']);
+		$this->mailer->expects($this->any())->method('validateMailAddress')->willReturn(true);
 
 		$u1 = $userManager->createUser('testFed', md5(time()));
 		$u2 = $userManager->createUser('testFed2', md5(time()));
@@ -1033,6 +1036,7 @@ class ShareByMailProviderTest extends TestCase {
 			->willReturn(new \OC\Share20\Share($rootFolder, $userManager));
 
 		$provider = $this->getInstance(['sendMailNotification', 'createShareActivity']);
+		$this->mailer->expects($this->any())->method('validateMailAddress')->willReturn(true);
 
 		$u1 = $userManager->createUser('testFed', md5(time()));
 		$u2 = $userManager->createUser('testFed2', md5(time()));
