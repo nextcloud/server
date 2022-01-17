f5a7d761fb5b250ec990e22e0ba812179c4551e3
John Molakvoæ <skjnldsv@users.noreply.github.com>
2022-01-17 09:31:27 +0100

    Fix failed rebase

--- lib/private/AppFramework/Http/Request.php
+++ lib/private/AppFramework/Http/Request.php
@@ -269,7 +269,7 @@ class Request implements \ArrayAccess, \Countable, IRequest {
 			case 'parameters':
 			case 'params':
 				return $this->getContent(true);
-			default;
+			default:
 				return isset($this[$name])
 					? $this[$name]
 					: null;
