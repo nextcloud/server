ae8085fbf8786c7db26696f3408ff9756d8846ff
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-01-19 14:46:40 +0100

    Change l10n directory
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- apps/settings/src/components/AuthTokenSection.vue
+++ apps/settings/src/components/AuthTokenSection.vue
@@ -99,7 +99,7 @@ export default {
 				.then(tap(data => this.tokens.push(data.deviceToken)))
 				.catch(err => {
 					console.error.bind('could not create app password', err)
-					OC.Notification.showTemporary(t('core', 'Error while creating device token'))
+					OC.Notification.showTemporary(t('settings', 'Error while creating device token'))
 					throw err
 				})
 		},
@@ -113,7 +113,7 @@ export default {
 				.then(tap(() => console.debug('app token scope updated')))
 				.catch(err => {
 					console.error.bind('could not update app token scope', err)
-					OC.Notification.showTemporary(t('core', 'Error while updating device token scope'))
+					OC.Notification.showTemporary(t('settings', 'Error while updating device token scope'))
 
 					// Restore
 					token.scope[scope] = oldVal
@@ -131,7 +131,7 @@ export default {
 				.then(tap(() => console.debug('app token name updated')))
 				.catch(err => {
 					console.error.bind('could not update app token name', err)
-					OC.Notification.showTemporary(t('core', 'Error while updating device token name'))
+					OC.Notification.showTemporary(t('settings', 'Error while updating device token name'))
 
 					// Restore
 					token.name = oldName
@@ -152,7 +152,7 @@ export default {
 				.then(tap(() => console.debug('app token deleted')))
 				.catch(err => {
 					console.error.bind('could not delete app token', err)
-					OC.Notification.showTemporary(t('core', 'Error while deleting the token'))
+					OC.Notification.showTemporary(t('settings', 'Error while deleting the token'))
 
 					// Restore
 					// eslint-disable-next-line vue/no-mutating-props
@@ -175,7 +175,7 @@ export default {
 				token.type = 2
 			} catch (err) {
 				console.error('could not wipe app token', err)
-				OC.Notification.showTemporary(t('core', 'Error while wiping the device with the token'))
+				OC.Notification.showTemporary(t('settings', 'Error while wiping the device with the token'))
 			}
 		},
 	},
