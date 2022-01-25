eed25090d94d78f321ec214ca13137e6612fde9e
Vincent Petry <vincent@nextcloud.com>
2022-01-25 17:47:58 +0100

    Add direct arg to login flow
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>
    Co-Authored-by: Carl Schwan <carl@carlschwan.eu>

--- core/templates/loginflow/authpicker.php
+++ core/templates/loginflow/authpicker.php
@@ -46,7 +46,7 @@ $urlGenerator = $_['urlGenerator'];
 	<br/>
 
 	<p id="redirect-link">
-		<a href="<?php p($urlGenerator->linkToRoute('core.ClientFlowLogin.grantPage', ['stateToken' => $_['stateToken'], 'clientIdentifier' => $_['clientIdentifier'], 'oauthState' => $_['oauthState']])) ?>">
+		<a href="<?php p($urlGenerator->linkToRoute('core.ClientFlowLogin.grantPage', ['stateToken' => $_['stateToken'], 'clientIdentifier' => $_['clientIdentifier'], 'oauthState' => $_['oauthState'], 'direct' => $_['direct']])) ?>">
 			<input type="submit" class="login primary icon-confirm-white" value="<?php p($l->t('Log in')) ?>">
 		</a>
 	</p>
@@ -62,6 +62,9 @@ $urlGenerator = $_['urlGenerator'];
 		</p>
 		<input type="hidden" name="stateToken" value="<?php p($_['stateToken']) ?>" />
 		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
+		<?php if ($_['direct'] !== 0) { ?>
+			<input type="hidden" name="direct" value="<?php p($_['direct']) ?>">
+		<?php } ?>
 		<input id="submit-app-token-login" type="submit" class="login primary icon-confirm-white" value="<?php p($l->t('Grant access')) ?>">
 	</form>
 </div>
