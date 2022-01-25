f48df63bb66436f9e5197e7bb81c13fa2f28cea0
Vincent Petry <vincent@nextcloud.com>
2022-01-25 17:59:37 +0100

    Also add direct outside the redirect url
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- core/templates/loginflow/authpicker.php
+++ core/templates/loginflow/authpicker.php
@@ -62,6 +62,7 @@ $urlGenerator = $_['urlGenerator'];
 		</p>
 		<input type="hidden" name="stateToken" value="<?php p($_['stateToken']) ?>" />
 		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
+		<input type="hidden" name="direct" value="<?php p($_['direct']) ?>">
 		<input id="submit-app-token-login" type="submit" class="login primary icon-confirm-white" value="<?php p($l->t('Grant access')) ?>">
 	</form>
 </div>
