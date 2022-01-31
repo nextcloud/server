714a5b7ae2dc5549da87275ac9cad49abfcc2513
Joas Schilling <coding@schilljs.com>
2022-01-31 21:23:07 +0100

    Use the new option to signaling insensitivity
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files_sharing/lib/ExpireSharesJob.php
+++ apps/files_sharing/lib/ExpireSharesJob.php
@@ -25,6 +25,7 @@
 namespace OCA\Files_Sharing;
 
 use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\IJob;
 use OCP\BackgroundJob\TimedJob;
 use OCP\IDBConnection;
 use OCP\Share\Exceptions\ShareNotFound;
@@ -50,6 +51,7 @@ class ExpireSharesJob extends TimedJob {
 
 		// Run once a day
 		$this->setInterval(24 * 60 * 60);
+		$this->setTimeSensitivity(IJob::TIME_INSENSITIVE);
 	}
 
 
