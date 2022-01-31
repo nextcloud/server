b8e0a3dbdd61877050328c9ed5ad72922c330a97
Joas Schilling <coding@schilljs.com>
2022-01-31 21:23:07 +0100

    Use the new option to signaling insensitivity
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/dav/lib/BackgroundJob/UploadCleanup.php
+++ apps/dav/lib/BackgroundJob/UploadCleanup.php
@@ -29,6 +29,7 @@ namespace OCA\DAV\BackgroundJob;
 
 use OC\User\NoUserException;
 use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\IJob;
 use OCP\BackgroundJob\IJobList;
 use OCP\BackgroundJob\TimedJob;
 use OCP\Files\File;
@@ -51,6 +52,7 @@ class UploadCleanup extends TimedJob {
 
 		// Run once a day
 		$this->setInterval(60 * 60 * 24);
+		$this->setTimeSensitivity(IJob::TIME_INSENSITIVE);
 	}
 
 	protected function run($argument) {
