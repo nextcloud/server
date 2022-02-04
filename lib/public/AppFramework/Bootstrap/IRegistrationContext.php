e3244361bada8bc79384f49e127a9a310f05ae89
Christopher Ng <chrng8@gmail.com>
2022-02-04 20:49:11 +0000

    Allow registration of migrators
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- lib/public/AppFramework/Bootstrap/IRegistrationContext.php
+++ lib/public/AppFramework/Bootstrap/IRegistrationContext.php
@@ -295,4 +295,15 @@ interface IRegistrationContext {
 	 * @since 24.0.0
 	 */
 	public function registerCalendarRoomBackend(string $class): void;
+
+	/**
+	 * Register an implementation of \OCP\UserMigration\IMigrator that
+	 * will handle the implementation of a migrator
+	 *
+	 * @param string $migratorClass
+	 * @psalm-param class-string<\OCP\UserMigration\IMigrator> $migratorClass
+	 * @return void
+	 * @since 24.0.0
+	 */
+	public function registerUserMigrator(string $migratorClass): void;
 }
