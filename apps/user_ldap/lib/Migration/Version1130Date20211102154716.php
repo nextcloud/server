a737a2561bb9ef7a71e9633bb6abacb7b0beaedb
Côme Chilliet <come.chilliet@nextcloud.com>
2022-03-08 12:10:52 +0100

    Fix similar potential problems with fetchOne loops
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/user_ldap/lib/Migration/Version1130Date20211102154716.php
+++ apps/user_ldap/lib/Migration/Version1130Date20211102154716.php
@@ -259,7 +259,7 @@ class Version1130Date20211102154716 extends SimpleMigrationStep {
 
 		$result = $select->executeQuery();
 		$idList = [];
-		while ($id = $result->fetchOne()) {
+		while (($id = $result->fetchOne()) !== false) {
 			$idList[] = $id;
 		}
 		$result->closeCursor();
