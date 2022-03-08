b561f15542f3877e61c18461b3ec63afa6d22104
Boris Budini <boris@budini.xyz>
2022-03-08 13:28:22 +0100

    Update Backend.php

--- apps/dav/lib/CalDAV/Reminder/Backend.php
+++ apps/dav/lib/CalDAV/Reminder/Backend.php
@@ -67,8 +67,8 @@ class Backend {
 		$query->select(['cr.*', 'co.calendardata', 'c.displayname', 'c.principaluri'])
 			->from('calendar_reminders', 'cr')
 			->where($query->expr()->lte('cr.notification_date', $query->createNamedParameter($this->timeFactory->getTime())))
-			->leftJoin('cr', 'calendarobjects', 'co', $query->expr()->eq('cr.object_id', 'co.id'))
-			->leftJoin('cr', 'calendars', 'c', $query->expr()->eq('cr.calendar_id', 'c.id'))
+			->join('cr', 'calendarobjects', 'co', $query->expr()->eq('cr.object_id', 'co.id'))
+			->join('cr', 'calendars', 'c', $query->expr()->eq('cr.calendar_id', 'c.id'))
 			->groupBy(['cr.event_hash','cr.type','cr.uid']);
 		$stmt = $query->execute();
 
