c818ac6de4bd631e57772e3602fba1eaff180ff6
Joas Schilling <coding@schilljs.com>
2022-03-10 14:24:15 +0100

    Bump version
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [22, 2, 5, 1];
+$OC_Version = [22, 2, 5, 2];
 
 // The human readable string
 $OC_VersionString = '22.2.5';
