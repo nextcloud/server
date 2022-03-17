eafa64d5a5f92668f28a3a8950a908d0c1f8ab4a
Robin Appelman <robin@icewind.nl>
2022-03-17 14:52:33 +0100

    cache the path of the certificate bundle
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Security/CertificateManager.php
+++ lib/private/Security/CertificateManager.php
@@ -61,6 +61,8 @@ class CertificateManager implements ICertificateManager {
 	/** @var ISecureRandom */
 	protected $random;
 
+	private ?string $bundlePath = null;
+
 	/**
 	 * @param \OC\Files\View $view relative to data/
 	 * @param IConfig $config
@@ -190,6 +192,7 @@ class CertificateManager implements ICertificateManager {
 		if (!Filesystem::isValidPath($name) or Filesystem::isFileBlacklisted($name)) {
 			throw new \Exception('Filename is not valid');
 		}
+		$this->bundlePath = null;
 
 		$dir = $this->getPathToCertificates() . 'uploads/';
 		if (!$this->view->file_exists($dir)) {
@@ -217,6 +220,8 @@ class CertificateManager implements ICertificateManager {
 		if (!Filesystem::isValidPath($name)) {
 			return false;
 		}
+		$this->bundlePath = null;
+
 		$path = $this->getPathToCertificates() . 'uploads/';
 		if ($this->view->file_exists($path . $name)) {
 			$this->view->unlink($path . $name);
