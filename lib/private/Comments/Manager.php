86821ddf4803abd504ab704b9a4cc4335b1acc93
Vitor Mattos <vitor@php.rio>
2022-03-30 09:17:40 -0300

    Increase check if support reactions
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/private/Comments/Manager.php
+++ lib/private/Comments/Manager.php
@@ -1074,7 +1074,7 @@ class Manager implements ICommentsManager {
 	 * @since 24.0.0
 	 */
 	public function supportReactions(): bool {
-		return $this->dbConn->supports4ByteText();
+		return $this->emojiHelper->doesPlatformSupportEmoji();
 	}
 
 	/**
