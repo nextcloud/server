ce7b6ffed2186c6a1a46549669fb1f3cb4451114
Louis Chemineau <louis@chmn.me>
2022-03-31 11:38:26 +0200

    Prevent column name conflict between tables
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/private/Share20/DefaultShareProvider.php
+++ lib/private/Share20/DefaultShareProvider.php
@@ -643,7 +643,10 @@ class DefaultShareProvider implements IShareProvider {
 
 	public function getSharesInFolder($userId, Folder $node, $reshares) {
 		$qb = $this->dbConn->getQueryBuilder();
-		$qb->select('*')
+		$qb->select('s.*',
+				'f.fileid', 'f.path', 'f.permissions AS f_permissions', 'f.storage', 'f.path_hash',
+				'f.parent AS f_parent', 'f.name', 'f.mimetype', 'f.mimepart', 'f.size', 'f.mtime', 'f.storage_mtime',
+				'f.encrypted', 'f.unencrypted_size', 'f.etag', 'f.checksum')
 			->from('share', 's')
 			->andWhere($qb->expr()->orX(
 				$qb->expr()->eq('item_type', $qb->createNamedParameter('file')),
