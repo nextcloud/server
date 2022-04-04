d3b9d49c22cbf5e12ab803e66e03ce6decb8454b
Joas Schilling <coding@schilljs.com>
2022-04-04 07:06:55 +0200

    Deduplicate storage ids in list before reusing
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Files/Cache/Storage.php
+++ lib/private/Files/Cache/Storage.php
@@ -240,6 +240,7 @@ class Storage {
 				->from('mounts')
 				->where($query->expr()->eq('mount_id', $query->createNamedParameter($mountId, IQueryBuilder::PARAM_INT)));
 			$storageIds = $query->executeQuery()->fetchAll(\PDO::FETCH_COLUMN);
+			$storageIds = array_unique($storageIds);
 
 			$query = $db->getQueryBuilder();
 			$query->delete('filecache')
