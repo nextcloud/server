553239c0834f4089d7ad9b75691264d41de9e069
cld4h <20869428+cld4h@users.noreply.github.com>
2022-04-22 10:00:00 +0800

    Fix issue #31692 of occ files:scan
    occ files:scan reports error "Implicit conversion from float XXX to int loses precision"
    
    Signed-off-by: cld4h <20869428+cld4h@users.noreply.github.com>

--- apps/files/lib/Command/Scan.php
+++ apps/files/lib/Command/Scan.php
@@ -291,7 +291,7 @@ class Scan extends Base {
 	protected function formatExecTime() {
 		$secs = round($this->execTime);
 		# convert seconds into HH:MM:SS form
-		return sprintf('%02d:%02d:%02d', ($secs / 3600), ($secs / 60 % 60), $secs % 60);
+		return sprintf('%02d:%02d:%02d', (int)($secs / 3600), ( (int)($secs / 60) % 60), $secs % 60);
 	}
 
 	protected function reconnectToDatabase(OutputInterface $output): Connection {
