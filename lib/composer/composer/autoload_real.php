1f92440b79e90bd3337407ed55ea7d8474ee0ddf
John Molakvoæ <skjnldsv@protonmail.com>
2022-04-26 07:57:39 +0200

    Fix mexitek/phpcolors
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- lib/composer/composer/autoload_real.php
+++ lib/composer/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit53792487c5a8370acc0b06b1a864ff4c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit53792487c5a8370acc0b06b1a864ff4c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit53792487c5a8370acc0b06b1a864ff4c', 'loadClassLoader'));
