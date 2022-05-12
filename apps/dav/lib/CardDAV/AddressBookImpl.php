28b0da59ce84eec55d3e6f38fe64ea40560cc0df
Thomas Citharel <tcit@tcit.fr>
2022-05-12 17:32:47 +0200

    Fix a doctype in OCP for IAddressBook
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- apps/dav/lib/CardDAV/AddressBookImpl.php
+++ apps/dav/lib/CardDAV/AddressBookImpl.php
@@ -12,6 +12,7 @@
  * @author Joas Schilling <coding@schilljs.com>
  * @author John Molakvoæ <skjnldsv@protonmail.com>
  * @author Julius Härtl <jus@bitgrid.net>
+ * @author Thomas Citharel <nextcloud@tcit.fr>
  * @author Thomas Müller <thomas.mueller@tmit.eu>
  *
  * @license AGPL-3.0
@@ -206,7 +207,7 @@ class AddressBookImpl implements IAddressBook {
 	}
 
 	/**
-	 * @param object $id the unique identifier to a contact
+	 * @param int $id the unique identifier to a contact
 	 * @return bool successful or not
 	 * @since 5.0.0
 	 */
