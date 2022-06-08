6df0a79e5d335c5d6a23b839818932ec6e365c6e
Vincent Petry <vincent@nextcloud.com>
2022-06-08 17:16:37 +0200

    Enable files_external for tests
    Because we have more tests now that require classes from this app.
    
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- tests/enable_all.php
+++ tests/enable_all.php
@@ -18,6 +18,7 @@ function enableApp($app) {
 
 enableApp('files_sharing');
 enableApp('files_trashbin');
+enableApp('files_external');
 enableApp('encryption');
 enableApp('user_ldap');
 enableApp('files_versions');
