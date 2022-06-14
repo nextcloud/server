f5e2b0b22f51cdb67c06c368e921052d25aacc2e
Robin Appelman <robin@icewind.nl>
2022-06-14 15:57:59 +0200

    allow storing multiple mounts for the same rootid in the mount cache
    currently `[$userId, $rootId]` is used as the unique key for storing mounts in the mount cache,
    however there are cases where the same rootid is mounted in multiple places for a user which currently leads to not all of those mounts being added to the cache.
    
    Previously this didn't matter as the mount cache was only used to list users with access to a specific file, so a user having access to the file multiple times didn' change anything.
    
    With 24 the mount cache is used for more cases and multiple mounts for the same id becomes relevant.
    While I think there isn't a real negative effect atm besides missing the optimized path we should ensure that the mounts are properly listed
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- core/Migrations/Version13000Date20170718121200.php
+++ core/Migrations/Version13000Date20170718121200.php
@@ -149,7 +149,7 @@ class Version13000Date20170718121200 extends SimpleMigrationStep {
 			$table->addIndex(['storage_id'], 'mounts_storage_index');
 			$table->addIndex(['root_id'], 'mounts_root_index');
 			$table->addIndex(['mount_id'], 'mounts_mount_id_index');
-			$table->addUniqueIndex(['user_id', 'root_id'], 'mounts_user_root_index');
+			$table->addIndex(['user_id', 'root_id', 'mount_point'], 'mounts_user_root_path_index', [], ['lengths' => [null, null, 128]]);
 		} else {
 			$table = $schema->getTable('mounts');
 			$table->addColumn('mount_id', Types::BIGINT, [
