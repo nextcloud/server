c0d88cb07fff00999c6cc3777f8d2ff188d6a348
Daniel Kesselberg <mail@danielkesselberg.de>
2022-06-20 17:07:58 +0200

    Revert "don't try to get custom certs for s3 primary storage"
    This reverts commit 1156214a269572b4460d8aa8a599076520f26b58.
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/private/Files/ObjectStore/S3ConnectionTrait.php
+++ lib/private/Files/ObjectStore/S3ConnectionTrait.php
@@ -121,14 +121,8 @@ trait S3ConnectionTrait {
 			)
 		);
 
-		// since we store the certificate bundles on the primary storage, we can't get the bundle while setting up the primary storage
-		if (!isset($this->params['primary_storage'])) {
-			/** @var ICertificateManager $certManager */
-			$certManager = \OC::$server->get(ICertificateManager::class);
-			$certPath = $certManager->getAbsoluteBundlePath();
-		} else {
-			$certPath = \OC::$SERVERROOT . '/resources/config/ca-bundle.crt';
-		}
+		/** @var ICertificateManager $certManager */
+		$certManager = \OC::$server->get(ICertificateManager::class);
 
 		$options = [
 			'version' => isset($this->params['version']) ? $this->params['version'] : 'latest',
@@ -139,7 +133,7 @@ trait S3ConnectionTrait {
 			'signature_provider' => \Aws\or_chain([self::class, 'legacySignatureProvider'], ClientResolver::_default_signature_provider()),
 			'csm' => false,
 			'use_arn_region' => false,
-			'http' => ['verify' => $certPath],
+			'http' => ['verify' => $certManager->getAbsoluteBundlePath()],
 		];
 		if ($this->getProxy()) {
 			$options['http']['proxy'] = $this->getProxy();
