f5236cde1a2b9ec87794768568155344fa3a4461
Christopher Ng <chrng8@gmail.com>
2022-07-04 19:20:49 +0000

    Change tooltip title of files grid view toggle dynamically
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- apps/files/templates/index.php
+++ apps/files/templates/index.php
@@ -6,7 +6,7 @@
 		aria-label="<?php p($l->t('Toggle grid view'))?>"
 		<?php if ($_['showgridview']) { ?>checked="checked" <?php } ?>/>
 	<label id="view-toggle" for="showgridview" tabindex="0" class="button <?php p($_['showgridview'] ? 'icon-toggle-filelist' : 'icon-toggle-pictures') ?>"
-		title="<?php p($l->t('Toggle grid view'))?>"></label>
+		title="<?php p($_['showgridview'] ? $l->t('Show list view') : $l->t('Show grid view'))?>"></label>
 
 	<?php foreach ($_['appContents'] as $content) { ?>
 	<div id="app-content-<?php p($content['id']) ?>" class="hidden viewcontainer">
