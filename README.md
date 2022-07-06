860f67ae8af3ffeb891c5cda4b4a463487328858
Daniel Calviño Sánchez <danxuliu@gmail.com>
2022-07-06 17:36:30 +0200

    Fix branch selection in stable24
    Signed-off-by: Joas Schilling <coding@schilljs.com>
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Nextcloud Server ☁
-[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/nextcloud/server/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/nextcloud/server/?branch=master)
-[![codecov](https://codecov.io/gh/nextcloud/server/branch/master/graph/badge.svg)](https://codecov.io/gh/nextcloud/server)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/nextcloud/server/badges/quality-score.png?b=stable24)](https://scrutinizer-ci.com/g/nextcloud/server/?branch=stable24)
+[![codecov](https://codecov.io/gh/nextcloud/server/branch/stable24/graph/badge.svg)](https://codecov.io/gh/nextcloud/server)
 [![CII Best Practices](https://bestpractices.coreinfrastructure.org/projects/209/badge)](https://bestpractices.coreinfrastructure.org/projects/209)
 
 **A safe home for all your data.**
--- build/update-apps.sh
+++ build/update-apps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Update Nextcloud apps from latest git master
+# Update Nextcloud apps from latest git stable24
 # For local development environment
 # Use from Nextcloud server folder with `./build/update-apps.sh`
 #
 # It automatically:
 # - goes through all apps which are not shipped via server
 # - shows the app name in bold and uses whitespace for separation
-# - changes to master and pulls quietly
+# - changes to stable24 and pulls quietly
 # - shows the 3 most recent commits for context
-# - removes branches merged into master
+# - removes branches merged into stable24
 # - … could even do the build steps if they are consistent for the apps (like `make`)
 
-find apps* -maxdepth 2 -name .git -exec sh -c 'cd {}/../ && printf "\n\033[1m${PWD##*/}\033[0m\n" && git checkout master && git pull --quiet -p && git --no-pager log -3 --pretty=format:"%h %Cblue%ar%x09%an %Creset%s" && printf "\n" && git branch --merged master | grep -v "master$" | xargs git branch -d && cd ..' \;
+find apps* -maxdepth 2 -name .git -exec sh -c 'cd {}/../ && printf "\n\033[1m${PWD##*/}\033[0m\n" && git checkout stable24 && git pull --quiet -p && git --no-pager log -3 --pretty=format:"%h %Cblue%ar%x09%an %Creset%s" && printf "\n" && git branch --merged stable24 | grep -v "stable24$" | xargs git branch -d && cd ..' \;
