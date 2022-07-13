ba9815fbe1abfaf87bab8453929dbfd9538d2d6c
Joas Schilling <coding@schilljs.com>
2022-07-13 13:05:18 +0200

    Translate error when user already exists
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/settings/src/store/users.js
+++ apps/settings/src/store/users.js
@@ -30,6 +30,7 @@ import api from './api'
 import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
 import logger from '../logger'
+import { showError } from '@nextcloud/dialogs'
 
 const orderGroups = function(groups, orderBy) {
 	/* const SORT_USERCOUNT = 1;
@@ -552,6 +553,13 @@ const actions = {
 				.then((response) => dispatch('addUserData', userid || response.data.ocs.data.id))
 				.catch((error) => { throw error })
 		}).catch((error) => {
+			const statusCode = error?.response?.data?.ocs?.meta?.statuscode
+
+			if (statusCode === 102) {
+				showError(t('settings', 'User already exists.'))
+				throw error
+			}
+
 			commit('API_FAILURE', { userid, error })
 			throw error
 		})
