ac736b2f82adfe73176fa78620e617ff17dbc91e
Julius Härtl <jus@bitgrid.net>
2022-08-24 17:19:17 +0200

    Do not prevent scaling due to accessibility
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -22,7 +22,7 @@ $getUserAvatar = static function (int $size) use ($_): string {
 				p($theme->getTitle());
 			?>
 		</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0 user-scalable=no, viewport-fit=cover" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<?php if ($theme->getiTunesAppId() !== '') { ?>
 		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
