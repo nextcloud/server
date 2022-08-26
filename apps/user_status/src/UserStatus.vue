4beae14fee064234f7da2c23da55d0226eede215
Julius Härtl <jus@bitgrid.net>
2022-08-26 07:18:47 +0200

    Avoid using lists if there are none on the dashboard
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/user_status/src/UserStatus.vue
+++ apps/user_status/src/UserStatus.vue
@@ -20,7 +20,7 @@
   -->
 
 <template>
-	<li>
+	<component :is="elementTag">
 		<div class="user-status-menu-item">
 			<!-- Username display -->
 			<a v-if="!inline"
@@ -51,7 +51,7 @@
 		<!-- Status management modal -->
 		<SetStatusModal v-if="isModalOpen"
 			@close="closeModal" />
-	</li>
+	</component>
 </template>
 
 <script>
@@ -94,6 +94,9 @@ export default {
 		}
 	},
 	computed: {
+		elementTag() {
+			return this.inline ? 'div' : 'li'
+		},
 		/**
 		 * The profile page link
 		 *
