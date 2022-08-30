654d649edba7fd592fdc2d834463c1a983233dfd
Arthur Schiwon <blizzz@arthur-schiwon.de>
2022-08-30 17:29:44 +0200

    25.0.0 beta 4
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = [25, 0, 0, 7];
+$OC_Version = [25, 0, 0, 8];
 
 // The human readable string
-$OC_VersionString = '25.0.0 beta 3';
+$OC_VersionString = '25.0.0 beta 4';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
