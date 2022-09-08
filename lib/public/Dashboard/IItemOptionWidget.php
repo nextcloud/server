2302ca1fcb41c26072fd07190ce940e171fec16e
Julien Veyssier <eneiluj@posteo.net>
2022-09-08 11:48:00 +0200

    add IItemOptionWidget to define some item-related parameters, only getItemIconsRound() for now
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/public/Dashboard/IItemOptionWidget.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2022 Julien Veyssier <eneiluj@posteo.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCP\Dashboard;
+
+/**
+ * Allow getting widget options
+ *
+ * @since 25.0.0
+ */
+interface IItemOptionWidget extends IWidget {
+	/**
+	 * Should the item icons be rendered round (or raw/square) by the clients?
+	 *
+	 * @return bool
+	 */
+	public function getItemIconsRound(): bool;
+}
