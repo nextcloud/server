1998ac7b06e8845719db5cd276db59ac053bbcd2
Vincent Petry <vincent@nextcloud.com>
2022-09-15 17:10:15 +0200

    Increase timeout for apps management
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- tests/acceptance/features/bootstrap/AppsManagementContext.php
+++ tests/acceptance/features/bootstrap/AppsManagementContext.php
@@ -243,7 +243,7 @@ class AppsManagementContext implements Context, ActorAwareInterface {
 	 */
 	public function iSeeTheAppBundles() {
 		Assert::assertTrue(
-			$this->actor->find(self::rowForApp('Auditing / Logging'), 2)->isVisible()
+			$this->actor->find(self::rowForApp('Auditing / Logging'), 10)->isVisible()
 		);
 		Assert::assertTrue(
 			$this->actor->find(self::rowForApp('LDAP user and group backend'), 2)->isVisible()
