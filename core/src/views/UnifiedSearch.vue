e73ef4291d1b76b75547de1c49f789e9c81ddaa6
Carl Schwan <carl@carlschwan.eu>
2022-09-19 16:45:30 +0200

    Clear search results when remove search query
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- core/src/views/UnifiedSearch.vue
+++ core/src/views/UnifiedSearch.vue
@@ -410,6 +410,9 @@ export default {
 
 			// Do not search if not long enough
 			if (this.query.trim() === '' || this.isShortQuery) {
+				for (const type of this.typesIDs) {
+					this.$delete(this.results, type)
+				}
 				return
 			}
 
