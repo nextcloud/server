5e416c529e7b4370d3ae2f191535b768bfca6941
Julius Härtl <jus@bitgrid.net>
2022-10-14 23:44:29 +0200

    Use proper storage method for writing skeleton files
    otherwise the filecache will have a wrong size for skeleton files
    
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/legacy/OC_Util.php
+++ lib/private/legacy/OC_Util.php
@@ -255,7 +255,7 @@ class OC_Util {
 						closedir($dir);
 						return;
 					}
-					stream_copy_to_stream($sourceStream, $child->fopen('w'));
+					$child->putContent($sourceStream);
 				}
 			}
 		}
