nextcloud/server

commit 45eefc221d4439c99fd2c8badfe93dc3af40f028

Disable sharing calendars via link when sharik via link is disabled
This mod disallows sharing calendars via link when `shareapi_allow_links`
is disabled.

Related: https://github.com/nextcloud/calendar/issues/525
Related: https://github.com/nextcloud/calendar/issues/4399
Author-Change-Id: IB#1126264
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

apps/dav/lib/CalDAV/CalDavBackend.php
@@ -3,6 +3,7 @@
  * @copyright Copyright (c) 2016, ownCloud, Inc.
  * @copyright Copyright (c) 2018 Georg Ehrke
  * @copyright Copyright (c) 2020, leith abdulla (<online-nextcloud@eleith.com>)
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Chih-Hsuan Yen <yan12125@gmail.com>
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
@@ -512,6 +513,10 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 	 * @return array
 	 */
 	public function getPublicCalendars() {
+		// Sharing via link must be enabled.
+		if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no' ) {
+			return array_values([]);
+		}
 		$fields = array_column($this->propertyMap, 0);
 		$fields[] = 'a.id';
 		$fields[] = 'a.uri';
@@ -570,6 +575,10 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 	 * @throws NotFound
 	 */
 	public function getPublicCalendar($uri) {
+		// Sharing via link must be enabled.
+		if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no' ) {
+			throw new NotFound('Node with name \'' . $uri . '\' could not be found');
+		}
 		$fields = array_column($this->propertyMap, 0);
 		$fields[] = 'a.id';
 		$fields[] = 'a.uri';

apps/dav/lib/CalDAV/Publishing/PublishPlugin.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @copyright Copyright (c) 2016 Thomas Citharel <tcit@tcit.fr>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
  * @author Georg Ehrke <oc.list@georgehrke.com>
@@ -203,6 +204,11 @@ class PublishPlugin extends ServerPlugin {
 				if ($limitSharingToOwner && !$isOwner) {
 					return;
 				}
+			}
+
+			// Sharing via link must be enabled.
+			if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no') {
+				return;
 			}
 
 			$node->setPublishStatus(true);

commit bfd8c622d99f24d7769db39b095ce00176e6da8a

Disable sharing calendars via link adjusted
Fixes: 45eefc221d4439c99fd2c8badfe93dc3af40f028
Related: https://github.com/nextcloud/server/pull/34372
Author-Change-Id: IB#1126264
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

apps/dav/lib/CalDAV/CalDavBackend.php
@@ -3,7 +3,6 @@
  * @copyright Copyright (c) 2016, ownCloud, Inc.
  * @copyright Copyright (c) 2018 Georg Ehrke
  * @copyright Copyright (c) 2020, leith abdulla (<online-nextcloud@eleith.com>)
- * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Chih-Hsuan Yen <yan12125@gmail.com>
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
@@ -513,10 +512,6 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 	 * @return array
 	 */
 	public function getPublicCalendars() {
-		// Sharing via link must be enabled.
-		if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no' ) {
-			return array_values([]);
-		}
 		$fields = array_column($this->propertyMap, 0);
 		$fields[] = 'a.id';
 		$fields[] = 'a.uri';
@@ -575,10 +570,6 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 	 * @throws NotFound
 	 */
 	public function getPublicCalendar($uri) {
-		// Sharing via link must be enabled.
-		if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no' ) {
-			throw new NotFound('Node with name \'' . $uri . '\' could not be found');
-		}
 		$fields = array_column($this->propertyMap, 0);
 		$fields[] = 'a.id';
 		$fields[] = 'a.uri';

apps/dav/lib/CalDAV/PublicCalendarRoot.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
  * @author Georg Ehrke <oc.list@georgehrke.com>
@@ -29,6 +30,7 @@ use OCP\IConfig;
 use OCP\IL10N;
 use Psr\Log\LoggerInterface;
 use Sabre\DAV\Collection;
+use Sabre\DAV\Exception\Forbidden;
 
 class PublicCalendarRoot extends Collection {
 
@@ -70,6 +72,10 @@ class PublicCalendarRoot extends Collection {
 	 * @inheritdoc
 	 */
 	public function getChild($name) {
+		// Sharing via link is allowed by default, but if the option is set it should be checked.
+		if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no' ) {
+			throw new \Sabre\DAV\Exception\Forbidden();
+		}
 		$calendar = $this->caldavBackend->getPublicCalendar($name);
 		return new PublicCalendar($this->caldavBackend, $calendar, $this->l10n, $this->config, $this->logger);
 	}

apps/dav/lib/CalDAV/Publishing/PublishPlugin.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @copyright Copyright (c) 2016 Thomas Citharel <tcit@tcit.fr>
- * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
  * @author Georg Ehrke <oc.list@georgehrke.com>
@@ -204,11 +203,6 @@ class PublishPlugin extends ServerPlugin {
 				if ($limitSharingToOwner && !$isOwner) {
 					return;
 				}
-			}
-
-			// Sharing via link must be enabled.
-			if ($this->config->getAppValue('core', 'shareapi_allow_links', 'yes') === 'no') {
-				return;
 			}
 
 			$node->setPublishStatus(true);

apps/dav/lib/Server.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Arthur Schiwon <blizzz@arthur-schiwon.de>
  * @author Bjoern Schiessle <bjoern@schiessle.org>
@@ -181,10 +182,12 @@ class Server {
 
 			$this->server->addPlugin(new \Sabre\CalDAV\Notifications\Plugin());
 			$this->server->addPlugin(new DAV\Sharing\Plugin($authBackend, \OC::$server->getRequest(), \OC::$server->getConfig()));
-			$this->server->addPlugin(new \OCA\DAV\CalDAV\Publishing\PublishPlugin(
-				\OC::$server->getConfig(),
-				\OC::$server->getURLGenerator()
-			));
+			if (\OC::$server->getConfig()->getAppValue('core', 'shareapi_allow_links', 'yes') === 'yes') {
+				$this->server->addPlugin(new \OCA\DAV\CalDAV\Publishing\PublishPlugin(
+					\OC::$server->getConfig(),
+					\OC::$server->getURLGenerator()
+				));
+			}
 		}
 
 		// addressbook plugins