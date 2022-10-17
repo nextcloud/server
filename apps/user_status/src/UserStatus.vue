a18f5e76d564d7eaf4b9d70a406689d1501d6ae4
Carl Schwan <carl@carlschwan.eu>
2022-10-17 23:40:24 +0200

    Make css rule important
    Co-authored-by: Joas Schilling <213943+nickvergessen@users.noreply.github.com>
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- apps/user_status/src/UserStatus.vue
+++ apps/user_status/src/UserStatus.vue
@@ -283,7 +283,7 @@ export default {
 			margin-right: 10px;
 			opacity: 1 !important;
 			background-size: 16px;
-			vertical-align: middle;
+			vertical-align: middle !important;
 		}
 
 		// In dashboard
