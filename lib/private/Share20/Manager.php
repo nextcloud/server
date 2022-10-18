e09fabd47c4c184c43726b3af79ff44b030a3e42
Robin Appelman <robin@icewind.nl>
2022-10-18 14:31:04 +0200

    remove no longer needed code
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -2088,8 +2088,4 @@ class Manager implements IManager {
 			yield from $provider->getAllShares();
 		}
 	}
-
-	public function getProviderForType(int $shareType): IShareProvider {
-		return $this->factory->getProviderForType($shareType);
-	}
 }
