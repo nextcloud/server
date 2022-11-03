56f24cff9f1c2b7aed81886f93404b055bb90107
Côme Chilliet <come.chilliet@nextcloud.com>
2022-11-03 11:53:51 +0100

    Avoid a crash when a PHP extension has no version
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/App/PlatformRepository.php
+++ lib/private/App/PlatformRepository.php
@@ -50,7 +50,7 @@ class PlatformRepository {
 			$ext = new \ReflectionExtension($name);
 			try {
 				$prettyVersion = $ext->getVersion();
-				$prettyVersion = $this->normalizeVersion($prettyVersion);
+				$prettyVersion = $this->normalizeVersion($prettyVersion ?? '0');
 			} catch (\UnexpectedValueException $e) {
 				$prettyVersion = '0';
 				$prettyVersion = $this->normalizeVersion($prettyVersion);
@@ -111,6 +111,9 @@ class PlatformRepository {
 					continue 2;
 			}
 
+			if ($prettyVersion === null) {
+				continue;
+			}
 			try {
 				$prettyVersion = $this->normalizeVersion($prettyVersion);
 			} catch (\UnexpectedValueException $e) {
