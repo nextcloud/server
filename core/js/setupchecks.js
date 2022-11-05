ea41f21bbea03e3d75e87b14d258a5ba30975399
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-11-05 18:14:41 +0100

    l10n: Shorten link
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- core/js/setupchecks.js
+++ core/js/setupchecks.js
@@ -427,7 +427,7 @@
 						messages.push({
 							msg: t(
 								'core',
-								'It seems like you are running a 32-bit PHP version. Nextcloud 26 and higher require 64-bit. Please upgrade your OS and PHP to 64-bit! For further details read {linkstart}the documentation page about this ↗{linkend}.'
+								'It seems like you are running a 32-bit PHP version. Nextcloud 26 and higher require 64-bit. Please upgrade your OS and PHP to 64-bit! For further details read {linkstart}the documentation page ↗{linkend} about this.'
 								.replace('{linkstart}', '<a target="_blank" rel="noreferrer noopener" class="external" href="' + OC.theme.docPlaceholderUrl.replace('PLACEHOLDER', 'admin-system-requirements') + '">')
 								.replace('{linkend}', '</a>'),
 							),
