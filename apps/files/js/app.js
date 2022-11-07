e606db168a353d38f49e2c644787b65c84c452a5
Daniel Calviño Sánchez <danxuliu@gmail.com>
2022-11-08 00:53:12 +0100

    Fix sidebar not automatically hidden in Files app
    Although the Files app creates the legacy sidebar (details view) it is
    then replaced with the newer Vue app sidebar. Due to this ".detailsView"
    no longer finds an element and therefore nothing was hidden when
    "hideAppSidebar($('.detailsView'))" was called (for example, when
    changing to another section).
    
    However, "OC.Apps.hideAppSidebar()" does not properly work either with
    the Vue sidebar used in the Files app (once hidden the sidebar is not
    shown again). For simplicity, and to avoid any possible side effect in
    other apps from changing "OC.Apps.hideAppSidebar", now
    "OC.Files.Sidebar.close()" is used instead.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/files/js/app.js
+++ apps/files/js/app.js
@@ -307,7 +307,7 @@
 					dir: e.dir ? e.dir : '/'
 				};
 				this._changeUrl(params.view, params.dir);
-				OC.Apps.hideAppSidebar($('.detailsView'));
+				OCA.Files.Sidebar.close();
 				this.navigation.getActiveContainer().trigger(new $.Event('urlChanged', params));
 				window._nc_event_bus.emit('files:navigation:changed')
 			}
@@ -338,7 +338,7 @@
 		_onChangeViewerMode: function(e) {
 			var state = !!e.viewerModeEnabled;
 			if (e.viewerModeEnabled) {
-				OC.Apps.hideAppSidebar($('.detailsView'));
+				OCA.Files.Sidebar.close();
 			}
 			$('#app-navigation').toggleClass('hidden', state);
 			$('.app-files').toggleClass('viewer-mode no-sidebar', state);
