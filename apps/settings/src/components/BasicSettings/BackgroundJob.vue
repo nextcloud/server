a93f8b694562e21f118d56d31487aa11a18d8403
Joas Schilling <coding@schilljs.com>
2022-11-07 11:01:22 +0100

    Show the correct warning for system cron vs. webcron/ajax
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/settings/src/components/BasicSettings/BackgroundJob.vue
+++ apps/settings/src/components/BasicSettings/BackgroundJob.vue
@@ -29,11 +29,11 @@
 				{{ t('settings', 'Last job execution ran {time}. Something seems wrong.', {time: relativeTime}) }}
 			</NcNoteCard>
 
-			<NcNoteCard v-else-if="longExecutionNotCron" type="warning">
+			<NcNoteCard v-else-if="longExecutionCron" type="warning">
 				{{ t('settings', "Some jobs have not been executed since {maxAgeRelativeTime}. Please consider increasing the execution frequency.", {maxAgeRelativeTime}) }}
 			</NcNoteCard>
 
-			<NcNoteCard v-else-if="longExecutionCron" type="warning">
+			<NcNoteCard v-else-if="longExecutionNotCron" type="warning">
 				{{ t('settings', "Some jobs have not been executed since {maxAgeRelativeTime}. Please consider switching to system cron.", {maxAgeRelativeTime}) }}
 			</NcNoteCard>
 
