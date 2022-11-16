b2af916872b88d1cb1739904ec66fc4417ac0112
Julius Härtl <jus@bitgrid.net>
2022-11-16 20:37:57 +0100

    Take some shortcut when setting up the application class
    When getting the application class we can ask the app container directly
    and skip the server container as it won't find a match and fallback to
    the app container anyways
    
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/ServerContainer.php
+++ lib/private/ServerContainer.php
@@ -139,10 +139,13 @@ class ServerContainer extends SimpleContainer {
 	public function query(string $name, bool $autoload = true) {
 		$name = $this->sanitizeName($name);
 
-		try {
-			return parent::query($name, false);
-		} catch (QueryException $e) {
-			// Continue with general autoloading then
+		if (strpos($name, 'OCA\\') !== 0) {
+			// Skip server container query for app namespace classes
+			try {
+				return parent::query($name, false);
+			} catch (QueryException $e) {
+				// Continue with general autoloading then
+			}
 		}
 
 		// In case the service starts with OCA\ we try to find the service in
