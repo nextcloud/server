0570327c7e47b6ce81b3ee62bf35021dba93bc4e
Artur Neumann <artur@jankaritech.com>
2022-11-21 17:28:21 +0545

    public interface to invalidate tokens of user
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -157,6 +157,7 @@ use OCA\Theming\Util;
 use OCP\Accounts\IAccountManager;
 use OCP\App\IAppManager;
 use OCP\Authentication\LoginCredentials\IStore;
+use OCP\Authentication\Token\IProvider as OCPIProvider;
 use OCP\BackgroundJob\IJobList;
 use OCP\Collaboration\AutoComplete\IManager;
 use OCP\Command\IBus;
@@ -536,6 +537,7 @@ class Server extends ServerContainer implements IServerContainer {
 		});
 		$this->registerAlias(IStore::class, Store::class);
 		$this->registerAlias(IProvider::class, Authentication\Token\Manager::class);
+		$this->registerAlias(OCPIProvider::class, Authentication\Token\Manager::class);
 
 		$this->registerService(\OC\User\Session::class, function (Server $c) {
 			$manager = $c->get(IUserManager::class);
