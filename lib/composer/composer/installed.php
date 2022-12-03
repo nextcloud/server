77a358e89e2bbcdd6dfa694950a3e124c608c5a3
charleypaulus <charleypaulus@hotmail.com>
2022-12-03 15:21:33 +0100

    Ran composer install --no-dev
    Signed-off-by: charleypaulus <charleypaulus@hotmail.com>

--- lib/composer/composer/installed.php
+++ lib/composer/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '0f5cbad3088d38849b7be27871422c18171ebf65',
+        'reference' => '6545dd5ce1e4315b3583564bd439ce16745a1874',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '0f5cbad3088d38849b7be27871422c18171ebf65',
+            'reference' => '6545dd5ce1e4315b3583564bd439ce16745a1874',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../',
             'aliases' => array(),
