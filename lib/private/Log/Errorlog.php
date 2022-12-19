SINAKZM/own_nextcloud

fix(logging): Fix array to string conversion in errorlog writer
Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

lib/private/Log/Errorlog.php
@@ -25,24 +25,27 @@
 
 namespace OC\Log;
 
+use OC\SystemConfig;
 use OCP\Log\IWriter;
 
-class Errorlog implements IWriter {
+class Errorlog extends LogDetails implements IWriter {
 
 	/** @var string */
 	protected $tag;
 
-	public function __construct(string $tag = 'owncloud') {
+	public function __construct(SystemConfig $config, string $tag = 'owncloud') {
+		parent::__construct($config);
 		$this->tag = $tag;
 	}
 
 	/**
-	 * write a message in the log
+	 * Write a message in the log
+	 *
 	 * @param string $app
-	 * @param string $message
+	 * @param string|array $message
 	 * @param int $level
 	 */
 	public function write(string $app, $message, int $level) {
-		error_log('[' . $this->tag . ']['.$app.']['.$level.'] '.$message);
+		error_log('[' . $this->tag . ']['.$app.']['.$level.'] '.$this->logDetailsAsJSON($app, $message, $level));
 	}
 }

nextcloud/server

lib/private/Log/Errorlog.php
@@ -28,24 +28,27 @@ declare(strict_types=1);
 
 namespace OC\Log;
 
+use OC\SystemConfig;
 use OCP\Log\IWriter;
 
-class Errorlog implements IWriter {
+class Errorlog extends LogDetails implements IWriter {
 
 	/** @var string */
 	protected $tag;
 
-	public function __construct(string $tag = 'nextcloud') {
+	public function __construct(SystemConfig $config, string $tag = 'nextcloud') {
+		parent::__construct($config);
 		$this->tag = $tag;
 	}
 
 	/**
-	 * write a message in the log
+	 * Write a message in the log
+	 *
 	 * @param string $app
-	 * @param string $message
+	 * @param string|array $message
 	 * @param int $level
 	 */
 	public function write(string $app, $message, int $level) {
-		error_log('[' . $this->tag . ']['.$app.']['.$level.'] '.$message);
+		error_log('[' . $this->tag . ']['.$app.']['.$level.'] '.$this->logDetailsAsJSON($app, $message, $level));
 	}
 }