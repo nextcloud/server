eec1ebb84810c20e11fc483782c6ec8403bf84b2
Bennet Becker <bbecker@pks.mpg.de>
2022-12-23 13:53:20 +0100

    Updating unit test for NewUserMailHelper, to test for autosumbitted header
    Signed-off-by: Bennet Becker <bbecker@pks.mpg.de>

--- apps/settings/tests/Mailer/NewUserMailHelperTest.php
+++ apps/settings/tests/Mailer/NewUserMailHelperTest.php
@@ -42,6 +42,7 @@ use OCP\IL10N;
 use OCP\IURLGenerator;
 use OCP\IUser;
 use OCP\L10N\IFactory;
+use OCP\Mail\AutoSubmittedValue;
 use OCP\Mail\IEMailTemplate;
 use OCP\Mail\IMailer;
 use OCP\Security\ICrypto;
@@ -867,6 +868,10 @@ EOF;
 			->expects($this->once())
 			->method('useTemplate')
 			->with($emailTemplate);
+		$message
+			->expects($this->once())
+			->method('setAutoSubmitted')
+			->with(AutoSubmittedValue::AUTO_GENERATED);
 		$this->defaults
 			->expects($this->once())
 			->method('getName')
