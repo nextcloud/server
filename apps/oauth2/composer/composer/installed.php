98d06e184ddc25418b5efd6266feace4cc378b4d
Artur Neumann <artur@jankaritech.com>
2023-01-06 17:08:21 +0545

    autoloaderchecker
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- apps/oauth2/composer/composer/installed.php
+++ apps/oauth2/composer/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '144514e49e25b7b123fd535902fee97fa39fb446',
+        'reference' => 'abbf18b05fae8e7ac2ad13d45ecbf17522281ece',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '144514e49e25b7b123fd535902fee97fa39fb446',
+            'reference' => 'abbf18b05fae8e7ac2ad13d45ecbf17522281ece',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
