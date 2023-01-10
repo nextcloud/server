c5eb39b8ffa346a98426452445e2494b417cc86a
Côme Chilliet <come.chilliet@nextcloud.com>
2023-01-10 14:59:43 +0100

    Fix theming migration repair step by passing correct type for argument
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/theming/lib/Jobs/MigrateBackgroundImages.php
+++ apps/theming/lib/Jobs/MigrateBackgroundImages.php
@@ -68,10 +68,9 @@ class MigrateBackgroundImages extends QueuedJob {
 		$this->logger = $logger;
 	}
 
-	protected function run($argument): void {
-		if (!isset($argument['stage'])) {
-			// not executed in 25.0.0?!
-			$argument['stage'] = self::STAGE_PREPARE;
+	protected function run(mixed $argument): void {
+		if (!is_array($argument) || !isset($argument['stage'])) {
+			throw new \Exception('Job '.self::class.' called with wrong argument');
 		}
 
 		switch ($argument['stage']) {
@@ -99,10 +98,10 @@ class MigrateBackgroundImages extends QueuedJob {
 			$userIds = $result->fetchAll(\PDO::FETCH_COLUMN);
 			$this->storeUserIdsToProcess($userIds);
 		} catch (\Throwable $t) {
-			$this->jobList->add(self::class, self::STAGE_PREPARE);
+			$this->jobList->add(self::class, ['stage' => self::STAGE_PREPARE]);
 			throw $t;
 		}
-		$this->jobList->add(self::class, self::STAGE_EXECUTE);
+		$this->jobList->add(self::class, ['stage' => self::STAGE_EXECUTE]);
 	}
 
 	/**
