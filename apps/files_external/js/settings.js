a0f70988f426aa3c327c95537f3e62bae2461707
Vincent Petry <vincent@nextcloud.com>
2023-01-11 14:39:39 +0100

    Extend ext storage params to contain default value
    Extend the external storage configuration parameters definition to allow
    to specify a default value
    
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- apps/files_external/js/settings.js
+++ apps/files_external/js/settings.js
@@ -1054,6 +1054,14 @@ MountConfigListView.prototype = _.extend({
 			newElement = $('<input type="text" class="'+classes.join(' ')+'" data-parameter="'+parameter+'" placeholder="'+ trimmedPlaceholder+'" />');
 		}
 
+		if (placeholder.defaultValue) {
+			if (placeholder.type === MountConfigListView.ParameterTypes.BOOLEAN) {
+				newElement.find('input').prop('checked', placeholder.defaultValue);
+			} else {
+				newElement.val(placeholder.defaultValue);
+			}
+		}
+
 		if (placeholder.tooltip) {
 			newElement.attr('title', placeholder.tooltip);
 		}
