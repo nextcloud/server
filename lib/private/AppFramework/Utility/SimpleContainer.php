03c90f66ae702c77891ed5e0239bf033eba021bc
Robin Appelman <robin@icewind.nl>
2023-02-09 11:40:55 +0100

    fix phpdoc template
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/AppFramework/Utility/SimpleContainer.php
+++ lib/private/AppFramework/Utility/SimpleContainer.php
@@ -45,7 +45,7 @@ class SimpleContainer implements ArrayAccess, ContainerInterface, IContainer {
 	}
 
 	/**
-	 * @template T
+	 * @template T of object
 	 * @param ReflectionClass<T> $class the class to instantiate
 	 * @return T the created class
 	 * @suppress PhanUndeclaredClassInstanceof
