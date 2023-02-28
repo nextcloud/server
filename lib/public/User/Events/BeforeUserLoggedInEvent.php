f874239f62fbae51668d46b65326e2e1a3ad5e4e
Maxence Lange <maxence@artificial-owl.com>
2023-02-28 00:10:46 -0100

    dispatch BeforeUserLoggedInEvent
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/public/User/Events/BeforeUserLoggedInEvent.php
+++ lib/public/User/Events/BeforeUserLoggedInEvent.php
@@ -24,8 +24,10 @@ declare(strict_types=1);
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 namespace OCP\User\Events;
 
+use OCP\Authentication\IApacheBackend;
 use OCP\EventDispatcher\Event;
 
 /**
@@ -41,11 +43,13 @@ class BeforeUserLoggedInEvent extends Event {
 
 	/**
 	 * @since 18.0.0
+	 * @since 26.0.0 password can be null
 	 */
-	public function __construct(string $username, string $password) {
+	public function __construct(string $username, ?string $password, ?IApacheBackend $backend = null) {
 		parent::__construct();
 		$this->username = $username;
 		$this->password = $password;
+		$this->backend = $backend;
 	}
 
 	/**
@@ -59,8 +63,19 @@ class BeforeUserLoggedInEvent extends Event {
 
 	/**
 	 * @since 18.0.0
+	 * @since 26.0.0 value can be null
 	 */
-	public function getPassword(): string {
+	public function getPassword(): ?string {
 		return $this->password;
+	}
+
+	/**
+	 * return backend if available (or null)
+	 *
+	 * @return IApacheBackend|null
+	 * @since 26.0.0
+	 */
+	public function getBackend(): ?IApacheBackend {
+		return $this->backend;
 	}
 }
