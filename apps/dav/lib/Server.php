rotdrop/nextcloud-server

apps/dav: add some OSX specific quirks.
Signed-off-by: Claus-Justus Heine <himself@claus-justus-heine.de>

apps/dav/lib/Server.php
@@ -109,6 +109,8 @@ class Server {
 		// Add maintenance plugin
 		$this->server->addPlugin(new \OCA\DAV\Connector\Sabre\MaintenancePlugin(\OC::$server->getConfig(), \OC::$server->getL10N('dav')));
 
+		$this->server->addPlugin(new \OCA\DAV\Connector\Sabre\QuirksPlugin());
+
 		// Backends
 		$authBackend = new Auth(
 			\OC::$server->getSession(),

nextcloud/server

apps/dav/lib/Server.php
@@ -110,6 +110,8 @@ class Server {
 		// Add maintenance plugin
 		$this->server->addPlugin(new \OCA\DAV\Connector\Sabre\MaintenancePlugin(\OC::$server->getConfig(), \OC::$server->getL10N('dav')));
 
+		$this->server->addPlugin(new \OCA\DAV\Connector\Sabre\AppleQuirksPlugin());
+
 		// Backends
 		$authBackend = new Auth(
 			\OC::$server->getSession(),