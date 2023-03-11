0046cd89b28965d871bf93f75b4548133e74faf8
Robin Windey <ro.windey@gmail.com>
2023-03-11 12:18:21 +0000

    Minor improvements for devcontainer
    * Autostart apache2
    * Apply occ installation on start
    * Autostart Xdebug on request
    * Add DevContainer Xdebug profile
    
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: MohammadReza vahedi <34796044+mrvahedi68@users.noreply.github.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,3 +6,25 @@ git submodule update --init
 
 # Codespace config
 cp .devcontainer/codespace.config.php config/codespace.config.php
+
+# Set git safe.directory
+git config --global --add safe.directory /var/www/html
+
+# Onetime installation setup
+if [ ! -f "data/.devcontainer-install-complete" ]; then
+    echo "Running NC installation"
+    sudo -u www-data php occ maintenance:install \
+        --verbose \
+        --database=pgsql \
+        --database-name=postgres \
+        --database-host=127.0.0.1 \
+        --database-port=5432 \
+        --database-user=postgres \
+        --database-pass=postgres \
+        --admin-user admin \
+        --admin-pass admin && \
+    touch "data/.devcontainer-install-complete"
+fi
+
+# Clear caches and stuff ...
+service apache2 restart
