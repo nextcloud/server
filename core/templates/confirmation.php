aa8df9b05fc97c497f72b12347a2639d1bb396b9
Thomas Citharel <tcit@tcit.fr>
2023-03-20 14:38:26 +0100

    fix(provisionning_api): Remove parameters that are not set into template
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- core/templates/confirmation.php
+++ core/templates/confirmation.php
@@ -8,15 +8,12 @@
 /** @var \OCP\Defaults $theme */
 ?>
 <div class="update">
-	<form method="POST" action="<?php print_unescaped($_['targetUrl']);?>">
+	<form method="POST">
 		<h2><?php p($_['title']) ?></h2>
 		<p><?php p($_['message']) ?></p>
 		<div class="buttons">
 			<input type="submit" class="primary" value="<?php p($_['action']); ?>">
 		</div>
-		<?php foreach ($_['parameters'] as $name => $value) {?>
-			<input type="hidden" name="<?php p($name); ?>" value="<?php p($value); ?>">
-		<?php } ?>
 		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
 	</form>
 </div>
