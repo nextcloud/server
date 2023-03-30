992b65e4a62807d25757820365dc5610a5fc9b91
Arthur Schiwon <blizzz@arthur-schiwon.de>
2023-03-30 16:22:53 +0200

    fix default_language doc
    Since commit 0228bc6e66cbcb2848eacb41f1de6e7f63ebcb65 the browser
    language always is preferred.
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- config/config.sample.php
+++ config/config.sample.php
@@ -165,13 +165,14 @@ $CONFIG = [
 /**
  * This sets the default language on your Nextcloud server, using ISO_639-1
  * language codes such as ``en`` for English, ``de`` for German, and ``fr`` for
- * French. It overrides automatic language detection on public pages like login
- * or shared items. User's language preferences configured under "personal ->
- * language" override this setting after they have logged in. Nextcloud has two
- * distinguished language codes for German, 'de' and 'de_DE'. 'de' is used for
- * informal German and 'de_DE' for formal German. By setting this value to 'de_DE'
- * you can enforce the formal version of German unless the user has chosen
- * something different explicitly.
+ * French. The default_language parameter is only used, when the browser does
+ * not send any language, and the user hasn’t configured own language
+ * preferences.
+ *
+ * Nextcloud has two distinguished language codes for German, 'de' and 'de_DE'.
+ * 'de' is used for informal German and 'de_DE' for formal German. By setting
+ * this value to 'de_DE' you can enforce the formal version of German unless
+ * the user has chosen something different explicitly.
  *
  * Defaults to ``en``
  */
