dfbd1fbe79fb8201257a6c52352042586100e41e
Robin Windey <ro.windey@gmail.com>
2023-03-31 06:40:04 +0000

    Dedicated DevContainer user + NVM
    * Use dedicated DevContainer user to run Apache (ensure file permissions)
    * Install NVM for node
    
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set proper permissions and start webserver
-chown -R www-data:www-data /var/www/html && service apache2 start
+sudo service apache2 start
 
 while sleep 1000; do :; done
