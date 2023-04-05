541607349ba488d6d591ef926c9559bc33046108
julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>
2023-04-05 10:27:16 +0200

    Move heading to the main content
    Signed-off-by: julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -57,9 +57,6 @@ p($theme->getTitle());
 		</div>
 
 		<header role="banner" id="header">
-			<h1 class="hidden-visually" id="page-heading-level-1">
-				<?php p(!empty($_['pageTitle'])?$_['pageTitle']:$theme->getName()); ?>
-			</h1>
 			<div class="header-left">
 				<a href="<?php print_unescaped($_['logoUrl'] ?: link_to('', 'index.php')); ?>"
 					aria-label="<?php p($l->t('Go to %s', [$_['logoUrl'] ?: $_['defaultAppName']])); ?>"
@@ -89,6 +86,9 @@ p($theme->getTitle());
 		</form>
 
 		<main id="content" class="app-<?php p($_['appid']) ?>">
+			<h1 class="hidden-visually" id="page-heading-level-1">
+				<?php p(!empty($_['pageTitle'])?$_['pageTitle']:$theme->getName()); ?>
+			</h1>
 			<?php print_unescaped($_['content']); ?>
 		</main>
 		<div id="profiler-toolbar"></div>
