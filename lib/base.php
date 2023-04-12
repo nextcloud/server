90c53df248b862fff63e015a371232a5db9d6792
Maxence Lange <maxence@artificial-owl.com>
2023-04-12 09:34:38 -0100

    app type extended_authentication
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/base.php
+++ lib/base.php
@@ -1026,6 +1026,7 @@ class OC {
 
 		// Always load authentication apps
 		OC_App::loadApps(['authentication']);
+		OC_App::loadApps(['extended_authentication']);
 
 		// Load minimum set of apps
 		if (!\OCP\Util::needUpgrade()
