829b79984b24b437533f78d3ff51796ba37c1d12
Robin Appelman <robin@icewind.nl>
2023-04-21 17:25:18 +0200

    add command to get a file from the filesystem
    Signed-off-by: Robin Appelman <robin@icewind.nl>

---
+++ core/Command/Info/Get.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2023 Robin Appelman <robin@icewind.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OC\Core\Command\Info;
+
+
+use OCP\Files\File;
+use OCP\Util;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Get extends Command {
+	private FileUtils $fileUtils;
+
+	public function __construct(FileUtils $fileUtils) {
+		$this->fileUtils = $fileUtils;
+		parent::__construct();
+	}
+
+	protected function configure(): void {
+		$this
+			->setName('info:file:get')
+			->setDescription('Get the contents of a file')
+			->addArgument('file', InputArgument::REQUIRED, "File id or path")
+			->addOption('output', 'o', InputOption::VALUE_REQUIRED, "Target file to output to");
+	}
+
+	public function execute(InputInterface $input, OutputInterface $output): int {
+		$fileInput = $input->getArgument('file');
+		$outputName = $input->getOption('output');
+		$node = $this->fileUtils->getNode($fileInput);
+
+		if ($node instanceof File) {
+			$isTTY = stream_isatty(STDOUT);
+			if ($outputName === null && $isTTY && $node->getMimePart() !== 'text') {
+				$output->writeln([
+					"<error>Warning: Binary output can mess up your terminal</error>",
+					"         Use '--output STDOUT' to output it to the terminal anyway",
+					"         Or '--output <FILE>' to save to a file instead"
+				]);
+				return 1;
+			}
+			$source = $node->fopen('r');
+			$target = (!$outputName || strtolower($outputName) === 'stdout') ? STDOUT : fopen($outputName, 'w');
+			stream_copy_to_stream($source, $target);
+			return 0;
+		} else {
+			$output->writeln("<error>$fileInput is a directory</error>");
+			return 1;
+		}
+	}
+
+}
