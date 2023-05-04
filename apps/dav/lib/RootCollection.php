f42cae2bdb55c18686c4fc58ff13db89845508f0
Arthur Schiwon <blizzz@arthur-schiwon.de>
2023-05-04 21:58:08 +0200

    chore: polish SystemTagsInUseCollection
    - DI SystemTagManager
    - add some comments and doc
    - catch NoUserException
    - add return type hints
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/dav/lib/RootCollection.php
+++ apps/dav/lib/RootCollection.php
@@ -134,10 +134,7 @@ class RootCollection extends SimpleCollection {
 			$groupManager,
 			\OC::$server->getEventDispatcher()
 		);
-		$systemTagInUseCollection = new SystemTag\SystemTagsInUseCollection(
-			$userSession,
-			$rootFolder
-		);
+		$systemTagInUseCollection = \OCP\Server::get(SystemTag\SystemTagsInUseCollection::class);
 		$commentsCollection = new Comments\RootCollection(
 			\OC::$server->getCommentsManager(),
 			$userManager,
