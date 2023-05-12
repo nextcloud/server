6daf962b4cf3c480801f9042679951ba735154f4
Daniel Calviño Sánchez <danxuliu@gmail.com>
2023-05-12 17:51:13 +0200

    Split long line
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/settings/templates/settings/admin/sharing.php
+++ apps/settings/templates/settings/admin/sharing.php
@@ -123,7 +123,8 @@
 <?php if ($_['passwordExcludedGroupsFeatureEnabled']) { ?>
 			<span id="selectPasswordsExcludedGroups" class="double-indent <?php if (!$_['enforceLinkPassword']) { p('hidden'); } ?>">
 				<label for="shareapi_enforce_links_password_excluded_groups"><?php p($l->t('Exclude groups from password requirements:'));?></label><br/>
-				<input name="shareapi_enforce_links_password_excluded_groups" id="passwordsExcludedGroups" value="<?php p($_['passwordExcludedGroups']) ?>" style="width: 400px" class="noJSAutoUpdate double-indent"/>
+				<input name="shareapi_enforce_links_password_excluded_groups" id="passwordsExcludedGroups" value="<?php p($_['passwordExcludedGroups']) ?>"
+					   style="width: 400px" class="noJSAutoUpdate double-indent"/>
 			</span><br/>
 <?php } ?>
 
