095b43f3964f2bf0489507c4a52915134992817a
Julius Härtl <jus@bitgrid.net>
2023-05-16 07:43:00 +0200

    feat: Add occ user:sync-account-data for updating oc_accounts information from user backends
    This can be useful in cases where the state between user backend and
    oc_accounts has become inconsistent.
    
    Usually the account data is updated once the change on the user backend
    is detected. Potential leftovers from older bugs
    (https://github.com/nextcloud/user_saml/pull/582) might though never get
    updated. This could lead to the contacts menu never showing the correct
    display name. The contacts menu is read from the system address book,
    which is only updated from oc_accounts.
    
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- core/register_command.php
+++ core/register_command.php
@@ -192,6 +192,7 @@ if (\OC::$server->getConfig()->getSystemValue('installed', false)) {
 	$application->add(new OC\Core\Command\User\Setting(\OC::$server->getUserManager(), \OC::$server->getConfig()));
 	$application->add(new OC\Core\Command\User\ListCommand(\OC::$server->getUserManager(), \OC::$server->getGroupManager()));
 	$application->add(new OC\Core\Command\User\Info(\OC::$server->getUserManager(), \OC::$server->getGroupManager()));
+	$application->add(new OC\Core\Command\User\SyncAccountDataCommand(\OC::$server->getUserManager(), \OC::$server->get(\OCP\Accounts\IAccountManager::class)));
 	$application->add(new OC\Core\Command\User\AddAppPassword(\OC::$server->get(\OCP\IUserManager::class), \OC::$server->get(\OC\Authentication\Token\IProvider::class), \OC::$server->get(\OCP\Security\ISecureRandom::class), \OC::$server->get(\OCP\EventDispatcher\IEventDispatcher::class)));
 
 	$application->add(new OC\Core\Command\Group\Add(\OC::$server->getGroupManager()));
