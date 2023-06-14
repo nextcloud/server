fd6990a33b5ca01f082944ecd379246e3406aab2
Julius Härtl <jus@bitgrid.net>
2023-06-14 14:39:15 +0200

    fix: Only read unencrypted_size when file is actually encrypted
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/Files/FileInfo.php
+++ lib/private/Files/FileInfo.php
@@ -205,7 +205,7 @@ class FileInfo implements \OCP\Files\FileInfo, \ArrayAccess {
 		if ($includeMounts) {
 			$this->updateEntryfromSubMounts();
 
-			if (isset($this->data['unencrypted_size']) && $this->data['unencrypted_size'] > 0) {
+			if ($this->isEncrypted() && isset($this->data['unencrypted_size']) && $this->data['unencrypted_size'] > 0) {
 				return $this->data['unencrypted_size'];
 			} else {
 				return isset($this->data['size']) ? 0 + $this->data['size'] : 0;
@@ -227,7 +227,7 @@ class FileInfo implements \OCP\Files\FileInfo, \ArrayAccess {
 	 * @return bool
 	 */
 	public function isEncrypted() {
-		return $this->data['encrypted'];
+		return $this->data['encrypted'] ?? false;
 	}
 
 	/**
