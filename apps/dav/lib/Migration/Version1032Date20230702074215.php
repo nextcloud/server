e8d684d91cc69ac2ff649545acda0b5952dfd02b
Thomas Citharel <tcit@tcit.fr>
2023-06-30 11:30:27 +0200

    fix(dav): change type of propertyvalue column to blob
    The propertyvalue column can contain null 0x00 characters values because of serializing PHP objects since https://github.com/nextcloud/server/pull/30368. This truncates data in text fields, but not blob fields. We start by removing invalid value and altering the column to match the new type.
    
    That's what Sabre PDO's being doing in the first place 🙈
    
    Closes #37754
    
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

---
+++ apps/dav/lib/Migration/Version1032Date20230702074215.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\DAV\Migration;
+
+use Closure;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version1032Date20230702074215 extends SimpleMigrationStep {
+
+	public function __construct(protected IDBConnection $connection) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		$propertiesTable = $schema->getTable('properties');
+		$propertiesTable->addColumn('propertyvalue', Types::BLOB, [
+			'notnull' => false
+		]);
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+		if ($this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+			$this->connection->executeStatement('UPDATE `*prefix*properties` SET `propertyvalue` = propertyvaluenew::bytea');
+		} else {
+			$query = $this->connection->getQueryBuilder();
+			$query->update('properties')
+				->set('propertyvalue', 'propertyvaluenew')
+				->executeStatement();
+		}
+	}
+}
---
+++ apps/dav/lib/Migration/Version1032Date20230702074341.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\DAV\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version1032Date20230702074341 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		$propertiesTable = $schema->getTable('properties');
+		$propertiesTable->changeColumn('propertyvalue',  [
+			'notnull' => true
+		]);
+		$propertiesTable->dropColumn('propertyvaluenew');
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+}
