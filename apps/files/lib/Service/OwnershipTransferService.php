df0a396b4cfcd0834eb056a0c4190d65c8496ae7
Hamid Dehnavi <hamid.dev.pro@gmail.com>
2023-07-07 04:45:34 +0330

    Refactor "substr" calls to improve code readability
    Signed-off-by: Hamid Dehnavi <hamid.dev.pro@gmail.com>

--- apps/files/lib/Service/OwnershipTransferService.php
+++ apps/files/lib/Service/OwnershipTransferService.php
@@ -447,7 +447,7 @@ class OwnershipTransferService {
 		$progress = new ProgressBar($output, count($sourceShares));
 		$prefix = "$destinationUid/files";
 		$finalShareTarget = '';
-		if (substr($finalTarget, 0, strlen($prefix)) === $prefix) {
+		if (str_starts_with($finalTarget, $prefix)) {
 			$finalShareTarget = substr($finalTarget, strlen($prefix));
 		}
 		foreach ($sourceShares as $share) {
@@ -457,7 +457,7 @@ class OwnershipTransferService {
 				if (trim($path, '/') !== '') {
 					$pathToCheck = '/' . trim($path) . '/';
 				}
-				if (substr($share->getTarget(), 0, strlen($pathToCheck)) !== $pathToCheck) {
+				if (!str_starts_with($share->getTarget(), $pathToCheck)) {
 					continue;
 				}
 				$shareTarget = $share->getTarget();
