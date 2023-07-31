43ac71adeeeabfc62382635332ad358912398c8c
Christopher Ng <chrng8@gmail.com>
2023-07-31 12:10:50 -0700

    feat: init files_reminders migration
    Signed-off-by: Christopher Ng <chrng8@gmail.com>
    (cherry picked from commit e16001b8eb6b5f87dfba57cca94adb88cf6b1d45)

---
+++ apps/files_reminders/lib/Db/Reminder.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2023 Christopher Ng <chrng8@gmail.com>
+ *
+ * @author Christopher Ng <chrng8@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\FilesReminders\Db;
+
+use OCP\AppFramework\Db\Entity;
+
+/**
+ * @method void setUserId(string $userId)
+ * @method string getUserId()
+ *
+ * @method void setFileId(int $fileId)
+ * @method int getFileId()
+ *
+ * @method void setRemindAt(int $remindAt)
+ * @method int getRemindAt()
+ *
+ * @method void setNotified(bool $notified)
+ * @method bool getNotified()
+ */
+class Reminder extends Entity {
+	protected string $userId;
+	protected int $fileId;
+	protected int $remindAt;
+	protected bool $notified = false;
+
+	public function __construct() {
+		$this->addType('userId', 'string');
+		$this->addType('fileId', 'integer');
+		$this->addType('remindAt', 'integer');
+		$this->addType('notified', 'boolean');
+	}
+}
