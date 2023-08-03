d1e3c0278d1ef4d59c835ad727a42f3c0369b3c8
Jordan Brown <code@jore.cc>
2023-08-03 15:30:45 +1000

    feat(occ): Add --all option for occ user:lastseen
    Signed-off-by: Jordan Brown <code@jore.cc>

--- core/Command/User/LastSeen.php
+++ core/Command/User/LastSeen.php
@@ -31,6 +31,7 @@ use OCP\IUserManager;
 use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class LastSeen extends Base {
@@ -40,33 +41,38 @@ class LastSeen extends Base {
 		parent::__construct();
 	}
 
-	protected function configure() {
+	protected function configure(): void {
 		$this
 			->setName('user:lastseen')
 			->setDescription('shows when the user was logged in last time')
 			->addArgument(
 				'uid',
-				InputArgument::REQUIRED,
+				InputArgument::OPTIONAL,
 				'the username'
-			);
+			)
+			->addOption(
+				'all',
+				null,
+				InputOption::VALUE_NONE,
+				'shows a list of when all users were last logged in'
+			)
+		;
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output): int {
-		$user = $this->userManager->get($input->getArgument('uid'));
-		if (is_null($user)) {
-			$output->writeln('<error>User does not exist</error>');
-			return 1;
-		}
-
-		$lastLogin = $user->getLastLogin();
-		if ($lastLogin === 0) {
-			$output->writeln('Account ' . $user->getUID() .
-				' has never logged in, yet.');
+	protected function execute(InputInterface $input, OutputInterface $output): int	{
+		$singleUserId = $input->getArgument('uid');
+		if ($singleUserId) {
+			$user = $this->userManager->get($singleUserId);
+			if (is_null($user)) {
+				$output->writeln('<error>User does not exist</error>');
+				return 1;
+			}
+			$users = [$user];
+		} elseif ($input->getOption('all')) {
+			$users = $this->userManager->search('');
 		} else {
-			$date = new \DateTime();
-			$date->setTimestamp($lastLogin);
-			$output->writeln($user->getUID() .
-				'`s last login: ' . $date->format('d.m.Y H:i'));
+			$output->writeln("<error>Please specify a username, or \"--all\" to list all</error>");
+			return 1;
 		}
 		return 0;
 	}
