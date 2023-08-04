0131005066a64e94a695af4434d3dc700853ede9
Christopher Ng <chrng8@gmail.com>
2023-08-04 09:22:58 -0700

    fix: load script
    Signed-off-by: Christopher Ng <chrng8@gmail.com>
    (cherry picked from commit 89692cf8a1dac636302b7918580960f6a8a1d690)

--- apps/files_reminders/lib/Listener/LoadAdditionalScriptsListener.php
+++ apps/files_reminders/lib/Listener/LoadAdditionalScriptsListener.php
@@ -28,13 +28,22 @@ namespace OCA\FilesReminders\Listener;
 
 use OCA\Files\Event\LoadAdditionalScriptsEvent;
 use OCA\FilesReminders\AppInfo\Application;
+use OCP\App\IAppManager;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\Util;
 
 class LoadAdditionalScriptsListener implements IEventListener {
+	public function __construct(
+		private IAppManager $appManager,
+	) {}
+
 	public function handle(Event $event): void {
 		if (!($event instanceof LoadAdditionalScriptsEvent)) {
+			return;
+		}
+
+		if (!$this->appManager->isEnabledForUser('notifications')) {
 			return;
 		}
 
