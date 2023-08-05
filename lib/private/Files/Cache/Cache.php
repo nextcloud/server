7b7d1e3c0911639cba4c20eba7d0a8352091643f
John Molakvoæ <skjnldsv@users.noreply.github.com>
2023-08-05 12:05:36 +0200

    fix: simplify `sourceData` check
    Co-authored-by: Git'Fellow <12234510+solracsf@users.noreply.github.com>
    Signed-off-by: John Molakvoæ <skjnldsv@users.noreply.github.com>

--- lib/private/Files/Cache/Cache.php
+++ lib/private/Files/Cache/Cache.php
@@ -668,7 +668,7 @@ class Cache implements ICache {
 			$targetPath = $this->normalize($targetPath);
 
 			$sourceData = $sourceCache->get($sourcePath);
-			if ($sourceData === false || $sourceData === null) {
+			if (!$sourceData) {
 				throw new \Exception('Invalid source storage path: ' . $sourcePath);
 			}
 
