15ea32c1e1e25c0e2bb9b0997fc97c282ef853b5
Ferdinand Thiessen <opensource@fthiessen.de>
2023-08-16 14:12:40 +0200

    fix: Prevent PHP warnings when optional CacheEntry attributes are unset
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/Files/Cache/Cache.php
+++ lib/private/Files/Cache/Cache.php
@@ -149,7 +149,7 @@ class Cache implements ICache {
 	 * get the stored metadata of a file or folder
 	 *
 	 * @param string | int $file either the path of a file or folder or the file id for a file or folder
-	 * @return ICacheEntry|false the cache entry as array of false if the file is not found in the cache
+	 * @return ICacheEntry|false the cache entry as array or false if the file is not found in the cache
 	 */
 	public function get($file) {
 		$query = $this->getQueryBuilder();
