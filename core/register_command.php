f7bf468e22e74be7e00d3ffa2356ce4212f12760
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-08-24 11:26:27 -0300

    Use autowiring when registering commands
    Co-authored-by: Joas Schilling <213943+nickvergessen@users.noreply.github.com>
    Signed-off-by: Lucas Azevedo <lhs_azevedo@hotmail.com>

--- core/register_command.php
+++ core/register_command.php
@@ -193,8 +193,8 @@ if (\OC::$server->getConfig()->getSystemValue('installed', false)) {
 	$application->add(new OC\Core\Command\User\ListCommand(\OC::$server->getUserManager(), \OC::$server->getGroupManager()));
 	$application->add(new OC\Core\Command\User\Info(\OC::$server->getUserManager(), \OC::$server->getGroupManager()));
 	$application->add(new OC\Core\Command\User\AddAppPassword(\OC::$server->get(\OCP\IUserManager::class), \OC::$server->get(\OC\Authentication\Token\IProvider::class), \OC::$server->get(\OCP\Security\ISecureRandom::class), \OC::$server->get(\OCP\EventDispatcher\IEventDispatcher::class)));
-	$application->add(new OC\Core\Command\User\AuthTokens(\OC::$server->get(\OCP\IUserManager::class), \OC::$server->get(\OC\Authentication\Token\IProvider::class)));
-	$application->add(new OC\Core\Command\User\DeleteAuthToken(\OC::$server->get(\OC\Authentication\Token\IProvider::class)));
+	$application->add(\OC::$server->get(\OC\Core\Command\User\AuthTokens::class));
+	$application->add(\OC::$server->get(\OC\Core\Command\User\DeleteAuthToken::class));
 
 	$application->add(new OC\Core\Command\Group\Add(\OC::$server->getGroupManager()));
 	$application->add(new OC\Core\Command\Group\Delete(\OC::$server->getGroupManager()));
