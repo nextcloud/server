ddab01cfd869dabb25d5a2f787316463df80afa4
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2023-08-25 21:06:36 +0200

    add AVIF
    Signed-off-by: JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>

--- lib/composer/composer/autoload_classmap.php
+++ lib/composer/composer/autoload_classmap.php
@@ -1506,6 +1506,7 @@ return array(
     'OC\\Preview\\Watcher' => $baseDir . '/lib/private/Preview/Watcher.php',
     'OC\\Preview\\WatcherConnector' => $baseDir . '/lib/private/Preview/WatcherConnector.php',
     'OC\\Preview\\WebP' => $baseDir . '/lib/private/Preview/WebP.php',
+    'OC\\Preview\\AVIF' => $baseDir . '/lib/private/Preview/AVIF.php',
     'OC\\Preview\\XBitmap' => $baseDir . '/lib/private/Preview/XBitmap.php',
     'OC\\Profile\\Actions\\EmailAction' => $baseDir . '/lib/private/Profile/Actions/EmailAction.php',
     'OC\\Profile\\Actions\\FediverseAction' => $baseDir . '/lib/private/Profile/Actions/FediverseAction.php',
