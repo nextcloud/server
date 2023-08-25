bb75dab93a639b5fabb1f13b587c22719be95da8
Josh Richards <josh.t.richards@gmail.com>
2023-08-25 09:20:27 -0400

    (enh) Change occ background-job:list limit default 10->100
    The default limit of 10 seems too conservative. Nearly all environments >100 and most <10. At the same time, this higher default limit still remains reasonable to avoid problems in really big environments.
    
    Signed-off-by: Josh Richards <josh.t.richards@gmail.com>

--- core/Command/Background/ListCommand.php
+++ core/Command/Background/ListCommand.php
@@ -53,7 +53,7 @@ class ListCommand extends Base {
 				'l',
 				InputOption::VALUE_OPTIONAL,
 				'Number of jobs to retrieve',
-				'10'
+				'100'
 			)->addOption(
 				'offset',
 				'o',
@@ -67,6 +67,7 @@ class ListCommand extends Base {
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
 		$jobs = $this->jobList->getJobsIterator($input->getOption('class'), (int)$input->getOption('limit'), (int)$input->getOption('offset'));
+		$output->writeln("Output is currently limited to first " .  $input->getOption('limit') . " jobs. Specify `-l, --limit[=LIMIT]` to override.");
 		$this->writeTableInOutputFormat($input, $output, $this->formatJobs($jobs));
 		return 0;
 	}
