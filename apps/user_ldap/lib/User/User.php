b0f849312c71297329ad53ff45424ea70457e47c
Marcel Klehr <mklehr@gmx.net>
2023-09-05 11:24:54 +0200

    LDAP: Increase profile picture limit to 512

--- apps/user_ldap/lib/User/User.php
+++ apps/user_ldap/lib/User/User.php
@@ -570,7 +570,7 @@ class User {
 
 
 		//make sure it is a square and not bigger than 128x128
-		$size = min([$this->image->width(), $this->image->height(), 128]);
+		$size = min([$this->image->width(), $this->image->height(), 512]);
 		if (!$this->image->centerCrop($size)) {
 			$this->logger->error('croping image for avatar failed for '.$this->dn, ['app' => 'user_ldap']);
 			return false;
