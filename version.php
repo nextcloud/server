58fdbded2f296448a9e1b042bee57008bc1bba79
Arthur Schiwon <blizzz@arthur-schiwon.de>
2023-09-08 09:38:29 +0200

    27.1.0 RC3
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [27, 1, 0, 4];
+$OC_Version = [27, 1, 0, 5];
 
 // The human-readable string
-$OC_VersionString = '27.1.0 RC2';
+$OC_VersionString = '27.1.0 RC3';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
