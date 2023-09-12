500374a8e7528b81e882d43c06b625839208474d
Côme Chilliet <come.chilliet@nextcloud.com>
2023-09-12 16:03:40 +0200

    Fix registerEventListener signature
    It seems now psalm correctly supports this.
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/public/AppFramework/Bootstrap/IRegistrationContext.php
+++ lib/public/AppFramework/Bootstrap/IRegistrationContext.php
@@ -129,7 +129,7 @@ interface IRegistrationContext {
 	 * @param string $event preferably the fully-qualified class name of the Event sub class to listen for
 	 * @psalm-param string|class-string<T> $event preferably the fully-qualified class name of the Event sub class to listen for
 	 * @param string $listener fully qualified class name (or ::class notation) of a \OCP\EventDispatcher\IEventListener that can be built by the DI container
-	 * @psalm-param class-string<\OCP\EventDispatcher\IEventListener> $listener fully qualified class name that can be built by the DI container
+	 * @psalm-param class-string<\OCP\EventDispatcher\IEventListener<T>> $listener fully qualified class name that can be built by the DI container
 	 * @param int $priority The higher this value, the earlier an event
 	 *                      listener will be triggered in the chain (defaults to 0)
 	 *
