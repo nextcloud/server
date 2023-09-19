cf2b9e83531ea8e69076dc932543e8fdbdf95a29
fenn-cs <fenn25.fn@gmail.com>
2023-09-19 11:57:43 +0100

    Consider share type for enforced share expiry dates
    We have three kind of share expiry date enforcements.
    
    - `isDefaultExpireDateEnforced`
    - `isDefaultInternalExpireDateEnforced`
    - `isDefaultRemoteExpireDateEnforced`
    
    Before these commit, `isExpiryDateEnforced` that is used to
    disable/enable the set expiry date checkbox does not take those into consideration
    which is problematic as those have different applications.
    
    In addition, this commit now uses `isExpiryDateEnforced` to disable/enable the input for
    expiry date that shows up before the creation of link shares.
    
    Here, `hasExpirationDate` is also removed from 'SharingEntryLink` component as it is
    not used.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -360,7 +360,13 @@ export default {
 		 */
 		hasExpirationDate: {
 			get() {
-				return !!this.share.expireDate || this.config.isDefaultInternalExpireDateEnforced
+				if (this.isPublicShare) {
+					return !!this.share.expireDate || this.config.isDefaultExpireDateEnforced
+				}
+				if (this.isRemoteShare) {
+					return !!this.share.expireDate || this.config.isDefaultInternalExpireDateEnforced || this.config.isDefaultExpireDateEnforced
+				}
+				return !!this.share.expireDate || this.config.isDefaultInternalExpireDateEnforced || this.config.isDefaultExpireDateEnforced
 			},
 			set(enabled) {
 				this.share.expireDate = enabled
@@ -393,7 +399,7 @@ export default {
 			return this.fileInfo.type === 'dir'
 		},
 		dateMaxEnforced() {
-			if (!this.isRemote && this.config.isDefaultInternalExpireDateEnforced) {
+			if (!this.isRemoteShare && this.config.isDefaultInternalExpireDateEnforced) {
 				return new Date(new Date().setDate(new Date().getDate() + 1 + this.config.defaultInternalExpireDate))
 			} else if (this.config.isDefaultRemoteExpireDateEnforced) {
 				return new Date(new Date().setDate(new Date().getDate() + 1 + this.config.defaultRemoteExpireDate))
@@ -421,9 +427,6 @@ export default {
 		},
 		isPasswordEnforced() {
 			return this.isPublicShare && this.config.enforcePasswordForPublicLink
-		},
-		isExpiryDateEnforced() {
-			return this.config.isDefaultInternalExpireDateEnforced
 		},
 		defaultExpiryDate() {
 			if ((this.isGroupShare || this.isUserShare) && this.config.isDefaultInternalExpireDateEnabled) {
