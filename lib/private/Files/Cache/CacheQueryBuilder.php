a75a93af8e140809abe50429601cb3d07f35245c
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2023-09-25 15:07:29 +0200

    feat(files): Allow advanced search for files
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- lib/private/Files/Cache/CacheQueryBuilder.php
+++ lib/private/Files/Cache/CacheQueryBuilder.php
@@ -71,7 +71,7 @@ class CacheQueryBuilder extends QueryBuilder {
 	public function selectFileCache(string $alias = null, bool $joinExtendedCache = true) {
 		$name = $alias ?: 'filecache';
 		$this->select("$name.fileid", 'storage', 'path', 'path_hash', "$name.parent", "$name.name", 'mimetype', 'mimepart', 'size', 'mtime',
-			'storage_mtime', 'encrypted', 'etag', 'permissions', 'checksum', 'unencrypted_size')
+			'storage_mtime', 'encrypted', 'etag', "$name.permissions", 'checksum', 'unencrypted_size')
 			->from('filecache', $name);
 
 		if ($joinExtendedCache) {
--- lib/private/Files/Cache/SearchBuilder.php
+++ lib/private/Files/Cache/SearchBuilder.php
@@ -192,6 +192,8 @@ class SearchBuilder {
 		} elseif ($field === 'path' && $type === ISearchComparison::COMPARE_EQUAL && $operator->getQueryHint(ISearchComparison::HINT_PATH_EQ_HASH, true)) {
 			$field = 'path_hash';
 			$value = md5((string)$value);
+		} elseif ($field === 'owner') {
+			$field = 'uid_owner';
 		}
 		return [$field, $value, $type];
 	}
@@ -208,6 +210,9 @@ class SearchBuilder {
 			'favorite' => 'boolean',
 			'fileid' => 'integer',
 			'storage' => 'integer',
+			'share_with' => 'string',
+			'share_type' => 'integer',
+			'owner' => 'string',
 		];
 		$comparisons = [
 			'mimetype' => ['eq', 'like'],
@@ -220,6 +225,9 @@ class SearchBuilder {
 			'favorite' => ['eq'],
 			'fileid' => ['eq'],
 			'storage' => ['eq'],
+			'share_with' => ['eq'],
+			'share_type' => ['eq'],
+			'owner' => ['eq'],
 		];
 
 		if (!isset($types[$operator->getField()])) {
