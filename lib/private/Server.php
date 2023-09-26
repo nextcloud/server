961ccee5f6cd08d8e06aea7f2e4e432cdb7d88e0
Côme Chilliet <come.chilliet@nextcloud.com>
2023-09-26 11:42:37 +0200

    Make it clear in the deprecation comment that DI is preferred
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -1724,7 +1724,7 @@ class Server extends ServerContainer implements IServerContainer {
 	 * @param string $app appid
 	 * @param string $lang
 	 * @return IL10N
-	 * @deprecated 20.0.0 use \OCP\Util::getL10N instead
+	 * @deprecated 20.0.0 use DI of IL10N instead, or \OCP\Util::getL10N as a last resort
 	 */
 	public function getL10N($app, $lang = null) {
 		return $this->get(IFactory::class)->get($app, $lang);
