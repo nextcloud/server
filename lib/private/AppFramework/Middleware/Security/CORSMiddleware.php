4623fd12d329bd346f624335e9fb0e99374b974e
Alexander Piskun <bigcat88@icloud.com>
2023-10-06 13:46:37 +0300

    fixed Drone test
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/private/AppFramework/Middleware/Security/CORSMiddleware.php
+++ lib/private/AppFramework/Middleware/Security/CORSMiddleware.php
@@ -39,6 +39,7 @@ use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\Response;
 use OCP\AppFramework\Middleware;
 use OCP\IRequest;
+use OCP\ISession;
 use ReflectionMethod;
 
 /**
@@ -98,7 +99,7 @@ class CORSMiddleware extends Middleware {
 				return;
 			}
 			// Skip CORS check for requests with AppAPI auth.
-			if ($this->session->getSession()->get('app_api') === true) {
+			if ($this->session->getSession() instanceof ISession && $this->session->getSession()->get('app_api') === true) {
 				return;
 			}
 			$this->session->logout();
