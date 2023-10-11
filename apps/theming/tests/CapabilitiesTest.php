e69e201af81c777aa75c739d55f0c2fe3d53236f
jld3103 <jld3103yt@gmail.com>
2023-10-11 19:01:59 +0200

    fix(theming): Correctly expose user and admin theming
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- apps/theming/tests/CapabilitiesTest.php
+++ apps/theming/tests/CapabilitiesTest.php
@@ -36,6 +36,7 @@ use OCP\App\IAppManager;
 use OCP\Files\IAppData;
 use OCP\IConfig;
 use OCP\IURLGenerator;
+use OCP\IUserSession;
 use Test\TestCase;
 
 /**
@@ -56,6 +57,8 @@ class CapabilitiesTest extends TestCase {
 	/** @var Util|\PHPUnit\Framework\MockObject\MockObject */
 	protected $util;
 
+	protected IUserSession $userSession;
+
 	/** @var Capabilities */
 	protected $capabilities;
 
@@ -66,7 +69,8 @@ class CapabilitiesTest extends TestCase {
 		$this->url = $this->getMockBuilder(IURLGenerator::class)->getMock();
 		$this->config = $this->createMock(IConfig::class);
 		$this->util = $this->createMock(Util::class);
-		$this->capabilities = new Capabilities($this->theming, $this->util, $this->url, $this->config);
+		$this->userSession = $this->createMock(IUserSession::class);
+		$this->capabilities = new Capabilities($this->theming, $this->util, $this->url, $this->config, $this->userSession);
 	}
 
 	public function dataGetCapabilities() {
@@ -165,13 +169,13 @@ class CapabilitiesTest extends TestCase {
 			->method('getSlogan')
 			->willReturn($slogan);
 		$this->theming->expects($this->atLeast(1))
-			->method('getColorPrimary')
+			->method('getDefaultColorPrimary')
 			->willReturn($color);
 		$this->theming->expects($this->exactly(3))
 			->method('getLogo')
 			->willReturn($logo);
 		$this->theming->expects($this->once())
-			->method('getTextColorPrimary')
+			->method('getDefaultTextColorPrimary')
 			->willReturn($textColor);
 
 		$util = new Util($this->config, $this->createMock(IAppManager::class), $this->createMock(IAppData::class), $this->createMock(ImageManager::class));
