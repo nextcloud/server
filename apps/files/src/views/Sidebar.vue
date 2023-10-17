154a9e3d33d83d234d78e1c68e34b49ad7f86364
Varun Patil <varunpatil@ucla.edu>
2023-10-16 21:04:53 -0700

    sidebar: allow turning off tags view by default
    With #37065, there is no way to hide the tags from the sidebar
    
    by default when they are not relevant or redundant (e.g. the tab
    
    may already show the file's tags). This can be annyoing especially
    
    when the file has many tags. This patch adds an option to hide
    
    the tags from the sidebar by default (the user can still open
    
    the tags tab manually).
    
    This also reduces one request when opening the sidebar when the
    
    tags are turned off, since all tags don't need to be fetched
    
    anymore.
    
    Signed-off-by: Varun Patil <varunpatil@ucla.edu>
    
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>
    
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- apps/files/src/views/Sidebar.vue
+++ apps/files/src/views/Sidebar.vue
@@ -36,7 +36,7 @@
 		<!-- TODO: create a standard to allow multiple elements here? -->
 		<template v-if="fileInfo" #description>
 			<div class="sidebar__description">
-				<SystemTags v-if="isSystemTagsEnabled"
+				<SystemTags v-if="isSystemTagsEnabled && showTagsDefault"
 					v-show="showTags"
 					:file-id="fileInfo.id"
 					@has-tags="value => showTags = value" />
@@ -138,6 +138,7 @@ export default {
 			// reactive state
 			Sidebar: OCA.Files.Sidebar.state,
 			showTags: false,
+			showTagsDefault: true,
 			error: null,
 			loading: true,
 			fileInfo: null,
@@ -455,7 +456,7 @@ export default {
 		 * Toggle the tags selector
 		 */
 		toggleTags() {
-			this.showTags = !this.showTags
+			this.showTagsDefault = this.showTags = !this.showTags
 		},
 
 		/**
@@ -527,6 +528,15 @@ export default {
 				document.querySelector('#content')?.classList.remove('with-sidebar--full')
 					|| document.querySelector('#content-vue')?.classList.remove('with-sidebar--full')
 			}
+		},
+
+		/**
+		 * Allow to set whether tags should be shown by default from OCA.Files.Sidebar
+		 *
+		 * @param {boolean} showTagsDefault - Whether or not to show the tags by default.
+		 */
+		setShowTagsDefault(showTagsDefault) {
+			this.showTagsDefault = showTagsDefault
 		},
 
 		/**
