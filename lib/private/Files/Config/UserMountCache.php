8b1b5dff65b85ba869b0b1ccb4986279fc3193ad
Robin Appelman <robin@icewind.nl>
2023-10-19 17:46:21 +0200

    disable test only filter in usermountcache
    lets see what breaks
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Files/Config/UserMountCache.php
+++ lib/private/Files/Config/UserMountCache.php
@@ -29,7 +29,6 @@
 namespace OC\Files\Config;
 
 use OCP\Cache\CappedMemoryCache;
-use OCA\Files_Sharing\SharedMount;
 use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\Diagnostics\IEventLogger;
 use OCP\Files\Config\ICachedMountFileInfo;
@@ -78,10 +77,6 @@ class UserMountCache implements IUserMountCache {
 
 	public function registerMounts(IUser $user, array $mounts, array $mountProviderClasses = null) {
 		$this->eventLogger->start('fs:setup:user:register', 'Registering mounts for user');
-		// filter out non-proper storages coming from unit tests
-		$mounts = array_filter($mounts, function (IMountPoint $mount) {
-			return $mount instanceof SharedMount || ($mount->getStorage() && $mount->getStorage()->getCache());
-		});
 		/** @var ICachedMountInfo[] $newMounts */
 		$newMounts = array_map(function (IMountPoint $mount) use ($user) {
 			// filter out any storages which aren't scanned yet since we aren't interested in files from those storages (yet)
