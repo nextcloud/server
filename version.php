64324aac504434751f4bc6e2222032e69a96fc4a
John Molakvoæ <skjnldsv@protonmail.com>
2023-10-19 13:52:09 +0200

    27.1.3 RC2
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [27, 1, 3, 0];
+$OC_Version = [27, 1, 3, 1];
 
 // The human-readable string
-$OC_VersionString = '27.1.3 RC1';
+$OC_VersionString = '27.1.3 RC2';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
