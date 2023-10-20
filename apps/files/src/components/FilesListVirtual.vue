4d578f37e6b7e9f296236d3937fd4caf962d31d8
Varun Patil <varunpatil@ucla.edu>
2023-10-20 09:33:28 -0700

    fix(files): correct item height
    1. grid: typo? The icon size is 160 - 44 + (44 / 2) = 138px
    2. non-grid: border is already included in height because the tr
       is a border-box (made this explicit to be safe)
    
    Signed-off-by: Varun Patil <varunpatil@ucla.edu>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- apps/files/src/components/FilesListVirtual.vue
+++ apps/files/src/components/FilesListVirtual.vue
@@ -366,6 +366,7 @@ export default Vue.extend({
 			width: 100%;
 			user-select: none;
 			border-bottom: 1px solid var(--color-border);
+			box-sizing: border-box;
 			user-select: none;
 			height: var(--row-height);
 		}
