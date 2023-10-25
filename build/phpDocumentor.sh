c1a7ee6e16ba9f905bf43bafdc289c64de31683c
John Molakvoæ <skjnldsv@users.noreply.github.com>
2023-10-25 08:29:08 +0200

    fix: use default php version
    Signed-off-by: John Molakvoæ <skjnldsv@users.noreply.github.com>

--- build/phpDocumentor.sh
+++ build/phpDocumentor.sh
@@ -4,4 +4,4 @@ wget https://phpdoc.org/phpDocumentor.phar
 
 mkdir -p api/
 
-php7.4 phpDocumentor.phar -t "./api" -d "./lib/public" --title="Nextcloud PHP API ($BRANCH)"
+php phpDocumentor.phar -t "./api" -d "./lib/public" --title="Nextcloud PHP API ($BRANCH)"
