4c7bd978e181e1076bd61346c280fb4912834bbd
John Molakvoæ <skjnldsv@protonmail.com>
2023-10-25 08:33:38 +0200

    fix(build): phpDocumentor usage
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- build/phpDocumentor.sh
+++ build/phpDocumentor.sh
@@ -4,4 +4,4 @@ wget https://phpdoc.org/phpDocumentor.phar
 
 mkdir -p api/
 
-php phpDocumentor.phar -t "./api" -d "./lib/public" --title="Nextcloud PHP API ($BRANCH)"
+php phpDocumentor.phar --target=./api --directory=./lib/public --title="Nextcloud PHP API ($BRANCH)"
