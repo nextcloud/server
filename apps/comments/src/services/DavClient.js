b03fd6e363ef75ec69c5150b8992a838f724ce45
Varun Patil <varunpatil@ucla.edu>
2023-10-30 10:52:08 -0700

    fix(dav): multiple fixes in usage of webdav library
    1. Refresh token on update
    2. Fix some very weird imports
    3. Patch fetch instead of request to prevent accessing impl details
    
    Signed-off-by: Varun Patil <varunpatil@ucla.edu>

--- apps/comments/src/services/DavClient.js
+++ apps/comments/src/services/DavClient.js
@@ -22,16 +22,23 @@
 
 import { createClient } from 'webdav'
 import { getRootPath } from '../utils/davUtils.js'
-import { getRequestToken } from '@nextcloud/auth'
+import { getRequestToken, onRequestTokenUpdate } from '@nextcloud/auth'
 
 // init webdav client
-const client = createClient(getRootPath(), {
-	headers: {
-		// Add this so the server knows it is an request from the browser
-		'X-Requested-With': 'XMLHttpRequest',
-		// Inject user auth
-		requesttoken: getRequestToken() ?? '',
-	},
-})
+const client = createClient(getRootPath())
+
+// set CSRF token header
+const setHeaders = (token) => {
+  client.setHeaders({
+    // Add this so the server knows it is an request from the browser
+    'X-Requested-With': 'XMLHttpRequest',
+    // Inject user auth
+    requesttoken: token ?? '',
+  })
+}
+
+// refresh headers when request token changes
+onRequestTokenUpdate(setHeaders)
+setHeaders(getRequestToken())
 
 export default client
