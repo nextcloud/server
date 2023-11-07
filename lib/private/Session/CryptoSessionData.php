bcd7d59a20183c76164ceab59366bde1d3a74cff
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-11-07 09:13:48 +0100

    fix(session): Do not log fresh/empty session as error
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/private/Session/CryptoSessionData.php
+++ lib/private/Session/CryptoSessionData.php
@@ -32,6 +32,7 @@ namespace OC\Session;
 use OCP\ISession;
 use OCP\Security\ICrypto;
 use OCP\Session\Exceptions\SessionNotAvailableException;
+use function json_decode;
 use function OCP\Log\logger;
 
 /**
@@ -80,19 +81,24 @@ class CryptoSessionData implements \ArrayAccess, ISession {
 
 	protected function initializeSession() {
 		$encryptedSessionData = $this->session->get(self::encryptedSessionName) ?: '';
-		try {
-			$this->sessionValues = json_decode(
-				$this->crypto->decrypt($encryptedSessionData, $this->passphrase),
-				true,
-				512,
-				JSON_THROW_ON_ERROR,
-			);
-		} catch (\Exception $e) {
-			logger('core')->critical('Could not decrypt or decode encrypted session data', [
-				'exception' => $e,
-			]);
+		if ($encryptedSessionData === '') {
+			// Nothing to decrypt
 			$this->sessionValues = [];
-			$this->regenerateId(true, false);
+		} else {
+			try {
+				$this->sessionValues = json_decode(
+					$this->crypto->decrypt($encryptedSessionData, $this->passphrase),
+					true,
+					512,
+					JSON_THROW_ON_ERROR,
+				);
+			} catch (\Exception $e) {
+				logger('core')->critical('Could not decrypt or decode encrypted session data', [
+					'exception' => $e,
+				]);
+				$this->sessionValues = [];
+				$this->regenerateId(true, false);
+			}
 		}
 	}
 
