960b95bb8ec1c4e93eb4bad030b9fe5907431ad0
Joas Schilling <coding@schilljs.com>
2023-11-09 06:59:42 +0100

    fix(migration): Fix files metadata migration
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [28, 0, 0, 3];
+$OC_Version = [28, 0, 0, 4];
 
 // The human-readable string
 $OC_VersionString = '28.0.0 beta 1';
