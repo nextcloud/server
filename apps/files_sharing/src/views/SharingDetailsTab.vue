6329da9352536fb6dfa3777eaae8e50ecb366d92
Christopher Ng <chrng8@gmail.com>
2023-11-16 16:05:00 -0800

    fix(a11y): Focus on sharing quick permission radio
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -16,7 +16,8 @@
 			</span>
 		</div>
 		<div class="sharingTabDetailsView__wrapper">
-			<div class="sharingTabDetailsView__quick-permissions">
+			<div ref="quickPermissions"
+				class="sharingTabDetailsView__quick-permissions">
 				<div>
 					<NcCheckboxRadioSwitch :button-variant="true"
 						:checked.sync="sharingPermission"
@@ -669,6 +670,10 @@ export default {
 		this.initializeAttributes()
 		console.debug('shareSentIn', this.share)
 		console.debug('config', this.config)
+	},
+
+	mounted() {
+		this.$refs.quickPermissions?.querySelector('input:checked')?.focus()
 	},
 
 	methods: {
