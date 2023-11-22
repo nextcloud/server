15f6ac6fddd0e9a3b841aa3ac51e435513e2b1bd
Tamino Bauknecht <dev@tb6.eu>
2023-11-22 21:28:10 +0100

    SymlinkManager: Introduce symlink table via database migration
    Signed-off-by: Tamino Bauknecht <dev@tb6.eu>

---
+++ core/Migrations/SymlinkSupportMigration.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2023 Tamino Bauknecht <dev@tb6.eu>
+ *
+ * @author Tamino Bauknecht <dev@tb6.eu>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OC\Core\Migrations;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Introduce symlinks table
+ */
+class SymlinkSupportMigration extends SimpleMigrationStep {
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('symlinks')) {
+			$table = $schema->createTable('symlinks');
+
+			$table->addColumn('id', Types::BIGINT, [
+				'notnull' => true,
+				'length' => 64,
+				'autoincrement' => true,
+			]);
+			$table->addColumn('storage', Types::BIGINT, [
+				'notnull' => true,
+				'length' => 20,
+				'default' => 0,
+			]);
+			$table->addColumn('path', Types::STRING, [
+				'notnull' => true,
+				'length' => 4000,
+			]);
+			$table->addColumn('name', Types::STRING, [
+				'notnull' => false,
+				'length' => 255,
+			]);
+			$table->addColumn('last_updated', Types::INTEGER, [
+				'notnull' => false,
+				'length' => 4,
+				'default' => 0,
+				'unsigned' => true,
+			]);
+
+			$table->setPrimaryKey(['id'], 'symlinks_id_index');
+			$table->addUniqueIndex(['path'], 'symlinks_path_index');
+			$table->addIndex(['last_updated'], 'symlinks_updated');
+
+			return $schema;
+		}
+
+		return null;
+	}
+}
