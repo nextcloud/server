9d86a0444daaa6b98fa9013c42609a6d1b042173
fenn-cs <fenn25.fn@gmail.com>
2023-11-29 15:06:51 +0100

    Improve share logic for enforced password & expiry date
    * It's possible for the admin to enforce and expiry date after, some
     shares have been created. This commit makes possible to update the
     share with the new admin constraints.
    
    * This commit would users to modify enforced expiry to anything within
     range and less than the enforced limit in the pre-create dialog for public
     shares.
    
    * This commit fixes, unable to update share without updating password.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -78,8 +78,8 @@
 				</div>
 			</div>
 			<div class="sharingTabDetailsView__advanced-control">
-				<NcButton type="tertiary"
-					id="advancedSectionAccordionAdvancedControl"
+				<NcButton id="advancedSectionAccordionAdvancedControl"
+					type="tertiary"
 					alignment="end-reverse"
 					aria-controls="advancedSectionAccordionAdvanced"
 					:aria-expanded="advancedControlExpandedValue"
@@ -91,8 +91,11 @@
 					</template>
 				</NcButton>
 			</div>
-			<div v-if="advancedSectionAccordionExpanded" id="advancedSectionAccordionAdvanced" class="sharingTabDetailsView__advanced"
-				aria-labelledby="advancedSectionAccordionAdvancedControl" role="region">
+			<div v-if="advancedSectionAccordionExpanded"
+				id="advancedSectionAccordionAdvanced"
+				class="sharingTabDetailsView__advanced"
+				aria-labelledby="advancedSectionAccordionAdvancedControl"
+				role="region">
 				<section>
 					<NcInputField v-if="isPublicShare"
 						:value.sync="share.label"
@@ -427,19 +430,6 @@ export default {
 		isFolder() {
 			return this.fileInfo.type === 'dir'
 		},
-		maxExpirationDateEnforced() {
-			if (this.isExpiryDateEnforced) {
-				if (this.isPublicShare) {
-					return this.config.defaultExpirationDate
-				}
-				if (this.isRemoteShare) {
-					return this.config.defaultRemoteExpirationDateString
-				}
-				// If it get's here then it must be an internal share
-				return this.config.defaultInternalExpirationDate
-			}
-			return null
-		},
 		/**
 		 * @return {boolean}
 		 */
@@ -477,9 +467,6 @@ export default {
 		},
 		isGroupShare() {
 			return this.share.type === this.SHARE_TYPES.SHARE_TYPE_GROUP
-		},
-		isRemoteShare() {
-			return this.share.type === this.SHARE_TYPES.SHARE_TYPE_REMOTE_GROUP || this.share.type === this.SHARE_TYPES.SHARE_TYPE_REMOTE
 		},
 		isNewShare() {
 			return this.share.id === null || this.share.id === undefined
@@ -654,7 +641,7 @@ export default {
 		},
 		advancedControlExpandedValue() {
 			return this.advancedSectionAccordionExpanded ? 'true' : 'false'
-		}
+		},
 	},
 	watch: {
 		setCustomPermissions(isChecked) {
@@ -731,6 +718,12 @@ export default {
 				return
 			}
 
+			// If there is an enforced expiry date, then existing shares created before enforcement
+			// have no expiry date, hence we set it here.
+			if (!this.isValidShareAttribute(this.share.expireDate) && this.isExpiryDateEnforced) {
+				this.hasExpirationDate = true
+			}
+
 			if (
 				this.isValidShareAttribute(this.share.password)
 				|| this.isValidShareAttribute(this.share.expireDate)
@@ -786,16 +779,12 @@ export default {
 			if (!this.writeNoteToRecipientIsChecked) {
 				this.share.note = ''
 			}
-
 			if (this.isPasswordProtected) {
-				if (this.isValidShareAttribute(this.share.newPassword)) {
+				if (this.hasUnsavedPassword && this.isValidShareAttribute(this.share.newPassword)) {
 					this.share.password = this.share.newPassword
 					this.$delete(this.share, 'newPassword')
-				} else {
-					if (this.isPasswordEnforced) {
-						this.passwordError = true
-						return
-					}
+				} else if (this.isPasswordEnforced && !this.isValidShareAttribute(this.share.password)) {
+					this.passwordError = true
 				}
 			} else {
 				this.share.password = ''
