869935f8efd56b6af5e5c917f956d6b6a7507038
Ferdinand Thiessen <opensource@fthiessen.de>
2023-11-30 20:37:46 +0100

    fix(cypress): Adjust user columns tests to be more specific when selecting a modal
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- cypress/e2e/settings/users_columns.cy.ts
+++ cypress/e2e/settings/users_columns.cy.ts
@@ -38,7 +38,7 @@ describe('Settings: Show and hide columns', function() {
 		// reset all visibility toggles
 		cy.get('.modal-container #settings-section_visibility-settings input[type="checkbox"]').uncheck({ force: true })
 
-		cy.get('.modal-container').within(() => {
+		cy.contains('.modal-container', 'User management settings').within(() => {
 			// enable the last login toggle
 			cy.get('[data-test="showLastLogin"] input[type="checkbox"]').check({ force: true })
 			// close the settings dialog
@@ -59,7 +59,7 @@ describe('Settings: Show and hide columns', function() {
 		// open the settings dialog
 		cy.get('.app-navigation-entry__settings').contains('User management settings').click()
 
-		cy.get('.modal-container').within(() => {
+		cy.contains('.modal-container', 'User management settings').within(() => {
 			// enable the language toggle
 			cy.get('[data-test="showLanguages"] input[type="checkbox"]').should('not.be.checked')
 			cy.get('[data-test="showLanguages"] input[type="checkbox"]').check({ force: true })
@@ -90,7 +90,7 @@ describe('Settings: Show and hide columns', function() {
 		// open the settings dialog
 		cy.get('.app-navigation-entry__settings').contains('User management settings').click()
 
-		cy.get('.modal-container').within(() => {
+		cy.contains('.modal-container', 'User management settings').within(() => {
 			// disable the last login toggle
 			cy.get('[data-test="showLastLogin"] input[type="checkbox"]').should('be.checked')
 			cy.get('[data-test="showLastLogin"] input[type="checkbox"]').uncheck({ force: true })
@@ -98,7 +98,7 @@ describe('Settings: Show and hide columns', function() {
 			// close the settings dialog
 			cy.get('button.modal-container__close').click()
 		})
-		cy.waitUntil(() => cy.get('.modal-container').should(el => assertNotExistOrNotVisible(el)))
+		cy.waitUntil(() => cy.contains('.modal-container', 'User management settings').should(el => assertNotExistOrNotVisible(el)))
 
 		// see that the last login column is not in the header
 		cy.get('[data-cy-user-list-header-last-login]').should('not.exist')
