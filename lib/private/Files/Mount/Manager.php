nguyenduongnhj/server-stable26

add more checks to ensure mounts aren't empty
Signed-off-by: Robin Appelman <robin@icewind.nl>

lib/private/Files/Mount/Manager.php
@@ -99,6 +99,15 @@ class Manager implements IMountManager {
 			return $this->pathCache[$path];
 		}
 
+
+
+		if (count($this->mounts) === 0) {
+			$this->setupManager->setupRoot();
+			if (count($this->mounts) === 0) {
+				throw new \Exception("No mounts even after explitcly setting up the root mounts");
+			}
+		}
+
 		$current = $path;
 		while (true) {
 			$mountPoint = $current . '/';
@@ -115,7 +124,7 @@ class Manager implements IMountManager {
 			}
 		}
 
-		throw new NotFoundException("No mount for path " . $path . " existing mounts: " . implode(",", array_keys($this->mounts)));
+		throw new NotFoundException("No mount for path " . $path . " existing mounts (" . count($this->mounts) ."): " . implode(",", array_keys($this->mounts)));
 	}
 
 	/**

nextcloud/server

lib/private/Files/Mount/Manager.php
@@ -101,6 +101,15 @@ class Manager implements IMountManager {
 			return $this->pathCache[$path];
 		}
 
+
+
+		if (count($this->mounts) === 0) {
+			$this->setupManager->setupRoot();
+			if (count($this->mounts) === 0) {
+				throw new \Exception("No mounts even after explicitly setting up the root mounts");
+			}
+		}
+
 		$current = $path;
 		while (true) {
 			$mountPoint = $current . '/';
@@ -117,7 +126,7 @@ class Manager implements IMountManager {
 			}
 		}
 
-		throw new NotFoundException("No mount for path " . $path . " existing mounts: " . implode(",", array_keys($this->mounts)));
+		throw new NotFoundException("No mount for path " . $path . " existing mounts (" . count($this->mounts) ."): " . implode(",", array_keys($this->mounts)));
 	}
 
 	/**