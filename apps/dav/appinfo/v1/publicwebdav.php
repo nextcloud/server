c6e3dc6217c6ec21b9e9761306df874cd01f0796
Florian Steffens <florian.steffens@nextcloud.com>
2023-12-05 09:37:14 +0100

    fix(webdav): multiple headers check

--- apps/dav/appinfo/v1/publicwebdav.php
+++ apps/dav/appinfo/v1/publicwebdav.php
@@ -73,7 +73,8 @@ $linkCheckPlugin = new \OCA\DAV\Files\Sharing\PublicLinkCheckPlugin();
 $filesDropPlugin = new \OCA\DAV\Files\Sharing\FilesDropPlugin();
 
 $server = $serverFactory->createServer($baseuri, $requestUri, $authPlugin, function (\Sabre\DAV\Server $server) use ($authBackend, $linkCheckPlugin, $filesDropPlugin) {
-	$isAjax = (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest');
+	$request = \OC::$server->getRequest();
+	$isAjax = (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && in_array('XMLHttpRequest', explode(',', $request->getHeader('X-Requested-With'))));
 	/** @var \OCA\FederatedFileSharing\FederatedShareProvider $shareProvider */
 	$federatedShareProvider = \OC::$server->query(\OCA\FederatedFileSharing\FederatedShareProvider::class);
 	if ($federatedShareProvider->isOutgoingServer2serverShareEnabled() === false && !$isAjax) {
