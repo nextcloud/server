0d6255bafc26be922c2adb7e99e1acd0b6805515
Ferdinand Thiessen <opensource@fthiessen.de>
2023-12-05 19:03:58 +0100

    fix: Update `@nextcloud/dialogs` to 5.0.3 fixing files copy move
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ dist/923-923.js
@@ -0,0 +1,3 @@
+/*! For license information please see 923-923.js.LICENSE.txt */
+"use strict";(self.webpackChunknextcloud=self.webpackChunknextcloud||[]).push([[923],{90923:(e,n,c)=>{c.d(n,{FilePickerVue:()=>s});const s=(0,c(20144).defineAsyncComponent)((()=>Promise.all([c.e(7874),c.e(8321)]).then(c.bind(c,94542))))}}]);
+//# sourceMappingURL=923-923.js.map?v=2686613ba9d88601b889
