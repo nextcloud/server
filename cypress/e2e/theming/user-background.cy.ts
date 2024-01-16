6df488df212757e246032f4eef972cfe47603c44
Grigorii K. Shartsev <me@shgk.me>
2024-01-16 18:40:33 +0100

    test(theming): update e2e tests after separating color button and preview
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- cypress/e2e/theming/user-background.cy.ts
+++ cypress/e2e/theming/user-background.cy.ts
@@ -168,7 +168,7 @@ describe('User select a bright custom color and remove background', function() {
 		cy.intercept('*/apps/theming/background/color').as('setColor')
 
 		// Pick one of the bright color preset
-		cy.get('button').contains('Change color').click()
+		cy.contains('button', 'Change color').click()
 		cy.get('.color-picker__simple-color-circle:eq(4)').click()
 
 		// Validate custom colour change
@@ -286,7 +286,7 @@ describe('User changes settings and reload the page', function() {
 	it('Select a custom color', function() {
 		cy.intercept('*/apps/theming/background/color').as('setColor')
 
-		cy.get('button').contains('Change color').click()
+		cy.contains('button', 'Change color').click()
 		cy.get('.color-picker__simple-color-circle:eq(5)').click()
 
 		// Validate clear background
