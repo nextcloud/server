61adb716fe1efbb2fab113791b58787390902a8b
fenn-cs <fenn25.fn@gmail.com>
2024-01-22 14:49:57 +0100

    Consider admin defaults when creating shares
    The current share logic always uses the default `BUNDLED_PERMISSIONS.ALL`
    which includes everything.
    
    This commit updates share creation logic to use `defaultPermissions` if set
    by admin for the creation of new shares.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- apps/files_sharing/src/services/ConfigService.js
+++ apps/files_sharing/src/services/ConfigService.js
@@ -32,6 +32,17 @@ export default class Config {
 	}
 
 	/**
+	 * Get default share permissions, if any
+	 *
+	 * @return {boolean}
+	 * @readonly
+	 * @memberof Config
+	 */
+	 get defaultPermissions() {
+		return this._shareConfig.defaultPermissions
+	}
+
+	/**
 	 * Is public upload allowed on link shares ?
 	 *
 	 * @return {boolean}
