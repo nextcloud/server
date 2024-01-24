8362025643bdbe6c93ce4b3c33e61c2c370039fc
Maxence Lange <maxence@artificial-owl.com>
2024-01-24 20:00:18 -0100

    better indexes
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- core/Migrations/Version29000Date20240124132202.php
+++ core/Migrations/Version29000Date20240124132202.php
@@ -34,39 +34,20 @@ use OCP\Migration\SimpleMigrationStep;
 /**
  * Create new fields for type and lazy loading in appconfig for the new IAppConfig API.
  */
-class Version29000Date20231126110901 extends SimpleMigrationStep {
+class Version29000Date20240124132202 extends SimpleMigrationStep {
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
 		$table = $schema->getTable('appconfig');
-		if ($table->hasColumn('lazy')) {
-			return null;
-		}
+		$table->addColumn('lazy', Types::SMALLINT, ['notnull' => true, 'default' => 0, 'length' => 1, 'unsigned' => true]);
 
 		/**
-		 * This code is now useless, after a discussion about boolean on oracle;
-		 * it has been decided to use another type for the lazy field
-		 *
-		 * a better migration process is available there:
-		 *
-		 * @see Version29000Date20240124132201 for the revert of current migration
-		 * @see Version29000Date20240124132202 for the new migration process
+		 * we only need an index on lazy, the group 'appid'+'configkey' already
+		 * exists as primary ({@see Version13000Date20170718121200})
 		 */
-		return null;
+		$table->addIndex(['lazy'], 'ac_lazy_i');
 
-		//		// type=2 means value is typed as MIXED
-		//		$table->addColumn('type', Types::INTEGER, ['notnull' => true, 'default' => 2]);
-		//		$table->addColumn('lazy', Types::BOOLEAN, ['notnull' => false, 'default' => false]);
-		//
-		//		if ($table->hasIndex('appconfig_config_key_index')) {
-		//			$table->dropIndex('appconfig_config_key_index');
-		//		}
-		//
-		//		$table->addIndex(['lazy'], 'ac_lazy_i');
-		//		$table->addIndex(['appid', 'lazy'], 'ac_app_lazy_i');
-		//		$table->addIndex(['appid', 'lazy', 'configkey'], 'ac_app_lazy_key_i');
-		//
-		//		return $schema;
+		return $schema;
 	}
 }
