02432159f5e065f43b608ceb38df5513fd221efa
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-26 14:56:12 +0100

    chore: Compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ dist/4720-4720.js
@@ -0,0 +1,3 @@
+/*! For license information please see 4720-4720.js.LICENSE.txt */
+"use strict";(self.webpackChunknextcloud=self.webpackChunknextcloud||[]).push([[4720],{84720:(e,n,c)=>{c.d(n,{FilePickerVue:()=>s});const s=(0,c(20144).defineAsyncComponent)((()=>Promise.all([c.e(7874),c.e(9847)]).then(c.bind(c,94378))))}}]);
+//# sourceMappingURL=4720-4720.js.map?v=3579bbfdda4bbff89d5b
