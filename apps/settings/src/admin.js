398b042af79f3be8e65a1f142c362da4318fdf7c
Ferdinand Thiessen <opensource@fthiessen.de>
2024-02-15 02:08:04 +0100

    enh(settings): Migrate WOFF2 loading check to a SetupCheck
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/settings/src/admin.js
+++ apps/settings/src/admin.js
@@ -110,10 +110,9 @@ window.addEventListener('DOMContentLoaded', () => {
 			OC.SetupChecks.checkProviderUrl(OC.getRootPath() + '/ocs-provider/', OC.theme.docPlaceholderUrl, $('#postsetupchecks').data('check-wellknown') === true),
 			OC.SetupChecks.checkSetup(),
 			OC.SetupChecks.checkGeneric(),
-			OC.SetupChecks.checkWOFF2Loading(OC.filePath('core', '', 'fonts/NotoSans-Regular-latin.woff2'), OC.theme.docPlaceholderUrl),
 			OC.SetupChecks.checkDataProtected(),
-		).then((check1, check2, check3, check4, check5, check6, check7, check8, check9, check10, check11) => {
-			const messages = [].concat(check1, check2, check3, check4, check5, check6, check7, check8, check9, check10, check11)
+		).then((check1, check2, check3, check4, check5, check6, check7, check8, check9, check10) => {
+			const messages = [].concat(check1, check2, check3, check4, check5, check6, check7, check8, check9, check10)
 			const $el = $('#postsetupchecks')
 			$('#security-warning-state-loading').addClass('hidden')
 
