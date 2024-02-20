14bdc11deeaee97cac24c8c80622bf81fc34c2db
Ferdinand Thiessen <opensource@fthiessen.de>
2024-02-20 13:08:45 +0100

    feat(settings): Refactor routing code to use named router views
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ apps/settings/src/router/routes.ts
@@ -0,0 +1,52 @@
+import type { RouteConfig } from 'vue-router'
+
+import { defineAsyncComponent } from 'vue'
+
+// Dynamic loading
+const AppStore = defineAsyncComponent(() => import(/* webpackChunkName: 'settings-apps-view' */'../views/AppStore.vue'))
+const AppStoreNavigation = defineAsyncComponent(() => import(/* webpackChunkName: 'settings-apps-view' */'../views/AppStoreNavigation.vue'))
+const AppstoreSidebar = defineAsyncComponent(() => import(/* webpackChunkName: 'settings-apps-view' */'../views/AppstoreSidebar.vue'))
+
+const UserManagement = defineAsyncComponent(() => import(/* webpackChunkName: 'settings-users' */'../views/UserManagement.vue'))
+const UserManagementNavigation = defineAsyncComponent(() => import(/* webpackChunkName: 'settings-users' */'../views/UserManagementNavigation.vue'))
+
+const routes: RouteConfig[] = [
+	{
+		name: 'users',
+		path: '/:index(index.php/)?settings/users',
+		components: {
+			default: UserManagement,
+			navigation: UserManagementNavigation,
+		},
+		props: true,
+		children: [
+			{
+				path: ':selectedGroup',
+				name: 'group',
+			},
+		],
+	},
+	{
+		path: '/:index(index.php/)?settings/apps',
+		name: 'apps',
+		components: {
+			default: AppStore,
+			navigation: AppStoreNavigation,
+			sidebar: AppstoreSidebar,
+		},
+		children: [
+			{
+				path: ':category',
+				name: 'apps-category',
+				children: [
+					{
+						path: ':id',
+						name: 'apps-details',
+					},
+				],
+			},
+		],
+	},
+]
+
+export default routes
