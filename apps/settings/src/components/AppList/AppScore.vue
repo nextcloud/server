53d7d64b12850cfdc8b6eecf40a0ddad13255693
Ferdinand Thiessen <opensource@fthiessen.de>
2024-02-22 16:41:17 +0100

    feat: Make the AppScorce component to use SVG icons
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/settings/src/components/AppList/AppScore.vue
+++ apps/settings/src/components/AppList/AppScore.vue
@@ -1,42 +1,88 @@
 <!--
-  - @copyright Copyright (c) 2018 Julius Härtl <jus@bitgrid.net>
-  -
-  - @author Julius Härtl <jus@bitgrid.net>
-  -
-  - @license GNU AGPL version 3 or any later version
-  -
-  - This program is free software: you can redistribute it and/or modify
-  - it under the terms of the GNU Affero General Public License as
-  - published by the Free Software Foundation, either version 3 of the
-  - License, or (at your option) any later version.
-  -
-  - This program is distributed in the hope that it will be useful,
-  - but WITHOUT ANY WARRANTY; without even the implied warranty of
-  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  - GNU Affero General Public License for more details.
-  -
-  - You should have received a copy of the GNU Affero General Public License
-  - along with this program. If not, see <http://www.gnu.org/licenses/>.
-  -
-  -->
+	- @copyright Copyright (c) 2018 Julius Härtl <jus@bitgrid.net>
+	-
+	- @author Julius Härtl <jus@bitgrid.net>
+	-
+	- @license GNU AGPL version 3 or any later version
+	-
+	- This program is free software: you can redistribute it and/or modify
+	- it under the terms of the GNU Affero General Public License as
+	- published by the Free Software Foundation, either version 3 of the
+	- License, or (at your option) any later version.
+	-
+	- This program is distributed in the hope that it will be useful,
+	- but WITHOUT ANY WARRANTY; without even the implied warranty of
+	- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	- GNU Affero General Public License for more details.
+	-
+	- You should have received a copy of the GNU Affero General Public License
+	- along with this program. If not, see <http://www.gnu.org/licenses/>.
+	-
+	-->
 
 <template>
-	<img :src="scoreImage" :alt="t('settings', 'Rating: {score}/10', {score:appScore})" class="app-score-image">
+	<span role="img"
+		:aria-label="title"
+		:title="title"
+		class="app-score__wrapper">
+		<NcIconSvgWrapper v-for="index in fullStars"
+			:key="`full-star-${index}`"
+			:path="mdiStar" />
+		<NcIconSvgWrapper v-if="hasHalfStar" :path="mdiStarHalfFull" />
+		<NcIconSvgWrapper v-for="index in emptyStars"
+			:key="`empty-star-${index}`"
+			:path="mdiStarOutline" />
+	</span>
 </template>
-<script>
-import { imagePath } from '@nextcloud/router'
+<script lang="ts">
+import NcIconSvgWrapper from '@nextcloud/vue/dist/Components/NcIconSvgWrapper.js'
+import { mdiStar, mdiStarHalfFull, mdiStarOutline } from '@mdi/js'
+import { translate as t } from '@nextcloud/l10n'
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
 	name: 'AppScore',
-	props: ['score'],
-	computed: {
-		appScore() {
-			return Math.round(this.score * 10)
+	components: {
+		NcIconSvgWrapper,
+	},
+	props: {
+		score: {
+			type: Number,
+			required: true,
 		},
-		scoreImage() {
-			const imageName = 'rating/s' + this.appScore + '.svg'
-			return imagePath('core', imageName)
+	},
+	setup() {
+		return {
+			mdiStar,
+			mdiStarHalfFull,
+			mdiStarOutline,
 		}
 	},
-}
+	computed: {
+		title() {
+			const appScore = (this.score * 5).toFixed(1)
+			return t('settings', 'Community rating: {score}/5', { score: appScore })
+		},
+		fullStars() {
+			return Math.floor(this.score * 5 + 0.25)
+		},
+		emptyStars() {
+			return Math.min(Math.floor((1 - this.score) * 5 + 0.25), 5 - this.fullStars)
+		},
+		hasHalfStar() {
+			return (this.fullStars + this.emptyStars) < 5
+		},
+	},
+})
 </script>
+<style scoped>
+.app-score__wrapper {
+	display: inline-flex;
+	color: #a08b00;
+	> * {
+		min-width: fit-content;
+		min-height: fit-content;
+		vertical-align: text-bottom;
+	}
+}
+</style>
