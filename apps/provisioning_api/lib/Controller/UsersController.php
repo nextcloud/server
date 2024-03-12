77d2d3cf53b9005fadac8740472aaf620503b6d7
Christopher Ng <chrng8@gmail.com>
2024-03-11 17:21:46 -0700

    fix(settings): Fix always empty disabled users list for subadmins
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- apps/provisioning_api/lib/Controller/UsersController.php
+++ apps/provisioning_api/lib/Controller/UsersController.php
@@ -256,7 +256,7 @@ class UsersController extends AUserData {
 						fn (IUser $user): string => $user->getUID(),
 						array_filter(
 							$group->searchUsers('', ($tempLimit === null ? null : $tempLimit - count($users))),
-							fn (IUser $user): bool => $user->isEnabled()
+							fn (IUser $user): bool => !$user->isEnabled()
 						)
 					)
 				);
