a57f9caaeb29ea6e3f30334063dccb0a3e2ca128
provokateurin <kate@provokateurin.de>
2024-03-14 13:06:53 +0100

    docs(AppFramework): Document CSRF check requirements
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/AppFramework/Http/Request.php
+++ lib/private/AppFramework/Http/Request.php
@@ -451,8 +451,10 @@ class Request implements \ArrayAccess, \Countable, IRequest {
 
 
 	/**
-	 * Checks if the CSRF check was correct
-	 * @return bool true if CSRF check passed
+	 * Checks if the request passes the CSRF checks.
+	 *
+	 * A request must always pass the strict cookie check, unless it has the OCS-APIRequest set or no session (@see cookieCheckRequired).
+	 * If the OCS-APIRequest is set or a valid CSRF token is sent the request will to pass.
 	 */
 	public function passesCSRFCheck(): bool {
 		if ($this->csrfTokenManager === null) {
