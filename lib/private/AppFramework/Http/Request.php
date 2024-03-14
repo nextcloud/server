9d1705259c832fdfde48a04dcf538d1ecb7c7007
provokateurin <kate@provokateurin.de>
2024-03-14 13:06:32 +0100

    fix(AppFramework): Allow requests with OCS-APIRequest header to pass CSRF checks
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/AppFramework/Http/Request.php
+++ lib/private/AppFramework/Http/Request.php
@@ -426,6 +426,10 @@ class Request implements \ArrayAccess, \Countable, IRequest {
 			return false;
 		}
 
+		if ($this->getHeader('OCS-APIRequest') !== '') {
+			return true;
+		}
+
 		if (isset($this->items['get']['requesttoken'])) {
 			$token = $this->items['get']['requesttoken'];
 		} elseif (isset($this->items['post']['requesttoken'])) {
