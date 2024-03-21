2525e73caacd272ca0e7833f9a5b60d15cd1bfea
Ferdinand Thiessen <opensource@fthiessen.de>
2024-03-21 18:59:36 +0100

    fix(DB): Sanitize `host` parameter for postgres databases when IPv6 address is passed
    Doctrine is using `pg_connect` with the `host` parameter, this does not allow IPv6 addresses in URI notation.
    So we need to extract the IP address and pass it directly
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/DB/ConnectionFactory.php
+++ lib/private/DB/ConnectionFactory.php
@@ -128,6 +128,15 @@ class ConnectionFactory {
 		$eventManager->addEventSubscriber(new SetTransactionIsolationLevel());
 		$additionalConnectionParams = array_merge($this->createConnectionParams(), $additionalConnectionParams);
 		switch ($normalizedType) {
+			case 'pgsql':
+				// pg_connect used by Doctrine DBAL does not support URI notation (enclosed in brackets)
+				$matches = [];
+				if (preg_match('/^\[([^\]]+)\]$/', $additionalConnectionParams['host'], $matches)) {
+					// Host variable carries a port or socket.
+					$additionalConnectionParams['host'] = $matches[1];
+				}
+				break;
+
 			case 'oci':
 				$eventManager->addEventSubscriber(new OracleSessionInit);
 				// the driverOptions are unused in dbal and need to be mapped to the parameters
