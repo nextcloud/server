4df1af40e34c713bba0cb656a2fb7fefcfdf3a2e
invario <67800603+invario@users.noreply.github.com>
2024-04-08 15:46:09 -0400

    Update lib/private/Preview/Imaginary.php
    Co-authored-by: Daniel <mail@danielkesselberg.de>
    Signed-off-by: invario <67800603+invario@users.noreply.github.com>

--- lib/private/Preview/Imaginary.php
+++ lib/private/Preview/Imaginary.php
@@ -76,7 +76,7 @@ class Imaginary extends ProviderV2 {
 		}
 		$imaginaryUrl = rtrim($imaginaryUrl, '/');
 
-		// Set to true if Imaginary was configured with "-mount" with the NextCloud data directory, else set to false
+		// Set to true if Imaginary was configured with "-mount" with the Nextcloud data directory, else set to false
 		$locallymounted = $this->config->getSystemValueString('preview_imaginary_locally_mounted',false);
 		if (!$locallymounted) {
 			// Object store, needed if data directory is not locally mounted 
