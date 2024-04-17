54d7caaec9f22862af28749e58a5ce8d6b0b5bee
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2024-04-17 09:49:45 +0200

    build(hub): 29.0.0 RC5
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [29, 0, 0, 17];
+$OC_Version = [29, 0, 0, 18];
 
 // The human-readable string
-$OC_VersionString = '29.0.0 RC4';
+$OC_VersionString = '29.0.0 RC5';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
