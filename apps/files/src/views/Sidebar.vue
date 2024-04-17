3c74559e4ce81ba5b0427125c67c5d31c33c0376
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-17 17:03:21 +0200

    fix(files): Close sidebar if shown node is deleted
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files/src/views/Sidebar.vue
+++ apps/files/src/views/Sidebar.vue
@@ -105,7 +105,7 @@
 import { getCurrentUser } from '@nextcloud/auth'
 import { getCapabilities } from '@nextcloud/capabilities'
 import { showError } from '@nextcloud/dialogs'
-import { emit } from '@nextcloud/event-bus'
+import { emit, subscribe, unsubscribe } from '@nextcloud/event-bus'
 import { File, Folder, formatFileSize } from '@nextcloud/files'
 import { encodePath } from '@nextcloud/paths'
 import { generateRemoteUrl, generateUrl } from '@nextcloud/router'
@@ -304,10 +304,13 @@ export default {
 		},
 	},
 	created() {
+		subscribe('files:node:deleted', this.onNodeDeleted)
+
 		window.addEventListener('resize', this.handleWindowResize)
 		this.handleWindowResize()
 	},
 	beforeDestroy() {
+		unsubscribe('file:node:deleted', this.onNodeDeleted)
 		window.removeEventListener('resize', this.handleWindowResize)
 	},
 
@@ -505,6 +508,16 @@ export default {
 			this.Sidebar.file = ''
 			this.showTags = false
 			this.resetData()
+		},
+
+		/**
+		 * Handle if the current node was deleted
+		 * @param {import('@nextcloud/files').Node} node The deleted node
+		 */
+		onNodeDeleted(node) {
+			if (this.fileInfo && node && this.fileInfo.id === node.fileid) {
+				this.close()
+			}
 		},
 
 		/**
