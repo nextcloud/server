5885a75a96cde79003fc9c03c449a28bf8aabf9c
Christopher Ng <chrng8@gmail.com>
2024-04-22 15:19:59 -0700

    feat: Add legal notice and privacy policy links to help page
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- apps/settings/templates/help.php
+++ apps/settings/templates/help.php
@@ -73,6 +73,16 @@
 				<a href="https://help.nextcloud.com" class="button" target="_blank" rel="noreferrer noopener">
 					<?php p($l->t('Forum')); ?> ↗
 				</a>
+				<?php if ($_['legalNoticeUrl']) { ?>
+					<a href="<?php print_unescaped($_['legalNoticeUrl']); ?>" class="button" target="_blank" rel="noreferrer noopener">
+						<?php p($l->t('Legal notice')); ?> ↗
+					</a>
+				<?php } ?>
+				<?php if ($_['privacyUrl']) { ?>
+					<a href="<?php print_unescaped($_['privacyUrl']); ?>" class="button" target="_blank" rel="noreferrer noopener">
+						<?php p($l->t('Privacy policy')); ?> ↗
+					</a>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
