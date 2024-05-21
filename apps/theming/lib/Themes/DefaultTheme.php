59b85b811b131a4cf7a3555d1bd466e2990010ea
Ferdinand Thiessen <opensource@fthiessen.de>
2024-05-21 22:08:06 +0200

    fix(theming): When selected a background image make header color independent of primary color
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/theming/lib/Themes/DefaultTheme.php
+++ apps/theming/lib/Themes/DefaultTheme.php
@@ -206,6 +206,7 @@ class DefaultTheme implements ITheme {
 			'--background-invert-if-dark' => 'no',
 			'--background-invert-if-bright' => 'invert(100%)',
 			'--background-image-invert-if-bright' => 'no',
+			'--background-image-color-text' => '#ffffff',
 		];
 
 		// Primary variables
