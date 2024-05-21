9880800aeeeae3d106f0bb3e82d98a65b8b77d00
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-05-21 07:55:01 +0200

    fix(auth): Update authtoken activity selectively
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/private/User/Session.php
+++ lib/private/User/Session.php
@@ -43,6 +43,7 @@ use OC\Authentication\Exceptions\PasswordlessTokenException;
 use OC\Authentication\Exceptions\PasswordLoginForbiddenException;
 use OC\Authentication\Token\IProvider;
 use OC\Authentication\Token\IToken;
+use OC\Authentication\Token\PublicKeyToken;
 use OC\Hooks\Emitter;
 use OC\Hooks\PublicEmitter;
 use OC_User;
@@ -774,6 +775,9 @@ class Session implements IUserSession, Emitter {
 		}
 
 		$dbToken->setLastCheck($now);
+		if ($dbToken instanceof PublicKeyToken) {
+			$dbToken->setLastActivity($now);
+		}
 		$this->tokenProvider->updateToken($dbToken);
 		return true;
 	}
