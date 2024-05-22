77979b147538b7abca83c378bcbd9beaccbf7d9d
Git'Fellow <12234510+solracsf@users.noreply.github.com>
2024-05-22 17:26:29 +0200

    Try create folder first and check if it exists after
    Co-authored-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
    Signed-off-by: Git'Fellow <12234510+solracsf@users.noreply.github.com>

--- lib/private/Files/Node/Folder.php
+++ lib/private/Files/Node/Folder.php
@@ -133,7 +133,7 @@ class Folder extends Node implements \OCP\Files\Folder {
 			$fullPath = $this->getFullPath($path);
 			$nonExisting = new NonExistingFolder($this->root, $this->view, $fullPath);
 			$this->sendHooks(['preWrite', 'preCreate'], [$nonExisting]);
-			if (!$this->view->is_dir($fullPath) && !$this->view->mkdir($fullPath)) {
+			if (!$this->view->mkdir($fullPath) && !$this->view->is_dir($fullPath)) {
 				throw new NotPermittedException('Could not create folder "' . $fullPath . '"');
 			}
 			$parent = dirname($fullPath) === $this->getPath() ? $this : null;
