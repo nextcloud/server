c9b90b9e0bee91f773d66883de75b42493c0baa6
Louis Chemineau <louis@chmn.me>
2024-05-22 11:55:47 +0200

    fix(files_sharing): Delete user shares if needed when user is removed from a group
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/private/Share20/ProviderFactory.php
+++ lib/private/Share20/ProviderFactory.php
@@ -109,6 +109,7 @@ class ProviderFactory implements IProviderFactory {
 				$this->serverContainer->getL10NFactory(),
 				$this->serverContainer->getURLGenerator(),
 				$this->serverContainer->query(ITimeFactory::class),
+				$this->serverContainer->get(IManager::class),
 			);
 		}
 
