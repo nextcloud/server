9d4b9440986afddc28dad99c7987f9c31513ae9f
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-27 17:39:07 +0200

    chore: Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- apps/comments/src/mixins/CommentView.ts
+++ apps/comments/src/mixins/CommentView.ts
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import axios from '@nextcloud/axios'
 import { getCurrentUser } from '@nextcloud/auth'
 import { loadState } from '@nextcloud/initial-state'
--- apps/dashboard/src/DashboardApp.vue
+++ apps/dashboard/src/DashboardApp.vue
@@ -1,3 +1,7 @@
+<!--
+ - SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
+ - SPDX-License-Identifier: AGPL-3.0-or-later
+ -->
 <template>
 	<div id="app-dashboard">
 		<h2>{{ greeting.text }}</h2>
