cc1686dba93278fb0e64d83f71d3c0a1e7e5a50a
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-30 20:13:41 +0200

    chore: Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- apps/updatenotification/src/init.ts
+++ apps/updatenotification/src/init.ts
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import { subscribe } from '@nextcloud/event-bus'
 import { loadState } from '@nextcloud/initial-state'
 import { generateOcsUrl } from '@nextcloud/router'
--- apps/workflowengine/src/components/Workflow.vue
+++ apps/workflowengine/src/components/Workflow.vue
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<div id="workflowengine">
 		<NcSettingsSection :name="t('workflowengine', 'Available flows')"
