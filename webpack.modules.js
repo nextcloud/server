d7f3dc390b0a9589eca84226db680aebbd32f424
skjnldsv <skjnldsv@protonmail.com>
2024-05-31 15:26:39 +0200

    fix(federatedfilesharing): import external federated share script
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- webpack.modules.js
+++ webpack.modules.js
@@ -81,6 +81,7 @@ module.exports = {
 		oauth2: path.join(__dirname, 'apps/oauth2/src', 'main.js'),
 	},
 	federatedfilesharing: {
+		'external': path.join(__dirname, 'apps/federatedfilesharing/src', 'external.js'),
 		'vue-settings-admin': path.join(__dirname, 'apps/federatedfilesharing/src', 'main-admin.js'),
 		'vue-settings-personal': path.join(__dirname, 'apps/federatedfilesharing/src', 'main-personal.js'),
 	},
