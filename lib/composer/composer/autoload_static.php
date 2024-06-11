efa2c2cb8589cd500c7b1a99ed0bb90c9973251a
Côme Chilliet <come.chilliet@nextcloud.com>
2024-06-11 16:51:35 +0200

    chore(composer): composer install --no-dev
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/composer/composer/autoload_static.php
+++ lib/composer/composer/autoload_static.php
@@ -17,10 +17,6 @@ class ComposerStaticInit749170dad3f5e7f9ca158f5a9f04f6a2
             'OC\\' => 3,
             'OCP\\' => 4,
         ),
-        'B' => 
-        array (
-            'Bamarni\\Composer\\Bin\\' => 21,
-        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,10 +31,6 @@ class ComposerStaticInit749170dad3f5e7f9ca158f5a9f04f6a2
         'OCP\\' => 
         array (
             0 => __DIR__ . '/../../..' . '/lib/public',
-        ),
-        'Bamarni\\Composer\\Bin\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/bamarni/composer-bin-plugin/src',
         ),
     );
 
