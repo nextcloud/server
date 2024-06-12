0d3c11d099704aba96a71cdea3532d1d0f3fe061
Daniel Kesselberg <mail@danielkesselberg.de>
2024-06-12 11:46:12 +0200

    test: add tests for ProfilePageController
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- core/Controller/ProfilePageController.php
+++ core/Controller/ProfilePageController.php
@@ -31,7 +31,6 @@ use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Services\IInitialState;
 use OCP\IRequest;
-use OCP\IUser;
 use OCP\IUserManager;
 use OCP\IUserSession;
 use OCP\Share\IManager as IShareManager;
@@ -81,6 +80,9 @@ class ProfilePageController extends Controller {
 	 * @NoCSRFRequired
 	 * @NoAdminRequired
 	 * @NoSubAdminRequired
+	 * @BruteForceProtection(action=user)
+	 * @UserRateThrottle(limit=30, period=120)
+	 * @AnonRateThrottle(limit=30, period=120)
 	 */
 	public function index(string $targetUserId): TemplateResponse {
 		$profileNotFoundTemplate = new TemplateResponse(
@@ -91,7 +93,11 @@ class ProfilePageController extends Controller {
 		);
 
 		$targetUser = $this->userManager->get($targetUserId);
-		if (!($targetUser instanceof IUser) || !$targetUser->isEnabled()) {
+		if ($targetUser === null) {
+			$profileNotFoundTemplate->throttle();
+			return $profileNotFoundTemplate;
+		}
+		if (!$targetUser->isEnabled()) {
 			return $profileNotFoundTemplate;
 		}
 		$visitingUser = $this->userSession->getUser();
