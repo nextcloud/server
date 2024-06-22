079d026d39565e7b5547741bd62bd69082347ad6
Ferdinand Thiessen <opensource@fthiessen.de>
2024-06-22 13:56:30 +0200

    feat(files): Allow uploading directories
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- __mocks__/@nextcloud/axios.ts
+++ __mocks__/@nextcloud/axios.ts
@@ -3,6 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 export default {
+	interceptors: {
+		response: {
+			use: () => {},
+		},
+		request: {
+			use: () => {},
+		},
+	},
 	get: async () => ({ status: 200, data: {} }),
 	delete: async () => ({ status: 200, data: {} }),
 	post: async () => ({ status: 200, data: {} }),
