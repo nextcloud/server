37a55357b979373ce200b08507b186a463317b59
fenn-cs <fenn25.fn@gmail.com>
2024-06-24 10:19:53 +0100

    fix(ExternalSharing): Handle template share from external sources
    The new sharing flow requires or implies that users should edit share before creating.
    External sources should not created the share IF we would upon sharing details tab on first request.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -244,6 +244,8 @@ import ShareRequests from '../mixins/ShareRequests.js'
 import ShareTypes from '../mixins/ShareTypes.js'
 import SharesMixin from '../mixins/SharesMixin.js'
 
+import { subscribe } from '@nextcloud/event-bus'
+
 import {
 	ATOMIC_PERMISSIONS,
 	BUNDLED_PERMISSIONS,
@@ -685,6 +687,7 @@ export default {
 
 	mounted() {
 		this.$refs.quickPermissions?.querySelector('input:checked')?.focus()
+		subscribe('files_sharing:external:add-share', this.handleExistingShareFromExternalSource)
 	},
 
 	methods: {
@@ -943,6 +946,9 @@ export default {
 			default:
 				return null // Or a default icon component if needed
 			}
+		},
+		handleExistingShareFromExternalSource(share) {
+			logger.info('Existing share from external source/app', { share })
 		},
 	},
 }
