280d70a5f42309b28df0baf675893abed2a391a2
Joas Schilling <coding@schilljs.com>
2024-06-26 10:40:31 +0200

    fix(federation): Fix missing protocol on CloudID remote
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Collaboration/Collaborators/RemotePlugin.php
+++ lib/private/Collaboration/Collaborators/RemotePlugin.php
@@ -157,7 +157,7 @@ class RemotePlugin implements ISearchPlugin {
 	public function splitUserRemote(string $address): array {
 		try {
 			$cloudId = $this->cloudIdManager->resolveCloudId($address);
-			return [$cloudId->getUser(), $cloudId->getRemote()];
+			return [$cloudId->getUser(), $this->cloudIdManager->removeProtocolFromUrl($cloudId->getRemote(), true)];
 		} catch (\InvalidArgumentException $e) {
 			throw new \InvalidArgumentException('Invalid Federated Cloud ID', 0, $e);
 		}
