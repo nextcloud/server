415edcac9b48983b2266576080bdf4c2fd23482a
Christopher Ng <chrng8@gmail.com>
2024-06-27 16:56:35 -0700

    chore: More explicit splitHash typing
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- lib/private/Security/Hasher.php
+++ lib/private/Security/Hasher.php
@@ -79,7 +79,7 @@ class Hasher implements IHasher {
 	/**
 	 * Get the version and hash from a prefixedHash
 	 * @param string $prefixedHash
-	 * @return null|array Null if the hash is not prefixed, otherwise array('version' => 1, 'hash' => 'foo')
+	 * @return null|array{version: int, hash: string} Null if the hash is not prefixed, otherwise array('version' => 1, 'hash' => 'foo')
 	 */
 	protected function splitHash(string $prefixedHash): ?array {
 		$explodedString = explode('|', $prefixedHash, 2);
