d3bfcec7ea70555a3701bedcffe1df951b5a324c
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-06-28 16:05:53 -0400

    feat: Add template filler component
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

---
+++ apps/files/src/components/TemplateFiller.vue
@@ -0,0 +1,26 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'TemplateFiller',
+
+	components: {},
+
+	props: {},
+
+	methods: {},
+})
+</script>
+
+<style scoped>
+	
+</style>
