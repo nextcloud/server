1a71b3c6792e22bb6db386fc3dace7b06df2d084
Joas Schilling <coding@schilljs.com>
2024-07-04 15:18:39 +0200

    fix(CI): Use the PHP version tag
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files_external/tests/start-apache.sh
+++ apps/files_external/tests/start-apache.sh
@@ -2,7 +2,7 @@
 
 docker rm -f apache 2>/dev/null > /dev/null
 
-docker run -d --name apache -v $2:/var/www/html -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test icewind1991/samba-krb-test-apache 1>&2
+docker run -d --name apache -v $2:/var/www/html -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test icewind1991/samba-krb-test-apache:$3 1>&2
 APACHE_IP=$(docker inspect apache --format '{{.NetworkSettings.IPAddress}}')
 
 # add the dns record for apache
