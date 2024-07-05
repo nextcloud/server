656e109d2fe4aa8af3b68350cb1be342c4f4a5b8
fenn-cs <fenn25.fn@gmail.com>
2024-07-05 15:47:41 +0100

    fix(SharingDetailsView): Prevent illegal unselection of read permissions
    With the exception of "file drop" on link shares all other shares need the read permissions at least.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -180,7 +180,7 @@
 						{{ t('files_sharing', 'Custom permissions') }}
 					</NcCheckboxRadioSwitch>
 					<section v-if="setCustomPermissions" class="custom-permissions-group">
-						<NcCheckboxRadioSwitch :disabled="!allowsFileDrop && share.type === SHARE_TYPES.SHARE_TYPE_LINK"
+						<NcCheckboxRadioSwitch :disabled="!canRemoveReadPermission"
 							:checked.sync="hasRead"
 							data-cy-files-sharing-share-permissions-checkbox="read">
 							{{ t('files_sharing', 'Read') }}
@@ -602,6 +602,9 @@ export default {
 			// allowed to revoke it too (but not to grant it again).
 			return (this.fileInfo.canDownload() || this.canDownload)
 		},
+		canRemoveReadPermission() {
+			return this.allowsFileDrop && this.share.type === this.SHARE_TYPES.SHARE_TYPE_LINK
+		},
 		// if newPassword exists, but is empty, it means
 		// the user deleted the original password
 		hasUnsavedPassword() {
@@ -821,6 +824,10 @@ export default {
 					this.advancedSectionAccordionExpanded = true
 					this.setCustomPermissions = true
 				}
+			}
+			// Read permission required for share creation
+			if (!this.canRemoveReadPermission) {
+				this.hasRead = true
 			}
 		},
 		handleCustomPermissions() {
