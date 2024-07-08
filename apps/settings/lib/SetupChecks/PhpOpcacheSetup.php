39f052407adae530e425df1d85c29be513da5f5e
Git'Fellow <12234510+solracsf@users.noreply.github.com>
2024-07-08 10:43:20 +0200

    fix(setupchecks): Skip checking for OPcache if running from CLI
    Signed-off-by: Git'Fellow <12234510+solracsf@users.noreply.github.com>

--- apps/settings/lib/SetupChecks/PhpOpcacheSetup.php
+++ apps/settings/lib/SetupChecks/PhpOpcacheSetup.php
@@ -114,6 +114,11 @@ class PhpOpcacheSetup implements ISetupCheck {
 	}
 
 	public function run(): SetupResult {
+		// Skip OPcache checks if running from CLI
+		if (PHP_SAPI === 'cli') {
+			return SetupResult::success($this->l10n->t('Checking from CLI, OPcache checks have been skipped.'));
+		}
+
 		[$level,$recommendations] = $this->getOpcacheSetupRecommendations();
 		if (!empty($recommendations)) {
 			return match($level) {
