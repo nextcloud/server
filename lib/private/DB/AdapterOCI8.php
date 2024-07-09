61f3289a96022d1e2db671914cdaae0fc1bc100f
Joas Schilling <coding@schilljs.com>
2024-07-09 13:00:13 +0200

    I have no idea what I'm doing
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/DB/AdapterOCI8.php
+++ lib/private/DB/AdapterOCI8.php
@@ -28,8 +28,8 @@ class AdapterOCI8 extends Adapter {
 		$statement = str_ireplace('UNIX_TIMESTAMP()', self::UNIX_TIMESTAMP_REPLACEMENT, $statement);
 
 		$statement = preg_replace(
-			'/^INSERT INTO (.*) VALUES (.*) INTO (.*)$/',
-			'INSERT INTO ${1} VALUES ${2} RETURNING pk_id INTO ${3}',
+			'/^INSERT INTO (".*")( ?\(.*) ?VALUES( ?\(.*)$/',
+			'DECLARE vRowid ROWID; BEGIN INSERT INTO ${1} VALUES ${2} RETURNING ROWID INTO vRowid dbms_output.put_line(vRowid); END',
 			$statement
 		);
 
