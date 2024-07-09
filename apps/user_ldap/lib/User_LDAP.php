64ae882b491241e749c7109220dcdbd489fcd450
Côme Chilliet <come.chilliet@nextcloud.com>
2024-07-09 09:27:10 +0200

    fix(user_ldap): Correctly cache that user exists to avoid a request on mapping new user
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/user_ldap/lib/User_LDAP.php
+++ apps/user_ldap/lib/User_LDAP.php
@@ -612,7 +612,6 @@ class User_LDAP extends BackendUtility implements IUserBackend, UserInterface, I
 							$uuid,
 							true
 						);
-						$this->access->cacheUserExists($username);
 					} else {
 						$this->logger->warning(
 							'Failed to map created LDAP user with userid {userid}, because UUID could not be determined',
