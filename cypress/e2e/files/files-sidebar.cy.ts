3f34ef91154813cd703c65238007380a0dd9260a
Ferdinand Thiessen <opensource@fthiessen.de>
2024-07-11 01:44:00 +0200

    fix(files): Close the sidebar on navigation
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- cypress/e2e/files/files-sidebar.cy.ts
+++ cypress/e2e/files/files-sidebar.cy.ts
@@ -22,6 +22,7 @@
 
 import type { User } from '@nextcloud/cypress'
 import { getRowForFile, navigateToFolder, triggerActionForFile } from './FilesUtils'
+import { assertNotExistOrNotVisible } from '../settings/usersUtils'
 
 describe('Files: Sidebar', { testIsolation: true }, () => {
 	let user: User
@@ -43,7 +44,10 @@ describe('Files: Sidebar', { testIsolation: true }, () => {
 
 		triggerActionForFile('file', 'details')
 
-		cy.get('[data-cy-sidebar]').should('be.visible')
+		cy.get('[data-cy-sidebar]')
+			.should('be.visible')
+			.findByRole('heading', { name: 'file' })
+			.should('be.visible')
 	})
 
 	it('changes the current fileid', () => {
@@ -56,9 +60,29 @@ describe('Files: Sidebar', { testIsolation: true }, () => {
 		cy.url().should('contain', `apps/files/files/${fileId}`)
 	})
 
-	it('closes the sidebar on delete', () => {
+	it('changes the sidebar content on other file', () => {
 		cy.visit('/apps/files')
 		getRowForFile('file').should('be.visible')
+
+		triggerActionForFile('file', 'details')
+
+		cy.get('[data-cy-sidebar]')
+			.should('be.visible')
+			.findByRole('heading', { name: 'file' })
+			.should('be.visible')
+
+		triggerActionForFile('folder', 'details')
+		cy.get('[data-cy-sidebar]')
+			.should('be.visible')
+			.findByRole('heading', { name: 'folder' })
+			.should('be.visible')
+	})
+
+	it('closes the sidebar on navigation', () => {
+		cy.visit('/apps/files')
+
+		getRowForFile('file').should('be.visible')
+		getRowForFile('folder').should('be.visible')
 
 		// open the sidebar
 		triggerActionForFile('file', 'details')
@@ -67,11 +91,33 @@ describe('Files: Sidebar', { testIsolation: true }, () => {
 		// wait for the sidebar to be settled
 		cy.wait(500)
 
+		// if we navigate to the folder
+		navigateToFolder('folder')
+		// the sidebar should not be visible anymore
+		cy.get('[data-cy-sidebar]')
+			.should(assertNotExistOrNotVisible)
+	})
+
+	it('closes the sidebar on delete', () => {
+		cy.intercept('DELETE', `**/remote.php/dav/files/${user.userId}/file`).as('deleteFile')
+		// visit the files app
+		cy.visit('/apps/files')
+		getRowForFile('file').should('be.visible')
+		// open the sidebar
+		triggerActionForFile('file', 'details')
+		// validate it is open
+		cy.get('[data-cy-sidebar]').should('be.visible')
+		// delete the file
 		triggerActionForFile('file', 'delete')
-		cy.get('[data-cy-sidebar]').should('not.exist')
+		cy.wait('@deleteFile', { timeout: 10000 })
+		// see the sidebar is closed
+		cy.get('[data-cy-sidebar]')
+			.should(assertNotExistOrNotVisible)
 	})
 
 	it('changes the fileid on delete', () => {
+		cy.intercept('DELETE', `**/remote.php/dav/files/${user.userId}/folder/other`).as('deleteFile')
+
 		cy.uploadContent(user, new Blob([]), 'text/plain', '/folder/other').then((response) => {
 			const otherFileId = Number.parseInt(response.headers['oc-fileid'] ?? '0')
 			cy.login(user)
@@ -90,6 +136,8 @@ describe('Files: Sidebar', { testIsolation: true }, () => {
 			cy.wait(500)
 
 			triggerActionForFile('other', 'delete')
+			cy.wait('@deleteFile')
+
 			cy.get('[data-cy-sidebar]').should('not.exist')
 			// Ensure the URL is changed
 			cy.url().should('not.contain', `apps/files/files/${otherFileId}`)
