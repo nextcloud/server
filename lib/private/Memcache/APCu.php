b19652a2add61d652e0e8385c44de942eee611a1
Robin Appelman <robin@icewind.nl>
2024-07-10 22:32:47 +0200

    chore: cleanup acpu inc and dec
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Memcache/APCu.php
+++ lib/private/Memcache/APCu.php
@@ -73,22 +73,8 @@ class APCu extends Cache implements IMemcache {
 	 * @return int | bool
 	 */
 	public function inc($key, $step = 1) {
-		$this->add($key, 0);
-		/**
-		 * TODO - hack around a PHP 7 specific issue in APCu
-		 *
-		 * on PHP 7 the apcu_inc method on a non-existing object will increment
-		 * "0" and result in "1" as value - therefore we check for existence
-		 * first
-		 *
-		 * on PHP 5.6 this is not the case
-		 *
-		 * see https://github.com/krakjoe/apcu/issues/183#issuecomment-244038221
-		 * for details
-		 */
-		return apcu_exists($this->getPrefix() . $key)
-			? apcu_inc($this->getPrefix() . $key, $step)
-			: false;
+		$success = null;
+		return apcu_inc($this->getPrefix() . $key, $step, $success, self::DEFAULT_TTL);
 	}
 
 	/**
@@ -99,18 +85,6 @@ class APCu extends Cache implements IMemcache {
 	 * @return int | bool
 	 */
 	public function dec($key, $step = 1) {
-		/**
-		 * TODO - hack around a PHP 7 specific issue in APCu
-		 *
-		 * on PHP 7 the apcu_dec method on a non-existing object will decrement
-		 * "0" and result in "-1" as value - therefore we check for existence
-		 * first
-		 *
-		 * on PHP 5.6 this is not the case
-		 *
-		 * see https://github.com/krakjoe/apcu/issues/183#issuecomment-244038221
-		 * for details
-		 */
 		return apcu_exists($this->getPrefix() . $key)
 			? apcu_dec($this->getPrefix() . $key, $step)
 			: false;
