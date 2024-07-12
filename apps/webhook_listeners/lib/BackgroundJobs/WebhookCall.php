696ece2f52f5c78987c48fa569b07ec0d4ac0660
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-07-12 23:29:55 +0300

    feat: webhooks_listeners app support for sending direct requests to ExApps using AppAPI.
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- apps/webhook_listeners/lib/BackgroundJobs/WebhookCall.php
+++ apps/webhook_listeners/lib/BackgroundJobs/WebhookCall.php
@@ -11,11 +11,15 @@ namespace OCA\WebhookListeners\BackgroundJobs;
 
 use OCA\WebhookListeners\Db\AuthMethod;
 use OCA\WebhookListeners\Db\WebhookListenerMapper;
+use OCP\App\IAppManager;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\QueuedJob;
 use OCP\Http\Client\IClientService;
 use OCP\ICertificateManager;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 class WebhookCall extends QueuedJob {
 	public function __construct(
@@ -23,6 +27,7 @@ class WebhookCall extends QueuedJob {
 		private ICertificateManager $certificateManager,
 		private WebhookListenerMapper $mapper,
 		private LoggerInterface $logger,
+		private IAppManager $appManager,
 		ITimeFactory $timeFactory,
 	) {
 		parent::__construct($timeFactory);
@@ -49,15 +54,39 @@ class WebhookCall extends QueuedJob {
 					$options['headers'] = array_merge($options['headers'], $authHeaders);
 					break;
 			}
-			$response = $client->request($webhookListener->getHttpMethod(), $webhookListener->getUri(), $options);
+			$webhookUri = $webhookListener->getUri();
+			$exAppId = $webhookListener->getAppId();
+			if ($exAppId !== null && str_starts_with($webhookUri, "/")) {
+				// ExApp is awaiting a direct request to itself using AppAPI
+				if (!$this->appManager->isInstalled('app_api')) {
+					throw new RuntimeException('AppAPI is disabled or not installed.');
+				}
+				try {
+					$appApiFunctions = \OCP\Server::get(\OCA\AppAPI\PublicFunctions::class);
+				} catch (ContainerExceptionInterface | NotFoundExceptionInterface) {
+					throw new RuntimeException('Could not get AppAPI public functions.');
+				}
+				$exApp = $appApiFunctions->getExApp($exAppId);
+				if ($exApp === null) {
+					throw new RuntimeException('ExApp ' . $exAppId . ' is missing.');
+				} elseif (!$exApp['enabled']) {
+					throw new RuntimeException('ExApp ' . $exAppId . ' is disabled.');
+				}
+				$response = $appApiFunctions->exAppRequest($exAppId, $webhookUri, $webhookListener->getUserId(), $webhookListener->getHttpMethod(), [], $options);
+				if (is_array($response) && isset($response['error'])) {
+					throw new RuntimeException(sprintf('Error during request to ExApp(%s): %s', $exAppId, $response['error']));
+				}
+			} else {
+				$response = $client->request($webhookListener->getHttpMethod(), $webhookUri, $options);
+			}
 			$statusCode = $response->getStatusCode();
 			if ($statusCode >= 200 && $statusCode < 300) {
 				$this->logger->debug('Webhook returned status code '.$statusCode, ['body' => $response->getBody()]);
 			} else {
-				$this->logger->warning('Webhook returned unexpected status code '.$statusCode, ['body' => $response->getBody()]);
+				$this->logger->warning('Webhook(' . $webhookId . ') returned unexpected status code '.$statusCode, ['body' => $response->getBody()]);
 			}
 		} catch (\Exception $e) {
-			$this->logger->error('Webhook call failed: '.$e->getMessage(), ['exception' => $e]);
+			$this->logger->error('Webhook(' . $webhookId . ') call failed: '.$e->getMessage(), ['exception' => $e]);
 		}
 	}
 }
