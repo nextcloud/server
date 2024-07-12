87aa1267a84cef6f8d6bd43956fab2787c80627c
Josh <josh.t.richards@gmail.com>
2024-07-12 18:59:45 -0400

    fix(Mailer): Fix sendmail binary fallback feat: add debug logging to sendmail binary finder
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- apps/settings/lib/Settings/Admin/Mail.php
+++ apps/settings/lib/Settings/Admin/Mail.php
@@ -6,6 +6,7 @@
 namespace OCA\Settings\Settings\Admin;
 
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IBinaryFinder;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\Settings\IDelegatedSettings;
@@ -32,7 +33,7 @@ class Mail implements IDelegatedSettings {
 	public function getForm() {
 		$parameters = [
 			// Mail
-			'sendmail_is_available' => (bool)\OC_Helper::findBinaryPath('sendmail'),
+			'sendmail_is_available' => (bool)\OCP\Server::get(IBinaryFinder::class)->findBinaryPath('sendmail'),
 			'mail_domain' => $this->config->getSystemValue('mail_domain', ''),
 			'mail_from_address' => $this->config->getSystemValue('mail_from_address', ''),
 			'mail_smtpmode' => $this->config->getSystemValue('mail_smtpmode', ''),
