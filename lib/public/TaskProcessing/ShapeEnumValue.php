0584ebf628837a81f536033c73540ffc24bc3c22
Julien Veyssier <julien-nc@posteo.net>
2024-07-24 15:57:04 +0200

    fix(taskprocessing): add license
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/public/TaskProcessing/ShapeEnumValue.php
+++ lib/public/TaskProcessing/ShapeEnumValue.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 namespace OCP\TaskProcessing;
 
+/**
+ * Data object for input output shape enum slot value
+ * @since 30.0.0
+ */
 class ShapeEnumValue implements \JsonSerializable {
 	/**
 	 * @param string $name
