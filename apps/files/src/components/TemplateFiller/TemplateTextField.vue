3390fc1b9456c3acbd6dacbb86ae4deb1784cf09
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-07-24 17:18:06 -0400

    fix: Add SPDX header to new file
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- apps/files/src/components/TemplateFiller/TemplateTextField.vue
+++ apps/files/src/components/TemplateFiller/TemplateTextField.vue
@@ -1,3 +1,8 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
 	<div class="template-field__text">
 		<label :for="fieldId">
