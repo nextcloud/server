641560004f425a608e64fe1cd77e596486b1f5e5
provokateurin <kate@provokateurin.de>
2024-07-25 13:14:50 +0200

    refactor(systemtags): Replace security annotations with respective attributes
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- apps/systemtags/lib/Controller/LastUsedController.php
+++ apps/systemtags/lib/Controller/LastUsedController.php
@@ -6,6 +6,7 @@
 namespace OCA\SystemTags\Controller;
 
 use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\IConfig;
 use OCP\IRequest;
@@ -31,9 +32,7 @@ class LastUsedController extends Controller {
 		$this->userSession = $userSession;
 	}
 
-	/**
-	 * @NoAdminRequired
-	 */
+	#[NoAdminRequired]
 	public function getLastUsedTagIds() {
 		$lastUsed = $this->config->getUserValue($this->userSession->getUser()->getUID(), 'systemtags', 'last_used', '[]');
 		$tagIds = json_decode($lastUsed, true);
