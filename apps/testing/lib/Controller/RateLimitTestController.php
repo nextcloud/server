1cc7a620b54c32616dcfb06ece9e85f673145258
provokateurin <kate@provokateurin.de>
2024-07-25 13:14:50 +0200

    refactor(testing): Replace security annotations with respective attributes
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- apps/testing/lib/Controller/RateLimitTestController.php
+++ apps/testing/lib/Controller/RateLimitTestController.php
@@ -6,30 +6,30 @@
 namespace OCA\Testing\Controller;
 
 use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\Attribute\AnonRateLimit;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
+use OCP\AppFramework\Http\Attribute\PublicPage;
+use OCP\AppFramework\Http\Attribute\UserRateLimit;
 use OCP\AppFramework\Http\JSONResponse;
 
 class RateLimitTestController extends Controller {
 	/**
-	 * @PublicPage
-	 * @NoCSRFRequired
-	 *
-	 * @UserRateThrottle(limit=5, period=100)
-	 * @AnonRateThrottle(limit=1, period=100)
-	 *
 	 * @return JSONResponse
 	 */
+	#[PublicPage]
+	#[NoCSRFRequired]
+	#[UserRateLimit(limit: 5, period: 100)]
+	#[AnonRateLimit(limit: 1, period: 100)]
 	public function userAndAnonProtected() {
 		return new JSONResponse();
 	}
 
 	/**
-	 * @PublicPage
-	 * @NoCSRFRequired
-	 *
-	 * @AnonRateThrottle(limit=1, period=10)
-	 *
 	 * @return JSONResponse
 	 */
+	#[PublicPage]
+	#[NoCSRFRequired]
+	#[AnonRateLimit(limit: 1, period: 10)]
 	public function onlyAnonProtected() {
 		return new JSONResponse();
 	}
