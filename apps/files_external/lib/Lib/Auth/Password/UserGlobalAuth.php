06443611479b6e3a4924bb6f571583af37f4c714
Daniel Calviño Sánchez <danxuliu@gmail.com>
2024-07-30 03:05:27 +0200

    fix: Fix unmodified placeholder replacing the actual value when updating
    When updating global storages and user storages a property is not
    updated by "StoragesService::updateStorage()" if the value matches the
    unmodified placeholder. However, userglobal storages are not updated
    through the "StoragesService"; as only the authentication mechanism is
    updated it is directly done with "saveBackendOptions()" in
    "IUserProvided" or "UserGlobalAuth". Due to this the unmodified
    placeholder value needs to be explicitly checked in those cases and
    replaced by the actual value (note that in this case it is not possible
    to just skip updating a specific property).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/files_external/lib/Lib/Auth/Password/UserGlobalAuth.php
+++ apps/files_external/lib/Lib/Auth/Password/UserGlobalAuth.php
@@ -29,6 +29,7 @@ declare(strict_types=1);
 namespace OCA\Files_External\Lib\Auth\Password;
 
 use OCA\Files_External\Lib\Auth\AuthMechanism;
+use OCA\Files_External\Lib\DefinitionParameter;
 use OCA\Files_External\Lib\InsufficientDataForMeaningfulAnswerException;
 use OCA\Files_External\Lib\StorageConfig;
 use OCA\Files_External\Service\BackendService;
@@ -61,6 +62,12 @@ class UserGlobalAuth extends AuthMechanism {
 		if (!isset($backendOptions['user']) && !isset($backendOptions['password'])) {
 			return;
 		}
+
+		if ($backendOptions['password'] === DefinitionParameter::UNMODIFIED_PLACEHOLDER) {
+			$oldCredentials = $this->credentialsManager->retrieve($user->getUID(), self::CREDENTIALS_IDENTIFIER);
+			$backendOptions['password'] = $oldCredentials['password'];
+		}
+
 		// make sure we're not setting any unexpected keys
 		$credentials = [
 			'user' => $backendOptions['user'],
