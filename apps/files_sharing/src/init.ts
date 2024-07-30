d6518196cc9c855e11535e9cb3cb48156741b5de
Ferdinand Thiessen <opensource@fthiessen.de>
2024-07-30 18:52:59 +0200

    fix(files_sharing): Add missing "note to recipient"
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_sharing/src/init.ts
+++ apps/files_sharing/src/init.ts
@@ -3,23 +3,29 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 import { addNewFileMenuEntry, registerDavProperty } from '@nextcloud/files'
+import { registerAccountFilter } from './filters/AccountFilter'
+import { entry as newFileRequest } from './new/newFileRequest.ts'
+
+import registerNoteToRecipient from './files_headers/noteToRecipient'
 import registerSharingViews from './views/shares'
 
-import { entry as newFileRequest } from './new/newFileRequest'
 import './actions/acceptShareAction'
 import './actions/openInFilesAction'
 import './actions/rejectShareAction'
 import './actions/restoreShareAction'
 import './actions/sharingStatusAction'
-import { registerAccountFilter } from './filters/AccountFilter'
 
 registerSharingViews()
 
 addNewFileMenuEntry(newFileRequest)
 
+registerDavProperty('nc:note', { nc: 'http://nextcloud.org/ns' })
 registerDavProperty('nc:sharees', { nc: 'http://nextcloud.org/ns' })
 registerDavProperty('nc:share-attributes', { nc: 'http://nextcloud.org/ns' })
 registerDavProperty('oc:share-types', { oc: 'http://owncloud.org/ns' })
 registerDavProperty('ocs:share-permissions', { ocs: 'http://open-collaboration-services.org/ns' })
 
 registerAccountFilter()
+
+// Add "note to recipient" message
+registerNoteToRecipient()
