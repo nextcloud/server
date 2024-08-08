124288f534ec487554b8c542450f8b10baf76da6
Ferdinand Thiessen <opensource@fthiessen.de>
2024-08-08 13:45:59 +0200

    fix(systemtags): Sub folders should be opened in files
    Currently this is simply broken and here are two ways:
    1. Open subfolders in files view
    2. Implement logic to save last request
    
    1 is the way this is now implemented, basically copy-paste from the recent view.
    2 is much more complicated because if we get `/2/foo/bar` as the path we need to know the source of `foo`, so we would need at least 2 requests or cache the previous directory. We do not do it like this for any view so lets just stick with 1 for now.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/systemtags/src/init.ts
+++ apps/systemtags/src/init.ts
@@ -4,9 +4,11 @@
  */
 import { registerDavProperty, registerFileAction } from '@nextcloud/files'
 import { action as inlineSystemTagsAction } from './files_actions/inlineSystemTagsAction.js'
+import { action as openInFilesAction } from './files_actions/openInFilesAction.js'
 import { registerSystemTagsView } from './files_views/systemtagsView.js'
 
 registerDavProperty('nc:system-tags')
 registerFileAction(inlineSystemTagsAction)
+registerFileAction(openInFilesAction)
 
 registerSystemTagsView()
