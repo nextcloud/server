e480fd39a2775bfb5c11dcc1adc39e7361cb64e5
Anna Larch <anna@nextcloud.com>
2024-08-19 13:28:04 +0200

    fix(caldav): add missing handlers
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- apps/dav/lib/CalDAV/WebcalCaching/RefreshWebcalService.php
+++ apps/dav/lib/CalDAV/WebcalCaching/RefreshWebcalService.php
@@ -55,7 +55,7 @@ class RefreshWebcalService {
 		}
 
 
-		$webcalData = $this->connection->queryWebcalFeed($subscription, $mutations);
+		$webcalData = $this->connection->queryWebcalFeed($subscription);
 		if (!$webcalData) {
 			return;
 		}
