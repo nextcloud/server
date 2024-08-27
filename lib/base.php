135b270807eb26cb0dcc05206a30b303a34fec93
Maxence Lange <maxence@artificial-owl.com>
2024-08-27 12:29:04 -0100

    fix(setup): ignore long session login during installation
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/base.php
+++ lib/base.php
@@ -448,10 +448,15 @@ class OC {
 		$sessionName = OC_Util::getInstanceId();
 
 		try {
+			$logger = null;
+			if (Server::get(\OC\SystemConfig::class)->getValue('installed', false)) {
+				$logger = logger('core');
+			}
+
 			// set the session name to the instance id - which is unique
 			$session = new \OC\Session\Internal(
 				$sessionName,
-				logger('core'),
+				$logger,
 			);
 
 			$cryptoWrapper = Server::get(\OC\Session\CryptoWrapper::class);
