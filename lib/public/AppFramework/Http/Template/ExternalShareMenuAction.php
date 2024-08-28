61d687631bcb3780d35ca767a8b38252a488effa
Ferdinand Thiessen <opensource@fthiessen.de>
2024-08-28 13:17:04 +0200

    chore(ExternalShareMenuAction): Remove unused legacy properties
    Keep them in the constructor to not break the API,
    but they are not used anymore.
    This way of adding a share was deprecated in Nextcloud 12 (2016!),
    in favor of the federated share API, in Nextcloud 28 this way to create a share was removed.
    
    So we can cleanup as all it takes now to create a federeated share is the share token + federated user ID.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/public/AppFramework/Http/Template/ExternalShareMenuAction.php
+++ lib/public/AppFramework/Http/Template/ExternalShareMenuAction.php
@@ -11,29 +11,18 @@ namespace OCP\AppFramework\Http\Template;
  * @since 14.0.0
  */
 class ExternalShareMenuAction extends SimpleMenuAction {
-	/** @var string */
-	private $owner;
-
-	/** @var string */
-	private $displayname;
-
-	/** @var string */
-	private $shareName;
 
 	/**
 	 * ExternalShareMenuAction constructor.
 	 *
-	 * @param string $label
-	 * @param string $icon
-	 * @param string $owner
-	 * @param string $displayname
-	 * @param string $shareName
+	 * @param string $label Translated label
+	 * @param string $icon Icon CSS class
+	 * @param string $owner Owner user ID (unused)
+	 * @param string $displayname Display name of the owner (unused)
+	 * @param string $shareName Name of the share (unused)
 	 * @since 14.0.0
 	 */
 	public function __construct(string $label, string $icon, string $owner, string $displayname, string $shareName) {
 		parent::__construct('save', $label, $icon);
-		$this->owner = $owner;
-		$this->displayname = $displayname;
-		$this->shareName = $shareName;
 	}
 }
