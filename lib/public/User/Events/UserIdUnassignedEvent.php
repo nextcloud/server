7d881b1bc35e24cba29184f11d4b28188fe487ff
Côme Chilliet <come.chilliet@nextcloud.com>
2024-09-10 16:20:30 +0200

    chore: Add descriptions for new events and fix copyright year
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/public/User/Events/UserIdUnassignedEvent.php
+++ lib/public/User/Events/UserIdUnassignedEvent.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
@@ -12,6 +12,7 @@ namespace OCP\User\Events;
 use OCP\EventDispatcher\Event;
 
 /**
+ * Emitted after removing the mapping between an external user and an internal userid
  * @since 31.0.0
  */
 class UserIdUnassignedEvent extends Event {
@@ -19,7 +20,7 @@ class UserIdUnassignedEvent extends Event {
 	 * @since 31.0.0
 	 */
 	public function __construct(
-		private string $userId,
+		private readonly string $userId,
 	) {
 		parent::__construct();
 	}
