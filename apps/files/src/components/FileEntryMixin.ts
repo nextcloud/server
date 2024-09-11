d97e17b92007af7c87c0622488545dcde8b08276
Ferdinand Thiessen <opensource@fthiessen.de>
2024-09-11 22:29:31 +0200

    fix(files): Add more visual move / copy notification
    * Resolves: https://github.com/nextcloud/server/issues/46645
    
    This adds loading toast notification while the move operation is running.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files/src/components/FileEntryMixin.ts
+++ apps/files/src/components/FileEntryMixin.ts
@@ -73,8 +73,9 @@ export default defineComponent({
 		uniqueId() {
 			return hashCode(this.source.source)
 		},
+
 		isLoading() {
-			return this.source.status === NodeStatus.LOADING
+			return this.source.status === NodeStatus.LOADING || this.loading !== ''
 		},
 
 		/**
