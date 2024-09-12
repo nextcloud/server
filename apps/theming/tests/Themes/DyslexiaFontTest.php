4957ee67ccf38c68b56473a25a8c8cdfa6fe407a
Joas Schilling <coding@schilljs.com>
2024-09-12 23:04:16 +0200

    fix(tests): Test OTF font file
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/theming/tests/Themes/DyslexiaFontTest.php
+++ apps/theming/tests/Themes/DyslexiaFontTest.php
@@ -163,7 +163,7 @@ class DyslexiaFontTest extends TestCase {
 			->with('htaccess.IgnoreFrontController', false)
 			->willReturn($prettyUrlsEnabled);
 
-		$this->assertStringContainsString("'$webRoot/apps/theming/fonts/OpenDyslexic-Regular.woff'", $this->dyslexiaFont->getCustomCss());
+		$this->assertStringContainsString("'$webRoot/apps/theming/fonts/OpenDyslexic-Regular.otf'", $this->dyslexiaFont->getCustomCss());
 		$this->assertStringNotContainsString('index.php', $this->dyslexiaFont->getCustomCss());
 	}
 }
