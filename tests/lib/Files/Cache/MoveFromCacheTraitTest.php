b8464efa6940ea8be128bed29386409c2c96ace8
Robin Appelman <robin@icewind.nl>
2024-09-18 13:54:37 +0200

    test: put parent items into cache in tests
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- tests/lib/Files/Cache/MoveFromCacheTraitTest.php
+++ tests/lib/Files/Cache/MoveFromCacheTraitTest.php
@@ -9,6 +9,7 @@
 namespace Test\Files\Cache;
 
 use OC\Files\Cache\MoveFromCacheTrait;
+use OCP\Files\Cache\ICacheEntry;
 
 class FallBackCrossCacheMoveCache extends \OC\Files\Cache\Cache {
 	use MoveFromCacheTrait;
@@ -27,5 +28,8 @@ class MoveFromCacheTraitTest extends CacheTest {
 		$this->storage2 = new \OC\Files\Storage\Temporary([]);
 		$this->cache = new FallBackCrossCacheMoveCache($this->storage);
 		$this->cache2 = new FallBackCrossCacheMoveCache($this->storage2);
+
+		$this->cache->insert('', ['size' => 0, 'mtime' => 0, 'mimetype' => ICacheEntry::DIRECTORY_MIMETYPE]);
+		$this->cache2->insert('', ['size' => 0, 'mtime' => 0, 'mimetype' => ICacheEntry::DIRECTORY_MIMETYPE]);
 	}
 }
