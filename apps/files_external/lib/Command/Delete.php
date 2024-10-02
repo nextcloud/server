91b9562bddf05c35c7f0f184341ec31df9045291
Joas Schilling <coding@schilljs.com>
2024-10-02 23:58:31 +0200

    ci: Fix psalm by typing the QuestionHelper
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files_external/lib/Command/Delete.php
+++ apps/files_external/lib/Command/Delete.php
@@ -12,6 +12,7 @@ use OCA\Files_External\Service\GlobalStoragesService;
 use OCA\Files_External\Service\UserStoragesService;
 use OCP\IUserManager;
 use OCP\IUserSession;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -64,6 +65,7 @@ class Delete extends Base {
 			$listInput->setOption('output', $input->getOption('output'));
 			$listCommand->listMounts(null, [$mount], $listInput, $output);
 
+			/** @var QuestionHelper $questionHelper */
 			$questionHelper = $this->getHelper('question');
 			$question = new ConfirmationQuestion('Delete this mount? [y/N] ', false);
 
