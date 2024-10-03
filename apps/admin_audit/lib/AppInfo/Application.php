015c5efdb5b701b55dfee238abcdb4b480ca5faf
Côme Chilliet <come.chilliet@nextcloud.com>
2024-10-03 10:56:42 +0200

    fix(admin_audit): Listen to the right events
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/admin_audit/lib/AppInfo/Application.php
+++ apps/admin_audit/lib/AppInfo/Application.php
@@ -38,12 +38,11 @@ use OCP\Authentication\TwoFactorAuth\TwoFactorProviderChallengeFailed;
 use OCP\Authentication\TwoFactorAuth\TwoFactorProviderChallengePassed;
 use OCP\Console\ConsoleEvent;
 use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\Events\Node\BeforeNodeDeletedEvent;
 use OCP\Files\Events\Node\BeforeNodeReadEvent;
 use OCP\Files\Events\Node\BeforeNodeRenamedEvent;
-use OCP\Files\Events\Node\BeforeNodeWrittenEvent;
 use OCP\Files\Events\Node\NodeCopiedEvent;
 use OCP\Files\Events\Node\NodeCreatedEvent;
-use OCP\Files\Events\Node\NodeDeletedEvent;
 use OCP\Files\Events\Node\NodeRenamedEvent;
 use OCP\Files\Events\Node\NodeWrittenEvent;
 use OCP\Group\Events\GroupCreatedEvent;
@@ -195,16 +194,9 @@ class Application extends App implements IBootstrap {
 		);
 
 		$eventDispatcher->addListener(
-			BeforeNodeWrittenEvent::class,
-			function (BeforeNodeWrittenEvent $event) use ($fileActions): void {
-				$fileActions->write($event);
-			}
-		);
-
-		$eventDispatcher->addListener(
 			NodeWrittenEvent::class,
 			function (NodeWrittenEvent $event) use ($fileActions): void {
-				$fileActions->update($event);
+				$fileActions->write($event);
 			}
 		);
 
@@ -216,8 +208,8 @@ class Application extends App implements IBootstrap {
 		);
 
 		$eventDispatcher->addListener(
-			NodeDeletedEvent::class,
-			function (NodeDeletedEvent $event) use ($fileActions): void {
+			BeforeNodeDeletedEvent::class,
+			function (BeforeNodeDeletedEvent $event) use ($fileActions): void {
 				$fileActions->delete($event);
 			}
 		);
