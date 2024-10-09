403061d2f03243ff56ea65b456d962e5199c7fa2
Grigorii K. Shartsev <me@shgk.me>
2024-10-09 12:37:33 +0200

    fix(files): handle empty view with error
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- apps/files/src/utils/davUtils.ts
+++ apps/files/src/utils/davUtils.ts
@@ -39,3 +39,37 @@ export function humanizeWebDAVError(error: unknown) {
 
 	return t('files', 'Unknown error')
 }
+
+/**
+ * Whether error is a WebDAVClientError
+ * @param error - Any exception
+ * @return {boolean} - Whether error is a WebDAVClientError
+ */
+function isWebDAVClientError(error: unknown): error is WebDAVClientError {
+	return error instanceof Error && 'status' in error && 'response' in error
+}
+
+/**
+ * Get a localized error message from webdav request
+ * @param error - An exception from webdav request
+ * @return {string} Localized error message for end user
+ */
+export function humanizeWebDAVError(error: unknown) {
+	if (error instanceof Error) {
+		if (isWebDAVClientError(error)) {
+			const status = error.status || error.response?.status || 0
+			if ([400, 404, 405].includes(status)) {
+				return t('files', 'Folder not found')
+			} else if (status === 403) {
+				return t('files', 'This operation is forbidden')
+			} else if (status === 500) {
+				return t('files', 'This directory is unavailable, please check the logs or contact the administrator')
+			} else if (status === 503) {
+				return t('files', 'Storage is temporarily not available')
+			}
+		}
+		return t('files', 'Unexpected error: {error}', { error: error.message })
+	}
+
+	return t('files', 'Unknown error')
+}
