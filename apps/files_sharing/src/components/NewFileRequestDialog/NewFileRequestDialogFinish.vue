2496e5db9ea2278e30558fcbb3d222dbb1009137
rakekniven <2069590+rakekniven@users.noreply.github.com>
2024-10-10 21:51:36 +0200

    chore(l10n): Use default plural handling
    Reported at Transifex.
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- apps/files_sharing/src/components/NewFileRequestDialog/NewFileRequestDialogFinish.vue
+++ apps/files_sharing/src/components/NewFileRequestDialog/NewFileRequestDialogFinish.vue
@@ -205,11 +205,11 @@ export default defineComponent({
 
 			// Warn about duplicate emails
 			if (duplicateEmails.length > 0) {
-				showError(n('files_sharing', '1 email address already added', '{count} email addresses already added', duplicateEmails.length, { count: duplicateEmails.length }))
+				showError(n('files_sharing', '{count} email address already added', '{count} email addresses already added', duplicateEmails.length, { count: duplicateEmails.length }))
 			}
 
 			if (validEmails.length > 0) {
-				showSuccess(n('files_sharing', '1 email address added', '{count} email addresses added', validEmails.length, { count: validEmails.length }))
+				showSuccess(n('files_sharing', '{count} email address added', '{count} email addresses added', validEmails.length, { count: validEmails.length }))
 			}
 
 			this.email = ''
