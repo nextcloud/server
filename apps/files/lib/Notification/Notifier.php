fca5e23375efcc96a2c044bc13a3833a977039a6
Maksim Sukharev <antreesy.web@gmail.com>
2024-10-16 14:58:41 +0200

    fix: add notification icon for transfer ownership
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- apps/files/lib/Notification/Notifier.php
+++ apps/files/lib/Notification/Notifier.php
@@ -88,6 +88,10 @@ class Notifier implements INotifier, IDismissableNotifier {
 			throw new \InvalidArgumentException('Unhandled app');
 		}
 
+		$imagePath = $this->urlGenerator->imagePath('files', 'folder-move.svg');
+		$iconUrl = $this->urlGenerator->getAbsoluteURL($imagePath);
+		$notification->setIcon($iconUrl);
+
 		return match($notification->getSubject()) {
 			'transferownershipRequest' => $this->handleTransferownershipRequest($notification, $languageCode),
 			'transferownershipRequestDenied' => $this->handleTransferOwnershipRequestDenied($notification, $languageCode),
