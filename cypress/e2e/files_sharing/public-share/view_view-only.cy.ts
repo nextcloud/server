3e6d18aaf63fe6ca350d4def240de39682af614f
skjnldsv <skjnldsv@protonmail.com>
2024-10-26 15:29:53 +0200

    fix(files_sharing): fix view only actions button selector
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- cypress/e2e/files_sharing/public-share/view_view-only.cy.ts
+++ cypress/e2e/files_sharing/public-share/view_view-only.cy.ts
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import { getActionsForFile, getRowForFile, navigateToFolder } from '../../files/FilesUtils.ts'
+import { getActionButtonForFile, getRowForFile, navigateToFolder } from '../../files/FilesUtils.ts'
 import { openSharingPanel } from '../FilesSharingUtils.ts'
 
 describe('files_sharing: Public share - View only', { testIsolation: true }, () => {
@@ -85,7 +85,7 @@ describe('files_sharing: Public share - View only', { testIsolation: true }, () 
 	})
 
 	it('Only download action is actions available', () => {
-		getActionsForFile('foo.txt')
+		getActionButtonForFile('foo.txt')
 			.should('be.visible')
 			.click()
 
