17948296c6a5da4f8e1b6d017ab374e53dc9302b
Daniel Kesselberg <mail@danielkesselberg.de>
2024-10-30 12:25:26 +0100

    Revert "fix(dav): Always respond custom error page on exceptions"
    This reverts commit 9992e7d4395a773fec7148cf5b4111f894cb40b7.
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -36,6 +36,7 @@
  */
 namespace OCA\DAV;
 
+use OC\Files\Filesystem;
 use OCA\DAV\AppInfo\PluginManager;
 use OCA\DAV\BulkUpload\BulkUploadPlugin;
 use OCA\DAV\CalDAV\BirthdayService;
@@ -73,7 +74,7 @@ use OCA\DAV\DAV\PublicAuth;
 use OCA\DAV\DAV\ViewOnlyPlugin;
 use OCA\DAV\Events\SabrePluginAddEvent;
 use OCA\DAV\Events\SabrePluginAuthInitEvent;
-use OCA\DAV\Files\ErrorPagePlugin;
+use OCA\DAV\Files\BrowserErrorPagePlugin;
 use OCA\DAV\Files\LazySearchBackend;
 use OCA\DAV\Profiler\ProfilerPlugin;
 use OCA\DAV\Provisioning\Apple\AppleProvisioningPlugin;
@@ -247,7 +248,9 @@ class Server {
 			$this->server->addPlugin(new FakeLockerPlugin());
 		}
 
-		$this->server->addPlugin(new ErrorPagePlugin($this->request, \OC::$server->getConfig()));
+		if (BrowserErrorPagePlugin::isBrowserRequest($request)) {
+			$this->server->addPlugin(new BrowserErrorPagePlugin());
+		}
 
 		$lazySearchBackend = new LazySearchBackend();
 		$this->server->addPlugin(new SearchPlugin($lazySearchBackend));
