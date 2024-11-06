c3c8b7e2a3cbb7809891bdd13559127d984ad7f8
Joas Schilling <coding@schilljs.com>
2024-11-06 21:31:41 +0100

    fix(richobjectstrings): Add missing placeholder validation
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/public/RichObjectStrings/IValidator.php
+++ lib/public/RichObjectStrings/IValidator.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -12,10 +15,16 @@ namespace OCP\RichObjectStrings;
  */
 interface IValidator {
 	/**
+	 * Only alphanumeric, dash, underscore and got are allowed, starting with a character
+	 * @since 31.0.0
+	 */
+	public const PLACEHOLDER_REGEX = '[A-Za-z][A-Za-z0-9\-_.]+';
+
+	/**
 	 * @param string $subject
-	 * @param array[] $parameters
+	 * @param array<non-empty-string, array> $parameters
 	 * @throws InvalidObjectExeption
 	 * @since 11.0.0
 	 */
-	public function validate($subject, array $parameters);
+	public function validate(string $subject, array $parameters): void;
 }
