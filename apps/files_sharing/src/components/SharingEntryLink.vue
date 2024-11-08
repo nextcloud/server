1b726cdb8e9a03e3a0148241c9d1794cd7c9463b
skjnldsv <skjnldsv@protonmail.com>
2024-11-08 16:13:32 +0100

    fix(files_sharing): share assignment issue in SharingEntryLink
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- apps/files_sharing/src/components/SharingEntryLink.vue
+++ apps/files_sharing/src/components/SharingEntryLink.vue
@@ -539,8 +539,10 @@ export default {
 		}
 	},
 	mounted() {
-		this.defaultExpirationDateEnabled = this.config.defaultExpirationDate instanceof Date
-		this.share.expireDate = this.defaultExpirationDateEnabled ? this.formatDateToString(this.config.defaultExpirationDate) : ''
+		if (this.share) {
+			this.defaultExpirationDateEnabled = this.config.defaultExpirationDate instanceof Date
+			this.share.expireDate = this.defaultExpirationDateEnabled ? this.formatDateToString(this.config.defaultExpirationDate) : ''
+		}
 	},
 
 	methods: {
