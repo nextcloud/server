e3d25d0bcdaa3fec869028f9c3a4443685d3ffd9
nfebe <fenn25.fn@gmail.com>
2024-11-18 17:43:43 +0100

    fix: Only show share permission if re-sharable
    Signed-off-by: nfebe <fenn25.fn@gmail.com>
    
    [skip ci]

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -541,6 +541,9 @@ export default {
 			return t('files_sharing', 'Update share')
 
 		},
+		resharingIsPossible() {
+			return this.config.isResharingAllowed && this.share.type !== ShareType.Link && this.share.type !== ShareType.Email
+		},
 		/**
 		 * Can the sharer set whether the sharee can edit the file ?
 		 *
@@ -690,7 +693,7 @@ export default {
 				[ATOMIC_PERMISSIONS.DELETE]: this.t('files_sharing', 'Delete'),
 			}
 
-			return [ATOMIC_PERMISSIONS.READ, ATOMIC_PERMISSIONS.CREATE, ATOMIC_PERMISSIONS.UPDATE, ATOMIC_PERMISSIONS.SHARE, ATOMIC_PERMISSIONS.DELETE]
+			return [ATOMIC_PERMISSIONS.READ, ATOMIC_PERMISSIONS.CREATE, ATOMIC_PERMISSIONS.UPDATE, ...(this.resharingIsPossible ? [ATOMIC_PERMISSIONS.SHARE] : []), ATOMIC_PERMISSIONS.DELETE]
 				.filter((permission) => hasPermissions(this.share.permissions, permission))
 				.map((permission, index) => index === 0
 					? translatedPermissions[permission]
