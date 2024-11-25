5581f0c7bf698b26db0b68b0e420a0f9da490c8b
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2024-11-25 11:17:29 +0100

    chore: remove legacy action to test remote endpoint
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- apps/files_sharing/tests/Controller/ExternalShareControllerTest.php
+++ apps/files_sharing/tests/Controller/ExternalShareControllerTest.php
@@ -8,11 +8,8 @@ namespace OCA\Files_Sharing\Tests\Controllers;
 
 use OCA\Files_Sharing\Controller\ExternalSharesController;
 use OCA\Files_Sharing\External\Manager;
-use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Http\JSONResponse;
-use OCP\Http\Client\IClient;
 use OCP\Http\Client\IClientService;
-use OCP\Http\Client\IResponse;
 use OCP\IConfig;
 use OCP\IRequest;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -78,91 +75,5 @@ class ExternalShareControllerTest extends \Test\TestCase {
 			->with(4);
 
 		$this->assertEquals(new JSONResponse(), $this->getExternalShareController()->destroy(4));
-	}
-
-	public function testRemoteWithValidHttps(): void {
-		$client = $this->createMock(IClient::class);
-		$response = $this->createMock(IResponse::class);
-		$response
-			->expects($this->exactly(2))
-			->method('getBody')
-			->willReturnOnConsecutiveCalls(
-				'Certainly not a JSON string',
-				'{"installed":true,"maintenance":false,"version":"8.1.0.8","versionstring":"8.1.0","edition":""}'
-			);
-		$client
-			->expects($this->any())
-			->method('get')
-			->willReturn($response);
-
-		$this->clientService
-			->expects($this->exactly(2))
-			->method('newClient')
-			->willReturn($client);
-
-		$this->assertEquals(new DataResponse('https'), $this->getExternalShareController()->testRemote('nextcloud.com'));
-	}
-
-	public function testRemoteWithWorkingHttp(): void {
-		$client = $this->createMock(IClient::class);
-		$response = $this->createMock(IResponse::class);
-		$client
-			->method('get')
-			->willReturn($response);
-		$response
-			->expects($this->exactly(5))
-			->method('getBody')
-			->willReturnOnConsecutiveCalls(
-				'Certainly not a JSON string',
-				'Certainly not a JSON string',
-				'Certainly not a JSON string',
-				'Certainly not a JSON string',
-				'{"installed":true,"maintenance":false,"version":"8.1.0.8","versionstring":"8.1.0","edition":""}'
-			);
-		$this->clientService
-			->expects($this->exactly(5))
-			->method('newClient')
-			->willReturn($client);
-
-		$this->assertEquals(new DataResponse('http'), $this->getExternalShareController()->testRemote('nextcloud.com'));
-	}
-
-	public function testRemoteWithInvalidRemote(): void {
-		$client = $this->createMock(IClient::class);
-		$response = $this->createMock(IResponse::class);
-		$client
-			->expects($this->exactly(6))
-			->method('get')
-			->willReturn($response);
-		$response
-			->expects($this->exactly(6))
-			->method('getBody')
-			->willReturn('Certainly not a JSON string');
-		$this->clientService
-			->expects($this->exactly(6))
-			->method('newClient')
-			->willReturn($client);
-
-		$this->assertEquals(new DataResponse(false), $this->getExternalShareController()->testRemote('nextcloud.com'));
-	}
-
-	public function dataRemoteWithInvalidRemoteURLs(): array {
-		return [
-			['nextcloud.com?query'],
-			['nextcloud.com/#anchor'],
-			['nextcloud.com/;tomcat'],
-		];
-	}
-
-	/**
-	 * @dataProvider dataRemoteWithInvalidRemoteURLs
-	 * @param string $remote
-	 */
-	public function testRemoteWithInvalidRemoteURLs(string $remote): void {
-		$this->clientService
-			->expects($this->never())
-			->method('newClient');
-
-		$this->assertEquals(new DataResponse(false), $this->getExternalShareController()->testRemote($remote));
 	}
 }
