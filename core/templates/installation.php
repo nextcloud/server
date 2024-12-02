4de1eebd5e970cdab3f00e1de09c39a0d671ab84
Fabian Berg <fabian.berg@hb9hil.org>
2024-12-02 13:28:15 +0100

    fix(installer): Label field as Account name instead of username
    to match the the other fields in v30
    
    Signed-off-by: Fabian Berg <fabian.berg@hb9hil.org>

--- core/templates/installation.php
+++ core/templates/installation.php
@@ -40,7 +40,7 @@ script('core', 'install');
 	<fieldset id="adminaccount">
 		<legend><?php print_unescaped($l->t('Create an <strong>admin account</strong>')); ?></legend>
 		<p>
-			<label for="adminlogin"><?php p($l->t('Username')); ?></label>
+			<label for="adminlogin"><?php p($l->t('Account name')); ?></label>
 			<input type="text" name="adminlogin" id="adminlogin"
 				value="<?php p($_['adminlogin']); ?>"
 				autocomplete="off" autocapitalize="none" spellcheck="false" autofocus required>
