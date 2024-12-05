e05ee52720cbd0653a65d133a88d7a8f08bc29ee
Arthur Schiwon <blizzz@arthur-schiwon.de>
2024-12-05 12:06:19 +0100

    build(hub): 28.0.13
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [28, 0, 13, 0];
+$OC_Version = [28, 0, 13, 1];
 
 // The human-readable string
-$OC_VersionString = '28.0.13 RC1';
+$OC_VersionString = '28.0.13';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
