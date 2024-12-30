f08b7959b846f9bb0e0adf8fefc7f98e91923ad9
SebastianKrupinski <krupinskis05@gmail.com>
2024-12-30 15:37:47 -0500

    feat: OCC and OCS Calendar Import/Export
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- apps/dav/lib/CalDAV/CalendarImpl.php
+++ apps/dav/lib/CalDAV/CalendarImpl.php
@@ -8,12 +8,16 @@ declare(strict_types=1);
  */
 namespace OCA\DAV\CalDAV;
 
+use Exception;
+use InvalidArgumentException;
 use Generator;
 use OCA\DAV\CalDAV\Auth\CustomPrincipalPlugin;
 use OCA\DAV\CalDAV\InvitationResponse\InvitationResponseServer;
 use OCP\Calendar\CalendarExportOptions;
+use OCP\Calendar\CalendarImportOptions;
 use OCP\Calendar\Exceptions\CalendarException;
 use OCP\Calendar\ICalendarExport;
+use OCP\Calendar\ICalendarImport;
 use OCP\Calendar\ICalendarIsEnabled;
 use OCP\Calendar\ICalendarIsShared;
 use OCP\Calendar\ICalendarIsWritable;
@@ -26,11 +30,14 @@ use Sabre\VObject\Component\VCalendar;
 use Sabre\VObject\Component\VEvent;
 use Sabre\VObject\Component\VTimeZone;
 use Sabre\VObject\ITip\Message;
+use Sabre\VObject\Node;
 use Sabre\VObject\Property;
 use Sabre\VObject\Reader;
+use Sabre\VObject\UUIDUtil;
+
 use function Sabre\Uri\split as uriSplit;
 
-class CalendarImpl implements ICreateFromString, IHandleImipMessage, ICalendarIsWritable, ICalendarIsShared, ICalendarExport, ICalendarIsEnabled {
+class CalendarImpl implements ICreateFromString, IHandleImipMessage, ICalendarIsWritable, ICalendarIsShared, ICalendarImport, ICalendarExport, ICalendarIsEnabled {
 	public function __construct(
 		private Calendar $calendar,
 		/** @var array<string, mixed> */
---
+++ lib/public/Calendar/CalendarImportOptions.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCP\Calendar;
+
+/**
+ * Calendar Import Options
+ *
+ * @since 32.0.0
+ */
+final class CalendarImportOptions {
+
+	public const VALIDATE_NONE = 0;
+	public const VALIDATE_SKIP = 1;
+	public const VALIDATE_FAIL = 2;
+	public const VALIDATE_OPTIONS = [
+		self::VALIDATE_NONE,
+		self::VALIDATE_SKIP,
+		self::VALIDATE_FAIL,
+	];
+	public const ERROR_CONTINUE = 0;
+	public const ERROR_FAIL = 1;
+	public const ERROR_OPTIONS = [
+		self::ERROR_CONTINUE,
+		self::ERROR_FAIL,
+	];
+
+	private string $format = 'ical';
+	private bool $supersede = false;
+	private int $errors = self::ERROR_FAIL;
+	private int $validate = self::VALIDATE_SKIP;
+
+	/**
+	 * Gets the import format
+	 *
+	 * @return string ical, jcal, xcal, etc, defaults to ical
+	 */
+	public function getFormat(): string {
+		return $this->format;
+	}
+
+	/**
+	 * Sets the import format
+	 *
+	 * @param string $format ical, jcal, xcal, etc, defaults to ical
+	 */
+	public function setFormat(string $format): void {
+		$this->format = $format;
+	}
+
+	/**
+	 * Gets whether to supersede existing objects
+	 */
+	public function getSupersede(): bool {
+		return $this->supersede;
+	}
+
+	/**
+	 * Sets whether to supersede existing objects
+	 */
+	public function setSupersede(bool $supersede): void {
+		$this->supersede = $supersede;
+	}
+
+	/**
+	 * Gets how to handle object errors
+	 *
+	 * @return int 0 - continue, 1 - fail
+	 */
+	public function getErrors(): int {
+		return $this->errors;
+	}
+
+	/**
+	 * Sets how to handle object errors
+	 *
+	 * @param int $errors 0 - continue, 1 - fail
+	 *
+	 * @template $errors of self::ERROR_*
+	 */
+	public function setErrors(int $errors): void {
+		if (!in_array($errors, self::ERROR_OPTIONS, true)) {
+			throw new \InvalidArgumentException("Invalid errors handling type <$errors> specified, only ERROR_CONTINUE or ERROR_FAIL allowed");
+		}
+		$this->errors = $errors;
+	}
+
+	/**
+	 * Gets how to handle object validation
+	 *
+	 * @return int 0 - no validation, 1 - validate and skip on issue, 2 - validate and fail on issue
+	 */
+	public function getValidate(): int {
+		return $this->validate;
+	}
+
+	/**
+	 * Sets how to handle object validation
+	 *
+	 * @param int $validate 0 - no validation, 1 - validate and skip on issue, 2 - validate and fail on issue
+	 *
+	 * @template $validate of self::VALIDATE_*
+	 */
+	public function setValidate(int $validate): void {
+		if (!in_array($validate, self::VALIDATE_OPTIONS, true)) {
+			throw new \InvalidArgumentException("Invalid validation handling type <$validate> specified, only VALIDATE_NONE, VALIDATE_SKIP or VALIDATE_FAIL allowed");
+		}
+		$this->validate = $validate;
+	}
+
+}
