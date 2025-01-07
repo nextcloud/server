d64c72ba2f5b3dffc6a4c26f168701f5f7c914d3
provokateurin <kate@provokateurin.de>
2025-01-07 15:41:19 +0100

    docs(HTTP): Add proper docs for CORS attribute
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/public/AppFramework/Http/Attribute/CORS.php
+++ lib/public/AppFramework/Http/Attribute/CORS.php
@@ -28,7 +28,9 @@ namespace OCP\AppFramework\Http\Attribute;
 use Attribute;
 
 /**
- * Attribute for controller methods that can also be accessed by not logged-in user
+ * Attribute for controller methods that can also be accessed by other websites.
+ * See https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS for an explanation of the functionality and the security implications.
+ * See https://docs.nextcloud.com/server/latest/developer_manual/digging_deeper/rest_apis.html on how to implement it in your controller.
  *
  * @since 27.0.0
  */
