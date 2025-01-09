AceAiAid/nextcloud_server

fix: block moving files to it's own parent with dav
Signed-off-by: Robin Appelman <robin@icewind.nl>

apps/dav/lib/Connector/Sabre/FilesPlugin.php
@@ -203,6 +203,11 @@ class FilesPlugin extends ServerPlugin {
 		if (!$sourceNodeFileInfo->isDeletable()) {
 			throw new Forbidden($source . ' cannot be deleted');
 		}
+
+		// The source is not allowed to be the parent of the target
+		if (str_starts_with($source, $target . '/')) {
+			throw new Forbidden($source . ' cannot be moved to it\'s parent');
+		}
 	}
 
 	/**

nextcloud/server

apps/dav/lib/Connector/Sabre/FilesPlugin.php
@@ -199,6 +199,11 @@ class FilesPlugin extends ServerPlugin {
 				throw new Forbidden($source . " cannot be deleted");
 			}
 		}
+
+		// The source is not allowed to be the parent of the target
+		if (str_starts_with($source, $destination . '/')) {
+			throw new Forbidden($source . ' cannot be moved to it\'s parent');
+		}
 	}
 
 	/**