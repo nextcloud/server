2ee38139f392e07e2a83a96a52a090585de30027
Robin Appelman <robin@icewind.nl>
2025-01-20 21:16:14 +0100

    chore: update icewind/smb to 3.7.0
    Signed-off-by: Robin Appelman <robin@icewind.nl>

---
+++ apps/files_external/3rdparty/icewind/smb/src/Exception/InvalidTicket.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2022 Robin Appelman <robin@icewind.nl>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace Icewind\SMB\Exception;
+
+class InvalidTicket extends Exception {
+
+}
