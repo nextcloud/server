20dc3d50282c728cb878d1964d8b251e37359caf
Luka Trovic <luka@nextcloud.com>
2025-01-29 15:44:53 +0100

    fix(files_sharing): skip expiration notify for invalid share record
    Signed-off-by: Luka Trovic <luka@nextcloud.com>
    
    [skip ci]

--- apps/files_sharing/lib/Command/ExiprationNotification.php
+++ apps/files_sharing/lib/Command/ExiprationNotification.php
@@ -26,6 +26,7 @@ declare(strict_types=1);
  */
 namespace OCA\Files_Sharing\Command;
 
+use OCA\Files_Sharing\OrphanHelper;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\IDBConnection;
 use OCP\Notification\IManager as NotificationManager;
@@ -48,7 +49,9 @@ class ExiprationNotification extends Command {
 	public function __construct(ITimeFactory $time,
 		NotificationManager $notificationManager,
 		IDBConnection $connection,
-		ShareManager $shareManager) {
+		ShareManager $shareManager,
+		private OrphanHelper $orphanHelper,
+	) {
 		parent::__construct();
 
 		$this->notificationManager = $notificationManager;
@@ -80,7 +83,8 @@ class ExiprationNotification extends Command {
 		foreach ($shares as $share) {
 			if ($share->getExpirationDate() === null
 				|| $share->getExpirationDate()->getTimestamp() < $minTime->getTimestamp()
-				|| $share->getExpirationDate()->getTimestamp() > $maxTime->getTimestamp()) {
+				|| $share->getExpirationDate()->getTimestamp() > $maxTime->getTimestamp()
+				|| !$this->orphanHelper->isShareValid($share->getSharedBy(), $share->getNodeId())) {
 				continue;
 			}
 
