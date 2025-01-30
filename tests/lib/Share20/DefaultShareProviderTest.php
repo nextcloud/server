9626ff81bd33389564f79dca0e1038a5cf513187
Louis Chemineau <louis@chmn.me>
2025-01-30 10:24:58 +0100

    fix: Exclude non accepted shares when computing access list
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- tests/lib/Share20/DefaultShareProviderTest.php
+++ tests/lib/Share20/DefaultShareProviderTest.php
@@ -2698,6 +2698,7 @@ class DefaultShareProviderTest extends \Test\TestCase {
 			->setShareType(IShare::TYPE_USER)
 			->setPermissions(\OCP\Constants::PERMISSION_ALL);
 		$share1 = $this->provider->create($share1);
+		$share1 = $provider->acceptShare($share1, $u2->getUid());
 
 		$share2 = $shareManager->newShare();
 		$share2->setNode($folder2)
@@ -2709,6 +2710,9 @@ class DefaultShareProviderTest extends \Test\TestCase {
 		$share2 = $this->provider->create($share2);
 
 		$shareManager->deleteFromSelf($share2, $u4->getUID());
+
+		$share2 = $provider->acceptShare($share2, $u3->getUid());
+		$share2 = $provider->acceptShare($share2, $u4->getUid());
 
 		$share3 = $shareManager->newShare();
 		$share3->setNode($file1)
@@ -2726,6 +2730,7 @@ class DefaultShareProviderTest extends \Test\TestCase {
 			->setShareType(IShare::TYPE_USER)
 			->setPermissions(\OCP\Constants::PERMISSION_READ);
 		$share4 = $this->provider->create($share4);
+		$share4 = $provider->acceptShare($share4, $u5->getUid());
 
 		$result = $provider->getAccessList([$folder1, $folder2, $file1], false);
 
@@ -2795,6 +2800,7 @@ class DefaultShareProviderTest extends \Test\TestCase {
 			->setShareType(IShare::TYPE_USER)
 			->setPermissions(\OCP\Constants::PERMISSION_ALL);
 		$share1 = $this->provider->create($share1);
+		$share1 = $provider->acceptShare($share1, $u2->getUid());
 
 		$share2 = $shareManager->newShare();
 		$share2->setNode($folder2)
@@ -2804,6 +2810,8 @@ class DefaultShareProviderTest extends \Test\TestCase {
 			->setShareType(IShare::TYPE_GROUP)
 			->setPermissions(\OCP\Constants::PERMISSION_ALL);
 		$share2 = $this->provider->create($share2);
+		$share2 = $provider->acceptShare($share2, $u3->getUid());
+		$share2 = $provider->acceptShare($share2, $u4->getUid());
 
 		$shareManager->deleteFromSelf($share2, $u4->getUID());
 
@@ -2823,6 +2831,7 @@ class DefaultShareProviderTest extends \Test\TestCase {
 			->setShareType(IShare::TYPE_USER)
 			->setPermissions(\OCP\Constants::PERMISSION_READ);
 		$share4 = $this->provider->create($share4);
+		$share4 = $provider->acceptShare($share4, $u5->getUid());
 
 		$result = $provider->getAccessList([$folder1, $folder2, $file1], true);
 
