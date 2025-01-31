b5a3fc58fd1a01690ffe77409c2bdb0cfeb1b226
Ferdinand Thiessen <opensource@fthiessen.de>
2025-01-31 14:58:03 +0100

    test(files): Make scrolling tests independent from magic values
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- cypress/e2e/files/FilesUtils.ts
+++ cypress/e2e/files/FilesUtils.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import type { User } from '@nextcloud/cypress'
+
 export const getRowForFileId = (fileid: number) => cy.get(`[data-cy-files-list-row-fileid="${fileid}"]`)
 export const getRowForFile = (filename: string) => cy.get(`[data-cy-files-list-row-name="${CSS.escape(filename)}"]`)
 
@@ -175,4 +177,74 @@ export const haveValidity = (validity: string | RegExp) => {
 		return (el: JQuery<HTMLElement>) => expect((el.get(0) as HTMLInputElement).validationMessage).to.equal(validity)
 	}
 	return (el: JQuery<HTMLElement>) => expect((el.get(0) as HTMLInputElement).validationMessage).to.match(validity)
+}
+
+export const deleteFileWithRequest = (user: User, path: string) => {
+	// Ensure path starts with a slash and has no double slashes
+	path = `/${path}`.replace(/\/+/g, '/')
+
+	cy.request('/csrftoken').then(({ body }) => {
+		const requestToken = body.token
+		cy.request({
+			method: 'DELETE',
+			url: `${Cypress.env('baseUrl')}/remote.php/dav/files/${user.userId}` + path,
+			headers: {
+				requestToken,
+			},
+			retryOnStatusCodeFailure: true,
+		})
+	})
+}
+
+export const triggerFileListAction = (actionId: string) => {
+	cy.get(`button[data-cy-files-list-action="${CSS.escape(actionId)}"]`).last()
+		.should('exist').click({ force: true })
+}
+
+export const reloadCurrentFolder = () => {
+	cy.intercept('PROPFIND', /\/remote.php\/dav\//).as('propfind')
+	cy.get('[data-cy-files-content-breadcrumbs]').findByRole('button', { description: 'Reload current directory' }).click()
+	cy.wait('@propfind')
+}
+
+/**
+ * Enable the grid mode for the files list.
+ * Will fail if already enabled!
+ */
+export function enableGridMode() {
+	cy.intercept('**/apps/files/api/v1/config/grid_view').as('setGridMode')
+	cy.findByRole('button', { name: 'Switch to grid view' })
+		.should('be.visible')
+		.click()
+	cy.wait('@setGridMode')
+}
+
+/**
+ * Calculate the needed viewport height to limit the visible rows of the file list.
+ * Requires a logged in user.
+ *
+ * @param rows The number of rows that should be displayed at the same time
+ */
+export function calculateViewportHeight(rows: number): Cypress.Chainable<number> {
+	cy.visit('/apps/files')
+
+	return cy.get('[data-cy-files-list]')
+		.should('be.visible')
+		.then((filesList) => {
+			const windowHeight = Cypress.$('body').outerHeight()!
+			// Size of other page elements
+			const outerHeight = Math.ceil(windowHeight - filesList.outerHeight()!)
+			// Size of before and filters
+			const beforeHeight = Math.ceil(Cypress.$('.files-list__before').outerHeight()!)
+			const filterHeight = Math.ceil(Cypress.$('.files-list__filters').outerHeight()!)
+			// Size of the table header
+			const tableHeaderHeight = Math.ceil(Cypress.$('[data-cy-files-list-thead]').outerHeight()!)
+			// table row height
+			const rowHeight = Math.ceil(Cypress.$('[data-cy-files-list-tbody] tr').outerHeight()!)
+
+			// sum it up
+			const viewportHeight = outerHeight + beforeHeight + filterHeight + tableHeaderHeight + rows * rowHeight
+			cy.log(`Calculated viewport height: ${viewportHeight} (${outerHeight} + ${beforeHeight} + ${filterHeight} + ${tableHeaderHeight} + ${rows} * ${rowHeight})`)
+			return cy.wrap(viewportHeight)
+		})
 }
