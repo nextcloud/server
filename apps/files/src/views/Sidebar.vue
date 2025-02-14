c87a6ba811afbf472db00781102afb987fe9e3d6
skjnldsv <skjnldsv@protonmail.com>
2025-02-14 16:10:20 +0100

    fix(files): attach sidebar to main content for vue debug
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- apps/files/src/views/Sidebar.vue
+++ apps/files/src/views/Sidebar.vue
@@ -38,8 +38,7 @@
 				<SystemTags v-if="isSystemTagsEnabled && showTagsDefault"
 					v-show="showTags"
 					:disabled="!fileInfo?.canEdit()"
-					:file-id="fileInfo.id"
-					@has-tags="value => showTags = value" />
+					:file-id="fileInfo.id" />
 				<LegacyView v-for="view in views"
 					:key="view.cid"
 					:component="view"
@@ -93,19 +92,20 @@
 		</template>
 	</NcAppSidebar>
 </template>
-<script>
-import { getCurrentUser } from '@nextcloud/auth'
-import { getCapabilities } from '@nextcloud/capabilities'
-import { showError } from '@nextcloud/dialogs'
+<script lang="ts">
+import { davRemoteURL, davRootPath, File, Folder, formatFileSize } from '@nextcloud/files'
+import { defineComponent } from 'vue'
 import { emit, subscribe, unsubscribe } from '@nextcloud/event-bus'
-import { File, Folder, davRemoteURL, davRootPath, formatFileSize } from '@nextcloud/files'
 import { encodePath } from '@nextcloud/paths'
-import { generateUrl } from '@nextcloud/router'
-import { ShareType } from '@nextcloud/sharing'
-import { mdiStar, mdiStarOutline } from '@mdi/js'
 import { fetchNode } from '../services/WebdavClient.ts'
-import axios from '@nextcloud/axios'
+import { generateUrl } from '@nextcloud/router'
+import { getCapabilities } from '@nextcloud/capabilities'
+import { getCurrentUser } from '@nextcloud/auth'
+import { mdiStar, mdiStarOutline } from '@mdi/js'
+import { ShareType } from '@nextcloud/sharing'
+import { showError } from '@nextcloud/dialogs'
 import $ from 'jquery'
+import axios from '@nextcloud/axios'
 
 import NcAppSidebar from '@nextcloud/vue/dist/Components/NcAppSidebar.js'
 import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton.js'
@@ -120,7 +120,7 @@ import SidebarTab from '../components/SidebarTab.vue'
 import SystemTags from '../../../systemtags/src/components/SystemTags.vue'
 import logger from '../logger.ts'
 
-export default {
+export default defineComponent({
 	name: 'Sidebar',
 
 	components: {
@@ -464,7 +464,10 @@ export default {
 		 * Toggle the tags selector
 		 */
 		toggleTags() {
-			this.showTagsDefault = this.showTags = !this.showTags
+			// toggle
+			this.showTags = !this.showTags
+			// save the new state
+			this.setShowTagsDefault(this.showTags)
 		},
 
 		/**
@@ -585,7 +588,7 @@ export default {
 			this.hasLowHeight = document.documentElement.clientHeight < 1024
 		},
 	},
-}
+})
 </script>
 <style lang="scss" scoped>
 .app-sidebar {
