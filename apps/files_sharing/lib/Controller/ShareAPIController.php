338ab643657802c572f8515c8ecc354e34df17c7
skjnldsv <skjnldsv@protonmail.com>
2025-02-19 16:10:19 +0100

    fix(files_sharing): rate limit share creation 10 times per 10 minutes
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- apps/files_sharing/lib/Controller/ShareAPIController.php
+++ apps/files_sharing/lib/Controller/ShareAPIController.php
@@ -558,6 +558,7 @@ class ShareAPIController extends OCSController {
 	 * 200: Share created
 	 */
 	#[NoAdminRequired]
+	#[UserRateLimit(limit: 10, period: 600)]
 	public function createShare(
 		?string $path = null,
 		?int $permissions = null,
@@ -2150,7 +2151,7 @@ class ShareAPIController extends OCSController {
 	 * 200: The email notification was sent successfully
 	 */
 	#[NoAdminRequired]
-	#[UserRateLimit(limit: 5, period: 120)]
+	#[UserRateLimit(limit: 10, period: 600)]
 	public function sendShareEmail(string $id, $password = ''): DataResponse {
 		try {
 			$share = $this->getShareById($id);
