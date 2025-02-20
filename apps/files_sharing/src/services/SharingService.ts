534f49f9fe0e2259409f81afc704d597611531c3
Ferdinand Thiessen <opensource@fthiessen.de>
2025-02-20 09:45:02 +0100

    chore(deps): Update `@nextcloud/vue` to v8.23.1
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_sharing/src/services/SharingService.ts
+++ apps/files_sharing/src/services/SharingService.ts
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+/// <reference types="@nextcloud/typings" />
 // TODO: Fix this instead of disabling ESLint!!!
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
