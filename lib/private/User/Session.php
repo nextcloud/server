dc68d66945f5ce64433d7c260805ee9aaecd65f4
Cleopatra Enjeck M. <patrathewhiz@gmail.com>
2025-02-24 06:17:00 +0000

    fix: Use case insensitive check when validating login name
    Signed-off-by: Cleopatra Enjeck M. <patrathewhiz@gmail.com>

--- lib/private/User/Session.php
+++ lib/private/User/Session.php
@@ -779,7 +779,7 @@ class Session implements IUserSession, Emitter {
 	 * Check if login names match
 	 */
 	private function validateTokenLoginName(?string $loginName, IToken $token): bool {
-		if ($token->getLoginName() !== $loginName) {
+		if (strtolower($token->getLoginName() ?? '') !== strtolower($loginName ?? '')) {
 			// TODO: this makes it impossible to use different login names on browser and client
 			// e.g. login by e-mail 'user@example.com' on browser for generating the token will not
 			//      allow to use the client token with the login name 'user'.
