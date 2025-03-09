a1106acccdd347d0b07e0804851269dbf0e5be5a
invario <67800603+invario@users.noreply.github.com>
2025-03-09 10:42:47 -0400

    Add switch to force colorspace to 2020_ncl
    Some HDR videos have their colorspace tagged either incorrectly or with a yet to be integrated value resulting in ffmpeg being unable to read it and assigning it a value of "reserved".  This results in a fail during preview generation. This adds a switch to ffmpeg to force the colorspace to 2020_ncl which is used by all HDR videos.
    
    Signed-off-by: invario <67800603+invario@users.noreply.github.com>

--- lib/private/Preview/Movie.php
+++ lib/private/Preview/Movie.php
@@ -134,7 +134,7 @@ class Movie extends ProviderV2 {
 			// added new configuration switch to enable hdr video detection, defaults to disabled
 			$ffmpeg_enable_hdr = $this->config->getSystemValueBool('preview_ffmpeg_enable_hdr',false);
 			if ($ffmpeg_enable_hdr) {
-            	// load ffprobe path from configuration, otherwise generate binary path using ffmpeg binary path
+				// load ffprobe path from configuration, otherwise generate binary path using ffmpeg binary path
 				$ffprobe_binary = is_string($this->config->getSystemValue('preview_ffprobe_path',null)) ? $this->config->getSystemValue('preview_ffprobe_path',null) : pathinfo($this->binary,PATHINFO_DIRNAME) . '/ffprobe';
 				// run ffprobe on the video file to get value of "color_transfer"
 				$hdr_test_cmd = [$ffprobe_binary,'-v','quiet',
@@ -149,14 +149,16 @@ class Movie extends ProviderV2 {
 					$test_hdr_stdout = trim(stream_get_contents($test_hdr_pipes[1]));
 					$test_hdr_stderr = trim(stream_get_contents($test_hdr_pipes[2]));
 					$test_hdr_returnCode = proc_close($test_hdr_proc);
-					$test_hdr_output = $test_hdr_stdout;
+					$test_hdr_output = $test_hdr_stdout, $test_hdr_stderr;
                 }
-				// only values of "smpte2084" and "arib-std-b67" indicate an HDR video so change $cmd to generate for HDR. otherwise, it is SDR, so do nothing
+				// Only values of "smpte2084" and "arib-std-b67" indicate an HDR video so change $cmd to generate for HDR.
+				// Force colorspace to '2020_ncl' because some videos are tagged incorrectly as 'reserved' resulting in fail
+				// Otherwise, it is SDR, so do nothing
 				if ($test_hdr_output === 'smpte2084' || $test_hdr_output === 'arib-std-b67') {
 					$cmd = [$this->binary, '-y', '-ss', (string)$second,
 					'-i', $absPath,
 					'-f', 'mjpeg', '-vframes', '1',
-					'-vf', 'zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p',
+					'-vf', 'zscale=min=2020_ncl:t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p',
 					$tmpPath];
 				}
 			}
