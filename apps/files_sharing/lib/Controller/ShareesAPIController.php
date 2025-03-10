00d08a4f692f66a697080dbd353bcd6d66a6b67b
Ferdinand Thiessen <opensource@fthiessen.de>
2025-03-10 16:24:26 +0100

    fix(lookup-server): disable lookup server for non-global scale setups
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_sharing/lib/Controller/ShareesAPIController.php
+++ apps/files_sharing/lib/Controller/ShareesAPIController.php
@@ -22,6 +22,7 @@ use OCP\Collaboration\Collaborators\ISearch;
 use OCP\Collaboration\Collaborators\ISearchResult;
 use OCP\Collaboration\Collaborators\SearchResultType;
 use OCP\Constants;
+use OCP\GlobalScale\IConfig as GlobalScaleIConfig;
 use OCP\IConfig;
 use OCP\IRequest;
 use OCP\IURLGenerator;
@@ -175,12 +176,11 @@ class ShareesAPIController extends OCSController {
 		$this->limit = $perPage;
 		$this->offset = $perPage * ($page - 1);
 
-		// In global scale mode we always search the loogup server
-		$lookup = $this->config->getSystemValueBool('gs.enabled', false)
-			|| $this->config->getAppValue('files_sharing', 'lookupServerEnabled', 'no') === 'yes';
-		$this->result['lookupEnabled'] = $lookup;
+		// In global scale mode we always search the lookup server
+		$this->result['lookupEnabled'] = Server::get(GlobalScaleIConfig::class)->isGlobalScaleEnabled();
+		// TODO: Reconsider using lookup server for non-global-scale federation
 
-		[$result, $hasMoreResults] = $this->collaboratorSearch->search($search, $shareTypes, $lookup, $this->limit, $this->offset);
+		[$result, $hasMoreResults] = $this->collaboratorSearch->search($search, $shareTypes, $this->result['lookupEnabled'], $this->limit, $this->offset);
 
 		// extra treatment for 'exact' subarray, with a single merge expected keys might be lost
 		if (isset($result['exact'])) {
