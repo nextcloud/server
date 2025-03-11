bcb2c50c6f3bdd754b5b22f09f39943a4b77a595
Joas Schilling <coding@schilljs.com>
2025-03-11 15:54:58 +0100

    fix(caldav): Restore old private events and deprecate them
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ apps/dav/lib/Events/CalendarObjectDeletedEvent.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCA\DAV\Events;
+
+use OCP\EventDispatcher\Event;
+
+/**
+ * Class CalendarObjectDeletedEvent
+ *
+ * @package OCA\DAV\Events
+ * @since 20.0.0
+ * @deprecated 31.0.2 Use {@see \OCP\Calendar\Events\CalendarObjectDeletedEvent} instead
+ */
+class CalendarObjectDeletedEvent extends Event {
+
+	/**
+	 * CalendarObjectDeletedEvent constructor.
+	 *
+	 * @param int $calendarId
+	 * @param array $calendarData
+	 * @param array $shares
+	 * @param array $objectData
+	 * @since 20.0.0
+	 */
+	public function __construct(
+		private int $calendarId,
+		private array $calendarData,
+		private array $shares,
+		private array $objectData,
+	) {
+		parent::__construct();
+	}
+
+	/**
+	 * @return int
+	 * @since 20.0.0
+	 */
+	public function getCalendarId(): int {
+		return $this->calendarId;
+	}
+
+	/**
+	 * @return array
+	 * @since 20.0.0
+	 */
+	public function getCalendarData(): array {
+		return $this->calendarData;
+	}
+
+	/**
+	 * @return array
+	 * @since 20.0.0
+	 */
+	public function getShares(): array {
+		return $this->shares;
+	}
+
+	/**
+	 * @return array
+	 * @since 20.0.0
+	 */
+	public function getObjectData(): array {
+		return $this->objectData;
+	}
+}
