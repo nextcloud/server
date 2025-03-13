d2178e571ef919497f17c1159f1c4e3a32c2d855
Côme Chilliet <come.chilliet@nextcloud.com>
2025-03-13 15:07:53 +0100

    fix: Fix download activity for folders
    Remove duplicate activity publishing from share controller download,
     listen to BeforeZipCreatedEvent to publish activity for folders, and
     cache folders activity to avoid sending activity for each file in the
     folder.
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/files_sharing/lib/Listener/BeforeNodeReadListener.php
+++ apps/files_sharing/lib/Listener/BeforeNodeReadListener.php
@@ -12,32 +12,79 @@ namespace OCA\Files_Sharing\Listener;
 use OCA\Files_Sharing\Activity\Providers\Downloads;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\Files\Events\BeforeZipCreatedEvent;
 use OCP\Files\Events\Node\BeforeNodeReadEvent;
 use OCP\Files\File;
+use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
+use OCP\Files\NotFoundException;
 use OCP\Files\Storage\ISharedStorage;
+use OCP\ICache;
+use OCP\ICacheFactory;
 use OCP\IRequest;
 use OCP\IUserSession;
 use OCP\Share\IShare;
 
 /**
- * @template-implements IEventListener<BeforeNodeReadEvent|Event>
+ * @template-implements IEventListener<BeforeNodeReadEvent|BeforeZipCreatedEvent|Event>
  */
 class BeforeNodeReadListener implements IEventListener {
+	private ICache $cache;
 
 	public function __construct(
 		private IUserSession $userSession,
 		private IRootFolder $rootFolder,
-		protected \OCP\Activity\IManager $activityManager,
+		private \OCP\Activity\IManager $activityManager,
 		private IRequest $request,
+		ICacheFactory $cacheFactory,
 	) {
+		$this->cache = $cacheFactory->createDistributed('files_sharing_activity_events');
 	}
 
 	public function handle(Event $event): void {
-		if (!($event instanceof BeforeNodeReadEvent)) {
+		if ($event instanceof BeforeZipCreatedEvent) {
+			$this->handleBeforeZipCreatedEvent($event);
+		} elseif ($event instanceof BeforeNodeReadEvent) {
+			$this->handleBeforeNodeReadEvent($event);
+		}
+	}
+
+	public function handleBeforeZipCreatedEvent(BeforeZipCreatedEvent $event): void {
+		$files = $event->getFiles();
+		if (count($files) !== 0) {
+			/* No need to do anything, activity will be triggered for each file in the zip by the BeforeNodeReadEvent */
 			return;
 		}
 
+		$node = $event->getFolder();
+		if (!($node instanceof Folder)) {
+			return;
+		}
+
+		try {
+			$storage = $node->getStorage();
+		} catch (NotFoundException) {
+			return;
+		}
+
+		if (!$storage->instanceOfStorage(ISharedStorage::class)) {
+			return;
+		}
+
+		/** @var ISharedStorage $storage */
+		$share = $storage->getShare();
+
+		if (!in_array($share->getShareType(), [IShare::TYPE_EMAIL, IShare::TYPE_LINK])) {
+			return;
+		}
+
+		/* Cache that that folder download activity was published */
+		$this->cache->set($this->request->getId(), $node->getPath(), 3600);
+
+		$this->singleFileDownloaded($share, $node);
+	}
+
+	public function handleBeforeNodeReadEvent(BeforeNodeReadEvent $event): void {
 		$node = $event->getNode();
 		if (!($node instanceof File)) {
 			return;
@@ -56,13 +103,23 @@ class BeforeNodeReadListener implements IEventListener {
 		/** @var ISharedStorage $storage */
 		$share = $storage->getShare();
 
+		if (!in_array($share->getShareType(), [IShare::TYPE_EMAIL, IShare::TYPE_LINK])) {
+			return;
+		}
+
+		$path = $this->cache->get($this->request->getId());
+		if (is_string($path) && str_starts_with($node->getPath(), $path)) {
+			/* An activity was published for a containing folder already */
+			return;
+		}
+
 		$this->singleFileDownloaded($share, $node);
 	}
 
 	/**
-	 * create activity if a single file was downloaded from a link share
+	 * create activity if a single file or folder was downloaded from a link share
 	 */
-	protected function singleFileDownloaded(IShare $share, File $node): void {
+	protected function singleFileDownloaded(IShare $share, File|Folder $node): void {
 		$fileId = $node->getId();
 
 		$userFolder = $this->rootFolder->getUserFolder($share->getSharedBy());
@@ -74,10 +131,18 @@ class BeforeNodeReadListener implements IEventListener {
 		$parameters = [$userPath];
 
 		if ($share->getShareType() === IShare::TYPE_EMAIL) {
-			$subject = Downloads::SUBJECT_SHARED_FILE_BY_EMAIL_DOWNLOADED;
+			if ($node instanceof File) {
+				$subject = Downloads::SUBJECT_SHARED_FILE_BY_EMAIL_DOWNLOADED;
+			} else {
+				$subject = Downloads::SUBJECT_SHARED_FOLDER_BY_EMAIL_DOWNLOADED;
+			}
 			$parameters[] = $share->getSharedWith();
 		} elseif ($share->getShareType() === IShare::TYPE_LINK) {
-			$subject = Downloads::SUBJECT_PUBLIC_SHARED_FILE_DOWNLOADED;
+			if ($node instanceof File) {
+				$subject = Downloads::SUBJECT_PUBLIC_SHARED_FILE_DOWNLOADED;
+			} else {
+				$subject = Downloads::SUBJECT_PUBLIC_SHARED_FOLDER_DOWNLOADED;
+			}
 			$remoteAddress = $this->request->getRemoteAddress();
 			$dateTime = new \DateTime();
 			$dateTime = $dateTime->format('Y-m-d H');
