e29bb182ad4fac639d9dd1b9cc68b0a2b6acec73
Côme Chilliet <come.chilliet@nextcloud.com>
2025-03-18 15:16:45 +0100

    feat: Add context and test steps for activity in sharing
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

---
+++ build/integration/features/bootstrap/Activity.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+use Behat\Gherkin\Node\TableNode;
+use PHPUnit\Framework\Assert;
+
+trait Activity {
+	use BasicStructure;
+
+	/**
+	 * @Then last activity should be
+	 * @param TableNode $activity
+	 */
+	public function lastActivityIs(TableNode $activity): void {
+		$this->sendRequestForJSON('GET', '/apps/activity/api/v2/activity');
+		$this->theHTTPStatusCodeShouldBe('200');
+		$data = json_decode($this->response->getBody()->getContents(), true);
+		$activities = $data['ocs']['data'];
+		$lastActivity = array_pop($activities);
+		foreach ($activity->getRowsHash() as $key => $value) {
+			Assert::assertEquals($value, $lastActivity[$key]);
+		}
+	}
+}
