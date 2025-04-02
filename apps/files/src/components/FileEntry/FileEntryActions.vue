8078dde33672550d14c8794e99ab6cd06a06fbd1
skjnldsv <skjnldsv@protonmail.com>
2025-04-02 09:18:37 +0200

    fix(files): right click actions menu flicker
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- apps/files/src/components/FileEntry/FileEntryActions.vue
+++ apps/files/src/components/FileEntry/FileEntryActions.vue
@@ -22,8 +22,9 @@
 			type="tertiary"
 			:force-menu="enabledInlineActions.length === 0 /* forceMenu only if no inline actions */"
 			:inline="enabledInlineActions.length"
-			:open.sync="openedMenu"
-			@close="openedSubmenu = null">
+			:open="openedMenu"
+			@close="onMenuClose"
+			@closed="onMenuClosed">
 			<!-- Default actions list-->
 			<NcActionButton v-for="action, index in enabledMenuActions"
 				:key="action.id"
@@ -242,7 +243,7 @@ export default defineComponent({
 	},
 
 	watch: {
-		// Close any submenu when the menu is closed
+		// Close any submenu when the menu state changes
 		openedMenu() {
 			this.openedSubmenu = null
 		},
@@ -333,6 +334,20 @@ export default defineComponent({
 					menuAction.$el.querySelector('button')?.focus()
 				}
 			})
+		},
+
+		onMenuClose() {
+			// We reset the submenu state when the menu is closing
+			this.openedSubmenu = null
+		},
+
+		onMenuClosed() {
+			// TODO: remove timeout once https://github.com/nextcloud-libraries/nextcloud-vue/pull/6683 is merged
+			// and updated on server.
+			setTimeout(() => {
+				// We reset the actions menu state when the menu is finally closed
+				this.openedMenu = false
+			}, 100)
 		},
 
 		t,
