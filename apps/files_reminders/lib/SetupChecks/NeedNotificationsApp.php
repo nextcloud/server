55929c942d59e789995be7bc9601566eb04a6684
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-04 16:12:45 +0200

    fix(files_reminders): add l10n to constructor of setup check
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_reminders/lib/SetupChecks/NeedNotificationsApp.php
+++ apps/files_reminders/lib/SetupChecks/NeedNotificationsApp.php
@@ -10,12 +10,14 @@ declare(strict_types=1);
 namespace OCA\FilesReminders\SetupChecks;
 
 use OCP\App\IAppManager;
+use OCP\IL10N;
 use OCP\SetupCheck\ISetupCheck;
 use OCP\SetupCheck\SetupResult;
 
 class NeedNotificationsApp implements ISetupCheck {
 	public function __construct(
 		private IAppManager $appManager,
+		private IL10N $l10n,
 	) {
 	}
 
@@ -28,7 +30,7 @@ class NeedNotificationsApp implements ISetupCheck {
 	}
 
 	public function run(): SetupResult {
-		if ($this->appManager->isInstalled('notifications')) {
+		if ($this->appManager->isEnabledForAnyone('notifications')) {
 			return SetupResult::success($this->l10n->t('This files_reminder can work properly.'));
 		} else {
 			return SetupResult::warning($this->l10n->t('The files_reminder app needs the notification app to work properly. You should either enable notifications or disable files_reminder.'));
