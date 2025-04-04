b29207ce6be6a6fe0bd0a911a2c506bbe999d994
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-04 16:02:00 +0200

    fix(files_reminders): add missing import
    - Resolves https://github.com/nextcloud/server/issues/51943
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_reminders/lib/SetupChecks/NeedNotificationsApp.php
+++ apps/files_reminders/lib/SetupChecks/NeedNotificationsApp.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\DAV\SetupChecks;
+namespace OCA\FilesReminders\SetupChecks;
 
 use OCP\App\IAppManager;
 use OCP\SetupCheck\ISetupCheck;
