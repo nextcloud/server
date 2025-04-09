6a3c53def3155dcff4aa6bd532e34beab8918035
Joas Schilling <coding@schilljs.com>
2025-04-09 10:22:40 +0200

    fix(federation): Don't load the addressbook when resolving a cloud ID
    Instead we delay the lookup of the display name until it is actually used
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/lib/Federation/CloudIdTest.php
+++ tests/lib/Federation/CloudIdTest.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2017 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -7,25 +10,42 @@
 namespace Test\Federation;
 
 use OC\Federation\CloudId;
+use OC\Federation\CloudIdManager;
+use OCP\Federation\ICloudIdManager;
+use PHPUnit\Framework\MockObject\MockObject;
 use Test\TestCase;
 
+/**
+ * @group DB
+ */
 class CloudIdTest extends TestCase {
-	public function dataGetDisplayCloudId() {
+	protected CloudIdManager&MockObject $cloudIdManager;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->cloudIdManager = $this->createMock(CloudIdManager::class);
+		$this->overwriteService(ICloudIdManager::class, $this->cloudIdManager);
+	}
+
+	public function dataGetDisplayCloudId(): array {
 		return [
-			['test@example.com', 'test@example.com'],
-			['test@http://example.com', 'test@example.com'],
-			['test@https://example.com', 'test@example.com'],
+			['test@example.com', 'test', 'example.com', 'test@example.com'],
+			['test@http://example.com', 'test', 'http://example.com', 'test@example.com'],
+			['test@https://example.com', 'test', 'https://example.com', 'test@example.com'],
+			['test@https://example.com', 'test', 'https://example.com', 'Beloved Amy@example.com', 'Beloved Amy'],
 		];
 	}
 
 	/**
 	 * @dataProvider dataGetDisplayCloudId
-	 *
-	 * @param string $id
-	 * @param string $display
 	 */
-	public function testGetDisplayCloudId($id, $display): void {
-		$cloudId = new CloudId($id, '', '');
+	public function testGetDisplayCloudId(string $id, string $user, string $remote, string $display, ?string $addressbookName = null): void {
+		$this->cloudIdManager->expects($this->once())
+			->method('getDisplayNameFromContact')
+			->willReturn($addressbookName);
+
+		$cloudId = new CloudId($id, $user, $remote);
 		$this->assertEquals($display, $cloudId->getDisplayId());
 	}
 }
