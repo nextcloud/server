f5f773bd1cb2eb0ea2539390aacca84acb988f7e
Maxence Lange <maxence@artificial-owl.com>
2025-04-16 09:08:42 -0100

    fix(sharing): ignore inexisting memberships during reshare check
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- apps/files_sharing/lib/Controller/ShareAPIController.php
+++ apps/files_sharing/lib/Controller/ShareAPIController.php
@@ -1992,7 +1992,7 @@ class ShareAPIController extends OCSController {
 					return true;
 				}
 				return false;
-			} catch (ContainerExceptionInterface $e) {
+			} catch (\Exception) {
 				return false;
 			}
 		}
