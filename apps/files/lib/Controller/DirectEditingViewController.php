6afb8d797cad257755f91276e46ffb289fe4d44f
lagsys <dev@lag.systems>
2025-05-11 01:01:04 +0200

    Avoid loading text editor using token URL
    Signed-off-by: lagsys <dev@lag.systems>

--- apps/files/lib/Controller/DirectEditingViewController.php
+++ apps/files/lib/Controller/DirectEditingViewController.php
@@ -13,10 +13,13 @@ use OCP\AppFramework\Http\Attribute\PublicPage;
 use OCP\AppFramework\Http\Attribute\UseSession;
 use OCP\AppFramework\Http\NotFoundResponse;
 use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\RedirectResponse;
 use OCP\DirectEditing\IManager;
 use OCP\DirectEditing\RegisterDirectEditorEvent;
 use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\IRootFolder;
 use OCP\IRequest;
+use OCP\IURLGenerator;
 use Psr\Log\LoggerInterface;
 
 #[OpenAPI(scope: OpenAPI::SCOPE_IGNORE)]
@@ -27,6 +30,8 @@ class DirectEditingViewController extends Controller {
 		private IEventDispatcher $eventDispatcher,
 		private IManager $directEditingManager,
 		private LoggerInterface $logger,
+		private IRootFolder $rootFolder,
+		private IURLGenerator $urlGenerator
 	) {
 		parent::__construct($appName, $request);
 	}
@@ -40,8 +45,30 @@ class DirectEditingViewController extends Controller {
 	#[UseSession]
 	public function edit(string $token): Response {
 		$this->eventDispatcher->dispatchTyped(new RegisterDirectEditorEvent($this->directEditingManager));
+		$token = $this->directEditingManager->getToken($token);
+		$token->useTokenScope();
 		try {
-			return $this->directEditingManager->edit($token);
+			$params = ['view' => 'files'];
+
+			$baseFolder = $this->rootFolder->getUserFolder($token->getUser());
+			$node = $baseFolder->getFirstNodeById($token->getFile()->getId());
+
+			if ($node) {
+				if ($node instanceof Folder) {
+					// set the full path to enter the folder
+					$params['dir'] = $baseFolder->getRelativePath($node->getPath());
+				} else {
+					// set parent path as dir
+					$params['dir'] = $baseFolder->getRelativePath($node->getParent()->getPath());
+					// open the file by default (opening the viewer)
+					$params['openfile'] = 'true';
+				}
+			}
+			$params['fileid'] = $token->getFile()->getId();
+
+			return new RedirectResponse($this->urlGenerator->linkToRoute('files.view.indexViewFileid', $params));
+
+
 		} catch (Exception $e) {
 			$this->logger->error($e->getMessage(), ['exception' => $e]);
 			return new NotFoundResponse();
