cfc41960388af724f4deb211e60a16a5a008855f
Robin Appelman <robin@icewind.nl>
2025-05-12 17:35:17 +0200

    feat: add mount id to info:storage(s)
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- core/Command/Info/Storage.php
+++ core/Command/Info/Storage.php
@@ -12,7 +12,6 @@ use OC\Core\Command\Base;
 use OCP\IDBConnection;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Storage extends Base {
