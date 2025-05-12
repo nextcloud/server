1e72620169ca689d6aa3a7b1d47524bfd7bbe1b4
Robin Appelman <robin@icewind.nl>
2025-05-12 18:33:26 +0200

    fix: fix unjailedroot of nested jails if there are other wrappers in between
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Files/Cache/Wrapper/CacheJail.php
+++ lib/private/Files/Cache/Wrapper/CacheJail.php
@@ -31,10 +31,13 @@ class CacheJail extends CacheWrapper {
 	) {
 		parent::__construct($cache, $dependencies);
 
-		if ($cache instanceof CacheJail) {
-			$this->unjailedRoot = $cache->getSourcePath($root);
-		} else {
-			$this->unjailedRoot = $root;
+		$this->unjailedRoot = $root;
+		$parent = $cache;
+		while ($parent instanceof CacheWrapper) {
+			if ($parent instanceof CacheJail) {
+				$this->unjailedRoot = $parent->getSourcePath($this->unjailedRoot);
+			}
+			$parent = $parent->getCache();
 		}
 	}
 
