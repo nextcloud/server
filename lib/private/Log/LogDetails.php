01e2e3076702cd0a2b1dfa35f9a6fa97e4af1d37
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-13 11:18:20 +0200

    fix(log): Only log client request id if present, and at the end
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/Log/LogDetails.php
+++ lib/private/Log/LogDetails.php
@@ -47,10 +47,8 @@ abstract class LogDetails {
 			$userAgent = '--';
 		}
 		$version = $this->config->getValue('version', '');
-		$clientReqId = $request->getHeader('X-Request-Id');
 		$entry = compact(
 			'reqId',
-			'clientReqId',
 			'level',
 			'time',
 			'remoteAddr',
@@ -62,6 +60,10 @@ abstract class LogDetails {
 			'userAgent',
 			'version'
 		);
+		$clientReqId = $request->getHeader('X-Request-Id');
+		if ($clientReqId !== '') {
+			$entry['clientReqId'] = $clientReqId;
+		}
 
 		if (is_array($message)) {
 			// Exception messages are extracted and the exception is put into a separate field
