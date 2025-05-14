ad34a7c99174316b5fe09b60a72af88360c007e9
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-14 18:42:42 +0200

    feat: Add a configuration toggle for lazy objects in DI
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/base.php
+++ lib/base.php
@@ -631,6 +631,9 @@ class OC {
 			exit();
 		}
 
+		// Enable lazy loading if activated
+		\OC\AppFramework\Utility\SimpleContainer::$useLazyObjects = (bool)self::$config->getValue('enable_lazy_objects');
+
 		// setup the basic server
 		self::$server = new \OC\Server(\OC::$WEBROOT, self::$config);
 		self::$server->boot();
