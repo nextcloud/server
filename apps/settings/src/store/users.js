1301cf5a94d9697da045f5b9177dabb6268cae26
nfebe <fenn25.fn@gmail.com>
2025-05-15 15:24:03 +0100

    fix(settings): Prevent double request on manager change
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- apps/settings/src/store/users.js
+++ apps/settings/src/store/users.js
@@ -780,9 +780,8 @@ const actions = {
 
 		try {
 			await api.requireAdmin()
-			const commitValue = value === '' ? '' : value
 			await api.put(generateOcsUrl('cloud/users/{userid}', { userid }), { key, value })
-			return context.commit('setUserData', { userid, key, value: commitValue })
+			return context.commit('setUserData', { userid, key, value })
 		} catch (error) {
 			context.commit('API_FAILURE', { userid, error })
 			throw error
