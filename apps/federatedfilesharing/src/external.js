e7c31da4067c82234dcac445861d2b89745f3ea7
Mahdi Baghbani <Mahdi.baghbani1@gmail.com>
2025-05-17 15:47:23 +0330

    Update apps/federatedfilesharing/src/external.js
    Co-authored-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Signed-off-by: Mahdi Baghbani <Mahdi.baghbani1@gmail.com>

--- apps/federatedfilesharing/src/external.js
+++ apps/federatedfilesharing/src/external.js
@@ -35,7 +35,7 @@ window.OCA.Sharing.showAddExternalDialog = function(share, passwordProtected, ca
 		.replace(/\/$/, '') // remove trailing slash
 
 	showRemoteShareDialog(name, owner, remote, passwordProtected)
-		.then(({ accepted, password }) => callback(accepted, { ...share, password }))
+		.then((password) => callback(true, { ...share, password }))
 		// eslint-disable-next-line n/no-callback-literal
 		.catch(() => callback(false, share))
 }
