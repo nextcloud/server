ea0f589c327ef0cbf2e114265fcd5898a203605a
Louis Chemineau <louis@chmn.me>
2025-05-21 11:18:39 +0200

    feat: Support limit argument in `getSeenUsers`
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/public/IUserManager.php
+++ lib/public/IUserManager.php
@@ -239,8 +239,10 @@ interface IUserManager {
 	 * The offset argument allows the caller to continue the iteration at a specific offset.
 	 *
 	 * @param int $offset from which offset to fetch
+	 * @param int|null $limit maximum number of records to fetch
 	 * @return \Iterator<IUser> list of IUser object
 	 * @since 29.0.15
+	 * @since 29.0.16 Added the $limit argument
 	 */
-	public function getSeenUsers(int $offset = 0): \Iterator;
+	public function getSeenUsers(int $offset = 0, ?int $limit = null): \Iterator;
 }
