8b9882c5e67d45673c7c0748345f35a44f73ab76
Marcel Klehr <mklehr@gmx.net>
2025-05-23 09:34:28 +0200

    fix: Correct translation method use
    Co-authored-by: Joas Schilling <213943+nickvergessen@users.noreply.github.com>
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- apps/settings/lib/SetupChecks/TaskProcessingPickupSpeed.php
+++ apps/settings/lib/SetupChecks/TaskProcessingPickupSpeed.php
@@ -55,9 +55,9 @@ class TaskProcessingPickupSpeed implements ISetupCheck {
 		}
 
 		if ($slowCount / $taskCount < self::MAX_SLOW_PERCENTAGE) {
-			return SetupResult::success($this->l10n->t('The task pickup speed has been ok in the last {hours} hours.', ['hours' => self::TIME_SPAN]));
+			return SetupResult::success($this->l10n->n('The task pickup speed has been ok in the last %n hour.', 'The task pickup speed has been ok in the last %n hours.', self::TIME_SPAN));
 		} else {
-			return SetupResult::warning($this->l10n->t('The task pickup speed has been slow in the last {hours} hours. Many tasks took longer than 4 minutes to be picked up. Consider setting up a worker to process tasks in the background.', ['hours' => self::TIME_SPAN]), 'https://docs.nextcloud.com/server/latest/admin_manual/ai/overview.html#improve-ai-task-pickup-speed');
+			return SetupResult::warning($this->l10n->n('The task pickup speed has been slow in the last %n hour. Many tasks took longer than 4 minutes to be picked up. Consider setting up a worker to process tasks in the background.', 'The task pickup speed has been slow in the last %n hours. Many tasks took longer than 4 minutes to be picked up. Consider setting up a worker to process tasks in the background.', self::TIME_SPAN), 'https://docs.nextcloud.com/server/latest/admin_manual/ai/overview.html#improve-ai-task-pickup-speed');
 		}
 	}
 }
