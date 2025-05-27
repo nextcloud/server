7eb607c8b9da0445f3b07c304882fba5675020f7
Robin Appelman <robin@icewind.nl>
2025-05-27 19:29:44 +0200

    perf: set the folder size in the same query as we create it
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Files/ObjectStore/ObjectStoreStorage.php
+++ lib/private/Files/ObjectStore/ObjectStoreStorage.php
@@ -65,7 +65,7 @@ class ObjectStoreStorage extends \OC\Files\Storage\Common implements IChunkedFil
 		$this->logger = \OCP\Server::get(LoggerInterface::class);
 	}
 
-	public function mkdir($path, bool $force = false) {
+	public function mkdir($path, bool $force = false, array $metadata = []) {
 		$path = $this->normalizePath($path);
 		if (!$force && $this->file_exists($path)) {
 			$this->logger->warning("Tried to create an object store folder that already exists: $path");
@@ -75,7 +75,7 @@ class ObjectStoreStorage extends \OC\Files\Storage\Common implements IChunkedFil
 		$mTime = time();
 		$data = [
 			'mimetype' => 'httpd/unix-directory',
-			'size' => 0,
+			'size' => $metadata['size'] ?? 0,
 			'mtime' => $mTime,
 			'storage_mtime' => $mTime,
 			'permissions' => \OCP\Constants::PERMISSION_ALL,
@@ -712,11 +712,7 @@ class ObjectStoreStorage extends \OC\Files\Storage\Common implements IChunkedFil
 			if ($cache->inCache($to)) {
 				$cache->remove($to);
 			}
-			$this->mkdir($to);
-			$cacheEntry = $cache->get(($to));
-			$cache->update($cacheEntry->getId(), [
-				'size' => $sourceEntry->getSize(),
-			]);
+			$this->mkdir($to, false, ['size' => $sourceEntry->getSize()]);
 
 			foreach ($sourceCache->getFolderContentsById($sourceEntry->getId()) as $child) {
 				$this->copyInner($sourceCache, $child, $to . '/' . $child->getName());
