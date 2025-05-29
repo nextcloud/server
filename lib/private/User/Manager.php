8b70f59015b4a7eae9a893c884de9d943697ddaa
Joas Schilling <coding@schilljs.com>
2025-05-29 09:18:12 +0200

    chore(cs): Apply autofixes
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/User/Manager.php
+++ lib/private/User/Manager.php
@@ -724,7 +724,7 @@ class Manager extends PublicEmitter implements IUserManager {
 
 		// User ID is too long
 		if (strlen($uid) > IUser::MAX_USERID_LENGTH) {
-		    // TRANSLATORS User ID is too long
+			// TRANSLATORS User ID is too long
 			throw new \InvalidArgumentException($l->t('Username is too long'));
 		}
 
