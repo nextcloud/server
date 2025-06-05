9ff3f600c0090c3ec11526cc617fa825c238b560
Annabel Church <215145+arc64@users.noreply.github.com>
2025-06-05 11:45:15 +0000

    fix(settings): show active forced locale or language instead of 'No locale set' (fixes #41543)
    Signed-off-by: Annabel Church <215145+arc64@users.noreply.github.com>

--- apps/settings/src/components/PersonalInfo/LocaleSection/LocaleSection.vue
+++ apps/settings/src/components/PersonalInfo/LocaleSection/LocaleSection.vue
@@ -14,6 +14,9 @@
 			:other-locales="otherLocales"
 			:locale.sync="locale" />
 
+		<span v-else-if="forcedLocale && forcedLocale.name">
+			{{ t('settings', 'Locale is forced to {locale} by the administrator', { locale: forcedLocale.name }) }}
+		</span>
 		<span v-else>
 			{{ t('settings', 'No locale set') }}
 		</span>
@@ -22,13 +25,12 @@
 
 <script>
 import { loadState } from '@nextcloud/initial-state'
+import { t } from '@nextcloud/l10n'
 
 import Locale from './Locale.vue'
 import HeaderBar from '../shared/HeaderBar.vue'
 
 import { ACCOUNT_SETTING_PROPERTY_ENUM, ACCOUNT_SETTING_PROPERTY_READABLE_ENUM } from '../../../constants/AccountPropertyConstants.js'
-
-const { localeMap: { activeLocale, localesForLanguage, otherLocales } } = loadState('settings', 'personalInfoParameters', {})
 
 export default {
 	name: 'LocaleSection',
@@ -38,12 +40,26 @@ export default {
 		HeaderBar,
 	},
 
-	data() {
+	setup() {
+		// Non reactive instance properties
 		return {
 			propertyReadable: ACCOUNT_SETTING_PROPERTY_READABLE_ENUM.LOCALE,
-			localesForLanguage,
-			otherLocales,
-			locale: activeLocale,
+		}
+	},
+
+	data() {
+		const state = loadState('settings', 'personalInfoParameters', {})
+		const { activeLocale, localesForLanguage, otherLocales, forcedLocale } = state.localeMap || {}
+		return {
+			locale: activeLocale || null,
+			forcedLocale: forcedLocale && forcedLocale.name
+				? {
+					code: forcedLocale.code,
+					name: forcedLocale.name,
+				}
+				: null,
+			localesForLanguage: forcedLocale ? [] : (localesForLanguage || []),
+			otherLocales: forcedLocale ? [] : (otherLocales || []),
 		}
 	},
 
@@ -53,8 +69,13 @@ export default {
 		},
 
 		isEditable() {
-			return Boolean(this.locale)
+			// Return false if there's no locale data or if locale is forced
+			return !this.forcedLocale && (this.locale !== null || this.localesForLanguage.length > 0)
 		},
+	},
+
+	methods: {
+		t,
 	},
 }
 </script>
