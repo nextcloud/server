44c7a95ac48a50a327ef1f5b4e198d281851c8b4
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-07 16:35:46 +0200

    test: reenable localstack tests (bug fixed) but disable MinIO versioned copy test
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_external/tests/Storage/VersionedAmazonS3Test.php
+++ apps/files_external/tests/Storage/VersionedAmazonS3Test.php
@@ -26,4 +26,12 @@ class VersionedAmazonS3Test extends Amazons3Test {
 			$this->markTestSkipped("s3 backend doesn't seem to support versioning");
 		}
 	}
+
+	public function testCopyOverWriteDirectory(): void {
+		if (isset($this->config['minio'])) {
+			$this->markTestSkipped('MinIO has a bug with batch deletion on versioned storages, see https://github.com/minio/minio/issues/21366');
+		}
+
+		parent::testCopyOverWriteDirectory();
+	}
 }
