9fa9ffd22971c3324209f95a899ed5e34c744237
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-07 16:35:46 +0200

    test: reenable localstack tests (bug fixed) but disable MinIO versioned copy test
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_external/tests/Storage/Amazons3Test.php
+++ apps/files_external/tests/Storage/Amazons3Test.php
@@ -17,7 +17,7 @@ use OCA\Files_External\Lib\Storage\AmazonS3;
  * @package OCA\Files_External\Tests\Storage
  */
 class Amazons3Test extends \Test\Files\Storage\Storage {
-	private $config;
+	protected $config;
 	/** @var AmazonS3 */
 	protected $instance;
 
@@ -25,7 +25,7 @@ class Amazons3Test extends \Test\Files\Storage\Storage {
 		parent::setUp();
 
 		$this->config = include('files_external/tests/config.amazons3.php');
-		if (! is_array($this->config) or ! $this->config['run']) {
+		if (!is_array($this->config) || !$this->config['run']) {
 			$this->markTestSkipped('AmazonS3 backend not configured');
 		}
 		$this->instance = new AmazonS3($this->config);
@@ -41,12 +41,5 @@ class Amazons3Test extends \Test\Files\Storage\Storage {
 
 	public function testStat(): void {
 		$this->markTestSkipped('S3 doesn\'t update the parents folder mtime');
-	}
-
-	public function testHashInFileName(): void {
-		if (isset($this->config['localstack'])) {
-			$this->markTestSkipped('Localstack has a bug with hashes in filename');
-		}
-		parent::testHashInFileName();
 	}
 }
