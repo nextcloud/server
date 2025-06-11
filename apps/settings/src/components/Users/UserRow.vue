c2a294453949f7fafbbb15fa10630e3e2b0277ad
Maksim Sukharev <antreesy.web@gmail.com>
2025-06-11 14:39:37 +0200

    fix(UserRow): fallback to group name in case of undefined availableGroup
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- apps/settings/src/components/Users/UserRow.vue
+++ apps/settings/src/components/Users/UserRow.vue
@@ -422,7 +422,7 @@ export default {
 				.map(group => {
 					// Try to match with more extensive group data
 					const availableGroup = this.availableGroups.find(g => g.id === group.id)
-					return availableGroup.name ?? group.name ?? group.id
+					return availableGroup?.name ?? group.name ?? group.id
 				})
 				.join(', ')
 		},
@@ -432,7 +432,7 @@ export default {
 				.map(group => {
 					// Try to match with more extensive group data
 					const availableGroup = this.availableSubAdminGroups.find(g => g.id === group.id)
-					return availableGroup.name ?? group.name ?? group.id
+					return availableGroup?.name ?? group.name ?? group.id
 				})
 				.join(', ')
 		},
