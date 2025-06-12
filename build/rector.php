3cc34ac29da76e17952bfc84ddc1a5f066680d8f
Robin Appelman <robin@icewind.nl>
2025-06-12 18:33:43 +0200

    chore: add UseSpecificWillMethodRector to rector configuration
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- build/rector.php
+++ build/rector.php
@@ -12,6 +12,7 @@ use PhpParser\Node;
 use Rector\CodingStyle\Contract\ClassNameImport\ClassNameImportSkipVoterInterface;
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\UseSpecificWillMethodRector;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\ValueObject\Application\File;
 
@@ -79,6 +80,9 @@ $config = RectorConfig::configure()
 	// ->withPhpSets()
 	->withImportNames(importShortClasses:false)
 	->withTypeCoverageLevel(0)
+	->withRules([
+		UseSpecificWillMethodRector::class
+	])
 	->withConfiguredRule(ClassPropertyAssignToConstructorPromotionRector::class, [
 		'inline_public' => true,
 		'rename_property' => true,
