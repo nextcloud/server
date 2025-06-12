15b1127632d31419d0af95c919454f6cd5ba3ed3
Julius Knorr <jus@bitgrid.net>
2025-06-12 11:47:05 +0200

    tmp: Skip size and etag propagation updates for end to end metadata
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- lib/private/Files/Cache/Updater.php
+++ lib/private/Files/Cache/Updater.php
@@ -120,6 +120,11 @@ class Updater implements IUpdater {
 			$sizeDifference = null;
 		}
 
+		$appDataPath = 'appdata_' . \OC_Util::getInstanceId() . '/end_to_end_encryption/meta-data';
+		if (str_starts_with($path, $appDataPath)) {
+			return;
+		}
+
 		// scanner didn't provide size info, fallback to full size calculation
 		if ($this->cache instanceof Cache && $sizeDifference === null) {
 			$this->cache->correctFolderSize($path, $data);
