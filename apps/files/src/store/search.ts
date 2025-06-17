e965eea73f440d90da9b124af48efe2fd230274e
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-17 19:28:36 +0200

    fixup! feat(files): add search view

--- apps/files/src/store/search.ts
+++ apps/files/src/store/search.ts
@@ -1,4 +1,10 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import type { INode } from '@nextcloud/files'
+import type RouterService from '../services/RouterService'
 import type { SearchScope } from '../types'
 
 import { defineStore } from 'pinia'
@@ -25,8 +31,49 @@ export const useSearchStore = defineStore('search', () => {
 	 */
 	const scope = ref<SearchScope>('filter')
 
-	watch(query, () => {
+	watch(query, (old, current) => {
+		// skip if only whitespaces changed
+		if (old.trim() === current.trim()) {
+			return
+		}
+
+		// emit the search event to update the filter
 		emit('files:search:updated', { query: query.value, scope: scope.value })
+
+		if (scope.value === 'filter') {
+			// all done for filtering
+			return
+		}
+
+		const router = window.OCP.Files.Router as RouterService
+		if (query.value === '' && router.params.view === 'search') {
+			return router.goToRoute(
+				undefined,
+				{
+					view: 'files',
+				},
+				{
+					...router.query,
+					query: undefined,
+				},
+			)
+		}
+
+		// we only use the directory if we search locally
+		const dir = scope.value === 'locally' ? base.value?.path : undefined
+		const isSearch = router.params.view === 'search'
+
+		router.goToRoute(
+			undefined,
+			{
+				view: 'search',
+			},
+			{
+				query: query.value,
+				dir,
+			},
+			isSearch,
+		)
 	})
 
 	return {
