a402a84544533fbc2169a92e31937eb2cbe9dc3b
Richard Steinmetz <richard@steinmetz.cloud>
2025-06-20 10:13:56 +0200

    fix: revive always storing lowercased email addresses
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

---
+++ core/Migrations/Version32000Date20250620081925.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\Core\Migrations;
+
+/**
+ * Run the old migration Version24000Date20211210141942 again.
+ */
+class Version32000Date20250620081925 extends Version24000Date20211210141942 {
+}
