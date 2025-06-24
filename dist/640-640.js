d1f0b363bd5f87d4dc550359aa1d288e6338c07f
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-06-24 19:04:35 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- dist/640-640.js
+++ dist/640-640.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunknextcloud=self.webpackChunknextcloud||[]).push([[640],{60640:(e,c,l)=>{l.d(c,{FilePickerVue:()=>n});const n=(0,l(85471).$V)((()=>Promise.all([l.e(4208),l.e(2983)]).then(l.bind(l,84057))))}}]);
-//# sourceMappingURL=640-640.js.map?v=19a5506f19df079e3812
+"use strict";(self.webpackChunknextcloud=self.webpackChunknextcloud||[]).push([[640],{60640:(e,c,l)=>{l.d(c,{FilePickerVue:()=>n});const n=(0,l(85471).$V)((()=>Promise.all([l.e(4208),l.e(5810),l.e(2983)]).then(l.bind(l,84057))))}}]);
+//# sourceMappingURL=640-640.js.map?v=eeb4f7c2b5e3c52c0dbf
