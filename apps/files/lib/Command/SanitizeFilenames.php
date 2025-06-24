dffc7412010d4c50e68e1587c9a3371136a4dcd7
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-24 11:07:45 +0200

    fix(files): add more debug output to the filename sanitizing command
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files/lib/Command/SanitizeFilenames.php
+++ apps/files/lib/Command/SanitizeFilenames.php
@@ -137,8 +137,9 @@ class SanitizeFilenames extends Base {
 				$this->output->writeln('<comment>skipping: ' . $node->getPath() . ' (file is locked)</>');
 			} catch (NotPermittedException) {
 				$this->output->writeln('<comment>skipping: ' . $node->getPath() . ' (no permissions)</>');
-			} catch (Exception) {
+			} catch (Exception $error) {
 				$this->output->writeln('<error>failed: ' . $node->getPath() . '</>');
+				$this->output->writeln('<error>' . $error->getMessage() . '</>', OutputInterface::OUTPUT_NORMAL | OutputInterface::VERBOSITY_VERBOSE);
 			}
 
 			if ($node instanceof Folder) {
