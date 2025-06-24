e1d7274491151c68e9f6b239b33c7bc825bd03e6
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-24 15:03:30 +0200

    feat(files): add `search` view
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ apps/files/src/views/SearchEmptyView.vue
@@ -0,0 +1,45 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import { mdiMagnifyClose } from '@mdi/js'
+import { t } from '@nextcloud/l10n'
+import debounce from 'debounce'
+import NcButton from '@nextcloud/vue/components/NcButton'
+import NcEmptyContent from '@nextcloud/vue/components/NcEmptyContent'
+import NcIconSvgWrapper from '@nextcloud/vue/components/NcIconSvgWrapper'
+import NcInputField from '@nextcloud/vue/components/NcInputField'
+import { getPinia } from '../store/index.ts'
+import { useSearchStore } from '../store/search.ts'
+
+const searchStore = useSearchStore(getPinia())
+const debouncedUpdate = debounce((value: string) => {
+	searchStore.query = value
+}, 500)
+</script>
+
+<template>
+	<NcEmptyContent :name="t('files', 'No search results for “{query}”', { query: searchStore.query })">
+		<template #icon>
+			<NcIconSvgWrapper :path="mdiMagnifyClose" />
+		</template>
+		<template #action>
+			<NcInputField class="search-empty-view__input"
+				:label="t('files', 'Search for files')"
+				:model-value="searchStore.query"
+				type="search"
+				@update:model-value="debouncedUpdate" />
+			<NcButton v-if="searchStore.scope === 'locally'" @click="searchStore.scope = 'globally'">
+				{{ t('files', 'Search globally') }}
+			</NcButton>
+		</template>
+	</NcEmptyContent>
+</template>
+
+<style scoped lang="scss">
+.search-empty-view__input {
+	min-width: min(400px, 50vw);
+}
+</style>
