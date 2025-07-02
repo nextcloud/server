af059cbf612afb31ef2c7ef813080e79ffde7e45
Julien Veyssier <julien-nc@posteo.net>
2025-07-02 12:10:01 +0200

    feat(TaskProcessing): add audio-to-audio chat task type
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/private/TaskProcessing/Manager.php
+++ lib/private/TaskProcessing/Manager.php
@@ -589,6 +589,7 @@ class Manager implements IManager {
 			\OCP\TaskProcessing\TaskTypes\ContextAgentInteraction::ID => \OCP\Server::get(\OCP\TaskProcessing\TaskTypes\ContextAgentInteraction::class),
 			\OCP\TaskProcessing\TaskTypes\TextToTextProofread::ID => \OCP\Server::get(\OCP\TaskProcessing\TaskTypes\TextToTextProofread::class),
 			\OCP\TaskProcessing\TaskTypes\TextToSpeech::ID => \OCP\Server::get(\OCP\TaskProcessing\TaskTypes\TextToSpeech::class),
+			\OCP\TaskProcessing\TaskTypes\AudioToAudioChat::ID => \OCP\Server::get(\OCP\TaskProcessing\TaskTypes\AudioToAudioChat::class),
 		];
 
 		foreach ($context->getTaskProcessingTaskTypes() as $providerServiceRegistration) {
