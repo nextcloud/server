77c21c8d5e6f22d9b1b49961adef1e8b0cc63dc3
divyanshsirohi <divyansh250604@gmail.com>
2025-07-04 21:51:25 +0530

    Fix: Add spacing between Shared label and icon in AppMenuEntry.vue

--- core/src/components/AppMenuEntry.vue
+++ core/src/components/AppMenuEntry.vue
@@ -85,7 +85,7 @@ watch(() => props.app.name, calculateSize)
 		text-overflow: ellipsis;
 		overflow: hidden;
 		letter-spacing: -0.5px;
-		gap: 8px 
+		margin-left: 8px 
 	}
 	body[dir=rtl] &__label {
 		transform: translateX(50%) !important;
@@ -93,6 +93,7 @@ watch(() => props.app.name, calculateSize)
 
 	&__icon {
 		font-size: var(--app-menu-entry-font-size);
+		margin-right: 8px;
 	}
 
 	&--active {
