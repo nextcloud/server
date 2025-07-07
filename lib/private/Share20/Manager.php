5a4faba3a87ee8f1881f82ddb6dc2ae8618b79bf
Robin Appelman <robin@icewind.nl>
2025-07-07 17:03:14 +0200

    fix: don't check if a custom share folder is configured for non-user shares
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -706,7 +706,7 @@ class Manager implements IManager {
 			// Generate the target
 			$defaultShareFolder = $this->config->getSystemValue('share_folder', '/');
 			$allowCustomShareFolder = $this->config->getSystemValueBool('sharing.allow_custom_share_folder', true);
-			if ($allowCustomShareFolder) {
+			if ($share->getShareType() === IShare::TYPE_USER && $allowCustomShareFolder) {
 				$shareFolder = $this->config->getUserValue($share->getSharedWith(), Application::APP_ID, 'share_folder', $defaultShareFolder);
 			} else {
 				$shareFolder = $defaultShareFolder;
