1ea9b5f3b24383a263858955e9d3d3608fa1dd68
provokateurin <kate@provokateurin.de>
2025-07-14 09:28:24 +0200

    fixup! perf(AppConfig): Cache loading the app config from the database

--- lib/private/AppConfig.php
+++ lib/private/AppConfig.php
@@ -22,6 +22,7 @@ use OCP\Exceptions\AppConfigIncorrectTypeException;
 use OCP\Exceptions\AppConfigTypeConflictException;
 use OCP\Exceptions\AppConfigUnknownKeyException;
 use OCP\IAppConfig;
+use OCP\ICache;
 use OCP\ICacheFactory;
 use OCP\IConfig;
 use OCP\IDBConnection;
@@ -69,12 +70,15 @@ class AppConfig implements IAppConfig {
 	/** @var ?array<string, string> */
 	private ?array $appVersionsCache = null;
 
+	private ?ICache $localCache;
+
 	public function __construct(
 		protected IDBConnection $connection,
 		protected LoggerInterface $logger,
 		protected ICrypto $crypto,
-		protected ICacheFactory $cacheFactory,
+		ICacheFactory $cacheFactory,
 	) {
+		$this->localCache = $cacheFactory->isLocalCacheAvailable() ? $cacheFactory->createLocal() : null;
 	}
 
 	/**
@@ -861,6 +865,8 @@ class AppConfig implements IAppConfig {
 		}
 		$this->valueTypes[$app][$key] = $type;
 
+		$this->clearLocalCache();
+
 		return true;
 	}
 
@@ -1120,6 +1126,7 @@ class AppConfig implements IAppConfig {
 		unset($this->lazyCache[$app][$key]);
 		unset($this->fastCache[$app][$key]);
 		unset($this->valueTypes[$app][$key]);
+		$this->clearLocalCache();
 	}
 
 	/**
@@ -1149,6 +1156,7 @@ class AppConfig implements IAppConfig {
 	public function clearCache(bool $reload = false): void {
 		$this->lazyLoaded = $this->fastLoaded = false;
 		$this->lazyCache = $this->fastCache = $this->valueTypes = [];
+		$this->clearLocalCache();
 
 		if (!$reload) {
 			return;
@@ -1219,6 +1227,21 @@ class AppConfig implements IAppConfig {
 		$this->loadConfig($app, null);
 	}
 
+	private function getLocalCacheKey(?bool $lazy): string {
+		return 'app-config' . ($lazy !== null ? '-' . ($lazy === true ? 'lazy' : 'non-lazy') : '');
+	}
+
+	private function clearLocalCache(): void {
+		if ($this->localCache === null) {
+			return;
+		}
+
+		$keys = array_map(fn (?bool $lazy) => $this->getLocalCacheKey($lazy), [null, true, false]);
+		foreach ($keys as $key) {
+			$this->localCache->remove($key);
+		}
+	}
+
 	/**
 	 * Load normal config or config set as lazy loaded
 	 *
@@ -1235,10 +1258,9 @@ class AppConfig implements IAppConfig {
 			$this->logger->debug($exception->getMessage(), ['exception' => $exception, 'app' => $app]);
 		}
 
-		$cache = $this->cacheFactory->isLocalCacheAvailable() ? $this->cacheFactory->createLocal() : null;
-		$cacheKey = 'app-config' . ($lazy !== null ? '-' . ($lazy === true ? 'lazy' : 'non-lazy') : '');
+		$cacheKey = $this->getLocalCacheKey($lazy);
 
-		$rows = $cache?->get($cacheKey);
+		$rows = $this->localCache?->get($cacheKey);
 		if ($rows === null) {
 			$qb = $this->connection->getQueryBuilder();
 			$qb->from('appconfig');
@@ -1257,7 +1279,7 @@ class AppConfig implements IAppConfig {
 			$result->closeCursor();
 
 			// The TTL is low to avoid syncing issues in clustered setups, but it still greatly improves performance when many requests per second are sent.
-			$cache?->set($cacheKey, $rows, 1);
+			$this->localCache?->set($cacheKey, $rows, 1);
 		}
 
 		foreach ($rows as $row) {
