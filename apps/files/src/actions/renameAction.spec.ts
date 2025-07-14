c16273864d7f3b29f95d6bc40503b2bdfe62930c
Hamza <hamzamahjoubi221@gmail.com>
2025-07-14 14:32:53 +0200

    fix: hide rename action in trashbin
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- apps/files/src/actions/renameAction.spec.ts
+++ apps/files/src/actions/renameAction.spec.ts
@@ -4,13 +4,21 @@
  */
 import { action } from './renameAction'
 import { expect } from '@jest/globals'
-import { File, Permission, View, FileAction } from '@nextcloud/files'
+import { File, Folder, Permission, View, FileAction } from '@nextcloud/files'
 import eventBus from '@nextcloud/event-bus'
+import { useFilesStore } from '../store/files'
+import { pinia } from '../store/index.ts'
 
 const view = {
 	id: 'files',
 	name: 'Files',
 } as View
+
+beforeEach(() => {
+	const root = new Folder({ owner: 'test', source: 'https://cloud.domain.com/remote.php/dav/files/admin/', id: 1, permissions: Permission.CREATE })
+	const files = useFilesStore(pinia)
+	files.setRoot({ service: 'files', root })
+})
 
 describe('Rename action conditions tests', () => {
 	test('Default values', () => {
@@ -26,7 +34,7 @@ describe('Rename action conditions tests', () => {
 describe('Rename action enabled tests', () => {
 	test('Enabled for node with UPDATE permission', () => {
 		const file = new File({
-			id: 1,
+			id: 2,
 			source: 'https://cloud.domain.com/remote.php/dav/files/admin/foobar.txt',
 			owner: 'admin',
 			mime: 'text/plain',
@@ -39,7 +47,7 @@ describe('Rename action enabled tests', () => {
 
 	test('Disabled for node without DELETE permission', () => {
 		const file = new File({
-			id: 1,
+			id: 2,
 			source: 'https://cloud.domain.com/remote.php/dav/files/admin/foobar.txt',
 			owner: 'admin',
 			mime: 'text/plain',
@@ -54,13 +62,13 @@ describe('Rename action enabled tests', () => {
 		window.OCA = { Files: { Sidebar: {} } }
 
 		const file1 = new File({
-			id: 1,
+			id: 2,
 			source: 'https://cloud.domain.com/remote.php/dav/files/admin/foo.txt',
 			owner: 'admin',
 			mime: 'text/plain',
 		})
 		const file2 = new File({
-			id: 1,
+			id: 2,
 			source: 'https://cloud.domain.com/remote.php/dav/files/admin/bar.txt',
 			owner: 'admin',
 			mime: 'text/plain',
@@ -76,7 +84,7 @@ describe('Rename action exec tests', () => {
 		jest.spyOn(eventBus, 'emit')
 
 		const file = new File({
-			id: 1,
+			id: 2,
 			source: 'https://cloud.domain.com/remote.php/dav/files/admin/foobar.txt',
 			owner: 'admin',
 			mime: 'text/plain',
