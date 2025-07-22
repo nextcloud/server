1ca389c653f45891de8ddfafb1d2dc33da4260e9
Ferdinand Thiessen <opensource@fthiessen.de>
2025-07-23 00:48:21 +0200

    fix(files_sharing): only show "shared with me" information if shared
    The variable is intialized to `{}` which is truthy, so instead just
    check if there is a user assigned to the share info.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_sharing/src/views/SharingTab.vue
+++ apps/files_sharing/src/views/SharingTab.vue
@@ -240,8 +240,7 @@ export default {
 		 * @return {boolean}
 		 */
 		isSharedWithMe() {
-			return this.sharedWithMe !== null
-				&& this.sharedWithMe !== undefined
+			return !!this.sharedWithMe?.user
 		},
 
 		/**
