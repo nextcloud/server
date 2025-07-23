b847d2f23f39228bd245246f5576d457b7ca373b
Ferdinand Thiessen <opensource@fthiessen.de>
2025-07-23 12:57:14 +0200

    fix(files_sharing): restore state when updating share failed
    We need to save the previous state - here the password - so that if the
    update fails we can revert the shown state.
    This happens e.g. if you have the password policy app and try to add an
    unsecure password.
    
    To reproduce (with password policy):
    1. Create new link share
    2. enable password protection
    3. use insecure password like `1234`
    4. save share
    
    Now you see that the update failed, but the password protection is still
    enabled. This happened because `password` and `newPassword` were
    misused. `password` was already set when `newPassword` was not saved so
    we could not know to what we need to reset when the update failed.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files_sharing/src/components/SharingEntryLink.vue
+++ apps/files_sharing/src/components/SharingEntryLink.vue
@@ -68,10 +68,10 @@
 				{{ config.enforcePasswordForPublicLink ? t('files_sharing', 'Password protection (enforced)') : t('files_sharing', 'Password protection') }}
 			</NcActionCheckbox>
 
-			<NcActionInput v-if="pendingEnforcedPassword || share.password"
+			<NcActionInput v-if="pendingEnforcedPassword || isPasswordProtected"
 				class="share-link-password"
 				:label="t('files_sharing', 'Enter a password')"
-				:value.sync="share.password"
+				:value.sync="share.newPassword"
 				:disabled="saving"
 				:required="config.enableLinkPasswordByDefault || config.enforcePasswordForPublicLink"
 				:minlength="isPasswordPolicyEnabled && config.passwordPolicy.minLength"
@@ -109,7 +109,8 @@
 				</template>
 			</NcActionInput>
 
-			<NcActionButton @click.prevent.stop="onNewLinkShare(true)">
+			<NcActionButton :disabled="pendingEnforcedPassword && !share.newPassword"
+				@click.prevent.stop="onNewLinkShare(true)">
 				<template #icon>
 					<CheckIcon :size="20" />
 				</template>
@@ -638,6 +639,7 @@ export default {
 
 				// create share & close menu
 				const share = new Share(shareDefaults)
+				share.newPassword = share.password
 				const component = await new Promise(resolve => {
 					this.$emit('add:share', share, resolve)
 				})
@@ -830,7 +832,7 @@ export default {
 		 */
 		onPasswordSubmit() {
 			if (this.hasUnsavedPassword) {
-				this.share.password = this.share.newPassword.trim()
+				this.share.newPassword = this.share.newPassword.trim()
 				this.queueUpdate('password')
 			}
 		},
@@ -845,7 +847,7 @@ export default {
 		 */
 		onPasswordProtectedByTalkChange() {
 			if (this.hasUnsavedPassword) {
-				this.share.password = this.share.newPassword.trim()
+				this.share.newPassword = this.share.newPassword.trim()
 			}
 
 			this.queueUpdate('sendPasswordByTalk', 'password')
