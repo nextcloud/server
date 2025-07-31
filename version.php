612e06466adb971d11f90396cf40fa1479b73954
skjnldsv <skjnldsv@protonmail.com>
2025-07-31 08:53:35 +0200

    fix(core): ensure unique vcategory
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [31, 0, 7, 1];
+$OC_Version = [31, 0, 7, 2];
 
 // The human-readable string
 $OC_VersionString = '31.0.7';
