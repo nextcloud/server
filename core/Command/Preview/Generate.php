7b7d70fbc2dacb63cfd46c63ec81c7a7641db813
Josh <josh.t.richards@gmail.com>
2025-07-30 21:20:55 -0400

    refactor(occ): misc tidying of `preview:generate` command
    Code re-use:
    - Use `FileUtils` instead of dedicated code
    
    Code readability:
    - Naming variables
    - Formatting of code
    - Functionality grouping
    
    Usability:
    
    - Tidy up output messages
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- core/Command/Preview/Generate.php
+++ core/Command/Preview/Generate.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace OC\Core\Command\Preview;
 
+use OC\Core\Command\Info\FileUtils;
 use OCP\Files\Config\IUserMountCache;
 use OCP\Files\File;
 use OCP\Files\IRootFolder;
@@ -20,66 +21,107 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Generate a preview for a file from the command-line.
+ *
+ * Useful in automations and for troubleshooting preview generation issues.
+ *
+ * @since 27.0.0
+ */
 class Generate extends Command {
 	public function __construct(
 		private IRootFolder $rootFolder,
 		private IUserMountCache $userMountCache,
 		private IPreview $previewManager,
+		private FileUtils $fileUtils,
 	) {
 		parent::__construct();
 	}
 
-	protected function configure() {
+	protected function configure(): void {
 		$this
 			->setName('preview:generate')
-			->setDescription('generate a preview for a file')
-			->addArgument('file', InputArgument::REQUIRED, 'path or fileid of the file to generate the preview for')
-			->addOption('size', 's', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'size to generate the preview for in pixels, defaults to 64x64', ['64x64'])
-			->addOption('crop', 'c', InputOption::VALUE_NONE, 'crop the previews instead of maintaining aspect ratio')
-			->addOption('mode', 'm', InputOption::VALUE_REQUIRED, "mode for generating uncropped previews, 'cover' or 'fill'", IPreview::MODE_FILL);
+			->setDescription('Generates a preview for a file')
+			->setHelp('Generates a preview for an individual file for automation or troubleshooting purposes.')
+			->addArgument(
+				'file',
+				InputArgument::REQUIRED,
+				'file id or Nextcloud path'
+			)
+			->addOption(
+				'size',
+				's',
+				InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
+				'preview size(s) to generate in pixels, defaults to 64x64',
+				['64x64']
+			)
+			->addOption(
+				'crop',
+				'c',
+				InputOption::VALUE_NONE,
+				'crop the previews instead of maintaining aspect ratio'
+			)
+			->addOption(
+				'mode',
+				'm',
+				InputOption::VALUE_REQUIRED,
+				'mode for generating uncropped previews, \'cover\' or \'fill\'',
+				IPreview::MODE_FILL
+			)
+		;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
+		// parse and check `file` argument value (can be a file id or path to a specific file relative to the data directory
 		$fileInput = $input->getArgument('file');
-		$sizes = $input->getOption('size');
-		$sizes = array_map(function (string $size) use ($output) {
+		$node = $this->fileUtils->getNode($fileInput);
+		if (!node) {
+			$output->writeln("<error>File ($file) does not exist</error>");
+			return self::FAILURE;
+		}
+		if (!$node instanceof File) {
+			$output->writeln("<error>specified file ($file) is not a file (did you specify a folder by accident?)</error>");
+			return self::INVALID;
+		}
+		// No point in continuing if there isn't a configured preview provider for the file
+		if (!$this->previewManager->isAvailable($node)) {
+			$output->writeln('<error>File of type ' . $node->getMimetype() . ' does not have a preview generator configured.</error>');
+			return self::FAILURE;
+		}
+
+		// parse and check `size` option value(s) ("64x64" if not specified)
+		$sizeInput = $input->getOption('size');
+		// parse size option value(s)
+		//	(e.g. ["128"] or ["128x128"] or even ["64x64", "128x128"])
+		$sizes = array_map(function (string $size) use ($output): array|null {
 			if (str_contains($size, 'x')) {
 				$sizeParts = explode('x', $size, 2);
 			} else {
 				$sizeParts = [$size, $size];
 			}
 			if (!is_numeric($sizeParts[0]) || !is_numeric($sizeParts[1] ?? null)) {
-				$output->writeln("<error>Invalid size $size</error>");
+				// output error here to be able to inform which one size entry caused it
+				$output->writeln("<error>Size ($size) is invalid</error>"); 
 				return null;
 			}
-
 			return array_map('intval', $sizeParts);
-		}, $sizes);
+		}, $sizeInput);
 		if (in_array(null, $sizes)) {
-			return 1;
+			// error output already provided so no need for it here
+			return self::FAILURE;
 		}
-
+		
+		// parse the `crop` option value
+		$crop = $input->getOption('crop');
+		
+		// parse and check the `mode` option value
 		$mode = $input->getOption('mode');
 		if ($mode !== IPreview::MODE_FILL && $mode !== IPreview::MODE_COVER) {
-			$output->writeln("<error>Invalid mode $mode</error>");
-			return 1;
-		}
-		$crop = $input->getOption('crop');
-		$file = $this->getFile($fileInput);
-		if (!$file) {
-			$output->writeln("<error>File $fileInput not found</error>");
-			return 1;
-		}
-		if (!$file instanceof File) {
-			$output->writeln("<error>Can't generate previews for folders</error>");
-			return 1;
+			$output->writeln("<error>Mode ($mode) is invalid</error>");
+			return self::INVALID;
 		}
 
-		if (!$this->previewManager->isAvailable($file)) {
-			$output->writeln('<error>No preview generator available for file of type' . $file->getMimetype() . '</error>');
-			return 1;
-		}
-
+		// generate the specifification(s) of the preview size(s) generate
 		$specifications = array_map(function (array $sizes) use ($crop, $mode) {
 			return [
 				'width' => $sizes[0],
@@ -89,30 +131,12 @@ class Generate extends Command {
 			];
 		}, $sizes);
 
-		$this->previewManager->generatePreviews($file, $specifications);
-		if (count($specifications) > 1) {
-			$output->writeln('generated <info>' . count($specifications) . '</info> previews');
-		} else {
-			$output->writeln('preview generated');
-		}
-		return 0;
-	}
+		// generate the actual requested previews
+		$this->previewManager->generatePreviews($node, $specifications);
 
-	private function getFile(string $fileInput): ?Node {
-		if (is_numeric($fileInput)) {
-			$mounts = $this->userMountCache->getMountsForFileId((int)$fileInput);
-			if (!$mounts) {
-				return null;
-			}
-			$mount = $mounts[0];
-			$userFolder = $this->rootFolder->getUserFolder($mount->getUser()->getUID());
-			return $userFolder->getFirstNodeById((int)$fileInput);
-		} else {
-			try {
-				return $this->rootFolder->get($fileInput);
-			} catch (NotFoundException $e) {
-				return null;
-			}
-		}
+		// inform the user what we did if we were successful
+		$output->writeln('Generated <info>' . count($specifications) . '</info> preview(s)');
+		
+		return self::SUCCESS;
 	}
 }
