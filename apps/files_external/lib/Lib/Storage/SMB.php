fc585e0ef5850688de9922fdd15fe2e1b955f381
Robin Appelman <robin@icewind.nl>
2025-08-05 17:39:12 +0200

    fix: handle smb timeouts as storage not available
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_external/lib/Lib/Storage/SMB.php
+++ apps/files_external/lib/Lib/Storage/SMB.php
@@ -354,7 +354,7 @@ class SMB extends Common implements INotifyStorage {
 			if ($retry) {
 				return $this->stat($path, false);
 			} else {
-				throw $e;
+				throw new StorageNotAvailableException($e->getMessage(), $e->getCode(), $e);
 			}
 		}
 		if ($this->remoteIsShare() && $this->isRootDir($path)) {
