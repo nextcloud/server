eed32483b16d2afe4b7a9e27808117e65f468829
Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>
2025-08-08 16:03:09 +0200

    fixup! fixup! fix(dav): make file-properties serializable
    Signed-off-by: Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>

--- apps/dav/lib/Paginate/PaginateCache.php
+++ apps/dav/lib/Paginate/PaginateCache.php
@@ -18,6 +18,7 @@ use OCP\Security\ISecureRandom;
 class PaginateCache {
 	public const TTL = 60 * 60;
 	private const CACHE_COUNT_SUFFIX = 'count';
+
 	private ICache $cache;
 
 	public function __construct(
