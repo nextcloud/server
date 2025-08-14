b72bdc9242507401448f96312cdb87f453bc9bd3
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-08-14 16:10:29 +0200

    fixup! chore: migrate HiDrive Next ionos theme
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- apps/theming/lib/Themes/IonosTheme.php
+++ apps/theming/lib/Themes/IonosTheme.php
@@ -284,7 +284,7 @@ class IonosTheme extends DefaultTheme implements ITheme {
 		// https://html.spec.whatwg.org/multipage/semantics.html#meta-color-scheme
 		return [[
 			'name' => 'color-scheme',
-			'content' => 'light', // Remove only when dark mode is supported
+			'content' => 'light',
 		]];
 	}
 }
