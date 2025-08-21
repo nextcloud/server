65510371449913c8e205a605a02d84d3501fb41a
Josh Richards <josh.t.richards@gmail.com>
2025-08-21 09:55:06 -0400

    fix: ... WIP
    Signed-off-by: Josh Richards <josh.t.richards@gmail.com>

--- apps/dav/appinfo/v1/publicwebdav.php
+++ apps/dav/appinfo/v1/publicwebdav.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2016-2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 use OC\Files\Filesystem;
 use OC\Files\Storage\Wrapper\PermissionsMask;
 use OC\Files\View;
@@ -29,40 +32,48 @@ use OCP\IUserSession;
 use OCP\L10N\IFactory;
 use OCP\Security\Bruteforce\IThrottler;
 use OCP\Server;
+use OCP\Share\IManager;
 use Psr\Log\LoggerInterface;
-
-/* Loads in the scope of /public.php */
+use Sabre\DAV\Exception\NotAuthenticated;
+use Sabre\DAV\Exception\NotFound;
 
 /**
- * Preparation
+ * Implements the Public WebDAV server endpoint (v1)
+ *
+ * - Fires up a WebDAV server for handling a `/public.php/webdav` request.
+ * - Loads in the scope of /public.php.
+ * - Not to be confused with the v2 public WebDAV server endpoint reachable via `/public.php/dav`.
+ *
+ * Caveats:
+ * - Lacks support for upload chunking (traditional as well as S3 upload chunk streaming).
+ * - Only supports Basic Auth with/ Share Token as Username (`LegacyPublicAuth`).
+ * - Effectively a legacy endpoint (officially deprecated?)
  */
 
-// Load ncessary apps
-$RUNTIME_APPTYPES = ['filesystem', 'authentication', 'logging'];
-OC_App::loadApps($RUNTIME_APPTYPES);
+// Load essential apps
+Server::get(IAppManager::class)->loadApps(['authentication', 'filesystem', 'logging']);
 
-// Turn off output buffer
+// Prevent memory problems if output buffering is enabled (an extra safeguard) 
 while (ob_get_level()) {
 	ob_end_clean();
 }
 
-// Close session otherwise streaming will lead to a blocked instance
+// Prevent a blocked instance when streaming
 Server::get(ISession::class)->close();
 
-/**
- * Set-up
- */
+// So that we can reference them for $viewCallback
+$linkCheckPlugin = new PublicLinkCheckPlugin();
+$filesDropPlugin = new FilesDropPlugin();
 
-// DAV Authentication
+// Set-up authentication 
 $authBackend = new LegacyPublicAuth(
 	Server::get(IRequest::class),
-	Server::get(\OCP\Share\IManager::class),
+	Server::get(IManager::class),
 	Server::get(ISession::class),
 	Server::get(IThrottler::class)
 );
 $authPlugin = new \Sabre\DAV\Auth\Plugin($authBackend);
 
-// Server
 $serverFactory = new ServerFactory(
 	Server::get(IConfig::class),
 	Server::get(LoggerInterface::class),
@@ -76,26 +87,31 @@ $serverFactory = new ServerFactory(
 	Server::get(IFactory::class)->get('dav'), // L10N
 );
 
-$linkCheckPlugin = new PublicLinkCheckPlugin();
-$filesDropPlugin = new FilesDropPlugin();
-/** @var callable $viewCallback returns the View for this DAV endpoint */
+/**
+ * This overly long closure is used to return the View for use by the WebDAV server.
+ *
+ * It should probably be refactored as it currently:
+ *
+ * - handles some plugin configuration
+ * - does permissions checks
+ * - adds storage wrappers
+ * - checks for AJAX
+ * - checks for outgoing S2S
+ */
+
+/** @var callable $viewCallback */
 $viewCallback =	function (\Sabre\DAV\Server $server) use (
 	$authBackend,
 	$linkCheckPlugin,
-	$filesDropPlugin,
+	$filesDropPlugin
 ): ?View {
-	$isAjax = in_array(
-		'XMLHttpRequest',
-		explode(',', $_SERVER['HTTP_X_REQUESTED_WITH'] ?? '')
-	);
-	/** @var FederatedShareProvider $shareProvider */
-	$federatedShareProvider = Server::get(FederatedShareProvider::class);
-	if (
-		$federatedShareProvider->isOutgoingServer2serverShareEnabled() === false
-		&& !$isAjax
-	) {
+	$isAjax = in_array('XMLHttpRequest', explode(',', $_SERVER['HTTP_X_REQUESTED_WITH'] ?? ''));
+	$isOutgoingS2sShareEnabled = Server::get(FederatedShareProvider::class)->isOutgoingServer2serverShareEnabled();
+
+	// Access to this endpoint is only permitted if Outgoing Server2Server Sharing is enabled or via AJAX
+	if (!$isOutgoingS2sShareEnabled && !$isAjax) {
 		// this is what is thrown when trying to access a non-existing share
-		throw new \Sabre\DAV\Exception\NotAuthenticated();
+		throw new NotAuthenticated();
 	}
 
 	$share = $authBackend->getShare();
@@ -103,37 +119,35 @@ $viewCallback =	function (\Sabre\DAV\Server $server) use (
 	$isReadable = $share->getPermissions() & Constants::PERMISSION_READ;
 	$fileId = $share->getNodeId();
 
-	// FIXME: should not add storage wrappers outside of preSetup, need to find a better way
+	/**
+	 * Add storage wrappers (should really not do this here, outside of preSetup)
+	 */
+
+	// Temporarily disable warning
 	$previousLog = Filesystem::logWarningWhenAddingStorageWrapper(false);
-	Filesystem::addStorageWrapper(
-		'sharePermissions',
-		function ($mountPoint, $storage) use ($share) {
-		    return new PermissionsMask(
-				[
-				    'storage' => $storage,
-				    'mask' => $share->getPermissions() | Constants::PERMISSION_SHARE
-				]
-			);
-	    }
-	);
-	Filesystem::addStorageWrapper(
-		'shareOwner',
-		function ($mountPoint, $storage) use ($share) {
-		    return new PublicOwnerWrapper(
-				[
-				    'storage' => $storage,
-				    'owner' => $share->getShareOwner()
-				]
-			);
-	});
+
+	$sharePermissionsWrapper = function ($mountPoint, $storage) use ($share) {
+		return new PermissionsMask(
+			['storage' => $storage, 'mask' => $share->getPermissions() | Constants::PERMISSION_SHARE]
+		);
+	};
+	Filesystem::addStorageWrapper('sharePermissions', $sharePermissionsWrapper);
+
+	$shareOwnerWrapper = function ($mountPoint, $storage) use ($share) {
+		return new PublicOwnerWrapper(['storage' => $storage, 'owner' => $share->getShareOwner()]);
+	};
+	Filesystem::addStorageWrapper('shareOwner', $shareOwnerWrapper);
+
+	// Restore prior logging state
 	Filesystem::logWarningWhenAddingStorageWrapper($previousLog);
 
 	$rootFolder = Server::get(IRootFolder::class);
 	$userFolder = $rootFolder->getUserFolder($owner);
 	$node = $userFolder->getFirstNodeById($fileId);
 	if (!$node) {
-		throw new \Sabre\DAV\Exception\NotFound();
+		throw new NotFound();
 	}
+
 	$linkCheckPlugin->setFileInfo($node);
 
 	// If not readable (files_drop) enable the filesdrop plugin
@@ -146,6 +160,7 @@ $viewCallback =	function (\Sabre\DAV\Server $server) use (
 	return $view;
 };
 
+/** var OCA\DAV\Connector\Sabre\Server $server */
 $server = $serverFactory->createServer(
 	false,
 	$baseuri, /** @var string $baseuri defined in remote.php */
@@ -153,16 +168,14 @@ $server = $serverFactory->createServer(
 	$authPlugin,
 	$viewCallback,
 );
+
+// Note: various "default" plugins are handled within `createServer()` 
 $server->addPlugin($linkCheckPlugin);
 $server->addPlugin($filesDropPlugin);
 
-// Trigger any other listening plugins
-// Note: `createServer()` loads various plugins internally
+// Give apps a chance to inject additional plugins
 $event = new BeforeSabrePubliclyLoadedEvent($server);
-$dispatcher = Server::get(IEventDispatcher::class);
-$dispatcher->dispatchTyped($event);
+Server::get(IEventDispatcher::class)->dispatchTyped($event);
 
-/**
- * Start the Server!
- */
+// Start the Server!
 $server->start();
