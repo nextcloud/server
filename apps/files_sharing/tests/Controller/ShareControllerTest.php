ba338753be65c5bc9243a1ce81d3f139b0f83ebd
Louis Chemineau <louis@chmn.me>
2025-08-21 17:21:04 +0200

    feat(file_sharing): Provide template creator list in public shares
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- apps/files_sharing/tests/Controller/ShareControllerTest.php
+++ apps/files_sharing/tests/Controller/ShareControllerTest.php
@@ -31,6 +31,7 @@ use OCP\EventDispatcher\IEventDispatcher;
 use OCP\Files\File;
 use OCP\Files\IRootFolder;
 use OCP\Files\NotFoundException;
+use OCP\Files\Template\ITemplateManager;
 use OCP\IAppConfig;
 use OCP\IConfig;
 use OCP\IL10N;
@@ -68,6 +69,7 @@ class ShareControllerTest extends \Test\TestCase {
 	private Manager&MockObject $shareManager;
 	private IPreview&MockObject $previewManager;
 	private IUserManager&MockObject $userManager;
+	private ITemplateManager&MockObject $templateManager;
 	private IInitialState&MockObject $initialState;
 	private IURLGenerator&MockObject $urlGenerator;
 	private ISecureRandom&MockObject $secureRandom;
@@ -87,6 +89,7 @@ class ShareControllerTest extends \Test\TestCase {
 		$this->config = $this->createMock(IConfig::class);
 		$this->appConfig = $this->createMock(IAppConfig::class);
 		$this->userManager = $this->createMock(IUserManager::class);
+		$this->templateManager = $this->createMock(ITemplateManager::class);
 		$this->initialState = $this->createMock(IInitialState::class);
 		$this->federatedShareProvider = $this->createMock(FederatedShareProvider::class);
 		$this->federatedShareProvider->expects($this->any())
@@ -114,6 +117,7 @@ class ShareControllerTest extends \Test\TestCase {
 					$this->defaults,
 					$this->config,
 					$this->createMock(IRequest::class),
+					$this->templateManager,
 					$this->initialState,
 					$this->appConfig,
 				)
@@ -338,6 +342,7 @@ class ShareControllerTest extends \Test\TestCase {
 			'owner' => 'ownerUID',
 			'ownerDisplayName' => 'ownerDisplay',
 			'isFileRequest' => false,
+			'templates' => [],
 		];
 
 		$response = $this->shareController->showShare();
@@ -485,6 +490,7 @@ class ShareControllerTest extends \Test\TestCase {
 			'isFileRequest' => false,
 			'note' => 'The note',
 			'label' => 'A label',
+			'templates' => [],
 		];
 
 		$response = $this->shareController->showShare();
