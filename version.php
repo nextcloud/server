012a3a458417dfbe209ab0de838969a79d5915c2
Worker Builder <worker-builder@nextcloud.com>
2025-08-22 17:47:43 +0000

    build(hub): 32.0.0 beta 2
    Signed-off-by: Worker Builder <worker-builder@nextcloud.com>

--- version.php
+++ version.php
@@ -9,10 +9,10 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [32, 0, 0, 4];
+$OC_Version = [32, 0, 0, 5];
 
 // The human-readable string
-$OC_VersionString = '32.0.0 beta 1';
+$OC_VersionString = '32.0.0 beta 2';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
