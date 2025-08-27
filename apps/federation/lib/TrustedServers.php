arturhg/nextcloud-server

fix(federation): remove background jobs when removing trusted servers
Add event listener to remove matching background jobs for trusted server url.

Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

apps/federation/lib/TrustedServers.php
@@ -88,7 +88,7 @@ class TrustedServers {
 	public function removeServer(int $id): void {
 		$server = $this->dbHandler->getServerById($id);
 		$this->dbHandler->removeServer($id);
-		$this->dispatcher->dispatchTyped(new TrustedServerRemovedEvent($server['url_hash']));
+		$this->dispatcher->dispatchTyped(new TrustedServerRemovedEvent($server['url'], $server['url_hash']));
 
 	}
 

lib/public/Federation/Events/TrustedServerRemovedEvent.php
@@ -14,14 +14,23 @@ use OCP\EventDispatcher\Event;
  * @since 25.0.0
  */
 class TrustedServerRemovedEvent extends Event {
-	private string $urlHash;
 
 	/**
 	 * @since 25.0.0
+	 * @since 32.0.0 Added $url argument
 	 */
-	public function __construct(string $urlHash) {
+	public function __construct(
+		private readonly string $url,
+		private readonly string $urlHash
+	) {
 		parent::__construct();
-		$this->urlHash = $urlHash;
+	}
+
+	/**
+	 * @since 32.0.0
+	 */
+	public function getUrl(): string {
+		return $this->url;
 	}
 
 	/**

nextcloud/server

apps/federation/lib/TrustedServers.php
@@ -88,7 +88,7 @@ class TrustedServers {
 	public function removeServer(int $id): void {
 		$server = $this->dbHandler->getServerById($id);
 		$this->dbHandler->removeServer($id);
-		$this->dispatcher->dispatchTyped(new TrustedServerRemovedEvent($server['url_hash']));
+		$this->dispatcher->dispatchTyped(new TrustedServerRemovedEvent($server['url_hash'], $server['url']));
 
 	}
 

lib/public/Federation/Events/TrustedServerRemovedEvent.php
@@ -14,14 +14,16 @@ use OCP\EventDispatcher\Event;
  * @since 25.0.0
  */
 class TrustedServerRemovedEvent extends Event {
-	private string $urlHash;
 
 	/**
 	 * @since 25.0.0
+	 * @since 32.0.0 Added $url argument
 	 */
-	public function __construct(string $urlHash) {
+	public function __construct(
+		private readonly string $urlHash,
+		private readonly ?string $url = null,
+	) {
 		parent::__construct();
-		$this->urlHash = $urlHash;
 	}
 
 	/**
@@ -29,5 +31,12 @@ class TrustedServerRemovedEvent extends Event {
 	 */
 	public function getUrlHash(): string {
 		return $this->urlHash;
+	}
+
+	/**
+	 * @since 32.0.0
+	 */
+	public function getUrl(): ?string {
+		return $this->url;
 	}
 }