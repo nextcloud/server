97fc1c8ad6fb96535430f1eef9a7fcd6797bb62c
nfebe <fenn25.fn@gmail.com>
2025-09-16 17:46:21 +0100

    refactor(ShareApiController): Streamline share providers & add error logging
    1. Consolidated the repetitive provider code into a clean loop
    
    2. Added exception handling to log unexpected errors
    
    Signed-off-by: nfebe <fenn25.fn@gmail.com>
    
    [skip ci]

--- apps/files_sharing/lib/Controller/ShareAPIController.php
+++ apps/files_sharing/lib/Controller/ShareAPIController.php
@@ -1689,7 +1689,14 @@ class ShareAPIController extends OCSController {
 	 * @throws ShareNotFound
 	 */
 	private function getShareById(string $id): IShare {
-		$share = null;
+		$providers = [
+			'ocinternal' => null, // No type check needed
+			'ocCircleShare' => IShare::TYPE_CIRCLE,
+			'ocMailShare' => IShare::TYPE_EMAIL,
+			'ocRoomShare' => null,
+			'deck' => IShare::TYPE_DECK,
+			'sciencemesh' => IShare::TYPE_SCIENCEMESH,
+		];
 
 		// First check if it is an internal share.
 		try {
@@ -1705,8 +1712,6 @@ class ShareAPIController extends OCSController {
 				$share = $this->shareManager->getShareById('ocCircleShare:' . $id, $this->currentUser);
 				return $share;
 			}
-		} catch (ShareNotFound $e) {
-			// Do nothing, just try the other share type
 		}
 
 		try {
