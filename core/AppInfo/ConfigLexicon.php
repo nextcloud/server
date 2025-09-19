71551254b1aaca62a2850efd9c122e440a7536de
Maxence Lange <maxence@artificial-owl.com>
2025-09-19 15:31:37 -0100

    fix(ocm-invites): add route to invite accept dialog
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- core/AppInfo/ConfigLexicon.php
+++ core/AppInfo/ConfigLexicon.php
@@ -28,6 +28,8 @@ class ConfigLexicon implements ILexicon {
 	public const SHARE_LINK_EXPIRE_DATE_ENFORCED = 'shareapi_enforce_expire_date';
 	public const USER_LANGUAGE = 'lang';
 	public const OCM_DISCOVERY_ENABLED = 'ocm_discovery_enabled';
+	public const OCM_INVITE_ACCEPT_DIALOG = 'ocm_invite_accept_dialog';
+
 	public const USER_LOCALE = 'locale';
 	public const USER_TIMEZONE = 'timezone';
 
@@ -87,6 +89,7 @@ class ConfigLexicon implements ILexicon {
 			),
 			new Entry(self::LASTCRON_TIMESTAMP, ValueType::INT, 0, 'timestamp of last cron execution'),
 			new Entry(self::OCM_DISCOVERY_ENABLED, ValueType::BOOL, true, 'enable/disable OCM', lazy: true),
+			new Entry(self::OCM_INVITE_ACCEPT_DIALOG, ValueType::STRING, '', 'route to local invite accept dialog', lazy: true, note: 'set as empty string to disable feature'),
 		];
 	}
 
