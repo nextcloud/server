5642b6b3369ec89c20b39051985d32f2571c319e
provokateurin <kate@provokateurin.de>
2025-10-02 10:35:01 +0200

    fix(Share20\Manager): Explicitly setup the filesystem for the user
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -11,6 +11,7 @@ use ArrayIterator;
 use OC\Core\AppInfo\ConfigLexicon;
 use OC\Files\Filesystem;
 use OC\Files\Mount\MoveableMount;
+use OC\Files\SetupManager;
 use OC\KnownUser\KnownUserService;
 use OC\Share\Constants as ShareConstants;
 use OC\Share20\Exception\ProviderException;
@@ -42,6 +43,7 @@ use OCP\Security\Events\ValidatePasswordPolicyEvent;
 use OCP\Security\IHasher;
 use OCP\Security\ISecureRandom;
 use OCP\Security\PasswordContext;
+use OCP\Server;
 use OCP\Share;
 use OCP\Share\Events\BeforeShareCreatedEvent;
 use OCP\Share\Events\BeforeShareDeletedEvent;
@@ -95,6 +97,12 @@ class Manager implements IManager {
 		// The constructor of LegacyHooks registers the listeners of share events
 		// do not remove if those are not properly migrated
 		$this->legacyHooks = new LegacyHooks($this->dispatcher);
+
+		$user = $this->userSession->getUser();
+		if ($user !== null) {
+			// The VerifyMountPointEvent is required for Talk and Deck, so an explicit FS setup is required for sharing.
+			Server::get(SetupManager::class)->setupForUser($user);
+		}
 	}
 
 	/**
