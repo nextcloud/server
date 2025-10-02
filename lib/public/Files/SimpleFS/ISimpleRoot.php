d628760b6049ca2817a397a3ac54e038ea1f5c13
Josh <josh.t.richards@gmail.com>
2025-10-02 13:30:05 -0400

    refactor(SimpleFS): Update ISimpleRoot documentation
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/public/Files/SimpleFS/ISimpleRoot.php
+++ lib/public/Files/SimpleFS/ISimpleRoot.php
@@ -10,35 +10,42 @@ use OCP\Files\NotFoundException;
 use OCP\Files\NotPermittedException;
 
 /**
- * Interface ISimpleRoot
+ * Interface for representing and manipulating root of a simple folder structure in Nextcloud's virtual filesystem.
+ *
+ * Provides methods for listing, creating, and retrieving folders within the root.
  *
  * @since 11.0.0
+ * @api
  */
 interface ISimpleRoot {
 	/**
-	 * Get the folder with name $name
+	 * Get all folders at the root of the simple filesystem.
 	 *
-	 * @throws NotFoundException
-	 * @throws \RuntimeException
-	 * @since 11.0.0
-	 */
-	public function getFolder(string $name): ISimpleFolder;
-
-	/**
-	 * Get all the Folders
-	 *
-	 * @return ISimpleFolder[]
-	 * @throws NotFoundException
-	 * @throws \RuntimeException
+	 * @return ISimpleFolder[] Array of ISimpleFolder instances representing each folder.
+	 * @throws NotFoundException If no folders are found.
+	 * @throws \RuntimeException For general runtime errors.
 	 * @since 11.0.0
 	 */
 	public function getDirectoryListing(): array;
 
 	/**
-	 * Create a new folder named $name
+	 * Get the folder named $name from the root of the simple filesystem.
 	 *
-	 * @throws NotPermittedException
-	 * @throws \RuntimeException
+	 * @param string $name The name of the folder to retrieve.
+	 * @return ISimpleFolder The folder instance corresponding to the provided name.
+	 * @throws NotFoundException If the folder with the given name does not exist.
+	 * @throws \RuntimeException For general runtime errors.
+	 * @since 11.0.0
+	 */
+	public function getFolder(string $name): ISimpleFolder;
+
+	/**
+	 * Creates a new folder named $name at the root of the simple filesystem.
+	 *
+	 * @param string $name The name of the new folder to create.
+	 * @return ISimpleFolder The newly created folder instance.
+	 * @throws NotPermittedException If folder creation is not permitted.
+	 * @throws \RuntimeException For general runtime errors.
 	 * @since 11.0.0
 	 */
 	public function newFolder(string $name): ISimpleFolder;
