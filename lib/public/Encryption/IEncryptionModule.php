d4e2146ab5654721af184befe0bf345d23173bd8
Josh <josh.t.richards@gmail.com>
2025-10-06 12:22:27 -0400

    chore: Add/fix type hints and improve docs in IEncryptionModule
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/public/Encryption/IEncryptionModule.php
+++ lib/public/Encryption/IEncryptionModule.php
@@ -11,165 +11,159 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Interface IEncryptionModule
+ * Interface for encryption modules for use by Nextcloud's Server-side Encryption (SSE).
+ *
+ * This interface allows Nextcloud SSE to support multiple interchangeable encryption modules.
  *
  * @since 8.1.0
  */
 interface IEncryptionModule {
 	/**
-	 * @return string defining the technical unique id
+	 * Returns the technical unique ID of the encryption module.
+	 *
+	 * @return string Technical unique ID.
 	 * @since 8.1.0
 	 */
-	public function getId();
+	public function getId(): string;
 
 	/**
-	 * In comparison to getKey() this function returns a human readable (maybe translated) name
+	 * Returns a human-readable (possibly translated) display name for the encryption module.
 	 *
-	 * @return string
+	 * @return string Display name.
 	 * @since 8.1.0
 	 */
-	public function getDisplayName();
+	public function getDisplayName(): string;
 
 	/**
-	 * start receiving chunks from a file. This is the place where you can
-	 * perform some initial step before starting encrypting/decrypting the
-	 * chunks
+	 * Initializes the encryption/decryption process for a file. This is the entry point for receiving file chunks.
 	 *
-	 * @param string $path to the file
-	 * @param string $user who read/write the file (null for public access)
-	 * @param string $mode php stream open mode
-	 * @param array $header contains the header data read from the file
-	 * @param array $accessList who has access to the file contains the key 'users' and 'public'
+	 * @param string $path Path to the file.
+	 * @param string $user User reading or writing the file (null for public access).
+	 * @param string $mode PHP stream open mode.
+	 * @param array $header Header data read from the file.
+	 * @param array $accessList List of users and public access; contains the keys 'users' and 'public'.
 	 *
-	 * @return array $header contain data as key-value pairs which should be
-	 *               written to the header, in case of a write operation
-	 *               or if no additional data is needed return a empty array
+	 * @return array Header data as key-value pairs to be written to the header in case of a write operation,
+	 *               or an empty array if no additional data is needed.
 	 * @since 8.1.0
 	 */
-	public function begin($path, $user, $mode, array $header, array $accessList);
+	public function begin(string $path, string $user, string $mode, array $header, array $accessList): array;
 
 	/**
-	 * last chunk received. This is the place where you can perform some final
-	 * operation and return some remaining data if something is left in your
-	 * buffer.
+	 * Finalizes the encryption/decryption process. If there is remaining data in the buffer, it can be returned here.
 	 *
-	 * @param string $path to the file
-	 * @param string $position id of the last block (looks like "<Number>end")
+	 * @param string $path Path to the file.
+	 * @param string $position ID of the last block (format: "<Number>end").
 	 *
-	 * @return string remained data which should be written to the file in case
-	 *                of a write operation
+	 * @return string Remaining data to be written to the file for write operations.
 	 *
 	 * @since 8.1.0
-	 * @since 9.0.0 parameter $position added
+	 * @since 9.0.0 Parameter $position added.
 	 */
-	public function end($path, $position);
+	public function end(string $path, string $position): string;
 
 	/**
-	 * encrypt data
+	 * Encrypts data.
 	 *
-	 * @param string $data you want to encrypt
-	 * @param string $position position of the block we want to encrypt (starts with '0')
+	 * @param string $data Data to encrypt.
+	 * @param string $position Position of the block to encrypt (starts with '0').
 	 *
-	 * @return mixed encrypted data
+	 * @return string Encrypted data.
 	 *
 	 * @since 8.1.0
-	 * @since 9.0.0 parameter $position added
+	 * @since 9.0.0 Parameter $position added.
 	 */
-	public function encrypt($data, $position);
+	public function encrypt(string $data, string $position): string;
 
 	/**
-	 * decrypt data
+	 * Decrypts data.
 	 *
-	 * @param string $data you want to decrypt
-	 * @param int|string $position position of the block we want to decrypt
+	 * @param string $data Data to decrypt.
+	 * @param int|string $position Position of the block to decrypt.
 	 *
-	 * @return mixed decrypted data
+	 * @return string Decrypted data.
 	 *
 	 * @since 8.1.0
-	 * @since 9.0.0 parameter $position added
+	 * @since 9.0.0 Parameter $position added.
 	 */
-	public function decrypt($data, $position);
+	public function decrypt(string $data, int|string $position): string;
 
 	/**
-	 * update encrypted file, e.g. give additional users access to the file
+	 * Updates the encrypted file, for example, granting additional users access.
 	 *
-	 * @param string $path path to the file which should be updated
-	 * @param string $uid of the user who performs the operation
-	 * @param array $accessList who has access to the file contains the key 'users' and 'public'
-	 * @return boolean
+	 * @param string $path Path to the file to update.
+	 * @param string $uid User performing the operation.
+	 * @param array $accessList List of users and public access; contains the keys 'users' and 'public'.
+	 * @return bool True on success, false otherwise.
 	 * @since 8.1.0
 	 */
-	public function update($path, $uid, array $accessList);
+	public function update(string $path, string $uid, array $accessList): bool;
 
 	/**
-	 * should the file be encrypted or not
+	 * Determines whether the file should be encrypted.
 	 *
-	 * @param string $path
-	 * @return boolean
+	 * @param string $path Path to the file.
+	 * @return bool True if the file should be encrypted, false otherwise.
 	 * @since 8.1.0
 	 */
-	public function shouldEncrypt($path);
+	public function shouldEncrypt(string $path): bool;
 
 	/**
-	 * get size of the unencrypted payload per block.
-	 * ownCloud read/write files with a block size of 8192 byte
+	 * Returns the size of the unencrypted payload per block.
+	 * ownCloud reads/writes files with a block size of 8192 bytes.
 	 *
-	 * @param bool $signed
-	 * @return int
-	 * @since 8.1.0 optional parameter $signed was added in 9.0.0
+	 * @param bool $signed Whether the block is signed.
+	 * @return int Size of the unencrypted block.
+	 * @since 8.1.0 Optional parameter $signed was added in 9.0.0.
 	 */
-	public function getUnencryptedBlockSize($signed = false);
+	public function getUnencryptedBlockSize(bool $signed = false): int;
 
 	/**
-	 * check if the encryption module is able to read the file,
-	 * e.g. if all encryption keys exists
+	 * Checks if the encryption module is able to read the file (e.g., if all encryption keys exist).
 	 *
-	 * @param string $path
-	 * @param string $uid user for whom we want to check if they can read the file
-	 * @return boolean
+	 * @param string $path Path to the file.
+	 * @param string $uid User for whom to check readability.
+	 * @return bool True if readable, false otherwise.
 	 * @since 8.1.0
 	 */
-	public function isReadable($path, $uid);
+	public function isReadable(string $path, string $uid): bool;
 
 	/**
-	 * Initial encryption of all files
+	 * Performs initial encryption of all files.
 	 *
-	 * @param InputInterface $input
-	 * @param OutputInterface $output write some status information to the terminal during encryption
+	 * @param InputInterface $input Input interface.
+	 * @param OutputInterface $output Output interface for status information during encryption.
 	 * @since 8.2.0
 	 */
-	public function encryptAll(InputInterface $input, OutputInterface $output);
+	public function encryptAll(InputInterface $input, OutputInterface $output): void;
 
 	/**
-	 * prepare encryption module to decrypt all files
+	 * Prepares the encryption module to decrypt all files.
 	 *
-	 * @param InputInterface $input
-	 * @param OutputInterface $output write some status information to the terminal during encryption
-	 * @param $user (optional) for which the files should be decrypted, default = all users
-	 * @return bool return false on failure or if it isn't supported by the module
+	 * @param InputInterface $input Input interface.
+	 * @param OutputInterface $output Output interface for status information during decryption.
+	 * @param string $user (optional) User for whom the files should be decrypted. If omitted, decrypts for all users.
+	 * @return bool False on failure or if not supported by the module.
 	 * @since 8.2.0
 	 */
-	public function prepareDecryptAll(InputInterface $input, OutputInterface $output, $user = '');
+	public function prepareDecryptAll(InputInterface $input, OutputInterface $output, string $user = ''): bool;
 
 	/**
-	 * Check if the module is ready to be used by that specific user.
-	 * In case a module is not ready - because e.g. key pairs have not been generated
-	 * upon login this method can return false before any operation starts and might
-	 * cause issues during operations.
+	 * Checks if the module is ready to be used by the specified user.
+	 * Returns false if key pairs have not been generated for the user.
 	 *
-	 * @param string $user
-	 * @return boolean
+	 * @param string $user User to check.
+	 * @return bool True if ready, false otherwise.
 	 * @since 9.1.0
 	 */
-	public function isReadyForUser($user);
+	public function isReadyForUser(string $user): bool;
 
 	/**
-	 * Does the encryption module needs a detailed list of users with access to the file?
-	 * For example if the encryption module uses per-user encryption keys and needs to know
-	 * the users with access to the file to encrypt/decrypt it.
+	 * Indicates whether the encryption module needs a detailed list of users with access to the file.
+	 * For example, modules using per-user encryption keys may require this information.
 	 *
+	 * @return bool True if a detailed access list is required, false otherwise.
 	 * @since 13.0.0
-	 * @return bool
 	 */
-	public function needDetailedAccessList();
+	public function needDetailedAccessList(): bool;
 }
