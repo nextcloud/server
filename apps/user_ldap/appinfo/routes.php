c62166241a39f20cb4a47d00882c8da731e50c10
Côme Chilliet <come.chilliet@nextcloud.com>
2025-10-07 11:44:06 +0200

    chore(user_ldap): Remove ajax endpoints
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- apps/user_ldap/appinfo/routes.php
+++ apps/user_ldap/appinfo/routes.php
@@ -7,14 +7,6 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-$this->create('user_ldap_ajax_clearMappings', 'apps/user_ldap/ajax/clearMappings.php')
-	->actionInclude('user_ldap/ajax/clearMappings.php');
-$this->create('user_ldap_ajax_getNewServerConfigPrefix', 'apps/user_ldap/ajax/getNewServerConfigPrefix.php')
-	->actionInclude('user_ldap/ajax/getNewServerConfigPrefix.php');
-$this->create('user_ldap_ajax_testConfiguration', 'apps/user_ldap/ajax/testConfiguration.php')
-	->actionInclude('user_ldap/ajax/testConfiguration.php');
-$this->create('user_ldap_ajax_wizard', 'apps/user_ldap/ajax/wizard.php')
-	->actionInclude('user_ldap/ajax/wizard.php');
 
 return [
 	'routes' => [
