770ad6249e8498f727ada31958ecb19a3b71bcb0
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-10-09 16:01:11 +0200

    fix(admin-delegation): Prevent delegation to group if delegation already exist
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- apps/settings/tests/Service/AuthorizedGroupServiceTest.php
+++ apps/settings/tests/Service/AuthorizedGroupServiceTest.php
@@ -12,6 +12,8 @@ namespace OCA\Settings\Tests\Service;
 use OC\Settings\AuthorizedGroup;
 use OC\Settings\AuthorizedGroupMapper;
 use OCA\Settings\Service\AuthorizedGroupService;
+use OCA\Settings\Service\ConflictException;
+use OCP\AppFramework\Db\DoesNotExistException;
 use PHPUnit\Framework\MockObject\MockObject;
 use Test\TestCase;
 
@@ -26,10 +28,71 @@ class AuthorizedGroupServiceTest extends TestCase {
 		$this->service = new AuthorizedGroupService($this->mapper);
 	}
 
+	public function testCreateSuccessWhenNoDuplicateExists(): void {
+		$groupId = 'testgroup';
+		$class = 'TestClass';
+
+		// Mock that no existing assignment is found (throws DoesNotExistException)
+		$this->mapper->expects($this->once())
+			->method('findByGroupIdAndClass')
+			->with($groupId, $class)
+			->willThrowException(new DoesNotExistException('Not found'));
+
+		// Mock the successful creation
+		$expectedGroup = new AuthorizedGroup();
+		$expectedGroup->setGroupId($groupId);
+		$expectedGroup->setClass($class);
+		$expectedGroup->setId(123);
+
+		$this->mapper->expects($this->once())
+			->method('insert')
+			->willReturn($expectedGroup);
+
+		$result = $this->service->create($groupId, $class);
+
+		$this->assertInstanceOf(AuthorizedGroup::class, $result);
+		$this->assertEquals($groupId, $result->getGroupId());
+		$this->assertEquals($class, $result->getClass());
+	}
+
+	public function testCreateThrowsConflictExceptionWhenDuplicateExists(): void {
+		$groupId = 'testgroup';
+		$class = 'TestClass';
+
+		// Mock that an existing assignment is found
+		$existingGroup = new AuthorizedGroup();
+		$existingGroup->setGroupId($groupId);
+		$existingGroup->setClass($class);
+		$existingGroup->setId(42);
+
+		$this->mapper->expects($this->once())
+			->method('findByGroupIdAndClass')
+			->with($groupId, $class)
+			->willReturn($existingGroup);
+
+		// Mapper insert should never be called when duplicate exists
+		$this->mapper->expects($this->never())
+			->method('insert');
+
+		$this->expectException(ConflictException::class);
+		$this->expectExceptionMessage('Group is already assigned to this class');
+
+		$this->service->create($groupId, $class);
+	}
+
 	public function testCreateAllowsDifferentGroupsSameClass(): void {
 		$groupId1 = 'testgroup1';
 		$groupId2 = 'testgroup2';
 		$class = 'TestClass';
+
+		// Mock that no duplicate exists for group1
+		$this->mapper->expects($this->exactly(2))
+			->method('findByGroupIdAndClass')
+			->willReturnCallback(function ($groupId, $classArg) use ($groupId1, $groupId2, $class) {
+				$this->assertContains($groupId, [$groupId1, $groupId2]);
+				$this->assertEquals($class, $classArg);
+				throw new DoesNotExistException('Not found');
+			});
 
 		$expectedGroup1 = new AuthorizedGroup();
 		$expectedGroup1->setGroupId($groupId1);
@@ -59,6 +122,15 @@ class AuthorizedGroupServiceTest extends TestCase {
 		$groupId = 'testgroup';
 		$class1 = 'TestClass1';
 		$class2 = 'TestClass2';
+
+		// Mock that no duplicate exists for either class
+		$this->mapper->expects($this->exactly(2))
+			->method('findByGroupIdAndClass')
+			->willReturnCallback(function ($groupIdArg, $class) use ($groupId, $class1, $class2) {
+				$this->assertEquals($groupId, $groupIdArg);
+				$this->assertContains($class, [$class1, $class2]);
+				throw new DoesNotExistException('Not found');
+			});
 
 		$expectedGroup1 = new AuthorizedGroup();
 		$expectedGroup1->setGroupId($groupId);
