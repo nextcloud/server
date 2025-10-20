e1bd7afc85bcf3f05dbe221e1056c8fada8aa03d
Charles Taborin <charles.taborin@leviia.com>
2025-10-20 14:29:00 +0200

    docs(auth): add copyright notice for Charles Taborin

--- apps/settings/lib/Events/AfterAuthTokenCreatedEvent.php
+++ apps/settings/lib/Events/AfterAuthTokenCreatedEvent.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+/**
+ * SPDX-FileCopyrightText: 2025 Charles Taborin <charles.taborin@gmail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 namespace OCA\Settings\Events;
 
 use OCP\EventDispatcher\Event;
