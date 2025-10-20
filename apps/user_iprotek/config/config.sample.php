b985b90cd1a609895e4b2e64f5775d8aea9f66e8
drimcaster <drimcaster@gmail.com>
2025-10-20 18:42:59 +0800

    Update config.sample.php

--- apps/user_iprotek/config/config.sample.php
+++ apps/user_iprotek/config/config.sample.php
@@ -2,7 +2,7 @@
 // apps/user_laravel/config/config.php
 
 return [
-    'laravel_api_url' => 'https://api.example.com',
+    'iprotek_api_url' => 'https://api.example.com',
     'client_id' => 'your-client-id',
     'client_secret' => 'your-client-secret',
 ];
