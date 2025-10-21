1301573fdcb415390754678e6f30142d8e8dfdff
Maxence Lange <maxence@artificial-owl.com>
2025-10-21 11:38:42 -0100

    fix(lexicon): trigger core upgrade
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [32, 0, 1, 0];
+$OC_Version = [32, 0, 1, 1];
 
 // The human-readable string
 $OC_VersionString = '32.0.1 RC1';
