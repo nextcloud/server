963f2807107b2d3953632476aa12786b06ea0f0d
Louis Chmn <louis@chmn.me>
2025-10-22 14:49:41 +0200

    test(files): Adapte to new @nextcloud/files version
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- apps/files/src/composables/useFileListHeaders.spec.ts
+++ apps/files/src/composables/useFileListHeaders.spec.ts
@@ -5,8 +5,7 @@
 
 import { beforeEach, describe, expect, it, jest } from '@jest/globals'
 import { useFileListHeaders } from './useFileListHeaders.ts'
-import { Header } from '@nextcloud/files'
-import * as ncFiles from '@nextcloud/files'
+import { Header, getFileListHeaders } from '@nextcloud/files'
 
 jest.mock('@nextcloud/files', () => ({
 	...jest.requireActual<typeof import('@nextcloud/files')>('@nextcloud/files'),
@@ -21,22 +20,22 @@ describe('useFileListHeaders', () => {
 	it('gets the headers', () => {
 		const header = new Header({ id: '1', order: 5, render: jest.fn(), updated: jest.fn() })
 		// @ts-expect-error its mocked
-		ncFiles.getFileListHeaders.mockImplementationOnce(() => [header])
+		getFileListHeaders.mockImplementationOnce(() => [header])
 
 		const headers = useFileListHeaders()
 		expect(headers.value).toEqual([header])
-		expect(ncFiles.getFileListHeaders).toBeCalled()
+		expect(getFileListHeaders).toBeCalled()
 	})
 
 	it('headers are sorted', () => {
 		const header = new Header({ id: '1', order: 10, render: jest.fn(), updated: jest.fn() })
 		const header2 = new Header({ id: '2', order: 5, render: jest.fn(), updated: jest.fn() })
 		// @ts-expect-error its mocked
-		ncFiles.getFileListHeaders.mockImplementationOnce(() => [header, header2])
+		getFileListHeaders.mockImplementationOnce(() => [header, header2])
 
 		const headers = useFileListHeaders()
 		// lower order first
 		expect(headers.value.map(({ id }) => id)).toStrictEqual(['2', '1'])
-		expect(ncFiles.getFileListHeaders).toBeCalled()
+		expect(getFileListHeaders).toBeCalled()
 	})
 })
