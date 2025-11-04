14cd485ce2c541ae46827a1eebc676b3db711a43
Carl Schwan <carl.schwan@nextcloud.com>
2025-11-04 16:01:41 +0100

    feat(entity): Attributes for Entity
    Signed-off-by: Carl Schwan <carl.schwan@nextcloud.com>

--- lib/private/Tagging/Tag.php
+++ lib/private/Tagging/Tag.php
@@ -7,11 +7,16 @@
  */
 namespace OC\Tagging;
 
+use OCP\AppFramework\Db\Attribute\Column;
+use OCP\AppFramework\Db\Attribute\Table;
 use OCP\AppFramework\Db\Entity;
+use OCP\DB\Types;
 
 /**
  * Class to represent a tag.
  *
+ * @method string getId()
+ * @method void setId(string $id)
  * @method string getOwner()
  * @method void setOwner(string $owner)
  * @method string getType()
@@ -19,59 +24,29 @@ use OCP\AppFramework\Db\Entity;
  * @method string getName()
  * @method void setName(string $name)
  */
+#[Table(name: 'vcategory', useSnowflakeId: true)]
 class Tag extends Entity {
-	protected $owner;
-	protected $type;
-	protected $name;
+	#[Column(name: 'uid', type: Types::STRING, length: 64, nullable: false)]
+	protected ?string $owner = null;
+
+	#[Column(name: 'type', type: Types::STRING, length: 64, nullable: false)]
+	protected ?string $type = null;
+
+	#[Column(name: 'category', type: Types::STRING, length: 255, nullable: false)]
+	protected ?string $name = null;
 
 	/**
 	 * Constructor.
 	 *
-	 * @param string $owner The tag's owner
-	 * @param string $type The type of item this tag is used for
-	 * @param string $name The tag's name
+	 * @param ?string $owner The tag's owner
+	 * @param ?string $type The type of item this tag is used for
+	 * @param ?string $name The tag's name
 	 */
-	public function __construct($owner = null, $type = null, $name = null) {
+	public function __construct(?string $owner = null, ?string $type = null, ?string $name = null) {
+		parent::__construct();
+
 		$this->setOwner($owner);
 		$this->setType($type);
 		$this->setName($name);
-	}
-
-	/**
-	 * Transform a database columnname to a property
-	 *
-	 * @param string $columnName the name of the column
-	 * @return string the property name
-	 * @todo migrate existing database columns to the correct names
-	 * to be able to drop this direct mapping
-	 */
-	public function columnToProperty(string $columnName): string {
-		if ($columnName === 'category') {
-			return 'name';
-		}
-
-		if ($columnName === 'uid') {
-			return 'owner';
-		}
-
-		return parent::columnToProperty($columnName);
-	}
-
-	/**
-	 * Transform a property to a database column name
-	 *
-	 * @param string $property the name of the property
-	 * @return string the column name
-	 */
-	public function propertyToColumn(string $property): string {
-		if ($property === 'name') {
-			return 'category';
-		}
-
-		if ($property === 'owner') {
-			return 'uid';
-		}
-
-		return parent::propertyToColumn($property);
 	}
 }
