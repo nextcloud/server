c9d8e97b3b77627a6aa8875e7ff1b3704370fa65
Josh <josh.t.richards@gmail.com>
2025-11-05 11:38:34 -0500

    refactor: make SecureRandom more robust
    - Use a constant for the default $characters set (still same chars)
    - Add more robust  reasonableness checks for $characters set
    - Switch docblocks to point at interface docs to avoid duplication
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/private/Security/SecureRandom.php
+++ lib/private/Security/SecureRandom.php
@@ -2,44 +2,53 @@
 
 declare(strict_types=1);
 /**
- * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2016-2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 namespace OC\Security;
 
 use OCP\Security\ISecureRandom;
 
 /**
- * Class SecureRandom provides a wrapper around the random_int function to generate
- * secure random strings. For PHP 7 the native CSPRNG is used, older versions do
- * use a fallback.
+ * Secure random string generator recommended for tokens, passwords, secrets, and similar security use cases.
  *
- * Usage:
- * \OC::$server->get(ISecureRandom::class)->generate(10);
- * @package OC\Security
+ * @see \OCP\Security\ISecureRandom
  */
 class SecureRandom implements ISecureRandom {
-	/**
-	 * Generate a secure random string of specified length.
-	 * @param int $length The length of the generated string
-	 * @param string $characters An optional list of characters to use if no character list is
-	 *                           specified all valid base64 characters are used.
-	 * @throws \LengthException if an invalid length is requested
-	 */
+
+	/** @inheritdoc */
 	public function generate(
 		int $length,
-		string $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+		string $characters = ISecureRandom::CHAR_BASE64_RFC4648,
 	): string {
+
 		if ($length <= 0) {
-			throw new \LengthException('Invalid length specified: ' . $length . ' must be bigger than 0');
+			throw new \LengthException(
+				'Invalid length specified: ' . $length . ' must be greater than 0'
+			);
 		}
 
+		if (
+			// Check for ASCII-only (no multibyte characters)
+			!mb_check_encoding($characters, 'ASCII')
+			// Check for uniqueness: number of unique bytes must equal original length
+			|| strlen(count_chars($characters, 3)) !== strlen($characters)
+			// Check minimum length
+			|| strlen($characters) < 4
+		) {
+			throw new \InvalidArgumentException(
+				'Character set must be ASCII-only, unique, and at least four characters long.'
+			);
+		}
+
+		// Build string by selecting random characters from $characters and appending
 		$maxCharIndex = \strlen($characters) - 1;
 		$randomString = '';
-
 		while ($length > 0) {
 			$randomNumber = \random_int(0, $maxCharIndex);
+			// Safe: $characters is guaranteed ASCII; indexed access is byte-correct.
 			$randomString .= $characters[$randomNumber];
 			$length--;
 		}
