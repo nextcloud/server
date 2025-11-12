d1b00335d69b094aba359a3a20666f212410e3f2
Ferdinand Thiessen <opensource@fthiessen.de>
2025-11-12 19:02:25 +0100

    fix(files): use correct types for the Settings
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/files/src/store/userconfig.ts
+++ apps/files/src/store/userconfig.ts
@@ -16,6 +16,7 @@ import { ref, set } from 'vue'
 const initialUserConfig = loadState<UserConfig>('files', 'config', {
 	crop_image_previews: true,
 	default_view: 'files',
+	folder_tree: true,
 	grid_view: false,
 	show_files_extensions: true,
 	show_hidden: false,
@@ -36,7 +37,7 @@ export const useUserConfigStore = defineStore('userconfig', () => {
 	 * @param key The config key
 	 * @param value The new value
 	 */
-	function onUpdate(key: string, value: boolean): void {
+	function onUpdate<Key extends string>(key: Key, value: UserConfig[Key]): void {
 		set(userConfig.value, key, value)
 	}
 
@@ -46,7 +47,7 @@ export const useUserConfigStore = defineStore('userconfig', () => {
 	 * @param key The config key
 	 * @param value The new value
 	 */
-	async function update(key: string, value: boolean): Promise<void> {
+	async function update<Key extends string>(key: Key, value: UserConfig[Key]): Promise<void> {
 		// only update if a user is logged in (not the case for public shares)
 		if (getCurrentUser() !== null) {
 			await axios.put(generateUrl('/apps/files/api/v1/config/{key}', { key }), {
