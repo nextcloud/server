31780327812011f0cbfaa95b32bf660613c96c27
Daniel Kesselberg <mail@danielkesselberg.de>
2025-11-20 16:26:58 +0100

    chore: remove codecov bundle analysis
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,6 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 const WebpackSPDXPlugin = require('./build/WebpackSPDXPlugin.js')
 
 const modules = require('./webpack.modules.js')
-const { codecovWebpackPlugin } = require('@codecov/webpack-plugin')
 
 const appVersion = readFileSync('./version.php').toString().match(/OC_Version.+\[([0-9]{2})/)?.[1] ?? 'unknown'
 const isDev = process.env.NODE_ENV === 'development'
@@ -226,11 +225,6 @@ const config = {
 		new webpack.IgnorePlugin({
 			resourceRegExp: /^\.\/locale$/,
 			contextRegExp: /moment\/min$/,
-		}),
-		codecovWebpackPlugin({
-			enableBundleAnalysis: !isDev && !isTesting,
-			bundleName: 'nextcloud',
-			telemetry: false,
 		}),
 	],
 	externals: {
