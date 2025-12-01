265a9acece1e74cf7cc99519b0a76aac87c1a814
provokateurin <kate@provokateurin.de>
2025-12-01 15:18:33 +0100

    feat(ObjectStore): Allow overriding arguments per bucket
    Signed-off-by: provokateurin <kate@provokateurin.de>
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/private/Files/ObjectStore/S3ConnectionTrait.php
+++ lib/private/Files/ObjectStore/S3ConnectionTrait.php
@@ -35,6 +35,10 @@ trait S3ConnectionTrait {
 			throw new \Exception('Bucket has to be configured.');
 		}
 
+		if (isset($params['multibucket']) && $params['multibucket'] === true && isset($params['perBucket'][$params['bucket']])) {
+			$params = array_merge($params, $params['perBucket'][$params['bucket']]);
+		}
+
 		$this->id = 'amazon::' . $params['bucket'];
 
 		$this->test = isset($params['test']);
