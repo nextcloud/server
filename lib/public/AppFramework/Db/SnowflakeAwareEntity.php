4eaed44cc20a91e4ef2a398e5bb082b469f2347c
Anna Larch <anna@nextcloud.com>
2025-12-02 14:25:42 +0100

    fixup! feat(snowflake): extend Entity class to support snowflakes

--- lib/public/AppFramework/Db/SnowflakeAwareEntity.php
+++ lib/public/AppFramework/Db/SnowflakeAwareEntity.php
@@ -2,24 +2,41 @@
 
 namespace OCP\AppFramework\Db;
 
-use OC\Snowflake\Decoder;
-use OC\Snowflake\Generator;
 use OCP\Server;
+use OCP\Snowflake\IDecoder;
+use OCP\Snowflake\IGenerator;
 
+/**
+ * @method string getId()
+ */
 class SnowflakeAwareEntity extends Entity {
+	/** @var string $id */
+	public $id;
+
+	/** @var array<string, \OCP\DB\Types::*> */
+	private array $_fieldTypes = ['id' => 'string'];
 
 	#[\Override]
-	public function setId(): string {
-		$generator = Server::get(Generator::class);
-		$this->id = $generator->nextId();
+	public function setId(): void {
+		if (empty($this->id)) {
+			$generator = Server::get(IGenerator::class);
+			$this->id = $generator->nextId();
+			$this->markFieldUpdated('id');
+		}
 	}
 
-	public function getCreatedAt(): \DateTimeImmutable {
-		$decoder = Server::get(Decoder::class);
+	public function getCreatedAt(): ?\DateTimeImmutable {
+		if (empty($this->id)) {
+			return null;
+		}
+		$decoder = Server::get(IDecoder::class);
 		return $decoder->decode($this->id)['created_at'];
 	}
 
 	public function getDecodedId(): array {
-		return Server::get(Decoder::class)->decode($this->id);
+		if (empty($this->id)) {
+			return [];
+		}
+		return Server::get(IDecoder::class)->decode($this->id);
 	}
 }
