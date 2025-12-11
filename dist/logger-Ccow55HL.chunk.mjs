2c4b5c6b6830742535c6ae93f07262f31923a7a6
skjnldsv <skjnldsv@protonmail.com>
2025-12-11 12:56:02 +0100

    chore: compile assets
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- dist/logger-Ccow55HL.chunk.mjs
+++
@@ -1,8 +0,0 @@
-const appName = "nextcloud-ui";
-const appVersion = "1.0.0";
-import { g as getLoggerBuilder } from "./Plus-CFgExibL.chunk.mjs";
-const logger = getLoggerBuilder().setApp("dav").detectUser().build();
-export {
-  logger as l
-};
-//# sourceMappingURL=logger-Ccow55HL.chunk.mjs.map
---
+++ dist/logger-pPJfWtPZ.chunk.mjs
@@ -0,0 +1,2 @@
+import{g as t}from"./Plus-x3tSd1hP.chunk.mjs";const o=t().setApp("dav").detectUser().build();export{o as l};
+//# sourceMappingURL=logger-pPJfWtPZ.chunk.mjs.map
