02cfef6b6c0b2f3ef6e4506556734d8bcc2c4541
Côme Chilliet <come.chilliet@nextcloud.com>
2025-12-16 11:57:34 +0100

    fix: Use new getOrCreate method and fix code issues
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/Files/Node/UserFolder.php
+++ lib/private/Files/Node/UserFolder.php
@@ -10,6 +10,7 @@ namespace OC\Files\Node;
 
 use OC\Files\Storage\Wrapper\Quota;
 use OC\Files\View;
+use OCP\Files\Folder as IFolder;
 use OCP\Files\IRootFolder;
 use OCP\Files\IUserFolder;
 use OCP\ICacheFactory;
@@ -25,7 +26,7 @@ class UserFolder extends Folder implements IUserFolder {
 		IRootFolder $root,
 		View $view,
 		string $path,
-		Folder $parent,
+		IFolder $parent,
 		protected IConfig $config,
 		protected IUser $user,
 		protected ICacheFactory $cacheFactory,
