e509c3dc24795250820b2ec4737feba389c45c92
Michał Roszak <m.roszakos@gmail.com>
2026-01-06 12:14:33 +0100

    refactor (2fa): Correct IStatelessProvider license
    Signed-off-by: Michał Roszak <m.roszakos@gmail.com>

--- lib/public/Authentication/TwoFactorAuth/IStatelessProvider.php
+++ lib/public/Authentication/TwoFactorAuth/IStatelessProvider.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 namespace OCP\Authentication\TwoFactorAuth;
