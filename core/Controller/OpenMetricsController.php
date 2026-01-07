green-coding-solutions/nextcloud-server

feat(openmetrics): introduce OpenMetrics exporter
Expose a `/metrics` endpoint with some basic metrics

Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

core/Controller/OpenMetricsController.php
@@ -1,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OC\Core\Controller;
+
+use OC\OpenMetrics\Exporter;
+use OC\Security\Ip\Address;
+use OC\Security\Ip\Range;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
+use OCP\AppFramework\Http\Attribute\PublicPage;
+use OCP\IConfig;
+use OCP\IRequest;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricTypes;
+use OCP\OpenMetrics\MetricValue;
+use Psr\Log\LoggerInterface;
+
+/**
+ * OpenMetrics controller
+ *
+ * Gather and display metrics
+ *
+ * @package OC\Core\Controller
+ */
+class OpenMetricsController extends Controller {
+	public function __construct(
+		string $appName,
+		IRequest $request,
+		private IConfig $config,
+		private Exporter $exporter,
+		private LoggerInterface $logger,
+	) {
+		parent::__construct($appName, $request);
+	}
+
+	#[NoCSRFRequired]
+	#[PublicPage]
+	#[FrontpageRoute(verb: 'GET', url: '/metrics')]
+	public function export(): Http\Response {
+		if (!$this->isRemoteAddressAllowed()) {
+			return new Http\Response(Http::STATUS_FORBIDDEN);
+		}
+
+		return new Http\StreamTraversableResponse(
+			$this->generate(),
+			Http::STATUS_OK,
+			[
+				'Content-Type' => 'application/openmetrics-text; version=1.0.0; charset=utf-8',
+			]
+		);
+	}
+
+	private function isRemoteAddressAllowed(): bool {
+		$clientAddress = new Address($this->request->getRemoteAddress());
+		$allowedRanges = $this->config->getSystemValue('openmetrics_allowed_clients', ['127.0.0.0/16', '::1/128']);
+		if (!is_array($allowedRanges)) {
+			$this->logger->warning('Invalid configuration for "openmetrics_allowed_clients"');
+			return false;
+		}
+
+		foreach ($allowedRanges as $range) {
+			$range = new Range($range);
+			if ($range->contains($clientAddress)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private function generate(): \Generator {
+		$exporter = $this->exporter;
+
+		foreach ($exporter() as $family) {
+			$output = '';
+			$name = $family->name();
+			if ($family->type() !== MetricTypes::unknown) {
+				$output = '# TYPE nextcloud_' . $name . ' ' . $family->type()->name . "\n";
+			}
+			if ($family->unit() !== '') {
+				$output .= '# UNIT nextcloud_' . $name . ' ' . $family->unit() . "\n";
+			}
+			if ($family->help() !== '') {
+				$output .= '# HELP nextcloud_' . $name . ' ' . $family->help() . "\n";
+			}
+			foreach ($family->metrics() as $metric) {
+				$output .= 'nextcloud_' . $name . $this->formatLabels($metric) . ' ' . $this->formatValue($metric);
+				if ($metric->timestamp !== null) {
+					$output .= ' ' . $this->formatTimestamp($metric);
+				}
+				$output .= "\n";
+			}
+			$output .= "\n";
+
+			yield $output;
+		}
+
+		$elapsed = (string)(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']);
+		yield <<<SUMMARY
+			# TYPE nextcloud_exporter_duration gauge
+			# UNIT nextcloud_exporter_duration seconds
+			# HELP nextcloud_exporter_duration Exporter run time
+			nextcloud_exporter_duration $elapsed
+
+			# EOF
+
+			SUMMARY;
+	}
+
+	private function formatLabels(Metric $metric): string {
+		if (empty($metric->labels)) {
+			return '';
+		}
+
+		$labels = [];
+		foreach ($metric->labels as $label => $value) {
+			$labels[] .= $label . '=' . $this->escapeString((string)$value);
+		}
+
+		return '{' . implode(',', $labels) . '}';
+	}
+
+	private function escapeString(string $string): string {
+		return json_encode(
+			$string,
+			JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR,
+			1
+		);
+	}
+
+	private function formatValue(Metric $metric): string {
+		if (is_bool($metric->value)) {
+			return $metric->value ? '1' : '0';
+		}
+		if ($metric->value instanceof MetricValue) {
+			return $metric->value->value;
+		}
+
+		return (string)$metric->value;
+	}
+
+	private function formatTimestamp(Metric $metric): string {
+		return (string)$metric->timestamp;
+	}
+}

lib/private/OpenMetrics/Exporters/AppsCount.php
@@ -1,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\App\IAppManager;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricTypes;
+use Override;
+
+/**
+ * Export statistics about apps
+ *
+ * @since 33.0.0
+ */
+class AppsCount implements IMetricFamily {
+	public function __construct(
+		private IAppManager $appManager,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'apps_count';
+	}
+
+	#[Override]
+	public function type(): MetricTypes {
+		return MetricTypes::gauge;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return 'applications';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Applications in Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$installedAppsCount = count($this->appManager->getAppInstalledVersions(false));
+		$enabledAppsCount = count($this->appManager->getEnabledApps());
+		$disabledAppsCount = $installedAppsCount - $enabledAppsCount;
+		yield new Metric(
+			$disabledAppsCount,
+			['status' => 'disabled'],
+		);
+		yield new Metric(
+			$enabledAppsCount,
+			['status' => 'enabled'],
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/AppsInfo.php
@@ -1,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\App\IAppManager;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricTypes;
+use Override;
+
+/**
+ * Export information about enabled applications
+ *
+ * @since 33.0.0
+ */
+class AppsInfo implements IMetricFamily {
+	public function __construct(
+		private IAppManager $appManager,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'apps_info';
+	}
+
+	#[Override]
+	public function type(): MetricTypes {
+		return MetricTypes::info;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return '';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Enabled applications in Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		yield new Metric(
+			1,
+			$this->appManager->getAppInstalledVersions(true),
+			time()
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/Maintenance.php
@@ -1,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OC\SystemConfig;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricTypes;
+use OCP\Server;
+use Override;
+
+/**
+ * Export maintenance state
+ *
+ * @since 33.0.0
+ */
+class Maintenance implements IMetricFamily {
+	public function name(): string {
+		return 'maintenance';
+	}
+
+	#[Override]
+	public function type(): MetricTypes {
+		return MetricTypes::info;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return '';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Maintenance status of Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$systemConfig = Server::get(SystemConfig::class);
+		yield new Metric(
+			(bool)$systemConfig->getValue('maintenance', false)
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/RunningJobs.php
@@ -1,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\IDBConnection;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricTypes;
+use Override;
+
+/**
+ * Export the number of running jobs by type
+ *
+ * @since 33.0.0
+ */
+class RunningJobs implements IMetricFamily {
+	public function __construct(
+		private IDBConnection $connection,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'jobs_running';
+	}
+
+	#[Override]
+	public function type(): MetricTypes {
+		return MetricTypes::gauge;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return 'jobs';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Number of running jobs';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$qb = $this->connection->getQueryBuilder();
+		$result = $qb->select($qb->func()->count('*', 'nb'), 'class')
+			->from('jobs')
+			->where($qb->expr()->gt('reserved_at', $qb->createNamedParameter(0)))
+			->groupBy('class')
+			->executeQuery();
+
+		// If no result, return a metric with count '0'
+		if ($result->rowCount() === 0) {
+			yield new Metric(0);
+			return;
+		}
+
+		while ($row = $result->fetch()) {
+			yield new Metric($row['nb'], ['class' => $row['class']]);
+		}
+	}
+}

tests/Core/Controller/OpenMetricsControllerTest.php
@@ -1,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace Tests\Core\Controller;
+
+use OC\Core\Controller\OpenMetricsController;
+use OC\OpenMetrics\Exporter;
+use OCP\AppFramework\Http\IOutput;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\StreamTraversableResponse;
+use OCP\IConfig;
+use OCP\IRequest;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+use Test\TestCase;
+
+class OpenMetricsControllerTest extends TestCase {
+	private IRequest&MockObject $request;
+	private IConfig&MockObject $config;
+	private Exporter&MockObject $exporter;
+	private LoggerInterface&MockObject $logger;
+	private OpenMetricsController $controller;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->request = $this->createMock(IRequest::class);
+		$this->request->method('getRemoteAddress')
+			->willReturn('192.168.1.1');
+		$this->config = $this->createMock(IConfig::class);
+		$this->exporter = $this->createMock(Exporter::class);
+		$this->logger = $this->createMock(LoggerInterface::class);
+		$this->controller = new OpenMetricsController('core', $this->request, $this->config, $this->exporter, $this->logger);
+	}
+
+	public function testGetMetrics(): void {
+		$output = $this->createMock(IOutput::class);
+		$fullOutput = '';
+		$output->method('setOutput')
+			->willReturnCallback(function ($output) use (&$fullOutput) {
+				$fullOutput .= $output;
+			});
+		$this->config->expects($this->once())
+			->method('getSystemValue')
+			->with('openmetrics_allowed_clients')
+			->willReturn(['192.168.0.0/16']);
+		$response = $this->controller->export();
+		$this->assertInstanceOf(StreamTraversableResponse::class, $response);
+		$this->assertEquals('200', $response->getStatus());
+		$this->assertEquals('application/openmetrics-text; version=1.0.0; charset=utf-8', $response->getHeaders()['Content-Type']);
+		$expected = <<<EXPECTED
+			# TYPE nextcloud_exporter_duration gauge
+			# UNIT nextcloud_exporter_duration seconds
+			# HELP nextcloud_exporter_duration Exporter run time
+			nextcloud_exporter_duration %f
+
+			# EOF
+
+			EXPECTED;
+		$response->callback($output);
+		$this->assertStringMatchesFormat($expected, $fullOutput);
+	}
+
+	public function testGetMetricsFromForbiddenIp(): void {
+		$this->config->expects($this->once())
+			->method('getSystemValue')
+			->with('openmetrics_allowed_clients')
+			->willReturn(['1.2.3.4']);
+		$response = $this->controller->export();
+		$this->assertInstanceOf(Response::class, $response);
+		$this->assertEquals('403', $response->getStatus());
+	}
+}

nextcloud/server

core/Controller/OpenMetricsController.php
@@ -1,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OC\Core\Controller;
+
+use OC\OpenMetrics\ExporterManager;
+use OC\Security\Ip\Address;
+use OC\Security\Ip\Range;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
+use OCP\AppFramework\Http\Attribute\PublicPage;
+use OCP\IConfig;
+use OCP\IRequest;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricType;
+use OCP\OpenMetrics\MetricValue;
+use Psr\Log\LoggerInterface;
+
+/**
+ * OpenMetrics controller
+ *
+ * Gather and display metrics
+ *
+ * @package OC\Core\Controller
+ */
+class OpenMetricsController extends Controller {
+	public function __construct(
+		string $appName,
+		IRequest $request,
+		private IConfig $config,
+		private ExporterManager $exporterManager,
+		private LoggerInterface $logger,
+	) {
+		parent::__construct($appName, $request);
+	}
+
+	#[NoCSRFRequired]
+	#[PublicPage]
+	#[FrontpageRoute(verb: 'GET', url: '/metrics')]
+	public function export(): Http\Response {
+		if (!$this->isRemoteAddressAllowed()) {
+			return new Http\Response(Http::STATUS_FORBIDDEN);
+		}
+
+		return new Http\StreamTraversableResponse(
+			$this->generate(),
+			Http::STATUS_OK,
+			[
+				'Content-Type' => 'application/openmetrics-text; version=1.0.0; charset=utf-8',
+			]
+		);
+	}
+
+	private function isRemoteAddressAllowed(): bool {
+		$clientAddress = new Address($this->request->getRemoteAddress());
+		$allowedRanges = $this->config->getSystemValue('openmetrics_allowed_clients', ['127.0.0.0/16', '::1/128']);
+		if (!is_array($allowedRanges)) {
+			$this->logger->warning('Invalid configuration for "openmetrics_allowed_clients"');
+			return false;
+		}
+
+		foreach ($allowedRanges as $range) {
+			$range = new Range($range);
+			if ($range->contains($clientAddress)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private function generate(): \Generator {
+		foreach ($this->exporterManager->export() as $family) {
+			yield $this->formatFamily($family);
+		}
+
+		$elapsed = (string)(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']);
+		yield <<<SUMMARY
+			# TYPE nextcloud_exporter_duration gauge
+			# UNIT nextcloud_exporter_duration seconds
+			# HELP nextcloud_exporter_duration Exporter run time
+			nextcloud_exporter_duration $elapsed
+
+			# EOF
+
+			SUMMARY;
+	}
+
+	private function formatFamily(IMetricFamily $family): string {
+		$output = '';
+		$name = $family->name();
+		if ($family->type() !== MetricType::unknown) {
+			$output = '# TYPE nextcloud_' . $name . ' ' . $family->type()->name . "\n";
+		}
+		if ($family->unit() !== '') {
+			$output .= '# UNIT nextcloud_' . $name . ' ' . $family->unit() . "\n";
+		}
+		if ($family->help() !== '') {
+			$output .= '# HELP nextcloud_' . $name . ' ' . $family->help() . "\n";
+		}
+		foreach ($family->metrics() as $metric) {
+			$output .= 'nextcloud_' . $name . $this->formatLabels($metric) . ' ' . $this->formatValue($metric);
+			if ($metric->timestamp !== null) {
+				$output .= ' ' . $this->formatTimestamp($metric);
+			}
+			$output .= "\n";
+		}
+		$output .= "\n";
+
+		return $output;
+	}
+
+	private function formatLabels(Metric $metric): string {
+		if (empty($metric->labels)) {
+			return '';
+		}
+
+		$labels = [];
+		foreach ($metric->labels as $label => $value) {
+			$labels[] .= $label . '=' . $this->escapeString((string)$value);
+		}
+
+		return '{' . implode(',', $labels) . '}';
+	}
+
+	private function escapeString(string $string): string {
+		return json_encode(
+			$string,
+			JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR,
+			1
+		);
+	}
+
+	private function formatValue(Metric $metric): string {
+		if (is_bool($metric->value)) {
+			return $metric->value ? '1' : '0';
+		}
+		if ($metric->value instanceof MetricValue) {
+			return $metric->value->value;
+		}
+
+		return (string)$metric->value;
+	}
+
+	private function formatTimestamp(Metric $metric): string {
+		return (string)$metric->timestamp;
+	}
+}

lib/private/OpenMetrics/Exporters/AppsCount.php
@@ -1,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\App\IAppManager;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricType;
+use Override;
+
+/**
+ * Export statistics about apps
+ */
+class AppsCount implements IMetricFamily {
+	public function __construct(
+		private IAppManager $appManager,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'apps_count';
+	}
+
+	#[Override]
+	public function type(): MetricType {
+		return MetricType::gauge;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return 'applications';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Number of apps in Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$installedAppsCount = count($this->appManager->getAppInstalledVersions(false));
+		$enabledAppsCount = count($this->appManager->getEnabledApps());
+		$disabledAppsCount = $installedAppsCount - $enabledAppsCount;
+		yield new Metric(
+			$disabledAppsCount,
+			['status' => 'disabled'],
+		);
+		yield new Metric(
+			$enabledAppsCount,
+			['status' => 'enabled'],
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/AppsInfo.php
@@ -1,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\App\IAppManager;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricType;
+use Override;
+
+/**
+ * Export information about enabled applications
+ */
+class AppsInfo implements IMetricFamily {
+	public function __construct(
+		private IAppManager $appManager,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'apps_info';
+	}
+
+	#[Override]
+	public function type(): MetricType {
+		return MetricType::info;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return '';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Enabled applications in Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		yield new Metric(
+			1,
+			$this->appManager->getAppInstalledVersions(true),
+			time()
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/Maintenance.php
@@ -1,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OC\SystemConfig;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricType;
+use OCP\Server;
+use Override;
+
+/**
+ * Export maintenance state
+ */
+class Maintenance implements IMetricFamily {
+	public function name(): string {
+		return 'maintenance';
+	}
+
+	#[Override]
+	public function type(): MetricType {
+		return MetricType::info;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return '';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Maintenance status of Nextcloud';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$systemConfig = Server::get(SystemConfig::class);
+		yield new Metric(
+			(bool)$systemConfig->getValue('maintenance', false)
+		);
+	}
+}

lib/private/OpenMetrics/Exporters/RunningJobs.php
@@ -1,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\OpenMetrics\Exporters;
+
+use Generator;
+use OCP\IDBConnection;
+use OCP\OpenMetrics\IMetricFamily;
+use OCP\OpenMetrics\Metric;
+use OCP\OpenMetrics\MetricType;
+use Override;
+
+/**
+ * Export the number of running jobs by type
+ */
+class RunningJobs implements IMetricFamily {
+	public function __construct(
+		private IDBConnection $connection,
+	) {
+	}
+
+	#[Override]
+	public function name(): string {
+		return 'jobs_running';
+	}
+
+	#[Override]
+	public function type(): MetricType {
+		return MetricType::gauge;
+	}
+
+	#[Override]
+	public function unit(): string {
+		return 'jobs';
+	}
+
+	#[Override]
+	public function help(): string {
+		return 'Number of running jobs';
+	}
+
+	#[Override]
+	public function metrics(): Generator {
+		$qb = $this->connection->getQueryBuilder();
+		$result = $qb->select($qb->func()->count('*', 'nb'), 'class')
+			->from('jobs')
+			->where($qb->expr()->gt('reserved_at', $qb->createNamedParameter(0)))
+			->groupBy('class')
+			->executeQuery();
+
+		// If no result, return a metric with count '0'
+		if ($result->rowCount() === 0) {
+			yield new Metric(0);
+			return;
+		}
+
+		foreach ($result->iterateAssociative() as $row) {
+			yield new Metric($row['nb'], ['class' => $row['class']]);
+		}
+	}
+}

tests/Core/Controller/OpenMetricsControllerTest.php
@@ -1,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace Tests\Core\Controller;
+
+use OC\Core\Controller\OpenMetricsController;
+use OC\OpenMetrics\ExporterManager;
+use OCP\AppFramework\Http\IOutput;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\StreamTraversableResponse;
+use OCP\IConfig;
+use OCP\IRequest;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+use Test\TestCase;
+
+class OpenMetricsControllerTest extends TestCase {
+	private IRequest&MockObject $request;
+	private IConfig&MockObject $config;
+	private ExporterManager&MockObject $exporterManager;
+	private LoggerInterface&MockObject $logger;
+	private OpenMetricsController $controller;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->request = $this->createMock(IRequest::class);
+		$this->request->method('getRemoteAddress')
+			->willReturn('192.168.1.1');
+		$this->config = $this->createMock(IConfig::class);
+		$this->exporterManager = $this->createMock(ExporterManager::class);
+		$this->logger = $this->createMock(LoggerInterface::class);
+		$this->controller = new OpenMetricsController('core', $this->request, $this->config, $this->exporterManager, $this->logger);
+	}
+
+	public function testGetMetrics(): void {
+		$output = $this->createMock(IOutput::class);
+		$fullOutput = '';
+		$output->method('setOutput')
+			->willReturnCallback(function ($output) use (&$fullOutput) {
+				$fullOutput .= $output;
+			});
+		$this->config->expects($this->once())
+			->method('getSystemValue')
+			->with('openmetrics_allowed_clients')
+			->willReturn(['192.168.0.0/16']);
+		$response = $this->controller->export();
+		$this->assertInstanceOf(StreamTraversableResponse::class, $response);
+		$this->assertEquals('200', $response->getStatus());
+		$this->assertEquals('application/openmetrics-text; version=1.0.0; charset=utf-8', $response->getHeaders()['Content-Type']);
+		$expected = <<<EXPECTED
+			# TYPE nextcloud_exporter_duration gauge
+			# UNIT nextcloud_exporter_duration seconds
+			# HELP nextcloud_exporter_duration Exporter run time
+			nextcloud_exporter_duration %f
+
+			# EOF
+
+			EXPECTED;
+		$response->callback($output);
+		$this->assertStringMatchesFormat($expected, $fullOutput);
+	}
+
+	public function testGetMetricsFromForbiddenIp(): void {
+		$this->config->expects($this->once())
+			->method('getSystemValue')
+			->with('openmetrics_allowed_clients')
+			->willReturn(['1.2.3.4']);
+		$response = $this->controller->export();
+		$this->assertInstanceOf(Response::class, $response);
+		$this->assertEquals('403', $response->getStatus());
+	}
+}