fc94930b16b4ec313cdb3a7eea5dd852ee0e42f6
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-01-13 14:06:54 -0500

    fix(app-settings): provide groups by initial state
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- apps/settings/lib/Controller/AppSettingsController.php
+++ apps/settings/lib/Controller/AppSettingsController.php
@@ -69,6 +69,7 @@ class AppSettingsController extends Controller {
 		private CategoryFetcher $categoryFetcher,
 		private AppFetcher $appFetcher,
 		private IFactory $l10nFactory,
+		private IGroupManager $groupManager,
 		private BundleFetcher $bundleFetcher,
 		private Installer $installer,
 		private IURLGenerator $urlGenerator,
@@ -93,6 +94,13 @@ class AppSettingsController extends Controller {
 		$this->initialState->provideInitialState('appstoreEnabled', $this->config->getSystemValueBool('appstoreenabled', true));
 		$this->initialState->provideInitialState('appstoreBundles', $this->getBundles());
 		$this->initialState->provideInitialState('appstoreUpdateCount', count($this->getAppsWithUpdates()));
+
+		$groups = array_map(static fn (IGroup $group): array => [
+			'id' => $group->getGID(),
+			'name' => $group->getDisplayName(),
+		], $this->groupManager->search('', 5));
+
+		$this->initialState->provideInitialState('usersSettings', [ 'systemGroups' => $groups]);
 
 		if ($this->appManager->isEnabledForAnyone('app_api')) {
 			try {
