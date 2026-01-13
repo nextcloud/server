108858daefc29ad91d8ceebcb4d04cf1da4b428e
Ferdinand Thiessen <opensource@fthiessen.de>
2026-01-13 14:44:53 +0100

    refactor(encryption): migrate to Vue 3 and Typescript and script setup
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/encryption/tests/Settings/AdminTest.php
+++ apps/encryption/tests/Settings/AdminTest.php
@@ -10,6 +10,8 @@ namespace OCA\Encryption\Tests\Settings;
 
 use OCA\Encryption\Settings\Admin;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\IAppConfig;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\ISession;
@@ -29,16 +31,20 @@ class AdminTest extends TestCase {
 	protected IConfig&MockObject $config;
 	protected IUserManager&MockObject $userManager;
 	protected ISession&MockObject $session;
+	protected IInitialState&MockObject $initialState;
+	protected IAppConfig&MockObject $appConfig;
 
 	protected function setUp(): void {
 		parent::setUp();
 
-		$this->l = $this->getMockBuilder(IL10N::class)->getMock();
-		$this->logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
-		$this->userSession = $this->getMockBuilder(IUserSession::class)->getMock();
-		$this->config = $this->getMockBuilder(IConfig::class)->getMock();
-		$this->userManager = $this->getMockBuilder(IUserManager::class)->getMock();
-		$this->session = $this->getMockBuilder(ISession::class)->getMock();
+		$this->l = $this->createMock(IL10N::class);
+		$this->logger = $this->createMock(LoggerInterface::class);
+		$this->userSession = $this->createMock(IUserSession::class);
+		$this->config = $this->createMock(IConfig::class);
+		$this->userManager = $this->createMock(IUserManager::class);
+		$this->session = $this->createMock(ISession::class);
+		$this->initialState = $this->createMock(IInitialState::class);
+		$this->appConfig = $this->createMock(IAppConfig::class);
 
 		$this->admin = new Admin(
 			$this->l,
@@ -46,11 +52,18 @@ class AdminTest extends TestCase {
 			$this->userSession,
 			$this->config,
 			$this->userManager,
-			$this->session
+			$this->session,
+			$this->initialState,
+			$this->appConfig,
 		);
 	}
 
 	public function testGetForm(): void {
+		$this->appConfig
+			->method('getValueBool')
+			->willReturnMap([
+				['encryption', 'recoveryAdminEnabled', true]
+			]);
 		$this->config
 			->method('getAppValue')
 			->willReturnCallback(function ($app, $key, $default) {
@@ -62,13 +75,17 @@ class AdminTest extends TestCase {
 				}
 				return $default;
 			});
-		$params = [
-			'recoveryEnabled' => '1',
-			'initStatus' => '0',
-			'encryptHomeStorage' => true,
-			'masterKeyEnabled' => true
-		];
-		$expected = new TemplateResponse('encryption', 'settings-admin', $params, '');
+
+		$this->initialState
+			->expects(self::once())
+			->method('provideInitialState')
+			->with('adminSettings', [
+				'recoveryEnabled' => true,
+				'initStatus' => '0',
+				'encryptHomeStorage' => true,
+				'masterKeyEnabled' => true
+			]);
+		$expected = new TemplateResponse('encryption', 'settings', renderAs: '');
 		$this->assertEquals($expected, $this->admin->getForm());
 	}
 
