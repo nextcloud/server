d34905b26c3646e1af3d27c5f3ec21587e8186a6
Julien Veyssier <julien-nc@posteo.net>
2026-01-14 13:30:07 +0100

    feat(user-picker): fix REUSE issues
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- apps/user_picker/src/views/ProfilePickerReferenceWidget.vue
+++ apps/user_picker/src/views/ProfilePickerReferenceWidget.vue
@@ -1,3 +1,8 @@
+<!--
+  - SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
 	<div class="profile-reference">
 		<div class="profile-reference__wrapper">
