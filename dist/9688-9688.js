930c69a4f4097ea995ca5524b22e100f19948e52
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-01-18 23:18:30 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- dist/9688-9688.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunknextcloud=globalThis.webpackChunknextcloud||[]).push([[9688],{99688(e,l,c){c.d(l,{FilePickerVue:()=>i});const i=(0,c(85471).$V)(()=>Promise.all([c.e(4208),c.e(9165),c.e(6045)]).then(c.bind(c,16045)))}}]);
-//# sourceMappingURL=9688-9688.js.map?v=6268ffd17587275b3020
