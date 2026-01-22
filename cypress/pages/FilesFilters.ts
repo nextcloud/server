6e4dcc6ce041dfb8d55d4c63e5c4597d5be160b4
Ferdinand Thiessen <opensource@fthiessen.de>
2026-01-22 12:21:42 +0100

    test: adjust Cypress tests for refactored files app UI
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- cypress/pages/FilesFilters.ts
+++ cypress/pages/FilesFilters.ts
@@ -7,8 +7,62 @@
  * Page object model for the files filters
  */
 export class FilesFilterPage {
-	filterContainter() {
-		return cy.get('[data-cy-files-filters]')
+	/**
+	 * Get the filters menu button (only on narrow and medium widths)
+	 */
+	getFiltersMenuToggle() {
+		return cy.get('[data-test-id="files-list-filters"]')
+			.findByRole('button', { name: 'Filters' })
+	}
+
+	/**
+	 * Get and trigger the filter within the menu (only on narrow and medium widths)
+	 *
+	 * @param name - The name of the filter button
+	 */
+	triggerFilterMenu(name: string | RegExp) {
+		cy.get('[data-test-id="files-list-filters"]')
+			.findByRole('button', { name: 'Filters' })
+			.should('be.visible')
+			.as('filtersMenuToggle')
+			.click()
+
+		cy.get('@filtersMenuToggle')
+			.should('have.attr', 'aria-expanded', 'true')
+
+		cy.findByRole('menu')
+			.should('be.visible')
+			.findByRole('menuitem', { name })
+			.should('be.visible')
+			.click()
+	}
+
+	/**
+	 * Get and trigger the filter button if the files list is wide enough to show all filters
+	 *
+	 * @param name - The name of the filter button
+	 */
+	triggerFilterButton(name: string | RegExp) {
+		cy.get('[data-test-id="files-list-filters"]')
+			.findByRole('button', { name })
+			.should('be.visible')
+			.click()
+	}
+
+	triggerFilter(name: string | RegExp) {
+		cy.get('[data-cy-files-list]')
+			.should('be.visible')
+			.if(($el) => expect($el.get(0).clientWidth).to.be.gte(1024))
+			.then(() => this.triggerFilterButton(name))
+			.else()
+			.then(() => this.triggerFilterMenu(name))
+	}
+
+	closeFilterMenu() {
+		cy.get('[data-test-id="files-list-filters"]')
+			.findAllByRole('button')
+			.filter('[aria-expanded="true"]')
+			.click({ multiple: true })
 	}
 
 	activeFiltersList() {
