930a39900010867ec892b08a21ea8fcc6e2c3fa4
silver <s.szmajduch@posteo.de>
2026-01-27 11:13:22 +0100

    feat(files_versions): Auto-reload versions tab on file
    Listen for files:node:updated events and automatically refresh the
    versions list when the current file is saved, eliminating the need to
    manually close and reopen the sidebar to see new versions.
    
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- apps/files_versions/src/views/VersionTab.vue
+++ apps/files_versions/src/views/VersionTab.vue
@@ -142,11 +142,28 @@ export default {
 	},
 	mounted() {
 		subscribe('files_versions:restore:restored', this.fetchVersions)
+		subscribe('files:node:updated', this.handleNodeUpdated)
 	},
 	beforeUnmount() {
 		unsubscribe('files_versions:restore:restored', this.fetchVersions)
+		unsubscribe('files:node:updated', this.handleNodeUpdated)
 	},
 	methods: {
+		/**
+		 * Handle files:node:updated event to reload versions when the current file is saved
+		 *
+		 * @param {object} node The updated node
+		 */
+		handleNodeUpdated(node) {
+			// Reload if this is the currently open file
+			if (this.fileInfo && node.fileid === this.fileInfo.id) {
+				// Delay to let the server create the new version
+				setTimeout(() => {
+					this.fetchVersions()
+				}, 1000)
+			}
+		},
+
 		/**
 		 * Update current fileInfo and fetch new data
 		 *
