green-coding-solutions/nextcloud-server

refactor(appstore): migrate daemon selection dialog to Typescript and Vue 3
Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialog.vue
@@ -1,0 +1,49 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import NcDialog from '@nextcloud/vue/components/NcDialog'
+import DaemonSelectionDialogList from './DaemonSelectionDialogList.vue'
+
+/**
+ * Whether the dialog is shown
+ */
+const show = defineModel<boolean>('show')
+
+defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions
+}>()
+
+/**
+ * Close the dialog
+ */
+function closeModal() {
+	show.value = false
+}
+</script>
+
+<template>
+	<NcDialog
+		:open="show"
+		:name="t('settings', 'Choose Deploy Daemon for {appName}', { appName: app.name })"
+		size="normal"
+		@update:open="closeModal">
+		<DaemonSelectionDialogList
+			:app="app"
+			:deploy-options="deployOptions"
+			@close="closeModal" />
+	</NcDialog>
+</template>

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialogList.vue
@@ -1,0 +1,85 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import { generateUrl } from '@nextcloud/router'
+import NcButton from '@nextcloud/vue/components/NcButton'
+import NcEmptyContent from '@nextcloud/vue/components/NcEmptyContent'
+import FormatListBullet from 'vue-material-design-icons/FormatListBulleted.vue'
+import DaemonSelectionDialogListEntry from './DaemonSelectionDialogListEntry.vue'
+import { useExAppsStore } from '../../store/exApps.ts'
+
+defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions | undefined
+}>()
+
+const emit = defineEmits(['close'])
+
+const appApiAdminPage = generateUrl('/settings/admin/app_api')
+
+const store = useExAppsStore()
+
+/**
+ * Close the modal
+ */
+function closeModal() {
+	emit('close')
+}
+</script>
+
+<template>
+	<div class="daemon-selection-list">
+		<ul
+			v-if="store.dockerDaemons.length > 0"
+			:aria-label="t('settings', 'Registered Deploy daemons list')">
+			<DaemonSelectionDialogListEntry
+				v-for="daemon in store.dockerDaemons"
+				:key="daemon.id"
+				:daemon="daemon"
+				:is-default="store.defaultDaemon!.name === daemon.name"
+				:app="app"
+				:deploy-options="deployOptions"
+				@close="closeModal" />
+		</ul>
+		<NcEmptyContent
+			v-else
+			class="daemon-selection-list__empty-content"
+			:name="t('settings', 'No Deploy daemons configured')"
+			:description="t('settings', 'Register a custom one or setup from available templates')">
+			<template #icon>
+				<FormatListBullet :size="20" />
+			</template>
+			<template #action>
+				<NcButton :href="appApiAdminPage">
+					{{ t('settings', 'Manage Deploy daemons') }}
+				</NcButton>
+			</template>
+		</NcEmptyContent>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.daemon-selection-list {
+	max-height: 350px;
+	overflow-y: scroll;
+	padding: 2rem;
+
+	&__empty-content {
+		margin-top: 0;
+		text-align: center;
+	}
+}
+</style>

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialogListEntry.vue
@@ -1,0 +1,69 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployDaemon, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import { computed } from 'vue'
+import NcListItem from '@nextcloud/vue/components/NcListItem'
+import { useExAppsStore } from '../../store/exApps.ts'
+
+const props = defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+	/**
+	 * The daemon to use
+	 */
+	daemon: IDeployDaemon
+	/**
+	 * Whether this daemon is the default one
+	 */
+	isDefault: boolean
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions
+}>()
+
+const emit = defineEmits<{
+	close: []
+}>()
+
+const store = useExAppsStore()
+const itemTitle = computed(() => `${props.daemon.name} - ${props.daemon.display_name}`)
+
+/**
+ * Close the modal
+ */
+function closeModal() {
+	emit('close')
+}
+
+/**
+ * Enable the app using the selected daemon
+ */
+function selectDaemonAndInstall() {
+	store.enable(props.app.id, props.daemon, props.deployOptions)
+	closeModal()
+}
+</script>
+
+<template>
+	<NcListItem
+		:name="itemTitle"
+		:details="isDefault ? t('settings', 'Default') : ''"
+		:force-display-actions="true"
+		:counter-number="daemon.exAppsCount"
+		:active="isDefault"
+		counter-type="highlighted"
+		@click.stop="selectDaemonAndInstall">
+		<template #subname>
+			{{ daemon.accepts_deploy_id }}
+		</template>
+	</NcListItem>
+</template>

nextcloud/server

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialog.vue
@@ -1,0 +1,49 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import NcDialog from '@nextcloud/vue/components/NcDialog'
+import DaemonSelectionDialogList from './DaemonSelectionDialogList.vue'
+
+/**
+ * Whether the dialog is shown
+ */
+const show = defineModel<boolean>('show')
+
+defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions
+}>()
+
+/**
+ * Close the dialog
+ */
+function closeModal() {
+	show.value = false
+}
+</script>
+
+<template>
+	<NcDialog
+		:open="show"
+		:name="t('appstore', 'Choose Deploy Daemon for {appName}', { appName: app.name })"
+		size="normal"
+		@update:open="closeModal">
+		<DaemonSelectionDialogList
+			:app="app"
+			:deploy-options="deployOptions"
+			@close="closeModal" />
+	</NcDialog>
+</template>

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialogList.vue
@@ -1,0 +1,85 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import { generateUrl } from '@nextcloud/router'
+import NcButton from '@nextcloud/vue/components/NcButton'
+import NcEmptyContent from '@nextcloud/vue/components/NcEmptyContent'
+import FormatListBullet from 'vue-material-design-icons/FormatListBulleted.vue'
+import DaemonSelectionDialogListEntry from './DaemonSelectionDialogListEntry.vue'
+import { useExAppsStore } from '../../store/exApps.ts'
+
+defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions | undefined
+}>()
+
+const emit = defineEmits(['close'])
+
+const appApiAdminPage = generateUrl('/settings/admin/app_api')
+
+const store = useExAppsStore()
+
+/**
+ * Close the modal
+ */
+function closeModal() {
+	emit('close')
+}
+</script>
+
+<template>
+	<div class="daemon-selection-list">
+		<ul
+			v-if="store.dockerDaemons.length > 0"
+			:aria-label="t('appstore', 'Registered Deploy daemons list')">
+			<DaemonSelectionDialogListEntry
+				v-for="daemon in store.dockerDaemons"
+				:key="daemon.id"
+				:daemon="daemon"
+				:is-default="store.defaultDaemon!.name === daemon.name"
+				:app="app"
+				:deploy-options="deployOptions"
+				@close="closeModal" />
+		</ul>
+		<NcEmptyContent
+			v-else
+			class="daemon-selection-list__empty-content"
+			:name="t('appstore', 'No Deploy daemons configured')"
+			:description="t('appstore', 'Register a custom one or setup from available templates')">
+			<template #icon>
+				<FormatListBullet :size="20" />
+			</template>
+			<template #action>
+				<NcButton :href="appApiAdminPage">
+					{{ t('appstore', 'Manage Deploy daemons') }}
+				</NcButton>
+			</template>
+		</NcEmptyContent>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.daemon-selection-list {
+	max-height: 350px;
+	overflow-y: scroll;
+	padding: 2rem;
+
+	&__empty-content {
+		margin-top: 0;
+		text-align: center;
+	}
+}
+</style>

apps/appstore/src/components/DaemonSelectionDialog/DaemonSelectionDialogListEntry.vue
@@ -1,0 +1,69 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import type { IAppstoreExApp, IDeployDaemon, IDeployOptions } from '../../apps.d.ts'
+
+import { t } from '@nextcloud/l10n'
+import { computed } from 'vue'
+import NcListItem from '@nextcloud/vue/components/NcListItem'
+import { useExAppsStore } from '../../store/exApps.ts'
+
+const props = defineProps<{
+	/**
+	 * The app to enable
+	 */
+	app: IAppstoreExApp
+	/**
+	 * The daemon to use
+	 */
+	daemon: IDeployDaemon
+	/**
+	 * Whether this daemon is the default one
+	 */
+	isDefault: boolean
+	/**
+	 * Deployment options
+	 */
+	deployOptions?: IDeployOptions
+}>()
+
+const emit = defineEmits<{
+	close: []
+}>()
+
+const store = useExAppsStore()
+const itemTitle = computed(() => `${props.daemon.name} - ${props.daemon.display_name}`)
+
+/**
+ * Close the modal
+ */
+function closeModal() {
+	emit('close')
+}
+
+/**
+ * Enable the app using the selected daemon
+ */
+function selectDaemonAndInstall() {
+	store.enable(props.app.id, props.daemon, props.deployOptions)
+	closeModal()
+}
+</script>
+
+<template>
+	<NcListItem
+		:name="itemTitle"
+		:details="isDefault ? t('appstore', 'Default') : ''"
+		:force-display-actions="true"
+		:counter-number="daemon.exAppsCount"
+		:active="isDefault"
+		counter-type="highlighted"
+		@click.stop="selectDaemonAndInstall">
+		<template #subname>
+			{{ daemon.accepts_deploy_id }}
+		</template>
+	</NcListItem>
+</template>