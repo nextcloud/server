63195fbf33700b93c6f7ff9c1183f71f8d19463b
Marcel Klehr <mklehr@gmx.net>
2026-02-10 13:26:47 +0100

    feat(user_ldap): Add config for partial search compatibility with ActiveDirectory
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- apps/user_ldap/lib/Access.php
+++ apps/user_ldap/lib/Access.php
@@ -1568,7 +1568,12 @@ class Access extends LDAPUtility {
 		if ($term === '') {
 			$result = '*';
 		} elseif ($allowEnum) {
-			$result = $term . '*';
+			$activeDirectoryCompat = $this->appConfig->getValueBool('user_ldap', 'partial_search_active_directory_compatibility', false);
+			if ($activeDirectoryCompat) {
+				$result = '*' . $term . '*';
+			} else {
+				$result = $term . '*';
+			}
 		}
 		return $result;
 	}
