3693cbeaf9700e67e780cd8a46703633f6c07f16
provokateurin <kate@provokateurin.de>
2026-02-12 12:09:19 +0100

    fix(IMetadataVersion): Use correct return type for getMetadata
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- build/rector-strict.php
+++ build/rector-strict.php
@@ -13,6 +13,7 @@ return (require __DIR__ . '/rector-shared.php')
 		$nextcloudDir . '/core/BackgroundJobs/ExpirePreviewsJob.php',
 		$nextcloudDir . '/lib/public/IContainer.php',
 		$nextcloudDir . '/apps/dav/lib/Connector/Sabre/Node.php',
+		$nextcloudDir . '/apps/files_versions/lib/Versions/IMetadataVersion.php',
 	])
 	->withPreparedSets(
 		deadCode: true,
