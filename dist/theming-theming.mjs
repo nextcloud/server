b812040314f847d6acaf07512b99fa3e3fb57177
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-02-15 12:39:59 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- dist/theming-theming.mjs
+++ dist/theming-theming.mjs
@@ -1,2 +1,2 @@
-import{l as o}from"./index-DCpg1aui.chunk.mjs";window.OCA.Theming=o("theming","data");
+import{l as o}from"./index-Ma7sfat2.chunk.mjs";window.OCA.Theming=o("theming","data");
 //# sourceMappingURL=theming-theming.mjs.map
