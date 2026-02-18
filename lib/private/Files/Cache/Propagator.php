dbdb7ce74dc24035ce413ff2a0a110bbc9d2f853
provokateurin <kate@provokateurin.de>
2026-02-18 13:43:22 +0100

    fix(Propagator): Skip groupfolders root entries
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/Files/Cache/Propagator.php
+++ lib/private/Files/Cache/Propagator.php
@@ -8,6 +8,7 @@
 namespace OC\Files\Cache;
 
 use OC\DB\Exceptions\DbalException;
+use OC\Files\Storage\LocalRootStorage;
 use OC\Files\Storage\Wrapper\Encryption;
 use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\Files\Cache\IPropagator;
@@ -68,6 +69,10 @@ class Propagator implements IPropagator {
 		$storageId = $this->storage->getStorageCache()->getNumericId();
 
 		$parents = $this->getParents($internalPath);
+		if ($this->storage->instanceOfStorage(LocalRootStorage::class) && str_starts_with($internalPath, '__groupfolders')) {
+			// Remove '' and '__groupfolders'
+			$parents = array_slice($parents, 2);
+		}
 
 		if ($this->inBatch) {
 			foreach ($parents as $parent) {
