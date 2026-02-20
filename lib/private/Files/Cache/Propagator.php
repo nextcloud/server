41d12ac013409edc0b7e85e5f27abcc79b0e8753
Carl Schwan <carlschwan@kde.org>
2026-02-20 17:36:34 +0100

    fix(propagator): Don't run queries if no parents
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- lib/private/Files/Cache/Propagator.php
+++ lib/private/Files/Cache/Propagator.php
@@ -79,6 +79,10 @@ class Propagator implements IPropagator {
 			}
 		}
 
+		if ($parents === []) {
+			return;
+		}
+
 		if ($this->inBatch) {
 			foreach ($parents as $parent) {
 				$this->addToBatch($parent, $time, $sizeDifference);
