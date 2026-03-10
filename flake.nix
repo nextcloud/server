dbba408195d44cd40fe67289d0ebba264144de4c
provokateurin <kate@provokateurin.de>
2026-03-10 16:25:56 +0100

    chore(deps): Update haze to 2.2.0
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# The flake is compatible with >=stable30, so make sure to backport all changes to stable30 and up.
 {
   nixConfig = {
     extra-substituters = [ "https://fossar.cachix.org" ];
@@ -16,7 +17,10 @@
 	};
     haze = {
       url = "git+https://codeberg.org/icewind/haze.git";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        phps.follows = "phps";
+      };
     };
   };
 
