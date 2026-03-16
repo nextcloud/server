5f80f26799967920b5a0527a302c4ac4439c530c
Joas Schilling <coding@schilljs.com>
2026-03-16 08:38:16 +0100

    chore: Fix SPDX header
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files_sharing/lib/External/ExternalShareMapper.php
+++ apps/files_sharing/lib/External/ExternalShareMapper.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2025 Nextcloud GmbH
- * SPDX-FileContributor: Carl Schwan
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
