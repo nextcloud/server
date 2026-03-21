e4436ddc8dd78cf741ea2ee9bedcd659afa4050b
Jonas <jonas@freesources.org>
2026-03-21 15:08:05 +0100

    fix(ICalendar): allow to search for event URI
    Signed-off-by: Jonas <jonas@freesources.org>

--- apps/dav/tests/unit/CalDAV/CalDavBackendTest.php
+++ apps/dav/tests/unit/CalDAV/CalDavBackendTest.php
@@ -1840,6 +1840,72 @@ EOD;
 		$this->assertEquals('Missing DTSTART 2', $results[3]['objects'][0]['SUMMARY'][0]);
 	}
 
+	public function testSearchByUri(): void {
+		$calendarId = $this->createTestCalendar();
+		$uris = [];
+		$calData = [];
+
+		$uris[] = static::getUniqueID('calobj');
+		$calData[] = <<<'EOD'
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:Nextcloud Calendar
+BEGIN:VEVENT
+CREATED;VALUE=DATE-TIME:20260323T093039Z
+UID:search-by-uri-test1
+LAST-MODIFIED;VALUE=DATE-TIME:20260323T093039Z
+DTSTAMP;VALUE=DATE-TIME:20260323T093039Z
+SUMMARY:First Test Event
+DTSTART;VALUE=DATE-TIME:20260323T093039Z
+DTEND;VALUE=DATE-TIME:20260323T093039Z
+CLASS:PUBLIC
+END:VEVENT
+END:VCALENDAR
+EOD;
+
+		$uris[] = static::getUniqueID('calobj');
+		$calData[] = <<<'EOD'
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:Nextcloud Calendar
+BEGIN:VEVENT
+CREATED;VALUE=DATE-TIME:20260323T093039Z
+UID:search-by-uri-test2
+LAST-MODIFIED;VALUE=DATE-TIME:20260323T093039Z
+DTSTAMP;VALUE=DATE-TIME:20260323T093039Z
+SUMMARY:Second Test Event
+DTSTART;VALUE=DATE-TIME:20260323T093039Z
+DTEND;VALUE=DATE-TIME:20260323T093039Z
+CLASS:PUBLIC
+END:VEVENT
+END:VCALENDAR
+EOD;
+
+		foreach ($uris as $i => $uri) {
+			$this->backend->createCalendarObject($calendarId, $uri, $calData[$i]);
+		}
+
+		$calendarInfo = [
+			'id' => $calendarId,
+			'principaluri' => 'user1',
+			'{http://owncloud.org/ns}owner-principal' => 'user1',
+		];
+
+		// Searching by first event's URI returns this event
+		$results = $this->backend->search($calendarInfo, '', [], ['uri' => $uris[0]], null, null);
+		$this->assertCount(1, $results);
+		$this->assertEquals($uris[0], $results[0]['uri']);
+
+		// Searching by second event's URI returns this event
+		$results = $this->backend->search($calendarInfo, '', [], ['uri' => $uris[1]], null, null);
+		$this->assertCount(1, $results);
+		$this->assertEquals($uris[1], $results[0]['uri']);
+
+		// Searching by a non-existent URI returns nothing
+		$result = $this->backend->search($calendarInfo, '', [], ['uri' => 'nonexistant.ical'], null, null);
+		$this->assertCount(0, $result);
+	}
+
 	public function testUnshare(): void {
 		$principalGroup = 'principal:' . self::UNIT_TEST_GROUP;
 		$principalUser = 'principal:' . self::UNIT_TEST_USER;
