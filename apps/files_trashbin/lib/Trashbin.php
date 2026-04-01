e73482915447937b4bd2bda7745648ea7226ae0d
Robin Appelman <robin@icewind.nl>
2026-04-01 19:17:25 +0200

    fix: catch all exceptions during trashbin cache move
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_trashbin/lib/Trashbin.php
+++ apps/files_trashbin/lib/Trashbin.php
@@ -352,7 +352,7 @@ class Trashbin implements IEventListener {
 				}
 				$trashStorage->getUpdater()->update($trashInternalPath, null, $sizeDifference);
 			}
-		} catch (CopyRecursiveException $e) {
+		} catch (\Exception $e) {
 			$moveSuccessful = false;
 			if ($trashStorage->file_exists($trashInternalPath)) {
 				$trashStorage->unlink($trashInternalPath);
