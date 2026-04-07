fd174d62fcdaf9dba5cb7dc915e830a6fce8b401
Côme Chilliet <come.chilliet@nextcloud.com>
2026-04-07 16:08:01 +0200

    fix: Fix TemplateLayout tests
    The behaviour of getAppNameFromPath is really different from what was
     mocked, so I’m not sure whether the class behaves as initially
     intended. I adapted the test to match the class behavior for now.
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/lib/TemplateLayoutTest.php
+++ tests/lib/TemplateLayoutTest.php
@@ -49,8 +49,8 @@ class TemplateLayoutTest extends \Test\TestCase {
 
 	#[\PHPUnit\Framework\Attributes\DataProvider('dataVersionHash')]
 	public function testVersionHash(
-		string|false $path,
-		string|false $file,
+		string $path,
+		string $file,
 		bool $installed,
 		bool $debug,
 		string $expected,
@@ -58,13 +58,13 @@ class TemplateLayoutTest extends \Test\TestCase {
 		$this->appManager->expects(self::any())
 			->method('getAppVersion')
 			->willReturnCallback(fn ($appId) => match ($appId) {
-				'shippedApp' => 'shipped_1',
-				'otherApp' => 'other_2',
+				'shipped' => 'shipped_1',
+				'other' => 'other_2',
 				default => "$appId",
 			});
 		$this->appManager->expects(self::any())
 			->method('isShipped')
-			->willReturnCallback(fn (string $app) => $app === 'shippedApp');
+			->willReturnCallback(fn (string $app) => $app === 'shipped');
 
 		$this->config->expects(self::atLeastOnce())
 			->method('getSystemValueBool')
@@ -80,31 +80,20 @@ class TemplateLayoutTest extends \Test\TestCase {
 		$this->request->method('getPathInfo')
 			->willReturn('/' . $path);
 
-		$this->templateLayout = $this->getMockBuilder(TemplateLayout::class)
-			->onlyMethods(['getAppNamefromPath'])
-			->setConstructorArgs([
-				$this->config,
-				$this->appConfig,
-				$this->appManager,
-				$this->initialState,
-				$this->navigationManager,
-				$this->templateManager,
-				$this->serverVersion,
-				$this->request,
-			])
-			->getMock();
+		$this->templateLayout = new TemplateLayout(
+			$this->config,
+			$this->appConfig,
+			$this->appManager,
+			$this->initialState,
+			$this->navigationManager,
+			$this->templateManager,
+			$this->serverVersion,
+			$this->request,
+		);
 
 		$layout = $this->templateLayout->getPageTemplate(TemplateResponse::RENDER_AS_ERROR, '');
 
-		self::invokePrivate(TemplateLayout::class, 'versionHash', ['version_hash']);
-
-		$this->templateLayout->expects(self::any())
-			->method('getAppNamefromPath')
-			->willReturnCallback(fn ($appName) => match($appName) {
-				'apps/shipped' => 'shippedApp',
-				'other/app.css' => 'otherApp',
-				default => false,
-			});
+		self::invokePrivate($this->templateLayout, 'versionHash', ['version_hash']);
 
 		$hash = self::invokePrivate($this->templateLayout, 'getVersionHashSuffix', [$path, $file]);
 		self::assertEquals($expected, $hash);
@@ -114,9 +103,8 @@ class TemplateLayoutTest extends \Test\TestCase {
 		return [
 			'no hash if in debug mode' => ['apps/shipped', 'style.css', true, true, ''],
 			'only version hash if not installed' => ['apps/shipped', 'style.css', false, false, '?v=version_hash'],
-			'version hash with cache buster if app not found' => ['unknown/path', '', true, false, '?v=version_hash-42'],
-			'version hash with cache buster if neither path nor file provided' => [false, false, true, false, '?v=version_hash-42'],
-			'app version hash if external app' => ['', 'other/app.css', true, false, '?v=' . substr(md5('other_2'), 0, 8) . '-42'],
+			'version hash with cache buster if neither path nor file provided' => ['', '', true, false, '?v=version_hash-42'],
+			'app version hash if external app' => ['apps/other', 'app.css', true, false, '?v=' . substr(md5('other_2'), 0, 8) . '-42'],
 			'app version and version hash if shipped app' => ['apps/shipped', 'style.css', true, false, '?v=' . substr(md5('shipped_1-version_hash'), 0, 8) . '-42'],
 			'prefer path over file' => ['apps/shipped', 'other/app.css', true, false, '?v=' . substr(md5('shipped_1-version_hash'), 0, 8) . '-42'],
 		];
