f5e928845317040aff9d6960d64037023cdf4fc0
Ferdinand Thiessen <opensource@fthiessen.de>
2026-04-08 23:09:51 +0200

    fix(dav): do not list intermediate upload folders
    fix(dav): do not list intermediate upload folders
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>
    
    [skip ci]

--- apps/dav/lib/Upload/UploadHome.php
+++ apps/dav/lib/Upload/UploadHome.php
@@ -29,6 +29,7 @@ use OC\Files\Filesystem;
 use OC\Files\View;
 use OCA\DAV\Connector\Sabre\Directory;
 use Sabre\DAV\Exception\Forbidden;
+use Sabre\DAV\Exception\MethodNotAllowed;
 use Sabre\DAV\ICollection;
 
 class UploadHome implements ICollection {
