1e9b70832eaebfafdf23f46af4bb825c93d16022
Daniel Calviño Sánchez <danxuliu@gmail.com>
2026-04-15 03:34:21 +0200

    fix: Fix app name not translated in the title of public pages
    @nextcloud/vue automatically adds the localized app name to the page
    title if a page heading but not an explicit page title is given. The
    localized app name is expected to be provided in the "core->apps"
    initial state, but that was set only when rendering a page as a
    registered user. In public pages that data was not provided, so the
    title contained the app ID rather than the localized app name.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/private/TemplateLayout.php
+++ lib/private/TemplateLayout.php
@@ -163,6 +163,8 @@ class TemplateLayout {
 				$page->assign('appid', $appId);
 				$page->assign('bodyid', 'body-public');
 
+				$this->initialState->provideInitialState('core', 'apps', array_values($this->navigationManager->getAll()));
+
 				// Set logo link target
 				$logoUrl = $this->config->getSystemValueString('logo_url', '');
 				$page->assign('logoUrl', $logoUrl);
