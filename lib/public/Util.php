1dc52152156376ac626ea796c9e63fd1e62cc0cd
Ferdinand Thiessen <opensource@fthiessen.de>
2026-04-15 13:18:40 +0200

    chore: adjust version in doc block
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/public/Util.php
+++ lib/public/Util.php
@@ -199,7 +199,7 @@ class Util {
 	 * This is used to ensure that the global state is initialized before all other scripts.
 	 *
 	 * @param string $name - The script name
-	 * @since 34.0.0
+	 * @since 33.0.3
 	 */
 	private static function scriptOrderValue(string $name): int {
 		return match($name) {
