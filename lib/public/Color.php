988fa90ca98c59b86af76446aefd7d148d455e8e
Josh <josh.t.richards@gmail.com>
2026-04-19 20:52:01 -0400

    refactor(Color): add stronger typing and update inaccurate comments
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/public/Color.php
+++ lib/public/Color.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -8,20 +9,18 @@ namespace OCP;
 
 /**
  * Simple RGB color container
+ *
  * @since 25.0.0
  */
 class Color {
-	private int $r;
-	private int $g;
-	private int $b;
-
 	/**
 	 * @since 25.0.0
 	 */
-	public function __construct($r, $g, $b) {
-		$this->r = $r;
-		$this->g = $g;
-		$this->b = $b;
+	public function __construct(
+		private int $r,
+		private int $g,
+		private int $b,
+	) {
 	}
 
 	/**
@@ -61,7 +60,7 @@ class Color {
 	}
 
 	/**
-	 * Returns the blue component of this color as an int from 0 to 255
+	 * Returns the blue color component of this color as an int from 0 to 255
 	 *
 	 * @since 25.0.0
 	 */
@@ -79,7 +78,7 @@ class Color {
 	}
 
 	/**
-	 * Returns the name of the color in the format "#RRGGBB"; i.e. a "#" character followed by three two-digit hexadecimal numbers.
+	 * Returns the hex triplet color value as a string ("#RRGGBB")
 	 *
 	 * @since 25.0.0
 	 */
@@ -87,35 +86,46 @@ class Color {
 		return sprintf('#%02x%02x%02x', $this->r, $this->g, $this->b);
 	}
 
+	// Utility Functions
+
 	/**
-	 * Mix two colors
+	 * Generate a progression of colors starting with $color1 and moving toward $color2.
 	 *
-	 * @param int $steps the number of intermediate colors that should be generated for the palette
-	 * @param Color $color1 the first color
-	 * @param Color $color2 the second color
-	 * @return list<Color>
+	 * @param int $steps Total number of colors to return (including $color1, but excluding $color2); should be at least 2
+	 * @param Color $color1 The starting color (index 0 of the returned list)
+	 * @param Color $color2 The target color used to calculate the transition
+	 * @return list<Color> The list of colors starting with $color1 up to but not including $color2
 	 * @since 25.0.0
 	 */
 	public static function mixPalette(int $steps, Color $color1, Color $color2): array {
 		$palette = [$color1];
-		$step = self::stepCalc($steps, [$color1, $color2]);
+		[$rDelta, $gDelta, $bDelta] = self::calculateDeltas($steps, $color1, $color2);
+
 		for ($i = 1; $i < $steps; $i++) {
-			$r = intval($color1->red() + ($step[0] * $i));
-			$g = intval($color1->green() + ($step[1] * $i));
-			$b = intval($color1->blue() + ($step[2] * $i));
-			$palette[] = new Color($r, $g, $b);
+			$palette[] = new Color(
+				// TODO: Consider using round() instead of (int) truncation for more accurate color transitions.
+				(int)($color1->red() + ($rDelta * $i)),
+				(int)($color1->green() + ($gDelta * $i)),
+				(int)($color1->blue() + ($bDelta * $i)),
+			);
 		}
+
 		return $palette;
 	}
 
 	/**
-	 * Alpha blend another color with a given opacity to this color
+	 * Blend this color over a source color.
 	 *
-	 * @return Color The new color
+	 * An opacity of 0 returns $source, and 1 returns this color.
+	 *
+	 * @param float $opacity Opacity of this color, expected in the range 0.0 to 1.0
+	 * @param Color $source The source/background color
+	 * @return Color The blended color
 	 * @since 25.0.0
 	 */
 	public function alphaBlending(float $opacity, Color $source): Color {
 		return new Color(
+			// TODO: Consider using round() instead of (int) truncation for more accurate color transitions.
 			(int)((1 - $opacity) * $source->red() + $opacity * $this->red()),
 			(int)((1 - $opacity) * $source->green() + $opacity * $this->green()),
 			(int)((1 - $opacity) * $source->blue() + $opacity * $this->blue())
@@ -123,17 +133,21 @@ class Color {
 	}
 
 	/**
-	 * Calculate steps between two Colors
-	 * @param int $steps start color
-	 * @param Color[] $ends end color
-	 * @return array{0: float, 1: float, 2: float} [r,g,b] steps for each color to go from $steps to $ends
+	 * Calculate the per-channel change (RGB deltas) required to transition between two colors.
+	 *
+	 * @param int $count The number of intervals to divide the transition into >0
+	 * @param Color $start The starting color
+	 * @param Color $end The target color
+	 * @return array{0: float, 1: float, 2: float} The per-channel [r, g, b] increment required for each interval
 	 * @since 25.0.0
 	 */
-	private static function stepCalc(int $steps, array $ends): array {
-		$step = [];
-		$step[0] = ($ends[1]->red() - $ends[0]->red()) / $steps;
-		$step[1] = ($ends[1]->green() - $ends[0]->green()) / $steps;
-		$step[2] = ($ends[1]->blue() - $ends[0]->blue()) / $steps;
-		return $step;
+	private static function calculateDeltas(int $count, Color $start, Color $end): array {
+		$deltas = [];
+
+		$deltas[0] = ($end->red() - $start->red()) / $count;
+		$deltas[1] = ($end->green() - $start->green()) / $count;
+		$deltas[2] = ($end->blue() - $start->blue()) / $count;
+
+		return $deltas;
 	}
 }
