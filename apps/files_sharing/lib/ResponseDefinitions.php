2a1a48b8ffe768bc331fca83d936020d5fe83434
Robin Appelman <robin@icewind.nl>
2026-04-21 18:18:50 +0200

    chore: psalm fixes
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_sharing/lib/ResponseDefinitions.php
+++ apps/files_sharing/lib/ResponseDefinitions.php
@@ -70,7 +70,7 @@ namespace OCA\Files_Sharing;
  *     token: ?string,
  *     uid_file_owner: string,
  *     uid_owner: string,
- *     url?: string,
+ *     url?: string|null,
  * }
  *
  * @psalm-type Files_SharingDeletedShare = array{
