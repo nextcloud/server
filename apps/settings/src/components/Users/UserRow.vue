9e82d25f2284b85509cdcfde30e96b9571139f28
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-04-24 10:19:36 +0200

    refactor(settings): inline UserRowMixin into UserRow -e Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

--- apps/settings/src/components/Users/UserRow.vue
+++ apps/settings/src/components/Users/UserRow.vue
@@ -132,11 +132,11 @@ import { getCurrentUser } from '@nextcloud/auth'
 import { showSuccess } from '@nextcloud/dialogs'
 import { formatFileSize, parseFileSize } from '@nextcloud/files'
 import { confirmPassword } from '@nextcloud/password-confirmation'
+import { useFormatDateTime } from '@nextcloud/vue'
 import NcAvatar from '@nextcloud/vue/components/NcAvatar'
 import NcLoadingIcon from '@nextcloud/vue/components/NcLoadingIcon'
 import NcProgressBar from '@nextcloud/vue/components/NcProgressBar'
 import UserRowActions from './UserRowActions.vue'
-import UserRowMixin from '../../mixins/UserRowMixin.js'
 import { isObfuscated } from '../../utils/userUtils.ts'
 
 const productName = window.OC.theme.productName
@@ -150,10 +150,6 @@ export default {
 		NcProgressBar,
 		UserRowActions,
 	},
-
-	mixins: [
-		UserRowMixin,
-	],
 
 	props: {
 		user: {
@@ -198,6 +194,19 @@ export default {
 		},
 	},
 
+	setup(props) {
+		const { formattedFullTime } = useFormatDateTime(props.user.firstLoginTimestamp * 1000, {
+			relativeTime: false,
+			format: {
+				timeStyle: 'short',
+				dateStyle: 'short',
+			},
+		})
+		return {
+			formattedFullTime,
+		}
+	},
+
 	data() {
 		return {
 			rand: Math.random().toString(36).substring(2),
@@ -213,6 +222,56 @@ export default {
 	computed: {
 		isObfuscated() {
 			return isObfuscated(this.user)
+		},
+
+		usedQuota() {
+			let quota = this.user.quota.quota
+			if (quota > 0) {
+				quota = Math.min(100, Math.round(this.user.quota.used / quota * 100))
+			} else {
+				const usedInGB = this.user.quota.used / (10 * Math.pow(2, 30))
+				// asymptotic curve approaching 50% at 10GB to visualize used space with infinite quota
+				quota = 95 * (1 - (1 / (usedInGB + 1)))
+			}
+			return isNaN(quota) ? 0 : quota
+		},
+
+		userLanguage() {
+			const availableLanguages = this.languages[0].languages.concat(this.languages[1].languages)
+			const userLang = availableLanguages.find((lang) => lang.code === this.user.language)
+			if (typeof userLang !== 'object' && this.user.language !== '') {
+				return {
+					code: this.user.language,
+					name: this.user.language,
+				}
+			} else if (this.user.language === '') {
+				return false
+			}
+			return userLang
+		},
+
+		userFirstLogin() {
+			if (this.user.firstLoginTimestamp > 0) {
+				return this.formattedFullTime
+			}
+			if (this.user.firstLoginTimestamp < 0) {
+				return t('settings', 'Unknown')
+			}
+			return t('settings', 'Never')
+		},
+
+		userLastLoginTooltip() {
+			if (this.user.lastLoginTimestamp > 0) {
+				return OC.Util.formatDate(this.user.lastLoginTimestamp * 1000)
+			}
+			return ''
+		},
+
+		userLastLogin() {
+			if (this.user.lastLoginTimestamp > 0) {
+				return OC.Util.relativeModifiedDate(this.user.lastLoginTimestamp * 1000)
+			}
+			return t('settings', 'Never')
 		},
 
 		showConfig() {
