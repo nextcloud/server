074d706089e8f5a03c2ada8b82d7f4eb696fba55
rakekniven <2069590+rakekniven@users.noreply.github.com>
2026-05-01 20:34:25 +0200

    chore(l10n): Plural needed to support multiplural languages
    Reported at Transifex
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- apps/settings/lib/SetupChecks/TaskProcessingWorkerIsRunning.php
+++ apps/settings/lib/SetupChecks/TaskProcessingWorkerIsRunning.php
@@ -57,7 +57,7 @@ class TaskProcessingWorkerIsRunning implements ISetupCheck {
 		$lastIteration = (int)$this->appConfig->getValueString('core', 'ai.taskprocessing_worker_last_iteration', lazy: true);
 		if ($lastIteration > $this->timeFactory->now()->getTimestamp() - (60 * self::IS_RUNNING_IN_LAST_X_MINUTES)) {
 			return SetupResult::success(
-				$this->l10n->n('The Task Processing worker has run in the last minute.', 'The Task Processing worker has run in the last %n minutes.', self::IS_RUNNING_IN_LAST_X_MINUTES)
+				$this->l10n->n('The Task Processing worker has run in the last %n minute.', 'The Task Processing worker has run in the last %n minutes.', self::IS_RUNNING_IN_LAST_X_MINUTES)
 			);
 		}
 
