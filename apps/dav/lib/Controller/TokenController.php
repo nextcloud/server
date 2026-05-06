894b177572186459f8cb781f0752e6808e436008
Micke Nordin <kano@sunet.se>
2026-05-06 12:29:15 +0200

    feat(JWT): Switch the access_token to a JWT
    By using a JWT that the receiver can verify against a public key, we
    make it easy for third party services to validate the token
    independently. This is relevant for example in the context of webapp
    shares that may defer to a separate service to deliver the actual
    webapp.
    
    NOTE: This requires https://github.com/nextcloud/3rdparty/pull/2413 to
    be merged before this.
    
    Signed-off-by: Micke Nordin <kano@sunet.se>
    Signed-off-by: Enrique Pérez Arnaud <enrique@cazalla.net>

--- apps/dav/lib/Controller/TokenController.php
+++ apps/dav/lib/Controller/TokenController.php
@@ -7,6 +7,7 @@
 
 namespace OCA\DAV\Controller;
 
+use Firebase\JWT\JWT;
 use OC\Authentication\Token\IProvider;
 use OC\OCM\OCMSignatoryManager;
 use OC\Security\Signature\Model\IncomingSignedRequest;
@@ -31,6 +32,8 @@ use OCP\Security\Signature\Exceptions\SignatureException;
 use OCP\Security\Signature\Exceptions\SignatureNotFoundException;
 use OCP\Security\Signature\IIncomingSignedRequest;
 use OCP\Security\Signature\ISignatureManager;
+use OCP\Security\Signature\Model\Signatory;
+use OCP\Share\IManager as IShareManager;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -50,6 +53,7 @@ class TokenController extends ApiController {
 		private readonly OCMSignatoryManager $signatoryManager,
 		private readonly IAppConfig $appConfig,
 		private readonly OcmTokenMapMapper $ocmTokenMapMapper,
+		private readonly IShareManager $shareManager,
 	) {
 		parent::__construct('dav', $request);
 	}
@@ -79,6 +83,29 @@ class TokenController extends ApiController {
 			$this->logger->warning('Invalid token request signature', ['exception' => $e]);
 			throw new IncomingRequestException('Invalid signature');
 		}
+	}
+
+	/**
+	 * @return array{0: string, 1: string} [JWS algorithm, key material accepted by firebase/php-jwt]
+	 * @throws \RuntimeException if the key cannot be parsed or its type is unsupported
+	 */
+	private function resolveJwtSigningKey(string $privateKeyPem): array {
+		$key = openssl_pkey_get_private($privateKeyPem);
+		if ($key === false) {
+			throw new \RuntimeException('Cannot parse signatory private key');
+		}
+		$details = openssl_pkey_get_details($key);
+
+		if (isset($details['rsa'])) {
+			$algorithm = $details['bits'] >= 4096 ? 'RS512' : 'RS256';
+			return [$algorithm, $privateKeyPem];
+		}
+		if (isset($details['ed25519']['priv_key'])) {
+			$secretKey = sodium_crypto_sign_secretkey(sodium_crypto_sign_seed_keypair($details['ed25519']['priv_key']));
+			return ['EdDSA', base64_encode($secretKey)];
+		}
+
+		throw new \RuntimeException('Unsupported signatory key type for JWT access token');
 	}
 
 	/**
@@ -139,13 +166,42 @@ class TokenController extends ApiController {
 				);
 			}
 
-			$accessTokenString = $this->random->generate(
-				64,
-				ISecureRandom::CHAR_UPPER . ISecureRandom::CHAR_LOWER . ISecureRandom::CHAR_DIGITS
-			);
+			// Revoke the previous access token for this refresh token, if any.
+			$existingMapping = $this->ocmTokenMapMapper->findByRefreshToken($refreshToken);
+			if ($existingMapping !== null) {
+				try {
+					$this->tokenProvider->invalidateTokenById(
+						$token->getUID(),
+						$existingMapping->getAccessTokenId()
+					);
+				} catch (\Exception) {
+					// Token may already be gone; ignore.
+				}
+				$this->ocmTokenMapMapper->delete($existingMapping);
+			}
 
+			$share = $this->shareManager->getShareByToken($refreshToken);
 			$expiresIn = 3600; // 1 hour in seconds
-			$expiresAt = $this->timeFactory->getTime() + $expiresIn;
+			$issuedAt = $this->timeFactory->getTime();
+			$expiresAt = $issuedAt + $expiresIn;
+
+			$signatory = $this->signatoryManager->getLocalSignatory();
+			$keyId = $signatory->getKeyId();
+			$issuer = parse_url($keyId, PHP_URL_SCHEME) . '://' . Signatory::extractIdentityFromUri($keyId);
+
+			[$jwtAlgorithm, $jwtKey] = $this->resolveJwtSigningKey($signatory->getPrivateKey());
+
+			$payload = [
+				'iss' => $issuer,
+				'sub' => $share->getShareOwner(),
+				'aud' => $share->getSharedWith(),
+				'client_id' => (string)$token->getId(),
+				'iat' => $issuedAt,
+				'exp' => $expiresAt,
+				'jti' => $this->random->generate(16, ISecureRandom::CHAR_LOWER . ISecureRandom::CHAR_UPPER . ISecureRandom::CHAR_DIGITS),
+			];
+
+			$accessTokenString = JWT::encode($payload, $jwtKey, $jwtAlgorithm, $keyId, ['typ' => 'at+jwt']);
 
 			$accessToken = $this->tokenProvider->generateToken(
 				$accessTokenString,
