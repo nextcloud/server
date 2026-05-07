0ffb295233d71b080b5a8fb981920cd99f88af07
Joud <Joud@iastate.edu>
2026-05-07 00:27:52 -0500

    fix: correct typo 'occured' to 'occurred' in SetupCheckManager
    Signed-off-by: Joud <Joud@iastate.edu>

--- lib/private/SetupCheck/SetupCheckManager.php
+++ lib/private/SetupCheck/SetupCheckManager.php
@@ -33,7 +33,7 @@ class SetupCheckManager implements ISetupCheckManager {
 			try {
 				$setupResult = $setupCheckObject->run();
 			} catch (\Throwable $t) {
-				$setupResult = SetupResult::error("An exception occured while running the setup check:\n$t");
+				$setupResult = SetupResult::error("An exception occurred while running the setup check:\n$t");
 				$this->logger->error('Exception running check ' . get_class($setupCheckObject) . ': ' . $t->getMessage(), ['exception' => $t]);
 			}
 			$setupResult->setName($setupCheckObject->getName());
