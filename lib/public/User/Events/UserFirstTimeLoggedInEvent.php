8e638135d366091bae030da407873673ee46363a
Robin Appelman <robin@icewind.nl>
2026-05-11 16:59:29 +0200

    feat: make first login event webhook compatible
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/public/User/Events/UserFirstTimeLoggedInEvent.php
+++ lib/public/User/Events/UserFirstTimeLoggedInEvent.php
@@ -9,12 +9,13 @@ declare(strict_types=1);
 namespace OCP\User\Events;
 
 use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IWebhookCompatibleEvent;
 use OCP\IUser;
 
 /**
  * @since 28.0.0
  */
-class UserFirstTimeLoggedInEvent extends Event {
+class UserFirstTimeLoggedInEvent extends Event implements IWebhookCompatibleEvent {
 	/**
 	 * @since 28.0.0
 	 */
@@ -29,5 +30,14 @@ class UserFirstTimeLoggedInEvent extends Event {
 	 */
 	public function getUser(): IUser {
 		return $this->user;
+	}
+
+	#[\Override]
+	public function getWebhookSerializable(): array {
+		return [
+			'user_id' => $this->user->getUID(),
+			'display_name' => $this->user->getDisplayName(),
+			'email' => $this->user->getEMailAddress(),
+		];
 	}
 }
