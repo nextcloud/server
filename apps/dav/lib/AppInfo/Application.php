2f9700399cea5de2b837ec676caf26571ba2229e
Hamza <hamzamahjoubi221@gmail.com>
2026-05-24 14:54:15 +0200

    fix(caldav-delegation): send notification to delegator
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- apps/dav/lib/AppInfo/Application.php
+++ apps/dav/lib/AppInfo/Application.php
@@ -48,6 +48,7 @@ use OCA\DAV\Listener\AddMissingIndicesListener;
 use OCA\DAV\Listener\AddressbookListener;
 use OCA\DAV\Listener\BirthdayListener;
 use OCA\DAV\Listener\CalendarContactInteractionListener;
+use OCA\DAV\Listener\CalendarDelegateActionListener;
 use OCA\DAV\Listener\CalendarDeletionDefaultUpdaterListener;
 use OCA\DAV\Listener\CalendarFederationNotificationListener;
 use OCA\DAV\Listener\CalendarObjectReminderUpdaterListener;
@@ -216,6 +217,12 @@ class Application extends App implements IBootstrap {
 		$context->registerEventListener(CalendarObjectCreatedEvent::class, CalendarFederationNotificationListener::class);
 		$context->registerEventListener(CalendarObjectUpdatedEvent::class, CalendarFederationNotificationListener::class);
 		$context->registerEventListener(CalendarObjectDeletedEvent::class, CalendarFederationNotificationListener::class);
+
+		$context->registerEventListener(CalendarObjectCreatedEvent::class, CalendarDelegateActionListener::class);
+		$context->registerEventListener(CalendarObjectUpdatedEvent::class, CalendarDelegateActionListener::class);
+		$context->registerEventListener(CalendarObjectDeletedEvent::class, CalendarDelegateActionListener::class);
+		$context->registerEventListener(CalendarObjectMovedToTrashEvent::class, CalendarDelegateActionListener::class);
+		$context->registerEventListener(CalendarObjectRestoredEvent::class, CalendarDelegateActionListener::class);
 
 		$context->registerNotifierService(NotifierCalDAV::class);
 		$context->registerNotifierService(NotifierCardDAV::class);
