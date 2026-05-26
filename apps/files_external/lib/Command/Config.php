29adfe4dff1147465a17f3937708163d24fef175
Robin Appelman <robin@icewind.nl>
2026-05-26 20:41:18 +0200

    feat: allow loading external storage config values from file
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_external/lib/Command/Config.php
+++ apps/files_external/lib/Command/Config.php
@@ -15,6 +15,7 @@ use OCA\Files_External\Service\GlobalStoragesService;
 use OCP\AppFramework\Http;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Config extends Base {
@@ -40,7 +41,12 @@ class Config extends Base {
 			)->addArgument(
 				'value',
 				InputArgument::OPTIONAL,
-				'value to set the config option to, when no value is provided the existing value will be printed'
+				'value to set the config option to; when omitted, the existing value is printed; with --value-from-file, this is treated as a file pat'
+			)->addOption(
+				'value-from-file',
+				null,
+				InputOption::VALUE_NONE,
+				'treat the value argument as a file path and read the config value from that file'
 			);
 		parent::configure();
 	}
@@ -58,6 +64,14 @@ class Config extends Base {
 
 		$value = $input->getArgument('value');
 		if ($value !== null) {
+			if ($input->getOption('value-from-file')) {
+				$file = $value;
+				$value = file_get_contents($file);
+				if ($value === false) {
+					$output->writeln('<error>Failed to load value from ' . $file . '</error>');
+					return 1;
+				}
+			}
 			$this->setOption($mount, $key, $value, $output);
 		} else {
 			$this->getOption($mount, $key, $output);
