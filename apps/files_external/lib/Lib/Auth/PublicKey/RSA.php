80f4df86cd85523ef0cde814c570db92831e4bf3
Robin Appelman <robin@icewind.nl>
2026-05-26 20:39:11 +0200

    fix: clear key password after loading key
    Signed-off-by: Robin Appelman <robin@icewind.nl>
    
    # Conflicts:
    #	apps/files_external/lib/Lib/Auth/PublicKey/RSA.php
    
    # Conflicts:
    #	apps/files_external/lib/Lib/Auth/PublicKey/RSA.php
    #	apps/files_external/lib/Lib/Auth/PublicKey/RSAPrivateKey.php

--- apps/files_external/lib/Lib/Auth/PublicKey/RSA.php
+++ apps/files_external/lib/Lib/Auth/PublicKey/RSA.php
@@ -67,6 +67,7 @@ class RSA extends AuthMechanism {
 				throw new \RuntimeException('unable to load private key');
 			}
 		}
+		$auth->setPassword('');
 		$storage->setBackendOption('public_key_auth', $auth);
 	}
 
