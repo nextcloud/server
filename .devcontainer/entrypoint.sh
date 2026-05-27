6abe4ca1c981a3263341c9d84e2b23a3733e8b34
Josh <josh.t.richards@gmail.com>
2026-05-27 11:47:29 -0400

    refactor(DevContainer): modernize / document entrypoint.sh
    Documents why the DevContainer doesn't exec apache in the foreground and slightly modernizes things by making start errors more obvious.
    
    `exec` isn't used here with `apache2-foreground` because - unlike in production environments - we don't want to tie the container lifecycle to the status of Apache. 
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -3,6 +3,10 @@
 # SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
+set -e
+
 sudo service apache2 start
 
-while sleep 1000; do :; done
+# Keep the devcontainer alive independently of Apache so developers can
+# restart services without tearing down the workspace container.
+exec sleep infinity
