40989a0537a85ce029253463daac1d8d1d9928cb
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-05-27 21:12:41 +0200

    test(settings): drop tests of util not present on stable33
    Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

--- apps/settings/src/components/AuthToken.spec.ts
+++ apps/settings/src/components/AuthToken.spec.ts
@@ -27,7 +27,6 @@ import NcNoteCard from '@nextcloud/vue/components/NcNoteCard'
 import AuthToken from './AuthToken.vue'
 import AuthTokenDeleteDialog from './AuthTokenDeleteDialog.vue'
 import { TokenType, useAuthTokenStore } from '../store/authtoken.ts'
-import { detect } from '../utils/userAgentDetect.ts'
 
 function makeToken(overrides: Partial<IToken> = {}): IToken {
 	return {
@@ -193,50 +192,3 @@ describe('AuthTokenDeleteDialog wipe-pending warning', () => {
 	})
 })
 
-describe('Android Chrome detection', () => {
-	it('modern Android Chrome (no Build/ string, post-2021) should match androidChrome', () => {
-		const ua = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36'
-		expect(detect(ua)).toEqual({
-			id: 'androidChrome',
-			version: '132',
-		})
-	})
-
-	it('legacy Android Chrome (with Build/ string, pre-2021) should match androidChrome', () => {
-		const ua = 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36'
-		expect(detect(ua)).toEqual({
-			id: 'androidChrome',
-			version: '130',
-		})
-	})
-
-	it('Android Chrome on tablet (no "Mobile" in UA) should match androidChrome', () => {
-		const ua = 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-		expect(detect(ua)).toEqual({
-			id: 'androidChrome',
-			version: '131',
-		})
-	})
-})
-
-describe('Desktop Chrome regression tests', () => {
-	it('Desktop Chrome on Linux should still match chrome', () => {
-		const ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
-		expect(detect(ua)).toEqual({
-			id: 'chrome',
-			version: '132',
-			os: 'Linux',
-		})
-	})
-})
-
-describe('Desktop Firefox regression tests', () => {
-	it('Desktop Firefox on Linux should still match firefox', () => {
-		const ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0'
-		expect(detect(ua)).toEqual({
-			id: 'firefox',
-			version: '124',
-			os: 'Linux',
-		})
-	})
-})
