2933ddd31d603c30e335df7acc6f914252deeb88
Carl Schwan <carlschwan@kde.org>
2026-06-02 16:47:02 +0200

    fix: Improve detection of frankenphp worker mode
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ $handler = static function () {
 	}
 };
 
-if (function_exists('frankenphp_handle_request')) {
+if (function_exists('frankenphp_handle_request') && isset($_SERVER['FRANKENPHP_WORKER']) && $_SERVER['FRANKENPHP_WORKER'] === '1') {
 	$maxRequests = (int)($_SERVER['MAX_REQUESTS'] ?? 0);
 	for ($nbRequests = 0; !$maxRequests || $nbRequests < $maxRequests; ++$nbRequests) {
 		$keepRunning = \frankenphp_handle_request($handler);
