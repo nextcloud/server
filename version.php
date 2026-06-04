2851f466b778a7f0aa7ede77a36c81fea564cec6
Worker Builder <worker-builder@nextcloud.com>
2026-06-04 10:09:49 +0000

    build(hub): 34.0.0 RC4
    Signed-off-by: Worker Builder <worker-builder@nextcloud.com>

--- version.php
+++ version.php
@@ -11,10 +11,10 @@ declare(strict_types=1);
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch level
 // when updating major/minor version number.
 
-$OC_Version = [34, 0, 0, 9];
+$OC_Version = [34, 0, 0, 10];
 
 // The human-readable string
-$OC_VersionString = '34.0.0 RC3';
+$OC_VersionString = '34.0.0 RC4';
 
 $OC_VersionCanBeUpgradedFrom = [
 	'nextcloud' => [
