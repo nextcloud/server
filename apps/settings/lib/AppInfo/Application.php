9d00f5696591062f51d9e175e60a5aaeeb02e3a0
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-02 12:00:21 +0200

    feat(settings): add setup check for redis memcache
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- apps/settings/lib/AppInfo/Application.php
+++ apps/settings/lib/AppInfo/Application.php
@@ -49,6 +49,7 @@ use OCA\Settings\SetupChecks\JavaScriptSourceMaps;
 use OCA\Settings\SetupChecks\LegacySSEKeyFormat;
 use OCA\Settings\SetupChecks\MaintenanceWindowStart;
 use OCA\Settings\SetupChecks\MemcacheConfigured;
+use OCA\Settings\SetupChecks\MemcacheLegacy;
 use OCA\Settings\SetupChecks\MimeTypeMigrationAvailable;
 use OCA\Settings\SetupChecks\MysqlRowFormat;
 use OCA\Settings\SetupChecks\MysqlUnicodeSupport;
@@ -193,6 +194,7 @@ class Application extends App implements IBootstrap {
 		$context->registerSetupCheck(LegacySSEKeyFormat::class);
 		$context->registerSetupCheck(MaintenanceWindowStart::class);
 		$context->registerSetupCheck(MemcacheConfigured::class);
+		$context->registerSetupCheck(MemcacheLegacy::class);
 		$context->registerSetupCheck(MimeTypeMigrationAvailable::class);
 		$context->registerSetupCheck(MysqlRowFormat::class);
 		$context->registerSetupCheck(MysqlUnicodeSupport::class);
