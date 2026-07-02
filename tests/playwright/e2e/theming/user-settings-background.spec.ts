07cdbe2204a7e996de7924992e1e6d059e5cf3db
Cristian Scheid <cristianscheid@gmail.com>
2026-07-02 17:13:24 -0300

    feat: add frontend test for custom custom background selection from file picker
    Signed-off-by: Cristian Scheid <cristianscheid@gmail.com>

--- tests/playwright/e2e/theming/user-settings-background.spec.ts
+++ tests/playwright/e2e/theming/user-settings-background.spec.ts
@@ -5,6 +5,8 @@
 
 import { expect } from '@playwright/test'
 import { test } from '../../support/fixtures/random-user-session.ts'
+import { BackgroundFilePickerDialogPage } from '../../support/sections/BackgroundFilePickerDialogPage.ts'
+import { mkdir, uploadContent } from '../../support/utils/dav.ts'
 import { getBodyThemingSnapshot, pickColor } from '../../support/utils/theming.ts'
 
 test('User can configure background and plain color', async ({ page }) => {
@@ -31,4 +33,27 @@ test('User can configure background and plain color', async ({ page }) => {
 
 	await page.reload()
 	await expect.poll(async () => (await getBodyThemingSnapshot(page)).backgroundImage).toBe('none')
+})
+
+test('User can pick a custom background from their files', async ({ page, user }) => {
+	await mkdir(page.request, user, '/folder')
+
+	// this is a minimal image (1x1 red pixel), encoded as base64
+	const imageBase64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWL6z8DwHwAAAP//A3ONEwAAAAZJREFUAwAFCgIByRpMngAAAABJRU5ErkJggg=='
+	// Buffer.alloc(0) did not work when selecting image as background, using base64 image instead
+	await uploadContent(page.request, user, Buffer.from(imageBase64, 'base64'), 'image/jpeg', '/folder/image.jpg')
+
+	await page.goto('settings/user/theming')
+	await page.getByRole('heading', { name: 'Background and color' }).waitFor({ state: 'visible' })
+
+	await page.getByRole('button', { name: 'Custom background' }).click()
+
+	const filePicker = new BackgroundFilePickerDialogPage(page)
+	await filePicker.openFolder('folder')
+	await filePicker.selectFile('image.jpg')
+	await filePicker.confirm()
+
+	await expect(page.getByRole('button', { name: 'Custom background', pressed: true })).toBeVisible()
+	// backgroundImage is like this: "url(\"<nc-instance>/apps/theming/background?v=<hash>\")"
+	await expect.poll(async () => (await getBodyThemingSnapshot(page)).backgroundImage).toContain('/apps/theming/background?')
 })
