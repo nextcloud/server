b0517d372117efb6138bbe7dadc03b404db98abc
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2026-07-08 15:58:28 +0200

    feat(previews): add file signature check before opening files
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- lib/private/Preview/Illustrator.php
+++ lib/private/Preview/Illustrator.php
@@ -11,19 +11,23 @@ namespace OC\Preview;
 
 //.ai
 class Illustrator extends Bitmap {
-	/**
-	 * {@inheritDoc}
-	 */
 	#[\Override]
 	public function getMimeType(): string {
 		return '/application\/illustrator/';
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	#[\Override]
 	protected function getAllowedMimeTypes(): string {
 		return '/application\/(illustrator|pdf)/';
+	}
+
+	#[\Override]
+	protected function getMagicStrings(): array {
+		return ["\x25\x50\x44\x46"];
+	}
+
+	#[\Override]
+	protected function getImagickFormatHint(): string {
+		return 'ai';
 	}
 }
