e8d545e04c45de66748d5308a0acfc569db3b16d
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2026-07-08 15:58:28 +0200

    feat(previews): add file signature check before opening files
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- lib/private/Preview/TIFF.php
+++ lib/private/Preview/TIFF.php
@@ -12,19 +12,28 @@ namespace OC\Preview;
 
 //.tiff
 class TIFF extends Bitmap {
-	/**
-	 * {@inheritDoc}
-	 */
 	#[\Override]
 	public function getMimeType(): string {
 		return '/image\/tiff/';
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	#[\Override]
 	protected function getAllowedMimeTypes(): string {
 		return '/image\/tiff/';
+	}
+
+	#[\Override]
+	protected function getMagicStrings(): array {
+		return [
+			"II*\x00",
+			"MM\x00*",
+			"II+\x00",
+			"MM\x00+",
+		];
+	}
+
+	#[\Override]
+	protected function getImagickFormatHint(): string {
+		return 'tiff';
 	}
 }
