ffbf65b387c81d352ce7860bd665654d3f06a520
Hamza <hamzamahjoubi221@gmail.com>
2026-07-08 15:11:42 +0200

    test(dav): fix ORA-00932: inconsistent datatypes: expected - got BLOB
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- apps/dav/tests/unit/CalDAV/Reminder/BackendTest.php
+++ apps/dav/tests/unit/CalDAV/Reminder/BackendTest.php
@@ -53,7 +53,7 @@ class BackendTest extends TestCase {
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(4, $rows);
+		$this->assertCount(5, $rows);
 
 		$this->reminderBackend->cleanRemindersForEvent(1);
 
@@ -73,7 +73,7 @@ class BackendTest extends TestCase {
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(4, $rows);
+		$this->assertCount(5, $rows);
 
 		$this->reminderBackend->cleanRemindersForCalendar(1);
 
@@ -93,7 +93,7 @@ class BackendTest extends TestCase {
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(4, $rows);
+		$this->assertCount(5, $rows);
 
 		$this->reminderBackend->removeReminder((int)$rows[3]['id']);
 
@@ -103,7 +103,7 @@ class BackendTest extends TestCase {
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(3, $rows);
+		$this->assertCount(4, $rows);
 	}
 
 	public function testGetRemindersToProcess(): void {
@@ -163,37 +163,54 @@ class BackendTest extends TestCase {
 	public function testGetAllScheduledRemindersForEvent(): void {
 		$rows = $this->reminderBackend->getAllScheduledRemindersForEvent(1);
 
-		$this->assertCount(2, $rows);
+		$this->assertCount(3, $rows);
 		unset($rows[0]['id']);
 		unset($rows[1]['id']);
+		unset($rows[2]['id']);
 
-		$this->assertEquals($rows[0], [
-			'calendar_id' => 1,
-			'object_id' => 1,
-			'uid' => 'asd',
-			'is_recurring' => false,
-			'recurrence_id' => 123458,
-			'is_recurrence_exception' => false,
-			'event_hash' => 'asd123',
-			'alarm_hash' => 'asd567',
-			'type' => 'EMAIL',
-			'is_relative' => true,
-			'notification_date' => 123456,
-			'is_repeat_based' => false,
-		]);
-		$this->assertEquals($rows[1], [
-			'calendar_id' => 1,
-			'object_id' => 1,
-			'uid' => 'asd',
-			'is_recurring' => false,
-			'recurrence_id' => 123458,
-			'is_recurrence_exception' => false,
-			'event_hash' => 'asd123',
-			'alarm_hash' => 'asd567',
-			'type' => 'AUDIO',
-			'is_relative' => true,
-			'notification_date' => 123456,
-			'is_repeat_based' => false,
+		$this->assertEqualsCanonicalizing($rows, [
+			[
+				'calendar_id' => 1,
+				'object_id' => 1,
+				'uid' => 'asd',
+				'is_recurring' => false,
+				'recurrence_id' => 123458,
+				'is_recurrence_exception' => false,
+				'event_hash' => 'asd123',
+				'alarm_hash' => 'asd567',
+				'type' => 'EMAIL',
+				'is_relative' => true,
+				'notification_date' => 123456,
+				'is_repeat_based' => false,
+			],
+			[
+				'calendar_id' => 1,
+				'object_id' => 1,
+				'uid' => 'asd',
+				'is_recurring' => false,
+				'recurrence_id' => 123458,
+				'is_recurrence_exception' => false,
+				'event_hash' => 'asd123',
+				'alarm_hash' => 'asd567',
+				'type' => 'AUDIO',
+				'is_relative' => true,
+				'notification_date' => 123456,
+				'is_repeat_based' => false,
+			],
+			[
+				'calendar_id' => 1,
+				'object_id' => 1,
+				'uid' => 'asd',
+				'is_recurring' => false,
+				'recurrence_id' => 123458,
+				'is_recurrence_exception' => false,
+				'event_hash' => 'asd123',
+				'alarm_hash' => 'duplicate',
+				'type' => 'EMAIL',
+				'is_relative' => true,
+				'notification_date' => 123456,
+				'is_repeat_based' => false,
+			],
 		]);
 	}
 
@@ -204,7 +221,7 @@ class BackendTest extends TestCase {
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(4, $rows);
+		$this->assertCount(5, $rows);
 
 		$this->reminderBackend->insertReminder(42, 1337, 'uid99', true, 12345678,
 			true, 'hash99', 'hash42', 'AUDIO', false, 12345670, false);
@@ -212,14 +229,15 @@ class BackendTest extends TestCase {
 		$query = Server::get(IDBConnection::class)->getQueryBuilder();
 		$rows = $query->select('*')
 			->from('calendar_reminders')
+			->orderBy('id')
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(5, $rows);
+		$this->assertCount(6, $rows);
 
-		unset($rows[4]['id']);
+		unset($rows[5]['id']);
 
-		$this->assertEquals($rows[4], [
+		$this->assertEquals($rows[5], [
 			'calendar_id' => '42',
 			'object_id' => '1337',
 			'is_recurring' => '1',
@@ -239,10 +257,11 @@ class BackendTest extends TestCase {
 		$query = Server::get(IDBConnection::class)->getQueryBuilder();
 		$rows = $query->select('*')
 			->from('calendar_reminders')
+			->orderBy('id')
 			->executeQuery()
 			->fetchAllAssociative();
 
-		$this->assertCount(4, $rows);
+		$this->assertCount(5, $rows);
 
 		$this->assertEquals($rows[3]['notification_date'], 123600);
 
@@ -384,6 +403,25 @@ class BackendTest extends TestCase {
 				'type' => $query->createNamedParameter('DISPLAY'),
 				'is_relative' => $query->createNamedParameter(1),
 				'notification_date' => $query->createNamedParameter(123600),
+				'is_repeat_based' => $query->createNamedParameter(0),
+			])
+			->executeStatement();
+
+		// add a duplicate to test getRemindersToProcess dedup
+		$query = Server::get(IDBConnection::class)->getQueryBuilder();
+		$query->insert('calendar_reminders')
+			->values([
+				'calendar_id' => $query->createNamedParameter(1),
+				'object_id' => $query->createNamedParameter(1),
+				'uid' => $query->createNamedParameter('asd'),
+				'is_recurring' => $query->createNamedParameter(0),
+				'recurrence_id' => $query->createNamedParameter(123458),
+				'is_recurrence_exception' => $query->createNamedParameter(0),
+				'event_hash' => $query->createNamedParameter('asd123'),
+				'alarm_hash' => $query->createNamedParameter('duplicate'),
+				'type' => $query->createNamedParameter('EMAIL'),
+				'is_relative' => $query->createNamedParameter(1),
+				'notification_date' => $query->createNamedParameter(123456),
 				'is_repeat_based' => $query->createNamedParameter(0),
 			])
 			->executeStatement();
