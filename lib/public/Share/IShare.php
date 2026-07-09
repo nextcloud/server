0f6df8ecc1ef5637cff17df8292fd2d38c2e7047
Tobias Knöppler <tobias@knoeppler.org>
2026-07-09 10:59:56 +0200

    feat(core): Implement interfaces for and management of one-time passwords
    Signed-off-by: Tobias Knöppler <tobias@knoeppler.org>

--- lib/public/Share/IShare.php
+++ lib/public/Share/IShare.php
@@ -14,6 +14,7 @@ use OCP\Files\File;
 use OCP\Files\Folder;
 use OCP\Files\Node;
 use OCP\Files\NotFoundException;
+use OCP\OneTimePassword\IOneTimePassword;
 use OCP\Share\Exceptions\IllegalIDChangeException;
 
 /**
@@ -467,6 +468,21 @@ interface IShare {
 	 * @since 9.0.0
 	 */
 	public function getPassword();
+
+	/**
+	 * Get the associated one-time password (if set)
+	 * @return ?IOneTimePassword The one-time password
+	 * @since 35.0.0
+	 */
+	public function getOneTimePassword(): ?IOneTimePassword;
+
+	/**
+	 * Set the associated one-time password
+	 * @param ?IOneTimePassword $otp
+	 * @return self
+	 * @since 35.0.0
+	 */
+	public function setOneTimePassword(?IOneTimePassword $otp): self;
 
 	/**
 	 * Returns whether the share is password protected by any means (e.g. password or OTP)
