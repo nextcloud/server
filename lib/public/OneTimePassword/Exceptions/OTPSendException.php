78614267cf7b8546b3dcd5166e753a38431850bd
Tobias Knöppler <tobias@knoeppler.org>
2026-07-09 10:59:56 +0200

    feat(core): Implement interfaces for and management of one-time passwords

---
+++ lib/public/OneTimePassword/Exceptions/OTPSendException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace OCP\OneTimePassword\Exceptions;
+
+/**
+ * @since 35.0.0
+ */
+class OTPSendException extends \Exception {
+}
