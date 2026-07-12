b615188200af646b7b02d9863d5bb89bb0109159
David Dreschner <david.dreschner@nextcloud.com>
2026-07-12 23:18:49 +0200

    chore(playwright): Try to analyze failures
    Signed-off-by: David Dreschner <david.dreschner@nextcloud.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
 	retries: process.env.CI ? 1 : 0,
 	workers: process.env.CI ? 1 : undefined,
 	reporter: process.env.CI ? [['blob'], ['dot'], ['github']] : 'html',
+	expect: {
+		timeout: 10_000,
+	},
 	use: {
 		baseURL: 'http://localhost:8042/index.php/',
 		trace: 'on-first-retry',
