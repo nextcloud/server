43438c1f7847ba9d4b19da00ea594faa06798508
Carl Schwan <carlschwan@kde.org>
2026-07-15 15:30:23 +0200

    fix: Adjust API @since version
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- apps/files_trashbin/lib/Trash/ITrashManager.php
+++ apps/files_trashbin/lib/Trash/ITrashManager.php
@@ -32,7 +32,7 @@ interface ITrashManager extends ITrashBackend {
 	 *
 	 * @param IUser $user
 	 * @return ?ITrashItem
-	 * @since 35.0.0
+	 * @since 32.0.13
 	 */
 	public function getTrashRootItem(IUser $user, string $name): ?ITrashItem;
 
