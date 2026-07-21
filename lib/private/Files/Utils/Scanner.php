39907a4259fcb90cb0c1e152a6ac6185476d6bac
Nick Manning <75833+seenickcode@users.noreply.github.com>
2026-07-21 15:43:13 +0200

    Update lib/private/Files/Utils/Scanner.php
    SGTM
    
    Co-authored-by: Côme Chilliet <91878298+come-nc@users.noreply.github.com>
    Signed-off-by: Nick Manning <75833+seenickcode@users.noreply.github.com>

--- lib/private/Files/Utils/Scanner.php
+++ lib/private/Files/Utils/Scanner.php
@@ -208,7 +208,7 @@ class Scanner extends PublicEmitter {
 			});
 			$scanner->listen('\OC\Files\Cache\Scanner', 'addToCache', function ($path, $storageId, $data, $fileId) use ($storage): void {
 				$this->triggerPropagator($storage, $path);
-				if ($fileId !== -1) {
+				if ($fileId > 0) {
 					$this->eventDispatcher->dispatchTyped(new FileCacheUpdated($storage, $path));
 				} else {
 					$this->eventDispatcher->dispatchTyped(new NodeAddedToCache($storage, $path));
