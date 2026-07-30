4f1c694af40f4d6699b28753469cb84f7054635e
Carl Schwan <carl@carlschwan.eu>
2026-07-30 12:38:11 +0200

    feat: Allow to register a class implementing IGlobalScaleService
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/private/ServiceUnavailableException.php
+++ lib/private/ServiceUnavailableException.php
@@ -9,5 +9,7 @@ declare(strict_types=1);
  */
 namespace OC;
 
-class ServiceUnavailableException extends \Exception {
+use Psr\Container\ContainerExceptionInterface;
+
+class ServiceUnavailableException extends \Exception implements ContainerExceptionInterface {
 }
