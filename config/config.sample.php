51b836e4386e1f095293cbb5d00d9d2f72c39fd3
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-03 00:46:44 +0000

    chore: Update minimum supported desktop version
    Signed-off-by: GitHub <noreply@github.com>

--- config/config.sample.php
+++ config/config.sample.php
@@ -2511,9 +2511,9 @@ $CONFIG = [
 	 * Changing this may cause older, unsupported clients to malfunction, potentially
 	 * leading to data loss or unexpected behavior.
 	 *
-	 * Defaults to ``3.2.82``
+	 * Defaults to ``3.3.50``
 	 */
-	'minimum.supported.desktop.version' => '3.2.82',
+	'minimum.supported.desktop.version' => '3.3.50',
 
 	/**
 	 * Specify the maximum Nextcloud desktop client version allowed to sync with this
