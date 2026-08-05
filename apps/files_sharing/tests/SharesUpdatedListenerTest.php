mamh-mixed/nextcloud-server

fix(files_sharing): skip unresolvable share recipients
Assisted-by: ClaudeCode:claude-opus-4-8
Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

apps/files_sharing/tests/SharesUpdatedListenerTest.php
@@ -19,6 +19,7 @@ use OCP\Share\Events\BeforeShareDeletedEvent;
 use OCP\Share\Events\ShareCreatedEvent;
 use OCP\Share\IManager;
 use OCP\Share\IShare;
+use OCP\User\Exceptions\UserNotFoundException;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Clock\ClockInterface;
@@ -114,6 +115,33 @@ class SharesUpdatedListenerTest extends \Test\TestCase {
 				$this->assertEquals($share, $eventShare);
 			});
 
+		$this->sharesUpdatedListener->handle($event);
+	}
+
+	public function testShareAddedSkipsUnresolvableUser(): void {
+		$share = $this->createMock(IShare::class);
+		$user1 = $this->createUser('user1', '');
+		$user2 = $this->createUser('user2', '');
+
+		$this->manager->method('getUsersForShare')
+			->willReturn([$user1, $user2]);
+
+		$event = new ShareCreatedEvent($share);
+
+		// user1 is an orphaned recipient that no backend can resolve
+		$this->shareRecipientUpdater
+			->expects($this->exactly(2))
+			->method('updateForAddedShare')
+			->willReturnCallback(function (IUser $user) use ($user1): void {
+				if ($user === $user1) {
+					throw new UserNotFoundException('Backends provided no user object');
+				}
+			});
+
+		// the failure is logged, not thrown
+		$this->logger->expects($this->once())->method('warning');
+
+		// must not throw: user2 is still processed
 		$this->sharesUpdatedListener->handle($event);
 	}
 

nextcloud/server

apps/files_sharing/tests/SharesUpdatedListenerTest.php
@@ -7,6 +7,7 @@
 
 namespace OCA\Files_Sharing\Tests;
 
+use OC\User\NoUserException;
 use OCA\Files_Sharing\Config\ConfigLexicon;
 use OCA\Files_Sharing\Event\UserShareAccessUpdatedEvent;
 use OCA\Files_Sharing\Listener\SharesUpdatedListener;
@@ -114,6 +115,33 @@ class SharesUpdatedListenerTest extends \Test\TestCase {
 				$this->assertEquals($share, $eventShare);
 			});
 
+		$this->sharesUpdatedListener->handle($event);
+	}
+
+	public function testShareAddedSkipsUnresolvableUser(): void {
+		$share = $this->createMock(IShare::class);
+		$user1 = $this->createUser('user1', '');
+		$user2 = $this->createUser('user2', '');
+
+		$this->manager->method('getUsersForShare')
+			->willReturn([$user1, $user2]);
+
+		$event = new ShareCreatedEvent($share);
+
+		// user1 is an orphaned recipient that no backend can resolve
+		$this->shareRecipientUpdater
+			->expects($this->exactly(2))
+			->method('updateForAddedShare')
+			->willReturnCallback(function (IUser $user) use ($user1): void {
+				if ($user === $user1) {
+					throw new NoUserException('Backends provided no user object');
+				}
+			});
+
+		// the failure is logged, not thrown
+		$this->logger->expects($this->once())->method('debug');
+
+		// must not throw: user2 is still processed
 		$this->sharesUpdatedListener->handle($event);
 	}
 