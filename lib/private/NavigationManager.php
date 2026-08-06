314cccb7f628907f1770b716b0614ebf0f99f38a
Ferdinand Thiessen <opensource@fthiessen.de>
2026-08-06 22:37:31 +0200

    fix(NavigationManager): properly register closures
    Lately there were quite some issues with the NavigationManager,
    so this tries to clarifies the implementation a bit.
    But more important this fixes an issue when closures are
    registered after the setup was already done (see added unit test).
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/NavigationManager.php
+++ lib/private/NavigationManager.php
@@ -28,15 +28,20 @@ use Psr\Log\LoggerInterface;
  * @psalm-import-type NavigationEntryOutput from INavigationManager
  */
 class NavigationManager implements INavigationManager {
+	protected ?string $activeEntry = null;
 	/** @var array<string, NavigationEntryOutput> */
 	protected array $entries = [];
 	/** @var list<callable(): NavigationEntry> */
 	protected array $closureEntries = [];
-	protected ?string $activeEntry = null;
-	protected array $unreadCounters = [];
-	protected bool $init = false;
 	/** User defined app order (cached for the `add` function) */
-	private ?array $customAppOrder = null;
+	protected ?array $customAppOrder = null;
+	/** @var array<string, int> */
+	protected array $unreadCounters = [];
+
+	/** true if the internal state has been initialized */
+	protected bool $initAppOrderDone = false;
+	/** true if all apps have been loaded by the App Manager */
+	protected bool $initSetupDone = false;
 	/** List of loaded app info */
 	private array $loadedAppInfo = [];
 
@@ -58,7 +63,8 @@ class NavigationManager implements INavigationManager {
 			$this->closureEntries[] = $entry;
 			return;
 		}
-		$this->init();
+		// if needed initialize the internal state to allow setting app order and default app
+		$this->initCustomAppOrder();
 
 		$id = $entry['id'];
 
@@ -172,7 +178,7 @@ class NavigationManager implements INavigationManager {
 
 		if ($resetInit) {
 			$this->loadedAppInfo = [];
-			$this->init = false;
+			$this->initAppOrderDone = false;
 		}
 	}
 
@@ -190,11 +196,11 @@ class NavigationManager implements INavigationManager {
 	 * Initialize the internal state.
 	 * This loads the default app mapping and user mapping for app ordering.
 	 */
-	private function init(): void {
-		if ($this->init) {
+	private function initCustomAppOrder(): void {
+		if ($this->initAppOrderDone) {
 			return;
 		}
-		$this->init = true;
+		$this->initAppOrderDone = true;
 
 		if ($this->customAppOrder === null) {
 			if ($this->userSession->isLoggedIn()) {
@@ -208,20 +214,23 @@ class NavigationManager implements INavigationManager {
 
 	/**
 	 * Setup the navigation manager.
+	 *
 	 * @internal - This is only used by Nextcloud core to setup the navigation manager. It is not intended for use by apps.
 	 */
 	public function setup(): void {
-		// Resolve app navigation closures
-		while ($c = array_pop($this->closureEntries)) {
-			$this->add($c());
-		}
-
 		// Resolve dynamically added navigation entries via event listeners
 		$this->eventDispatcher->dispatchTyped(new LoadAdditionalEntriesEvent());
+
+		// mark setup as done to allow performance optimizations
+		$this->initSetupDone = true;
 	}
 
 	/**
-	 * Resolve classic info.xml based navigation entires.
+	 * Resolve app navigation entries.
+	 *
+	 * This is called every time by any getter as some code for legacy reasons relies
+	 * on the navigation entries being available before the app loading is finished.
+	 * So once all apps are loaded we 
 	 * Some code relies on this to be available earlier then the app loading finished.
 	 * So we need to resolve the navigation entries here, even if not all apps are loaded yet.
 	 */
@@ -311,6 +320,19 @@ class NavigationManager implements INavigationManager {
 					'app' => $app,
 				] : []
 				));
+			}
+		}
+
+		// once all apps are loaded we can resolve the app navigation closures
+		if ($this->initSetupDone) {
+			// This has to be done on every call,
+			// as apps might add new navigation entries via closures at any time
+			while ($c = array_pop($this->closureEntries)) {
+				try {
+					$this->add($c());
+				} catch (\Throwable $e) {
+					$this->logger->error('Failed to add navigation entry from closure', ['exception' => $e]);
+				}
 			}
 		}
 	}
