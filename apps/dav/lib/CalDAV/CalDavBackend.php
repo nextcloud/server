424ee48496f8cb04eb6a81274ac0d9c8993cb5da
David Dreschner <david.dreschner@nextcloud.com>
2026-08-07 13:18:25 +0200

    feat(federation): Add accept/decline mechanism for federated calendars
    Signed-off-by: David Dreschner <david.dreschner@nextcloud.com>

--- apps/dav/lib/CalDAV/CalDavBackend.php
+++ apps/dav/lib/CalDAV/CalDavBackend.php
@@ -4083,7 +4083,10 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 	 * @return array<string, mixed>[]
 	 */
 	public function getFederatedCalendarsForUser(string $principalUri): array {
-		$federatedCalendars = $this->federatedCalendarMapper->findByPrincipalUri($principalUri);
+		$federatedCalendars = $this->federatedCalendarMapper->findByPrincipalUri(
+			$principalUri,
+			FederatedCalendarEntity::STATE_ACCEPTED,
+		);
 		return array_map(
 			static fn (FederatedCalendarEntity $entity) => $entity->toCalendarInfo(),
 			$federatedCalendars,
@@ -4092,6 +4095,10 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 
 	public function getFederatedCalendarByUri(string $principalUri, string $uri): ?array {
 		$federatedCalendar = $this->federatedCalendarMapper->findByUri($principalUri, $uri);
-		return $federatedCalendar?->toCalendarInfo();
+		if ($federatedCalendar === null
+			|| $federatedCalendar->getState() !== FederatedCalendarEntity::STATE_ACCEPTED) {
+			return null;
+		}
+		return $federatedCalendar->toCalendarInfo();
 	}
 }
