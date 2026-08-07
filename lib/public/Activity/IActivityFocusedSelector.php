ee180898133b1d4ae2b8436872d8121ae8ceac56
Côme Chilliet <come.chilliet@nextcloud.com>
2026-08-07 16:32:54 +0200

    fixup! feat(dav): Add an activity focused selector for calendar events
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/public/Activity/IActivityFocusedSelector.php
+++ lib/public/Activity/IActivityFocusedSelector.php
@@ -10,7 +10,9 @@ declare(strict_types=1);
 namespace OCP\Activity;
 
 use OCP\AppFramework\Attribute\Implementable;
+use OCP\DB\QueryBuilder\ICompositeExpression;
 use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\DB\QueryBuilder\IQueryFunction;
 
 /**
  * @since 35.0.0
@@ -21,7 +23,7 @@ interface IActivityFocusedSelector {
 	 * Extend query with joins if needed, and return an array of conditions to add
 	 * in the OR clause of the WHERE for filtering focused activity events
 	 *
-	 * @return list<string> conditions to add in the where clause
+	 * @return list<string|ICompositeExpression|IQueryFunction> conditions to add in the where clause
 	 * @since 35.0.0
 	 */
 	public function extendQuery(IQueryBuilder $query, string $userId): array;
