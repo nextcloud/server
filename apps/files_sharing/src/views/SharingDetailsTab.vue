4890d0961e8ecabb48e157c34c1f6008fe87f0ca
Daniel Calviño Sánchez <danxuliu@gmail.com>
2026-08-12 12:59:52 +0200

    fix: Fix disabling password when video verification is enabled
    If video verification is enabled it is not possible to save a share
    without a password. This could happen if the password is removed without
    unchecking first the video verification. Therefore, if the password is
    disabled now the video verification is also disabled when the share is
    saved.
    
    An alternative would be to disable the video verification in the share
    object as soon as the password is disabled, even if it was not saved
    yet. However the video verification was not disabled until the share is
    saved to ensure that, for example, a misclick on the password does not
    change the video verification state and forces the user to explicitly
    enable it again.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/files_sharing/src/views/SharingDetailsTab.vue
+++ apps/files_sharing/src/views/SharingDetailsTab.vue
@@ -982,7 +982,7 @@ export default {
 		},
 		async saveShare() {
 			const permissionsAndAttributes = ['permissions', 'attributes', 'note', 'expireDate']
-			const publicShareAttributes = ['label', 'hideDownload']
+			const publicShareAttributes = ['label', 'hideDownload', 'sendPasswordByTalk']
 			// Only include password if it's being actively changed
 			if (this.hasUnsavedPassword) {
 				publicShareAttributes.push('password')
@@ -1014,6 +1014,13 @@ export default {
 				}
 			} else {
 				this.share.password = ''
+			}
+
+			// "Video verification" must be disabled if the password was
+			// disabled, as it does not make sense and would also prevent
+			// saving if it is still enabled.
+			if (this.isPasswordProtectedByTalk && !this.isPasswordProtected) {
+				this.isPasswordProtectedByTalk = false
 			}
 
 			if (!this.hasExpirationDate) {
