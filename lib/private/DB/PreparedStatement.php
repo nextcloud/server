nextcloud/server

commit fcc63b9603596c1bc630998a792487ec1bbd2a51

refactor(dbal): move to modern calls
Signed-off-by: Git'Fellow <12234510+solracsf@users.noreply.github.com>
Signed-off-by: Carl Schwan <carlschwan@kde.org>

lib/private/DB/PreparedStatement.php
@@ -45,12 +45,22 @@ class PreparedStatement implements IPreparedStatement {
 
 	#[\Override]
 	public function fetch(int $fetchMode = PDO::FETCH_ASSOC) {
-		return $this->getResult()->fetch($fetchMode);
+		return match ($fetchMode) {
+			PDO::FETCH_ASSOC => $this->getResult()->fetchAssociative(),
+			PDO::FETCH_NUM => $this->getResult()->fetchNumeric(),
+			PDO::FETCH_COLUMN => $this->getResult()->fetchOne(),
+			default => $this->getResult()->fetch($fetchMode),
+		};
 	}
 
 	#[\Override]
 	public function fetchAll(int $fetchMode = PDO::FETCH_ASSOC): array {
-		return $this->getResult()->fetchAll($fetchMode);
+		return match ($fetchMode) {
+			PDO::FETCH_ASSOC => $this->getResult()->fetchAllAssociative(),
+			PDO::FETCH_NUM => $this->getResult()->fetchAllNumeric(),
+			PDO::FETCH_COLUMN => $this->getResult()->fetchFirstColumn(),
+			default => $this->getResult()->fetchAll($fetchMode),
+		};
 	}
 
 	#[\Override]
@@ -75,7 +85,7 @@ class PreparedStatement implements IPreparedStatement {
 
 	#[\Override]
 	public function execute($params = null): IResult {
-		return ($this->result = new ResultAdapter($this->statement->execute($params)));
+		return ($this->result = new ResultAdapter($this->statement->execute($params ?? [])));
 	}
 
 	#[\Override]

commit 217784bbff282d913d1773a1633a3c15b14657c2

fix(dbal): Regression on PreparedStatement caused by fcc63b9603596c1b
This small change included in https://github.com/nextcloud/server/pull/58891
broke PreparedStatement::bindValue and PreparedStatement::bindParam. The
empty array has different meaning on $params than the null value, purging all
the parameters previously passed to Statement.

The problem commit is included in v35.0.0beta1 and v35.0.0beta2 but not in
any v34 versions.

Signed-off-by: Pauli Järvinen <pauli.jarvinen@gmail.com>

lib/private/DB/PreparedStatement.php
@@ -85,7 +85,7 @@ class PreparedStatement implements IPreparedStatement {
 
 	#[\Override]
 	public function execute($params = null): IResult {
-		return ($this->result = new ResultAdapter($this->statement->execute($params ?? [])));
+		return ($this->result = new ResultAdapter($this->statement->execute($params)));
 	}
 
 	#[\Override]