8d95c90dadbc80400669d8ae9844adcf4f6f37db
Côme Chilliet <come.chilliet@nextcloud.com>
2026-08-18 16:47:58 +0200

    fix(tests): Add missing namespaces in sharing tests
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- apps/sharing/tests/CapabilitiesTest.php
+++ apps/sharing/tests/CapabilitiesTest.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-declare(strict_types=1);
+namespace OCA\Sharing\Tests;
 
 use NCU\Sharing\ISharingRegistry;
 use OCA\Sharing\AppInfo\Application;
