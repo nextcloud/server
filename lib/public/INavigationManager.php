2f75162883d8bd0e301a4d1c25b799b798f16647
provokateurin <kate@provokateurin.de>
2026-08-24 14:03:24 +0200

    fix(INavigationManager): Add missing type values
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/public/INavigationManager.php
+++ lib/public/INavigationManager.php
@@ -27,7 +27,7 @@ use OCP\AppFramework\Attribute\ExceptionalImplementable;
  *     icon?: string,
  *     color?: string,
  *     classes?: string,
- *     type?: 'link'|'action'|'settings',
+ *     type?: 'link'|'action'|'settings'|'guest'|'quota',
  * }
  * @psalm-type NavigationEntryOutput = array{
  *     id: string,
@@ -35,7 +35,7 @@ use OCP\AppFramework\Attribute\ExceptionalImplementable;
  *     href: string,
  *     icon: string,
  *     color?: string,
- *     type: 'link'|'action'|'settings',
+ *     type: 'link'|'action'|'settings'|'guest'|'quota',
  *     name: string,
  *     app?: string,
  *     default?: bool,
