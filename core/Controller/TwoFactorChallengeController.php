fc2066fe048b3e8fd48cbf274d586b653f1337e3
Côme Chilliet <come.chilliet@nextcloud.com>
2026-08-24 10:14:45 +0200

    fix(2fa): Add missing BruteForceProtection attribute
    fix(2fa): Add missing BruteForceProtection attribute
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>
    
    [skip ci]

--- core/Controller/TwoFactorChallengeController.php
+++ core/Controller/TwoFactorChallengeController.php
@@ -198,10 +198,12 @@ class TwoFactorChallengeController extends Controller {
 		$uid = $user->getUID();
 		$this->logger->warning("Two-factor challenge failed: $uid (Remote IP: $ip)");
 		$this->session->set('two_factor_auth_error', true);
-		return new RedirectResponse($this->urlGenerator->linkToRoute('core.TwoFactorChallenge.showChallenge', [
+		$response = new RedirectResponse($this->urlGenerator->linkToRoute('core.TwoFactorChallenge.showChallenge', [
 			'challengeProviderId' => $provider->getId(),
 			'redirect_url' => $redirect_url,
 		]));
+		$response->throttle(['user' => $uid, 'provider' => $challengeProviderId]);
+		return $response;
 	}
 
 	/**
