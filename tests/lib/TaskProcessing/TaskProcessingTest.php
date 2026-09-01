84fdfcca5ef9bc11326a057e2db20f1e97e811a0
Marcel Klehr <mklehr@gmx.net>
2026-09-01 09:36:53 +0200

    fix: Add IRemoteHostValidator to TaskProcessingTest dependencies
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- tests/lib/TaskProcessing/TaskProcessingTest.php
+++ tests/lib/TaskProcessing/TaskProcessingTest.php
@@ -739,6 +739,7 @@ class TaskProcessingTest extends \Test\TestCase {
 			Server::get(IUserSession::class),
 			Server::get(ICacheFactory::class),
 			Server::get(IFactory::class),
+			Server::get(IRemoteHostValidator::class),
 		);
 	}
 
