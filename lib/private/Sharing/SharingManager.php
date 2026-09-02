c6fd60fa48d13b2128c4879e7129f279b186b2ac
provokateurin <kate@provokateurin.de>
2026-09-02 14:10:16 +0200

    chore(Sharing): Remove ISharingLegacyBackend
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/Sharing/SharingManager.php
+++ lib/private/Sharing/SharingManager.php
@@ -189,14 +189,6 @@ final readonly class SharingManager implements ISharingManager, IEventListener {
 		// No need to update the last updated timestamp, because the share will be deleted anyway.
 
 		$ids = $this->backend->onOwnerDeleted($owner);
-
-		$legacyBackend = $this->registry->getLegacyBackend();
-		if ($legacyBackend instanceof ISharingLegacyBackend) {
-			foreach ($ids as $id) {
-				$legacyBackend->deleteShare($id);
-			}
-		}
-
 		if ($ids !== []) {
 			$this->eventDispatcher->dispatchTyped(new SharesDeletedEvent($ids));
 		}
@@ -755,11 +747,6 @@ final readonly class SharingManager implements ISharingManager, IEventListener {
 
 		$this->backend->deleteShare($share->id);
 
-		$legacyBackend = $this->registry->getLegacyBackend();
-		if ($legacyBackend instanceof ISharingLegacyBackend) {
-			$legacyBackend->deleteShare($share->id);
-		}
-
 		$this->eventDispatcher->dispatchTyped(new SharesDeletedEvent([$share->id]));
 	}
 
@@ -978,27 +965,6 @@ final readonly class SharingManager implements ISharingManager, IEventListener {
 						$share->permissions,
 					);
 				}
-			}
-
-			$legacyBackend = $this->registry->getLegacyBackend();
-			if ($legacyBackend instanceof ISharingLegacyBackend) {
-				$compatibleSourceTypes = array_fill_keys($legacyBackend->getCompatibleSourceTypes(), true);
-				foreach ($share->sources as $source) {
-					if (!isset($compatibleSourceTypes[$source->class])) {
-						throw new RuntimeException('The legacy backend ' . $legacyBackend::class . ' does not support this source type: ' . $source->class);
-					}
-				}
-
-				$compatibleRecipientTypes = array_fill_keys($legacyBackend->getCompatibleRecipientTypes(), true);
-				foreach ($share->recipients as $recipient) {
-					if (!isset($compatibleRecipientTypes[$recipient->class])) {
-						throw new RuntimeException(
-							'The legacy backend ' . $legacyBackend::class . ' does not support this recipient type: ' . $recipient->class
-						);
-					}
-				}
-
-				$legacyBackend->updateShare($share);
 			}
 		}
 
