3f3bfbb4602465298d1dec5f4323fb4c12e9b673
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-09-07 10:22:45 +0200

    fix(files_external): show that an unrestricted storage applies to all accounts
    Backport of #64103. The cherry-pick dropped appliesToAllAccounts because
    stable33 has no pruneUnusedAuthMechanismOptions to merge the import with,
    so the dialog called a function that was never added.
    
    Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

--- dist/theming-theming.mjs
+++ dist/theming-theming.mjs
@@ -1,2 +1,2 @@
-import{l as o}from"./index-DidlHano.chunk.mjs";window.OCA.Theming=o("theming","data");
+import{l as o}from"./index-C3KLQ_oe.chunk.mjs";window.OCA.Theming=o("theming","data");
 //# sourceMappingURL=theming-theming.mjs.map
