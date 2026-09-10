9ff5441bbf5b0cc98eaa41562ff3de92095b77a2
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-10 07:36:43 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ dist/NcActionCaption-BS8aNXZ_.chunk.mjs
@@ -0,0 +1,2 @@
+import{N as t}from"./NcModal-CApXrsyV.chunk.mjs";import{_ as e}from"./Web-CPFZEoRo.chunk.mjs";import{o as a,g as i,t as r}from"./preload-helper-Dc0eId0q.chunk.mjs";const s={name:"NcActionCaption",inject:{isInSemanticMenu:{from:t,default:!1}},props:{name:{type:String,required:!0}}},c=["role"];function p(m,u,n,d,f,o){return a(),i("li",{class:"app-navigation-caption",role:o.isInSemanticMenu&&"presentation"},r(n.name),9,c)}const g=e(s,[["render",p],["__scopeId","data-v-1009e96c"]]);export{g as N};
+//# sourceMappingURL=NcActionCaption-BS8aNXZ_.chunk.mjs.map
--- dist/NcActionCaption-CcuAMRVI.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{N as t}from"./NcModal-CJHIiy3v.chunk.mjs";import{_ as e}from"./Web-Dl1PlCyW.chunk.mjs";import{o as a,g as i,t as r}from"./preload-helper-Dc0eId0q.chunk.mjs";const s={name:"NcActionCaption",inject:{isInSemanticMenu:{from:t,default:!1}},props:{name:{type:String,required:!0}}},c=["role"];function p(m,u,n,d,f,o){return a(),i("li",{class:"app-navigation-caption",role:o.isInSemanticMenu&&"presentation"},r(n.name),9,c)}const g=e(s,[["render",p],["__scopeId","data-v-1009e96c"]]);export{g as N};
-//# sourceMappingURL=NcActionCaption-CcuAMRVI.chunk.mjs.map
