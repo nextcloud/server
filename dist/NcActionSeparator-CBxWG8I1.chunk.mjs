41e17b958623adf859094aad8e3e6ac697536b72
skjnldsv <skjnldsv@protonmail.com>
2026-09-10 10:16:38 +0200

    chore(assets): recompile assets
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- dist/NcActionSeparator-CBxWG8I1.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./Web-CPFZEoRo.chunk.mjs";import{o as a,g as t}from"./preload-helper-Dc0eId0q.chunk.mjs";const r={name:"NcActionSeparator"},e={class:"action action-separator action--disabled",role:"separator"};function n(s,c,i,p,d,l){return a(),t("li",e)}const u=o(r,[["render",n],["__scopeId","data-v-3e2324b7"]]);export{u as N};
-//# sourceMappingURL=NcActionSeparator-CBxWG8I1.chunk.mjs.map
---
+++ dist/NcActionSeparator-CXD3c4ed.chunk.mjs
@@ -0,0 +1,2 @@
+import{_ as o}from"./Web-DAo7hh-V.chunk.mjs";import{o as a,g as t}from"./preload-helper-DaivvT3M.chunk.mjs";const r={name:"NcActionSeparator"},e={class:"action action-separator action--disabled",role:"separator"};function n(s,c,i,p,d,l){return a(),t("li",e)}const u=o(r,[["render",n],["__scopeId","data-v-3e2324b7"]]);export{u as N};
+//# sourceMappingURL=NcActionSeparator-CXD3c4ed.chunk.mjs.map
