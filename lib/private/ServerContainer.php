41d9aa85c80debe2c4e2e0e20ff2dc01a92fddd7
Côme Chilliet <come.chilliet@nextcloud.com>
2026-09-10 14:21:53 +0200

    fix: Avoid using an exception for a normal fallback situation
    Building an exception object can be expensive, avoid building one only
     to fallback to server DI from applications.
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- lib/private/ServerContainer.php
+++ lib/private/ServerContainer.php
@@ -127,7 +127,12 @@ class ServerContainer extends SimpleContainer {
 		if (str_starts_with($name, 'OCA\\')) {
 			// In case the service starts with OCA\ we try to find the service in the apps container.
 			if (($appContainer = $this->getAppContainerForService($name)) !== null) {
-				return $appContainer->queryNoFallback($name, $chain);
+				$result = $appContainer->queryNoFallback($name, $chain);
+				if ($result !== null) {
+					return $result;
+				}
+				throw new QueryException('Could not resolve ' . $name . '!'
+					. ' Class can not be instantiated', 1);
 			}
 		}
 
