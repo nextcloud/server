14939c55e3a546e58f03dad6666adad583d56f43
kristian-zendato <kristian.zendato@nextcloud.com>
2026-09-14 11:52:04 +0000

    fix(sharing): Separate max and default expiration dates in link share UI
    Signed-off-by: kristian-zendato <kristian.zendato@nextcloud.com>

--- apps/files_sharing/lib/Capabilities.php
+++ apps/files_sharing/lib/Capabilities.php
@@ -47,6 +47,7 @@ class Capabilities implements ICapability {
 	 *             expire_date?: array{
 	 *                 enabled: bool,
 	 *                 days?: int,
+	 *                 default_days?: int,
 	 *                 enforced?: bool,
 	 *             },
 	 *             expire_date_internal?: array{
