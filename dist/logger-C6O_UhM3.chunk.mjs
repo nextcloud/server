5c0dd789ea370ccd5e6464c8d2a390ae2f493492
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-09-17 13:23:17 +0200

    fix(files_external): show that an unrestricted storage applies to all accounts
    Backport of #64103. The cherry-pick dropped appliesToAllAccounts because
    stable34 has no pruneUnusedAuthMechanismOptions to merge the import with,
    so the dialog called a function that was never added.
    
    Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

---
+++ dist/logger-C6O_UhM3.chunk.mjs
@@ -0,0 +1,2 @@
+import{g as t}from"./index-BRcsAfAF.chunk.mjs";const o=t().setApp("files_external").build();export{o as l};
+//# sourceMappingURL=logger-C6O_UhM3.chunk.mjs.map
--- dist/logger-COyHjIsS.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{g as e}from"./index-Cp0kcsps.chunk.mjs";const o=e().setApp("federatedfilesharing").build();export{o as l};
-//# sourceMappingURL=logger-COyHjIsS.chunk.mjs.map
