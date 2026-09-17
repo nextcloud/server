5c61b204532c92c37532276969f8be0425318b74
Josh <josh.t.richards@gmail.com>
2026-09-16 23:38:19 -0400

    chore(files_external): revert persisting priority
    Will do in its own PR.
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- apps/files_external/lib/Service/StoragesService.php
+++ apps/files_external/lib/Service/StoragesService.php
@@ -414,10 +414,6 @@ abstract class StoragesService {
 			$this->dbConfig->setAuthBackend($id, $updatedStorage->getAuthMechanism()->getIdentifier());
 		}
 
-		if ($updatedStorage->getPriority() !== $oldStorage->getPriority()) {
-			$this->dbConfig->setPriority($id, $updatedStorage->getPriority());
-		}
-
 		$this->triggerChangeHooks($oldStorage, $updatedStorage);
 
 		$this->updateOverwriteHomeFolders();
