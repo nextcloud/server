ed15b3fbba49c2f0769b68f5ce6305fa29e3c42f
Robin Appelman <robin@icewind.nl>
2026-09-19 19:09:06 +0200

    chore: drop DropFederatedInvitesTable
    the table name is now used by contacts, so the migration would end up removing the table that it added
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/cloud_federation_api/composer/composer/autoload_static.php
+++ apps/cloud_federation_api/composer/composer/autoload_static.php
@@ -31,7 +31,6 @@ class ComposerStaticInitCloudFederationAPI
         'OCA\\CloudFederationAPI\\Db\\OcmTokenMap' => __DIR__ . '/..' . '/../lib/Db/OcmTokenMap.php',
         'OCA\\CloudFederationAPI\\Db\\OcmTokenMapMapper' => __DIR__ . '/..' . '/../lib/Db/OcmTokenMapMapper.php',
         'OCA\\CloudFederationAPI\\Listener\\ShareDeletedListener' => __DIR__ . '/..' . '/../lib/Listener/ShareDeletedListener.php',
-        'OCA\\CloudFederationAPI\\Migration\\DropFederatedInvitesTable' => __DIR__ . '/..' . '/../lib/Migration/DropFederatedInvitesTable.php',
         'OCA\\CloudFederationAPI\\Migration\\Version1016Date202502262004' => __DIR__ . '/..' . '/../lib/Migration/Version1016Date202502262004.php',
         'OCA\\CloudFederationAPI\\Migration\\Version1017Date20260306120000' => __DIR__ . '/..' . '/../lib/Migration/Version1017Date20260306120000.php',
         'OCA\\CloudFederationAPI\\ResponseDefinitions' => __DIR__ . '/..' . '/../lib/ResponseDefinitions.php',
