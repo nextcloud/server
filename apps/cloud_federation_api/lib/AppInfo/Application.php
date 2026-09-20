080383afe0d7e5e960f4dcb02acefd1fde61212d
Joas Schilling <coding@schilljs.com>
2026-09-20 18:15:04 +0200

    fix: Finish cleanup
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/cloud_federation_api/lib/AppInfo/Application.php
+++ apps/cloud_federation_api/lib/AppInfo/Application.php
@@ -9,12 +9,10 @@ declare(strict_types=1);
 
 namespace OCA\CloudFederationAPI\AppInfo;
 
-use OCA\CloudFederationAPI\Listener\ShareDeletedListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\Share\Events\ShareDeletedEvent;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'cloud_federation_api';
