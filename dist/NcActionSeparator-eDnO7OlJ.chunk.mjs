d76605258cc632a045549baf3f64ec07a474bcff
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-26 10:56:13 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- dist/NcActionSeparator-eDnO7OlJ.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./Web-D6RX_wec.chunk.mjs";import{o as a,i as t}from"./preload-helper-0vEJIPm_.chunk.mjs";const r={name:"NcActionSeparator"},e={class:"action action-separator action--disabled",role:"separator"};function n(s,c,i,p,d,l){return a(),t("li",e)}const u=o(r,[["render",n],["__scopeId","data-v-3e2324b7"]]);export{u as N};
-//# sourceMappingURL=NcActionSeparator-eDnO7OlJ.chunk.mjs.map
